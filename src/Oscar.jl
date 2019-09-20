a03aed2e6cbd47e962f90535136a05536af74084
William Hart <goodwillhart@googlemail.com>
2019-09-20 15:10:03 +0200

    Initial Oscar repository.

---
+++ src/Oscar.jl
@@ -0,0 +1,15 @@
+module Oscar
+
+#=
+  We currently only import packages which:
+    * are registered
+    * have BinaryBuilder (including for dependencies)
+    * support Linux and OSX
+  More to come soon....
+=#
+import AbstractAlgebra
+import Nemo
+import Hecke
+
+
+end # module
