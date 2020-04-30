c5bf3a59c81920305714cdf16a7c997ddda237ab
Claus Fieker <fieker@mathematik.uni-kl.de>
2020-04-30 07:41:51 +0200

    fix Build to use proper paths

--- system/Build.jl
+++ system/Build.jl
@@ -4,15 +4,17 @@ Pkg.add("Libdl")
 
 using PackageCompiler, Libdl
 
-write("/tmp/CompileOscar.jl", """
+tmp = mktempdir(cleanup = false)
+CO = joinpath(tmp, "CompileOscar.jl")
+
+write(CO, """
 using Oscar
 using Pkg, Test
 Oscar.system("precompile.jl")
 """)
 
-sysimage="/tmp/Oscar.$(Libdl.dlext)"
-#PackageCompiler.create_sysimage([:Oscar], sysimage_path=sysimage, precompile_execution_file="/tmp/CompileOscar.jl")
-PackageCompiler.create_sysimage([:Oscar], sysimage_path=sysimage, precompile_statements_file="/tmp/Oscar")
+sysimage=joinpath(tmp, "Oscar.$(Libdl.dlext)")
+PackageCompiler.create_sysimage([:Oscar], sysimage_path=sysimage, precompile_execution_file=CO)
 
 println("(re)start julia as")
 println("\tjulia -J $(sysimage)")
