0ca504949b96a044b52ced67bc547453de948764
Claus Fieker <fieker@mathematik.uni-kl.de>
2020-06-05 17:56:33 +0200

    progress: half of the invar are there - and working

--- examples/GaloisGrp.jl
+++ examples/GaloisGrp.jl
@@ -158,6 +158,11 @@ end
 
 # TODO: add lots of more orbit related stuff
 
+function block_system(G::PermGroup, B::Vector{Int})
+  orb = GAP.Globals.Orbit(G.X, GAP.julia_to_gap(B), GAP.Globals.OnSets)
+  GAP.gap_to_julia(Vector{Vector{Int}}, orb)
+end
+
 # given a perm group G and a block B, compute a homomorphism into Sym(B^G)
 function action_on_blocks(G::PermGroup, B::Vector{Int})
   orb = GAP.Globals.Orbit(G.X, GAP.julia_to_gap(seed), GAP.Globals.OnSets)
@@ -256,6 +261,89 @@ function galois_group(f::fmpz_poly, p::Int = 31)
   return G, GC
 end
 
+function elementary_symmetric(g::Array{<:Any, 1}, i::Int)
+  return sum(prod(g[i] for i = s) for s = Hecke.subsets(Set(1:length(g)), i))
+end
+
+Oscar.ngens(S::SLPolyRing) = length(gens(S))
+
+function isprobably_invariant(g, p)
+  R = parent(g)
+  k = GF(next_prime(2^20))
+  n = ngens(R)
+  lp = [rand(k) for i=1:n]
+  return evaluate(g, lp) == evaluate(g, [lp[p(i)] for i=1:n])
+end
+
+function invariant(G, H)
+  @show "invar", G, H
+
+  S = slpoly_ring(ZZ, degree(G))
+  g = gens(S)
+
+  if isprimitive(G) && isprimitive(H)
+    if isodd(G) && iseven(H)
+      return sqrt_disc(g)
+    end
+  end
+
+  bG = all_blocks(G)
+  bH = all_blocks(H)
+
+  @show d = setdiff(bH, bG)
+  if length(d) > 0
+    @show "F-invar"
+    return prod(sum(g[b]) for b = block_system(H, d[1]))
+  else
+    for BB = bG
+      B = block_system(H, BB)
+      m = length(B)
+      l = length(B[1])
+      y = [sum(g[b]) for b = B]
+      d = [sqrt_disc(g[b]) for b = B]
+      D = sqrt_disc(y)
+      I = D
+      if all(p->isprobably_invariant(I, p), gens(H)) &&
+         any(p->!isprobably_invariant(I, p), gens(G))
+        @show "D-invar"
+        return I
+      end
+      I = elementary_symmetric(d, 1)
+      if all(p->isprobably_invariant(I, p), gens(H)) &&
+         any(p->!isprobably_invariant(I, p), gens(G))
+        @show "s1-invar"
+        return I
+      end
+      I = elementary_symmetric(d, m)
+      if all(p->isprobably_invariant(I, p), gens(H)) &&
+         any(p->!isprobably_invariant(I, p), gens(G))
+        @show "sm-invar"
+        return I
+      end
+      I = elementary_symmetric(d, 2)
+      if all(p->isprobably_invariant(I, p), gens(H)) &&
+         any(p->!isprobably_invariant(I, p), gens(G))
+        @show "s2-invar"
+        return I
+      end
+      I = D*elementary_symmetric(d, m)
+      if all(p->isprobably_invariant(I, p), gens(H)) &&
+         any(p->!isprobably_invariant(I, p), gens(G))
+        @show "D sm-invar"
+        return I
+      end
+    end
+
+    for B = bG
+    end
+  end
+
+  R, _ = PolynomialRing(ZZ, degree(G))
+  m = prod(gen(R, i)^i for i=1:degree(G)-1)
+  I = sum(orbit(H, m))
+  return I
+end
+
 function galois_group(K::AnticNumberField)
   d_min = div(degree(K), 4)
   d_max = typemax(Int)
@@ -281,39 +369,198 @@ function galois_group(K::AnticNumberField)
     end
   end
 
+  p = p_best
+
   @show "using $p_best with degree $d_max"
   @show "cycle types ", ct
 
   GC = GaloisCtx(Hecke.Globals.Zx(K.pol), p_best)
+  c = roots(GC, 5)
 
   S = subfields(K)
   bs = Array{Array{Int, 1}, 1}[]
-  c = roots(GC, 10)
   for (s, ms) = S
     if degree(s) == degree(K) || degree(s) == 1
       continue
     end
+    pr = 5
+    local v
+    while true
+      c = roots(GC, pr)
 
-    g = ms(gen(s))
-    d = map(parent(K.pol)(g), c)
-    b = Dict{typeof(c[1]), Array{Int, 1}}()
-    for i=1:length(c)
-      if Base.haskey(b, d[i])
-        push!(b[d[i]], i)
+      g = ms(gen(s))
+      d = map(parent(K.pol)(g), c)
+      b = Dict{typeof(c[1]), Array{Int, 1}}()
+      for i=1:length(c)
+        if Base.haskey(b, d[i])
+          push!(b[d[i]], i)
+        else
+          b[d[i]] = Int[i]
+        end
+      end
+      v = collect(values(b))
+      if any(x->length(x) != div(degree(K), degree(s)), v)
+        pr *= 2
+        if pr > 100
+          error("too bad")
+        end
       else
-        b[d[i]] = Int[i]
+        break
       end
     end
-    push!(bs, collect(values(b)))
+    sort!(v, lt = (x,y) -> minimum(x) < minimum(y))
+    push!(bs, v)
   end
 
   @show "Group needs to have block systems: ", bs
 
+  #selecting maximals only...
+  if length(bs) > 1
+    L = POSet(bs, (x,y) -> issubset(x[1], y[1]) || issubset(y[1], x[1]), 
+                  (x,y) -> issubset(x[1], y[1]) - issubset(y[1], x[1]))
+    bs = minimal_elements(L)              
+  end
+
   d = map(frobenius, c)
   si = [findfirst(y->y==x, c) for x = d]
-
   @show "Group needs ", si
-  return bs, si
+
+  G = symmetric_group(degree(K))
+  S = G
+  for b = bs
+    W = wreath_product(symmetric_group(length(b[1])), symmetric_group(length(b)))
+    s = S(vcat(b...))
+    G = intersection(G, W^s)[1]
+  end
+
+  @show "starting with ", G
+  nG = length(G)
+  while true
+    S = maximal_subgroup_reps(G)
+    for s = S
+      istransitive(s) || continue
+      @show GAP.Globals.TransitiveIdentification(G.X),
+            GAP.Globals.TransitiveIdentification(s.X)
+
+      I = invariant(G, s)
+
+      B = bound(GC, I)
+      @show value(B)
+      c = roots(GC, clog(2*value(B), p)+5)
+      local fd
+      cnt = 0
+      while true
+        cs = Set{typeof(c[1])}()
+        fd = []
+        for t = right_transversal(G, s)
+          e = evaluate(I, [c[inv(t)(i)] for i=1:degree(K)])
+          if e in cs
+            @show "darn, need tschirni", e, cs
+            local ts
+            while true
+              @show ts = rand(Hecke.Globals.Zx, 2:degree(K), -2:2)
+              if degree(ts) > 1
+                break
+              end
+            end
+            cnt += 1
+            if cnt > 5
+              error("bad")
+            end
+            B = bound(GC, I, ts)
+            @show value(B)
+            c = roots(GC, clog(2*value(B), p)+5)
+            c = map(ts, c)
+            break
+          end
+          push!(cs, e)
+          if e.length<2
+            l = coeff(e, 0)
+            lz = lift(l)
+            l = Hecke.mod_sym(lz, fmpz(p)^precision(l))
+            if abs(l) < value(B)
+              println(t, " => ", l)
+              push!(fd, t)
+            end
+          end
+        end
+        @show length(cs), index(G, s)
+        if length(cs) == index(G, s)
+          break
+        end
+      end
+      @show "descent via", fd, s
+      if length(fd)>0
+        G = intersection([s^inv(x) for x = fd]...)[1]
+        break
+      end
+    end
+    if length(G) == nG
+      return G
+    else
+      nG = length(G)
+    end
+  end
+  return bs, si, G
+end
+
+mutable struct POSet
+  elem::Array{Any, 1}
+  cmp::Function
+  can_cmp::Function
+
+  function POSet(elem::Array{<:Any, 1}, can_cmp::Function, cmp::Function)
+    r = new()
+    r.elem = elem
+    r.cmp = cmp
+    r.can_cmp = can_cmp
+    return r
+  end
+end
+
+struct POSetElem
+  e::Int
+  p::POSet
+  function POSetElem(L::POSet, i::Int)
+    return new(i, p)
+  end
+  function POSetElem(L::POSet, e::Any)
+    return new(findfirst(x->L>can_cmp(e, L.elem[i]) && L.cmp(e, L.elem[i]) == 0, 1:length(L.elem)), L)
+  end
+end
+
+function maximal_elements(L::POSet)
+  d = Dict{typeof(L.elem[1]), Array{Int, 1}}()
+  for i = 1:length(L.elem)
+    e = L.elem[i]
+    d[e] = Int[]
+    for j=1:length(L.elem)
+      if i == j
+        continue
+      end
+      if L.can_cmp(e, L.elem[j]) && L.cmp(e, L.elem[j]) > 0
+        push!(d[e], j)
+      end
+    end
+  end
+  return [x for (x,v) = d if length(v) == 0]
+end
+
+function minimal_elements(L::POSet)
+  d = Dict{typeof(L.elem[1]), Array{Int, 1}}()
+  for i = 1:length(L.elem)
+    e = L.elem[i]
+    d[e] = Int[]
+    for j=1:length(L.elem)
+      if i == j
+        continue
+      end
+      if L.can_cmp(e, L.elem[j]) && (L.cmp(e, L.elem[j]) < 0)
+        push!(d[e], j)
+      end
+    end
+  end
+  return [x for (x,v) = d if length(v) == 0]
 end
 
 end
