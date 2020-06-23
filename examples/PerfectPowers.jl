52cd7f161498aa476098492a7cb7f56ee2e79b6e
Claus Fieker <fieker@mathematik.uni-kl.de>
2020-06-23 09:51:12 +0200

    fast perfect power detection using p-adic lifting

--- examples/PerfectPowers.jl
+++ examples/PerfectPowers.jl
@@ -1,96 +1,55 @@
 module PerfectPowers
 
 using Oscar
+import Oscar: Nemo, Hecke
 
-import Oscar: Nemo
-
-mutable struct fmpz_limbs
-  d::Int
-  s::Int
-  l::Ptr{UInt}
-  
-  function fmpz_limbs(a::fmpz)
-    r = new()
-    r.d = a.d
-    if Nemo._fmpz_is_small(a)
-      r.s = -1
-      return r
-    end
-    r.s = Nemo.__fmpz_limbs(a.d)
-    @assert r.s > 0 #sign and length
-    r.l = unsafe_load(convert(Ptr{Ptr{UInt}}, unsigned(a.d) << 2) + 2*sizeof(Cint))
-    return r
-  end
-end
-
-function Base.iterate(a::fmpz_limbs, i::Int = 0)
-  if a.s == -1
-    i == 0 && return UInt(a.d), 1
-    return nothing
-  end
-  if i< a.s
-    return unsafe_load(a.l, i+1), i+1
-  end
-  return nothing
-end
-
-Base.length(a::fmpz_limbs) = a.s < 0 ? 1 : a.s
-
-function ispower_exact(a::fmpz, p::Int)
+function root_exact(a::fmpz, p::Val{2})
   #return the p-th root, or garbage of the correct size...
 
   @assert isodd(a)
 
-  if iseven(p)
-    fl, a = Hecke.ispower(a, p)
-    return fl ? a : fmpz(1)
-  end
   n = nbits(a)
-  s = div(n, p)+1 # bits in p-th root
-  w = div(s, 64)+1 #words in p-th root
+  s = div(n, 2)+1 # bits in p-th root
 
-  A = fmpz_limbs(a)
-  d = iterate(A)[1]
+  A = Hecke.Limbs(a, MSW=false)
+  d = A[1]
+  if d % 8 != 1
+    return fmpz(1)
+  end
   dd = d
 
-  l = invmod(p, UInt(2^32))
-  l = l*(2-l*p) # inv mod 2^64 - Julia cannot do this directly...
-
-  b = d^(p-1)
-  for i=2:7
-    d = (1+l)*d-l*b*d^(p+1)
+  b = d
+  for i=1:7
+    d = d*(1+div(1-b*d*d, 2))
   end
   if s < 64
-    return d*dd
+    d *= dd
+    nbits(d) <= s && return fmpz(d)
+    return fmpz(1)
   end
 
-  B = powmod(a, p-1, fmpz(2)^(64*w+1))
-  L = fmpz(l)
+  B = mod(a, fmpz(2)^(s+3))
   D = fmpz(d)
 
   M = fmpz(2)^(64)
-  i = 1
-  two = fmpz(2)
+  i = 64 
+  one = fmpz(1)
   #TODO; better chain of exponents, use mpn? more inline?
   #TODO: better mod 2^n, use mullow?
-  while i < w
+  while i < s
     Nemo.mul!(M, M, M)
-    T = L*p
-    Nemo.sub!(T, two, T)
-    Nemo.mul!(L, L, T)
-    Hecke.mod!(L, L, M)
+    ccall((:fmpz_fdiv_q_2exp, Nemo.libflint), Nothing, (Ref{fmpz}, Ref{fmpz}, Int), M, M, 2)
 
-    T = powmod(D, p+1, M)
+    T = powmod(D, 2, M)
     Nemo.mul!(T, T, B)
     Hecke.mod!(T, T, M)
+    Nemo.sub!(T, one, T)
+    ccall((:fmpz_fdiv_q_2exp, Nemo.libflint), Nothing, (Ref{fmpz}, Ref{fmpz}, Int), T, T, 1)
+    Nemo.add!(T, one, T)
+    Nemo.mul!(D, D, T)
+    Hecke.mod!(D, D, M)
 
-    Nemo.mul!(T, T, L)
-    Hecke.mod!(T, T, M)
-    S = 1+L
-    Nemo.mul!(S, S, D)
-    Hecke.mod!(S, S, M)
-    Nemo.sub!(D, S, T)
-    i *= 2
+    i = 2*i-2
   end
 
   Nemo.mul!(D, D, a)
@@ -102,30 +61,214 @@ function ispower_exact(a::fmpz, p::Int)
   return D
 end
 
+@inline function fmpz_trunc!(a::fmpz, i::Int)
+#  @assert a >= 0
+  ccall((:fmpz_fdiv_r_2exp, Nemo.libflint), Cvoid, (Ref{fmpz}, Ref{fmpz}, Clong), a, a, i)
+end
+
+function powmod_2exp(a::fmpz, p::Int, i::Int) #too slow - much worde than 
+                                              #powmod directly
+  @assert a > 0 && p > 0
+  a = copy(a)
+  while iseven(p)
+    Nemo.mul!(a, a, a)
+    fmpz_trunc!(a, i)
+    p >>= 1
+  end
+  b = a
+  p >>= 1
+  while p>0
+    while iseven(p)
+      Nemo.mul!(a, a, a)
+      fmpz_trunc!(a, i)
+      p >>= 1
+    end
+    Nemo.mul!(a, a, a)
+    fmpz_trunc!(a, i)
+    Nemo.mul!(b, b, a)
+    fmpz_trunc!(b, i)
+    p >>= 1
+  end
+
+  return b
+end
+
+function root_exact(a::fmpz, p::Int, extra_s::Int = 5, extra_w::Int = 1)
+  #return the p-th root, or garbage of the correct size...
+
+  @assert isodd(a)
+
+  p==2 && return root_exact(a, Val(2))
+
+  if iseven(p)
+    @show "slow", p
+    fl, a = Hecke.ispower(a, p)
+    return fl ? a : fmpz(1)
+  end
+  n = nbits(a)
+  s = div(n, p)+1 # bits in p-th root
+  w = div(s, 64)+1 #words in p-th root
+ 
+  A = Hecke.Limbs(a)
+  d = A[1]
+  dd = d
+
+  l = invmod(p, UInt(2^32))
+  l = l*(2-l*p) # inv mod 2^64 - Julia cannot do this directly...
+
+  b = d^(p-1)
+  pr = 1
+  for i=2:7
+    d = (1+l)*d-l*b*d^(p+1)
+    pr *= 2
+    if pr > s + extra_s
+      break
+    end
+  end
+  if s + extra_s < 64
+    d *= dd
+    d = d % 2^min(pr, 63)
+    iseven(d) && return fmpz(1)
+    nbits(d) <= s && return fmpz(d)
+    return fmpz(d)
+  end
+
+#  B = powmod_2exp(a, p-1, 64*w+1)
+  B = powmod(a, p-1, fmpz(2)^(64*(w+extra_w)))
+  L = fmpz(l)
+  D = fmpz(d)
+
+  M = fmpz(2)^(64)
+  i = 1
+  two = fmpz(2)
+  #TODO; better chain of exponents, use mpn? more inline?
+  #TODO: better mod 2^n (done), use mullow?
+  #TODO: find powmod for 2^n modulus
+  while i < w + extra_w
+    i *= 2
+    Nemo.mul!(M, M, M)
+    T = L*p
+    Nemo.sub!(T, two, T)
+    Nemo.add!(T, T, M)  #make pos
+    Nemo.mul!(L, L, T)
+    _x = L % M
+    fmpz_trunc!(L, i*64)
+    @assert _x == L
+#    Hecke.mod!(L, L, M)
+
+#    T = powmod_2exp(D, p+1, i*64)
+    T = powmod(D, p+1, M)
+    Nemo.mul!(T, T, B)
+    _x = T % M
+    fmpz_trunc!(T, i*64)
+    @assert _x == T
+#    Hecke.mod!(T, T, M)
+
+    Nemo.mul!(T, T, L)
+    _x = T % M
+    fmpz_trunc!(T, i*64)
+    @assert _x == T
+
+    S = 1+L
+    Nemo.mul!(S, S, D)
+    _x = S % M
+    fmpz_trunc!(S, i*64)
+    @assert S == _x
+#    Hecke.mod!(S, S, M)
+    Nemo.sub!(D, S, T)
+    if sign(D) < 0
+      Nemo.add!(D, D, M)
+    end
+  end
+
+  Nemo.mul!(D, D, a)
+  fmpz_trunc!(D, (w+extra_w)*64)
+#  Hecke.mod!(D, D, M)
+  if nbits(D) > s
+    return fmpz(1)
+  end
+
+  return D
+end
+
 function ispower_bernstein(a::fmpz)
 #https://cr.yp.to/lineartime/powers2-20060914-ams.pdf
   k, a = remove(a, fmpz(2))
+  l, a = remove(a, fmpz(3))
+  k = gcd(l, k)
+  l, a = remove(a, fmpz(5))
+  k = gcd(l, k)
+
+  p_test = next_prime(2^40)
+  a_test = a % p_test
 
   c = [a]
-  cl = clog(a, 2)
+  cl = clog(a, 7)
   if k>0
     cl = min(cl, k)
   end
-  for p = PrimesSet(2, cl)
+  no_p = 0
+  no_s = 0
+
+  f = 1
+
+  @time for p = PrimesSet(2, cl)
     if k % p != 0
       continue
     end
+    if p > cl
+      break
+    end
+    if p>2 && p < 2^15
+      aa = UInt(a % p^2)
+      if aa %p != 0 && powmod(aa, p-1, UInt(p^2)) != 1
+        no_s += 1
+        continue
+      end
+    end
+    no_p += 1
     pp = p
-    while pp <= cl
-      push!(c, ispower_exact(a, pp))
-      isone(c[end]) && break
+    d = root_exact(a, p, 10, 2)
+    if !isone(d)
+      if powmod(d, p, fmpz(p_test)) == a_test && d^p == a
+        f *= p
+        cl = min(cl, flog(d, 7))
+        a = d
+        a_test = a % p_test
+      else
+         d = fmpz(1)
+#        @show p, powmod(d, p, fmpz(p_test)) == a % p_test
+      end
+    end
+    while !isone(d) && pp <= cl
+      push!(c, d)
       pp *= p
+      d = root_exact(d, p)
+      if !isone(d)
+        if d^p== a
+          f *= p
+          cl = min(cl, flog(d, 7))
+          a = d
+          a_test = a % p_test
+        else
+          @show 1, p
+        end
+      end
     end
   end
-
-  c = coprime_base(c)
+  @show no_p, no_s
+#  @assert c[1] == a
+  
+  @show length(c), length(Set(c))
+  c = collect(Set(c))
+  @time c = [gcd(a, x) for x = c] #a should be a proper divisor of c[1]
+  c = [x for x = c if !isone(x)]
+  #not sure still neccessary
+  @time c = Hecke.coprime_base(c) #bernstein seems to be broken
+  @show length(c), length(Set(c))
   k = [valuation(a, p) for p = c]
-  return gcd(k)
+  @show no_p
+  return gcd(k)*f
 end
 
 export ispower_exact
