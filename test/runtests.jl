406e68bbd56b74f7dee559c0a5c5501f02497cd7
Claus Fieker <fieker@mathematik.uni-kl.de>
2020-09-03 16:45:14 +0200

    actually swwitch the new tests on

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,6 @@ using Test
 include("Rings/integer-test.jl")
 include("Rings/rational-test.jl")
 include("Rings/mpoly-test.jl")
+include("Rings/mpoly-graded-test.jl")
 include("Polymake/nmbthy-test.jl")
 include("Groups/runtests.jl")
