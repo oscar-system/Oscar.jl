e50b34e22eab773eac5b9ba5bfe0601c72aa6224
Rafael Fourquet <fourquet.rafael@gmail.com>
2020-11-10 19:58:16 +0100

    include docs from AbstractAlgebra

--- docs/make.jl
+++ docs/make.jl
@@ -1,18 +1,78 @@
 using Documenter, Oscar, DocumenterMarkdown
+using AbstractAlgebra
+
+AA_dir = pkgdir(AbstractAlgebra)
+Oscar_dir = pkgdir(Oscar)
+
+cp(joinpath(AA_dir, "docs", "src"), joinpath(Oscar_dir, "docs", "src", "AAsrc"), force=true)
+
+# TODO: load this directly from AbstractAlgebra.jl
+AA_pages = ["index.md",
+           "constructors.md",
+           "Rings" => [ "rings.md",
+                        "ncrings.md",
+                        "euclidean.md",
+                        "integer.md",
+                        "polynomial_rings.md",
+                        "polynomial.md",
+                        "ncpolynomial.md",
+                        "mpolynomial_rings.md",
+                        "mpolynomial.md",
+                        "series_rings.md",
+                        "series.md",
+                        "puiseux.md",
+                        "residue_rings.md",
+                        "residue.md"],
+           "Fields" => [ "fields.md",
+                         "fraction_fields.md",
+                         "fraction.md",
+                         "rational.md",
+                         "finfield.md",
+                         "real.md",
+                         "numberfield.md"],
+           "Groups" => [ "perm.md",
+                         "ytabs.md"],
+           "Modules" => [ "module.md",
+                          "free_module.md",
+                          "submodule.md",
+                          "quotient_module.md",
+                          "direct_sum.md",
+                          "module_homomorphism.md"],
+           "Matrices" => [ "matrix_spaces.md",
+                           "matrix.md",
+                           "matrix_algebras.md"],
+           "Maps" => [ "map.md",
+                       "functional_map.md",
+                       "map_cache.md",
+                       "map_with_inverse.md"],
+           "types.md" # Appendix A
+           ]
+
+# add "AAsrc" prefix to AA files
+replace!(AA_pages) do s
+    if s isa String
+        "AAsrc/" * s
+    else
+        replace!(s.second) do t
+            "AAsrc/" * t
+        end
+        s
+    end
+end
 
 makedocs(
          format   = Documenter.HTML(),
 #         format   = Markdown(),
          sitename = "Oscar.jl",
-         modules = [Oscar],
+         modules = [Oscar, AbstractAlgebra],
          clean = true,
          doctest = false,
-         pages    = [
-             "index.md",
-             "Rings" => [ "Rings/integer.md",
-			  "Rings/rational.md"],
-             "Groups" => [ "Groups/groups.md" ]
-         ]
+         pages = ["index.md",
+                  "Rings" => [ "Rings/integer.md",
+			       "Rings/rational.md"],
+                  "Groups" => [ "Groups/groups.md" ],
+                  "AbstractAlgebra.jl" => AA_pages
+                  ]
 )
 
 #deploydocs(
