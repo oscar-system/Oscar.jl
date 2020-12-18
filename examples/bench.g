b5586660c6329d4c7d65738226685394bef4b16c
delphinepol <pol@mathematik.uni-kl.de>
2020-12-18 10:41:10 +0100

    Test for Plane Curves Added tests for plane curves. Fixed minor bugs.

---
+++ examples/bench.g
@@ -0,0 +1,140 @@
+# Benchmark( func[, optrec] )
+#
+# func - a function taking no arguments
+# optrec - an optional record with various options
+#
+# Measures how long executing the given function "func" takes.
+# In order to improve accuracy, it invokes the function repeatedly.
+# Before each repetition, the garbage collector is run, and
+# (unless turned off by an option) the random number generators
+# are reset.
+# At the end, it outputs the average, median, and std deviation.
+#
+# Example:
+# gap> Benchmark(function() Factors(x^293-1); end);
+# .................................................
+# Performed 49 iterations, taking 201 milliseconds.
+# average: 4.1 +/- 0.11 (+/- 3%)
+# median: 4.06396484375
+# rec( avg := 4.09581, counter := 49, median := 4.06396, std := 0.109638, total := 200.695, var := 0.0120205 )
+#
+#
+# The following options are currently implemented:
+#
+#  minreps:  the minimal number of repetitions (default: 5);
+#            the function will be executed at least that often,\
+#            unless some other condition (maxreps exceeded, maxtime exceeded)
+#            aborts the benchmark early.
+#  mintime:  the minimal number of milliseconds that has to pass before
+#            benchmarking ends (default: 200)
+#            benchmarking will not stop before this time has passed,
+#            unless some other condition (maxreps exceeded, maxtime exceeded)
+#            aborts the benchmark early.
+#  maxreps:  the maximal number of repetitions (default: infinity)
+#            once this is reached, benchmarking stops immediately.
+#  maxtime:  the minimal number of milliseconds before benchmarking ends (default: 200)
+#            once this is reached, benchmarking stops immediately.
+#  showSummary:
+#  showProgress:
+#  resetRandom:  whether to reset the random number generators before each repetition (default: true)
+#
+#
+# TODO: allow passing a function that is executed before every test run.
+# That function can reset other state, flush caches etc.
+#
+Benchmark := function(func, arg...)
+    local opt, getTime, timings, total, t, i, res;
+    
+    opt := rec(
+                minreps := 5,
+                maxreps := infinity,
+                silent := false,
+                resetRandom := true,
+                mintime := 200,
+                maxtime := infinity,
+                showProgress := true,
+                showSummary := true,
+           );
+    
+    if Length(arg) = 1 and IsRecord(arg[1]) then
+        for i in RecNames(arg[1]) do
+            opt.(i) := arg[1].(i);
+        od;
+    elif Length(arg) <> 0 then
+        Error("Usage: Benchmark( func[, optrec] )");
+    fi;
+
+    # force mintime and maxtime to be floats
+    opt.mintime := Float(opt.mintime);
+    opt.maxtime := Float(opt.maxtime);
+
+    # if available, use NanosecondsSinceEpoch
+    if IsBound(NanosecondsSinceEpoch) then
+        getTime := function() return NanosecondsSinceEpoch()/1000000.; end;
+    else
+        getTime := Runtime;
+    fi;
+
+    timings := [];
+    total := 0.0;
+    i := 0;
+    # repeat at most opt.maxreps times, and at least opt.minreps
+    # times, resp. at least till opt.mintime milliseconds passed
+    while i < opt.maxreps and (opt.maxtime = infinity or total < opt.maxtime) and
+        (i < opt.minreps or total < opt.mintime) do
+        i := i + 1;
+
+        if opt.resetRandom then
+            Reset(GlobalMersenneTwister);
+            Reset(GlobalRandomSource);
+        fi;
+        GASMAN("collect");
+
+        t := getTime();
+        func();
+        t := getTime() - t;
+
+        total := total + t;
+        Add(timings, t);
+        if opt.showProgress then
+            #Print(".\c");
+            Print(['\r', "|/-\\"[1+(i mod 4)],'\c']);
+        fi;
+    od;
+    if opt.showProgress then
+        #Print("\n");
+        Print("\r   \c\r");
+    fi;
+
+    Sort(timings);
+
+    res := rec();
+    #res.timings := timings;
+    res.total := total;
+    res.counter := i;
+    res.avg := Sum(timings) * 1.0 / res.counter;
+    res.var := Sum(timings, t -> (t-res.avg)^2) / res.counter;
+    res.std := Sqrt(res.var);
+    if IsOddInt(res.counter) then
+        res.median := timings[(res.counter+1)/2];
+    else
+        res.median := (timings[(res.counter)/2] + timings[(res.counter+2)/2]) / 2.;
+    fi;
+
+    if opt.showSummary then
+        Print("Performed ", res.counter, " iterations, taking ", Round(res.total), " milliseconds.\n");
+        #Print("timings: ", timings, "\n");
+        Print("average: ", Round(100*res.avg)/100,
+            " +/- ", Round(100*res.std)/100,
+            " (+/- ", Round(100*res.std/res.avg), "%)",
+            "\n");
+        Print("median: ", res.median, "\n");
+    fi;
+
+    return res;
+end;
+
+if false then
+Benchmark(function() Factors(x^293-1); end);
+Benchmark(function() Factors(x^293-1); end, rec(maxreps:=10));
+fi;
---
+++ test/Examples/PlaneCurve-test.jl
@@ -0,0 +1,238 @@
+Oscar.example("PlaneCurve.jl")
+
+@testset "AffinePlaneCurve constructors" begin
+    R, (x,y) = PolynomialRing(QQ, ["x", "y"])
+    F = y^3*x^6 - y^6*x^2
+    C = AffinePlaneCurve(F)
+
+    @test C.eq == F
+    @test C.dimension == 1
+
+    degree(C)
+    @test C.degree == 9
+
+    curve_components(C)
+    @test C.components ==  Dict{AffinePlaneCurve{fmpq}, Int64}(AffinePlaneCurve(x) => 2, AffinePlaneCurve(y) => 3, AffinePlaneCurve(x^4 - y^3) => 1)
+
+    @test C == AffinePlaneCurve(2*F)
+end
+
+@testset "AffinePlaneCurve reducible functions" begin
+    R, (x,y) = PolynomialRing(QQ, ["x", "y"])
+    F = AffinePlaneCurve((x^2+y^2))
+    P = Point([QQ(0), QQ(0)])
+
+    @test isirreducible(F)
+    @test isreduced(F)
+    @test reduction(F) == F
+
+    G = AffinePlaneCurve(y^2)
+    @test !isirreducible(G)
+    @test !isreduced(G)
+    @test reduction(G) == AffinePlaneCurve(y)
+
+    H = AffinePlaneCurve(x*y)
+
+    @test !isirreducible(H)
+    @test isreduced(H)
+    @test reduction(H) == H
+
+    @test union(G, H) == AffinePlaneCurve(x*y^3)
+end
+
+@testset "AffinePlaneCurve intersection functions" begin
+    R, (x,y) = PolynomialRing(QQ, ["x", "y"])
+    F = AffinePlaneCurve(x*(x+y))
+    G = AffinePlaneCurve(x+y^2+1)
+    H = AffinePlaneCurve(x*(x+y)*y)
+    M = AffinePlaneCurve((x-y)*(x-2))
+
+    P = Point([QQ(0), QQ(0)])
+    Q = Point([QQ(2), QQ(-2)])
+
+    @test common_components(F, G) == []
+    @test common_components(F, H) == [AffinePlaneCurve(x*(x+y))]
+
+    @test curve_intersect(F, G) == [[], []]
+    @test curve_intersect(F, H) == [[AffinePlaneCurve(x*(x+y))], []]
+    @test curve_intersect(F, M) == [[], [P, Q]] || curve_intersect(F, M) == [[], [Q, P]]
+
+    @test !aretransverse(F, H, P)
+    @test !aretransverse(F, G, P)
+    @test aretransverse(F, M, Q)
+end
+
+@testset "AffinePlaneCurve int_multiplicity functions" begin
+    R, (x,y) = PolynomialRing(QQ, ["x", "y"])
+    F = AffinePlaneCurve((x^2+y^2)*(x^2 + y^2 + 2*y))
+    G = AffinePlaneCurve((x^2+y^2)*(y^3*x^6 - y^6*x^2))
+    L = curve_intersect(F, G)
+    P = Point([QQ(0), QQ(0)])
+    Q = Point([QQ(0), QQ(-2)])
+
+    @test L == [[AffinePlaneCurve(x^2+y^2)], [P, Q]] || L == [[AffinePlaneCurve(x^2+y^2)], [Q, P]]
+    @test intersection_multiplicity(F, G, Q) == 2
+    @test intersection_multiplicity(F, G, P) == -1
+end
+
+@testset "AffinePlaneCurve singularity functions" begin
+    R, (x,y) = PolynomialRing(QQ, ["x", "y"])
+
+    F = AffinePlaneCurve(x+y^2)
+
+    @test curve_singular_locus(F) == [[], []]
+    @test issmooth_curve(F)
+
+    H = AffinePlaneCurve(x*y*(x+y))
+    @test !issmooth_curve(H)
+
+    G = AffinePlaneCurve(x^2*(x+y)*(y^3-x^2))
+    S = curve_singular_locus(G)
+    P1 = Point([QQ(0), QQ(0)])
+    P2 = Point([QQ(-1), QQ(1)])
+    P3 = Point([QQ(2), QQ(-2)])
+    P4 = Point([QQ(1), QQ(2)])
+
+    @test S == [[AffinePlaneCurve(x)], [P1, P2]] || S == [[AffinePlaneCurve(x)], [P2, P1]]
+    @test !issmooth(G, P1)
+    @test issmooth(G, P3)
+
+    @test tangent(G, P3) == AffinePlaneCurve(x+y)
+    @test tangent_lines(G, P1) == Dict{AffinePlaneCurve{fmpq}, Int64}(AffinePlaneCurve(x) => 4, AffinePlaneCurve(x+y) => 1)
+
+    @test multiplicity(G, P1) == 5
+    @test multiplicity(G, P3) == 1
+    @test multiplicity(G, P4) == 0
+end
+
+@testset "ProjPlaneCurve constructors" begin
+    R, (x,y,z) = PolynomialRing(QQ, ["x", "y", "z"])
+    T = grade(R)
+	F = T(y^3*x^6 - y^6*x^2*z)
+    C = ProjPlaneCurve(F)
+
+    @test C.eq == F
+    @test C.dimension == 1
+
+    degree(C)
+    @test C.degree == 9
+
+    curve_components(C)
+    @test C.components ==  Dict{ProjPlaneCurve{fmpq}, Int64}(ProjPlaneCurve(T(x)) => 2, ProjPlaneCurve(T(y)) => 3, ProjPlaneCurve(T(x^4 - y^3*z)) => 1)
+
+    @test C == ProjPlaneCurve(2*F)
+end
+
+@testset "ProjPlaneCurve reducible functions" begin
+    R, (x,y,z) = PolynomialRing(QQ, ["x", "y", "z"])
+    T = grade(R)
+    F = ProjPlaneCurve(T(x^2+y^2))
+    P = Point([QQ(0), QQ(0), QQ(1)])
+
+    @test isirreducible(F)
+    @test isreduced(F)
+    @test reduction(F) == F
+
+    G = ProjPlaneCurve(T(y^2))
+    @test !isirreducible(G)
+    @test !isreduced(G)
+    @test reduction(G) == ProjPlaneCurve(T(y))
+
+    H = ProjPlaneCurve(T(x*y))
+
+    @test !isirreducible(H)
+    @test isreduced(H)
+    @test reduction(H) == H
+
+    @test union(G, H) == ProjPlaneCurve(T(x*y^3))
+end
+
+@testset "ProjPlaneCurve intersection functions" begin
+    R, (x,y,z) = PolynomialRing(QQ, ["x", "y", "z"])
+    T = grade(R)
+    F = ProjPlaneCurve(T(x*(x+y)))
+    G = ProjPlaneCurve(T(x*z+y^2+z^2))
+    H = ProjPlaneCurve(T(x*(x+y)*y))
+    M = ProjPlaneCurve((x-y)*(x-2*z))
+	PP = projective_space(QQ, 2)
+	
+    P = Oscar.Geometry.ProjSpcElem(PP[1], [QQ(0), QQ(0), QQ(1)])
+    Q = Oscar.Geometry.ProjSpcElem(PP[1], [QQ(2), QQ(-2), QQ(1)])
+	S = Oscar.Geometry.ProjSpcElem(PP[1], [QQ(0), QQ(1), QQ(0)])
+	Z = Oscar.Geometry.ProjSpcElem(PP[1], [QQ(1), QQ(-1), QQ(0)])
+
+    @test common_components(F, G) == []
+    @test common_components(F, H) == [ProjPlaneCurve(T(x*(x+y)))]
+
+    @test curve_intersect(PP[1], F, G) == [[], []]
+    @test curve_intersect(PP[1], F, H) == [[ProjPlaneCurve(T(x*(x+y)))], []]
+    @test curve_intersect(PP[1], ProjPlaneCurve(T(x+y+z)), ProjPlaneCurve(T(z))) == [[], [Z]]
+	
+	L = curve_intersect(PP[1], F, M)
+	
+	@test L[1] == []
+	@test length(L[2]) == 3
+	@test length(findall(x->x==P, L[2])) == 1
+	@test length(findall(x->x==Q, L[2])) == 1
+	@test length(findall(x->x==S, L[2])) == 1
+
+    @test !aretransverse(F, H, P)
+    @test !aretransverse(F, G, P)
+    @test aretransverse(F, M, Q)
+end
+
+@testset "ProjPlaneCurve int_multiplicity functions" begin
+    R, (x,y,z) = PolynomialRing(QQ, ["x", "y", "z"])
+    T = grade(R)
+	F = ProjPlaneCurve(T((x^2+y^2)*(x^2 + y^2 + 2*y*z)))
+    G = ProjPlaneCurve(T((x^2+y^2)*(y^3*x^6 - y^6*x^2*z)))
+	PP = projective_space(QQ, 2)
+	
+    L = curve_intersect(PP[1], F, G)
+    P = Oscar.Geometry.ProjSpcElem(PP[1], [QQ(0), QQ(0), QQ(1)])
+    Q = Oscar.Geometry.ProjSpcElem(PP[1], [QQ(0), QQ(-2), QQ(1)])
+
+    @test L[1] == [ProjPlaneCurve(T(x^2+y^2))]
+	@test length(L[2]) == 2
+	@test length(findall(x->x==P, L[2])) == 1
+	@test length(findall(x->x==Q, L[2])) == 1
+
+    @test intersection_multiplicity(F, G, Q) == 2
+    @test intersection_multiplicity(F, G, P) == -1
+end
+
+@testset "ProjPlaneCurve singularity functions" begin
+    R, (x,y,z) = PolynomialRing(QQ, ["x", "y", "z"])
+    T = grade(R)
+    PP = projective_space(QQ, 2)
+    F = ProjPlaneCurve(T(x*z+y^2))
+    P = Oscar.Geometry.ProjSpcElem(PP[1], [QQ(1), QQ(0), QQ(0)])
+
+    @test curve_singular_locus(F) == [[], []]
+    @test issmooth_curve(F)
+    @test multiplicity(F, P) == 1
+
+    H = ProjPlaneCurve(T(x*y*(x+y)))
+    @test !issmooth_curve(H)
+
+    G = ProjPlaneCurve(x^2*(x+y)*(y^3-x^2*z))
+    S = curve_singular_locus(G)
+    P1 = Oscar.Geometry.ProjSpcElem(PP[1], [QQ(0), QQ(0), QQ(1)])
+    P2 = Oscar.Geometry.ProjSpcElem(PP[1], [QQ(-1), QQ(1), QQ(1)])
+    P3 = Oscar.Geometry.ProjSpcElem(PP[1], [QQ(2), QQ(-2), QQ(1)])
+    P4 = Oscar.Geometry.ProjSpcElem(PP[1], [QQ(1), QQ(2), QQ(1)])
+
+    @test S[1] == [ProjPlaneCurve(T(x))]
+	@test length(S[2]) == 2
+	@test length(findall(x->x==P1, S[2])) == 1
+	@test length(findall(x->x==P2, S[2])) == 1
+    @test !issmooth(G, P1)
+    @test issmooth(G, P3)
+
+    @test tangent(G, P3) == ProjPlaneCurve(T(x+y))
+    @test tangent_lines(G, P1) == Dict{ProjPlaneCurve{fmpq}, Int64}(ProjPlaneCurve(T(x)) => 4, ProjPlaneCurve(T(x+y)) => 1)
+
+    @test multiplicity(G, P1) == 5
+    @test multiplicity(G, P3) == 1
+    @test multiplicity(G, P4) == 0
+end
