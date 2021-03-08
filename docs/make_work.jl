ccc5bbfc3763f3d8d336ee83f5869b3d857eabf4
Max Horn <max@quendi.de>
2021-03-09 00:01:56 +0100

    Don't deploy docs twice in CI, don't deploy locally

--- docs/make_work.jl
+++ docs/make_work.jl
@@ -227,22 +227,3 @@ makedocs(bib,
              "References" => "references.md",
          ]
 )
-
-#deploydocs(
-#   julia = "1.3",
-#   repo   = "github.com/oscar-system/Oscar.jl.git",
-#   target = "build",
-#   deps = nothing,
-#   make   = nothing,
-#   osname = "linux"
-#)
-
-deploydocs(
-   repo   = "github.com/oscar-system/Oscar.jl.git",
-#  deps = Deps.pip("pymdown-extensions", "pygments", "mkdocs", "python-markdown-math", "mkdocs-material", "mkdocs-cinder"),
-   deps = nothing,
-   target = "build",
-   push_preview = true,
-#  make = () -> run(`mkdocs build`),
-   make = nothing
-)
