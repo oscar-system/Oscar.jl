08adf467d54cf989396e2924563d3e86752b7fe0
William Hart <goodwillhart@googlemail.com>
2021-03-18 09:13:46 +0100

    Replace lt -> leading_term

--- src/Modules/FreeModules-graded.jl
+++ src/Modules/FreeModules-graded.jl
@@ -210,7 +210,7 @@ function degree(a::FreeModuleElem_dec)
         error("elem not homogenous")
       end
     else
-      if W.lt(ww, w) 
+      if W.leading_term(ww, w) 
         ww = w
       end
     end
@@ -454,7 +454,7 @@ function degree(h::T) where {T <: Map_dec}
     end
     dd = degree(hi) - degree(i)
     if isfiltered(R)
-      if R.lt(d, dd)
+      if R.leading_term(d, dd)
         d = dd
       end
     else
