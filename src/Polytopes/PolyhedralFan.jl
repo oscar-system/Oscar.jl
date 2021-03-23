2f4b4907b35b525a9aec0f056eef277744bd9d8f
Lars Kastner <lars.kastner@yahoo.dk>
2021-03-23 15:01:33 +0100

    Improve documentation

--- src/Polytopes/PolyhedralFan.jl
+++ src/Polytopes/PolyhedralFan.jl
@@ -7,8 +7,10 @@
 @doc Markdown.doc"""
    PolyhedralFan(Rays, Cones)
 
-    A polyhedral fan formed from the Rays by taking the cones corresponding to
-    the indices of the sets in Cones.
+A polyhedral fan formed from rays and cones made of these rays. The cones are
+given as an IncidenceMatrix, where the columns represent the rays and the rows
+represent the cones. There is a 1 at position (i,j) if cone i has ray j as
+extremal ray, otherwise there is a 0.
 """
 struct PolyhedralFan
    pm_fan::Polymake.BigObject
@@ -16,11 +18,26 @@ struct PolyhedralFan
       return new(pm)
    end
 end
+function PolyhedralFan(Rays::Union{Oscar.MatElem,AbstractMatrix}, Incidence::IncidenceMatrix)
+   arr = @Polymake.convert_to Array{Set{Int}} Polymake.common.rows(Incidence.pm_incidencematrix)
+   PolyhedralFan(Polymake.fan.PolyhedralFan{Polymake.Rational}(
+      INPUT_RAYS = matrix_for_polymake(Rays),
+      INPUT_CONES = arr,
+   ))
+end
+function PolyhedralFan(Rays::Union{Oscar.MatElem,AbstractMatrix}, LS::Union{Oscar.MatElem,AbstractMatrix}, Incidence::IncidenceMatrix)
+   arr = @Polymake.convert_to Array{Set{Int}} Polymake.common.rows(Incidence.pm_incidencematrix)
+   PolyhedralFan(Polymake.fan.PolyhedralFan{Polymake.Rational}(
+      INPUT_RAYS = matrix_for_polymake(Rays),
+      INPUT_LINEALITY = matrix_for_polymake(LS),
+      INPUT_CONES = arr,
+   ))
+end
 
 """
    pm_fan(PF::PolyhedralFan)
 
-Get the underlying polymake BigObject.
+Get the underlying polymake object, which can be used via Polymake.jl.
 """
 pm_fan(PF::PolyhedralFan) = PF.pm_fan
 
@@ -34,30 +51,7 @@ function PolyhedralFan(itr)
    PolyhedralFan(Polymake.fan.check_fan_objects(BigObjectArray))
 end
 
-#=
-function PolyhedralFan(Rays::Union{Oscar.MatElem,AbstractMatrix}, cones::AbstractArray{AbstractArray{Int64}})
-   PolyhedralFan(Polymake.fan.PolyhedralFan{Polymake.Rational}(
-      INPUT_RAYS = matrix_for_polymake(Rays),
-      INPUT_CONES = [[x-1 for x in cone] for cone in cones],
-   ))
-end
-=#
-function PolyhedralFan(Rays::Union{Oscar.MatElem,AbstractMatrix}, LS::Union{Oscar.MatElem,AbstractMatrix}, Incidence::IncidenceMatrix)
-   arr = @Polymake.convert_to Array{Set{Int}} Polymake.common.rows(Incidence.pm_incidencematrix)
-   PolyhedralFan(Polymake.fan.PolyhedralFan{Polymake.Rational}(
-      INPUT_RAYS = matrix_for_polymake(Rays),
-      INPUT_LINEALITY = matrix_for_polymake(LS),
-      INPUT_CONES = arr,
-   ))
-end
 
-function PolyhedralFan(Rays::Union{Oscar.MatElem,AbstractMatrix}, Incidence::IncidenceMatrix)
-   arr = @Polymake.convert_to Array{Set{Int}} Polymake.common.rows(Incidence.pm_incidencematrix)
-   PolyhedralFan(Polymake.fan.PolyhedralFan{Polymake.Rational}(
-      INPUT_RAYS = matrix_for_polymake(Rays),
-      INPUT_CONES = arr,
-   ))
-end
 
 #Same construction for when the user gives Array{Bool,2} as incidence matrix
 function PolyhedralFan(Rays::Union{Oscar.MatElem,AbstractMatrix}, LS::Union{Oscar.MatElem,AbstractMatrix}, Incidence::Array{Bool,2})
@@ -163,7 +157,9 @@ nmaximal_cones(PF::PolyhedralFan) = pm_fan(PF).N_MAXIMAL_CONES
 """
    ambient_dim(PF::PolyhedralFan)
 
-Returns the ambient dimension of a polyhedral fan.
+Returns the ambient dimension of a polyhedral fan, which is the dimension of
+the embedding space. This is equal to the dimension of the fan if the fan is
+full-dimensional.
 """
 ambient_dim(PF::PolyhedralFan) = pm_fan(PF).FAN_AMBIENT_DIM
 
