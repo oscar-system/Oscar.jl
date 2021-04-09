d1a64ff34ae115f198a57eeb8f256318cad9fa10
Tommy Hofmann <thofma@gmail.com>
2021-04-09 12:35:00 +0200

    Fix #403 by using Base.Integer instead of Oscar.integer

--- src/Rings/MPolyQuo.jl
+++ src/Rings/MPolyQuo.jl
@@ -203,7 +203,7 @@ end
 -(a::MPolyQuoElem, b::MPolyQuoElem) = check_parent(a, b) && MPolyQuoElem(a.f-b.f, a.P)
 -(a::MPolyQuoElem) = MPolyQuoElem(-a.f, a.P)
 *(a::MPolyQuoElem, b::MPolyQuoElem) = check_parent(a, b) && MPolyQuoElem(a.f*b.f, a.P)
-^(a::MPolyQuoElem, b::Integer) = MPolyQuoElem(Base.power_by_squaring(a.f, b), a.P)
+^(a::MPolyQuoElem, b::Base.Integer) = MPolyQuoElem(Base.power_by_squaring(a.f, b), a.P)
 
 function Oscar.mul!(a::MPolyQuoElem, b::MPolyQuoElem, c::MPolyQuoElem)
   a.f = b.f*c.f
