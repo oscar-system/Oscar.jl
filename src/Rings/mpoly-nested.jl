616d50d67afeb5409d0df7f5d76c0e3e3f99db3a
tthsqe12 <tthsqe12@gmail.com>
2021-04-30 11:48:42 +0200

    Nestings (#421)
    * avoid isa
    
    * rename mpoly-parametric -> mpoly-nested
    
    * add denest and renest
    
    * add nested gcd and factor
    
    * add cached=false
    
    * ensure AA version is not running

---
+++ src/Rings/mpoly-nested.jl
@@ -0,0 +1,299 @@
+##############################################################################
+#
+# Various special cases for nested rings. Currently:
+#  1. frac(R[t1, t2, ...])[x1, x2, ...]
+#  2. R[z1, ...]...[y1, ...][x1, ...] and univariate possibilities
+#
+##############################################################################
+
+export denest, renest
+
+##############################################################################
+#
+# 1. implementations for Q(t)[x]
+#
+##############################################################################
+
+# from Q(t1, t2)[x1, x2] to S = Q[x1, x2, t1, t2]
+# the product of the two outputs should be equal to the input
+function _remove_denominators(S::MPolyRing, f::MPolyElem)
+  R = parent(f)                   # Q(t1, t2)[x1, x2]
+  R2 = base_ring(base_ring(R))    # Q[t1, t2]
+  den = one(R2)
+  cont = zero(R2)
+  for (c1, e1) in zip(coefficients(f), exponent_vectors(f))
+    cont = gcd(cont, numerator(c1))
+    den = lcm(den, denominator(c1))
+  end
+  num = MPolyBuildCtx(S)
+  for (c1, e1) in zip(coefficients(f), exponent_vectors(f))
+    cf = divexact(den, denominator(c1))
+    if !iszero(cont)
+      cf *= divexact(numerator(c1), cont)
+    end
+    for (c2, e2) in zip(coefficients(cf), exponent_vectors(cf))
+      push_term!(num, c2, vcat(e1, e2))
+    end
+  end
+  return (finish(num), cont//den)
+end
+
+# from Q[x1, x2, t1, t2] to R = Q(t1, t2)[x1, x2]
+# the output is equal to the input
+function _restore_numerators(R::MPolyRing, g::MPolyElem)
+  S = parent(g)                   # Q[x1, x2, t1, t2]
+  R2 = base_ring(base_ring(R))    # Q[t1, t2]
+  n = nvars(R)
+  d = Dict{Vector{Int}, typeof(MPolyBuildCtx(R2))}()
+  for (c, e) in zip(coefficients(g), exponent_vectors(g))
+    e1 = e[1:n]
+    e2 = e[n+1:end]
+    if !haskey(d, e1)
+      d[e1] = MPolyBuildCtx(R2)
+    end
+    push_term!(d[e1], c, e2)
+  end
+  z = MPolyBuildCtx(R)
+  for (e, c) in d
+    push_term!(z, base_ring(R)(finish(c)), e)
+  end
+  return finish(z)
+end
+
+# if R2 is a singular ring, keep it that way since limited types can be factored
+function _add_variables(R2::Singular.PolyRing, n::Int)
+  n += nvars(R2)
+  x = map(i->"x"*string(i), 1:n)
+  return Singular.PolynomialRing(base_ring(R2), x, cached=false)[1]
+end
+
+function _add_variables(R2::Union{MPolyRing, PolyRing}, n::Int)
+  n += nvars(R2)
+  x = map(i->"x"*string(i), 1:n)
+  return PolynomialRing(base_ring(R2), x, cached=false)[1]
+end
+
+
+function Oscar.gcd(
+  a::AbstractAlgebra.Generic.MPoly{AbstractAlgebra.Generic.Frac{T}},
+  b::AbstractAlgebra.Generic.MPoly{AbstractAlgebra.Generic.Frac{T}}
+) where T <: MPolyElem
+
+  R = parent(a)                   # Q(t1, t2)[x1, x2]
+  R2 = base_ring(base_ring(R))    # Q[t1, t2]
+  R == parent(b) || error("parents do not match")
+  S = _add_variables(R2, nvars(R))
+  A = _remove_denominators(S, a)[1]
+  B = _remove_denominators(S, b)[1]
+  return _restore_numerators(R, gcd(A, B))    # not nec monic
+end
+
+
+function _convert_frac_fac(R, u, fac)
+  Rfac = Fac{elem_type(R)}()
+  Rfac.unit = R(u)
+  for (f, e) in fac
+    t = _restore_numerators(R, f)
+    if isconstant(t)
+      Rfac.unit *= t^e
+    else
+      Rfac[t] = e
+    end
+  end
+  return Rfac
+end
+
+function Oscar.factor(
+  a::AbstractAlgebra.Generic.MPoly{AbstractAlgebra.Generic.Frac{T}}
+) where T <: MPolyElem
+
+  R = parent(a)                   # Q(t1, t2)[x1, x2]
+  R2 = base_ring(base_ring(R))    # Q[t1, t2]
+  S = _add_variables(R2, nvars(R))
+  A, u = _remove_denominators(S, a)
+  return _convert_frac_fac(R, u, factor(A))
+end
+
+function Oscar.factor_squarefree(
+  a::AbstractAlgebra.Generic.MPoly{AbstractAlgebra.Generic.Frac{T}}
+) where T <: MPolyElem
+
+  R = parent(a)                   # Q(t1, t2)[x1, x2]
+  R2 = base_ring(base_ring(R))    # Q[t1, t2]
+  S = _add_variables(R2, nvars(R))
+  A, u = _remove_denominators(S, a)
+  return _convert_frac_fac(R, u, factor_squarefree(A))
+end
+
+##############################################################################
+#
+# 2. implementations for Q[z][y][x] ect
+#
+##############################################################################
+
+# denest for poly ring
+
+function _denest_recursive(BR::Union{MPolyRing, PolyRing},
+                           R::Union{MPolyRing, PolyRing}, n::Int)
+  return _denest_recursive(base_ring(BR), BR, n + nvars(R))
+end
+
+function _denest_recursive(BR, R::Union{MPolyRing, PolyRing}, n::Int)
+  return R, n
+end
+
+@doc Markdown.doc"""
+    denest(R::Union{PolyRing, MPolyRing})
+
+Returns a multivariate polynomial ring resulting from denesting an interated
+polynomial ring `R`.
+"""
+function denest(R::Union{PolyRing, MPolyRing})
+  R2, n = _denest_recursive(base_ring(R), R, 0)
+  return _add_variables(R2, n)
+end
+
+# denest for poly elem
+
+function _denest_recursive(r::MPolyBuildCtx, f::PolyElem{T},
+                           e0::Vector{Int}) where T <: Union{PolyElem, MPolyElem}
+  for i in degree(f):-1:0
+    _denest_recursive(r, coeff(f, i), vcat(e0, [i]))
+  end
+end
+
+function _denest_recursive(r::MPolyBuildCtx, f::PolyElem, e0::Vector{Int})
+  for i in degree(f):-1:0
+    push_term!(r, coeff(f, i), vcat(e0, [i]))
+  end
+end
+
+function _denest_recursive(r::MPolyBuildCtx, f::MPolyElem{T},
+                           e0::Vector{Int}) where T <: Union{PolyElem, MPolyElem}
+  for (c1, e1) in zip(coefficients(f), exponent_vectors(f))
+    _denest_recursive(r, c1, vcat(e0, e1))
+  end
+end
+
+function _denest_recursive(r::MPolyBuildCtx, f::MPolyElem, e0::Vector{Int})
+  for (c1, e1) in zip(coefficients(f), exponent_vectors(f))
+    push_term!(r, c1, vcat(e0, e1))
+  end
+end
+
+@doc Markdown.doc"""
+    denest(S::MPolyRing, f::Union{PolyElem, MPolyElem})
+
+Returns an element of `S` resulting from denesting a element `f` of an
+interated polynomial ring. The ring `S` should have the same base ring and
+number of variables as `denest(parent(f))`.
+"""
+function denest(S::MPolyRing, f::Union{PolyElem, MPolyElem})
+  r = MPolyBuildCtx(S)
+  _denest_recursive(r, f, Int[])
+  return finish(r)
+end
+
+# renest for poly
+
+function _renest_recursive_coeff(R::Union{MPolyRing{T}, PolyRing{T}},
+                                 off::Int, idxs::Vector{Int},
+                                 gcoeffs, gexps) where T <: Union{PolyElem, MPolyElem}
+  return _renest_recursive(base_ring(R), off, idxs, gcoeffs, gexps)
+end
+
+function _renest_recursive_coeff(R::Union{MPolyRing, PolyRing}, off::Int,
+                                 idxs::Vector{Int}, gcoeffs, gexps)
+  @assert length(idxs) == 1
+  return gcoeffs[idxs[1]]
+end
+
+function _renest_recursive(R::PolyRing, off::Int, idxs::Vector{Int}, gcoeffs, gexps)
+  d = Dict{Int, Vector{Int}}()   # exp => indices
+  for i in idxs
+    e1 = gexps[i][off]
+    if !haskey(d, e1)
+      d[e1] = [i]
+    else
+      append!(d[e1], i)
+    end
+  end
+  # TODO once PolyElem has a better required constructor
+  z = zero(R)
+  for (e1, ii) in d
+    z += _renest_recursive_coeff(R, off + 1, ii, gcoeffs, gexps)*gen(R)^e1
+  end
+  return z
+end
+
+function _renest_recursive(R::MPolyRing, off::Int, idxs::Vector{Int}, gcoeffs, gexps)
+  n = nvars(R)
+  d = Dict{Vector{Int}, Vector{Int}}()  # exp => indices
+  for i in idxs
+    e1 = gexps[i][off:off+n-1]
+    if !haskey(d, e1)
+      d[e1] = [i]
+    else
+      append!(d[e1], i)
+    end
+  end
+  z = MPolyBuildCtx(R)
+  for (e1, ii) in d
+    push_term!(z, _renest_recursive_coeff(R, off + n, ii, gcoeffs, gexps), e1)
+  end
+  return finish(z)
+end
+
+@doc Markdown.doc"""
+    renest(R::Union{PolyRing, MPolyRing}, g::MPolyElem)
+
+Returns an element of iterated polynomial ring `R` from its denested
+counterpart. The return satisfies `f == renest(R, denest(denest(R), f))`.
+"""
+function renest(R::Union{PolyRing, MPolyRing}, g::MPolyElem)
+  gcoeffs = collect(coefficients(g))
+  gexps = collect(exponent_vectors(g))
+  return _renest_recursive(R, 1, collect(1:length(gcoeffs)), gcoeffs, gexps)
+end
+
+function _nested_gcd(a, b)
+  R = parent(a)
+  R == parent(b) || error("parents do not match")
+  S = denest(R)
+  return renest(R, gcd(denest(S, a), denest(S, b)))
+end
+
+function Oscar.gcd(
+   a::AbstractAlgebra.Generic.Poly{T},
+   b::AbstractAlgebra.Generic.Poly{T}
+) where T <: Union{PolyElem, MPolyElem}
+   return _nested_gcd(a, b)
+end
+
+function Oscar.gcd(
+   a::AbstractAlgebra.Generic.MPoly{T},
+   b::AbstractAlgebra.Generic.MPoly{T}
+) where T <: Union{PolyElem, MPolyElem}
+   return _nested_gcd(a, b)
+end
+
+function _convert_iter_fac(R, fac::Fac)
+  Rfac = Fac{elem_type(R)}()
+  Rfac.unit = renest(R, fac.unit)
+  for (f, e) in fac
+    Rfac[renest(R, f)] = e
+  end
+  return Rfac
+end
+
+function Oscar.factor(a::Union{PolyElem{T}, MPolyElem{T}}) where
+                                                T <: Union{PolyElem, MPolyElem}
+  A = denest(denest(parent(a)), a)
+  return _convert_iter_fac(parent(a), factor(A))
+end
+
+function Oscar.factor_squarefree(a::Union{PolyElem{T}, MPolyElem{T}}) where
+                                                T <: Union{PolyElem, MPolyElem}
+  A = denest(denest(parent(a)), a)
+  return _convert_iter_fac(parent(a), factor_squarefree(A))
+end
---
+++ test/Rings/mpoly-nested-test.jl
@@ -0,0 +1,161 @@
+begin
+
+local check_gcd, check_factor
+
+function check_gcd(a, b, gdiv)
+  g = gcd(a, b)
+  if iszero(g)
+    @test iszero(a)
+    @test iszero(b)
+    return
+  end
+  @test iszero(gdiv) || divides(g, gdiv)[1]
+  a = divexact(a, g)
+  b = divexact(b, g)
+  @test isunit(gcd(a, b))
+end
+
+function check_factor(a, esum)
+  f = factor(a)
+
+  @test isunit(unit(f))
+  @test a == unit(f) * prod([p^e for (p, e) in f])
+  @test esum == sum(e for (p, e) in f)
+
+  f = factor_squarefree(a)
+
+  @test isunit(unit(f))
+  @test a == unit(f) * prod([p^e for (p, e) in f])
+end
+
+
+@testset "mpoly-nested.frac.gcd" begin
+  r, (t1, t2, t3) = PolynomialRing(QQ, ["t1", "t2", "t3"])
+  r = FractionField(r)
+  (t1, t2, t3) = map(r, (t1, t2, t3))
+  r, (x1, x2, x3, x4) = PolynomialRing(r, ["x1", "x2", "x3", "x4"])
+
+  check_gcd(zero(r), zero(r), zero(r))
+  check_gcd(zero(r), x1+t1, x1+t1)
+  check_gcd(x2+t2, zero(r), x2+t2)
+  check_gcd(zero(r), zero(r), zero(r))
+
+  g = (t2*x1+t1//(t2+t3)*x2+t3+x4*t1*t2*t3)*x1
+  a = (x3*t2//t1+t3)*x2
+  b = (x1+x3*x2*t1//(t2*t3))*x3
+  check_gcd(g*a, g*b, g)
+end
+
+@testset "mpoly-nested.frac.factor" begin
+  for (PR, Q) in ((PolynomialRing, QQ),
+                  (PolynomialRing, GF(101)),
+                  #(Singular.PolynomialRing, Singular.QQ) TODO enable after bug fixes
+                 )
+
+    r, (t1, t2, t3) = PR(Q, ["t1", "t2", "t3"])
+    r = FractionField(r)
+    (t1, t2, t3) = map(r, (t1, t2, t3))
+    r, (x1, x2, x3, x4) = PolynomialRing(r, ["x1", "x2", "x3", "x4"])
+
+    check_factor((t1+t2*t3)*(x1*1//t2^2+x2*1//t2+x3*1//t3)*
+                 (x1^2*t1+x2^2*t1//t2+x3^2*t1//t3), 2)
+
+    check_factor(1//(t1+t2*t3)*(x1*1//t2^2+x2*1//t2+x3*1//t3+x4)^3*
+                 (x1^2*t1+x2^2*t1//t2+x4*x3^2*t1//t3)^2, 5)
+  end
+end
+
+@testset "mpoly-nested.iterated.conversion" begin
+  R = QQ
+  R, x1 = PolynomialRing(R, "x1")
+  p = (x1 + 2)^2
+  @test p == renest(R, denest(denest(R), p))
+
+  R = QQ
+  R, (x1, x2) = PolynomialRing(R, ["x1", "x2"])
+  p = (x1*x2 + x1 + x2^2)^2
+  @test p == renest(R, denest(denest(R), p))
+
+  R = QQ
+  R, x3 = PolynomialRing(R, "x3")
+  R, x2 = PolynomialRing(R, "x2")
+  R, x1 = PolynomialRing(R, "x1")
+  p = (x1*x2*x3 + x1 + x2^2 + x3^3)^2
+  @test p == renest(R, denest(denest(R), p))
+
+  R = QQ
+  R, (x3, x4) = PolynomialRing(R, ["x3", "x4"])
+  R, x2 = PolynomialRing(R, "x2")
+  R, x1 = PolynomialRing(R, "x1")
+  p = (x1*x2*x3*x4 + x1 + x2^2 + x3^3 + x4^4)^2
+  @test p == renest(R, denest(denest(R), p))
+
+  R = QQ
+  R, x4 = PolynomialRing(R, "x4")
+  R, (x2, x3) = PolynomialRing(R, ["x2", "x3"])
+  R, x1 = PolynomialRing(R, "x1")
+  p = (x1*x2*x3*x4 + x1 + x2^2 + x3^3 + x4^4)^2
+  @test p == renest(R, denest(denest(R), p))
+
+  R = QQ
+  R, x4 = PolynomialRing(R, "x4")
+  R, x3 = PolynomialRing(R, "x3")
+  R, (x1, x2) = PolynomialRing(R, ["x1", "x2"])
+  p = (x1*x2*x3*x4 + x1 + x2^2 + x3^3 + x4^4)^2
+  @test p == renest(R, denest(denest(R), p))
+
+  R = QQ
+  R, x5 = PolynomialRing(R, "x5")
+  R, (x3, x4) = PolynomialRing(R, ["x3", "x4"])
+  R, (x1, x2) = PolynomialRing(R, ["x1", "x2"])
+  p = (x1*x2*x3*x4*x5 + x1 + x2^2 + x3^3 + x4^4 + x5^5)^2
+  @test p == renest(R, denest(denest(R), p))
+
+  R = QQ
+  R, (x4, x5) = PolynomialRing(R, ["x5", "x6"])
+  R, x3 = PolynomialRing(R, "x4")
+  R, (x1, x2) = PolynomialRing(R, ["x1", "x2"])
+  p = (x1*x2*x3*x4*x5 + x1 + x2^2 + x3^3 + x4^4 + x5^5)^2
+  @test p == renest(R, denest(denest(R), p))
+
+  R = QQ
+  R, (x4, x5) = PolynomialRing(R, ["x4", "x5"])
+  R, (x2, x3) = PolynomialRing(R, ["x2", "x3"])
+  R, x1 = PolynomialRing(R, "x1")
+  p = (x1*x2*x3*x4*x5 + x1 + x2^2 + x3^3 + x4^4 + x5^5)^2
+  @test p == renest(R, denest(denest(R), p))
+
+  R = QQ
+  R, (x5, x6) = PolynomialRing(R, ["x5", "x6"])
+  R, (x3, x4) = PolynomialRing(R, ["x3", "x4"])
+  R, (x1, x2) = PolynomialRing(R, ["x1", "x2"])
+  p = (x1*x2*x3*x4*x5*x6 + x1 + x2^2 + x3^3 + x4^4 + x5^5 + x6^6)^2
+  @test p == renest(R, denest(denest(R), p))
+end
+
+@testset "mpoly-nested.iterated.gcd_factor" begin
+  r3 = PolynomialRing(QQ, "x3")[1]
+  r2 = PolynomialRing(r3, "x3")[1]
+
+  R1 = PolynomialRing(r2, "x3")[1]
+  xs1 = [gen(R1), R1(gen(r2)), R1(gen(r3))]
+
+  R2 = PolynomialRing(r3, ["x1", "x2"])[1]
+  xs2 = [gen(R2, 1), gen(R2, 2), R2(gen(r3))]
+
+  for ((x1, x2, x3), R) in ((xs1, R1), (xs2, R2))
+    check_gcd(zero(R), zero(R), zero(R))
+    check_gcd(zero(R), x2+x3, x2+x3)
+    check_gcd(x1+x2, zero(R), x1+x2)
+    check_gcd(zero(R), zero(R), zero(R))
+
+    g = (x1 + 2*x2 + 3*x3)
+    a = (x1^2 + 4*x2^2 + 5*x3^2)
+    b = (x1^3 + 6*x2^3 + 7*x3^3)
+    check_gcd(g*a, g*b, g)
+
+    check_factor(g^2*a*b, 4)
+  end
+end
+
+end
