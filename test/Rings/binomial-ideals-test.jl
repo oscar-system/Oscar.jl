fc4d15743c7129e658710a1554c4acb915a9be7e
CarloSircana <sircana@mathematik.uni-kl.de>
2021-06-17 10:17:33 +0200

    typo

--- test/Rings/binomial-ideals-test.jl
+++ test/Rings/binomial-ideals-test.jl
@@ -35,7 +35,7 @@
       @test x in lI2
     end
     R, x = PolynomialRing(QQ, "x"=>1:3)
-    I = ideal(R, [x[3]^2*(x[1]^2-x[2]^2), x[3](x[1]^4-x[2]^4), x[3]^3])
+    I = ideal(R, [x[3]^2*(x[1]^2-x[2]^2), x[3]*(x[1]^4-x[2]^4), x[3]^3])
     ap = cellular_minimal_associated_primes(I)
     @test length(ap) == 1
     RQab = base_ring(ap[1])
