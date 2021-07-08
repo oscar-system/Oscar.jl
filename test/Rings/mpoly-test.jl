b845c7152f37b9b1426f6186190790ad997d6c07
Daniel Schultz <tthsqe12@gmail.com>
2021-07-08 16:06:44 +0200

    use MPolyHom_vars for mpoly embed_names

--- test/Rings/mpoly-test.jl
+++ test/Rings/mpoly-test.jl
@@ -107,6 +107,45 @@ end
   @test preimage(h, ideal(R, [I2, I3])) == ideal(S, [b, c])
 end
 
+@testset "Embedding names" begin
+
+  @test_throws Exception embed_names(QQ["x", "y"][1], QQ["x", "y", "x"][1])
+
+  D, (x1, z1, y1) = PolynomialRing(QQ, ["x", "z", "y"])
+  R, (y2, x2, t2, z2) = PolynomialRing(QQ, ["y", "x", "t", "z"])
+  m = embed_names(D, R)
+  @test domain(m) == D
+  @test codomain(m) == R
+  @test m(D(1)) == R(1)
+  @test m(x1) == x2
+  @test m(y1) == y2
+  @test m(z1) == z2
+  @test m(2 + x1 + y1^2//3 + z1^3) == 2 + x2 + y2^2//3 + z2^3
+#  @test m(ideal(D, [x1, y1^2, z1^3])) == ideal(R, [x2, y2^2, z2^3])
+  @test_throws Exception m(x2)
+
+  D, (x1, z1, y1) = PolynomialRing(QQ, ["x", "z", "y"])
+  R, (y2, x2) = PolynomialRing(GF(5), ["y", "x"])
+  m = embed_names(D, R)
+  @test m(D(1)) == R(1)
+  @test m(x1) == x2
+  @test m(y1) == y2
+  @test_throws Exception m(z1)
+  @test m(2 + x1 + y1^2//3) == 2 + x2 + y2^2//3
+  @test_throws Exception m(2 + x1 + y1^2//3 + z1*y1//2)
+#  @test_throws Exception m(ideal(D, [x1, y1^2, z1^3]))
+
+  D, (x1, y1) = PolynomialRing(QQ, ["x", "y"])
+  R, (y2, x2) = PolynomialRing(FractionField(QQ["t"][1]), ["y", "x"])
+  m = embed_names(D, R)
+  @test m(D(1)) == R(1)
+  @test m(x1) == x2
+  @test m(y1) == y2
+  @test m(x1 + y1^2//3 + x1^2*y1^3//2) == x2 + y2^2//3 + x2^2*y2^3//2
+#  @test m(ideal(D, [x1, y1^2])) == ideal(R, [x2, y2^2])
+
+end
+
 @testset "Ideal operations" begin
   R, (x, y) = PolynomialRing(QQ, ["x", "y"])
   f = x^2 + y^2
