2e9548dc9729b970b3262cfb927da84b4488fa65
Martin Bies <MBies87@googlemail.com>
2021-08-10 10:17:55 +0200

    Add first functionality

--- src/JToric.jl
+++ src/JToric.jl
@@ -1,5 +1,71 @@
 module JToric
 
-# Write your package code here.
+# load and export CapAndHomalg
+import CapAndHomalg
+export CapAndHomalg
+
+# use packages
+using Pkg
+using JToric
+
+"""
+Initializing function for 'JToric'.
+"""
+# initialization
+function __init__()
+    
+    # load necessary gap packages
+    CapAndHomalg.LoadPackage( "JConvex" )
+    CapAndHomalg.LoadPackage( "ToricV" )
+    
+    # inform that JToric has been loaded
+    print("Welome to JToric ")
+    printstyled("$version\n", color = :green)
+    println("Enjoy this software to perform computations on toric geometry!")
+    #println("Type: ?JToric for more information")
+    
+end
+
+"""
+    JToric.version
+
+The version number of the loaded `CapAndHomalg`.
+Please mention this number in any bug report.
+"""
+global version = 0
+
+if VERSION >= v"1.4"
+    deps = Pkg.dependencies()
+    if Base.haskey(deps, Base.UUID("9bfa4af0-0a41-40a3-86ae-7e8f6cc9e7ab"))
+        ver = Pkg.dependencies()[Base.UUID("9bfa4af0-0a41-40a3-86ae-7e8f6cc9e7ab")]
+        if occursin("/dev/", ver.source)
+            version = VersionNumber("$(ver.version)-dev")
+        else
+            version = VersionNumber("$(ver.version)")
+        end
+    else
+        version = "not installed"
+    end
+else
+    deps = Pkg.API.__installed(Pkg.PKGMODE_MANIFEST) #to also get installed dependencies
+    if haskey(deps, "JToric")
+        ver = deps["JToric"]
+        dir = dirname(@__DIR__)
+        if occursin("/dev/", dir)
+            version = VersionNumber("$(ver)-dev")
+        else
+            version = VersionNumber("$(ver)")
+        end
+    else
+        version = "not installed"
+    end
+end
+
+# export functions defined in JToric
+export ProjectiveSpace
+export IsSmooth
+
+# include code with function definitions
+include("ToricVarieties.jl")
 
 end
---
+++ src/ToricVarieties.jl
@@ -0,0 +1,12 @@
+# code that wraps projective space
+function ProjectiveSpace( x )
+    
+    return CapAndHomalg.GAP.Globals.ProjectiveSpace( x )
+    
+end
+
+function IsSmooth( x )
+    
+    return CapAndHomalg.GAP.Globals.IsSmooth( x )
+    
+end
