0af88547b673f86c9ddf56becdc9f7eeaa954db4
Martin Bies <MBies87@googlemail.com>
2021-08-11 12:24:56 +0200

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,26 @@
 using JToric
 using Test
+using GAP
+
+# load and export CapAndHomalg
+import CapAndHomalg
+export CapAndHomalg
 
 @testset "JToric.jl" begin
     # Write your tests here.
+
+    # load necessary gap packages
+    CapAndHomalg.LoadPackage( "JuliaInterface" )
+    CapAndHomalg.LoadPackage( "JConvex" )
+    CapAndHomalg.LoadPackage( "ToricV" )
+    
+    # Is P2 smooth?
+    P2 = JToric.ProjectiveSpace( 2 )
+    @test GAP.Globals.GAPToJulia( JToric.IsSmooth( P2 ) ) == true
+    
+    # Construct Hirzebruch surface
+    Rays = [[-1,5],[0,1],[1,0],[0,-1]]
+    Cones = [[1,2],[2,3],[3,4],[4,1]]
+    H5 = JToric.Fan( Rays, Cones )
+    
 end
