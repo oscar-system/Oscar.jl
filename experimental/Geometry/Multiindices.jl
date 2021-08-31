479098cac41acea5402763016ba230e1fe08ab28
HechtiDerLachs <85350711+HechtiDerLachs@users.noreply.github.com>
2021-08-31 14:11:00 +0200

    Update experimental/Geometry/Multiindices.jl
    Co-authored-by: Max Horn <max@quendi.de>

--- experimental/Geometry/Multiindices.jl
+++ experimental/Geometry/Multiindices.jl
@@ -26,8 +26,8 @@ end
 
 function Base.show( io::Base.TTY, i::OrderedMultiindex ) 
   outstr= "0"
-  for k in 1:length(i.index) 
-    outstr = outstr * " < " * string(i.index[k]) 
+  for k in i.index
+    outstr *= " < $k"
   end
   outstr = outstr * " <= " * string( i.n ) * "\n"
   #Base.show( io, "n = " * string(i.n) * "; p = " * string(i.p) * "; index = " * string( i.index ) )
