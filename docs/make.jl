a016eee35ef0e322897361e0054855cd5b3748fd
Max Horn <max@quendi.de>
2021-09-29 13:10:38 +0200

    Deploy the documentation to GitHub pages

--- docs/make.jl
+++ docs/make.jl
@@ -2,3 +2,11 @@ push!(LOAD_PATH,"../src/")
 using Documenter, JToric
 
 makedocs(sitename="JToric -- Toric Geometry in Julia")
+
+deploydocs(
+   repo   = "github.com/oscar-system/JToric.jl.git",
+   deps = nothing,
+   target = "build",
+   push_preview = true,
+   make = nothing
+)
