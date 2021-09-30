28849cf421fac640ea72d761ea83ffb205f9988c
Max Horn <max@quendi.de>
2021-09-30 11:53:33 +0200

    docs: don't push_preview, cleanup

--- docs/make.jl
+++ docs/make.jl
@@ -1,12 +1,9 @@
 push!(LOAD_PATH,"../src/")
 using Documenter, JToric
 
+# ensure JToric is loaded for the doc tests
+DocMeta.setdocmeta!(JToric, :DocTestSetup, :(using JToric, Random); recursive = true)
+
 makedocs(sitename="JToric -- Toric Geometry in Julia")
 
-deploydocs(
-   repo   = "github.com/oscar-system/JToric.jl.git",
-   deps = nothing,
-   target = "build",
-   push_preview = true,
-   make = nothing
-)
+deploydocs(repo = "github.com/oscar-system/JToric.jl.git")
