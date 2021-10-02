31fba7f28bf75c60a681436d2e60af74a067e33c
Max Horn <max@quendi.de>
2021-10-02 11:31:53 +0200

    Doctests use Oscar

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,7 @@ push!(LOAD_PATH,"../src/")
 using Documenter, JToric, DocumenterMarkdown
 
 # ensure JToric is loaded for the doc tests
-DocMeta.setdocmeta!(JToric, :DocTestSetup, :(using JToric, Random); recursive = true)
+DocMeta.setdocmeta!(JToric, :DocTestSetup, :(using JToric, Random; using JToric.Oscar); recursive = true)
 
 makedocs(sitename="JToric -- Toric Geometry in Julia")
 
--- test/doctest.jl
+++ test/doctest.jl
@@ -1,5 +1,5 @@
 using Documenter, JToric
 
-DocMeta.setdocmeta!(JToric, :DocTestSetup, :(using JToric, Random); recursive = true)
+DocMeta.setdocmeta!(JToric, :DocTestSetup, :(using JToric, Random; using JToric.Oscar); recursive = true)
 
 doctest(JToric)
