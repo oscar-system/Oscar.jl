6d081ee1971099e08513eb66ac7827cd9ec9deba
Lars Kastner <lars.kastner@yahoo.dk>
2021-10-08 21:11:19 +0200

    Polytopes: We collect all lattice points anyway

--- src/Polytopes/solving_integrally.jl
+++ src/Polytopes/solving_integrally.jl
@@ -8,6 +8,8 @@ Solve $Ax = b$ under $Cx >= d$, assumes a finite solution set.
 
 # Examples
 Find all $(x_1, x_2)\in\mathbb{Z}^2$ such that $x_1+x_2=7$, $x_1\ge 2$, and $x_2\ge 3$.
+The solutions are the rows of the output.
+Note that the output can be permuted, hence we sort it.
 ```jldoctest
 julia> A = fmpz_mat([1 1]);
 
@@ -17,30 +19,19 @@ julia> C = fmpz_mat([1 0; 0 1]);
 
 julia> d = zero_matrix(FlintZZ,2,1); d[1,1]=2; d[2,1]=3;
 
-julia> solve_mixed(A,b,C,d)
-[3   4]
-[2   5]
-[4   3]
+julia> sortslices(Matrix{BigInt}(solve_mixed(A, b, C, d)), dims=1)
+3×2 Matrix{BigInt}:
+ 2  5
+ 3  4
+ 4  3
 ```
 """
 function solve_mixed(A::fmpz_mat, b::fmpz_mat, C::fmpz_mat, d::fmpz_mat)
     eq = HalfspaceIterator(Matrix{BigInt}(A), vec(Matrix{BigInt}(b)))
     ineq = HalfspaceIterator(Matrix{BigInt}(-C), vec(Matrix{BigInt}(-d)))
     P = Polyhedron(ineq, eq)
-    inner = interior_lattice_points(P)
-    outer = boundary_lattice_points(P)
-
-    result = zero_matrix(FlintZZ, length(inner) + length(outer), ambient_dim(P))
-    i = 1
-    for v in inner
-        result[i, :] = Vector{BigInt}(v.p)
-        i += 1
-    end
-    for v in outer
-        result[i, :] = Vector{BigInt}(v.p)
-        i += 1
-    end
-    return result
+    LP = lattice_points(P)
+    return matrix(ZZ, ambient_dim(P), length(LP), hcat(LP...))'
 end
 
 
@@ -51,6 +42,8 @@ Solve $Ax = b$ under $Cx >= 0$, assumes a finite solution set.
 
 # Examples
 Find all $(x_1, x_2)\in\mathbb{Z}^2_{\ge 0}$ such that $x_1+x_2=3$.
+The solutions are the rows of the output.
+Note that the output can be permuted, hence we sort it.
 ```jldoctest
 julia> A = fmpz_mat([1 1]);
 
@@ -58,11 +51,12 @@ julia> b = zero_matrix(FlintZZ, 1,1); b[1,1]=3;
 
 julia> C = fmpz_mat([1 0; 0 1]);
 
-julia> solve_mixed(A,b,C)
-[1   2]
-[2   1]
-[0   3]
-[3   0]
+julia> sortslices(Matrix{BigInt}(solve_mixed(A, b, C)), dims=1)
+4×2 Matrix{BigInt}:
+ 0  3
+ 1  2
+ 2  1
+ 3  0
 ```
 """
 function solve_mixed(A::fmpz_mat, b::fmpz_mat, C::fmpz_mat)  # Ax == b && Cx >= 0
@@ -77,16 +71,19 @@ Solve $Ax<=b$, assumes finite set of solutions.
 
 # Examples
 The following gives the vertices of the square.
+The solutions are the rows of the output.
+Note that the output can be permuted, hence we sort it.
 ```jldoctest
 julia> A = fmpz_mat([1 0; 0 1; -1 0; 0 -1]);
 
 julia> b = zero_matrix(FlintZZ, 4,1); b[1,1]=1; b[2,1]=1; b[3,1]=0; b[4,1]=0;
 
-julia> solve_ineq(A,b)
-[0   0]
-[0   1]
-[1   0]
-[1   1]
+julia> sortslices(Matrix{BigInt}(solve_ineq(A, b)), dims=1)
+4×2 Matrix{BigInt}:
+ 0  0
+ 0  1
+ 1  0
+ 1  1
 ```
 """
 function solve_ineq(A::fmpz_mat, b::fmpz_mat)
@@ -101,16 +98,19 @@ Find all solutions to $Ax = b$, $x>=0$. Assumes a finite set of solutions.
 
 # Examples
 Find all $(x_1, x_2)\in\mathbb{Z}^2_{\ge 0}$ such that $x_1+x_2=3$.
+The solutions are the rows of the output.
+Note that the output can be permuted, hence we sort it.
 ```jldoctest
 julia> A = fmpz_mat([1 1]);
 
 julia> b = zero_matrix(FlintZZ, 1,1); b[1,1]=3;
 
-julia> solve_non_negative(A,b)
-[1   2]
-[2   1]
-[0   3]
-[3   0]
+julia> sortslices(Matrix{BigInt}(solve_non_negative(A, b)), dims=1)
+4×2 Matrix{BigInt}:
+ 0  3
+ 1  2
+ 2  1
+ 3  0
 ```
 """
 function solve_non_negative(A::fmpz_mat, b::fmpz_mat)
