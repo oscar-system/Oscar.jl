5a32b63e113acf399ace729b8cdd3802d0eb863b
Max Horn <max@quendi.de>
2021-10-14 23:25:39 +0200

    Drop unused Requires.jl dependency

--- src/Oscar.jl
+++ src/Oscar.jl
@@ -33,7 +33,8 @@ import Singular
 import Polymake
 import GAP
 import Pkg
-using Markdown, Test, Requires
+using Markdown
+using Test
 # to allow access to the cornerstones! Otherwise, not even import or using from the
 # user level will work as none of them will have been "added" by the user.
 # possibly all should add a doc string to the module?
