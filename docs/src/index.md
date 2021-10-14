78a2d26d3f152be8a393c06ab3b48c627be7fdb2
Max Horn <max@quendi.de>
2021-10-14 22:53:45 +0200

    Strip JToric for integration into Oscar.jl

--- docs/src/index.md
+++
@@ -1,3 +0,0 @@
-# Welcome to JToric
-
-JToric is a software package for Normal Toric Varieties in Julia.
--- test/doctest.jl
+++
@@ -1,5 +0,0 @@
-using Documenter, JToric
-
-DocMeta.setdocmeta!(JToric, :DocTestSetup, :(using JToric, Random; using JToric.Oscar); recursive = true)
-
-doctest(JToric)
