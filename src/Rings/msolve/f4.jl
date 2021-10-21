49eb83b48c632a30f75a5a47982de789fba94327
ederc <ederc@mathematik.uni-kl.de>
2021-10-21 14:56:40 +0200

    fix documentation of f4

--- src/Rings/msolve/f4.jl
+++ src/Rings/msolve/f4.jl
@@ -3,47 +3,44 @@ import Libdl: dlopen, dlsym, dlclose
 
 export f4
 
-"""
-    f4(I[, hts::Int=17, nthrds::Int=1, maxpairs::Int=0, resetht::Int=0,
-            laopt::Int=1, reducegb::Int=0, pbmfiles::Int=0,
-            infolevel::Int=0, monorder::Symbol=:degrevlex])
+@doc Markdown.doc"""
+    f4(I, <keyword arguments>)
 
-Compute a Groebner basis of the given ideal I w.r.t. to the given monomial
-order using Faugere's F4 algorithm. The function takes a Singular ideal as
-input and returns a Singular ideal. At the moment only finite fields up to 31-bit and the degree reverse lexicographical ordering is supported.
+Compute a Groebner basis of the given ideal I w.r.t. to the degree reverse lexicographical monomial ordering using Faugère's F4 algorithm, see [Fau99](@cite) for more information.
+
+**Note**: At the moment only ground fields of characteristic `p`, `p` prime, `p < 2^{31}` are supported.
 
 # Arguments
-* `I::Singular.sideal`: ideal to compute a Groebner basis for.
-* `initial_hts::Int=17`: hash table size log_2; default is 17, i.e. 2^17 as initial hash
-                table size.
-* `nr_thrds::Int=1`:  number of threads; default is 1.
-* `max_nr_pairs::Int=0`:  maximal number of pairs selected for one matrix; default is
-                      0, i.e. no restriction. If matrices get too big or consume
-                      too much memory this is a good parameter to play with.
-* `la_option::Int=1`: option for linear algebra to be used. there are different
-                  linear algebra routines implemented:
-    -  `1`: exact sparse-dense computation (default),
-    -  `2`: exact sparse computation,
-    - `42`: probabilistic sparse-dense computation,
-    - `43`: exact sparse then probabilistic dense computation,
-    - `44`: probabilistic sparse computation.
-* `reduce_gb::Int=1`:  reduce final basis; default is 1.
-* `info_level::Int=0`: info level for printout:
-    - `0`: no printout (default),
-    - `1`:  a summary of the computational data is printed at the beginning and
-    the end of the computation,
-    - `2`: also dynamical information for each round resp. matrix is printed.
+- `Ì::MPolyIdeal`: input ideal.
+- `initial_hts::Int=17`: initial hash table size `log_2`.
+- `nr_thrds::Int=1`: number of threads for parallel linear algebra.
+- `max_nr_pairs::Int=0`: maximal number of pairs per matrix, only bounded by minimal degree if `0`.
+- `la_option::Int=2`: linear algebra option: exact sparse-dense (`1`), exact sparse (`2`, default), probabilistic sparse-dense (`42`), probabilistic sparse(`44`).
+- reduce_gb::Int=1`: compute a reduced Gröbner basis for `I`
+- info_level::Int=0`: info level printout: off (`0`, default), summary (`1`), detailed (`2`).
+
+# Examples
+```jldoctest
+julia> R,(x,y,z) = PolynomialRing(FiniteField(101), ["x","y","z"])
+(Multivariate Polynomial Ring in x, y, z over Galois field with characteristic 101, gfp_mpoly[x, y, z])
+
+julia> I = ideal(R, [x+2*y+2*z-1, x^2+2*y^2+2*z^2-x, 2*x*y+2*y*z-y])
+ideal(x + 2*y + 2*z + 100, x^2 + 100*x + 2*y^2 + 2*z^2, 2*x*y + 2*y*z + 100*y)
+
+julia> f4(I)
+Oscar.BiPolyArray{gfp_mpoly}(gfp_mpoly[#undef, #undef, #undef, #undef], Singular Ideal over Singular Polynomial Ring (ZZ/101),(x,y,z),(dp(3),C) with generators (x + 2*y + 2*z - 1, y*z - 19*z^2 + 10*y + 40*z, y^2 - 41*z^2 + 20*y - 20*z, z^3 + 28*z^2 - 37*y + 13*z), Multivariate Polynomial Ring in x, y, z over Galois field with characteristic 101, Singular Polynomial Ring (ZZ/101),(x,y,z),(dp(3),C), false, #undef)
+```
 """
 function f4(
-        I::MPolyIdeal;                # input generators
-        initial_hts::Int=17,                  # hash table size, default 2^17
-        nr_thrds::Int=1,                # number of threads
-        max_nr_pairs::Int=0,              # number of pairs maximally chosen
-                                      # in symbolic preprocessing
-        la_option::Int=2,                 # linear algebra option
-        reduce_gb::Int=1,             # reduce final basis
-        info_level::Int=0              # info level for print outs
+        I::MPolyIdeal;
+        initial_hts::Int=17,
+        nr_thrds::Int=1,
+        max_nr_pairs::Int=0,
+        la_option::Int=2,
+        reduce_gb::Int=1,
+        info_level::Int=0
         )
+
     singular_assure(I)
     SI    = I.gens.S
     R     = I.gens.Sx
@@ -115,7 +112,8 @@ function f4(
     gb_cf_conv  = Nothing
     jl_cf       = Nothing
 
-    I.gb = BiPolyArray(base_ring(I), basis)
+    I.gb      = BiPolyArray(base_ring(I), basis)
+    I.gb.isGB = true
     
     return I.gb
 end
