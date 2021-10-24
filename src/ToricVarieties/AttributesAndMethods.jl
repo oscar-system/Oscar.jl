7dfaac1a6b56f7839108f2db756b81ad739f28df
Martin Bies <MBies87@googlemail.com>
2021-10-24 13:15:39 -0400

    Organize implementation of operations for normal toric varieties by subjects

--- src/ToricVarieties/AttributesAndMethods.jl
+++ src/ToricVarieties/AttributesAndMethods.jl
@@ -1,6 +1,7 @@
-######################
-# 1: Attributes of ToricVarieties
-######################
+############################
+# Dimensions
+############################
+
 
 @doc Markdown.doc"""
     dim(v::AbstractNormalToricVariety)
@@ -32,6 +33,25 @@ export dim_of_torusfactor
 
 
 @doc Markdown.doc"""
+    ith_betti_number(v::AbstractNormalToricVariety, i::Int)
+
+Compute the i-th Betti number of the normal toric variety `v`.
+"""
+function ith_betti_number(v::AbstractNormalToricVariety, i::Int)
+    if isodd(i)
+        return 0
+    end
+    k = div(i, 2)
+    f_vector = Vector{Int}(pm_ntv(v).F_VECTOR)
+    pushfirst!(f_vector, 1)
+    betti_number = sum((-1)^(i-k) * binomial(i,k) * f_vector[dim(v) - i + 1] for i=k:dim(v))
+    return betti_number
+    
+end
+export ith_betti_number
+
+
+@doc Markdown.doc"""
     euler_characteristic(v::AbstractNormalToricVariety)
 
 Computes the Euler characteristic of the normal toric variety `v`.
@@ -43,241 +63,9 @@ end
 export euler_characteristic
 
 
-"""
-    nef_cone(v::NormalToricVariety)
-
-Computes the nef cone of the normal toric variety `v`.
-
-# Examples
-```jldoctest
-julia> pp = toric_projective_space(2)
-A normal toric variety corresponding to a polyhedral fan in ambient dimension 2
-
-julia> nef = nef_cone(pp)
-A polyhedral cone in ambient dimension 1
-
-julia> dim(nef)
-1
-```
-"""
-nef_cone(v::NormalToricVariety) = Cone(pm_ntv(v).NEF_CONE)
-export nef_cone
-
-
-"""
-    mori_cone(v::NormalToricVariety)
-
-Computes the mori cone of the normal toric variety `v`.
-
-# Examples
-```jldoctest
-julia> pp = toric_projective_space(2)
-A normal toric variety corresponding to a polyhedral fan in ambient dimension 2
-
-julia> mori = mori_cone(pp)
-A polyhedral cone in ambient dimension 1
-
-julia> dim(mori)
-1
-```
-"""
-mori_cone(v::NormalToricVariety) = Cone(pm_ntv(v).MORI_CONE)
-export mori_cone
-
-
-@doc Markdown.doc"""
-    affine_open_covering(v::NormalToricVariety)
-
-Computes an affine open cover of the normal toric variety `v`, i.e. returns a list of affine toric varieties.
-
-# Examples
-```jldoctest
-julia> p2 = toric_projective_space(2)
-A normal toric variety corresponding to a polyhedral fan in ambient dimension 2
-
-julia> affine_open_covering(p2)
-3-element Vector{AffineNormalToricVariety}:
- A normal toric variety corresponding to a polyhedral fan in ambient dimension 2
- A normal toric variety corresponding to a polyhedral fan in ambient dimension 2
- A normal toric variety corresponding to a polyhedral fan in ambient dimension 2
-```
-"""
-function affine_open_covering(v::NormalToricVariety)
-    charts = Vector{AffineNormalToricVariety}(undef, pm_ntv(v).N_MAXIMAL_CONES)
-    for i in 1:pm_ntv(v).N_MAXIMAL_CONES
-        charts[i] = AffineNormalToricVariety(Cone(Polymake.fan.cone(pm_ntv(v), i-1)))
-    end
-    return charts
-end
-
-
-@doc Markdown.doc"""
-    affine_open_covering(v::AffineNormalToricVariety)
-
-Computes an affine open cover of the affine normal toric variety `v`, i.e. returns this very variety.
-
-# Examples
-```jdoctest
-julia> C = Oscar.positive_hull([1 0; 0 1])
-A polyhedral cone in ambient dimension 2
-
-julia> antv = AffineNormalToricVariety(C)
-A normal toric variety corresponding to a polyhedral fan in ambient dimension 2
-
-julia> affine_open_covering(antv)
-1-element Vector{AffineNormalToricVariety}:
- A normal toric variety corresponding to a polyhedral fan in ambient dimension 2
-```
-"""
-function affine_open_covering(v::AffineNormalToricVariety)
-    return [v]
-end
-export affine_open_covering
-
-
-@doc Markdown.doc"""
-    fan_of_variety(v::NormalToricVariety)
-
-Computes the fan of a normal toric variety `v`.
-
-# Examples
-```jdoctest
-julia> p2 = toric_projective_space(2)
-A normal toric variety corresponding to a polyhedral fan in ambient dimension 2
-
-julia> fan_of_variety(p2)
-A polyhedral fan in ambient dimension 2
-```
-"""
-function fan_of_variety(v::NormalToricVariety)
-    return PolyhedralFan(pm_ntv(v))
-end
-export fan_of_variety
-
-
-@doc Markdown.doc"""
-    fan(v::NormalToricVariety)
-
-A convenience method for fan_of_variety of a normal toric variety `v`.
-"""
-function fan(v::NormalToricVariety)
-    return fan_of_variety(v)
-end
-export fan
-
-
-@doc Markdown.doc"""
-    torusinvariant_divisor_group(v::NormalToricVariety)
-
-Computes the torusinvariant divisor group of a normal toric variety `v`.
-
-# Examples
-```jdoctest
-julia> p2 = toric_projective_space(2)
-A normal toric variety corresponding to a polyhedral fan in ambient dimension 2
-
-julia> torusinvariant_divisor_group(p2)
-GrpAb: Z^3
-```
-"""
-function torusinvariant_divisor_group(v::AbstractNormalToricVariety)
-    return abelian_group([0 for i in 1:pm_ntv(v).N_RAYS])
-end
-export torusinvariant_divisor_group
-
-
-@doc Markdown.doc"""
-    character_lattice(v::AbstractNormalToricVariety)
-
-Computes the character lattice of a normal toric variety `v`.
-
-# Examples
-```jdoctest
-julia> p2 = toric_projective_space(2)
-A normal toric variety corresponding to a polyhedral fan in ambient dimension 2
-
-julia> character_lattice(p2)
-GrpAb: Z^2
-```
-"""
-function character_lattice(v::AbstractNormalToricVariety)
-    return abelian_group([0 for i in 1:pm_ntv(v).FAN_DIM])
-end
-export character_lattice
-
-
-@doc Markdown.doc"""
-    map_from_character_to_principal_divisors(v::AbstractNormalToricVariety)
-
-Computes the map from the character lattice to the group of principal divisors of a normal toric variety `v`.
-
-# Examples
-```jdoctest
-julia> p2 = toric_projective_space(2)
-A normal toric variety corresponding to a polyhedral fan in ambient dimension 2
-
-julia> map_from_character_to_principal_divisors(p2)
-Map with following data
-Domain:
-=======
-Abelian group with structure: Z^2
-Codomain:
-=========
-Abelian group with structure: Z^3
-```
-"""
-function map_from_character_to_principal_divisors(v::AbstractNormalToricVariety)
-    matrix = Matrix{Int}(Oscar.Polymake.common.primitive(pm_ntv(v).RAYS))
-    abstract_matrix = AbstractAlgebra.matrix(ZZ, size(matrix,2), size(matrix,1), vec(matrix))
-    return hom(character_lattice(v), torusinvariant_divisor_group(v), abstract_matrix)
-end
-export map_from_character_to_principal_divisors
-
-
-@doc Markdown.doc"""
-    class_group(v::AbstractNormalToricVariety)
-
-Computes the class group of the normal toric variety `v`.
-
-# Examples
-```jdoctest
-julia> p2 = toric_projective_space(2)
-A normal toric variety corresponding to a polyhedral fan in ambient dimension 2
-
-julia> class_group(p2)
-(General) abelian group with relation matrix
-[1 0 -1; 0 1 -1]
-```
-"""
-function class_group(v::AbstractNormalToricVariety)
-    return cokernel(map_from_character_to_principal_divisors(v))[1]
-end
-export class_group
-
-
-@doc Markdown.doc"""
-    map_from_weil_divisors_to_class_group(v::AbstractNormalToricVariety)
-
-Computes the map from the group of Weil divisors to the class of group of a normal toric variety `v`.
-
-# Examples
-```jdoctest
-julia> map_from_weil_divisors_to_class_group(p2)
-Map with following data
-Domain:
-=======
-Abelian group with structure: Z^3
-Codomain:
-=========
-(General) abelian group with relation matrix
-[0 0 0; 0 0 0; 0 0 0; 1 0 -1; 0 1 -1]
-with structure of Abelian group with structure: Z
-```
-"""
-function map_from_weil_divisors_to_class_group(v::AbstractNormalToricVariety)
-    return cokernel(map_from_character_to_principal_divisors(v))[2]
-end
-export map_from_weil_divisors_to_class_group
+############################
+# Rings and ideals
+############################
 
 
 @doc Markdown.doc"""
@@ -399,6 +187,79 @@ end
 export irrelevant_ideal
 
 
+############################
+# Characters, Weil divisor and the class group
+############################
+
+
+@doc Markdown.doc"""
+    character_lattice(v::AbstractNormalToricVariety)
+
+Computes the character lattice of a normal toric variety `v`.
+
+# Examples
+```jdoctest
+julia> p2 = toric_projective_space(2)
+A normal toric variety corresponding to a polyhedral fan in ambient dimension 2
+
+julia> character_lattice(p2)
+GrpAb: Z^2
+```
+"""
+function character_lattice(v::AbstractNormalToricVariety)
+    return abelian_group([0 for i in 1:pm_ntv(v).FAN_DIM])
+end
+export character_lattice
+
+
+@doc Markdown.doc"""
+    torusinvariant_divisor_group(v::NormalToricVariety)
+
+Computes the torusinvariant divisor group of a normal toric variety `v`.
+
+# Examples
+```jdoctest
+julia> p2 = toric_projective_space(2)
+A normal toric variety corresponding to a polyhedral fan in ambient dimension 2
+
+julia> torusinvariant_divisor_group(p2)
+GrpAb: Z^3
+```
+"""
+function torusinvariant_divisor_group(v::AbstractNormalToricVariety)
+    return abelian_group([0 for i in 1:pm_ntv(v).N_RAYS])
+end
+export torusinvariant_divisor_group
+
+
+@doc Markdown.doc"""
+    map_from_character_to_principal_divisors(v::AbstractNormalToricVariety)
+
+Computes the map from the character lattice to the group of principal divisors of a normal toric variety `v`.
+
+# Examples
+```jdoctest
+julia> p2 = toric_projective_space(2)
+A normal toric variety corresponding to a polyhedral fan in ambient dimension 2
+
+julia> map_from_character_to_principal_divisors(p2)
+Map with following data
+Domain:
+=======
+Abelian group with structure: Z^2
+Codomain:
+=========
+Abelian group with structure: Z^3
+```
+"""
+function map_from_character_to_principal_divisors(v::AbstractNormalToricVariety)
+    matrix = Matrix{Int}(Oscar.Polymake.common.primitive(pm_ntv(v).RAYS))
+    abstract_matrix = AbstractAlgebra.matrix(ZZ, size(matrix,2), size(matrix,1), vec(matrix))
+    return hom(character_lattice(v), torusinvariant_divisor_group(v), abstract_matrix)
+end
+export map_from_character_to_principal_divisors
+
+
 @doc Markdown.doc"""
     torusinvariant_prime_divisors(v::AbstractNormalToricVariety)
 
@@ -426,28 +287,188 @@ end
 export torusinvariant_prime_divisors
 
 
-######################
-# 2: Methods of ToricVarieties
-######################
+@doc Markdown.doc"""
+    class_group(v::AbstractNormalToricVariety)
+
+Computes the class group of the normal toric variety `v`.
+
+# Examples
+```jdoctest
+julia> p2 = toric_projective_space(2)
+A normal toric variety corresponding to a polyhedral fan in ambient dimension 2
+
+julia> class_group(p2)
+(General) abelian group with relation matrix
+[1 0 -1; 0 1 -1]
+```
+"""
+function class_group(v::AbstractNormalToricVariety)
+    return cokernel(map_from_character_to_principal_divisors(v))[1]
+end
+export class_group
 
 
 @doc Markdown.doc"""
-    ith_betti_number(v::AbstractNormalToricVariety, i::Int)
+    map_from_weil_divisors_to_class_group(v::AbstractNormalToricVariety)
 
-Compute the i-th Betti number of the normal toric variety `v`.
+Computes the map from the group of Weil divisors to the class of group of a normal toric variety `v`.
+
+# Examples
+```jdoctest
+julia> map_from_weil_divisors_to_class_group(p2)
+Map with following data
+Domain:
+=======
+Abelian group with structure: Z^3
+Codomain:
+=========
+(General) abelian group with relation matrix
+[0 0 0; 0 0 0; 0 0 0; 1 0 -1; 0 1 -1]
+with structure of Abelian group with structure: Z
+```
 """
-function ith_betti_number(v::AbstractNormalToricVariety, i::Int)
-    if isodd(i)
-        return 0
-    end
-    k = div(i, 2)
-    f_vector = Vector{Int}(pm_ntv(v).F_VECTOR)
-    pushfirst!(f_vector, 1)
-    betti_number = sum((-1)^(i-k) * binomial(i,k) * f_vector[dim(v) - i + 1] for i=k:dim(v))
-    return betti_number
-    
+function map_from_weil_divisors_to_class_group(v::AbstractNormalToricVariety)
+    return cokernel(map_from_character_to_principal_divisors(v))[2]
 end
-export ith_betti_number
+export map_from_weil_divisors_to_class_group
+
+
+############################
+# Cones and fans
+############################
+
+
+"""
+    nef_cone(v::NormalToricVariety)
+
+Computes the nef cone of the normal toric variety `v`.
+
+# Examples
+```jldoctest
+julia> pp = toric_projective_space(2)
+A normal toric variety corresponding to a polyhedral fan in ambient dimension 2
+
+julia> nef = nef_cone(pp)
+A polyhedral cone in ambient dimension 1
+
+julia> dim(nef)
+1
+```
+"""
+nef_cone(v::NormalToricVariety) = Cone(pm_ntv(v).NEF_CONE)
+export nef_cone
+
+
+"""
+    mori_cone(v::NormalToricVariety)
+
+Computes the mori cone of the normal toric variety `v`.
+
+# Examples
+```jldoctest
+julia> pp = toric_projective_space(2)
+A normal toric variety corresponding to a polyhedral fan in ambient dimension 2
+
+julia> mori = mori_cone(pp)
+A polyhedral cone in ambient dimension 1
+
+julia> dim(mori)
+1
+```
+"""
+mori_cone(v::NormalToricVariety) = Cone(pm_ntv(v).MORI_CONE)
+export mori_cone
+
+
+@doc Markdown.doc"""
+    fan_of_variety(v::NormalToricVariety)
+
+Computes the fan of a normal toric variety `v`.
+
+# Examples
+```jdoctest
+julia> p2 = toric_projective_space(2)
+A normal toric variety corresponding to a polyhedral fan in ambient dimension 2
+
+julia> fan_of_variety(p2)
+A polyhedral fan in ambient dimension 2
+```
+"""
+function fan_of_variety(v::NormalToricVariety)
+    return PolyhedralFan(pm_ntv(v))
+end
+export fan_of_variety
+
+
+@doc Markdown.doc"""
+    fan(v::NormalToricVariety)
+
+A convenience method for fan_of_variety of a normal toric variety `v`.
+"""
+function fan(v::NormalToricVariety)
+    return fan_of_variety(v)
+end
+export fan
+
+
+############################
+# Affine covering
+############################
+
+
+@doc Markdown.doc"""
+    affine_open_covering(v::NormalToricVariety)
+
+Computes an affine open cover of the normal toric variety `v`, i.e. returns a list of affine toric varieties.
+
+# Examples
+```jldoctest
+julia> p2 = toric_projective_space(2)
+A normal toric variety corresponding to a polyhedral fan in ambient dimension 2
+
+julia> affine_open_covering(p2)
+3-element Vector{AffineNormalToricVariety}:
+ A normal toric variety corresponding to a polyhedral fan in ambient dimension 2
+ A normal toric variety corresponding to a polyhedral fan in ambient dimension 2
+ A normal toric variety corresponding to a polyhedral fan in ambient dimension 2
+```
+"""
+function affine_open_covering(v::NormalToricVariety)
+    charts = Vector{AffineNormalToricVariety}(undef, pm_ntv(v).N_MAXIMAL_CONES)
+    for i in 1:pm_ntv(v).N_MAXIMAL_CONES
+        charts[i] = AffineNormalToricVariety(Cone(Polymake.fan.cone(pm_ntv(v), i-1)))
+    end
+    return charts
+end
+
+
+@doc Markdown.doc"""
+    affine_open_covering(v::AffineNormalToricVariety)
+
+Computes an affine open cover of the affine normal toric variety `v`, i.e. returns this very variety.
+
+# Examples
+```jdoctest
+julia> C = Oscar.positive_hull([1 0; 0 1])
+A polyhedral cone in ambient dimension 2
+
+julia> antv = AffineNormalToricVariety(C)
+A normal toric variety corresponding to a polyhedral fan in ambient dimension 2
+
+julia> affine_open_covering(antv)
+1-element Vector{AffineNormalToricVariety}:
+ A normal toric variety corresponding to a polyhedral fan in ambient dimension 2
+```
+"""
+function affine_open_covering(v::AffineNormalToricVariety)
+    return [v]
+end
+export affine_open_covering
+
+
+############################
+# Toric ideal
+############################
 
 
 # @doc Markdown.doc"""
