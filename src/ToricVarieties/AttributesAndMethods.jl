1fdfe557617a6a013248506a591675cfb21c31f9
Martin Bies <HereAround@users.noreply.github.com>
2021-10-28 19:20:31 -0400

    Better construction of zero vector
    Co-authored-by: Max Horn <max@quendi.de>

--- src/ToricVarieties/AttributesAndMethods.jl
+++ src/ToricVarieties/AttributesAndMethods.jl
@@ -272,7 +272,7 @@ function torusinvariant_prime_divisors(v::AbstractNormalToricVariety)
     ti_divisors = torusinvariant_divisor_group(v)
     prime_divisors = ToricDivisor[]
     for i in 1:rank(ti_divisors)
-        coeffs = fill(0, rank(ti_divisors))
+        coeffs = zeros(Int, rank(ti_divisors))
         coeffs[i] = 1
         push!(prime_divisors, ToricDivisor(v,coeffs))
     end
