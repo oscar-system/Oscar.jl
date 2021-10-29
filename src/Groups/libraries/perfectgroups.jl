3fc0cd18713df2df3d5dc35101a68bdbad2b8a83
Max Horn <max@quendi.de>
2021-10-29 21:45:00 +0200

    Change perfect_group to return permutation groups by default

--- src/Groups/libraries/perfectgroups.jl
+++ src/Groups/libraries/perfectgroups.jl
@@ -28,7 +28,7 @@ function perfect_group(::Type{T}, n::Int, m::Int) where T <: GAPGroup
    return G
 end
 
-perfect_group(n::Int, m::Int) = perfect_group(FPGroup,n,m)
+perfect_group(n::Int, m::Int) = perfect_group(PermGroup,n,m)
 
 """
     perfect_identification(G::GAPGroup)
