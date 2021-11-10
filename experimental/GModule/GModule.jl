a16464d59c170a0365fbb37b5e94f3d6cb64b14c
Claus Fieker <fieker@mathematik.uni-kl.de>
2021-11-10 17:23:21 +0100

    add several more constructions...

--- experimental/GModule/GModule.jl
+++ experimental/GModule/GModule.jl
@@ -102,6 +102,10 @@ function ^(C::GModule{<:Any, Generic.FreeModule{nf_elem}}, phi::Map{AnticNumberF
   return GModule(group(C), [hom(F, F, map_entries(phi, mat(x))) for x = C.ac])
 end
 
+function ^(C::GModule{<:Any, T}, h::Map{S, S}) where T <: S where S
+  return GModule(group(C), [inv(h)*x*h for x = C.ac])
+end
+
 function ^(C::GModule{<:Any, Generic.FreeModule{QabElem}}, phi::Map{QabField, QabField})
   F = free_module(codomain(phi), dim(C))
   return GModule(group(C), [hom(F, F, map_entries(phi, mat(x))) for x = C.ac])
@@ -117,36 +121,168 @@ function gmodule(k::Nemo.GaloisField, C::GModule{<:Any, Generic.FreeModule{fmpq}
   return GModule(group(C), [hom(F, F, map_entries(k, mat(x))) for x=C.ac])
 end
 
-function Gap(C::GModule{<:Any, Generic.FreeModule{gfp_elem}})
+function gmodule(mk::Map{AnticNumberField, <:FinField}, C::GModule{<:Any, Generic.FreeModule{nf_elem}})
+  k = codomain(mk)
+  @assert domain(mk) == base_ring(C)
+  F = free_module(k, dim(C))
+  return GModule(group(C), [hom(F, F, map_entries(mk, mat(x))) for x=C.ac])
+end
+
+function Hecke.modular_proj(C::GModule{T, Generic.FreeModule{nf_elem}}, me::Hecke.modular_env) where T
+  R = []
+  z = map(x->(Hecke.modular_proj(x.matrix, me)), C.ac)
+  for i=1:length(z[1])
+    F = free_module(base_ring(z[1][i]), dim(C))
+    @assert all(j->base_ring(z[j][i]) == base_ring(z[1][i]), 1:length(z))
+    push!(R, GModule(group(C), [hom(F, F, t[i]) for t = z]))
+    @assert all(i->base_ring(mat(R[end].ac[i])) == base_ring(R[end]), 1:length(R[end].ac))
+  end
+  return R
+end
+
+function Gap(C::GModule{<:Any, <:Generic.FreeModule{<:FinFieldElem}}, h=Oscar.ring_iso_oscar_gap(base_ring(C)))
   z = AbstractAlgebra.get_special(C, :Gap)
   if z !== nothing
     return z
   end
-  k = GAP.Globals.Z(Int(characteristic(base_ring(C))))
-  one = k^0
-  z = GAP.Globals.GModuleByMats(GAP.julia_to_gap([GAP.julia_to_gap(map(x->Int(x)*k, Matrix(lift(mat(x))))) for x = C.ac]), GAP.Globals.GF(Int(characteristic(base_ring(C)))))
+  z = GAP.Globals.GModuleByMats(GAP.julia_to_gap([GAP.julia_to_gap(map(h, Matrix(mat(x)))) for x = C.ac]), codomain(h))
   AbstractAlgebra.set_special(C, :Gap=>z)
   return z
 end
 
-function Oscar.isirreducible(C::GModule{<:Any, Generic.FreeModule{gfp_elem}})
+function Oscar.isirreducible(C::GModule{<:Any, <:Generic.FreeModule{<:FinFieldElem}})
   G = Gap(C)
   return GAP.Globals.MTX.IsIrreducible(G)
 end
 
-function isabsolutely_irreducible(C::GModule{<:Any, Generic.FreeModule{gfp_elem}})
+function isabsolutely_irreducible(C::GModule{<:Any, <:Generic.FreeModule{<:FinFieldElem}})
   G = Gap(C)
   return GAP.Globals.MTX.IsAbsolutelyIrreducible(G)
 end
 
-function isdecomposable(C::GModule{<:Any, Generic.FreeModule{gfp_elem}})
+function isdecomposable(C::GModule{<:Any, <:Generic.FreeModule{<:FinFieldElem}})
   G = Gap(C)
   return !GAP.Globals.MTX.IsIndecomposable(G)
 end
 
-function Oscar.hom(C::T, D::T) where T <: GModule{<:Any, Generic.FreeModule{gfp_elem}}
+function hom_base(C::T, D::T) where T <: GModule{<:Any, <:Generic.FreeModule{<:FinFieldElem}}
   @assert base_ring(C) == base_ring(D)
-  return GAP.Globals.MTX.BasisModuleHomomorphisms(Gap(C), Gap(D))
+  h = Oscar.ring_iso_oscar_gap(base_ring(C))
+  hb = GAP.Globals.MTX.BasisModuleHomomorphisms(Gap(C, h), Gap(D, h))
+  n = length(hb)
+  b = map(x->matrix(map(y->preimage(h, y), Oscar.GAP.gap_to_julia(Matrix{Any}, x))), hb)
+#  @show [mat(C.ac[i])*b[1] == b[1]*mat(D.ac[i]) for i=1:length(C.ac)]
+  return b
+end
+
+"""
+  C*T[i] = T[i]*D
+on return
+"""
+function hom_base(C::_T, D::_T) where _T <: GModule{<:Any, <:Generic.FreeModule{nf_elem}}
+  @assert base_ring(C) == base_ring(D)
+
+  p = Hecke.p_start
+  p = 2^10
+  p = 127
+  m_in = map(mat, C.ac)
+  m_out = map(mat, D.ac)
+  local T
+  pp = fmpz(1)
+  k = base_ring(C)
+  @assert base_ring(m_in[1]) == k
+  @assert base_ring(m_in[1]) == k
+  while true
+    p = next_prime(p)
+    me = modular_init(k, p)
+    z1 = Hecke.modular_proj(C, me)
+    if C === D
+      z2 = z1
+    else
+      z2 = Hecke.modular_proj(D, me)
+    end
+    t = []
+    for i=1:length(z1)
+      push!(t, hom_base(z1[i], z2[i]))
+    end
+    tt = [Hecke.modular_lift([t[i][j] for i=1:length(z1)], me) for j=1:length(t[1])]
+    @assert base_ring(tt[1]) == k
+    if isone(pp)
+      pp = fmpz(p)
+      T = tt
+    else
+      T = [induce_crt(tt[i], T[i], fmpz(p), pp) for i=1:length(T)]
+      @assert base_ring(T[1]) == k
+      pp *= p
+      S = []
+      for t = T
+        fl, s = induce_rational_reconstruction(t, pp)
+        fl || break
+        push!(S, s)
+      end
+      @assert base_ring(S[1]) == k
+      s = S[1]
+      if length(S) == length(T)
+        if all(s->all(i->m_in[i]*s ==  s*m_out[i], 1:length(m_in)), S)
+          return S
+        end
+      end
+    end
+  end
+end
+
+function gmodule(::FlintIntegerRing, C::GModule{<:Any, <:Generic.FreeModule{fmpq}})
+  ma = map(mat, C.ac)
+  M = identity_matrix(QQ, dim(C))
+  while true
+    N = reduce(vcat, [M*x for x = ma])
+    H = hnf(integral_split(N, ZZ)[1])[1:dim(C), :]
+    if H == M
+      break
+    end
+    M = map_entries(QQ, H)
+  end
+  M = inv(M)
+  h = hom(C.M, C.M, M)
+  D = C^h
+  F = free_module(ZZ, dim(C))
+  return gmodule(group(C), [hom(F, F, integral_split(mat(x), ZZ)[1]) for x = D.ac])
+end
+
+function Base.transpose(C::GModule{<:Any, <:Generic.FreeModule})
+  return gmodule(group(C), [hom(C.M, C.M, transpose(mat(x))) for x = C.ac])
+end
+
+function invariant_form()
+end
+
+function simplify(C::GModule{<:Any, <:Generic.FreeModule{fmpz}})
+end
+
+function Hecke.induce_crt(a::Generic.MatSpaceElem{nf_elem}, b::Generic.MatSpaceElem{nf_elem}, p::fmpz, q::fmpz)
+  c = parent(a)()
+  pi = invmod(p, q)
+  mul!(pi, pi, p)
+  pq = p*q
+  z = fmpz(0)
+
+  for i=1:nrows(a)
+    for j=1:ncols(a)
+      c[i,j] = Hecke.induce_inner_crt(a[i,j], b[i,j], pi, pq, z)
+    end
+  end
+  return c
+end
+
+function Hecke.induce_rational_reconstruction(a::Generic.MatSpaceElem{nf_elem}, pg::fmpz)
+  c = parent(a)()
+  for i=1:nrows(a)
+    for j=1:ncols(a)
+      fl, c[i,j] = rational_reconstruction(a[i,j], pg)
+      fl || return fl, c
+    end
+  end
+  return true, c
 end
 
 export irreducible_modules, isabsolutely_irreducible, isdecomposable
