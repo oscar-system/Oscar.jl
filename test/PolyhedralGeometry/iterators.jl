f014626d86d837d27409562cdf0cb0768712fa4d
Alexej Jordan <jordan@math.tu-berlin.de>
2021-11-17 04:49:18 +0100

    completely removed VectorIterator

--- test/PolyhedralGeometry/iterators.jl
+++ test/PolyhedralGeometry/iterators.jl
@@ -1,30 +1,4 @@
 @testset "iterators" begin
-
-    @testset "VectorIterator" begin
-        vecmat_old = [1 0 0; 0 1 0; 0 0 1; 1 1 1]
-        vecmat = [1 0 0; 1 2 3; 0 0 1; 1 1 1]
-        @testset "VectorIterator $T{$U}" for T in (PointVector, RayVector), U in (Polymake.Rational, Polymake.Integer)
-            vi = VectorIterator{T{U}}(vecmat_old)
-            @test vi isa VectorIterator
-            @test vi isa VectorIterator{T{U}}
-            @test eltype(vi) == T{U}
-            @test size(vi) == (4,)
-            @test length(vi) == 4
-            @test firstindex(vi) == 1
-            @test lastindex(vi) == 4
-            vi[2] = [1, 2, 3]
-            for i in 1:4
-                @test vi[i] isa T{U}
-                @test vi[i] == vecmat[i, :]
-            end
-            @test vi.m isa Polymake.Matrix{U}
-            @test matrix(vi) isa (U == Polymake.Rational ? fmpq_mat : fmpz_mat)
-            @test matrix(vi) == matrix(QQ, vecmat)
-            @test Oscar.matrix_for_polymake(vi) == vecmat
-            @test VectorIterator{T{U}}(matrix(vi)).m == vecmat
-        end
-        @test VectorIterator(vecmat) isa VectorIterator{PointVector{Polymake.Rational}}
-    end
     
     @testset "SubObjectIterator" begin
         # SubObjectIterator is flexible due to the `Function` object it stores, which is used for `getindex`.
