d32dec997f517047a75028f459950b660f9ab1fb
Lars Kastner <lars.kastner@yahoo.dk>
2021-12-14 14:11:39 +0100

    TropicalGeometry: Add documentation to intersect function

--- src/TropicalGeometry/TropicalVariety/standard_constructions.jl
+++ src/TropicalGeometry/TropicalVariety/standard_constructions.jl
@@ -1,3 +1,32 @@
+@doc Markdown.doc"""
+    intersect(TV1, TV2)
+
+Intersect two tropical varieties.
+
+# Examples
+```jldoctest
+julia> T = tropical_numbers(min)
+Tropical ring (min)
+
+julia> Txy,(x,y) = T["x","y"]
+(Multivariate Polynomial Ring in x, y over Tropical ring (min), AbstractAlgebra.Generic.MPoly{Oscar.TropicalNumbersElem{typeof(min)}}[x, y])
+
+julia> f1 = x+y+1
+x + y + (1)
+
+julia> f2 = x^2+y^2+T(-6)
+x^2 + y^2 + (-6)
+
+julia> hyp1 = TropicalHypersurface(f1)
+A min tropical hypersurface embedded in 2-dimensional Euclidian space
+
+julia> hyp2 = TropicalHypersurface(f2)
+A min tropical hypersurface embedded in 2-dimensional Euclidian space
+
+julia> tv12 = intersect(hyp1, hyp2)
+A min tropical variety of dimension 1 embedded in 2-dimensional Euclidian space
+```
+"""
 function intersect(TV1::TropicalVarietySupertype{M, EMB}, TV2::TropicalVarietySupertype{M, EMB}) where {M, EMB}
     pm_tv1 = pm_object(TV1)
     pm_tv2 = pm_object(TV2)
