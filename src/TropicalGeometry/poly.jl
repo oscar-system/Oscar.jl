49bc6c70a2110767339bf8518e221cf153aef11a
Yue Ren <yue.ren.kl@gmail.com>
2021-12-15 14:11:56 +0000

    fix: tropical_polynomial coefficients depend on min-max convention

--- src/TropicalGeometry/poly.jl
+++ src/TropicalGeometry/poly.jl
@@ -7,23 +7,37 @@ If M=max, returns a polynomial over the max-plus semiring.
 
 # Examples
 ```jldoctest
-julia> K = PadicField(7, 2);
+julia> K = PadicField(7, 2)
+Field of 7-adic numbers
 
-julia> Kxy, (x,y) = K["x", "y"];
+julia> Kxy, (x,y) = K["x", "y"]
+(Multivariate Polynomial Ring in x, y over Field of 7-adic numbers, AbstractAlgebra.Generic.MPoly{padic}[x, y])
 
-julia> f = 7*x+y+49;
+julia> f = 7*x+y+49
+(7^1 + O(7^3))*x + y + 7^2 + O(7^4)
 
 julia> tropical_polynomial(f,min)
+(1)*x + y + (2)
+
+julia> tropical_polynomial(f,max)
+(-1)*x + y + (-2)
 ```
 """
 function tropical_polynomial(f::AbstractAlgebra.Generic.MPoly{<:RingElement}, M::Union{typeof(min),typeof(max)})
     T = tropical_numbers(M)
+    if M==min
+        s=1
+    else
+        s=-1
+    end
+    println(s)
+
     Tx = PolynomialRing(T,[repr(x) for x in gens(parent(f))])
     tropf = inf(T)
 
     if base_ring(parent(f)) isa NonArchLocalField
         for expv in exponent_vectors(f)
-            tropf = tropf + T(valuation(coeff(f,expv)))*monomial(Tx[1],expv)
+            tropf = tropf + T(s*valuation(coeff(f,expv)))*monomial(Tx[1],expv)
         end
     else
         for expv in exponent_vectors(f)
