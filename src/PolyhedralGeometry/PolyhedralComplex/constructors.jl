504e379bd0f09c62c97a423f495ae84b1f85f921
Lars Kastner <lars.kastner@yahoo.dk>
2021-12-15 14:22:39 +0100

    PolyhedralGeometry: Access to basic properties of PolyhedralComplex

--- src/PolyhedralGeometry/PolyhedralComplex/constructors.jl
+++ src/PolyhedralGeometry/PolyhedralComplex/constructors.jl
@@ -15,13 +15,15 @@ pm_object(pc::PolyhedralComplex) = pc.pm_complex
 
 
 @doc Markdown.doc"""
-    PolyhedralComplex(Polytopes, Points, Rays, Lineality)
+    PolyhedralComplex(Polytopes, VR, far_vertices, Lineality)
 
 # Arguments
 - `Polytopes::IncidenceMatrix`: An incidence matrix; there is a 1 at position
   (i,j) if the ith polytope contains point j and 0 otherwise.
-- `Points::Matrix`: The points whose convex hulls make up the polyhedral
-  complex.
+- `VR::Matrix`: The points whose convex hulls make up the polyhedral
+  complex. This matrix also contains the far vertices.
+- `far_vertices::Vector{Int}`: Vector containing the indices of the rows
+  corresponding to the far vertices in `VR`.
 
 A polyhedral complex formed from points, rays, and lineality combined into
 polyhedra indicated by an incidence matrix, where the columns represent the
@@ -35,30 +37,34 @@ julia> IM = IncidenceMatrix([[1,2,3],[1,3,4]])
 [1, 3, 4]
 
 
-julia> V = [0 0; 1 0; 1 1; 0 1]
+julia> VR = [0 0; 1 0; 1 1; 0 1]
 4×2 Matrix{Int64}:
  0  0
  1  0
  1  1
  0  1
 
-julia> PC = PolyhedralComplex(IM, V)
+julia> PC = PolyhedralComplex(IM, VR)
 A polyhedral complex in ambient dimension 2
 ```
 """
-function PolyhedralComplex(Incidence::IncidenceMatrix, V::Union{SubObjectIterator{<:PointVector}, Oscar.MatElem,AbstractMatrix}, R::Union{SubObjectIterator{<:RayVector}, Oscar.MatElem,AbstractMatrix, Nothing} = nothing, L::Union{SubObjectIterator{<:RayVector}, Oscar.MatElem, AbstractMatrix, Nothing} = nothing)
-    VM = matrix_for_polymake(V)
-    RM = isnothing(R) || isempty(R) ? Polymake.Matrix{Polymake.Rational}(undef, 0, size(VM, 2)) : matrix_for_polymake(R)
-    LM = isnothing(L) || isempty(L) ? Polymake.Matrix{Polymake.Rational}(undef, 0, size(VM, 2)) : matrix_for_polymake(L)
-
+function PolyhedralComplex(Incidence::IncidenceMatrix, VR::Union{SubObjectIterator{<:Union{PointVector,PointVector}}, Oscar.MatElem, AbstractMatrix}, far_vertices::Union{Vector{Int}, Nothing} = nothing, L::Union{SubObjectIterator{<:RayVector}, Oscar.MatElem, AbstractMatrix, Nothing} = nothing)
+    VRM = matrix_for_polymake(VR)
+    LM = isnothing(L) || isempty(L) ? Polymake.Matrix{Polymake.Rational}(undef, 0, size(VRM, 2)) : matrix_for_polymake(L)
+    
     # Rays and Points are homogenized and combined and
+    points = homogenize(VRM, 1)
+    # If some vertices are far vertices, give them a leading 0
+    if !isnothing(far_vertices)
+        points[far_vertices,1] .= 0
+    end
+
     # Lineality is homogenized
-    points = stack(homogenize(VM, 1), homogenize(RM, 0))
     lineality = homogenize(LM, 0)
 
     PolyhedralComplex(Polymake.fan.PolyhedralComplex{Polymake.Rational}(
-        POINTS = matrix_for_polymake(points),
-        INPUT_LINEALITY = matrix_for_polymake(lineality),
+        POINTS = points,
+        INPUT_LINEALITY = lineality,
         INPUT_CONES = Incidence,
     ))
 end
--- src/PolyhedralGeometry/PolyhedralComplex/properties.jl
+++ src/PolyhedralGeometry/PolyhedralComplex/properties.jl
@@ -27,3 +27,74 @@ julia> ambient_dim(PC)
 """
 ambient_dim(PC::PolyhedralComplex) = Polymake.fan.ambient_dim(pm_object(PC))
 
+
+@doc Markdown.doc"""
+    vertices(as, PC)
+
+Return an iterator over the vertices of `PC` in the format defined by `as`.
+
+Optional arguments for `as` include
+* `PointVector`.
+
+# Examples
+The following code computes the vertices of the Minkowski sum of a triangle and
+a square:
+```jldoctest
+julia> P = simplex(2) + cube(2);
+
+julia> vertices(PointVector, P)
+5-element SubObjectIterator{PointVector{Polymake.Rational}}:
+ [-1, -1]
+ [2, -1]
+ [2, 1]
+ [-1, 2]
+ [1, 2]
+```
+"""
+vertices(as::Type{PointVector{T}}, PC::PolyhedralComplex) where T = SubObjectIterator{as}(pm_object(PC), _vertex_polyhedron, length(_vertex_indices(pm_object(PC))))
+
+
+function _all_vertex_indices(P::Polymake.BigObject)
+    vi = Polymake.get_attachment(P, "_vertex_indices")
+    if isnothing(vi)
+        A = P.VERTICES
+        vi = Polymake.Vector{Polymake.to_cxx_type(Int64)}(Vector(1:Polymake.nrows(A)))
+        Polymake.attach(P, "_all_vertex_indices", vi)
+    end
+    return vi
+end
+
+function _vertex_or_ray_polyhedron(::Type{Union{PointVector{T}, RayVector{T}}}, P::Polymake.BigObject, i::Base.Integer) where T 
+    A = P.VERTICES
+    if iszero(A[_all_vertex_indices(P)[i],1])
+        return RayVector{T}(P.VERTICES[_all_vertex_indices(P)[i], 2:end])
+    else
+        return PointVector{T}(P.VERTICES[_all_vertex_indices(P)[i], 2:end])
+    end
+end
+
+vertices(as::Type{Union{RayVector{T}, PointVector{T}}}, PC::PolyhedralComplex) where T = SubObjectIterator{as}(pm_object(PC), _vertex_or_ray_polyhedron, length(_all_vertex_indices(pm_object(PC))))
+
+vertices(::Type{Union{RayVector, PointVector}}, PC::PolyhedralComplex) = vertices(Union{RayVector{Polymake.Rational}, PointVector{Polymake.Rational}}, PC)
+
+vertices(PC::PolyhedralComplex) = vertices(Union{PointVector, RayVector}, PC)
+
+_maximal_polytope(::Type{Polyhedron}, PC::Polymake.BigObject, i::Base.Integer) = Polyhedron(Polymake.fan.polytope(PC, i-1))
+
+
+@doc Markdown.doc"""
+    maximal_polytopes(PC::PolyhedralComplex)
+
+Return the maximal polytopes of `PC`
+"""
+maximal_polytopes(PC::PolyhedralComplex) = SubObjectIterator{Polyhedron}(pm_object(PC), _maximal_polytope, nmaximal_polytopes(PC))
+
+
+@doc Markdown.doc"""
+    nmaximal_polytopes(PC::PolyhedralComplex)
+
+Return the number of maximal polytopes of `PC`
+"""
+nmaximal_polytopes(PC::PolyhedralComplex) = pm_object(PC).N_MAXIMAL_POLYTOPES
+
+
