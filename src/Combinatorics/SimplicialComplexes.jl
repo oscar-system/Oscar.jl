4da06ee5590297e4a6c62714e0de72da673d68d4
Michael Joswig <joswig@math.tu-berlin.de>
2021-12-21 14:07:30 +0100

    SimplicialComplex: Stanley-Reisner ring; docstrings

--- src/Combinatorics/SimplicialComplexes.jl
+++ src/Combinatorics/SimplicialComplexes.jl
@@ -3,19 +3,20 @@ import Oscar: Polymake, pm_object
 
 export
     SimplicialComplex,
-    bettinumbers,
+    betti_numbers,
     dim,
-    eulercharacteristic,
+    euler_characteristic,
     f_vector,
     h_vector,
     minimalnonfaces,
-    stanleyreisnerideal,
+    stanley_reisner_ideal,
+    stanley_reisner_ring,
     nvertices,
     load_simplicialcomplex,
     save_simplicialcomplex,
     complexprojectiveplane,
     realprojectiveplane,
-    kleinbottle,
+    klein_bottle,
     torus # requires a distinction from, e.g., an algebraic group
 
 ################################################################################
@@ -74,17 +75,52 @@ end
 ##  Properties
 ################################################################################
 
+@doc Markdown.doc"""
+    dim(SimplicialComplex)
+
+Dimension of an abstract simplicial complex.
+"""
 dim(K::SimplicialComplex) = pm_object(K).DIM
-nvertices(K::SimplicialComplex) = pm_object(K).N_VERTICES
-f_vector(K::SimplicialComplex) = Vector{Int}(pm_object(K).F_VECTOR)
-h_vector(K::SimplicialComplex) = Vector{Int}(pm_object(K).H_VECTOR)
-bettinumbers(K::SimplicialComplex) = Polymake.topaz.betti_numbers(pm_object(K))
-eulercharacteristic(K::SimplicialComplex) = pm_object(K).EULER_CHARACTERISTIC
 
 @doc Markdown.doc"""
-    minimalnonfaces(K::SimplicialComplex)
+    nvertices(SimplicialComplex)
 
-Compute the minimal non-faces of K.
+Number of vertices of an abstract simplicial complex.
+"""
+nvertices(K::SimplicialComplex) = pm_object(K).N_VERTICES
+
+@doc Markdown.doc"""
+    f_vector(SimplicialComplex)
+
+Face vector (number of faces per dimension) of an abstract simplicial complex.
+"""
+f_vector(K::SimplicialComplex) = Vector{Int}(pm_object(K).F_VECTOR)
+
+@doc Markdown.doc"""
+    f_vector(SimplicialComplex)
+
+H-vector of an abstract simplicial complex.
+"""
+h_vector(K::SimplicialComplex) = Vector{Int}(pm_object(K).H_VECTOR)
+
+@doc Markdown.doc"""
+    betti_numbers(SimplicialComplex)
+
+Rational Betti numbers of an abstract simplicial complex.
+"""
+betti_numbers(K::SimplicialComplex) = Polymake.topaz.betti_numbers(pm_object(K))
+
+@doc Markdown.doc"""
+    euler_characteristic(SimplicialComplex)
+
+Euler characteristic of an abstract simplicial complex.
+"""
+euler_characteristic(K::SimplicialComplex) = pm_object(K).EULER_CHARACTERISTIC
+
+@doc Markdown.doc"""
+    minimalnonfaces(SimplicialComplex)
+
+Minimal non-faces of an abstract simplicial complex.
 
 # Example
 ```jldoctest
@@ -103,30 +139,71 @@ function minimalnonfaces(K::SimplicialComplex)
 end
 
 @doc Markdown.doc"""
-    stanleyreisnerideal(K::SimplicialComplex)
+    stanley_reisner_ideal(SimplicialComplex)
 
-Compute the Stanley-Reisner ideal of K.
+Stanley-Reisner ideal of an abstract simplicial complex.
 
 # Example
 ```jldoctest
-julia> stanleyreisnerideal(realprojectiveplane())
+julia> stanley_reisner_ideal(realprojectiveplane())
 ideal(x1*x2*x3, x1*x2*x4, x1*x5*x6, x2*x5*x6, x1*x3*x6, x1*x4*x5, x3*x4*x5, x3*x4*x6, x2*x3*x5, x2*x4*x6)
 ```
 """
-function stanleyreisnerideal(K::SimplicialComplex)
+function stanley_reisner_ideal(K::SimplicialComplex)
     n = nvertices(K)
     R, () = PolynomialRing(ZZ, n)
-    SR = [ R([ZZ(1)], [_characteristicvector(f,n)]) for f in minimalnonfaces(K) ]
-    return ideal(SR)
+    return ideal([ R([ZZ(1)], [_characteristicvector(f,n)]) for f in minimalnonfaces(K) ])
 end
+
+@doc Markdown.doc"""
+    stanley_reisner_ring(SimplicialComplex)
+
+Stanley-Reisner ring of an abstract simplicial complex.
+
+# Example
+```jldoctest
+julia> K = SimplicialComplex([[1,2,3],[2,3,4]]);
+
+julia> stanley_reisner_ring(K)
+(Quotient of Multivariate Polynomial Ring in x1, x2, x3, x4 over Integer Ring by ideal(x1*x4), Map from
+Multivariate Polynomial Ring in x1, x2, x3, x4 over Integer Ring to Quotient of Multivariate Polynomial Ring in x1, x2, x3, x4 over Integer Ring by ideal(x1*x4) defined by a julia-function with inverse)
+"""
+function stanley_reisner_ring(K::SimplicialComplex)
+    I = stanley_reisner_ideal(K)
+    return quo(base_ring(I),I)
+end
+
 
 ################################################################################
 ##  Standard examples
 ################################################################################
 
+@doc Markdown.doc"""
+    torus()
+
+Császár's 7-vertex triangulation of the torus (surface).
+"""
 torus() = SimplicialComplex(Polymake.topaz.torus())
-kleinbottle() = SimplicialComplex(Polymake.topaz.klein_bottle())
+
+@doc Markdown.doc"""
+    klein_bottle()
+
+9-vertex triangulation of the Klein bottle.
+"""
+klein_bottle() = SimplicialComplex(Polymake.topaz.klein_bottle())
+
+@doc Markdown.doc"""
+    realprojectiveplane()
+
+6-vertex triangulation of the real projective plane.
+"""
 realprojectiveplane() = SimplicialComplex(Polymake.topaz.real_projective_plane())
+
+@doc Markdown.doc"""
+    complexprojectiveplane()
+
+9-vertex triangulation of the complex projective plane.
+"""
 complexprojectiveplane() = SimplicialComplex(Polymake.topaz.complex_projective_plane())
 
 ###############################################################################
