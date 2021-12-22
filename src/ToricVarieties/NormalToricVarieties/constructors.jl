48a68f78a48163cc65c2984729969365bfab764c
Martin Bies <MBies87@googlemail.com>
2021-12-22 14:54:32 +0100

    Cache properties of del Pezzo surfaces upon construction

--- src/ToricVarieties/NormalToricVarieties/constructors.jl
+++ src/ToricVarieties/NormalToricVarieties/constructors.jl
@@ -301,30 +301,96 @@ A normal toric variety corresponding to a polyhedral fan in ambient dimension 2
 ```
 """
 function del_pezzo(b::Int)
+    # check for valid input
     if b < 0
         throw(ArgumentError("Number of blowups for construction of delPezzo surfaces must be non-negative."))
-    end
-    if b == 0 
-        return toric_projective_space(2)
-    end
-    if b == 1
-        rays = [1 0; 0 1; -1 0; -1 -1]
-        cones = IncidenceMatrix([[1,2],[2,3],[3,4],[4,1]])
-        return NormalToricVariety(PolyhedralFan(rays, cones))
-    end
-    if b == 2
-        rays = [1 0; 0 1; -1 0; -1 -1; 0 -1]
-        cones = IncidenceMatrix([[1,2],[2,3],[3,4],[4,5],[5,1]])
-        return NormalToricVariety(PolyhedralFan(rays, cones))
-    end
-    if b == 3
-        rays = [1 0; 1 1; 0 1; -1 0; -1 -1; 0 -1]
-        cones = IncidenceMatrix([[1,2],[2,3],[3,4],[4,5],[5,6],[6,1]])
-        return NormalToricVariety(PolyhedralFan(rays, cones))
     end
     if b > 3
         throw(ArgumentError("delPezzo surfaces with more than 3 blowups are realized as subvarieties of toric ambient spaces. This is currently not supported."))
     end
+    
+    # special case of projective space
+    if b == 0 
+        return toric_projective_space(2)
+    end
+    
+    # construct the "true" toric del Pezzo surfaces
+    if b == 1
+        rays = [1 0; 0 1; -1 0; -1 -1]
+        cones = IncidenceMatrix([[1,2],[2,3],[3,4],[4,1]])
+    end
+    if b == 2
+        rays = [1 0; 0 1; -1 0; -1 -1; 0 -1]
+        cones = IncidenceMatrix([[1,2],[2,3],[3,4],[4,5],[5,1]])
+    end
+    if b == 3
+        rays = [1 0; 1 1; 0 1; -1 0; -1 -1; 0 -1]
+        cones = IncidenceMatrix([[1,2],[2,3],[3,4],[4,5],[5,6],[6,1]])
+    end
+    variety = NormalToricVariety(PolyhedralFan(rays, cones))
+    
+    # set properties
+    set_attribute!(variety, :isaffine, false)
+    set_attribute!(variety, :isprojective, true)
+    set_attribute!(variety, :isprojective_space, false)
+    set_attribute!(variety, :issmooth, true)
+    set_attribute!(variety, :iscomplete, true)
+    set_attribute!(variety, :hastorusfactor, false)
+    set_attribute!(variety, :isorbifold, true)
+    set_attribute!(variety, :issimplicial, true)
+    set_attribute!(variety, :isgorenstein, true)
+    set_attribute!(variety, :isq_gorenstein, true)
+    set_attribute!(variety, :isfano, true)
+    
+    # set attributes that depend on b
+    if b == 1
+        set_attribute!(variety, :euler_characteristic, 4)
+        set_attribute!(variety, :torusinvariant_divisor_group, free_abelian_group(4))    
+        ring = PolynomialRing(QQ, :x=>1:2, :e=>1, :x=>3)[1]
+        weights = [map_from_weil_divisors_to_class_group(variety)(x) for x in Hecke.gens(torusinvariant_divisor_group(variety))]
+        gens = Hecke.gens(cox_ring(variety))
+        set_attribute!(variety, :cox_ring, grade(ring,weights)[1])
+        set_attribute!(variety, :stanley_reisner_ideal, ideal([gens[1]*gens[3], gens[2]*gens[4]]))
+        set_attribute!(variety, :irrelevant_ideal, ideal([gens[3]*gens[4], gens[1]*gens[4], gens[1]*gens[2], gens[3]*gens[2]]))
+        set_attribute!(variety, :betti_number, [fmpz(1),fmpz(0),fmpz(2),fmpz(0),fmpz(1)])
+    end
+    if b == 2
+        set_attribute!(variety, :euler_characteristic, 5)
+        set_attribute!(variety, :torusinvariant_divisor_group, free_abelian_group(5))
+        ring = PolynomialRing(QQ, :x=>1:2, :e=>1, :x=>3, :e=>2)[1]
+        weights = [map_from_weil_divisors_to_class_group(variety)(x) for x in Hecke.gens(torusinvariant_divisor_group(variety))]
+        gens = Hecke.gens(cox_ring(variety))
+        set_attribute!(variety, :cox_ring, grade(ring,weights)[1])
+        set_attribute!(variety, :stanley_reisner_ideal, ideal([gens[1]*gens[3], gens[1]*gens[4], 
+                                                                                          gens[2]*gens[4], gens[2]*gens[5], gens[3]*gens[5]]))
+        set_attribute!(variety, :irrelevant_ideal, ideal([gens[3]*gens[4]*gens[5], gens[1]*gens[4]*gens[5], 
+                                                                                 gens[1]*gens[2]*gens[5], gens[1]*gens[2]*gens[3], gens[2]*gens[3]*gens[4]]))
+        set_attribute!(variety, :betti_number, [fmpz(1),fmpz(0),fmpz(3),fmpz(0),fmpz(1)])
+    end
+    if b == 3
+        set_attribute!(variety, :euler_characteristic, 6)
+        set_attribute!(variety, :torusinvariant_divisor_group, free_abelian_group(6))
+        ring = PolynomialRing(QQ, :x=>1, :e=>3, :x=>2, :e=>1, :x=>3, :e=>2)[1]
+        weights = [map_from_weil_divisors_to_class_group(variety)(x) for x in Hecke.gens(torusinvariant_divisor_group(variety))]
+        gens = Hecke.gens(cox_ring(variety))
+        set_attribute!(variety, :cox_ring, grade(ring,weights)[1])
+        set_attribute!(variety, :stanley_reisner_ideal, ideal([gens[1]*gens[3], gens[1]*gens[4], gens[1]*gens[5], gens[2]*gens[4], 
+                                                                                          gens[2]*gens[5], gens[2]*gens[6], gens[3]*gens[5], gens[3]*gens[6], gens[4]*gens[6]]))
+        set_attribute!(variety, :irrelevant_ideal, ideal([gens[3]*gens[4] *gens[5]*gens[6], gens[1]*gens[4] *gens[5]*gens[6],
+                                                                                 gens[1]*gens[2] *gens[5]*gens[6], gens[1]*gens[2] *gens[3]*gens[6],
+                                                                                 gens[1]*gens[2] *gens[3]*gens[4], gens[2]*gens[3] *gens[4]*gens[5]]))
+        set_attribute!(variety, :betti_number, [fmpz(1),fmpz(0),fmpz(4),fmpz(0),fmpz(1)])
+    end
+    
+    # set further attributes
+    set_attribute!(variety, :dim, 2)
+    set_attribute!(variety, :dim_of_torusfactor, 0)
+    set_attribute!(variety, :character_lattice, free_abelian_group(2))
+    set_attribute!(variety, :map_from_cartier_divisor_group_to_torus_invariant_divisor_group, Hecke.identity_map(torusinvariant_divisor_group(variety)))
+    set_attribute!(variety, :map_from_cartier_divisor_group_to_picard_group, map_from_weil_divisors_to_class_group(variety))
+    
+    # return the result
+    return variety    
 end
 export del_pezzo
 
