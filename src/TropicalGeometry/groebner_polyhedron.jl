b04325b470bd75c47ee8bfa1cbb6e075773b1f80
Yue Ren <yue.ren.kl@gmail.com>
2021-12-27 19:32:32 +0100

    TropicalGeometry: initial commit groebner_polyhedron.jl

---
+++ src/TropicalGeometry/groebner_polyhedron.jl
@@ -0,0 +1,47 @@
+###
+# Computing (tropical) Groebner polyhedra in Oscar
+# ================================================
+#
+# For a definition of tropical Groebner cones see Section 2.5 in:
+#   D. Maclagan, B. Sturmfels: Introduction to tropical geometry
+# To see how they can be computed using standard bases see:
+#   T. Markwig, Y. Ren: Computing tropical varieties over fields with valuation
+###
+
+
+#=======
+tropical Groebner polyhedron
+todo: proper documentation
+Example:
+
+val_2 = ValuationMap(QQ,2)
+Kx,(x,y,z) = PolynomialRing(QQ,3)
+w = [0,0,0]
+I = ideal([x+2*y,y+2*z])
+groebner_polyhedron(I,val_2,w)
+
+Kt,t = RationalFunctionField(QQ,"t")
+val_t = ValuationMap(Kt,t)
+Ktx,(x,y,z) = PolynomialRing(Kt,3)
+w = [0,0,0]
+I = ideal([x+t*y,y+t*z])
+groebner_polyhedron(I,val_t,w)
+=======#
+function groebner_polyhedron(I,val::ValuationMap{K,p} where {K,p},w::Vector{Int})
+  GB,LI = groebner_basis(I,val,w,return_lead=true)
+
+  A = zeros(Int,0,length(w))
+  b = zeros(Int,0)
+  for (f,lf) in zip(GB,LI)
+    leadexpv = exponent_vector(lf,1)
+    leadval = val(coeff(lf,1))
+    for (tailcoeff,tailexpv) in zip(coefficients(f),exponent_vectors(f))
+      tailval = val(tailcoeff)
+      A = vcat(A,transpose(tailexpv-leadexpv)) # todo: is there a better way of doing this line?
+      push!(b,tailval-leadval)
+    end
+  end
+
+  return Polyhedron(A,b)
+end
+export groebner_polyhedron
