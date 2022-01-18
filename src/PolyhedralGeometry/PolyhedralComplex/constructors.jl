71c8a6ddc8018dbb829c5f4b8460ca6310ee9494
Lars Kastner <lars.kastner@yahoo.dk>
2022-01-18 16:16:55 +0100

    PolyhedralGeometry: Incorporate second batch of comments by @fingolfin in #889

--- src/PolyhedralGeometry/PolyhedralComplex/constructors.jl
+++ src/PolyhedralGeometry/PolyhedralComplex/constructors.jl
@@ -15,15 +15,16 @@ pm_object(pc::PolyhedralComplex) = pc.pm_complex
 
 
 @doc Markdown.doc"""
-    PolyhedralComplex(Polytopes, VR, far_vertices, Lineality)
+    PolyhedralComplex(polyhedra, vr, far_vertices, L)
 
 # Arguments
-- `Polytopes::IncidenceMatrix`: An incidence matrix; there is a 1 at position
+- `polyhedra::IncidenceMatrix`: An incidence matrix; there is a 1 at position
   (i,j) if the ith polytope contains point j and 0 otherwise.
-- `VR::Matrix`: The points whose convex hulls make up the polyhedral
+- `vr::Matrix`: The points whose convex hulls make up the polyhedral
   complex. This matrix also contains the far vertices.
 - `far_vertices::Vector{Int}`: Vector containing the indices of the rows
-  corresponding to the far vertices in `VR`.
+  corresponding to the far vertices in `vr`.
+- `L::Matrix`: Generators of the lineality space of the polyhedral complex.
 
 A polyhedral complex formed from points, rays, and lineality combined into
 polyhedra indicated by an incidence matrix, where the columns represent the
@@ -37,22 +38,28 @@ julia> IM = IncidenceMatrix([[1,2,3],[1,3,4]])
 [1, 3, 4]
 
 
-julia> VR = [0 0; 1 0; 1 1; 0 1]
+julia> vr = [0 0; 1 0; 1 1; 0 1]
 4×2 Matrix{Int64}:
  0  0
  1  0
  1  1
  0  1
 
-julia> PC = PolyhedralComplex(IM, VR)
+julia> PC = PolyhedralComplex(IM, vr)
 A polyhedral complex in ambient dimension 2
 ```
 """
-function PolyhedralComplex(Incidence::IncidenceMatrix, VR::Union{SubObjectIterator{<:Union{PointVector,PointVector}}, Oscar.MatElem, AbstractMatrix}, far_vertices::Union{Vector{Int}, Nothing} = nothing, L::Union{SubObjectIterator{<:RayVector}, Oscar.MatElem, AbstractMatrix, Nothing} = nothing)
-    LM = isnothing(L) || isempty(L) ? Polymake.Matrix{Polymake.Rational}(undef, 0, size(VR, 2)) : L
+function PolyhedralComplex(
+                polyhedra::IncidenceMatrix, 
+                vr::Union{SubObjectIterator{<:Union{PointVector,PointVector}}, Oscar.MatElem, AbstractMatrix}, 
+                far_vertices::Union{Vector{Int}, Nothing} = nothing, 
+                L::Union{SubObjectIterator{<:RayVector}, 
+                Oscar.MatElem, AbstractMatrix, Nothing} = nothing
+            )
+    LM = isnothing(L) || isempty(L) ? Polymake.Matrix{Polymake.Rational}(undef, 0, size(vr, 2)) : L
 
     # Rays and Points are homogenized and combined and
-    points = homogenize(VR, 1)
+    points = homogenize(vr, 1)
     # If some vertices are far vertices, give them a leading 0
     if !isnothing(far_vertices)
         points[far_vertices,1] .= 0
@@ -64,7 +71,7 @@ function PolyhedralComplex(Incidence::IncidenceMatrix, VR::Union{SubObjectIterat
     PolyhedralComplex(Polymake.fan.PolyhedralComplex{Polymake.Rational}(
         POINTS = points,
         INPUT_LINEALITY = lineality,
-        INPUT_CONES = Incidence,
+        INPUT_CONES = polyhedra,
     ))
 end
 
