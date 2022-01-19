7f470004880679264481c01f0ee81df15d7459f2
Lars Kastner <lars.kastner@yahoo.dk>
2022-01-20 00:04:04 +0100

    Serialization: First steps with state
    - Introduce (Des|Se)rializerState for avoiding duplication and for
      storing context
    - Refactor previous simple interface for polymake serialization
    - Use JSON, but other formats should be possible

---
+++ src/Serialization/PolyhedralGeometry.jl
@@ -0,0 +1,217 @@
+using JSON
+
+function bigobject_to_dict(bo::Polymake.BigObject)
+    serialized = Polymake.call_function(Symbol("Core::Serializer"), :serialize, bo)
+    jsonstr = Polymake.call_function(:common, :encode_json, serialized)
+    return JSON.parse(jsonstr)
+end
+
+function save_intern(s::SerializerState, p::T) where {T <: Union{
+                    Cone, 
+#                    LinearProgram,
+                    PolyhedralComplex, 
+                    Polyhedron, 
+                    PolyhedralFan,
+                    SimplicialComplex,
+                    SubdivisionOfPoints
+                }
+            }
+    return bigobject_to_dict(pm_object(p))
+end
+
+function load_intern(s::DeserializerState, p::Type{T}, dict::Dict) where {T <: Union{
+                    Cone, 
+#                    LinearProgram,
+                    PolyhedralComplex, 
+                    Polyhedron, 
+                    PolyhedralFan,
+                    SimplicialComplex,
+                    SubdivisionOfPoints
+                }
+            }
+    bigobject = Polymake.call_function(:common, :deserialize_json_string, json(dict))
+    return p(bigobject)
+end
+
+##############################################################################
+function save_intern(s::SerializerState, lp::LinearProgram)
+    lpcoeffs = lp.polymake_lp.LINEAR_OBJECTIVE
+    serialized = Polymake.call_function(Symbol("Core::Serializer"), :serialize, lpcoeffs)
+    jsonstr = Polymake.call_function(:common, :encode_json, serialized)
+    return Dict(
+        :feasible_region => save_intern(s, lp.feasible_region),
+        :convention => lp.convention,
+        :lpcoeffs => JSON.parse(jsonstr)
+    )
+end
+
+function load_intern(s::DeserializerState, ::Type{LinearProgram}, dict::Dict)
+    fr = load_intern(s, Polyhedron, dict[:feasible_region])
+    conv = dict[:convention]
+    lpcoeffs = Polymake.call_function(:common, :deserialize_json_string, json(dict[:lpcoeffs]))
+    all = Polymake._lookup_multi(pm_object(fr), "LP")
+    index = 0
+    for i in 1:length(all)
+        if all[i].LINEAR_OBJECTIVE == lpcoeffs
+            index = i
+            break
+        end
+    end
+    lp = Polymake._lookup_multi(pm_object(fr), "LP", index-1)
+    return LinearProgram(fr, lp, Symbol(conv))
+end
+
+
+
+##############################################################################
+"""
+    load_from_polymake(::Type{Cone}, jsondict::Dict{String, Any})
+
+Load a cone stored in JSON format, given the filename as input.
+"""
+function load_from_polymake(::Type{Cone}, jsondict::Dict{String, Any})
+    bigobject = Polymake.call_function(:common, :deserialize_json_string, json(jsondict))
+    typename = Polymake.type_name(bigobject)
+    if typename != "Cone<Rational>"
+        throw(ArgumentError("Loaded object is not of polymake type Cone, it has type " * typename))
+    end
+    return Cone(bigobject)
+end
+
+
+##############################################################################
+"""
+    load_from_polymake(::Type{Polyhedron}, jsondict::Dict{String, Any})
+
+Load a polyhedron stored in JSON format, given the filename as input.
+"""
+function load_from_polymake(::Type{Polyhedron}, jsondict::Dict{String, Any})
+    bigobject = Polymake.call_function(:common, :deserialize_json_string, json(jsondict))
+    typename = Polymake.type_name(bigobject)
+    if typename != "Polytope<Rational>"
+        throw(ArgumentError("Loaded object is not of polymake type Polytope, it has type " * typename))
+    end
+    return Polyhedron(bigobject)
+end
+
+
+
+##############################################################################
+
+"""
+    load_from_polymake(::Type{PolyhedralFan}, jsondict::Dict{String, Any})
+
+Load a polyhedral fan stored in JSON format, given the filename as input.
+"""
+function load_from_polymake(::Type{PolyhedralFan}, jsondict::Dict{String, Any})
+    bigobject = Polymake.call_function(:common, :deserialize_json_string, json(jsondict))
+    typename = Polymake.type_name(bigobject)
+    if typename != "PolyhedralFan<Rational>"
+        throw(ArgumentError("Loaded object is not of polymake type PolyhedralFan, it has type " * typename))
+    end
+    return PolyhedralFan(bigobject)
+end
+
+
+
+
+##############################################################################
+
+"""
+    load_from_polymake(::Type{LinearProgram}, jsondict::Dict{String, Any})
+
+Load a linear program stored in JSON format, given the filename as input.
+"""
+function load_from_polymake(::Type{LinearProgram}, jsondict::Dict{String, Any})
+    fr = Polymake.call_function(:common, :deserialize_json_string, json(jsondict))
+    typename = Polymake.type_name(fr)
+    if typename != "Polytope<Rational>"
+        throw(ArgumentError("Loaded object is not of polymake type LinearProgram."))
+    end
+    if !Polymake.exists(fr, "LP")
+        throw(ArgumentError("Incomplete or corrupt data file."))
+    end
+    lp = fr.LP
+    conv = Polymake.get_attachment(lp, "convention")
+    if isnothing(conv)
+        throw(ArgumentError("Incomplete or corrupt data file."))
+    end
+    if conv == "max"
+        return LinearProgram(Polyhedron(fr), lp, :max)
+    elseif conv == "min"
+        return LinearProgram(Polyhedron(fr), lp, :min)
+    end
+end
+
+
+
+
+
+
+##############################################################################
+"""
+    load_from_polymake(::Type{SubdivisionOfPoints}, jsondict::Dict{String, Any})
+
+Load a subdivision of points stored in JSON format, given the filename as input.
+"""
+function load_from_polymake(::Type{SubdivisionOfPoints}, jsondict::Dict{String, Any})
+    bigobject = Polymake.call_function(:common, :deserialize_json_string, json(jsondict))
+    typename = Polymake.type_name(bigobject)
+    if typename != "SubdivisionOfPoints<Rational>"
+        throw(ArgumentError("Loaded object is not of polymake type SubdivisionOfPoints, it has type " * typename))
+    end
+    return SubdivisionOfPoints(bigobject)
+end
+
+
+
+##############################################################################
+
+"""
+    load_from_polymake(::Type{PolyhedralComplex}, jsondict::Dict{String, Any})
+
+Load a polyhedral complex stored in JSON format, given the filename as input.
+"""
+function load_from_polymake(::Type{PolyhedralComplex}, jsondict::Dict{String, Any})
+    bigobject = Polymake.call_function(:common, :deserialize_json_string, json(jsondict))
+    typename = Polymake.type_name(bigobject)
+    if typename != "PolyhedralComplex<Rational>"
+        throw(ArgumentError("Loaded object is not of polymake type PolyhedralComplex, it has type " * typename))
+    end
+    return PolyhedralComplex(bigobject)
+end
+
+
+
+
+
+
+# Distinguish between the various polymake datatypes.
+function load_polymake_object(jsondict::Dict{String, Any})
+    typename = jsondict["_type"]
+    if typename == "polytope::Cone<Rational>"
+        return load_from_polymake(Cone, jsondict)
+    elseif typename == "polytope::Polytope<Rational>"
+        if haskey(jsondict, "is_OSCAR_LP")
+            load_from_polymake(LinearProgram, jsondict)
+        else
+            load_from_polymake(Polyhedron, jsondict)
+        end
+    elseif typename == "fan::PolyhedralFan<Rational>"
+        return load_from_polymake(PolyhedralFan, jsondict)
+    elseif typename == "fan::PolyhedralComplex<Rational>"
+        return load_from_polymake(PolyhedralComplex, jsondict)
+    elseif typename == "fan::SubdivisionOfPoints<Rational>"
+        return load_from_polymake(SubdivisionOfPoints, jsondict)
+    elseif typename == "topaz::SimplicialComplex"
+        return load_from_polymake(SimplicialComplex, jsondict)
+    elseif typename == "common::GraphAdjacency<Undirected>"
+        return load_from_polymake(Graphs.Graph{Graphs.Undirected}, jsondict)
+    elseif typename == "common::GraphAdjacency<Directed>"
+        return load_from_polymake(Graphs.Graph{Graphs.Directed}, jsondict)
+    else 
+        throw(ArgumentError("Invalid polymake object."))
+    end
+end
+
+
---
+++ src/Serialization/main.jl
@@ -0,0 +1,137 @@
+using JSON
+using Pkg
+
+# struct which tracks state for (de)serialization
+struct SerializerState
+    # dict to track already serialized objects
+    objmap::IdDict{Any,Int}
+
+    # TODO: if we don't want to produce intermediate dictionaries (which is a lot of overhead), we would probably store an `IO` object here
+    # io::IO
+end
+
+function SerializerState()
+    return SerializerState(IdDict{Any, Int}())
+end
+
+struct DeserializerState
+    objs::Vector{Any}  # or perhaps Dict{Int,Any} to be resilient against corrupts/malicious files using huge ids
+end
+
+function DeserializerState()
+    return DeserializerState(Vector{Any}())
+end
+
+
+const backref_sym = Symbol("#backref")
+function backref(ref::Int)
+   return Dict(
+     :type=>backref_sym,
+     :version=>1, # ???
+     :id=>ref,
+     )
+end
+
+################################################################################
+# Some setup for encoding types as strings and back and for getting the version
+# info.
+const typeMap = Dict{Type, String}([
+    Int => "Base.Int",
+    Cone => "Oscar.Cone",
+    Polyhedron => "Oscar.Polyhedron",
+    PolyhedralFan => "Oscar.PolyhedralFan",
+    PolyhedralComplex => "Oscar.PolyhedralComplex",
+    SubdivisionOfPoints => "Oscar.SubdivisionOfPoints",
+    Graphs.Graph{Graphs.Undirected} => "Oscar.Graphs.Graph{Oscar.Graphs.Undirected}",
+    Graphs.Graph{Graphs.Directed} => "Oscar.Graphs.Graph{Oscar.Graphs.Directed}",
+    SimplicialComplex => "Oscar.SimplicialComplex",
+    LinearProgram => "Oscar.LinearProgram"
+])
+
+const reverseTypeMap = Dict{String, Type}(value => key for (key, value) in typeMap)
+
+function get_version_info()
+    pkginfo = [v for (p,v) in Pkg.dependencies() if v.name == "Oscar"]
+    pkginfo = pkginfo[1]
+    result = (:Oscar => ["https://github.com/oscar-system/Oscar.jl", pkginfo.version])
+end
+const versionInfo = get_version_info()
+
+
+################################################################################
+# High level
+function save(s::SerializerState, obj::T) where T
+    if !isprimitivetype(T) && !Base.issingletontype(T)
+        # if obj is already serialzed, just output
+        # a backref
+        ref = get(s.objmap, obj, nothing)
+        ref !== nothing && return backref(ref)
+        # otherwise, 
+        ref = s.objmap[obj] = length(s.objmap)
+    else
+        ref = -1
+    end
+    # invoke the actual serializer
+    typestr = typeMap[T]
+    return Dict(
+        :_ns => versionInfo,
+        :type => typestr,
+        :id => ref,
+        :data => save_intern(s, obj)
+    )
+end
+
+
+function load(s::DeserializerState, dict::Dict)
+    haskey(dict, :_ns) || throw(ArgumentError("Namespace is missing"))
+    _ns = dict[:_ns]
+    if haskey(_ns, :polymake)
+        # If this is a polymake file
+        return load_from_polymake(dict)
+    end
+    haskey(_ns, :Oscar) || throw(ArgumentError("Not an Oscar object"))
+    typestr = dict[:type]
+    id = dict[:id]
+    # TODO: deal with versions? enforce their presence?
+
+    if typestr == backref_sym
+        return s.objs[id]
+    end
+
+    T = reverseTypeMap[typestr]
+
+    Base.issingletontype(T) && return T()
+
+    # TODO: offer a generic handler for primitive
+    # types e.g. storing them as byte strings or so
+    #Base.isprimitivetype(T) && return load_primitivetype(T, dict) #
+
+    return load_intern(s, T, dict[:data])
+end
+
+################################################################################
+# Interacting with files
+function save(obj::T, filename::String) where T
+    state = SerializerState()
+    jsoncompatible = save(state, obj)
+    jsonstr = json(jsoncompatible)
+    open(filename, "w") do file
+        write(file, jsonstr)
+    end
+end
+
+
+function load(filename::String)
+    state = DeserializerState()
+    # Check for type of file somewhere here?
+    jsondict = JSON.parsefile(filename, dicttype=Dict{Symbol, Any})
+    return load(state, jsondict)
+end
+
+
+
+
+
+include("PolyhedralGeometry.jl")
+include("Combinatorics.jl")
+
