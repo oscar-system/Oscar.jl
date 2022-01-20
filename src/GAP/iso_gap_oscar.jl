36a8bbb9f2b4189bb32497afd795a48eb55a1edc
ThomasBreuer <sam@math.rwth-aachen.de>
2022-01-21 00:14:15 +0100

    next iteration for `iso_oscar_gap` and `iso_gap_oscar`
    - changed `iso_oscar_gap` and `iso_gap_oscar` for finite fields such that
      finite `AlgebraicExtension` fields on the GAP side are avoided,
      and incompatible defining polynomials on both sides are handled via
      an additional map on the Oscar side, using `embed`;
      this approach does not exclude meaningful fields on the GAP side,
      and simplifies the situation considerably
      (thanks for the suggestion, Max)
    
    - changed `iso_oscar_gap` and `iso_gap_oscar` methods in order to
      avoid code duplication; the idea is to have functions that have access
      to both domain and codomain of the intended mapping
    
    - rewrote more code to use `iso_oscar_gap`, in order to simplify the code;
      also moved `Groups/matrices/iso_oscar_gap.jl` to `GAP/iso_oscar_gap.jl`
    
    (Fields of the type `FqFiniteField` are currently not working,
    due to missing support for functions needed by `embed`;
    for examples, see the tests that are commented out.)

--- src/GAP/iso_gap_oscar.jl
+++ src/GAP/iso_gap_oscar.jl
@@ -19,11 +19,7 @@ end
 function _iso_gap_oscar(F::GAP.GapObj)
    if GAP.Globals.IsField(F)
      if GAPWrap.IsFinite(F)
-       if GAP.Globals.IsPrimeField(F)
-         return _iso_gap_oscar_field_finite_prime(F)
-       else
-         return _iso_gap_oscar_field_finite_ext(F)
-       end
+       return _iso_gap_oscar_field_finite(F)
      else
        if GAP.Globals.IsRationals(F)
          return _iso_gap_oscar_field_rationals(F)
@@ -38,110 +34,32 @@ function _iso_gap_oscar(F::GAP.GapObj)
    error("no method found")
 end
 
-# For finite prime fields, choose `GF(p)`
-# (of type `Nemo.GaloisField` if `p` is small (fits into `UInt64`),
-# `Nemo.GaloisFmpzField` otherwise)).
-function _iso_gap_oscar_field_finite_prime(F::GAP.GapObj)
-   p = characteristic(F)  # of type `fmpz`
-   if p < fmpz(2)^64
-     p = UInt64(p)
-   end
-   G = GF(p)
-   e = GAPWrap.One(F)
-
-   f(x) = G(fmpz(GAPWrap.IntFFE(x)))
-   finv(x::Union{Nemo.gfp_elem, Nemo.gfp_fmpz_elem}) = GAP.Obj(lift(x))*e
-
-   return MapFromFunc(f, finv, F, G)
-end
-
-# For finite non-prime fields, choose `FiniteField(pol, "z")`
-# (of type `FqNmodFiniteField` if `p` is small, `FqFiniteField` otherwise)
-# because we can choose the same polynomial as is used in GAP.
-function _iso_gap_oscar_field_finite_ext(F::GAP.GapObj)
-   p = characteristic(F)  # of type `fmpz`
-   if p < fmpz(2)^64
-     p = UInt64(p)
-   end
-   d = GAP.Globals.DegreeOverPrimeField(F)
-   e = GAPWrap.One(F)
-
-   R, x = PolynomialRing(GF(p), "x")
-   if GAP.Globals.IsPrimeField(GAP.Globals.LeftActingDomain(F))
-     pol_gap = GAP.Globals.DefiningPolynomial(F)
-     if GAPWrap.IsAlgebraicExtension(F)
-#FIXME:
-# As soon as the problem from https://github.com/gap-system/gap/issues/4694
-# is fixed, go back to `Basis_F = GAP.Globals.Basis(F)` also in this case.
-# Note that we want a basis in the filter `IsCanonicalBasisAlgebraicExtension`.
-        Basis_F = GAP.Globals.Objectify(GAP.Globals.NewType(
-                  GAP.Globals.FamilyObj(F),
-                  GAP.Globals.IsCanonicalBasisAlgebraicExtension),
-                  GAP.NewPrecord(0))
-        GAP.Globals.SetUnderlyingLeftModule(Basis_F, F)
-     else
-        Basis_F = GAP.Globals.Basis(F)
+function _iso_gap_oscar_field_finite(FG::GAP.GapObj)
+   p = characteristic(FG)  # of type `fmpz`
+   d = GAP.Globals.DegreeOverPrimeField(FG)
+   if d == 1
+     if p < fmpz(2)^64
+       p = UInt64(p)
      end
+     FO = GF(p)
    else
-     # The GAP field is not an extension of the prime field.
-     # What is a reasonable way to compute (on the GAP side) a polynomial
-     # w.r.t. the prime field, and to decompose field elements w.r.t.
-     # the corresponding basis?
-     error("extensions of extension fields are not supported")
+     FO = GF(p, d)
    end
 
-   coeffs = GAP.Globals.CoefficientsOfUnivariatePolynomial(pol_gap)
-   coeffs = [fmpz(Oscar.GAPWrap.IntFFE(x)) for x in coeffs]
-   pol = R(coeffs)
-   G = FiniteField(pol, "z")[1]
-   Basis_G = Vector{elem_type(G)}(undef, d)
-   Basis_G[1] = G(1)
-   for i = 2:d
-      Basis_G[i] = Basis_G[i - 1]*gen(G)
-   end
+   finv, f = _iso_oscar_gap_field_finite_functions(FO, FG)
 
-   function f(x)
-      v = GAPWrap.Coefficients(Basis_F, x)
-      v_int = [fmpz(GAPWrap.IntFFE(v[i])) for i = 1:length(v)]
-      return sum([v_int[i]*Basis_G[i] for i = 1:d])
-   end
-
-   function finv(x::Union{fq_nmod, fq})
-      v = [GAP.Obj(coeff(x, i)) for i in 0:(d - 1)]
-      return sum([v[i]*Basis_F[i] for i in 1:d])
-   end
-
-   return MapFromFunc(f, finv, F, G)
+   return MapFromFunc(f, finv, FG, FO)
 end
 
 function _iso_gap_oscar_field_rationals(F::GAP.GapObj)
    return MapFromFunc(x -> fmpq(x), x -> GAP.Obj(x), F, QQ)
 end
 
-function _iso_gap_oscar_field_cyclotomic(F::GAP.GapObj)
-   N = GAPWrap.Conductor(F)
-   cycpol = GAP.Globals.CyclotomicPol(N)
-   dim = length(cycpol)-1
-   G = CyclotomicField(N)[1]
+function _iso_gap_oscar_field_cyclotomic(FG::GAP.GapObj)
+   FO = CyclotomicField(GAPWrap.Conductor(FG))[1]
+   finv, f = _iso_oscar_gap_field_cyclotomic_functions(FO, FG)
 
-   function f(x)
-      GAPWrap.IsCyc(x) || error("$x is not a GAP cyclotomic")
-      denom = GAPWrap.DenominatorCyc(x)
-      n = GAPWrap.Conductor(x)
-      mod(N, n) == 0 || error("$x does not lie in the $N-th cyclotomic field")
-      coeffs = GAP.Globals.CoeffsCyc(x * denom, N)
-      GAP.Globals.ReduceCoeffs(coeffs, cycpol)
-      coeffs = Vector{fmpz}(coeffs)
-      coeffs = coeffs[1:dim]
-      return G(coeffs) // fmpz(denom)
-   end
-
-   function finv(x::Nemo.nf_elem)
-      coeffs = [Nemo.coeff(x, i) for i in 0:(N-1)]
-      return GAPWrap.CycList(GAP.GapObj(coeffs; recursive=true))
-   end
-
-   return MapFromFunc(f, finv, F, G)
+   return MapFromFunc(f, finv, FG, FO)
 end
 
 # Use a GAP attribute for caching the mapping.
