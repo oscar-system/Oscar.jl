0e8e4578dc991950039cef27dea5047b3a6d434d
Yue Ren <yue.ren.kl@gmail.com>
2022-01-28 20:05:15 +0000

    TropicalGeometry: fixed computing tropical reduced GB

--- src/TropicalGeometry/groebner_basis.jl
+++ src/TropicalGeometry/groebner_basis.jl
@@ -90,13 +90,13 @@ julia> val_3 = ValuationMap(QQ,3); # 3-adic valuation
 
 julia> w = [0,0,0,0,0,0];
 
-julia> groebner_basis(Cyclic5Homogenized, val_2, w)
+julia> groebner_basis(Cyclic5Homogenized, val_2, w, complete_reduction=true)
 
-julia> groebner_basis(Cyclic5Homogenized, val_3, w) # same as for val_2
+julia> groebner_basis(Cyclic5Homogenized, val_3, w, complete_reduction=true) # same as for val_2
 
-julia> groebner_basis(Katsura5Homogenized, val_2, w)
+julia> groebner_basis(Katsura5Homogenized, val_2, w, complete_reduction=true)
 
-julia> groebner_basis(Katsura5Homogenized, val_3, w) # different to val_2
+julia> groebner_basis(Katsura5Homogenized, val_3, w, complete_reduction=true) # different to val_2
 
 julia> Kt,t = RationalFunctionField(QQ,"t");
 
@@ -108,25 +108,49 @@ julia> Katsura5Homogenized_Kt = ideal([change_coefficient_ring(Kt,f) for f in ge
 
 julia> val_t = ValuationMap(Kt,t); # t-adic valuation
 
-julia> groebner_basis(Cyclic5Homogenized_Kt, val_t, w) # same leading monomials as for val_2 and val_3
+julia> groebner_basis(Cyclic5Homogenized_Kt, val_t, w, complete_reduction=true) # same leading monomials as for val_2 and val_3
 
-julia> groebner_basis(Katsura5Homogenized_Kt, val_t, w) # different leading monomials as for val_2
-                                                        # same leading monomials as for val_3
-
+julia> groebner_basis(Katsura5Homogenized_Kt, val_t, w, complete_reduction=true) # different leading monomials as for val_2
+                                                                                 # same leading monomials as for val_3
 ```
 """
 function groebner_basis(I::MPolyIdeal,val::ValuationMap,w::Vector{<: Union{Int,Rational{Int}} }; complete_reduction::Bool=false, return_lead::Bool=false)
+
+  ###
+  # Step 1: Compute a standard basis in the simulation ring
+  ###
   vvI = simulate_valuation(I,val)
   w = simulate_valuation(w,val)
-
   Rtx = base_ring(vvI)
   # todo: replace with groebner_bases in OSCAR once more orderings are supported
   S,_ = Singular.PolynomialRing(singular_ring(base_ring(Rtx)), map(string, Nemo.symbols(Rtx)), ordering = Singular.ordering_a(w)*Singular.ordering_dp())
   SI = Singular.Ideal(S, [S(g) for g in gens(vvI)])
+  vvGB = Singular.gens(Singular.std(SI,complete_reduction=complete_reduction))
 
-  vvGB = Singular.std(SI,complete_reduction=complete_reduction)
-  GB = desimulate_valuation(ideal(Rtx,Singular.gens(vvGB)),val)
 
+  ###
+  # Step 2: tighten simulation so that no two monomials of the standard basis elements have the same x-monomial
+  ###
+  vvGB = [S(tighten_simulation(Rtx(g),val)) for g in vvGB]
+
+
+  ###
+  # Step 3: if complete_reduction = true and val is non-trivial, eliminate tail-monomials contained in the leading ideal in the tropical sense
+  #  In the simulation, these monomials corresponds to tail-monomials contained in the leading ideal up to saturation by t
+  #  and elimination means eliminating them after multiplying by a sufficiently high power in t
+  ###
+  if complete_reduction==true && is_valuation_nontrivial(val)
+    sort!(vvGB,lt=x_monomial_lt) # sort vvGB by their leading x monomial from small to large
+    Singular.libSingular.set_option("OPT_INFREDTAIL", true)
+    for i in 1:length(vvGB)-1
+      for j in i+1:length(vvGB)
+        vvGB[j] = Singular.reduce(vvGB[j],Singular.std(Singular.Ideal(S,vvGB[i])))
+      end
+    end
+    Singular.libSingular.set_option("OPT_INFREDTAIL", false)
+  end
+
+  GB = desimulate_valuation(ideal(Rtx,vvGB),val)
   if return_lead
     vvLI = Singular.lead(vvGB)
     LI = desimulate_valuation(ideal(Rtx,Singular.gens(vvLI)),val)
@@ -135,3 +159,13 @@ function groebner_basis(I::MPolyIdeal,val::ValuationMap,w::Vector{<: Union{Int,R
 
   return gens(GB)
 end
+
+
+function x_monomial_lt(f::Singular.spoly,g::Singular.spoly)
+  expv_f = copy(Singular.leading_exponent_vector(f))
+  expv_g = copy(Singular.leading_exponent_vector(g))
+  popfirst!(expv_f)
+  popfirst!(expv_g)
+  return expv_f<expv_g
+end
+export x_monomial_lt
