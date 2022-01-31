f791b2b28861ae74debbf0370258fee4ba01ba9b
Lars Kastner <lars.kastner@yahoo.dk>
2022-01-31 16:50:29 +0100

    PolyhedralGeometry: Deal with fine and star triangulations until polymake provides corresponding functions

--- src/PolyhedralGeometry/triangulations.jl
+++ src/PolyhedralGeometry/triangulations.jl
@@ -1,7 +1,38 @@
-@doc Markdown.doc"""
-    all_triangulations(pts::AnyVecOrMat)
+function _is_full_triangulation(triang::Vector{Vector{Int}}, npoints::Int)
+    u = Set{Int}()
+    for v in triang
+        union!(u, v)
+        if length(u) == npoints
+            return true
+        end
+    end
+    return false
+end
 
-Compute all triangulations on the points given as the rows of `pts`.
+function _is_star_triangulation(triang::Vector{Vector{Int}})
+    u = Set{Int}()
+    for v in triang
+        if !(1 in v)
+            return false
+        end
+    end
+    return true
+end
+
+function _postprocess(triangs::Vector{Vector{Vector{Int}}}, full::Bool)
+    result = Polymake.to_one_based_indexing(triangs)
+    if full
+        result = [t for t in result if _is_full_triangulation(t)]
+    end
+    return result
+end
+
+@doc Markdown.doc"""
+    all_triangulations(pts::AnyVecOrMat; full=false)
+
+Compute all triangulations on the points given as the rows of `pts`. Optionally
+select `full=true` to output full triangulations only, i.e. those that use all
+given points.
 
 The return type is a `Vector{Vector{Vector{Int}}}` where each
 `Vector{Vector{Int}}` encodes a triangulation, in which a `Vector{Int}` encodes
@@ -27,12 +58,12 @@ julia> all_triangulations(V)
  [[1, 2, 4], [1, 3, 4]]
 ```
 """
-function all_triangulations(pts::Union{SubObjectIterator{<:PointVector}, AbstractMatrix, Oscar.MatElem})
+function all_triangulations(pts::Union{SubObjectIterator{<:PointVector}, AbstractMatrix, Oscar.MatElem}; full::Bool=false)
     input = homogenized_matrix(pts, 1)
     PC = Polymake.polytope.PointConfiguration(POINTS=input)
     triangs = Polymake.polytope.topcom_all_triangulations(PC)
     result = [[[e for e in simplex] for simplex in triang] for triang in triangs]
-    return Polymake.to_one_based_indexing(result)
+    return _postprocess(result, full)
 end
 
 
@@ -41,7 +72,7 @@ end
 
 Compute all triangulations that can be formed using the vertices of the given
 polytope `P`.
-
+p
 The return type is a `Vector{Vector{Vector{Int}}}` where each
 `Vector{Vector{Int}}` encodes a triangulation, in which a `Vector{Int}` encodes
 a simplex as the set of indices of the vertices of the simplex. I.e. the
@@ -59,18 +90,82 @@ julia> all_triangulations(c)
  [[1, 2, 4], [1, 3, 4]]
 ```
 """
-all_triangulations(P::Polyhedron) = all_triangulations(vertices(P))
+all_triangulations(P::Polyhedron) = all_triangulations(vertices(P); full=false)
 
 
 @doc Markdown.doc"""
-    regular_triangulations(pts::AnyVecOrMat)
+    star_triangulations(pts::AnyVecOrMat; full::Bool=false, regular::Bool=false)
+
+Return all star triangulations of the given point configuration, i.e. all
+simplices are required to contain the first point. Optionally only select the
+`regular` or `full` triangulations.
+
+The return type is a `Vector{Vector{Vector{Int}}}` where each
+`Vector{Vector{Int}}` encodes a triangulation, in which a `Vector{Int}` encodes
+a simplex as the set of indices of the vertices of the simplex. I.e. the
+`Vector{Int}` `[1,2,4]` corresponds to the simplex that is the convex hull of
+the first, second, and fourth input point.
+"""    
+function star_triangulations(pts::AnyVecOrMat; full::Bool=false, regular::Bool=false)
+    if regular
+        result = regular_triangulations(pts; full=full)
+    else
+        result = all_triangulations(pts; full=full)
+    end
+    return [t for t in result if _is_star_triangulation(t)]
+end
+@doc Markdown.doc"""
+    star_triangulations(P::Polyhedron; full::Bool=false, regular::Bool=false)
+
+Return all star triangulations of the given polyhedron, i.e. all simplices are
+required to contain the origin. If the origin is not among the vertices of the
+polyhedron, it is added. Optionally only select the `regular` or `full`
+triangulations.
+
+The output is a pair
+- The first entry contains the points of the point configuration as their order
+may be different than inside the polyhedron.
+- The second entry contains the triangulations as
+`Vector{Vector{Vector{Int}}}`, where each `Vector{Vector{Int}}` encodes a
+triangulation, in which a `Vector{Int}` encodes a simplex as the set of indices
+of the vertices of the simplex. I.e. the `Vector{Int}` `[1,2,4]` corresponds to
+the simplex that is the convex hull of the first, second, and fourth input
+point.
+
+# Examples
+A two-dimensional polyhedron has only one star triangulation.
+```jldoctest
+julia> hex = convex_hull([-1 -1; 0 -1; 1 0; 1 1; 0 1; -1 0])
+A polyhedron in ambient dimension 2
+
+julia> star_triangulations(hex)
+([0 0; -1 -1; 0 -1; 1 0; 1 1; 0 1; -1 0], [[[1, 2, 3], [1, 2, 7], [1, 3, 4], [1, 4, 5], [1, 5, 6], [1, 6, 7]]])
+```
+"""    
+function star_triangulations(P::Polyhedron; full::Bool=false, regular::Bool=false)
+    zero = [0 for i in 1:dim(P)]
+    contains(P, zero) || throw(ArgumentError("Input polyhedron must contain origin."))
+    V = vertices(P)
+    if zero in V
+        V = [v for v in V if v != 0]
+    end
+    pts = vcat(matrix(QQ, transpose(zero)), matrix(QQ, V))
+    return pts, star_triangulations(pts; full=full, regular=regular)
+end
+
+
+
+
+@doc Markdown.doc"""
+    regular_triangulations(pts::AnyVecOrMat; full=false)
 
 Compute all regular triangulations on the points given as the rows of `pts`.
 
 A triangulation is regular if it can be induced by weights, i.e. attach a
 weight to every point, take the convex hull of these new vectors and then take
 the subdivision corresponding to the facets visible from below (lower
-envelope).
+envelope). Optionally specify `full`, i.e. that every triangulation must use
+all points.
 
 The return type is a `Vector{Vector{Vector{Int}}}` where each
 `Vector{Vector{Int}}` encodes a triangulation, in which a `Vector{Int}` encodes
@@ -96,12 +191,12 @@ julia> regular_triangulations(V)
  [[1, 3, 4], [1, 2, 4]]
 ```
 """
-function regular_triangulations(pts::Union{SubObjectIterator{<:PointVector}, AbstractMatrix, Oscar.MatElem})
+function regular_triangulations(pts::Union{SubObjectIterator{<:PointVector}, AbstractMatrix, Oscar.MatElem}; full::Bool=false)
     input = homogenized_matrix(pts, 1)
     PC = Polymake.polytope.PointConfiguration(POINTS=input)
     triangs = Polymake.polytope.topcom_regular_triangulations(PC)
     result = [[[e for e in simplex] for simplex in triang] for triang in triangs]
-    return Polymake.to_one_based_indexing(result)
+    return _postprocess(result, full)
 end
 
 
@@ -133,7 +228,7 @@ julia> regular_triangulations(c)
  [[1, 3, 4], [1, 2, 4]]
 ```
 """
-regular_triangulations(P::Polyhedron) = regular_triangulations(vertices(P))
+regular_triangulations(P::Polyhedron) = regular_triangulations(vertices(P); full=false)
 
 
 @doc Markdown.doc"""
