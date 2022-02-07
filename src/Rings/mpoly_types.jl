85314278456af885291a43c29e746cec4f79fb9a
Tommy Hofmann <thofma@gmail.com>
2022-02-07 10:33:45 +0100

    not yet

--- src/Rings/mpoly_types.jl
+++ src/Rings/mpoly_types.jl
@@ -1,19 +1,15 @@
 export mpoly_type, mpoly_ring_type
 
-mpoly_ring_type(R::T) where {T<:AbstractAlgebra.Ring} = MPolyRing{elem_type(T)}
-mpoly_ring_type(::Type{T}) where {T<:AbstractAlgebra.Ring} = MPolyRing{elem_type(T)}
-mpoly_type(R::T) where {T<:AbstractAlgebra.Ring} = MPolyElem{elem_type(T)}
-mpoly_type(::Type{T}) where {T<:AbstractAlgebra.Ring} = MPolyElem{elem_type(T)}
+mpoly_ring_type(R::T) where {T<:AbstractAlgebra.Ring} = Generic.MPolyRing{elem_type(T)}
+mpoly_ring_type(::Type{T}) where {T<:AbstractAlgebra.Ring} = Generic.MPolyRing{elem_type(T)}
+mpoly_type(R::T) where {T<:AbstractAlgebra.Ring} = Generic.MPolyElem{elem_type(T)}
+mpoly_type(::Type{T}) where {T<:AbstractAlgebra.Ring} = Generic.MPolyElem{elem_type(T)}
 
-mpoly_ring_type(R::T) where {T<:AbstractAlgebra.Field} = MPolyRing{elem_type(T)}
-mpoly_ring_type(::Type{T}) where {T<:AbstractAlgebra.Field} = MPolyRing{elem_type(T)}
-mpoly_ring_type(a::T) where {T<:AbstractAlgebra.FieldElem} = MPolyRing{T}
-mpoly_ring_type(::Type{T}) where {T<:AbstractAlgebra.FieldElem} = MPolyRing{T}
+mpoly_ring_type(a::T) where {T<:AbstractAlgebra.RingElem} = Generic.MPolyRing{T}
+mpoly_ring_type(::Type{T}) where {T<:AbstractAlgebra.RingElem} = Generic.MPolyRing{T}
 
-mpoly_type(R::T) where {T<:AbstractAlgebra.Field} = MPolyElem{elem_type(T)}
-mpoly_type(::Type{T}) where {T<:AbstractAlgebra.Field} = MPolyElem{elem_type(T)}
-mpoly_type(a::T) where {T<:AbstractAlgebra.FieldElem} = MPolyElem{T}
-mpoly_type(::Type{T}) where {T<:AbstractAlgebra.FieldElem} = MPolyElem{T}
+mpoly_type(a::T) where {T<:AbstractAlgebra.RingElem} = Generic.MPolyElem{T}
+mpoly_type(::Type{T}) where {T<:AbstractAlgebra.RingElem} = Generic.MPolyElem{T}
 
 mpoly_ring_type(R::FlintRationalField) = FmpqMPolyRing
 mpoly_ring_type(::Type{FlintRationalField}) = FmpqMPolyRing
