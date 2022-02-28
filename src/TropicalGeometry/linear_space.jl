57c3149e5633c8aa2faddaa1dd0cbbd05219f796
Benjamin Schroeter <schrot@kth.se>
2022-02-28 18:29:43 +0100

    changed pluecker vector type to tropicalnumber

--- src/TropicalGeometry/linear_space.jl
+++ src/TropicalGeometry/linear_space.jl
@@ -60,18 +60,30 @@ end
 Construct a tropical linear space from its Pluecker vector
 
 # Examples
+> julia R = tropical_numbers(min);
+> julia plv = [R(e) for e in [2,1,1,0,0,zero(R)]];
+> julia L = TropicalLinearSpace(plv, 2, 4)
+> julia f_vector(L)
 """
-function TropicalLinearSpace(plv::Vector{Union{Rational,PosInf,Oscar.IntegerUnion}},nElement::Int,rank::Int; M::Union{typeof(min),typeof(max)}=min)
-    indexSet = findall(i->i!=inf, plv)
-    bases = [ sort(Hecke.subsets(Vector{Int}(0:nElement-1),rank))[i] for i in indexSet ]
-    val = Polymake.matroid.ValuatedMatroid{M}(BASES = bases, N_ELEMENTS = nElement,VALUATION_ON_BASES = Vector{Rational}(plv[indexSet]) )
+function TropicalLinearSpace_impl(plv, rank, nElements, M)
+    Zero = zero(tropical_numbers(M))
+    indexSet = findall(i->i!=Zero, plv)
+    bases = [ sort(Hecke.subsets(Vector{Int}(0:nElements-1), rank))[i] for i in indexSet ]
+    val = Polymake.matroid.ValuatedMatroid{M}(BASES = bases, N_ELEMENTS = nElements,VALUATION_ON_BASES = [plv[i].data for i in indexSet])
     #return Polymake.tropical.linear_space{min}(val)
     P = Polymake.tropical.linear_space{M}(val)
     P = PolyhedralComplex(P)
     return TropicalLinearSpace{M,true}(P)
 end
 
+TropicalLinearSpace(plv::Vector{TropicalNumbersElem{typeof(min)}},rank::IntegerUnion, nElements::IntegerUnion) = 
+TropicalLinearSpace_impl(plv, rank, nElements, min)
+
+TropicalLinearSpace(plv::Vector{TropicalNumbersElem{typeof(max)}},rank::IntegerUnion, nElements::IntegerUnion) = 
+TropicalLinearSpace_impl(plv, rank, nElements, max)
+
 #needs Oscar type as entry
+#TODO needs a valuation map
 function TropicalLinearSpace(tropicalmatrix::MatElem)
   plv = Nemo.minors(tropicalmatrix, min(size(tropicalmatrix)[1], size(tropicalmatrix)[2])) 
   rk = rank(tropicalmatrix)
