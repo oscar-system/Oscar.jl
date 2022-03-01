1f4a177555ea987da34caae8fb2e2c7fffc000e4
AlessioBorzi <alessio520@hotmail.it>
2022-03-01 14:34:34 +0100

    tropical numbers to tropical semiring

--- src/TropicalGeometry/linear_space.jl
+++ src/TropicalGeometry/linear_space.jl
@@ -49,19 +49,19 @@ end
 Construct a tropical linear space from its Pluecker vector
 
 # Examples
-julia> R = tropical_numbers(min);
+julia> R = tropical_semiring(min);
 julia> plv = [R(e) for e in [2,1,1,0,0,zero(R)]];
 julia> L = TropicalLinearSpace(plv, 2, 4)
 julia> f_vector(L)
 
 # Examples
-julia> Kt, t = RationalFunctionField(QQ,"t"); 
+julia> Kt, t = RationalFunctionField(QQ,"t");
 julia> val = ValuationMap(Kt,t);
 julia> A = matrix(Kt,[[t,4t,0,2],[1,4,1,t^2]])
 julia> TropicalLinearSpace(A, val)
 """
 function TropicalLinearSpace_impl(plv, rank, nElements, M)
-    Zero = zero(tropical_numbers(M))
+    Zero = zero(tropical_semiring(M))
     indexSet = findall(i->i!=Zero, plv)
     bases = [ sort(Hecke.subsets(Vector{Int}(0:nElements-1), rank))[i] for i in indexSet ]
     val = Polymake.matroid.ValuatedMatroid{M}(BASES = bases, N_ELEMENTS = nElements,VALUATION_ON_BASES = [plv[i].data for i in indexSet])
@@ -71,10 +71,10 @@ function TropicalLinearSpace_impl(plv, rank, nElements, M)
     return TropicalLinearSpace{M,true}(P)
 end
 
-TropicalLinearSpace(plv::Vector{TropicalNumbersElem{typeof(min)}},rank::IntegerUnion, nElements::IntegerUnion) = 
+TropicalLinearSpace(plv::Vector{TropicalSemiringElem{typeof(min)}},rank::IntegerUnion, nElements::IntegerUnion) =
 TropicalLinearSpace_impl(plv, rank, nElements, min)
 
-TropicalLinearSpace(plv::Vector{TropicalNumbersElem{typeof(max)}},rank::IntegerUnion, nElements::IntegerUnion) = 
+TropicalLinearSpace(plv::Vector{TropicalSemiringElem{typeof(max)}},rank::IntegerUnion, nElements::IntegerUnion) =
 TropicalLinearSpace_impl(plv, rank, nElements, max)
 
 #needs Oscar type as entry
@@ -82,7 +82,7 @@ TropicalLinearSpace_impl(plv, rank, nElements, max)
 #TODO requires a fix of ValuationMap
 function TropicalLinearSpace(tropicalmatrix::MatElem, val)
   plv = [val(p) for p in Nemo.minors(tropicalmatrix, min( nrows(tropicalmatrix), ncols(tropicalmatrix)) )]
-  plv =Oscar.tropical_numbers(min).(plv)
+  plv =Oscar.tropical_semiring(min).(plv)
   rk = rank(tropicalmatrix)
   nelement = max( nrows(tropicalmatrix), ncols(tropicalmatrix))
   println(typeof(plv))
@@ -99,8 +99,5 @@ function TropicalLinearSpace(tropicalmatrix::Matrix{Union{fmpq, fmpz}})
   return TropicalLinearSpace(matrix(base_ring(tropicalmatrix), tropicalmatrix))
 end
 
-
-###
-# 3. Basic properties
 # -------------------
 ###
