27d5badb1558544642d0382ab106a7def2f37beb
ThomasBreuer <sam@math.rwth-aachen.de>
2022-03-02 14:26:44 +0100

    added `describe` for `GrpAbFinGen`
    resolves #1132

--- test/Groups/GrpAb.jl
+++ test/Groups/GrpAb.jl
@@ -11,3 +11,15 @@
   @test issurjective(g)
   @test order(codomain(g)) == 2
 end
+
+@testset "describe for GrpAbFinGen" begin
+  @test describe(abelian_group(GrpAbFinGen, Int[])) == "0"
+  @test describe(abelian_group(GrpAbFinGen, Int[0])) == "Z"
+  @test describe(abelian_group(GrpAbFinGen, Int[0, 0])) == "Z^2"
+  @test describe(abelian_group(GrpAbFinGen, Int[2])) == "Z/2"
+  @test describe(abelian_group(GrpAbFinGen, Int[0, 2])) == "Z/2 + Z"
+  @test describe(abelian_group(GrpAbFinGen, Int[0, 0, 2])) == "Z/2 + Z^2"
+  @test describe(abelian_group(GrpAbFinGen, Int[2, 4])) == "Z/2 + Z/4"
+  @test describe(abelian_group(GrpAbFinGen, Int[0, 2, 4])) == "Z/2 + Z/4 + Z"
+  @test describe(abelian_group(GrpAbFinGen, Int[0, 0, 2, 4])) == "Z/2 + Z/4 + Z^2"
+end
