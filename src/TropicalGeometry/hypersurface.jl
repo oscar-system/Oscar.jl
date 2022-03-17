3d602a20edbc820be90d36e9114abf351cf34652
panizzut <panizzut@math.tu-berlin.de>
2022-03-17 14:51:36 +0100

    TropicalGeometry: changes in Hypersurface.jl based on Max's suggestions

--- src/TropicalGeometry/hypersurface.jl
+++ src/TropicalGeometry/hypersurface.jl
@@ -1,20 +1,23 @@
 ###
 # Tropical hypersurfaces in Oscar
-# ===============================
 ###
 
+export TropicalHypersurface,
+       dualsubdivision,
+       polynomial
 
+################################################################################
+#
+#  Definition
+#
+################################################################################
 
-###
-# 0. Definition
-# -------------
-# M = typeof(min) or typeof(max):
-#   min or max convention, affecting initial forms, tropical polynomial, etc.
-# EMB = true or false:
-#   embedded or abstract tropical hypersurface
+# We use M to record whether we are in the min/max case
+# M is either typeof(min) or typeof(max)
+# We use EMB to record whether the hypersurface is embedded or abstract
+# EMB is either true or false:
 #   embedded tropical variety = weighted polyhedral complex in euclidean space
 #   abstract tropical variety = weighted hypergraph with enumerated vertices
-###
 
 @attributes mutable struct TropicalHypersurface{M,EMB} <: TropicalVarietySupertype{M,EMB}
     polyhedralComplex::PolyhedralComplex
@@ -25,7 +28,6 @@
         return new{M,EMB}(Sigma)
     end
 end
-export TropicalHypersurface
 
 function pm_object(T::TropicalHypersurface)
     if has_attribute(T,:polymake_bigobject)
@@ -34,12 +36,12 @@ function pm_object(T::TropicalHypersurface)
     error("pm_object(T::TropicalHypersurface): Has no polymake bigobject")
 end
 
+################################################################################
+#
+#  Printing
+#
+################################################################################
 
-
-###
-# 1. Printing
-# -----------
-###
 
 function Base.show(io::IO, th::TropicalHypersurface{M, EMB}) where {M, EMB}
     if EMB
@@ -49,17 +51,16 @@ function Base.show(io::IO, th::TropicalHypersurface{M, EMB}) where {M, EMB}
     end
 end
 
-
-
-###
-# 2. Basic constructors
-# ---------------------
-###
+################################################################################
+#
+#  Constructors for tropical hypersurfaces
+#
+################################################################################
 
 @doc Markdown.doc"""
-    TropicalHypersurface(f::MPolyElem{TropicalSemiringElem})
+    TropicalHypersurface(f::AbstractAlgebra.Generic.MPoly{Oscar.TropicalSemiringElem{T}})
 
-Return the tropical hypersurface of a tropical polynomial.
+Return the tropical hypersurface defined by a tropical polynomial.
 
 # Examples
 ```jldoctest
@@ -76,20 +77,15 @@ julia> Tf = TropicalHypersurface(f)
 A min tropical hypersurface embedded in 2-dimensional Euclidian space
 ```
 """
-function TropicalHypersurface(
-    f::Union{AbstractAlgebra.Generic.MPoly{Oscar.TropicalSemiringElem{typeof(min)}},
-             AbstractAlgebra.Generic.MPoly{Oscar.TropicalSemiringElem{typeof(max)}}}
-        )
+function TropicalHypersurface(f::AbstractAlgebra.Generic.MPoly{Oscar.TropicalSemiringElem{T}}) where T
     if total_degree(f) <= 0
         error("Tropical hypersurfaces of constant polynomials not supported.")
     end
     M = convention(base_ring(f))
-
     fstr = Tuple(tropical_polynomial_to_polymake(f))
     pmpoly = Polymake.common.totropicalpolynomial(fstr...)
     pmhypproj = Polymake.tropical.Hypersurface{M}(POLYNOMIAL=pmpoly)
     pmhyp = Polymake.tropical.affine_chart(pmhypproj)
-
     Vf = TropicalHypersurface{M, true}(PolyhedralComplex(pmhyp))
     w = pmhypproj.WEIGHTS
     set_attribute!(Vf,:polymake_bigobject,pmhypproj)
@@ -102,7 +98,7 @@ end
 #     tropical_variety(f::Union{AbstractAlgebra.Generic.MPoly{Oscar.TropicalSemiringElem{typeof(min)}},
 #                               AbstractAlgebra.Generic.MPoly{Oscar.TropicalSemiringElem{typeof(max)}}})
 
-# Return the tropical variety of a tropical polynomial in form of a TropicalHypersurface
+# Return the tropical variety defined by a tropical polynomial in form of a TropicalHypersurface
 
 # # Examples
 # ```jldoctest
@@ -124,27 +120,30 @@ end
 #     return TropicalHypersurface(f)
 # end
 
-
 @doc Markdown.doc"""
-    TropicalHypersurface{M}(f::Union{AbstractAlgebra.Generic.MPoly{Oscar.TropicalSemiringElem{typeof(min)}},
-                                     AbstractAlgebra.Generic.MPoly{Oscar.TropicalSemiringElem{typeof(max)}}})
+    TropicalHypersurface{M}(f::AbstractAlgebra.Generic.MPoly{<:RingElement},M::Union{typeof(min),typeof(max)}=min)
 
-Return the tropical hypersurface of an algebraic polynomial.
+Return the tropical hypersurface defined by the tropicalization of an algebraic polynomial.
 If M=min, the tropical hypersurface will obey the min-convention.
 If M=max, the tropical hypersurface will obey the max-convention.
 If coefficient ring has a valuation, the tropical hypersurface will be constructed with respect to it.
 If coefficient ring has no valuation, the tropical hypersurface will be constructed with respect to the trivial valuation.
 
 # Examples
-julia> K = PadicField(7, 2)
+```jldoctest
+julia> K = PadicField(7, 2);
 
 julia> Kxy, (x,y) = K["x", "y"]
+(Multivariate Polynomial Ring in x, y over Field of 7-adic numbers, AbstractAlgebra.Generic.MPoly{padic}[x, y])
 
-julia> f = 7*x+y+49
+julia> f = 7*x+y+49;
 
 julia> TropicalHypersurface(f, min)
+A min tropical hypersurface embedded in 2-dimensional Euclidian space
 
 julia> TropicalHypersurface(f, max)
+A max tropical hypersurface embedded in 2-dimensional Euclidian space
+```
 """
 function TropicalHypersurface(f::AbstractAlgebra.Generic.MPoly{<:RingElement},M::Union{typeof(min),typeof(max)}=min)
     tropf = tropical_polynomial(f,M)
@@ -153,9 +152,8 @@ function TropicalHypersurface(f::AbstractAlgebra.Generic.MPoly{<:RingElement},M:
     set_attribute!(Tf,:algebraic_polynomial,f)
     set_attribute!(Tf,:tropical_polynomial,tropf)
     set_attribute!(Tf,:weights,w)
-  return Tf
+    return Tf
 end
-
 
 function TropicalHypersurface(f::AbstractAlgebra.Generic.MPoly{<:RingElement}, val::TropicalSemiringMap, M::Union{typeof(min),typeof(max)}=min)
     tropf = tropical_polynomial(f,val,M)
@@ -164,7 +162,7 @@ function TropicalHypersurface(f::AbstractAlgebra.Generic.MPoly{<:RingElement}, v
     set_attribute!(Tf,:algebraic_polynomial,f)
     set_attribute!(Tf,:tropical_polynomial,tropf)
     set_attribute!(Tf,:weights,w)
-  return Tf
+    return Tf
 end
 
 
@@ -193,12 +191,11 @@ end
 #     return TropicalHypersurface{M}(f)
 # end
 
-
-
-###
-# 3. Basic properties
-# -------------------
-###
+################################################################################
+#
+#  Basic properties for tropical hypersurfaces
+#
+################################################################################
 
 # todo: add examples for varieties, curves and linear spaces
 @doc Markdown.doc"""
@@ -207,7 +204,7 @@ end
 Return the dual subdivision of `TH` if it is embedded. Otherwise an error is thrown.
 
 # Examples
-A tropical hypersurface in RR^n is always of dimension n-1
+A tropical hypersurface in RR^n is always of dimension n-1.
 ```jldoctest
 julia> T = tropical_semiring(min);
 
@@ -222,7 +219,6 @@ A subdivision of points in ambient dimension 3
 ```
 """
 function dual_subdivision(TH::TropicalHypersurface{M,EMB}) where {M,EMB}
-    # not sure whether it makes sense to support abstract tropical hypersurfaces, but it can't hurt to check
     if !EMB
         error("tropical hypersurface not embedded")
     end
@@ -257,4 +253,4 @@ function polynomial(TH::TropicalHypersurface{M,EMB}) where {M,EMB}
     end
     return get_attribute(TH,:tropical_polynomial)
 end
-export polynomial
+
