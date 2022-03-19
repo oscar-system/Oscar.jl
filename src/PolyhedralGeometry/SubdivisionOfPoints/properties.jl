48f98053c39afdbcb1490e5dae20a7b56798bb2e
tbrysiewicz <taylorbrysiewicz@gmail.com>
2022-03-19 15:25:21 -0400

    trying to resolve doc error

--- src/PolyhedralGeometry/SubdivisionOfPoints/properties.jl
+++ src/PolyhedralGeometry/SubdivisionOfPoints/properties.jl
@@ -59,15 +59,16 @@ julia> moaepts = [4 0 0; 0 4 0; 0 0 4; 2 1 1; 1 2 1; 1 1 2]
  1  2  1
  1  1  2
 
- julia> moaeimnonreg0 = IncidenceMatrix([[4,5,6],[1,4,2],[2,4,5],[2,3,5],[3,5,6],[1,3,6],[1,4,6]])
- 7×6 IncidenceMatrix
- [4, 5, 6]
- [1, 2, 4]
- [2, 4, 5]
- [2, 3, 5]
- [3, 5, 6]
- [1, 3, 6]
- [1, 4, 6]
+julia> moaeimnonreg0 = IncidenceMatrix([[4,5,6],[1,4,2],[2,4,5],[2,3,5],[3,5,6],[1,3,6],[1,4,6]])
+7×6 IncidenceMatrix
+[4, 5, 6]
+[1, 2, 4]
+[2, 4, 5]
+[2, 3, 5]
+[3, 5, 6]
+[1, 3, 6]
+[1, 4, 6]
+
 
 julia> MOAE = SubdivisionOfPoints(moaepts, moaeimnonreg0);
 
