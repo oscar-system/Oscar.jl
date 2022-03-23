82fe2c9922fbb62bdd5975d9050254c067c2092c
Lars Kastner <lars.kastner@posteo.de>
2022-03-23 18:18:20 +0100

    Serialization: Allow serializing Number type

--- src/Serialization/basic_types.jl
+++ src/Serialization/basic_types.jl
@@ -1,14 +1,21 @@
 ################################################################################
 # fmpz
-function load_intern(s::DeserializerState, z::Type{fmpz}, dict::Dict)
-    return fmpz(dict[:val])
+function load_intern(s::DeserializerState, ::Type{fmpz}, str::String)
+    return fmpz(str)
 end
 
 function save_intern(s::SerializerState, z::fmpz)
-    return Dict(
-        :val => string(z)
-    )
+    return string(z)
 end
 
 
+################################################################################
+# Number
+function save_intern(s::SerializerState, z::Number)
+    return string(z)
+end
+
+function load_intern(s::DeserializerState, ::Type{T}, str::String) where {T<:Number}
+    return parse(T, str)
+end
 
