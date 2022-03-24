2eedc40dc9ce593050f7b8ac50ebae580b3a3e4e
anne <anne@krueger-berg.de>
2022-03-24 15:16:01 +0100

    erste Version tjurina_number und global_milnor_number

--- experimental/LibSingWrapper.jl
+++ experimental/LibSingWrapper.jl
@@ -1,6 +1,6 @@
 import Singular.LibSing: *
 
-export milnor
+export milnor, tjurina
 
 ### The Milnor algebra of an isolated hypersurface singularity f
 
@@ -18,7 +18,7 @@ function milnor(f::MPolyElem; point::Vector=[])
   iszero(f) && error("polynomial is zero")
   R = parent(f)
   kk = coefficient_ring(f)
-  # check whether we are working of a field of coefficients
+  # check whether we are working over a field of coefficients
   typeof(kk)<:AbstractAlgebra.Field || error("coefficient domain is not a field")
 
   # We can not provide a full default vector of zeroes in the 
@@ -56,14 +56,76 @@ function milnor(f::MPolyElem; point::Vector=[])
   return (stdI, milnor_number, milnor_kbase)
 end
 
+### The Tjurina algebra  of an isolated hypersurface singularity f
+@Markdown.doc """
+    tjurina(f::MPolyElem; point::Vector=[])
+
+For an isolated hypersurface singularity `f` at a point `point`,
+compute the triple `(gb,\tau,g)` where `gb` is a standard basis for the
+Tjurina ideal. `\tau` is the Tjurina number, and `g` is a ``k``-base of the
+Tjurina algebra.
+
+The default argument for `point` is the origin.
+""
+function tjurina(f::MPolyElem; point::Vector=[])
+  iszero(f) && error("input polynomial is zero")
+  R = parent(f)
+  kk = coefficient_ring(f)
+  # make sure that kk is a field
+  typeof(kk)<:AbstractAlgebra.Field || error("coefficient domain is not a field")
+
+  # Set sanity check on point or creation of vector of point, if default point 
+  k_point = kk.(point)
+  if length(point) != 0
+     # right number of coordinates?
+     length(k_point) == ngens(R) || error("vector does not have correct length")
+  else
+     # default case, need to set up point for origin
+     k_point = elem_type(kk)[zero(kk) for i in 1:ngens(R)]
+  end
+
+  # localize at point (setting up stage for Singular)
+  RL = Localization(MPolyComplementOfKPointIdeal(R,k_point))  
+
+  # compute the jacobian and tjurina ideals
+  Jf = jacobi_matrix(f)::AbstractAlgebra.Generic.MatSpaceElem{typeof(f)}
+  I=ideal(R,[Df[i,1] for i i n1:nrows(Df)]) + ideal(R,[f])
+  # now move to localization RL of R and do standard basis there
+  IL=RL(I)
+  stdIL = groebner_basis(IL)
+  
+  # now fill in the return values
+    Singular.dimension(singular_gens(stdI)) == 0 || error("singularity is not isolated")
+  tjurina_number = Singular.vdim(singular_gens(stdIL)) 
+  # make the conversion to the Oscar side 
+  tjurina_kbase = [to_oscar_side(stdIL, g) for g in gens(Singular.kbase(singular_gens(stdIL)))]
+  return (stdIL, tjurina_number, tjurina_kbase)
+end
+
 @Markdown.doc """
     global_milnor_number(f::MPolyElem)
 
-Computes the global Milnor number of a hypersurface `f` with 
+Computes the global Milnor number of an affine hypersurface `f` with 
 at most isolated singularities.
 """
 function global_milnor_number(f::MPolyElem)
-  error("not implemented")
+  iszero(f) && error("polynomial is zero")
+  R = parent(f)
+  kk = coefficient_ring(f)
+  # check whether we are working over a field of coefficients
+  typeof(kk)<:AbstractAlgebra.Field || error("coefficient domain is not a field")
+
+  # compute groebner basis of the jacobian ideal
+  Df = jacobi_matrix(f)::AbstractAlgebra.Generic.MatSpaceElem{typeof(f)}
+  I = ideal(R, [Df[i, 1] for i in 1:nrows(Df)])
+  stdI = groebner_basis(I)
+
+  # set up the resulting data
+  Singular.dimension(singular_gens(stdI)) == 0 || error("non-isolated singularities detected")
+    milnor_number = Singular.vdim(singular_gens(stdI)) 
+  # make the conversion to the Oscar side 
+  milnor_kbase = [to_oscar_side(stdI, g) for g in gens(Singular.kbase(singular_gens(stdI)))]
+  return (stdI, milnor_number, milnor_kbase)
 end
 
 @Markdown.doc """
