274c926d2d1dcd4cd69e6b4ec93978a39e46e429
Lars Kastner <lars.kastner@posteo.de>
2022-03-25 21:22:33 +0100

    Serialization: Reorder tests, add string test

--- test/Serialization/basic_types.jl
+++ test/Serialization/basic_types.jl
@@ -1,12 +1,12 @@
 @testset "basic_types" begin
     
     mktempdir() do path
-        @testset "Testing (de)serialization of $T" for T in 
-            [
+        @testset "Testing (de)serialization of $(T)" for T in 
+            (
                 UInt, UInt128, UInt16, UInt32, UInt64, UInt8,
                 Int, Int128, Int16, Int32, Int64, Int8,
                 Float16, Float32, Float64
-            ]
+            )
             original = T(1)
             filename = joinpath(path, string(T)*".json")
             save(original, filename)
@@ -14,19 +14,14 @@
             @test loaded isa T
             @test original == loaded
         end
-        
-        @testset "Testing (de)serialization of Vector{$T}" for T in 
-            [
-                UInt, UInt128, UInt16, UInt32, UInt64, UInt8,
-                Int, Int128, Int16, Int32, Int64, Int8,
-                Float16, Float32, Float64
-            ]
-            original = [T(1), T(2)]
-            filename = joinpath(path, string(T)*"_vec.json")
+
+        @testset "String" begin
+            original = "original"
+            filename = joinpath(path, "original.json")
             save(original, filename)
             loaded = load(filename)
-            @test loaded isa Vector{T}
-            @test original == loaded
+            @test loaded isa String
+            @test loaded == original
         end
     end
 end
