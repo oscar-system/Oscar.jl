e3fada3f688250f6aeb60cc66506a7f97482d22a
Matthias Zach <zach@math.uni-hannover.de>
2022-04-01 13:45:12 +0200

    Add tests for the covered schemes.

--- test/Schemes/CoveredSchemes.jl
+++ test/Schemes/CoveredSchemes.jl
@@ -38,3 +38,39 @@
   gyz_alt = compose(gxy, gxz)
   @test gyz == maximal_extension(gyz_alt)
 end
+
+@testset "covered schemes 1" begin
+  # manual glueing of ℙ³ and a quadric surface in there
+  R, (x,y,z,w) = QQ["x", "y", "z", "w"]
+  Rx, (yx,zx,wx) = QQ["y/x", "z/x", "w/x"]
+  Ry, (xy,zy,wy) = QQ["x/y", "z/y", "w/y"]
+  Rz, (xz,yz,wz) = QQ["x/z", "y/z", "w/z"]
+  Rw, (xw,yw,zw) = QQ["x/w", "y/w", "z/w"]
+
+  px = hom(R, Rx, [one(Rx), yx, zx, wx])
+  py = hom(R, Ry, [xy, one(Ry), zy, wy])
+  pz = hom(R, Rz, [xz, yz, one(Rz), wz])
+  pw = hom(R, Rw, [xw, yw, zw, one(Rw)])
+
+  F = x*w-y*z
+  I = ideal(R, F)
+  IP = Spec(R)
+  IPX = subscheme(IP, I)
+  Ux = Spec(Rx)
+  UXx = subscheme(Ux, ideal(Rx, px(F)))
+  Uy = Spec(Ry)
+  UXy = subscheme(Uy, ideal(Ry, py(F)))
+  Uz = Spec(Rz)
+  UXz = subscheme(Uz, ideal(Rz, pz(F)))
+  Uw = Spec(Rw)
+  UXw = subscheme(Uw, ideal(Rw, pw(F)))
+
+  Gxy = Glueing(UXx, yx, UXy, xy, [1//yx, zx//yx, wx//yx], [1//xy, zy//xy, wy//xy])
+
+  Gxz = Glueing(UXx, zx, UXz, xz, [1//zx, yx//zx, wx//zx], [yz//xz, 1//xz, wz//xz])
+  Gxw = Glueing(UXx, wx, UXw, xw, [1//wx, yx//wx, zx//wx], [yw//xw, zw//xw, 1//xw])
+
+  Gyxz = compose(Gxy, Gxz)
+  Gyz_by_ext = maximal_extension(Gyxz)
+  @test Gyxz == Gyz_by_ext
+end
