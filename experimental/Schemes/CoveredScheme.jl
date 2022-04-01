bc0340c28fbb685eabf98b9cefd1bd2dde7ae768
Matthias Zach <zach@math.uni-hannover.de>
2022-04-01 13:02:02 +0200

    Forgotten occurences.

--- experimental/Schemes/CoveredScheme.jl
+++ experimental/Schemes/CoveredScheme.jl
@@ -206,7 +206,7 @@ function standard_covering(X::ProjectiveScheme{CRT}) where {CRT<:AbstractAlgebra
   end
   CX = affine_cone(X)
   kk = base_ring(X)
-  S = homogeneous_coordinate_ring(X)
+  S = homog_poly_ring(X)
   r = fiber_dimension(X)
   U = Vector{affine_patch_type(X)}()
   # TODO: Check that all weights are equal to one. Otherwise the routine is not implemented.
@@ -257,7 +257,7 @@ function standard_covering(X::ProjectiveScheme{CRT}) where {CRT<:MPolyQuoLocaliz
   W = localized_ring(L)
   R = base_ring(L)
   kk = coefficient_ring(R)
-  S = homogeneous_coordinate_ring(X)
+  S = homog_poly_ring(X)
   r = fiber_dimension(X)
   U = Vector{affine_patch_type(X)}()
   pU = Dict{affine_patch_type(X), morphism_type(affine_patch_type(X), typeof(Y))}()
