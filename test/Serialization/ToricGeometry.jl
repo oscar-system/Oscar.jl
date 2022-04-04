bde829fd9c8247f1a3135494480286fe658a06ba
Lars Kastner <lars.kastner@posteo.de>
2022-04-04 20:58:12 +0200

    Serialization: Allow (de-)serializing toric divisors

--- test/Serialization/ToricGeometry.jl
+++ test/Serialization/ToricGeometry.jl
@@ -9,5 +9,19 @@
             @test rays(pp) == rays(loaded)
             @test ray_indices(maximal_cones(pp)) == ray_indices(maximal_cones(loaded))
         end
+
+        @testset "ToricDivisor" begin
+            pp = projective_space(NormalToricVariety, 2)
+            filename = joinpath(path, "td.json")
+            td0 = ToricDivisor(pp, [1,1,2])
+            td1 = ToricDivisor(pp, [1,1,3])
+            vtd = [td0, td1]
+            save(vtd, filename)
+            loaded = load(filename)
+            @test coefficients(td0) == coefficients(loaded[1])
+            @test coefficients(td1) == coefficients(loaded[2])
+            @test toric_variety(loaded[1]) == toric_variety(loaded[2])
+        end
     end
+
 end
