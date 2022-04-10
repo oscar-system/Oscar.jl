e8724dbbb158fec99d75fae383601dd076d2fcfc
Benjamin Schroeter <schrot@kth.se>
2022-04-10 18:20:19 +0200

    some first testcases

---
+++ test/Combinatorics/Matroids.jl
@@ -0,0 +1,55 @@
+@testset "Matroids" begin
+    @testset "standard examples" begin
+        for (M, values) in ((uniform_matroid(3,5), [3, 5, 10]),
+			    (fano_matroid(), [3,7,28]),
+			    (non_fano_matroid(), [3,7,29]),
+			    (pappus_matroid(), [3,9,75]),
+			    (non_pappus_matroid(), [3,9,76])
+			    (vamos_matroid(), [4,8,65]))
+	    @test M isa Matroid
+	    @test rank(M) == values[1]
+            @test length(matroid_groundset(M)) == values[2]
+	    @test length(bases(M)) == values[3]
+        end      
+    end
+
+    @testset "constructors" begin
+	mb1 = matroid_from_revlex_basis_encoding("*****0",2,4)
+	@test mb1 isa Matroid
+	@test rank(mb1) == 2
+	@test length(matroid_groundset(mb1)) == 4
+	@test length(bases(mb1)) == 5
+	@test rank(mb1,[3,4]) == 1
+	
+	#bases
+	mb2 = matroid_from_bases([[1,2],[1,3],[1,4],[2,3],[2,4]], 4)
+	@test mb2 isa Matroid
+	@test sort(bases(mb1)) == sort(bases(mb2))
+
+	B = Set{Set{Int}}([Set([1,2]), Set([1,3]), Set([1,4]), Set([2,3]), Set([2,4])])
+	mb3 = matroid_from_bases(B, 4)
+	@test mb3 isa Matroid
+	@test sort(bases(mb1)) == sort(bases(mb3))
+
+	mb4 = matroid_from_bases([[1,2],[1,'i'],[1,'j'],[2,'i'],[2,'j']],[1,2,'i','j'])
+	@test mb4 isa Matroid
+	@test rank(mb4) == 2
+	@test length(matroid_groundset(mb4)) == 4
+	@test length(bases(mb4)) == 5
+	@test rank(mb1,['i','j']) == 1
+
+	#TODO matroid with empty groundset, and  matroids with rank 0 
+	
+	@test_throws matroid_from_bases([[1,2],[3]], 3)
+	@test_throws matroid_from_bases([[1,2],[1,3],[2,4],[3,4]], 4)
+	@test_throws matroid_from_bases([[1,2],[1,3],[2,4],[3,4]], 3)
+
+	#TODO matroids from nonbases
+
+	#TODO circuits
+	
+	#TODO rest
+
+    end
+
+end
