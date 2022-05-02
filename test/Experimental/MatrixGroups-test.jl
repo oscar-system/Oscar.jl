27152252695959e005f2e8a0fb8306dde41c4db4
Daniel Rademacher <rademacher@art.rwth-aachen.de>
2022-05-02 14:08:36 +0200

    Test for matrix groups.

--- test/Experimental/MatrixGroups-test.jl
+++ test/Experimental/MatrixGroups-test.jl
@@ -1,9 +1,12 @@
 @testset "GAP matrix groups" begin
 
     m = matrix(ZZ, [0 1 ; -1 0])
-    gapM = wrap_for_gap(m)
+    gapM = Oscar.MatrixGroups._wrap_for_gap(m)
     G = GAP.Globals.Group(gapM)
     GAP.Globals.SetNiceMorphismForJuliaMatrixRepGroup(G)
     @test GAP.Globals.Size(G) == 4
     
+    m2 = matrix(QQ, [ -1 0; 0 1]);
+    G = Oscar.MatrixGroups.MatrixGroup([m,m2])
+    @test GAP.Globals.Size(G) == 8
 end
