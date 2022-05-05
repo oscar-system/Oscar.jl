42fc8f7b88b08a2b3455db6ce77b6fac5dd29423
Daniel Rademacher <rademacher@art.rwth-aachen.de>
2022-05-05 16:31:07 +0200

    Add documentation and tests.

--- test/Experimental/DiscreteLog-test.jl
+++ test/Experimental/DiscreteLog-test.jl
@@ -1,12 +1,10 @@
-@testset "Experimental.galois_group" begin
+@testset "Experimental.DiscreteLog" begin
 
-  Zx, x = ZZ["x"]
-  k, a = number_field(x^5-2)
-  G, C = galois_group(k)
-  @test transitive_identification(G) == 3
+  F = GF(3,4);
+  a = gen(F)^21;
+  @test Oscar.DiscreteLog.disc_log(gen(F), a) == 21
+  @test_throws "disc_log failed" Oscar.DiscreteLog.disc_log(one(F), a)
 
-  U = trivial_subgroup(G)[1]
-  L = fixed_field(C, U)
-  @test degree(L) == order(G)
-  @test length(roots(k.pol, L)) == 5
+  F2 = GF(3,5);
+  @test_throws AssertionError Oscar.DiscreteLog.disc_log(gen(F), gen(F2))
 end
