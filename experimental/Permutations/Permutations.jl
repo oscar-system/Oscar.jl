a22a3ce0509d7d5fe2c6a0f32d6d662bf59af06c
Daniel Rademacher <rademacher@art.rwth-aachen.de>
2022-05-05 13:06:51 +0200

    Good work.

--- experimental/Permutations/Permutations.jl
+++ experimental/Permutations/Permutations.jl
@@ -4,45 +4,21 @@ module Permutations
  using Oscar
  using MacroTools
 
-macro permNew3(ex)
+macro perm(ex)
     h = (ex).head
     arg = (ex).args
     res = []
     
     while h != :tuple
         arg1 = arg[1]
-        arguments = deleteat!(arg,1)
-        for i in 1:length(arguments)
-            if !isexpr(arguments[i],Int64)
-                arguments[i] = eval(arguments[i])
-            end
-        end
-        insert!(res,1,Tuple(x for x in arguments))
+        insert!(res,1,Expr(:vect,arg[2:length(arg)]...))
         h = (arg1).head
         arg = (arg1).args
     end
     
-    for i in 1:length(arg)
-        if !isexpr(arg[i],Int64)
-            arg[i] = eval(arg[i])
-        end
-    end
-    insert!(res,1,Tuple(x for x in arg))
-    
-    n = length(res)
-    if n == 1
-        return res[1]
-    end
-    if n == 2
-        return (res[1],res[2])
-    end
+    insert!(res,1,Expr(:vect,arg...))
 
-    r = (res[1],res[2])
-    for i in 3:n
-        r = (r, res[i])
-    end
-
-    return r
+    return :(Oscar.cperm($(res...)))
 end
 
 
