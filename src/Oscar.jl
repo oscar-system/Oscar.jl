ae178b905d1700b0af79f9588cd21e1857ddddb2
Benjamin Lorenz <lorenz@math.tu-berlin.de>
2022-05-12 22:26:06 +0200

    versioninfo: add commit and commitdate

--- src/Oscar.jl
+++ src/Oscar.jl
@@ -33,36 +33,39 @@ const jll_deps = String["Antic_jll", "Arb_jll", "Calcium_jll", "FLINT_jll", "GAP
                         "libpolymake_julia_jll", "libsingular_julia_jll", "msolve_jll",
                         "polymake_jll", "Singular_jll"];
 
-function _lookup_git_branch(dir::AbstractString)
+function _lookup_git_branch(dir::AbstractString; commit=false)
+   info = ""
    if length(Sys.which("git")) != nothing &&
          isdir(joinpath(dir,".git"))
       try
-         ref = cd(dir) do
-            readchomp(`git rev-parse --abbrev-ref HEAD`)
+         ref = readchomp(`git -C $dir rev-parse --abbrev-ref HEAD`)
+         info = " - #$(ref)"
+         if commit
+            c = readchomp(`git -C $dir show -s --format="%h -- %ci" HEAD`)
+            info = "$info, $c"
          end
-         return " - branch #$(ref)"
       catch
       end
    end
-   return ""
+   return info
 end
 
-function _deps_git_info(dep::Pkg.API.PackageInfo)
+function _deps_git_info(dep::Pkg.API.PackageInfo; commit=false)
    if dep.is_tracking_repo
-      return " - branch #$(dep.git_revision)"
+      return " - #$(dep.git_revision)"
    elseif dep.is_tracking_path
-      return _lookup_git_branch(dep.source)
+      return _lookup_git_branch(dep.source; commit=commit)
    end
    return ""
 end
 
-function _print_dependency_versions(io::IO, deps::AbstractArray{<:AbstractString}; padding="    ", suffix="", branch=false)
+function _print_dependency_versions(io::IO, deps::AbstractArray{<:AbstractString}; padding="    ", suffix="", branch=false, commit=false)
    width = maximum(length.(deps))+length(suffix)+2
    deps = filter(d->d.name in deps, collect(values(Pkg.dependencies())))
    deps = sort!(deps; by=x->x.name)
    for dep in deps
       print(io, "$(padding)$(rpad(dep.name*suffix, width, ' ')) v$(dep.version)")
-      println(io, branch ? _deps_git_info(dep) : "")
+      println(io, branch ? _deps_git_info(dep; commit=commit) : "")
    end
 end
 
@@ -76,14 +79,17 @@ Print the versions of all Oscar-related dependencies.
 - `jll::Bool=false`   : include binary packages (jll) in the output
 - `julia::Bool=false` : include julia `versioninfo` output
 """
-function versioninfo(io::IO=stdout; branch=false, jll=false, julia=false)
+function versioninfo(io::IO=stdout; branch=false, jll=false, julia=false, commit=false, full=false)
+   if full
+      branch = jll = julia = commit = true
+   end
    print(io, "OSCAR version $(VERSION_NUMBER)")
-   println(io, branch ? _lookup_git_branch(dirname(@__DIR__)) : "")
+   println(io, branch ? _lookup_git_branch(dirname(@__DIR__); commit=commit) : "")
    println(io, "  combining:")
-   _print_dependency_versions(io, cornerstones; suffix=".jl", branch=branch)
+   _print_dependency_versions(io, cornerstones; suffix=".jl", branch=branch, commit=commit)
    if jll
       println(io, "  building on:")
-      _print_dependency_versions(io, jll_deps; branch=branch)
+      _print_dependency_versions(io, jll_deps; branch=branch, commit=commit)
       println(io, "See `]st -m` for a full list of dependencies.")
    end
    if julia
