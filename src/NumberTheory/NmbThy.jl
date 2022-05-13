e2d8d896df1f95b2253c3f55a639905479ae796f
Max Horn <max@quendi.de>
2022-05-13 14:54:13 +0200

    Apply suggestions from code review

--- src/NumberTheory/NmbThy.jl
+++ src/NumberTheory/NmbThy.jl
@@ -211,11 +211,14 @@ end
 #
 @doc Markdown.doc"""
     disc_log(b::T, x::T) where {T <: FinFieldElem}
+
 Return an integer `s` such that $b^s = x$.
 If no such `x` exists, an exception is thrown.
-# Examples (jldoctest)
-```
+
+# Examples
+```jldoctest
 julia> F = GF(3,4); a = gen(F)^21;
+
 julia> disc_log(gen(F), a)
 21
 ```
