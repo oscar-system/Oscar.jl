72e431a2c859b8eaca163ef630455f3faa2e84c5
Antony <antonydellavecchia@gmail.com>
2022-05-17 18:59:39 +0200

    all field tests working but one

--- test/Serialization/Fields.jl
+++ test/Serialization/Fields.jl
@@ -9,16 +9,23 @@
             filename = joinpath(path, "polynomial_se.uv")
             save(q, filename)
             loaded = load(filename)
+            L = base_ring(parent(loaded))
+            h = hom(K, L, gen(L))
+            @test [h(c) for c in coefficients(q)] == collect(coefficients(loaded))
         end
 
         @testset "MV Polynomial Over Non Simple Extension" begin
             R, t = PolynomialRing(QQ, "t")
             K, a = NumberField([t^2 + 5, t^2 + 7], "a")
-            Kxy, (x, y) = K["x", "y"]
-            q = a[1] * x^2 + a[2] * y
+            Kxy, (x, y) = PolynomialRing(K, ["x", "y"])
+            p = a[1] * x^2 + a[2] * y
             filename = joinpath(path, "polynomial_nse.mv")
-            save(q, filename)
+            save(p, filename)
             loaded = load(filename)
+            loaded_parent = parent(loaded)
+            loaded_coeff_ring = coefficient_ring(loaded_parent)
+            h = hom(K, loaded_coeff_ring, gens(loaded_coeff_ring))
+            @test [h(c) for c in coefficients(p)] == collect(coefficients(loaded))
         end
 
         @testset "UV Polynomial Over Finite Field Extension" begin
@@ -29,18 +36,26 @@
             filename = joinpath(path, "polynomial_ffe.uv")
             save(p, filename)
             loaded = load(filename)
+            L = base_ring(parent(loaded))
+            h = hom(F, L, gen(L))
+            @test [h(c) for c in coefficients(p)] == collect(coefficients(loaded))
         end
 
         @testset "UV Polynomial Over Field Tower Extension" begin
             Qx, x = QQ["x"]
-            K, a = NumberField(x^3 - 2, "a", cached=true)
+            K, a = NumberField(x^3 - 2, "a")
             Ky, y = K["y"]
-            L, b = NumberField(y^2 + 1, "b", cached=true)
+            L, b = NumberField(y^2 + 1, "b")
             Lz, z = L["z"]
             p = a^2 * b * z^2 + a * z + b
             filename = joinpath(path, "polynomial_fte.uv")
             save(p, filename)
             loaded = load(filename)
+            loaded_base = base_ring(parent(loaded))
+            F = base_field(loaded_base)
+            h_1 = hom(K, F, gen(F))
+            h_2 = hom(L, loaded_base, h_1, gen(loaded_base))
+            @test [h_2(c) for c in coefficients(p)] == collect(coefficients(loaded))
         end
     end
 end
