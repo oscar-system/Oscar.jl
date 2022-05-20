55483e8363900029cf9683483bf9a8a1a2cc2fee
Antony <antonydellavecchia@gmail.com>
2022-05-20 10:52:51 +0200

    removed where T in field serializations

--- src/Serialization/Fields.jl
+++ src/Serialization/Fields.jl
@@ -51,17 +51,14 @@ end
 
 ################################################################################
 # SimpleNumField
-function save_internal(s::SerializerState,
-                       K::SimpleNumField{T}) where T
+function save_internal(s::SerializerState, K::SimpleNumField)
     return Dict(
         :def_pol => save_type_dispatch(s, defining_polynomial(K)),
         :var => save_type_dispatch(s, String(var(K)))
     )
 end
  
-function load_internal(s::DeserializerState,
-                       ::Type{<: SimpleNumField{T}},
-                       dict::Dict) where T
+function load_internal(s::DeserializerState, ::Type{<: SimpleNumField}, dict::Dict)
     def_pol = load_type_dispatch(s, dict[:def_pol], check_namespace=false)
     var = load_type_dispatch(s, dict[:var], check_namespace=false)
     K, _ = NumberField(def_pol, var, cached=false)
@@ -71,16 +68,13 @@ end
 
 ################################################################################
 # Fqnmodfinitefield
-function save_internal(s::SerializerState,
-                       K::FqNmodFiniteField)
+function save_internal(s::SerializerState, K::FqNmodFiniteField)
     return Dict(
         :def_pol => save_type_dispatch(s, defining_polynomial(K))
     )
 end
 
-function load_internal(s::DeserializerState,
-                       ::Type{FqNmodFiniteField},
-                       dict::Dict) where T
+function load_internal(s::DeserializerState, ::Type{FqNmodFiniteField}, dict::Dict)
     def_pol = load_type_dispatch(s, dict[:def_pol], check_namespace=false)
     K, _ = FiniteField(def_pol, cached=false)
     
@@ -88,8 +82,7 @@ function load_internal(s::DeserializerState,
 end
 
 #elements
-function save_internal(s::SerializerState,
-                       k::Union{nf_elem, fq_nmod, Hecke.NfRelElem{T}}) where T
+function save_internal(s::SerializerState, k::Union{nf_elem, fq_nmod, Hecke.NfRelElem}) 
     K = parent(k)
     polynomial = parent(defining_polynomial(K))(k)
     K_dict = save_type_dispatch(s, K)
@@ -101,8 +94,8 @@ function save_internal(s::SerializerState,
 end
 
 function load_internal(s::DeserializerState,
-                       ::Type{<: Union{nf_elem, fq_nmod, Hecke.NfRelElem{T}}},
-                       dict::Dict) where T
+                       ::Type{<: Union{nf_elem, fq_nmod, Hecke.NfRelElem}},
+                       dict::Dict)
     K = load_type_dispatch(s, dict[:parent], check_namespace=false)
     polynomial = load_type_dispatch(s, dict[:polynomial], check_namespace=false)
     return K(polynomial)
