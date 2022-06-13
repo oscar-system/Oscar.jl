151e99f4c7ae2ea5ce9166089a88f07f67f0ee81
Tommy Hofmann <thofma@gmail.com>
2022-06-13 15:20:51 +0200

    Fix field interface for QQAbField

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using Oscar
 using Test
 
+import Oscar.Nemo.AbstractAlgebra
+include(joinpath(pathof(AbstractAlgebra), "..", "..", "test", "Rings-conformance-tests.jl"))
+
 # Used in both Rings/slpolys-test.jl and StraightLinePrograms/runtests.jl
 const SLP = Oscar.StraightLinePrograms
 include("printing.jl")
