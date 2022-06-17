c612a18a75580eeb6baa24326dd12f30877c5356
antonydellavecchia <antonydellavecchia@gmail.com>
2022-06-17 13:54:32 +0200

    Adv/katsura ideals (#1402)

---
+++ test/Rings/special_ideals.jl
@@ -0,0 +1,15 @@
+@testset "Special Ideals" begin
+    @testset "Katsura Ideals" begin
+        I = katsura(5)
+        R = parent(gens(I)[1])
+        (x1, x2, x3, x4, x5, x6) = gens(R)
+        @test gens(I) == [
+            x1 + 2 * x2 + 2 * x3 + 2 * x4 + 2 * x5 + 2 * x6 - 1,
+            x1^2 - x1 + 2 * x2^2 + 2 * x3^2 + 2 * x4^2 + 2 * x5^2 + 2 * x6^2,
+            2 * x1 * x2 + 2 * x2 * x3 - x2 + 2 * x3 * x4 + 2 * x4 * x5 + 2 * x5 * x6,
+            2 * x1 * x3 + x2^2 + 2 * x2 * x4 + 2 * x3 * x5 - x3 + 2 * x4 * x6,
+            2 * x1 * x4 + 2 * x2 * x3 + 2 * x2 * x5 + 2 * x3 * x6 - x4,
+            2 * x1 * x5 + 2 * x2 * x4 + 2 * x2 * x6 + x3^2 - x5,
+        ]
+    end
+end
