074ebf88570c8d1944ec1fd48f1c86f629ae56e9
dan <tthsqe12@gmail.com>
2022-06-24 14:17:56 +0200

    all lifts are in singular.jl now

--- src/Rings/MPolyQuo.jl
+++ src/Rings/MPolyQuo.jl
@@ -745,55 +745,12 @@ function is_invertible_with_inverse(a::MPolyQuoElem)
 end
 
 is_unit(a::MPolyQuoElem) = is_invertible_with_inverse(a)[1]
+
 function inv(a::MPolyQuoElem)
   fl, b = is_invertible_with_inverse(a)
   fl || error("Element not invertible")
   return b
 end
-
-"""
-Tries to write the generators of `M` as linear combinations of generators in `SM`.
-Extremely low level, might migrate to Singular.jl and be hidd...
-
-Seems to be buggy
-
-Returns result, rest
-(Matrix(SM)-Matrix(rest) = Matrix(M)*Matrix(result))
-If SM is in M, rest is the null module
-otherwise: rest = SM
-"""
-function lift(M::Singular.sideal{T}, SM::Singular.sideal{T}) where T
-   R = base_ring(M)
-   R == base_ring(SM) || error("base rings must match")
-   ptr, rest_ptr = GC.@preserve M SM R Singular.libSingular.id_Lift(M.ptr, SM.ptr, R.ptr)
-   return Singular.Module(R, ptr), Singular.Module(R,rest_ptr)
-end
-
-@doc Markdown.doc"""
-    lift(M::sideal, SM::sideal, goodShape::Bool, isSB::Bool, divide::Bool)
-
-represents the generators of SM in terms of the generators of M.
-Returns result, rest
-(Matrix(SM)*U-Matrix(rest) = Matrix(M)*Matrix(result))
-If SM is in M, rest is the null module
-otherwise: rest = SM (if not divide)
-or: rest=normalform(SM,std(M))
-U is a diagonal matrix of units, differs from unity matrix only for local ring orderings
-
-goodShape: maximal non-zero index in generators of SM <= that of M
-isSB: generators of M form a Groebner basis
-divide: allow SM not to be a submodule of M
-"""
-function lift(M::Singular.sideal{T}, SM::Singular.sideal{T},
-                            goodShape::Bool, isSB::Bool, divide::Bool) where T
-   R = base_ring(M)
-   R == base_ring(SM) || error("base rings must match")
-   res, rest, U = GC.@preserve M SM R Singular.libSingular.id_Lift(M.ptr, SM.ptr,
-                                                goodShape, isSB, divide, R.ptr)
-   return (Singular.smodule{T}(R, res), Singular.smodule{T}(R, rest), Singular.smatrix{T}(R, U))
-end
-
-
 
 
 """
