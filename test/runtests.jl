ad85221c162e81d2e8c398c3505131837cc44a33
ederc <ederc@mathematik.uni-kl.de>
2022-07-27 13:53:05 +0200

    fixes test suite

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,13 +6,13 @@ include(joinpath(pathof(AbstractAlgebra), "..", "..", "test", "Rings-conformance
 
 # Used in both Rings/slpolys-test.jl and StraightLinePrograms/runtests.jl
 const SLP = Oscar.StraightLinePrograms
-#= include("printing.jl")
- =
- = include("PolyhedralGeometry/runtests.jl")
- = include("Combinatorics/runtests.jl")
- =
- = include("GAP/runtests.jl")
- = include("Groups/runtests.jl") =#
+include("printing.jl")
+
+include("PolyhedralGeometry/runtests.jl")
+include("Combinatorics/runtests.jl")
+
+include("GAP/runtests.jl")
+include("Groups/runtests.jl")
 
 include("Rings/runtests.jl")
 
