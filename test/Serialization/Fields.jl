d267168b0736baf41218adf8fcfd25b1748e8f02
antonydellavecchia <antonydellavecchia@gmail.com>
2022-07-28 14:44:46 +0200

    Adv/serialization ns field emb (#1478)
    * save and load methods
    
    * tests and safe type de/en coding
    
    Co-authored-by: Antony Della Vecchia <vecchia@blofield.math.tu-berlin.de>

--- test/Serialization/Fields.jl
+++ test/Serialization/Fields.jl
@@ -12,5 +12,22 @@
                 @test overlaps(E(gen(K)), loaded(g))
             end
         end
+
+        @testset "Simple Field Embeddings" begin
+            Qx, x = QQ["x"]
+            K, _ = NumberField([x^2 + 5, x^2 + 7])
+            E = complex_embeddings(K)[1]
+
+            test_save_load_roundtrip(path, E) do loaded
+                loaded_K = number_field(loaded)
+                loaded_gens = map(loaded, gens(loaded_K))
+                E_gens = map(E, gens(K))
+                compare_vector_1 = collect(zip(E_gens, loaded_gens))
+                compare_vector_2 = collect(zip(loaded_gens, E_gens))
+                check_overlaps = y -> isempty(filter(x -> !overlaps(x...), y))
+                @test check_overlaps(compare_vector_1)
+                @test check_overlaps(compare_vector_2)
+            end
+        end
     end
 end
