9a419074236897001ad8418f4696b51386930065
antonydellavecchia <antonydellavecchia@gmail.com>
2022-08-10 11:34:17 +0200

    Update src/Serialization/Rings.jl
    Co-authored-by: Max Horn <max@quendi.de>

--- src/Serialization/Rings.jl
+++ src/Serialization/Rings.jl
@@ -165,7 +165,7 @@ function load_internal_with_parent(s::DeserializerState,
                                    ::Type{<: PolyElem},
                                    dict::Dict,
                                    parent_ring::PolyRing)
-    # load parent in case serialzed parent needs to be checked against given parent
+    # load parent in case serialized parent needs to be checked against given parent
     _, _ = load_unknown_type(s, dict[:parent])
     
     coeff_ring = coefficient_ring(parent_ring)
