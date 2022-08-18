c40922e528213a4c49758adae1d37c270705b46a
dan <tthsqe12@gmail.com>
2022-08-18 15:44:23 +0200

    attach test code

--- test/Rings/runtests.jl
+++ test/Rings/runtests.jl
@@ -25,6 +25,7 @@ include("MPolyAnyMap/MPolyRing.jl")
 include("MPolyAnyMap/MPolyQuo.jl")
 include("MPolyAnyMap/AffineAlgebras.jl")
 
+include("GAlgebra-test.jl")
 include("FreeAssAlgIdeal-test.jl")
 
 include("binomial-ideals-test.jl")
