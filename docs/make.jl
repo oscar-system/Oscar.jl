c112c759c2c73790840a8daf09a3d5d9e162740e
Martin Bies <MBies87@googlemail.com>
2022-09-05 16:42:23 -0400

    Attempt to fix order of pages in online documentation

--- docs/make.jl
+++ docs/make.jl
@@ -4,6 +4,16 @@ using Documenter, FTheoryTools, DocumenterMarkdown
 # ensure FTheoryTools is loaded for the doc tests
 DocMeta.setdocmeta!(FTheoryTools, :DocTestSetup, :(using FTheoryTools, Random); recursive = true)
 
-makedocs(sitename="FTheoryTools")
+makedocs(
+    format = Documenter.HTML(),
+    sitename = "FTheoryTools",
+    modules = [FTheoryTools],
+    clean = true,
+    doctest = false,
+    pages = ["index.md",
+            "FTheoryTools/Introduction.md",
+            "Tools" => ["FTheoryTools/Functions.md"],
+            ]
+    )
 
 deploydocs(repo = "github.com/HereAround/FTheoryTools.jl.git")
