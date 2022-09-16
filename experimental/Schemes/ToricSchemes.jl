20ab1dab24e4575c35c5af589641b23021c2fd1a
Matthias Zach <zach@math.uni-hannover.de>
2022-09-16 11:33:39 +0200

    Add some comments [no-ci].

--- experimental/Schemes/ToricSchemes.jl
+++ experimental/Schemes/ToricSchemes.jl
@@ -24,7 +24,6 @@ underlying_scheme(X::ToricSpec) = X.X
 affine_normal_toric_variety(X::ToricSpec) = X.antv
 antv = affine_normal_toric_variety
 cone(X::ToricSpec) = cone(antv(X))
-hilbert_basis(X::ToricSpec) = matrix(ZZ, hilbert_basis(cone(X)))
 
 @Markdown.doc """
     torus_inclusions(X::ToricSpec)
@@ -34,6 +33,7 @@ containing the inclusions ``Tʳ⁽ⁱ⁾ ↪ X`` of the different
 tori. 
 """
 function torus_inclusions(X::ToricSpec)::Vector{<:AbsSpecMor}
+  #TODO: Fill in 
 end
 
 @Markdown.doc """
@@ -49,10 +49,11 @@ consisting of
  * the group action ``T × X → X``.
 """
 function torus_action(X::ToricSpec)::AbsSpecMor
+  #TODO: Fill in 
 end
 
 function Base.show(io::IO, X::ToricSpec) 
-  print(io, "Spec of a toric variety with Hilbert basis $(hilbert_basis(X))")
+  print(io, "Spec of a toric variety with cone spanned by $(rays(cone(X)))")
 end
 
 @attributes mutable struct ToricCoveredScheme{BRT, 
@@ -66,10 +67,22 @@ end
     C = maximal_cones(F)
 
     antv = affine_open_covering(ntv)
-    rings = [PolynomialRing(coefficient_ring(antv), ["x_$(i)_$(k)" for i in 1:ngens(base_ring(toric_ideal(antv[k])))], cached=false)[1] for k in 1:length(antv)]
+    rings = [PolynomialRing(coefficient_ring(antv[k]), ["x_$(i)_$(k)" for i in 1:ngens(base_ring(toric_ideal(antv[k])))], cached=false)[1] for k in 1:length(antv)]
     U = [ToricSpec(U, R=R) for (U, R) in zip(antv, rings)]
     cov = Covering(U)
     ### insert the glueings
+    # TODO: We need to
+    #  
+    #  * Find a Hilbert basis for the dual cone Cᵛ of every maximal cone C ⊂ F;
+    #  * relate them to the variables of the `base_ring`s of the toric ideals 
+    #    of the affine patches;
+    #  * find the elements -m ∈ Cᵛ for which τ = C ∩ {m = 0} is a common face 
+    #    with another cone C';
+    #  * express -m in terms of the Hilbert basis and localized in the corresponding 
+    #    element of the coordinate ring;
+    #  * glue the pairs of localizations by expressing either extended Hilbert 
+    #    basis in terms of the other.
+
     X = CoveredScheme(cov)
     return new{typeof(base_ring(X)), typeof(X)}(X, ntv)
   end
