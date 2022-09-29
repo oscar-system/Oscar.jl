deba19706543ca495b8e4f623368ce3d44c04deb
anne <anne@krueger-berg.de>
2022-09-29 09:18:13 +0200

    initial commit space germs

---
+++ docs/src/Experimental/Singularities/space_germs.md
@@ -0,0 +1,203 @@
+```@meta
+CurrentModule = Oscar
+```
+
+```@setup oscar
+using Oscar
+```
+
+```@contents
+Pages = ["space_germs.md"]
+```
+
+# Space Germs
+
+## [Generalities on Space germs](@id space_germ_generalities)
+
+The geometric notion of a space germ is a local concept. A space germ (X,x) at a point $x$ is an equivalence class of ringed spaces, each of which contains $x$ in its underlying topological space, and the equivalence relation is precisely the existence of an open neighbourhood of $x$ on which the spaces coincide.
+
+Depending on the kind of ringed space in question, space germs arise in
+different forms:
+
+  * a space germ in the context of affine schemes is the geometric object arising from a given scheme by localization at a point, leading to the stalk of the structure sheaf at the respective prime ideal.
+
+  * in the context of singularity theory, the (anti-)equivalence of categories between complex space germs and analytic ``{CC}``-algebras allows the direct definition of a space germ from algebraic data
+
+Note that analytic algebras as mentioned above, have two computational problems.
+On one hand, exact computations can only be performed over fields in OSCAR
+permitting exact computations, in particular not over ``{\mathbb R}`` or 
+``{\mathbb C}``. This usually does not pose a problem, if the input data is 
+in a exact smaller field. But unfortunately, also analytic algebras themselves 
+do not allow exact computations so that applications have to be considered 
+in a localization of an affine algebra. With due care, output may again be 
+interpreted in terms of a multivariate formal power series using the following 
+inclusions:
+```math
+{\mathbb Q}[\underline{x}]_{\langle x \rangle} \hookrightarrow
+  {\mathbb Q}\{\underline{x}\}\hookrightarrow
+  {\mathbb Q}[[\underline{x}]]
+```
+At particular points, where this difficulty of interpretation 
+manifests itself prominently, a suitable warning, note or example has been 
+placed (but certainly not everywhere). 
+
+Textbooks covering space germs in the sense of analytic algebras and
+singularity theory are:
+- [GLS07](@cite)
+- [dJP00](@cite)
+
+For the point of view of schemes, we refer to the page on schemes and the references given there; an example of a standard textbook is
+- [Har77](@cite).
+
+## Creating Space Germs in OSCAR
+
+In general, space germs in Oscar are created in the following ways:
+
+ * localization of an affine scheme at a point
+
+   ```julia
+   SpaceGerm(X::AffineScheme, I::Ideal)
+   ```
+   where I is a (maximal) ideal describing the chosen basering(X)-point on the scheme.  
+   **Provides:** SpaceGerm.
+   ```julia
+   germ_at-point(X::AffineScheme, I::Ideal)
+   ```
+   where I is a (maximal) ideal describing the chosen basering(X)-point on the scheme.  
+
+   **Provides:** SpaceGerm, restriction map.
+
+
+ * localization of a polynomial ring at a point
+
+   ```julia
+   germ_at_point(R::MPolyRing, I::MPolyIdeal)
+   germ_at_point(R::MPolyQuo, I::MPolyIdeal)
+   ```
+   where I is a maximal ideal describing the chosen base_ring(R)-point.
+
+   **Provides:** space germ and restriction map.
+
+ * localized ring with respect to complement of prime ideal or complement of maximal ideal)
+
+   ```julia
+   germ_at_point(R::MPolyLocalizedRing)
+   germ_at_point(R::MPolyQuoLocalizedRing)
+   ```
+
+   **Provides:** Space germ, restriction map  
+
+   (point inherited from underlying local ring)
+
+Morphisms of space germs are morphisms of the underlying local rings and hence
+also handled in this way in Oscar.
+
+## Basic functionality for space germs
+
+Most of the basic functionality immediately falls back to the underlying 
+affine algebra or affine scheme and is just provided on the geometric side 
+for convenience and consistence of functionality:
+
+### internal data of a space germ  
+
+ * Pass from the germ $(X,x)$ back to some affine scheme $X$ with the appropriate localization at $x$ by
+   ```julia
+   representative(X::SpaceGerm)
+   ```
+   **Provides:** AffineScheme   
+
+ * Given the space germ $(X,x)$, the point $x$ is returned by:
+
+   ```julia
+   point(X:SpaceGerm)
+   ```
+   **Provides:** Ideal
+
+!!! note
+    The returned ideal is a prime ideal in the ring of a representative of the germ. At first use of it or at the latest upon the first call of representative, the respective affine scheme is cached and subsequently used for all further purposes requiring a representative
+
+ 
+ * Given a space germ $(X,x)$, the corresponding local ring ${\mathcal O}_{(X,x)}$ is returned by:
+   ```julia
+   ring_of_germ(X::SpaceGerm)
+   ```
+   **Provides:** MPolyQuoLocalizedRing
+
+ * Analogously, the modulus of the ring of a given germ $(X,x)$ can be obtained by:
+   ```julia
+   ideal_of_germ(X::SpaceGerm)
+   ```
+   **Provides:** Ideal
+
+ * For technical reasons all germs $(X,x)$ in OSCAR are embedded and the smooth ambient germ can be accessed by:
+   ```julia
+   ambient_germ(X::SpaceGerm)
+   ```
+   **Provides:** SpaceGerm
+
+### containment/equality of space germs
+    
+ * containment  
+
+   ```julia
+   issubset(X::SpaceGerm, Y::SpaceGerm)
+   ```
+
+   Test whether X is a subgerm of Y.  
+
+   **Provides:** Boolean Value
+ 
+!!! note
+    The name 'issubset' has been chosen for consistency with other types, but is slightly misleading, as the function does not only test containment of the underlying sets, but in the scheme-theoretic sense.
+
+ * equality
+
+   ```julia
+   is_canonically_isomorphic(X::SpaceGerm, Y::SpaceGerm)
+   ```
+
+   **Provides:** Boolean Value
+
+!!! note
+    Equality of the germs X and Y is tested in the sense of equality of subgerms of the same ambient space germ.  
+
+
+ * emptyness
+
+   For the test of equality to the empty germ, a separate function is available:
+   ```julia
+   is_empty(X::SpaceGerm)
+   ```
+
+   **Provides:** Boolean Value
+
+   * intersection
+     ```julia
+     intersect(X::SpaceGerm,Y::SpaceGerm)
+     ```
+     Computes the intersection of two subgerms of a common larger germ.
+
+     **Provides**: SpaceGerm   
+
+### singular locus
+
+```julia
+singular_locus(X::SpaceGerm)
+```
+
+   Computes the germ of the singular locus of the given germ.
+
+   **Provides:** SpaceGerm  
+  
+If a test for smoothness is the goal, the following functions can be used
+
+```julia
+is_smooth(X::SpaceGerm)
+```
+
+   **Provides:** Boolean Value
+
+<!-- ```julia is_regular(X::SpaceGerm)```  still needs to be implemented -->
+
+
+<!-- ...to be continued -->
---
+++ experimental/Schemes/SpaceGerms.jl
@@ -0,0 +1,213 @@
+export SpaceGerm
+
+export representative, point
+
+export germ_at_point
+
+import AbstractAlgebra: Ring
+
+@Markdown.doc """
+    AbsSpaceGerm{BaseRingType<:Ring,RingType<:Ring}
+
+A space germ, i.e. a ringed space ``(X,O_{(X,x)})`` with local ring ``O_{(X,x)}`` of type `RingType` over a ring ``k`` of type `BaseRingType`.
+"""
+abstract type AbsSpaceGerm{BaseRingType<:Ring, RingType<:Ring} <: AbsSpec{BaseRingType, RingType} end
+
+function underlying_space_germ(X::AbsSpaceGerm) 
+  error("`underlying_space_germ(X)` not implemented for `X` of type $(typeof(X))")
+end
+
+####################################################################
+## two short hand definitions for internal use only
+####################################################################
+GermAtClosedPoint = Spec{<:Ring, 
+                         <:AbsLocalizedRing{<:Ring, <:Any, 
+                                            <:MPolyComplementOfKPointIdeal}
+                        }
+GermAtGeometricPoint = Spec{<:Ring, 
+                            <:AbsLocalizedRing{<:Ring, <:Any, 
+                                               <:MPolyComplementOfPrimeIdeal}
+                           }
+
+###################################################################
+## TODO; This should go to AffineSchemes
+## modulus should return something appropriate for all MPoly...Rings
+###################################################################
+
+localized_modulus(R::MPolyLocalizedRing)
+   return ideal(R,[zero(R)])
+end
+
+@Markdown.doc """
+    SpaceGerm{BaseRingType, RingType, SpecType}
+A space germ ``(X,O_{(X,x)}``, i.e. a ringed space with underlying scheme ``X`` of type SpecType and local ring ``O_{(X,x)}`` of type `RingType` over some base ring ``k`` of type `BaseRingType`.
+"""
+@attributes mutable struct SpaceGerm{BaseRingType<:Ring, RingType<:Ring, SpecType<:Spec} <: AbsSpaceGerm{BaseRingType, RingType}
+  X::SpecType
+
+  function SpaceGerm(X::GermAtClosedPoint)
+    return new{typeof(base_ring(X)), typeof(OO(X)), typeof(X)}(X)
+  end
+  
+  function SpaceGerm(X::GermAtGeometricPoint)
+    return new{typeof(base_ring(X)), typeof(OO(X)), typeof(X)}(X)
+  end
+end
+
+### Getter functions
+
+function underlying_scheme(X::SpaceGerm)
+  return X.X
+end
+
+@attr Spec function representative(X::SpaceGerm{<:Ring, <:MPolyQuoLocalizedRing})
+    R = base_ring(OO(X))
+    I = modulus(OO(X))
+    Q, _ = quo(R, I)
+    return Spec(Q)
+end
+
+@attr Spec function representative(X::SpaceGerm{<:Ring, <:MPolyLocalizedRing})
+    R = base_ring(OO(X))
+    return Spec(R)
+end
+
+function point(X::SpaceGerm{<:Any, <:Any, <:GermAtClosedPoint})
+  return point_coordinates(inverted_set(OO(X)))
+end
+
+function point(X::SpaceGerm{<:Any, <:Any, <:GermAtGeometricPoint})
+  return prime_ideal(inverted_set(OO(X)))
+end
+
+ring_of_germ(X::AbsSpaceGerm) = OO(X)
+
+function ideal_of_germ(X::AbsSpaceGerm{<:Ring,<:MPolyQuoLocalizedRing})
+    return localized_ring(OO(X))(modulus(OO(X))
+end
+
+function ideal_of_germ(X::AbsSpaceGerm{<:Ring,<:MPolyLocalizedRing})
+    return ideal(OO(X),[zero(OO(X))])
+end
+
+function ambient_germ(X::AbsSpaceGerm{<:Ring,<:MPolyQuoLocalizedRing})
+    Y,_ = germ_at_point(localized_ring(OO(X)))
+    return Y
+end
+
+function ambient_germ(X::AbsSpaceGerm{<:Ring,<:MPolyLocalizedRing})
+    return X
+end
+
+### constructors
+function SpaceGerm(X::Spec, I::MPolyLocalizedIdeal; check::Bool=true)
+  R = base_ring(base_ring(I))
+  R == ambient_ring(X) || error("rings are not compatible")
+  P = saturated_ideal(I)
+  if check
+    is_prime(P) || error("the given ideal is not prime")
+  end
+  U = complement_of_ideal(P, check=check)
+  Y = Spec(Localization(OO(X), U)[1])
+  return SpaceGerm(Y)
+end
+
+function SpaceGerm(X::Spec, I::MPolyQuoLocalizedIdeal; check::Bool=true)
+  R = base_ring(base_ring(I))
+  R == ambient_ring(X) || error("rings are not compatible")
+  P = saturated_ideal(I)
+  if check
+    is_prime(P) || error("the given ideal is not prime")
+  end
+  U = complement_of_ideal(P, check=check)
+  Y = Spec(Localization(OO(X), U)[1])
+  return SpaceGerm(Y)
+end
+
+function SpaceGerm(X::Spec, I::MPolyIdeal; check::Bool=true)
+  if check
+    is_prime(I) || error("the given ideal is not prime")
+  end
+  R = base_ring(I)
+  R == ambient_ring(X) || error("rings are not compatible")
+
+  U = complement_of_ideal(I, check=check)
+  Y = Spec(Localization(OO(X), U)[1])
+
+  return SpaceGerm(Y)
+end
+
+function SpaceGerm(X::Spec, I::MPolyQuoIdeal; check::Bool=true)
+  A = base_ring(I)
+  A == OO(X) || error("rings are not compatible")
+  R = base_ring(A)
+  P = ideal(R, lift.(gens(I))) + modulus(A)
+  if check
+    is_prime(P) || error("the given ideal is not prime")
+  end
+  U = complement_of_ideal(P, check=check)
+  Y = Spec(Localization(OO(X), U)[1])
+
+  return SpaceGerm(Y)
+end
+
+function germ_at_point(X::Spec, I::Ideal; check::Bool=true)
+  Y = SpaceGerm(X, I, check=check)
+  restr_map = SpecMor(Y, X, hom(OO(X), OO(Y), gens(OO(Y)), check=false), check=false)
+  return Y, restr_map
+end
+
+function germ_at_point(A::MPolyRing, I::Ideal; check::Bool=true)
+  X = Spec(A)
+  Y = SpaceGerm(X, I, check=check)
+  restr_map = SpecMor(Y, X, hom(OO(X), OO(Y), gens(OO(Y)), check=false), check=false)
+  return Y, restr_map
+end
+
+function germ_at_point(A::MPolyQuo, I::Ideal; check::Bool=true)
+  X = Spec(A)
+  Y = SpaceGerm(X, I, check=check)
+  restr_map = SpecMor(Y, X, hom(OO(X), OO(Y), gens(OO(Y)), check=false), check=false)
+  return Y, restr_map
+end
+
+LocalRing = Union{MPolyQuoLocalizedRing{<:Any, <:Any, <:Any, <:Any, 
+                                        <:MPolyComplementOfKPointIdeal},
+                  MPolyLocalizedRing{<:Any, <:Any, <:Any, <:Any, 
+                                     <:MPolyComplementOfKPointIdeal},
+                  MPolyQuoLocalizedRing{<:Any, <:Any, <:Any, <:Any, 
+                                        <:MPolyComplementOfPrimeIdeal},
+                  MPolyLocalizedRing{<:Any, <:Any, <:Any, <:Any, 
+                                     <:MPolyComplementOfPrimeIdeal}
+                 }
+
+function germ_at_point(A::LocalRing; check::Bool=true)
+  X = Spec(A)
+  Y = SpaceGerm(X)
+  restr_map = SpecMor(Y, X, hom(OO(X), OO(Y), gens(OO(Y)), check=false), check=false)
+  return Y, restr_map
+end
+
+### basic functionality for space germs
+
+##########################################################################################
+# note: issubset, isempty, is_canonically_isomorphic, intersect are inherited from Spec
+##########################################################################################
+
+function Base.union(X::AbsSpaceGerm, Y::AbsSpaceGerm)
+  ambient_ring(X)==ambient_ring(Y) || error("not subgerms of a common space germ")
+  point(X)==point(Y) || error("not the same point of the germ")
+  # comparison of points implicitly also checks that localization was performed at points
+  # otherwise 'point' is not implemented
+  I=intersect(localized_modulus(X),localized_modulus(Y))
+  Z,_ = germ_at_point(quo(localized_ring(OO(X)),I)
+  return Z
+end
+
+##########################################################################################
+# note: singular_locus, is_smooth and is_regular are inherited from Spec
+##########################################################################################
+
+
+
+
