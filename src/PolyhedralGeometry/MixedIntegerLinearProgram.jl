c8c2dfa48e369c6d2cf0da4ef76c54eac4a7bc8f
Lars Kastner <lars.kastner@posteo.de>
2022-10-07 22:27:25 +0200

    PolyhedralGeometry: Further restructuring for MILP

--- src/PolyhedralGeometry/MixedIntegerLinearProgram.jl
+++ src/PolyhedralGeometry/MixedIntegerLinearProgram.jl
@@ -1,43 +1,44 @@
 @doc Markdown.doc"""
-    LinearProgram(P, c; k = 0, convention = :max)
+    MixedIntegerLinearProgram(P, c; integer_variables = [], k = 0, convention = :max)
 
-The linear program on the feasible set `P` (a Polyhedron) with
-respect to the function x ↦ dot(c,x)+k.
+The mixed integer linear program on the feasible set `P` (a Polyhedron) with
+respect to the function x ↦ dot(c,x)+k, where $x_i\in\mathbb{Z}$ for all $i$ in
+`integer_variables`.
 
 """
-struct LinearProgram{T}
+struct MixedIntegerLinearProgram{T}
    feasible_region::Polyhedron{T}
-   polymake_lp::Polymake.BigObject
+   polymake_milp::Polymake.BigObject
    convention::Symbol
    
-   LinearProgram{T}(fr::Polyhedron{T}, lp::Polymake.BigObject, c::Symbol) where T<:scalar_types = new{T}(fr, lp, c)
+   MixedIntegerLinearProgram{T}(fr::Polyhedron{T}, milp::Polymake.BigObject, c::Symbol) where T<:scalar_types = new{T}(fr, milp, c)
 end
 
 # no default = `fmpq` here; scalar type can be derived from the feasible region
-LinearProgram(p::Polyhedron{T}, x...) where T<:scalar_types = LinearProgram{T}(p, x...)
+MixedIntegerLinearProgram(p::Polyhedron{T}, x...) where T<:scalar_types = MixedIntegerLinearProgram{T}(p, x...)
 
-function LinearProgram{T}(P::Polyhedron{T}, objective::AbstractVector; k = 0, convention = :max) where T<:scalar_types
+function MixedIntegerLinearProgram{T}(P::Polyhedron{T}, objective::AbstractVector; integer_variables=Vector{Int64}([]), k = 0, convention = :max) where T<:scalar_types
    if convention != :max && convention != :min
       throw(ArgumentError("convention must be set to :min or :max."))
    end
    ambDim = ambient_dim(P)
    size(objective, 1) == ambDim || error("objective has wrong dimension.")
-   lp = Polymake.polytope.LinearProgram{scalar_type_to_polymake[T]}(LINEAR_OBJECTIVE=homogenize(objective, k))
+   milp = Polymake.polytope.MixedIntegerLinearProgram{scalar_type_to_polymake[T]}(LINEAR_OBJECTIVE=homogenize(objective, k), INTEGER_VARIABLES=integer_variables)
    if convention == :max
-      Polymake.attach(lp, "convention", "max")
+      Polymake.attach(milp, "convention", "max")
    elseif convention == :min
-      Polymake.attach(lp, "convention", "min")
+      Polymake.attach(milp, "convention", "min")
    end
-   Polymake.add(pm_object(P), "LP", lp)
-   LinearProgram{T}(P, lp, convention)
+   Polymake.add(pm_object(P), "MILP", milp)
+   MixedIntegerLinearProgram{T}(P, milp, convention)
 end
 
-LinearProgram(Q::Polyhedron{T},  objective::AbstractVector; k = 0, convention = :max) where T<:scalar_types = LinearProgram{T}(Q, objective; k = k, convention = convention)
+MixedIntegerLinearProgram(Q::Polyhedron{T}, objective::AbstractVector; integer_variables=Vector{Int64}([]), k = 0, convention = :max) where T<:scalar_types = MixedIntegerLinearProgram{T}(Q, objective; integer_variables = integer_variables, k = k, convention = convention)
 
-LinearProgram{T}(A::Union{Oscar.MatElem,AbstractMatrix}, b, c::AbstractVector; k = 0, convention = :max)  where T =
-   LinearProgram{T}(Polyhedron{T}(A, b), c;  k = k, convention = convention)
+MixedIntegerLinearProgram{T}(A::Union{Oscar.MatElem,AbstractMatrix}, b, c::AbstractVector; integer_variables=Vector{Int64}([]), k = 0, convention = :max)  where T =
+   MixedIntegerLinearProgram{T}(Polyhedron{T}(A, b), c; integer_variables = integer_variables, k = k, convention = convention)
 
-LinearProgram(x...) = LinearProgram{fmpq}(x...)
+MixedIntegerLinearProgram(x...) = MixedIntegerLinearProgram{fmpq}(x...)
 
 
 ###############################################################################
@@ -45,21 +46,32 @@ LinearProgram(x...) = LinearProgram{fmpq}(x...)
 ### Display
 ###############################################################################
 ###############################################################################
-function Base.show(io::IO, LP::LinearProgram)
-    c=dehomogenize(LP.polymake_lp.LINEAR_OBJECTIVE)
-    k=LP.polymake_lp.LINEAR_OBJECTIVE[1]
-    print(io, "The linear program\n")
-    if LP.convention == :max
+function describe(io::IO, MILP::MixedIntegerLinearProgram)
+    c=dehomogenize(MILP.polymake_milp.LINEAR_OBJECTIVE)
+    k=MILP.polymake_milp.LINEAR_OBJECTIVE[1]
+    print(io, "The mixed integer linear program\n")
+    if MILP.convention == :max
       print(io, "   max")
-    elseif LP.convention == :min
+    elseif MILP.convention == :min
       print(io, "   min")
     end
     print(io, "{c⋅x + k | x ∈ P}\n")
-    print(io, "where P is a "*string(typeof(LP.feasible_region)))
-    print(io, " and\n   c=")
+    print(io, "where P is a "*string(typeof(MILP.feasible_region)))
+    print(io, "\n   c=")
     print(io, string(c'))
     print(io, "\n   k=")
     print(io, string(k))
+    print(io, "\n   ")
+    ivar = _integer_variables(MILP)
+    if length(ivar) == 0
+        print(io, "all entries of x in ZZ")
+    else
+        print(io, join(["x"*string(i) for i in ivar],",")*" in ZZ")
+    end
+end
+
+function Base.show(io::IO, MILP::MixedIntegerLinearProgram)
+    print(io, "A mixed integer linear program")
 end
 
 
@@ -68,76 +80,82 @@ end
 ### Access
 ###############################################################################
 ###############################################################################
+function _integer_variables(milp::MixedIntegerLinearProgram)
+    return milp.polymake_milp.INTEGER_VARIABLES
+end
 
 """
-    objective_function(LP::LinearProgram; as = :pair)
+    objective_function(MILP::MixedIntegerLinearProgram; as = :pair)
 
-Return the objective function x ↦ dot(c,x)+k of the linear program LP.
+Return the objective function x ↦ dot(c,x)+k of the mixed integer linear program MILP.
 The allowed values for `as` are
 * `pair`: Return the pair `(c,k)`
 * `function`: Return the objective function as a function.
 
 
 """
-function objective_function(lp::LinearProgram{T}; as::Symbol = :pair) where T<:scalar_types
+function objective_function(milp::MixedIntegerLinearProgram{T}; as::Symbol = :pair) where T<:scalar_types
    if as == :pair
-      return Vector{T}(dehomogenize(lp.polymake_lp.LINEAR_OBJECTIVE)),convert(T, lp.polymake_lp.LINEAR_OBJECTIVE[1])
+      return Vector{T}(dehomogenize(milp.polymake_milp.LINEAR_OBJECTIVE)),convert(T, milp.polymake_milp.LINEAR_OBJECTIVE[1])
    elseif as == :function
-      (c,k) = objective_function(lp, as = :pair)
+      (c,k) = objective_function(milp, as = :pair)
       return x -> sum(x.*c)+k
    else
        throw(ArgumentError("Unsupported `as` argument: $as"))
    end
 end
 
+@doc Markdown.doc"""
+    feasible_region(milp::MixedIntegerLinearProgram)
+
+Return the feasible region of the mixed integer linear program `milp`, which is
+a `Polyhedron`.
 """
-    feasible_region(lp::LinearProgram)
-
-Return the feasible region of the linear program `lp`, which is a `Polyhedron`.
-"""
-feasible_region(lp::LinearProgram) = lp.feasible_region
+feasible_region(milp::MixedIntegerLinearProgram) = milp.feasible_region
 
 
 ###############################################################################
 ###############################################################################
-### Solving of linear programs
+### Solving of mixed integer linear programs
 ###############################################################################
 ###############################################################################
 
 
-"""
-    optimal_vertex(LP::LinearProgram)
+@doc Markdown.doc"""
+    optimal_solution(MILP::MixedIntegerLinearProgram)
 
-Return either a point of the feasible region of `LP` which optimizes the objective
-function of `LP`, or `nothing` if no such point exists.
+Return either a point of the feasible region of `MILP` which optimizes the
+objective function of `MILP`, or `nothing` if no such point exists.
 
 # Examples
-The following example constructs a linear program over the three dimensional cube, and
-obtains the vertex of the cube which maximizes the function (x,y,z) ↦ x+2y-3z.
+Take the square $[-1/2,3/2]^2$ and optimize $[1,1]$ in different settings.
 ```jldoctest
-julia> C=cube(3)
-A polyhedron in ambient dimension 3
+julia> c = cube(2, -1//2, 3//2)
+A polyhedron in ambient dimension 2
 
-julia> LP=LinearProgram(C,[1,2,-3])
-The linear program
-   max{c⋅x + k | x ∈ P}
-where P is a Polyhedron{fmpq} and
-   c=Polymake.Rational[1 2 -3]
-   k=0
+julia> milp = MixedIntegerLinearProgram(c, [1,1], integer_variables=[1])
+A mixed integer linear program
 
-julia> optimal_vertex(LP)
-3-element PointVector{fmpq}:
+julia> optimal_solution(milp)
+2-element PointVector{fmpq}:
+ 1
+ 3//2
+
+julia> milp = MixedIntegerLinearProgram(c, [1,1])
+A mixed integer linear program
+
+julia> optimal_solution(milp)
+2-element PointVector{fmpq}:
  1
  1
- -1
 ```
 """
-function optimal_vertex(lp::LinearProgram{T}) where T<:scalar_types
+function optimal_solution(milp::MixedIntegerLinearProgram{T}) where T<:scalar_types
    opt_vert = nothing
-   if lp.convention == :max
-      opt_vert = lp.polymake_lp.MAXIMAL_VERTEX
+   if milp.convention == :max
+      opt_vert = milp.polymake_milp.MAXIMAL_SOLUTION
    else
-      opt_vert = lp.polymake_lp.MINIMAL_VERTEX
+      opt_vert = milp.polymake_milp.MINIMAL_SOLUTION
    end
    if opt_vert != nothing
       return PointVector{T}(dehomogenize(opt_vert))
@@ -147,50 +165,50 @@ function optimal_vertex(lp::LinearProgram{T}) where T<:scalar_types
 end
 
 
-"""
-    optimal_value(LP::LinearProgram)
+@doc Markdown.doc"""
+    optimal_value(MILP::MixedIntegerLinearProgram)
 
-Return, if it exists, the optimal value of the objective function of `LP` over the feasible region
-of `LP`. Otherwise, return `-inf` or `inf` depending on convention.
+Return, if it exists, the optimal value of the objective function of `MILP`
+over the feasible region of `MILP`. Otherwise, return `-inf` or `inf` depending
+on convention.
 
 # Examples
-The following example constructs a linear program over the three dimensional cube, and
-obtains the minimal value of the function (x,y,z) ↦ x+2y-3z over that cube.
+Take the square $[-1/2,3/2]^2$ and optimize $[1,1]$ in different settings.
 ```jldoctest
-julia> C=cube(3)
-A polyhedron in ambient dimension 3
+julia> c = cube(2, -1//2, 3//2)
+A polyhedron in ambient dimension 2
 
-julia> LP=LinearProgram(C,[1,2,-3]; convention = :min)
-The linear program
-   min{c⋅x + k | x ∈ P}
-where P is a Polyhedron{fmpq} and
-   c=Polymake.Rational[1 2 -3]
-   k=0
+julia> milp = MixedIntegerLinearProgram(c, [1,1], integer_variables=[1])
+A mixed integer linear program
 
-julia> optimal_value(LP)
--6
+julia> optimal_value(milp)
+5/2
+
+julia> milp = MixedIntegerLinearProgram(c, [1,1])
+A mixed integer linear program
+
+julia> optimal_value(milp)
+2
 ```
 """
-function optimal_value(lp::LinearProgram{T}) where T<:scalar_types
-   if lp.convention == :max
+function optimal_value(milp::MixedIntegerLinearProgram{T}) where T<:scalar_types
+   if milp.convention == :max
       # TODO: consider inf
-      # return convert(T, lp.polymake_lp.MAXIMAL_VALUE)
-      return lp.polymake_lp.MAXIMAL_VALUE
+      return milp.polymake_milp.MAXIMAL_VALUE
    else
-      # return convert(T, lp.polymake_lp.MINIMAL_VALUE)
-      return lp.polymake_lp.MINIMAL_VALUE
+      return milp.polymake_milp.MINIMAL_VALUE
    end
 end
 
 
 """
-    solve_lp(LP::LinearProgram)
+    solve_milp(MILP::MixedIntegerLinearProgram)
 
 Return a pair `(m,v)` where the optimal value `m` of the objective
- function of `LP` is attained at `v` (if `m` exists). If the optimum
+ function of `MILP` is attained at `v` (if `m` exists). If the optimum
  is not attained, `m` may be `inf` or `-inf` in which case `v` is
  `nothing`.
 """
-function solve_lp(lp::LinearProgram)
-   return optimal_value(lp),optimal_vertex(lp)
+function solve_milp(milp::MixedIntegerLinearProgram)
+   return optimal_value(milp),optimal_solution(milp)
 end
