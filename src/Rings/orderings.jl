b07ab5738fc64cb19ee80280be3c0a4ee44bd10f
dan <tthsqe12@gmail.com>
2022-10-21 17:41:24 +0200

    better oppposite of deglex

--- src/Rings/orderings.jl
+++ src/Rings/orderings.jl
@@ -1671,9 +1671,15 @@ end
 
 ############ opposite ordering ############
 
-# TODO return singular-friendly orderings if that is how they come in
+# TODO return more singular-friendly orderings if that is how they come in
 function _opposite_ordering(nvars::Int, o::SymbOrdering{T}) where T
   return SymbOrdering(T, nvars+1 .- o.vars)
+end
+
+function _opposite_ordering(nvars::Int, o::SymbOrdering{:deglex})
+  n = length(o.vars)
+  return MatrixOrdering(o.vars, fmpz_mat(1, n, ones(Int, n)), false)*
+         SymbOrdering(:revlex, o.vars)
 end
 
 # TODO ditto
