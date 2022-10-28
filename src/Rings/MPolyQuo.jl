97e8ecbb15740d53c11994fafa3f838e502a3e4e
anne <anne@krueger-berg.de>
2022-10-28 09:50:06 +0200

    make modulus agnostic to MPAnyNonQuoRing

--- src/Rings/MPolyQuo.jl
+++ src/Rings/MPolyQuo.jl
@@ -35,6 +35,8 @@ Base.getindex(Q::MPolyQuo, i::Int) = Q(Q.R[i])
 base_ring(W::MPolyQuo) = W.R
 coefficient_ring(W::MPolyQuo) = coefficient_ring(base_ring(W))
 modulus(W::MPolyQuo) = W.I
+# for convenience of the scripting user make modulus agnostic
+modulus(W::MPAnyNonQuoRing) = ideal(W,[zero(W)])
 
 default_ordering(Q::MPolyQuo) = default_ordering(base_ring(Q))
 
--- src/Rings/mpolyquo-localizations.jl
+++ src/Rings/mpolyquo-localizations.jl
@@ -162,10 +162,6 @@ function modulus(L::MPolyQuoLocalizedRing)
   return get_attribute(L, :modulus)::ideal_type(localized_ring_type(L))
 end
 
-### for compatibility -- also provide modulus in the trivial case
-modulus(R::MPolyLocalizedRing)=ideal(R,[zero(R)])
-
-
 @Markdown.doc """
     quotient_ring(L::MPolyQuoLocalizedRing)
 
