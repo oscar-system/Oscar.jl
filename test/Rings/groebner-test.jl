3bbdc51a6f479aa0b49f34aa23b2fe0058213cbd
ederc <ederc@mathematik.uni-kl.de>
2022-10-28 08:58:22 +0200

    Standard basis fixes (#1655)
    * rename std_basis to standard_basis
    
    * adds ordering as kwarg for standard_basis calls conforming with GB conventions
    
    * improves docu with nicer calls to monomial orderings
    
    * fixes kwarg for standard_basis in tests
    
    * fixes intermediate version of normal_form
    
    * fixes module standard_basis calls for kwargs
    
    * fixes groebner_basis for SubQuo to allow kwargs
    
    * fixes internal compute_standard_basis function

--- test/Rings/groebner-test.jl
+++ test/Rings/groebner-test.jl
@@ -35,7 +35,7 @@ end
    M = matrix_ordering([x, y, z], [1 1 1; 0 1 0; 1 0 0])
    @test gens(groebner_basis(I, ordering = M)) == [ x + y + z, 2*x^2 + 2*x*z + z^3 + z^2 ]
    @test_throws ErrorException groebner_basis(I, ordering = negdeglex([x, y, z]))
-   @test gens(std_basis(I, negdeglex([x, y, z]))) == [ x + y + z, 2*y^2 + 2*y*z + z^3 + z^2 ]
+   @test gens(standard_basis(I, ordering=negdeglex([x, y, z]))) == [ x + y + z, 2*y^2 + 2*y*z + z^3 + z^2 ]
 
    @test groebner_basis_with_transformation_matrix(I, ordering=lex([x])*lex([y,z])) == groebner_basis_with_transformation_matrix(I, ordering=lex([x, y, z]))
    @test groebner_basis_with_transformation_matrix(I, ordering=lex([z])*lex([y])*lex([x])) == groebner_basis_with_transformation_matrix(I, ordering=revlex([x, y, z]))
@@ -48,7 +48,7 @@ end
   R, (x, y) = QQ["x", "y"]
   I = ideal(R, [x^2*(x-1)-y^2, y^3*(x+y-6)])
   o = negdegrevlex(gens(R))
-  G = std_basis(I, o)
+  G = standard_basis(I, ordering=o)
   @test normal_form(x^5-5, I, o) == -5
   u = negdegrevlex([x])*negdegrevlex([y])
   @test ideal_membership(x^4, I, ordering=u)
