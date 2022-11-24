e4559640d6ec1492e3800141aa9af690cae26cf7
Matthias Zach <zach@math.uni-hannover.de>
2022-11-24 20:06:51 +0100

    Add some tests.

--- test/Schemes/CoveredProjectiveSchemes.jl
+++ test/Schemes/CoveredProjectiveSchemes.jl
@@ -12,3 +12,23 @@
   @test domain(p) === Bcov
   @test codomain(p) === covered_scheme(P)
 end
+
+@testset "blowups II" begin
+  R, (x,y,z) = QQ["x", "y", "z"]
+  A3 = Spec(R)
+  M = R[x y z; y-1 z-2 x-3]
+  I = ideal(R, minors(M, 2))
+  BlA3 = blow_up(A3, I)
+  p = covered_projection_to_base(BlA3)
+  @test domain(p) === covered_scheme(BlA3)
+  @test codomain(p)[1][1] === A3
+  @test is_smooth(covered_scheme(BlA3))
+
+  J = ideal(R, [x,y,z])
+  Blo = blow_up(A3, J)
+  p2 = covered_projection_to_base(Blo)
+  @test domain(p2) === covered_scheme(Blo)
+  @test codomain(p2)[1][1] === A3
+  @test is_smooth(covered_scheme(Blo))
+end
+
