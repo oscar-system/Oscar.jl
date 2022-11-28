8efd6fbf049f9475a33aaa78acad953771fa46cf
Benjamin Lorenz <benlorenz@users.noreply.github.com>
2022-11-28 12:37:16 +0100

    tests: fix global state for doctests (#1769)
    * tests: reset abelian_closure variable name after printing test
    
    * tests: StraightLinePrograms put tests in submodule to avoid polluting main namespace too much

--- test/StraightLinePrograms/runtests.jl
+++ test/StraightLinePrograms/runtests.jl
@@ -1,4 +1,12 @@
+# avoid polluting the main namespace to to preserve proper printing in doctests
+module SLPTest
+using ..Test
+using ..Oscar
+using ..Oscar: SLP
+
 include("setup.jl")
 include("straightline.jl")
 include("gap.jl")
 include("atlas.jl")
+
+end
