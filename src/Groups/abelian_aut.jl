88cbba1b5e2644874ba0dce85c8cfa06964eaff1
StevellM <muller@math.uni-sb.de>
2022-12-05 11:43:13 +0100

    orthogonal group non semi-regular split

--- src/Groups/abelian_aut.jl
+++ src/Groups/abelian_aut.jl
@@ -7,8 +7,17 @@ AutGrpAbTorElem = Union{AutomorphismGroupElem{GrpAbFinGen},AutomorphismGroupElem
 AbTorElem = Union{GrpAbFinGenElem,TorQuadModElem}
 
 function _isomorphic_gap_group(A::GrpAbFinGen; T=PcGroup)
-  iso = isomorphism(T, A)
-  return codomain(iso), iso, inv(iso)
+  if is_trivial(A)
+    A2 = abelian_group(T, [1])
+    Atop = isomorphism(PermGroup, A)
+    A2top = isomorphism(PermGroup, A2)
+    iso = compose(Atop, inv(A2top))
+    iso2 = compose(A2top, inv(Atop))
+  else
+    iso = isomorphism(T, A)
+    iso2 = inv(iso)
+  end
+  return codomain(iso), iso, iso2
 end
 
 """
@@ -215,11 +224,16 @@ end
 Return the full orthogonal group of this torsion quadratic module.
 """
 @attr AutomorphismGroup function orthogonal_group(T::TorQuadMod)
-  if is_degenerate(T)
+  # if T is not semi-regular, we want to be able to split its radical
+  # quadratic. If it is not the case, we return an error since we don't
+  # support this particular case.
+  if !is_semi_regular(T)
     i = radical_quadratic(T)[2]
     has_complement(i)[1] || error("The radical quadratic must split")
-    return _orthogonal_group_split_degenerate
+    return _orthogonal_group_split_degenerate(T)
   end
+  # if T is semi-regular, it is isometric to its normal form for which
+  # we know how to compute the isometries.
   N, i = normal_form(T)
   j = inv(i)
   gensOT = _compute_gens(N)
@@ -229,7 +243,26 @@ Return the full orthogonal group of this torsion quadratic module.
 end
 
 function _orthogonal_group_split_degenerate(T::TorQuadMod)
+  # we need a splitting T = rd + N, where rd is the radical
+  # quadratic and N is a normal form.
+  #
+  # If it is the case, since T splits as the sum of its primary parts,
+  # then each primary part splits its radical quadratic too.
+  # So, we split T into primary parts, since they don't "talk"
+  # to each others via isometries, we compute the orthogonal group
+  # of each primary part and we then "glue" the orthogonal groups
+  # using diagonal matrices on an isometric module to T which is nice enough.
   @assert has_complement(radical_quadratic(T)[2])[1]
+
+  # if T is "primary degenerate" then we can send it to the other function since
+  # T has only one primary part.
+  if is_prime_power_with_data(elementary_divisors(T)[end])[1]
+    return _orthogonal_group_split_degenerate_primary(T)
+  end
+
+  # we first create some blocks corresponding to the primary parts of T.
+  # We then compute the orthogonal sum of those blocks, which is a nice
+  # TorQuadMod isometric to T: we get then an isometry between those two modules.
   gensOT = fmpz_mat[]
   pd = sort(prime_divisors(order(T)))
   blocks = TorQuadModMor[primary_part(T, pd[1])[2]]
@@ -240,7 +273,86 @@ function _orthogonal_group_split_degenerate(T::TorQuadMod)
     @assert ok
     _T = domain(j)
     _f = primary_part(_T, pd[1])[2]
-    push!(blocks, compose(j, _f))
+    push!(blocks, compose(_f, j))
     popfirst!(pd)
   end
+  Torth, _, _ = Hecke._orthogonal_sum_with_injections_and_projections(domain.(blocks))
+  ok, phi = is_isometric_with_isometry(Torth, T)
+  @assert ok # Same module with different basis, somehow
+
+  # We compute the orthoognal group of each (primary) block. Since Torth is made
+  # diagonally, we can generate its orthogonal group by taking some diagonal matrices
+  # from taking the cartesian product on the generators of the blocks. We have nothing
+  # to add since we can't map different primary parts to each others.
+  orth_blocks = _orthogonal_group_split_degenerate_elementary.(domain.(blocks))
+  gensOTorth = fmpz_mat[]
+  for x in Hecke.cartesian_product_iterator(gens.(orth_blocks), inplace=false)
+    m = block_diagonal_matrix([matrix(f) for f in x])
+    push!(gensOTorth, m)
+  end
+
+  gensOTorth = TorQuadModMor[hom(Torth, Torth, g) for g in gensOTorth]
+  gensOT = fmpz_mat[compose(compose(inv(phi), g), phi).map_ab.map for g in gensOTorth]
+  return _orthogonal_group(T, gensOT, check=false)
 end
+
+function _orthogonal_group_split_degenerate_primary(T::TorQuadMod)
+  # we want only "primary" non semi regular modules that splits their
+  # radical quadratic
+  if is_semi_regular(T)
+    return orthogonal_group(T)
+  end
+  @assert is_prime_power_with_data(elementary_divisors(T)[end])[1]
+  rd, i = radical_quadratic(T)
+  ok, j = has_complement(i)
+  @assert ok
+  
+  # N now is isometric to a the normal form of T, so it is in particular
+  # semi-regular. We can already compute generators of the generators of
+  # its normal form, we then bring them back to N
+  N = domain(j)
+  @assert is_semi_regular(N)
+  NN, NtoNN = normal_form(N)
+  @assert is_bijective(NtoNN)
+  NNtoN = inv(NtoNN)
+  gensONN = TorQuadModMor[hom(NN, NN, m) for m in _compute_gens(NN)]
+  gensON = fmpz_mat[compose(compose(NtoNN, g), NNtoN).map_ab.map for g in gensONN]
+  
+  # for the rd, since its quadratic form is trivial, automorphism are just
+  # automorphism of the underlying abelian group.
+  gensOrd = fmpz_mat[]
+  OArd = automorphism_group(abelian_group(rd))
+  for f in gens(OArd)
+    push!(gensOrd, matrix(f))
+  end
+
+  # finally, we have to consider automorphism which maps N into rd: these are well
+  # defined because N and rd are orthogonal and the quadratic form on rd is trivial.
+  R, psi = hom(abelian_group(N), abelian_group(rd), task = :map)
+  Ntord = fmpz_mat[matrix(psi(f)) for f in gens(R)]
+  Torth, _, _ = Hecke._orthogonal_sum_with_injections_and_projections([rd, N])
+
+  # Same module with different basis
+  ok, phi = is_isometric_with_isometry(Torth, T)
+  @assert ok
+
+  # We have two kind of generators: block diagonal matrices coming from isometries
+  # of N and rd, and those acts identitcally on N and rd but which send N to rd.
+  # Combining all of them together, we have generators (maybe the set is too big
+  # compared to what is needed) for the orthogonal group of Torth, and so of T.
+  gensOTorth = fmpz_mat[]
+  for x in Hecke.cartesian_product_iterator([gensOrd, gensON], inplace=false)
+    m = block_diagonal_matrix(x)
+    push!(gensOTorth, m)
+  end
+  for m in Ntord
+    n1, n2 = size(m)
+    M = identity_matrix(ZZ, n1+n2)
+    M[(n2+1):end, 1:n2] = m
+    push!(gensOTorth, M)
+  end
+  gensOTorth = TorQuadModMor[hom(Torth, Torth, m) for m in gensOTorth]
+  gensOT = fmpz_mat[compose(compose(inv(phi), g), phi).map_ab.map for g in gensOTorth]
+  return _orthogonal_group(T, gensOT, check=false)
+end
+
