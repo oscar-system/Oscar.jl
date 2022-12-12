070108ea7b46bce7aad4c4eb5e8d82792cdf52e5
HechtiDerLachs <zach@mathematik.uni-kl.de>
2022-12-12 19:03:04 +0100

    Add Cartier divisors.

---
+++ experimental/Schemes/CartierDivisor.jl
@@ -0,0 +1,30 @@
+export CartierDivisor
+
+@attributes mutable struct CartierDivisor{
+                                          CoveredSchemeType<:AbsCoveredScheme
+                                         }
+    X::CoveredSchemeType
+    I::IdealSheaf
+
+    function CartierDivisor(
+            X::AbsCoveredScheme, 
+            D::IdDict{<:AbsSpec, <:RingElem};
+            check::Bool=true
+        )
+        all(x->(x in keys(D)), affine_charts(X)) || error("equations must be given on all affine charts of the variety")
+        ID = IdDict{AbsSpec, Ideal}()
+        for U in keys(D)
+            ID[U] = ideal(OO(U), D[U])
+        end
+        I = IdealSheaf(X, ID)
+        if check
+            # TODO: Check the cocycle condition
+        end
+        return new{typeof(X)}(X, I)
+    end
+end
+
+function (C::CartierDivisor)(U::AbsSpec)
+    return gens(C.I(U))[1]
+end
+
