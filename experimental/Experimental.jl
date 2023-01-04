112ed407f9ba4cafd628490b01fc9fbf55781871
Matthias Zach <85350711+HechtiDerLachs@users.noreply.github.com>
2023-01-04 15:38:07 +0100

    Improve coverings, round I (#1846)
    * Introduce SimplifiedSpec.
    
    * Introduce LazyGlueings.
    
    * Introduce flattening of open subschemes.
    
    * Small bugfix in localizations.
    
    Co-authored-by: HechtiDerLachs <zach@mathematik.uni-kl.de>

--- experimental/Experimental.jl
+++ experimental/Experimental.jl
@@ -20,4 +20,6 @@ include("Schemes/WeilDivisor.jl")
 include("Schemes/CoveredProjectiveSchemes.jl")
 
 include("Matrix/matrix.jl")
+include("Schemes/SimplifiedSpec.jl")
 include("Schemes/CoherentSheaves.jl")
+include("Schemes/LazyGlueing.jl")
