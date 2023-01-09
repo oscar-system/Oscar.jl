d744073a73392aa43f6844d79f9f070122e114db
Wolfram Decker <decker@mathematik.uni-kl.de>
2023-01-09 15:25:19 +0100

    more homological algebra functions

--- docs/src/CommutativeAlgebra/ModulesOverMultivariateRings/homological_algebra.md
+++ docs/src/CommutativeAlgebra/ModulesOverMultivariateRings/homological_algebra.md
@@ -73,43 +73,43 @@ tensor_product(G::ModuleFP...; task::Symbol = :none)
 tor(M::ModuleFP, N::ModuleFP, i::Int)
 ```
 
-## Flatness
+## Fitting Ideals
 
-Fitting ideals provide one way of characterizing flatness.  The functions `is_flat` and
-`non_flat_locus`  discussed below are based on this characterization.
-
-```@julia
+```@docs
 fitting_ideal(M::ModuleFP, i::Int)
 ```
 
-Return the `i`-th Fitting ideal of `M`.
+## Flatness
 
-```@julia
+Checking flatness in OSCAR relies on characterizing flatness in terms of Fitting ideals.
+
+```@docs
 is_flat(M::ModuleFP)
 ```
 
-Return `true` if `M` is flat, `false` otherwise.
-
-```@julia
+```@docs
 non_flat_locus(M::ModuleFP)
+```
+
+## Regular Sequence Test
+
+```@docs
+is_regular_sequence(V::Vector{T}, M::ModuleFP{T}) where T <: MPolyElem
+```
+
+## Koszul Homology
+
+```@docs
+koszul_homology(V::Vector{T}, M::ModuleFP{T}, p::Int)  where T <: MPolyElem
 ```
 
 ## Depth
 
-The function `depth` discussed below relies on the computation of Koszul cohomology.
+The computation of depth in OSCAR relies on expressing depth in terms of  Koszul cohomology. 
 
-```@julia
-koszul_homology(V::Vector, M:ModuleFP, i::Int)
+```@docs
+depth(I::MPolyIdeal{T}, M::ModuleFP{T})  where T <: MPolyElem
 ```
-
-```@julia
-depth(I::Ideal, M::ModuleFP)
-```
-
-Reurn the depth of `I` on `M`.
-
-
-
 
 
 
---
+++ test/Modules/homological-algebra_test.jl
@@ -0,0 +1,55 @@
+@testset "mpoly_affine_homological-algebra.fitting_ideal"
+begin
+ R, (x, y) = PolynomialRing(QQ, ["x", "y"]);
+ F = free_module(R, 3)
+ I = ideal(R, [zero(R)])
+ @test fitting_ideal(F, 2) == I
+ @test fitting_ideal(F, 3) == R
+end
+
+@testset "mpoly_affine_homological-algebra.is_flat"
+begin
+ R, (x, y) = PolynomialRing(QQ, ["x", "y"]);
+ F = free_module(R, 3)
+ @test is_flat(F) == true
+end
+
+@testset "mpoly_affine_homological-algebra.non_flat_locus"
+begin
+ R, (x, y) = PolynomialRing(QQ, ["x", "y"]);
+ F = free_module(R, 3)
+ @test non_flat_locus(F) == ideal(R, [one(R)])
+end
+
+@testset "mpoly_affine_homological-algebra.is_regular_sequence"
+begin
+ R, (x, y, z) = PolynomialRing(QQ, ["x", "y", "z"]);
+ F = free_module(R, 1)
+ U = matrix([x*y-y])
+ M = quo(F, U)[1]
+ V =  [x, x*z-z]
+ @test is_regular_sequence(V, M) == true
+ W = [x*z-z, x]
+ @test is_regular_sequence(W, M) == false
+end
+
+@testset "mpoly_affine_homological-algebra.koszul_homology"
+begin
+ R, (x, y, z) = PolynomialRing(QQ, ["x", "y", "z"]);
+ F = free_module(R, 1)
+ U = matrix([x*y-y])
+ M = quo(F, U)[1]
+ V =  [x, x*z-z]
+ @test is_zero(koszul_homology(V, M, 0)) == false
+ @test is_zero(koszul_homology(V, M, 1)) == true
+end
+
+@testset "mpoly_affine_homological-algebra.depth"
+begin
+ R, (x, y) = PolynomialRing(QQ, ["x", "y"]);
+ F = free_module(R, 1)
+ U = matrix([x*y])
+ M = quo(F, U)[1]
+ I = ideal(R, gens(R))
+ @test depth(I, M) == 1
+end
