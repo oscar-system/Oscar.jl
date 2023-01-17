010342ee3890ea0a9ccea14acfaa58bf46435bea
HechtiDerLachs <zach@mathematik.uni-kl.de>
2023-01-17 15:15:48 +0100

    Introduce morphism_of_covered_schemes for projective morphisms.

--- test/Schemes/CoveredScheme.jl
+++ test/Schemes/CoveredScheme.jl
@@ -174,3 +174,27 @@ end
   Z2cov = covered_scheme(Z2)
   @test is_integral(Z2cov)
 end
+
+@testset "conversion of morphisms" begin
+  P = projective_space(QQ, 2)
+  SP = ambient_coordinate_ring(P)
+  (x, y, z) = gens(SP)
+  m = ideal(SP, gens(SP))
+  m3 = m^3
+  n = ngens(m3)
+  Q = projective_space(QQ, n-1)
+  SQ = ambient_coordinate_ring(Q)
+  phi = hom(SQ, SP, gens(m3))
+  f = ProjectiveSchemeMor(P, Q, phi)
+  f_cov = covered_scheme_morphism(f)
+  @test domain(f_cov) === covered_scheme(P)
+  @test codomain(f_cov) === covered_scheme(Q)
+
+  A = [1 2 4; 1 3 9; 1 5 25]
+  v = A*[x, y, z]
+  psi = hom(SP, SP, v)
+  g = ProjectiveSchemeMor(P, P, psi)
+  g_cov = covered_scheme_morphism(g)
+
+  @test domain(g_cov) === codomain(g_cov) === covered_scheme(P)
+end
