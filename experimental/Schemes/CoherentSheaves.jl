0dbbc1df19c73024916084550c0505c599991ef1
HechtiDerLachs <zach@mathematik.uni-kl.de>
2023-01-26 20:06:50 +0100

    Introduce PullbackSheaf, DirectSumSheaf, and projectivization.
    These are two kinds of sheaves which, in particular, require
    a default_covering of sheaves of modules.
    
    The projectivization allows to create to a vector bundle
    its associated projective bundle.

--- experimental/Schemes/CoherentSheaves.jl
+++ experimental/Schemes/CoherentSheaves.jl
@@ -237,9 +237,7 @@ identifications given by the glueings in the `default_covering`.
       #
       # See whether we have anything cached for U
       haskey(MD, U) && return MD[U]
-
-      # If not, we are in case 3) above.
-      error("production of modules not implemented in this case")
+      error("module on $U was not found")
     end
 
     function production_func(
