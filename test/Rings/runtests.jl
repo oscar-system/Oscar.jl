5188e02ad4681e7e3909f171202884fd5004bafe
ederc <ederc@mathematik.uni-kl.de>
2023-01-27 10:42:07 +0100

    resets rings testsuite

--- test/Rings/runtests.jl
+++ test/Rings/runtests.jl
@@ -1,15 +1,15 @@
-#= include("integer-test.jl")
+include("integer-test.jl")
 include("rational-test.jl")
 include("mpoly-test.jl")
-include("orderings-test.jl") =#
-#= include("affine-alghom-test.jl")
+include("orderings-test.jl")
+include("affine-alghom-test.jl")
 include("mpoly-graded-test.jl")
 include("mpoly-local-test.jl")
 include("mpoly-localizations.jl")
 include("mpolyquo-localizations.jl")
 include("integer-localizations.jl")
-include("nmod-localizations.jl") =#
-#= include("mpoly-nested-test.jl") =#
+include("nmod-localizations.jl")
+include("mpoly-nested-test.jl")
 include("MPolyQuo_test.jl")
 include("groebner-test.jl")
 include("solving-test.jl")
