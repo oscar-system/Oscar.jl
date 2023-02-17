21ed8193108bbb05d4ca33082c8ddf4a4fac0678
John Abbott <John.Abbott@rptu.de>
2023-02-17 11:11:53 +0100

    ExteriorAlgebra-test.jl now listed in runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -43,6 +43,7 @@ include("Experimental/ModStdQt-test.jl")
 include("Experimental/ModStdNF-test.jl")
 include("Experimental/MPolyRingSparse-test.jl")
 include("Experimental/MatrixGroups-test.jl")
+include("Experimental/ExteriorAlgebra-test.jl")
 
 include("Geometry/K3Auto.jl")
 
