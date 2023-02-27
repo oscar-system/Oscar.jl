58a72146b24a806f2aa0d2dcaa5aa69be22fd730
StevellM <muller@math.uni-sb.de>
2023-02-27 16:43:38 +0100

    more on hmm

--- src/NumberTheory/LatWithIsom/HMM.jl
+++ src/NumberTheory/LatWithIsom/HMM.jl
@@ -9,9 +9,25 @@ function _get_quotient_split(P::Hecke.NfRelOrdIdl, i::Int)
   URPabs, mURPabs = unit_group(RPabs)
 
   function dlog(x::Hecke.NfRelElem)
-    @assert parent(x) === E
-    @assert is_integral(x)
-    return muRPabs\(mRPabs(OEabs(EabstoE\x)))
+    @assert parent(x) == E
+    d = denominator(x)
+    xabs = OEabs(d*EabstoE\(x))
+    dabs = OEabs(d)
+    F = prime_decomposition(OEabs, EabstoE\(minimum(P)))
+    for PP in F
+      @assert valuation(EasbtoE\(x), PP[1]) >= 0
+      api = OEabs(anti_uniformizer(PP[1]))
+      exp = valuation(OEabs(d), PP[1])
+      dabs *= api^exp
+      xabs *= api^exp
+    end
+
+    xabs_image = mURPabs\(mRPabs(xabs))
+    dabs_image = mURPabs\(mRPabs(dabs))
+
+    ret = xabs_image - dabs_image
+
+    return ret
   end
 
   function exp(k::GrpAbFinGenElem)
@@ -52,8 +68,24 @@ function _get_quotient_inert(P::Hecke.NfRelOrdIdl, i::Int)
 
   function dlog(x::Hecke.NfRelElem)
     @assert parent(x) === E
-    @assert is_integral(x)
-    return mS\(mK\(mURPabs\(mRPabs(OEabs(EabstoE\x)))))
+    d = denominator(x)
+    xabs = OEabs(EabstoE\(d*x))
+    dabs = OEabs(d)
+    F = prime_decomposition(OEabs, EabstoE\(minimum(P)))
+    for PP in F
+      @assert valuation(EabstoE\(x), PP[1]) >= 0
+      api = OEabs(anti_uniformizer(PP[1]))
+      exp = valuation(OEabs(d), PP[1])
+      dabs *= api^exp
+      xabs *= api^exp
+    end
+
+    xabs_image = mURPabs\(mRPabs(xabs))
+    dabs_image = mURPabs\(mRPabs(dabs))
+
+    ret = mS\(mK\(xabs_image - dabs_image))
+
+    return ret
   end
 
   return S, exp, dlog
@@ -107,18 +139,34 @@ function _get_quotient_ramified(P::Hecke.NfRelOrdIdl, i::Int)
 
   function dlog(x::Hecke.NfRelElem)
     @assert parent(x) === E
-    @assert is_integral(x)
-    return mS\(mK\(mURPabs\(mRPabs(OEabs(EabstoE\x)))))
+    d = denomiator(x)
+    xabs = OEabs(EasbtoE\(d*x))
+    dabs = OEabs(d)
+    F = prime_decomposition(OEabs, EabstoE\(minimum(P)))
+    for PP in F
+      @assert valuation(EabstoE\(x), PP[1]) >= 0
+      api = OEabs(anti_uniformizer(PP[1]))
+      exp = valuation(OEabs(d), PP[1])
+      dabs *= api^exp
+      xabs *= api^exp
+    end
+
+    xabs_image = mURPabs\(mRPabs(xabs))
+    dabs_image = mURPabs\(mRPabs(dabs))
+
+    ret = mS\(mK\ (xabs_image - dabs_image))
+    return ret
   end
 
   return S, exp, dlog
 end
 
-function _get_quotient(P::Hecke.NfRelOrdIdl, i::Int)
-  @assert is_prime(P)
-  @assert is_maximal(order(P))
-  OE = order(P)
-  F = prime_decomposition(OE, minimum(P))
+function _get_quotient(O::Hecke.NfRelOrd, p::Hecke.NfOrdIdl, i::Int)
+  @assert is_prime(p)
+  @assert is_maximal(order(p))
+  @assert order(p) === base_ring(O)
+  F = prime_decomposition(O, p)
+  P = F[1][1]
   if length(F) == 2
     S, dexp, dlog = _get_quotient_split(P, i)
   elseif F[1][2] == 1
@@ -126,8 +174,77 @@ function _get_quotient(P::Hecke.NfRelOrdIdl, i::Int)
   else
     S, dexp, dlog = _get_quotient_ramified(P, i)
   end
-  return S, dexp, dlog
+  return S, dexp, dlog, P
 end
 
-#function _get_big_quotient(E::)
+function _get_big_quotient(E::Hecke.NfRel, Fac::Vector{Tuple{Hecke.NfOrdIdl, Int}})
+  OE = maximal_order(e)
+  groups = []
+  exps = []
+  dlogs = []
+  Ps = []
+
+  if length(Fac) == 0
+    A = abelian_group()
+    function dlog(x::Hecke.NfRelElem); return one(A); end;
+    function exp(x::GrpAbFinGenElem); return one(E); end;
+    return A, dlog, exp
+  end
+
+  for i in 1:length(Fac)
+    p, e = Fac[i]
+    KK, f, g, P = _get_quotient(OE, p, e)
+    push!(groups, KK)
+    push!(exps, f)
+    push!(dlogs, g)
+    push!(Ps, P)
+  end
+
+  if length(groups) == 1
+    return groups[1], dlogs[1], exps[1]
+  end
+
+  G, inj, proj = biproduct(groups)
+
+  function dlog(x::Hecke.NfRelElem)
+    return sum([inj[i](dlogs[i](x)) for i in 1:length(Fac)])
+  end
+
+  function exp(x::GrpAbFinGenElem)
+    v = Hecke.NfRelOrdElem[OE(exps[i](proj[i](x))) for i in 1:length(Fac)]
+    a = crt(v, [Ps[i]^(3*Fac[i][2]) for i in 1:length(Ps)])
+    @assert dlog(a) == x
+    return a
+  end
+
+  return G, dlog, exp
+end
+
+function _is_special(L::Hecke.HermLat, p::Hecke.NfOrdIdl)
+  OE = base_ring(L)
+  E = nf(OE)
+  lp = prime_decomposition(OE, p)
+  if lp[1][2] != 2 || !is_even(rank(L))
+    return false
+  end
+  _, _R, S = jordan_decomposition(L, p)
+  R = [nrows(m) for m in _R]
+  for r in R
+    if r != 2
+      return false
+    end
+  end
+  P = lp[1][1]
+  e = valuation(different(OE), P)
+  for i in 1:length(S)
+    if !iseven(e- S[i])
+      return false
+    end
+  end
+  u = uniformizer(P)
+  s = involution(L)
+  su = s(u)
+  H = block_diagonal_matrix([matrix(E, 2, 2, [0 u^(S[i]); su^(S[i]) 0]) for i in 1:length(S)])
+  return is_locally_isometric(L, hermitian_lattice(E, gram = H), p)
+end
 
