96e6735e51e0d777cbbb4374fd4acae52833c121
Anne Frühbis-Krüger <77079696+afkafkafk13@users.noreply.github.com>
2023-03-04 08:58:31 +0100

    Update src/AlgebraicGeometry/Schemes/AffineSchemes/Objects/Properties.jl
    Co-authored-by: Max Horn <max@quendi.de>

--- src/AlgebraicGeometry/Schemes/AffineSchemes/Objects/Properties.jl
+++ src/AlgebraicGeometry/Schemes/AffineSchemes/Objects/Properties.jl
@@ -696,7 +696,7 @@ Return the boolean value whether an affine scheme `X` is connected.
 
 """
 @attr function is_connected(X::AbsSpec)
-  has_attribute(X,:is_connected) || error("not implemented yet")
+  error("not implemented yet")
 ## note for future implementation: expensive property
 ## 1) do primary decomposition
 ## 2) check connectedness of lowest two layers of the intersection lattice
