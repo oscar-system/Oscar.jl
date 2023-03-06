7a7fb747cd1d07bed7304a97cdbe5af8c2e0687b
ThomasBreuer <sam@math.rwth-aachen.de>
2023-03-06 13:33:29 +0100

    adjusted one more doctest ...

--- src/Groups/libraries/atlasgroups.jl
+++ src/Groups/libraries/atlasgroups.jl
@@ -23,7 +23,7 @@ julia> atlas_group("A5")  # alternating group A5
 Group([ (1,2)(3,4), (1,3,5) ])
 
 julia> atlas_group(MatrixGroup, "A5")
-Matrix group of degree 4 over Finite field of degree 1 over F_3
+Matrix group of degree 4 over Finite field of degree 1 over F_2
 
 julia> atlas_group("M11")  # Mathieu group M11
 Group([ (2,10)(4,11)(5,7)(8,9), (1,4,3,8)(2,5,6,9) ])
@@ -126,7 +126,7 @@ julia> h2, emb = atlas_subgroup("M11", 1);  h2
 Group([ (1,4)(2,10)(3,7)(6,9), (1,6,10,7,11,3,9,2)(4,5) ])
 
 julia> h3, emb = atlas_subgroup(MatrixGroup, "M11", 1 );  h3
-Matrix group of degree 10 over Galois field with characteristic 2
+Matrix group of degree 10 over Finite field of degree 1 over F_2
 
 julia> info = all_atlas_group_infos("M11", degree => 11);
 
