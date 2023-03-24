46d068dce994a0d48984d7bc996aab45e948a7fc
Simon Brandhorst <sbrandhorst@web.de>
2023-03-24 22:14:46 +0100

    pretty printing

--- src/AlgebraicGeometry/Schemes/AffineVariety/Objects/Methods.jl
+++ src/AlgebraicGeometry/Schemes/AffineVariety/Objects/Methods.jl
@@ -2,15 +2,47 @@
 # (1) Display
 ########################################################
 
+# detailed printing
+function Base.show(io::IO, ::MIME"text/plain", X::AffineVariety{<:Field,<:MPolyQuoRing})
+    println(io, "Affine variety")
+    print(io, " in ")
+    println(io, ambient_space(X))
+    println(io, "defined by")
+    print(io, modulus(OO(X)))
+end
+
+# one line printing
+function Base.show(io::IO, X::AffineVariety)
+    if get(io, :supercompact, false)
+      print(io, "Affine variety")
+    else
+      print(io, "Affine variety in ")
+      print(io, ambient_space(X))
+    end
+end
+
 function Base.show(io::IO, X::AffineVariety)
   d = dim(ambient_space(X))
   println(io, "Affine variety in $ambient_space(X)")
   println(io, "defined by")
-  println(io, modulus(OO(X)))
+  print(io, modulus(OO(X)))
+end
+
+
+# For affine space
+function Base.show(io::IO, ::MIME"text/plain", X::AffineVariety{<:Field,<:MPolyRing})
+    println(io, "Affine $(dim(X))-space")
+    print(io, " over ")
+    println(io, base_ring(X))
+    println(io, "with coordinates")
+    show(io, "text/plain", coordinates(X))
 end
 
 function Base.show(io::IO, X::AffineVariety{<:Field,<:MPolyRing})
-  d = dim(ambient_space(X))
-  print(io, "Affine $(dim(X))-space over $(base_ring(X))")
+    if get(io, :supercompact, false)
+      print(io, "Affine space")
+    else
+      print(io, "Affine $(dim(X))-space over ")
+      print(IOContext(io, :supercompact => true), base_ring(X))
+    end
 end
-
