18d5d06401dc94f05dacf692cfaba338fcefad4c
Antony <antonydellavecchia@gmail.com>
2023-03-27 15:59:06 +0200

    updates from review

--- src/PolyhedralGeometry/markov_basis.jl
+++ src/PolyhedralGeometry/markov_basis.jl
@@ -1,18 +1,21 @@
+
 @doc Markdown.doc"""
-    markov_basis(S; use_kernel=true)
-    markov_basis(P::Polyhedron{fmpq})
+    markov_basis(S::AbstractVector{<:AbstractVector{<:IntegerUnion}})
+    markov_basis(S::MatElem{<:IntegerUnion}; use_kernel::Bool=true)
+    markov_basis(S::AbstractMatrix{<:IntegerUnion}; use_kernel::Bool=true)
+    markov_basis(P::Polyhedron{QQFieldElem})
 
 
-Return a Markov basis for the lattice $L$. Where $L$ is
+Return a Markov basis for the lattice $L$, where $L$ is
 - either the integer kernel of a matrix when either `use_kernel=true` or the
   argument is of type `Polyhedron{fmpq}`,
 - otherwise $L$ is given by the span of the collection of integer vectors `S`.
 # Arguments
-- `S::AbstractVector{<:AbstractVector{U}} where U <: Union{Base.Integer, fmpz}`:
+- `S::AbstractVector{<:AbstractVector{U}} where U <: IntegerUnion`:
   Collection of integer vectors that span the columnspace of a matrix M,
   or when `use_kernel=false` collection of integer vectors that span an integer 
   lattice.
-- `S::Union{MatElem{U}, <: AbstractMatrix{U}} where U <: Union{Base.Integer, fmpz}`:
+- `S::Union{MatElem{U}, <: AbstractMatrix{U}} where U <: IntegerUnion`:
   A matrix whose integer kernel spans an integer lattice,
   or when `use_kernel=false` a matrix whose rows span an integer lattice.
 - `P::Polyhedron{fmpq}`: Polyhedron whose homogenized lattice points span the columnspace of a matrix.
@@ -39,8 +42,8 @@ ideal(x1*x4 - x2*x3)
 ```
 """
 function markov_basis(S:: Union{MatElem{U}, <: AbstractMatrix{U}};
-                      use_kernel=true) where U <: Union{Base.Integer, fmpz}
-    S = fmpz_mat(S)
+                      use_kernel::Bool=true) where U <: IntegerUnion
+    S = ZZRingElem_mat(S)
     if !use_kernel
         S = transpose(S)
     end
@@ -53,8 +56,8 @@ function markov_basis(S:: Union{MatElem{U}, <: AbstractMatrix{U}};
 end
 
 function markov_basis(S::AbstractVector{<:AbstractVector{U}};
-                      use_kernel::Bool=true) where U <: Union{Base.Integer, fmpz}
-    T = matrix(ZZ, Vector{Vector{fmpz}}(S))
+                      use_kernel::Bool=true) where U <: IntegerUnion
+    T = matrix(ZZ, Vector{Vector{ZZRingElem}}(S))
 
     if use_kernel
         T = transpose(T)
