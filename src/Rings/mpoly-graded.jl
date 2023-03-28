13a39a00c5271d6bf64d3176b611761d0516cdc0
HechtiDerLachs <zach@mathematik.uni-kl.de>
2023-03-28 11:55:32 +0200

    Add promote rule to remove ambiguity.

--- src/Rings/mpoly-graded.jl
+++ src/Rings/mpoly-graded.jl
@@ -2424,6 +2424,12 @@ function AbstractAlgebra.promote_rule(::Type{MPolyDecRingElem{S, T}}, ::Type{U})
   end
 end
 
+# For some reason the following is necessary to remove ambiguities
+function AbstractAlgebra.promote_rule(::Type{MPolyDecRingElem{S, T}}, ::Type{MPolyDecRingElem{S, T}}) where {S, T}
+  return MPolyDecRingElem{S, T}
+end
+
+
 ################################################################################
 #
 #  Random homogeneous polynomials
