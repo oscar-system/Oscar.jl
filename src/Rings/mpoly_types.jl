0d6e8ef4508def73043e43cae2aa060a9d2a876f
Max Horn <max@quendi.de>
2023-03-29 10:45:51 +0200

    Remove final mpoly_type methods
    They are now in Nemo

--- src/Rings/mpoly_types.jl
+++ src/Rings/mpoly_types.jl
@@ -1,9 +1,3 @@
-mpoly_type(::Type{QQFieldElem}) = QQMPolyRingElem
-
-mpoly_type(::Type{ZZRingElem}) = ZZMPolyRingElem
-
-mpoly_type(::Type{fpFieldElem}) = fpMPolyRingElem
-
 mpoly_dec_ring_type(A::T) where {T<:MPolyRing} = mpoly_dec_ring_type(typeof(A))
 mpoly_dec_type(A::T) where {T<:MPolyRing} = mpoly_dec_type(typeof(A))
 mpoly_dec_ring_type(::Type{T}) where {S<:RingElem, T<:MPolyRing{S}} = MPolyDecRing{S, T}
