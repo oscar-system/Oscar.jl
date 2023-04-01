85efc7dd9074aaf2a46e161f5107cafbbbcfc57e
Ulrich Thiel <thiel@mathematik.uni-kl.de>
2023-04-01 13:52:53 +0000

    Added quantum_analogs

--- experimental/JuLie.jl
+++ experimental/JuLie.jl
@@ -8,7 +8,9 @@ include("JuLie/partitions.jl")
 include("JuLie/schur_polynomials.jl")
 include("JuLie/tableaux.jl")
 
+include("JuLie/quantum_analogs.jl")
+
 end
 
 using .JuLie
-export Partition, partitions, ascending_partitions, dominates, conjugate, getindex_safe, num_partitions, schur_polynomial, Tableau, shape, semistandard_tableaux, is_standard, is_semistandard, standard_tableaux, schensted, hook_length, hook_lengths, num_standard_tableaux, reading_word, weight, bump!
+export Partition, partitions, ascending_partitions, dominates, conjugate, getindex_safe, num_partitions, schur_polynomial, Tableau, shape, semistandard_tableaux, is_standard, is_semistandard, standard_tableaux, schensted, hook_length, hook_lengths, num_standard_tableaux, reading_word, weight, bump!, quantum_integer, quantum_factorial, quantum_binomial
--- test/Experimental/JuLie-test.jl
+++ test/Experimental/JuLie-test.jl
@@ -1,3 +1,4 @@
 include("JuLie/tableaux.jl")
 include("JuLie/schur_polynomials.jl")
 include("JuLie/partitions.jl")
+include("JuLie/quantum_analogs.jl")
