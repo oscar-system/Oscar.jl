1e8721bd1d10fc2be377ccdf5b481d33c139417e
Martin Bies <MBies87@googlemail.com>
2023-04-04 10:16:37 +0200

    FTheoryTools: Use @vprint instead of @info

--- src/Oscar.jl
+++ src/Oscar.jl
@@ -249,6 +249,10 @@ function __init__()
 
     add_verbose_scope(:K3Auto)
     add_assert_scope(:K3Auto)
+    
+    add_verbose_scope(:GlobalTateModel)
+    add_verbose_scope(:GlobalWeierstrassModel)
+    
 end
 
 const PROJECT_TOML = Pkg.TOML.parsefile(joinpath(@__DIR__, "..", "Project.toml"))
