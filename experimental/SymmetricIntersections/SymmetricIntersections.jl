915aeb9de5c3ffe97ffcbeca20b6fbca8e61d22c
Max Horn <max@quendi.de>
2023-04-04 08:28:34 +0200

    Remove duplicate @req macro (#2201)

--- experimental/SymmetricIntersections/SymmetricIntersections.jl
+++ experimental/SymmetricIntersections/SymmetricIntersections.jl
@@ -1,15 +1,6 @@
 module SymInt
 using Oscar
 
-macro req(args...)
-  @assert length(args) == 2
-  quote
-    if !($(esc(args[1])))
-      throw(ArgumentError($(esc(args[2]))))
-    end
-  end
-end
-
 include("Types.jl")
 include("Elevators.jl")
 include("Representations.jl")
