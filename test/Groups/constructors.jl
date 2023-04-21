0f47ce3715eadf82b3766413c764379208c84e3d
ThomasBreuer <sam@math.rwth-aachen.de>
2023-04-21 15:53:22 +0200

    deal also with the trivial group

--- test/Groups/constructors.jl
+++ test/Groups/constructors.jl
@@ -78,14 +78,16 @@ end
 
   @test_throws ArgumentError cyclic_generator(symmetric_group(3))
 
-  for p in [next_prime(2^62), next_prime(ZZRingElem(2)^66)]
+  for p in [1, next_prime(2^62), next_prime(ZZRingElem(2)^66)]
     g = cyclic_group(p)
     @test is_cyclic(g)
     @test order(cyclic_generator(g)) == order(g)
     @test !is_dihedral_group(g)
     @test is_finite(g)
     @test order(g) == p
+  end
 
+  for p in [next_prime(2^62), next_prime(ZZRingElem(2)^66)]
     n = 2*ZZRingElem(p)
     g = dihedral_group(n)
     @test !is_cyclic(g)
