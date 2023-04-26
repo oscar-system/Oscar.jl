ec3d44162c9083b6951d47d6e1e54a024d993686
Martin Bies <MBies87@googlemail.com>
2023-04-26 14:31:43 +0200

    [FTheoryTools] Fiber analysis moved to methods

---
+++ experimental/FTheoryTools/src/TateModels/methods.jl
@@ -0,0 +1,61 @@
+#####################################################
+# 1: Fiber analysis
+#####################################################
+
+function analyze_fibers(model::GlobalTateModel, centers::Vector{<:Vector{<:Integer}})
+  # Ideal of the defining polynomial
+  hypersurface_ideal = ideal([tate_polynomial(model)])
+
+  # Toric ambient space
+  tas = toric_ambient_space(model)
+
+  # Various important ideals
+  irr = irrelevant_ideal(tas);
+  sri = stanley_reisner_ideal(tas);
+  lin = ideal_of_linear_relations(tas);
+
+  # Singular loci
+  sing_loc = singular_loci(model)
+
+  # Pick out the singular loci that are more singular than an I_1
+  # Then keep only the locus and not the extra info about it
+  interesting_singular_loci = map(tup -> tup[1], filter(locus -> locus[2][3] > 1, sing_loc))
+
+  # This is a kludge to map polynomials on the base into the ambient space, and should be fixed once the ambient space constructors supply such a map
+  base_coords = parent(gens(interesting_singular_loci[1])[1])
+  ambient_coords = parent(tate_polynomial(model))
+  base_to_ambient_ring_map = hom(base_coords, ambient_coords, gens(ambient_coords)[1:end-3])
+
+  # Resolved model
+  strict_transform, exceptionals, crepant, res_irr, res_sri, res_lin, res_S, res_S_gens, res_ring_map = _blowup_global_sequence(hypersurface_ideal, centers, irr, sri, lin)
+  if !crepant
+      @warn "The given sequence of blowups is not crepant"
+  end
+
+  loci_fiber_intersections = Tuple{MPolyIdeal{QQMPolyRingElem}, Vector{Tuple{Tuple{Int64, Int64}, Vector{MPolyIdeal{QQMPolyRingElem}}}}}[]
+  for locus in interesting_singular_loci
+      # Currently have to get the ungraded ideal generators by hand using .f
+      ungraded_locus = ideal(map(gen -> base_to_ambient_ring_map(gen).f, gens(locus)))
+
+      # Potential components of the fiber over this locus
+      # For now, we only consider the associated prime ideal,
+      #   but we may later want to actually consider the primary ideals
+      potential_components = map(pair -> pair[2], primary_decomposition(strict_transform + res_ring_map(ungraded_locus)))
+
+      # Filter out the trivial loci among the potential components
+      components = filter(component -> _is_nontrivial(component, res_irr), potential_components)
+
+      # Check the pairwise intersections of the components
+      intersections = Tuple{Tuple{Int64, Int64}, Vector{MPolyIdeal{QQMPolyRingElem}}}[]
+      for i in 1:length(components) - 1
+          for j in i + 1:length(components)
+              intersection = filter(candidate_locus -> _is_nontrivial(candidate_locus, res_irr), map(pair -> pair[2], primary_decomposition(components[i] + components[j])))
+              push!(intersections, ((i, j), intersection))
+          end
+      end
+
+      push!(loci_fiber_intersections, (ungraded_locus, intersections))
+  end
+
+  return loci_fiber_intersections
+end
