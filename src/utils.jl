8ab6e23e89c0905e6e497d40c7440a8d8077fa7c
Lars Kastner <lars.kastner@posteo.de>
2023-05-09 18:07:26 +0200

    [Oscar.jl] Move testsuite helpers to utils.jl

--- src/utils.jl
+++ src/utils.jl
@@ -1,3 +1,10 @@
+###############################################################################
+###############################################################################
+##
+##  versioninfo
+##
+###############################################################################
+###############################################################################
 # When a specific branch is loaded via `]add Package#branch` julia will only
 # create a checkout and keep a bare git repo in a separate directory.
 # In a bare repo HEAD will not point to the correct commit so we use the git
@@ -85,6 +92,14 @@ function versioninfo(io::IO=stdout; branch=false, jll=false, julia=false, commit
       println(io, Base.TAGGED_RELEASE_BANNER)
    end
 end
+
+###############################################################################
+###############################################################################
+##
+##  Documentation helpers
+##
+###############################################################################
+###############################################################################
 
 # use tempdir by default to ensure a clean manifest (and avoid modifying the project)
 function doc_init(;path=mktempdir())
@@ -178,4 +193,56 @@ $(VERSION). Running the doctests will produce errors that you do not expect."
   if doctest != false && !versioncheck
     @warn versionwarn
   end
+end
+
+###############################################################################
+###############################################################################
+##
+##  Testsuite helpers
+##
+###############################################################################
+###############################################################################
+@doc raw"""
+    test_module(file::AbstractString; new::Bool = true)
+
+Run the Oscar tests in the file `test/<file>.jl` where `file` may be a path.
+
+The optional parameter `new` takes the values `false` and `true` (default). If
+`true`, then the tests are run in a new session, otherwise the currently active
+session is used.
+
+For experimental modules, use [`test_experimental_module`](@ref) instead.
+"""
+function test_module(file::AbstractString; new::Bool=true)
+  julia_exe = Base.julia_cmd()
+  rel_test_file = normpath("test", "$file.jl")
+  test_file = joinpath(oscardir, rel_test_file)
+
+  if new
+    cmd = "using Test; using Oscar; Hecke.assertions(true); include(\"$test_file\");"
+    @info("spawning ", `$julia_exe -e \"$cmd\"`)
+    run(`$julia_exe -e $cmd`)
+  else
+    @req isdefined(Base.Main, :Test) "You need to do \"using Test\""
+    @info("Running tests for $rel_test_file in same session")
+    Base.include(Base.Main, test_file)
+  end
+end
+
+@doc raw"""
+    test_experimental_module(project::AbstractString; file::AbstractString="runtests", new::Bool = true)
+
+Run the Oscar tests in the file `experimental/<project>/test/<file>.jl`
+where `file` may be a path.
+The default is to run the entire test suite of the module `project`.
+
+The optional parameter `new` takes the values `false` and `true` (default). If
+`true`, then the tests are run in a new session, otherwise the currently active
+session is used.
+"""
+function test_experimental_module(
+  project::AbstractString; file::AbstractString="runtests", new::Bool=true
+)
+  test_file = "../experimental/$project/test/$file"
+  test_module(test_file; new)
 end
