dc52e691c30ccb5215706eba54c1e9de7fcd2cd9
Lars Kastner <lars.kastner@posteo.de>
2023-05-09 23:51:22 +0200

    [Oscar.jl] Split off experimental and some consts

--- src/utils/utils.jl
+++ src/utils/utils.jl
@@ -1,3 +1,8 @@
+const cornerstones = String["AbstractAlgebra", "GAP", "Hecke", "Nemo", "Polymake", "Singular"];
+const jll_deps = String["Antic_jll", "Arb_jll", "Calcium_jll", "FLINT_jll", "GAP_jll",
+                        "libpolymake_julia_jll", "libsingular_julia_jll",
+                        "polymake_jll", "Singular_jll"];
+
 include("versioninfo.jl")
 include("docs.jl")
 include("tests.jl")
