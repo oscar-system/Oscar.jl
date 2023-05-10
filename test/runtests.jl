989404d88364130f4941272133d6e29130b8f194
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2023-05-10 18:20:13 +0200

    Move `Aqua` tests to its own file
    to enable use of `Oscar.test_module` and the printing of used time per test in the CI.

--- test/runtests.jl
+++ test/runtests.jl
@@ -85,17 +85,7 @@ end
 # Used in both Rings/slpolys.jl and StraightLinePrograms/runtests.jl
 const SLP = Oscar.StraightLinePrograms
 
-Aqua.test_all(
-  Oscar;
-  ambiguities=false,      # TODO: fix ambiguities
-  unbound_args=false,     # TODO: fix unbound type parameters
-  undefined_exports=true,
-  project_extras=true,
-  stale_deps=false,       # some weird error with GAP_lib_jll
-  deps_compat=true,
-  project_toml_formatting=true,
-  piracy=false,           # TODO: check the reported methods to be moved upstream
-)
+include("Aqua.jl")
 
 include("printing.jl")
 
