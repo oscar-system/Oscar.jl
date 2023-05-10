ed9b488a1f0002475d82ee9ddfd53daf8617018a
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2023-05-10 16:09:12 +0200

    Add Aqua.jl for some static testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using Oscar
 using Test
+using Aqua
 using Documenter
 
 import Random
@@ -78,10 +79,24 @@ function include(str::String)
 end
 
 @static if compiletimes
-  Base.cumulative_compile_timing(true);
+  Base.cumulative_compile_timing(true)
 end
+
 # Used in both Rings/slpolys.jl and StraightLinePrograms/runtests.jl
 const SLP = Oscar.StraightLinePrograms
+
+Aqua.test_all(
+  Oscar;
+  ambiguities=false,      # TODO: fix ambiguities
+  unbound_args=false,     # TODO: fix unbound type parameters
+  undefined_exports=true,
+  project_extras=true,
+  stale_deps=false,       # some weird error with GAP_lib_jll
+  deps_compat=true,
+  project_toml_formatting=true,
+  piracy=false,           # TODO: check the reported methods to be moved upstream
+)
+
 include("printing.jl")
 
 include("PolyhedralGeometry/runtests.jl")
