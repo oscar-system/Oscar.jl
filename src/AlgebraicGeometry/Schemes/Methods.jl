19c918058c490f526624315b3fe8b5e442604a9c
Matthias Zach <zach@math.uni-hannover.de>
2023-05-15 16:42:29 +0200

    Fix docstrings.

--- src/AlgebraicGeometry/Schemes/Methods.jl
+++ src/AlgebraicGeometry/Schemes/Methods.jl
@@ -6,8 +6,7 @@ we compute ``X' = X ×ₖ Spec(R)`` and return a pair `(X', f)` where
 ``f : X' → X`` is the canonical morphism.
 
 !!! note
-We do not restrict `phi` to be a `Hecke.Map` so that one can also use 
-coercion, anonymous functions, etc. 
+    We do not restrict `phi` to be a `Hecke.Map` so that one can also use coercion, anonymous functions, etc. 
 """
 function base_change(phi::Any, X::Scheme)
   error("base_change not implemented for input of type $(typeof(X))")
@@ -25,12 +24,10 @@ where ``a : X' → X`` is the morphism from `base_change(phi, X)`,
 morphism on those fiber products.
 
 !!! note
-We do not restrict `phi` to be a `Hecke.Map` so that one can also use 
-coercion, anonymous functions, etc. 
+    We do not restrict `phi` to be a `Hecke.Map` so that one can also use coercion, anonymous functions, etc. 
 
 !!! note 
-The morphisms ``a`` and ``b`` can be passed as the optional arguments 
-`domain_map` and `codomain_map`, respectively. 
+    The morphisms ``a`` and ``b`` can be passed as the optional arguments `domain_map` and `codomain_map`, respectively. 
 """
 function base_change(phi::Any, f::SchemeMor;
     domain_map::SchemeMor, codomain_map::SchemeMor
