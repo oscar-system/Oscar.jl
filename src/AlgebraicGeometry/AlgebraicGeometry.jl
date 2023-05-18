f8b326e3aa72962a81791a226908492c6085c725
Yue Ren <yue.ren.kl@gmail.com>
2023-05-18 10:38:54 +0100

    TropicalGeometry: moved outside AlgebraicGeometry

--- src/AlgebraicGeometry/AlgebraicGeometry.jl
+++ src/AlgebraicGeometry/AlgebraicGeometry.jl
@@ -1,7 +1,5 @@
 include("Schemes/main.jl")
 include("ToricVarieties/JToric.jl")
-include("TropicalGeometry/main.jl")
 include("Surfaces/K3Auto.jl")
 include("Surfaces/SurfacesP4.jl")
 include("Miscellaneous/basics.jl")
-
