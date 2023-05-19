e617dbbae0716dadecdd9df761d6cdd1b0185f71
Claus Fieker <fieker@mathematik.uni-kl.de>
2023-05-19 10:31:24 +0200

    wrong degree...

--- test/Experimental/galois.jl
+++ test/Experimental/galois.jl
@@ -68,7 +68,7 @@ end
 @testset "SolveByRadicals" begin
   Qx, x = QQ["x"]
   K, r = solve(x^3+3*x+5)
-  @test absolute_degree(K) == 9
+  @test absolute_degree(K) == 12
   @test length(r) == 3
 end
 
