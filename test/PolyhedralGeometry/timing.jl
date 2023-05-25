92cf284b9b4a66cc03bb425ac468a6876deec85d
Aaruni Kaushik <akaushik@mathematik.uni-kl.de>
2023-05-25 11:55:10 +0200

    Review: better readability of if clause

--- test/PolyhedralGeometry/timing.jl
+++ test/PolyhedralGeometry/timing.jl
@@ -7,14 +7,9 @@
     # If running multiple workers, and many OMP threads, CPU oversubscription may occur
     # which fails the timing tests. In this situation, skip instead.
     try
-        if (length(workers())>1)
-            if haskey(ENV, "OMP_NUM_THREADS")
-                if (ENV["OMP_NUM_THREADS"] != "1")
-                    return
-                end
-            else
-                return
-            end
+        if (length(workers()) > 1)
+            haskey(ENV, "OMP_NUM_THREADS") || return
+            ENV["OMP_NUM_THREADS"] == "1" || return
         end
     catch
         # `using Distributed` wasn't executed, and therefore `workers()` is undefined
