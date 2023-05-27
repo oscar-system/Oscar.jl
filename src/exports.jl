098c309b3785ee141ebaa7d593681848ca029570
Thomas Breuer <sam@math.rwth-aachen.de>
2023-05-27 13:36:58 +0200

    added `multiplicities_eigenvalues` for characters (#2419)

--- src/exports.jl
+++ src/exports.jl
@@ -951,6 +951,7 @@ export multi_hilbert_series_reduced
 export multiplication_induced_morphism
 export multiplication_morphism
 export multiplicative_jordan_decomposition
+export multiplicities_eigenvalues
 export n_cones
 export n_connected_components
 export n_maximal_cells
