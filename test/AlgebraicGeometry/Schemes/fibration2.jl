af624a0c2e0bacd6a211893a1626622f820f81c0
Simon Brandhorst <brandhorst@math.uni-sb.de>
2023-06-02 17:01:33 +0200

    Eliptic K3 stuff

---
+++ test/AlgebraicGeometry/Schemes/fibration2.jl
@@ -0,0 +1,172 @@
+  add_verbosity_scope(:ellipticK3)
+  set_verbosity_level(:ellipticK3, 2)
+  k = GF(29)
+  IP1 = projective_space(k, ["s", "t"])
+
+  O0 = twisting_sheaf(IP1, 0)
+  O4 = twisting_sheaf(IP1, -4)
+  O6 = twisting_sheaf(IP1, -6)
+
+  E = direct_sum([O0, O4, O6])
+
+  X_proj = projectivization(E, var_names=["z", "x", "y"])
+
+  X = covered_scheme(X_proj)
+  C = Covering([X[1][i] for i in [1,3,4,6]])
+  for U in patches(C)
+    for V in patches(C)
+      add_glueing!(C, glueings(X[1])[(U, V)])
+    end
+  end
+
+
+  # The generic fiber of the elliptic fibration
+  # as an elliptic curve over k(t)
+  kt, t = polynomial_ring(k, :t)
+  kP1 = fraction_field(kt)
+  t = gen(kP1)
+  E = EllipticCurve(kP1, [(21*t^7+6*t^6+11*t^4),(21*t^10+15*t^9+17*t^7+18*t^6+t^5)])
+  # A basis for the Mordell-Weil group of E
+  mwl_basis = [E(collect(i)) for i in [(7*t^3 + 24*t^2 + 9, 9*t^5 + 18*t^4 + 12*t^3 + 8*t^2 + 2),
+ (13*t^3 + 9*t + 24, 2*t^5 + 7*t^4 + 6*t^3 + 16*t^2 + 16*t + 22),
+ (t^3 + 24*t^2 + 22*t + 5, 10*t^5 + 6*t^4 + 6*t^3 + 8*t^2 + 14*t + 3),
+ ((17*t^5 + 14*t^4 + 28*t^2 + 2*t + 1)//(t^2 + 11*t + 23),
+  (t^8 + 19*t^7 + 2*t^6 + t^5 + 26*t^4 + 2*t^3 + 26*t + 28)//(t^3 + 2*t^2 + 11*t + 25)),
+ ((11*t^5 + 22*t^4 + 23*t^3 + 14*t^2 + 17*t + 16)//(t^2 + 24*t + 28),
+  (22*t^8 + 6*t^7 + 21*t^6 + 24*t^5 + 4*t^4 + 23*t^3 + 25*t^2 + 15*t + 6)//(t^3 + 7*t^2 + 26*t + 17))]]
+
+  # The section P used for the fibration hop
+  P = -mwl_basis[1] - mwl_basis[5]
+
+  # Create the singular Weierstrass model S of the elliptic K3 surface
+  a = a_invars(E)
+  U = affine_charts(X)[1]
+  (x, y, t) = gens(OO(U))
+  @assert all(denominator(i)==1 for i in a)
+  a = [numerator(a)(t) for a in a]
+  (a1,a2,a3,a4,a6) = a
+  ft = y^2  + a1*x*y + a3*y - (x^3 + a2*x^2 + a4*x+a6)
+  I = IdealSheaf(X, U, [ft])
+
+  inc_S = Oscar.CoveredClosedEmbedding(X, I)
+  @test I === image_ideal(inc_S)
+  S = domain(inc_S)
+
+  I_sing = Oscar.ideal_sheaf_of_singular_locus(S)
+  I_sing_X = radical(pushforward(inc_S)(I_sing))
+
+  @test scheme(I_sing) === S
+  @test scheme(I_sing_X) === X
+
+  # The mwl_basis as ideal sheaves
+  UX = X[1][1]
+  sections = [ideal_sheaf(X,X[1][1],[OO(UX)(i) for i in [x*denominator(b[1])(t)-numerator(b[1])(t),y*denominator(b[2])(t)-numerator(b[2])(t)]]) for b in mwl_basis]
+  PonX = ideal_sheaf(X,X[1][1],[OO(UX)(i) for i in [x*denominator(P[1])(t)-numerator(P[1])(t),y*denominator(P[2])(t)-numerator(P[2])(t)]])
+
+  #X = CoveredScheme(C)
+  (x,y,t) = coordinates(UX)
+  fiber = IdealSheaf(X,UX,[t-1,ft])
+  (z,x,t) = coordinates(X[1][3])
+  zero_section = IdealSheaf(X,X[1][3],[x,z])
+
+  (x,y,t) = coordinates(UX)
+  A1_0 = IdealSheaf(X,UX,[t,ft])
+  (x,y,s) = ambient_coordinates(X[1][4])
+  E8_0 = IdealSheaf(X,X[1][4],OO(X[1][4]).([s,gens((modulus(OO(S[1][4]))))[1]]))
+  divisors = vcat([PonX, zero_section, fiber,A1_0,E8_0], sections)
+
+
+  # initialization for the while loop
+  X0 = X
+  Y0 = S
+  inc_Y0 = inc_S
+  # blow up points (one at a time) until smooth
+  exceptionals = []
+  divisors0 = divisors
+  count = 0
+  varnames= [:a,:b,:c,:d,:e,:f,:g,:h,:i,:j,:k,:l]
+  projectionsX = []
+  projectionsY = []
+  #weil_divs = []
+  while true
+    global count = count+1
+    @vprint :ellipticK3 1 "blowup number: $(count)\n"
+    @vprint :ellipticK3 2 "computing singular locus\n"
+    I_sing_Y0 = Oscar.ideal_sheaf_of_singular_locus(Y0)
+    @vprint :ellipticK3 2 "decomposing singular locus\n"
+    I_sing_Y0 = Oscar.maximal_associated_points(I_sing_Y0)
+    @vprint :ellipticK3 1 "number of singular points: $(length(I_sing_Y0))\n"
+    if length(I_sing_Y0)==0
+      break
+    end
+    I_sing_X0_1 = radical(pushforward(inc_Y0)(I_sing_Y0[1]))
+    prX1 = blow_up(I_sing_X0_1, covering=Oscar.simplified_covering(X0),
+                  var_name=varnames[count])
+    X1 = domain(prX1)
+    E1 = exceptional_divisor(prX1)
+
+    @vprint :ellipticK3 2 "computing strict transforms\n"
+    global exceptionals = [strict_transform(prX1, e) for e in exceptionals]
+    global divisors0 = [strict_transform(prX1, e) for e in divisors0]
+    push!(exceptionals,E1)
+
+    Y1, inc_Y1, pr_Y1 = strict_transform(prX1, inc_Y0)
+
+    #=
+    @vprint :ellipticK3 "computing strict transforms of previous exceptional divisors\n"
+    global weil_divs = Any[saturation(pullback(pr_Y1)(e), ideal_sheaf(pullback(inc_Y1)(E1))) for e in weil_divs]
+    @vprint :ellipticK3 "computing exceptional weil-divisor\n"
+    W1 = [radical(i) for i in keys(coefficient_dict(weil_divisor(pullback(inc_Y1)(E1))))]
+    #append!(weil_divs, W1)
+    @vprint :ellipticK3 "done\n"
+    =#
+
+    push!(projectionsX, prX1)
+    push!(projectionsY, pr_Y1)
+    simplify!(Y1)
+
+    global Y0 = Y1
+    global inc_Y0 = inc_Y1
+    global X0 = X1
+  end
+  # Restrict the exceptional divisors:
+  @vprint :ellipticK3 2 "Pull back divisors to Y0\n"
+  exceptionals_res = [pullback(inc_Y0)(e) for e in exceptionals]
+  divisors_res = [pullback(inc_Y0)(e) for e in divisors0]
+
+  #Compute the intersection matrix:
+  Ex = exceptionals_res
+  @vprint :ellipticK3 2 "Exceptional Cartier to Weil divisors\n"
+  ExWeil = weil_divisor.(Ex)   # too slow to be a test
+  @vprint :ellipticK3 2 "done\n"
+  n = length(Ex)
+  A = zero_matrix(ZZ, n, n)
+  for i in 1:n
+    A[i, i] = -ZZ(2)
+    for j in i+1:n
+     @assert length(components(ExWeil[i]))==1
+     ci = coefficient_dict((ExWeil[i]))[components(ExWeil[i])[1]]
+     cj = coefficient_dict((ExWeil[j]))[components(ExWeil[j])[1]]
+     A[i, j] = divexact(integral(intersect(ExWeil[i], Ex[j])),ci*cj)
+     A[j, i] = A[i,j]
+   end
+  end
+  # The exceptional Weil divisors have multiplicities
+  # this comes from the intersection with Y0
+  # Compute the reduced one
+  tmp = [collect(keys(coefficient_dict(i))) for i in ExWeil]
+  @assert all(length(i)==1 for i in tmp)
+  ExWeil = [i[1] for i in tmp]
+  I = [2,9,4,6,5,8,7,3,1]; A[I,I]
+  ExWeil = ExWeil[I]
+
+  f = projectionsY[1]
+  for g in projectionsY[2:end]
+    global f = g*f
+  end
+  (x,y,t) = coordinates(S[1][1])
+  fstar = pullback(f[Y0[1][1]])
+  linsys = prop217(Y0, E, P, 1, fstar(x), fstar(y), fstar(t),fstar(t)-1)
+  PonY,OonY,FonY = [WeilDivisor(i,ZZ,check=false) for i in divisors_res[1:3]]
+  D = OonY + PonY + FonY
+  L = linear_system(linsys, D, check=false)
---
+++ test/AlgebraicGeometry/Schemes/fibration_hop.jl
@@ -0,0 +1,107 @@
+
+
+"""
+    prop217(E::EllCrv, P::EllCrvPt, k)
+
+Compute a basis for the linear system
+|O + P + kF|
+on the  minimal elliptic (K3) surface defined by E.
+Here F is the class of a fiber O the zero section
+and P any non-torsion section.
+
+```jldoctest
+julia> kt,t = polynomial_ring(GF(29),:t);
+
+julia> ktfield = fraction_field(kt);
+
+julia> bk = [((17*t^4 + 23*t^3 + 18*t^2 + 2*t + 6, 8*t^5 + 2*t^4 + 6*t^3 + 25*t^2 + 24*t + 5 )),
+             ((17*t^6 + 3*t^5 + 16*t^4 + 4*t^3 + 13*t^2 + 6*t + 5)//(t^2 + 12*t + 7), (4*t^8 + 19*t^7 + 14*t^6 + 18*t^5 + 27*t^4 + 13*t^3 + 9*t^2 + 14*t + 12)//(t^3 + 18*t^2 + 21*t + 13) ),
+             ((17*t^6 + 10*t^5 + 24*t^4 + 15*t^3 + 22*t^2 + 27*t + 5)//(t^2 + 16*t + 6), (20*t^8 + 24*t^7 + 22*t^6 + 12*t^5 + 21*t^4 + 21*t^3 + 9*t^2 + 21*t + 12)//(t^3 + 24*t^2 + 18*t + 19) ),
+             ((17*t^8 + 21*t^7 + 20*t^5 + 24*t^4 + 21*t^3 + 4*t^2 + 9*t + 13)//(t^4 + 17*t^3 + 12*t^2 + 28*t + 28), (23*t^11 + 25*t^10 + 8*t^9 + 7*t^8 + 28*t^7 + 16*t^6 + 7*t^5 + 23*t^4 + 9*t^3 + 27*t^2 + 13*t + 13)//(t^6 + 11*t^5 + 14*t^4 + 13*t^3 + 6*t^2 + 18*t + 12) )];
+
+julia> E = EllipticCurve(ktfield,[3*t^8+24*t^7+22*t^6+15*t^5+28*t^4+20*t^3+16*t^2+26*t+16, 24*t^12+27*t^11+28*t^10+8*t^9+6*t^8+16*t^7+2*t^6+10*t^5+3*t^4+22*t^3+27*t^2+10*t+3]);
+
+julia> bk = [E(collect(i)) for i in bk];
+
+julia> prop217(E,bk[1],2)
+(dega, degb) = (2, 0)
+4-element Vector{Any}:
+ (1, 0)
+ (t, 0)
+ (t^2, 0)
+ (0, 1)
+
+```
+"""
+function prop217(E::EllCrv, P::EllCrvPt, k)
+  @req !iszero(P[3]) "P must not be torsion" # seems like we cannot check this
+  xn = numerator(P[1])
+  xd = denominator(P[1])
+  yn = numerator(P[2])
+  yd = denominator(P[2])
+  OP = divexact(max(degree(xd), degree(xn) - 4), 2)
+  dega = k + 2*OP
+  degb = k + 2*OP - 2 - divexact(degree(xd), 2) #?
+  base = base_ring(X)
+
+  R,ab = polynomial_ring(base,vcat([Symbol(:a,i) for i in 0:dega],[Symbol(:b,i) for i in 0:degb]),cached=false)
+  Rt, t1 = polynomial_ring(R,:t)
+  a = reduce(+,(ab[i+1]*t1^i for i in 0:dega), init=zero(Rt))
+  b = reduce(+,(ab[1+degb+j]*t1^j for j in 0:degb), init=zero(Rt))
+  c = a*xn(t1) - b*yn(t1)
+  _, r = divrem(c, xd(t1))
+
+  # setup the linear equations for coefficients of r to vanish
+  # and for the degree of c to be bounded above by
+  # k + 2*OP + 4 + degree(xd)
+  eqns = vcat(collect(coefficients(r)),[coeff(c,i) for i in (k + 2*OP + 4 + degree(xd)) + 1:degree(c)])
+
+  # collect the equations as a matrix
+  cc = [[coeff(j, abi) for abi in ab] for j in eqns]
+  M = matrix(base, cc)
+  kerdim, K = kernel(M)
+  @assert kerdim == 2*k + OP # prediced by Riemann-Roch
+  result = []
+  Bt = base_ring(base_field(E))
+  t = gen(Bt)
+  for j in 1:kerdim
+    aa = reduce(+, (K[i+1,j]*t^i for i in 0:dega), init=zero(Bt))
+    bb = reduce(+, (K[dega+i+2,j]*t^i for i in 0:degb), init=zero(Bt))
+    push!(result, (aa, bb))
+  end
+  return result
+end
+
+function prop217(X::AbsCoveredScheme, E::EllCrv, P::EllCrvPt, k, x, y, t,fiber)
+  FX = VarietyFunctionField(X)
+  xn = numerator(P[1])
+  xd = denominator(P[1])
+  yn = numerator(P[2])
+  yd = denominator(P[2])
+  @assert gcd(xn, xd)==1
+  @assert gcd(yn, yd)==1
+  sections = []
+  ab = prop217(E, P, k)
+  x = lifted_numerator(x)
+  y = lifted_numerator(y)
+  t = lifted_numerator(t)
+  d = divexact(yd, xd)(t)
+  den = lifted_numerator(fiber)^k*(x*xd(t) - xn(t))
+  #t^degree(d)
+  for (a,b) in ab
+    c = divexact(b*yn - a*xn, xd)
+    num = a(t)*x+b(t)*d*y + c(t)
+    push!(sections, FX(num,den))
+  end
+  return sections
+end
+
+
+function Oscar.saturation(I::IdealSheaf, J::IdealSheaf)
+  X = scheme(I)
+  K = IdDict{AbsSpec,Ideal}()
+  for U in X[1]
+    K[U] = saturation(I(U),J(U))
+  end
+  return IdealSheaf(X, K, check=false)
+end
