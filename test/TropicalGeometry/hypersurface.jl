9337b7248463f91361388e7393d24ac8df2827b1
Lars Kastner <lars.kastner@posteo.de>
2023-06-24 15:27:29 +0200

    TropicalGeometry: Tests for new conversion

---
+++ test/TropicalGeometry/hypersurface.jl
@@ -0,0 +1,14 @@
+@testset "hypersurface" begin
+  for addition in (max, min)
+    T = TropicalSemiring(addition)
+    R,(x,y,z) = PolynomialRing(T,3)
+    b = [T(10),T(3//2095), T(-5//2)]
+    m = [[1,0,1],[0,3,4],[1,0,7]]
+    f = R(b,m)
+    hyp = TropicalHypersurface(f)
+    coeffs, exps = Oscar.homogenize_and_convert_to_pm(f)
+    @test hyp isa TropicalHypersurface{addition}
+    @test coeffs == hyp.__attrs[:polymake_bigobject].COEFFICIENTS
+    @test exps == matrix(ZZ,hyp.__attrs[:polymake_bigobject].MONOMIALS)
+  end
+end
--- test/TropicalGeometry/runtests.jl
+++ test/TropicalGeometry/runtests.jl
@@ -3,3 +3,4 @@ using Oscar
 include("groebner_basis.jl")
 include("groebner_fan.jl")
 include("initial.jl")
+include("hypersurface.jl")
