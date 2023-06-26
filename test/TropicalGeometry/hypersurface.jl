ba231f5e99b03b6e6a1101b405d8e77a267f3bd0
Benjamin Lorenz <benlorenz@users.noreply.github.com>
2023-06-26 11:55:26 +0200

    Update test/TropicalGeometry/hypersurface.jl

--- test/TropicalGeometry/hypersurface.jl
+++ test/TropicalGeometry/hypersurface.jl
@@ -8,7 +8,7 @@
     hyp = TropicalHypersurface(f)
     coeffs, exps = Oscar.homogenize_and_convert_to_pm(f)
     @test hyp isa TropicalHypersurface{addition}
-    @test coeffs == hyp.__attrs[:polymake_bigobject].COEFFICIENTS
-    @test exps == matrix(ZZ,hyp.__attrs[:polymake_bigobject].MONOMIALS)
+    @test coeffs == get_attribute(hyp, :polymake_bigobject).COEFFICIENTS
+    @test exps == matrix(ZZ, get_attribute(hyp, :polymake_bigobject).MONOMIALS)
   end
 end
