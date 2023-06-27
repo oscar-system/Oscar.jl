e457b88f0bb4b5c97a811ebccb03033fdc42104c
voggesbe <101730185+voggesbe@users.noreply.github.com>
2023-06-27 15:14:39 +0200

    Add files via upload
    Added files for a root system and for creating simple Lie algebras via a root system type

--- experimental/LieAlgebras/src/root_systems.jl
+++ experimental/LieAlgebras/src/root_systems.jl
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 mutable struct RootSystem
   roots::Vector{Vector{Int}}
   simple_roots::Vector{Vector{Int}}
@@ -48,6 +49,38 @@ mutable struct RootSystem
     return new(Ro, sR, Ro1, t)
 >>>>>>> changed to rebase to master
   end
+=======
+# LieAlg.jl : Implements root systems (by copying most structures from GAP)
+#TODO: implement Weyl groups. Is it possible to give groups by generatrs and relations in Oscar?
+using Oscar
+
+GAP.Packages.load("sla")
+ 
+import Base: show, size, getindex
+
+mutable struct RootSystem <: AbstractVector{fmpq}
+   roots::Vector
+   simple_roots::Vector
+   positive_roots::Vector
+   root_system_type::String
+
+   function RootSystem(S::String)
+        l=length(S)
+	n=parse(Int64,S[2:l])
+	S1=GAP.Globals.String(S[1:1])[2:2]
+
+	RS=GAP.Globals.RootSystem(S1,n)
+	Ro_1=GAP.Globals.PositiveRoots(RS)
+	Ro_2=GAP.Globals.NegativeRoots(RS)
+	sR=GAP.Globals.SimpleSystem(RS)
+
+        sR=[[sR[i][j] for j=1:length(sR[i])] for i=1:length(sR)]
+	Ro1=[[Ro_1[i][j] for j=1:length(Ro_1[i])] for i=1:length(Ro_1)]
+	Ro2=[[Ro_2[i][j] for j=1:length(Ro_2[i])] for i=1:length(Ro_2)]
+	Ro=reduce(vcat, (Ro1, Ro2))
+	new(Ro,sR,Ro1,S)
+   end
+>>>>>>> Add files via upload
 end
 
 ###############################################################################
@@ -55,6 +88,7 @@ end
 #   Basic manipulation
 #
 ###############################################################################
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 @doc raw"""
@@ -127,6 +161,14 @@ getindex(R::RootSystem, r::Int) = getindex(R.roots, r)
 Return the Dynkin type of the root system `R`.
 """
 root_system_type(R::RootSystem) = R.root_system_type
+=======
+
+size(R::RootSystem,dim::Any)=size(R.roots,dim)
+
+size(R::RootSystem)=size(R.roots)
+
+getindex(R::RootSystem, r::Any) = getindex(R.roots, r)
+>>>>>>> Add files via upload
 
 ###############################################################################
 #
@@ -135,6 +177,7 @@ root_system_type(R::RootSystem) = R.root_system_type
 ###############################################################################
 
 function show(io::IO, R::RootSystem)
+<<<<<<< HEAD
   print(io, "Root system of type ")
   show(io, string(R.root_system_type[1]) * string(R.root_system_type[2]))
 <<<<<<< HEAD
@@ -177,10 +220,15 @@ function root_system(S::Symbol, n::Int64)
   return RootSystem(S, n)
 =======
 >>>>>>> changed to rebase to master
+=======
+   print(io, "Root system of type ")
+   show(io, R.root_system_type)
+>>>>>>> Add files via upload
 end
 
 ###############################################################################
 #
+<<<<<<< HEAD
 #   Comparison
 #
 ###############################################################################
@@ -425,5 +473,90 @@ function dynkin_diagram(R::RootSystem)
 	S = R.root_system_type
 	return dynkin_diagram(S[1],S[2])
 >>>>>>> changed to rebase to master
+=======
+#   further functions
+#
+###############################################################################
+
+function CartanMatrix(S::String)
+		S1=S[1:1]
+		l=length(S)
+		n=parse(Int64,S[2:l])
+		S1=GAP.Globals.String(S[1:1])[2:2]
+		m=GAP.Globals.Int(n)
+
+		RS=GAP.Globals.RootSystem(S1,n)
+		CG=GAP.Globals.CartanMatrix(RS)
+		C=Matrix{fmpq}(CG)
+	return C
+end
+
+function CartanMatrix(R::RootSystem)
+	S=R.root_system_type
+	C=CartanMatrix(S)
+	return C
+end
+
+function DynkinDiagram(S::String)
+	S1=S[1:1]
+	l=length(S)
+	n=parse(Int64,S[2:l])
+	D=""
+	if S1 == "A"
+		for i=1:(n-1)
+			D=D* string(i) * " - "
+		end
+		D=D* string(n)
+	elseif S1=="B"
+		for i=1:(n-2)
+			D=D* string(i) * " - "
+		end
+		D=D* string(n-1) * " >=> "*string(n)
+	elseif S1=="C"
+		for i=1:(n-2)
+			D=D* string(i) * " - "
+		end
+		D=D* string(n-1) * " <=< "*string(n)
+	elseif S1=="D"
+		for i=1:4*n-10
+			D=D*" "
+		end
+		D=D* string(n-1)*"\n"
+		for i=1:4*n-11
+			D=D*" "
+		end
+		D=D*"/\n"
+		for i=1:n-3
+			D=D* string(i) * " - "
+		end
+		D=D*string(n-2) *"\n"
+		for i=1:4*n-12
+			D=D*" "
+		end
+		D=D*" \\ \n"
+		for i=1:4*n-10
+			D=D*" "
+		end
+		D=D* string(n)
+	elseif S1=="E"
+		if n==6
+			D="1 - 3 - 4 - 5 - 6\n        |\n        2"
+		elseif n==7
+			D="1 - 3 - 4 - 5 - 6 - 7\n        |\n        2"
+		elseif n==8
+			D="1 - 3 - 4 - 5 - 6 - 7 - 8\n        |\n        2"
+		end
+	elseif S1=="F"
+		D="1 - 2 >=> 3 - 4"#this is changed from GAP
+	elseif S1=="G"
+		D="1 >>> 2"
+	end
+	print(D)
+end
+
+function DynkinDiagram(R::RootSystem)
+	S=R.root_system_type
+	DynkinDiagram(S)
+>>>>>>> Add files via upload
 end
 
