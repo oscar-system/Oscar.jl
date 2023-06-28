9e1b1d808c608aaf495c5959fdfa482efae605af
anne <anne@krueger-berg.de>
2023-06-28 12:22:35 +0200

    complete rewrite following reviewer's suggestions

--- experimental/Schemes/duValSing.jl
+++ experimental/Schemes/duValSing.jl
@@ -1,227 +1,208 @@
-function has_du_val_singularities(F::MPolyDecRingElem)::Bool
+function has_du_val_singularities(X::AbsProjectiveScheme{<:Field,<:Any})
+  return has_du_val_singularities(covered_scheme(X))
+end
 
-  R = parent(F)
-  is_graded(R) || error("only available for graded rings")
-  is_standard_graded(R) || error("only available for standard grading")
-  ncharts = ngens(R)
+function has_du_val_singularities(X::AbsSpec{<:Field,<:Any})
+  R = OO(X)
+  I = modulus(R)
+  
+  J = image_ideal(singular_locus(X)[2])
+  dim(J) == 0 || return false                            ## non-isolated
+  return is_du_val_singularity(X,J)
+end
 
-  ncharts == 4 || error("not a surface")            ## hypersurface in P^3
+function has_du_val_singularities(X::AbsCoveredScheme{<:Field})
+  C = (has_attribute(X0, :simplified_covering) ? simplified_covering(X0) : default_covering(X0))
 
-  JF = jacobi_ideal(F)
-  I_sl = JF + ideal(R,F)
-  dim(I_sl) > 0 || return true                      ## smooth, hence 'at most duVal'
-  decomp = minimal_primes(I_sl)
+  I = ideal_sheaf_of_singular_locus(X)
+  decomp = minimal_associated_points(I)
 
-  not_over_k = Vector{Tuple{MPolyIdeal,Int}}()
-  dehom_dict = IdDict{Int,Ideal}()
-
-  for I in decomp
-    d = dim(I)
-    d < 2 || return false                           ## non-isolated is not duVal
-    
-    ## It is definitely a point now, i.e. I becomes maximal after dehomog.
-    ## It shows up in at least one dehomogenizatied chart
-    i = 0
-    I_dehomog = dehomogenization(I,1)               ## initialize I_dehomog
-    while i < ncharts
-      i=i+1
-      I_dehomog = dehomogenization(I,i)
-      is_one(I_dehomog) || break
-    end
-
-    ## cache dehomogenized ideal
-    if !haskey(dehom_dict,i)
-      dehom_dict[i] = dehomogenization(I_sl,i)
-    end
-
-    ## make sure to do only the geometrically irreducible points first
-    ## and postpone all geometrically reducible points
-    if vector_space_dimension(quo(base_ring(I_dehomog),I_dehomog)[1]) > 1 
-      push!(not_over_k, (I_dehomog,i))
-      continue
-    end
-
-    ## now do the check for this geometrically irreducible point
-    _check_duval_at_point(I_dehomog,dehom_dict[i]) || return false    
-
-  end
-
-  ## handle geometrically reducible points afterwards
-  for (I,i) in not_over_k
-    li = absolute_primary_decomposition(I)
-    for (_, _, I2, _) in li
-      ## prepare setting to check over algebraic extension
-      r_changed = base_ring(I2)
-      kk = coefficient_ring(r_changed)
-      I_sl_change = ideal(r_changed, [change_coefficient_ring(kk,a) for a in gens(dehom_dict[i])])
-      ## now do the check (at one of the conjugate points)
-      _check_duval_at_point(I2,I_sl_change) || return false
+  ## we do the double loop here to avoid unnecessary checks
+  for J in decomp
+    for U in C
+      !isone(J(U)) || continue
+      is_du_val_singularity(X(U),J(U)) || return false    ## testing the point in one chart suffices
+      break                         
     end
   end
 
   return true
 end
 
-function _check_duval_at_point(I_r::MPolyIdeal,I_sl_r::MPolyIdeal)::Bool
-    ## initialize
-    r = base_ring(I_r)
+function is_du_val_singularity(X::AbsSpec{<:Field,<:Any},I::Union{MPolyIdeal, MPolyLocalizedIdeal})
+  OOX = OO(X)
+  dim(X) == 2 || error("Scheme not of dimension 2")
+  J = modulus(OOX)
+  !isone(J) || error("Scheme is empty")
+  !iszero(J) || return true                  ## X smooth
 
-    ## localize at point
-    a = Oscar.rational_point_coordinates(I_r)
-    U = complement_of_point_ideal(r,a)
-    rl, loc_map = localization(r,U)
-    I_loc = loc_map(I_sl_r)
+  R = base_ring(I)
+  kk = base_ring(R)
+  characteristic(kk) == 0 || error("only available in characteristic zero")
+  base_ring(OOX) === R || error("base rings need to be identical")
 
-    o = negdegrevlex(gens(r))
-
-    ## do quick version of beginning of Arnold's classifier
-    tau=vector_space_dimension(quo(rl,I_loc)[1])
-
-    corank = vector_space_dimension(quo(rl,I_loc+loc_map(I_r)^2)[1]) - 1
-                                         # only count degree 1 monomials
-    corank < 3 || return false           # at least T_3,3,3, not duVal
-    corank > 1 || return true            # A series
-
-    cubiccount = vector_space_dimension(quo(rl,I_loc+loc_map(I_r)^3)[1]) - corank - 1
-                                         # only count degree 2 monomials
-    cubiccount < 3 || return false       # at least X_9, not duVal 
-    cubiccount > 1 || return true        # D series
-
-    tau < 9 || return false              # at least J_10           
-    return true                          # E_k, k \in {6,7,8}
-end
-
-function has_du_val_singularities(J::MPolyIdeal{<:MPolyDecRingElem})
-  R = base_ring(J)
-  is_graded(R) || error("only available for graded rings")
-  is_standard_graded(R) || error("only available for standard grading")
-  ncharts = ngens(R)
-
-  # mind that dimensions are shifted by one as the methods refer to
-  # the polynomial ring and hence consider the affine cone
-  dim(J) == 3 || error("not a surface")
-  JM = jacobi_matrix(gens(J))
-  minvec = minors(JM,ncharts - 3)
-  I_sl = ideal(R,minvec) + J
-  dim(I_sl) > 0 || return true                 ## smooth
-
-  decomp = minimal_primes(I_sl)
-
-  not_over_k = Vector{Tuple{MPolyIdeal,Int}}()
-  dehom_dict = IdDict{Int,MatrixElem}()
-  Jdehom_dict = IdDict{Int,MPolyIdeal}()
-
-  for I in decomp
-    d = dim(I)
-    d < 2 || return false                      ## non-isolated is not duVal
-    
-    ## It is definitely a point now, i.e. I becomes maximal after dehomog.
-    ## It shows up in at least one dehomogenization chart
-    i = 0
-    I_dehomog = dehomogenization(I,1)
-    while i < ncharts
-      i=i+1
-      I_dehomog = dehomogenization(I,i)
-      is_one(I_dehomog) || break
-    end
-
-    ## cache dehomogenized matrix
-    if !haskey(dehom_dict,i)
-      dehom_dict[i] = map(x -> dehomogenization(x,i), JM[:,:])
-      Jdehom_dict[i] = dehomogenization(J,i)
-    end
-
-    ## make sure to do the geometrically irreducible points first
-    if vector_space_dimension(quo(base_ring(I_dehomog),I_dehomog)[1]) > 1
-      push!(not_over_k, (I_dehomog,i))
-      continue
-    end
-
-    _check_duval_at_point(I_dehomog,dehom_dict[i],Jdehom_dict[i]) || return false
-
+  dim(I) == 0 || error("second argument is not a point")
+  if get_attribute(I,:is_absolutely_prime, false)
+    return _check_duval_at_point(J,I)[1]
   end
 
-  ## handle geometrically reducible points afterwards
-  for (I,i) in not_over_k
-    li = absolute_primary_decomposition(I)
-    for (_, _, I2, _) in li
-      r_changed = base_ring(I2)
-      kk = coefficient_ring(r_changed)
-## Question: The following line is ugly, what is the nice way to move a matrix through change of coeff. field
-      JM_changed = matrix(r_changed, ncharts, ngens(J), [change_coefficient_ring(kk,a) for a=dehom_dict[i][:,:]])
-      J_changed = ideal(r_changed, [change_coefficient_ring(kk,a) for a=gens(Jdehom_dict[i])])
-      _check_duval_at_point(I2,JM_changed,J_changed)
-    end
+  decomp = absolute_primary_decomposition(I)
+  for (_,_,I2,mult) in decomp
+    set_attribute!(I,:is_absolutely_prime,true)
+    ## pass to algebraic extension
+    r_changed = base_ring(I2)
+    kk = coefficient_ring(r_changed)
+    J_changed = ideal(r_changed,  [change_coefficient_ring(kk,a, parent = r_changed) for a=gens(J)])
+    is_du_val_singularity(Spec(quo(r_changed,J_changed)[1]),I2) || return false
   end
-
+  
   return true
 end
 
-## CAUTION!!!
-## The following needs to be cleaned up properly after vdim for modules is available in Oscar
-## in particular corank and cubiccount should be vector_space_dimensions and all auxiliary modules from F4 on
-## are unnecessary once we can check the Tjurina number (vector_space_dimension(quo(F_shifted,F1)))
-##
-function _check_duval_at_point(I_r::MPolyIdeal, JM_dehomog::MatrixElem, J_dehomog::MPolyIdeal)  
-  ## go to chosen affine chart
-  r = base_ring(I_r)
-  I_max = ideal(r,gens(r))
+function decide_duval_at_point(X::AbsSpec{<:Field,<:Any},I::Union{MPolyIdeal, MPolyLocalizedIdeal})
+  OOX = OO(X)
+  dim(X) == 2 || error("Scheme not of dimension 2")
+  J = modulus(OOX)
+  !isone(J) || error("Scheme is empty")
+  !iszero(J) || return true                  ## X smooth
+
+  R = base_ring(I)
+  kk = base_ring(R)
+  characteristic(kk) == 0 || error("only available in characteristic zero")
+  base_ring(OOX) === R || error("base rings need to be identical")
+
+  dim(I) == 0 || error("second argument is not a point")
+  if get_attribute(I,:is_absolutely_prime, false)
+    li = _check_duval_at_point(J,I)
+    return [(li[1], I, li[2])]
+  end
+
+  result_vector = []
+  decomp = absolute_primary_decomposition(I)
+  for (_,_,I2,mult) in decomp
+    set_attribute!(I,:is_absolutely_prime,true)
+    ## pass to algebraic extension
+    r_changed = base_ring(I2)
+    kk = coefficient_ring(r_changed)
+    J_changed = ideal(r_changed,  [change_coefficient_ring(kk,a, parent = r_changed) for a=gens(J)])
+    tempvec = decide_duval_at_point(Spec(quo(r_changed,J_changed)[1]),I2)
+    for x in tempvec
+      x[1] || return [x]
+      push!(result_vector,x)
+    end
+  end
+@show result_vector
+
+  return result_vector
+end
+
+function _check_duval_at_point(IX::Ideal,Ipt::Ideal)
+  R = base_ring(IX)
+  R == base_ring(Ipt) || error("basering mismatch")
+  kk = base_ring(R)
+  characteristic(kk) == 0 || error("only available in characteristic zero")
+ 
+  JM = jacobi_matrix(gens(IX))
+
+  smooth = (:A,0)
 
   ## localize at point
-  a = Oscar.rational_point_coordinates(I_r)
-  U = complement_of_point_ideal(r,a)
-  rl, loc_map = localization(r,U)
-## Question: This following line is ugly, what is the nice way to move a matrix through extension of scalars
-  JM_loc = map(x ->loc_map(x), JM_dehomog[:,:])
+  a = Oscar.rational_point_coordinates(Ipt)
+  U = complement_of_point_ideal(R,a)
+  RL, loc_map = localization(R,U)
+  IX_loc = loc_map(IX)
+  JM_loc =  map(x ->loc_map(x), JM[:,:])
 
-  if !all(iszero, a)
-    Jm = Oscar.SubModuleOfFreeModule(JM_loc)
-    F = ambient_free_module(Jm)
-    Jm_shifted = Oscar.shifted_module(Jm)
+  if !all(iszero(a))
+    F_loc = free_module(RL,ngens(IX))
+    Jm = sub(F_loc,JM_loc)
+    Jm = Jm + loc_map(IX)*F_loc
+    Jm_shifted = Oscar. shifted_module(Jm)
     F_shifted = ambient_free_module(Jm_shifted)
-    Jm_shifted = Jm_shifted + Oscar.times(Oscar.shifted_ideal(loc_map(J_dehomog)), F_shifted)
   else
-    Jm_shifted = Oscar.SubModuleOfFreeModule(JM_dehomog)
-    F_shifted = ambient_free_module(Jm_shifted)
-    Jm_shifted = Jm_shifted + Oscar.times(J_dehomog, F_shifted)
+    F = free_module(R,ngens(IX))
+    Jm = sub(F,JM)[1]
+    Jm_shifted = Jm + (IX * F)[1]
+    F_shifted = F
   end
 
-  o = negdegrevlex(r)*lex(F_shifted)
+  o = negdegrevlex(R)*lex(F_shifted)
   F1 = leading_module(Jm_shifted,o)
-  F1_red = [reduce(gen(F_shifted,i),F1) for i in 1:ngens(F_shifted)]
-  F1_red = filter(x -> !is_zero(x), F1_red)
+  F1quo = quo(F_shifted, F1)[1]
 
-  length(F1_red) == 1 || return false  ## not a hypersurface singularity
+  constant_mons = vector_space_dimension(F1quo,0) 
+  constant_mons < 2 || return (false, typeof(smooth))                   ## not a hypersurface
+  constant_mons > 0 || return (true, smooth)                            ## no singularity
   
-  F2 = [reduce(a*gen(F_shifted,i),F1) for i in 1:ngens(F_shifted) for a in gens(I_max)]
-  F2_red = filter(x -> !is_zero(x),F2)
-  corank = length(F2_red)
-  # corank >=3 implies at least T_3,3,3, not duVal
-  corank < 3 || return false   
-  # corank ==1 implies A series        
-  corank > 1 || return true    
-   
-  F3 = [reduce(a*gen(F_shifted,i),F1) for i in 1:ngens(F_shifted) for a in gens(I_max^2)]
-  F3_red = filter(x -> !is_zero(x),F3)
-  cubiccount = length(F3_red)
-  # corank == 2 and cubiccount > 2 implies  at least X_9, not duVal 
-  cubiccount < 3 || return false       
-  # corank == 2 and cubiccount < 2 implies D series
-  cubiccount > 1 || return true        
+  tau = vector_space_dimension(F1quo)
 
-  # we are in E/J series
-#  tau = vector_space_dimension(SubquoModule(F,F1)[1])
-  F4 = [reduce(a*gen(F_shifted,i),F1) for i in 1:ngens(F_shifted) for a in gens(I_max^3)]
-  F4_red = filter(x -> !is_zero(x),F4)
-  F5 = [reduce(a*gen(F_shifted,i),F1) for i in 1:ngens(F_shifted) for a in gens(I_max^4)]
-  F5_red = filter(x -> !is_zero(x),F5)
-  F6 = [reduce(a*gen(F_shifted,i),F1) for i in 1:ngens(F_shifted) for a in gens(I_max^5)]
-  F6_red = filter(x -> !is_zero(x),F6)
+  corank = vector_space_dimension(F1quo,1)
+  corank < 3 || return (false,typeof(smooth))                           ## at least T_3,3,3 not duVal
+  corank > 1 || return (true,(:A,tau))                                  ## A_series
 
-  # tau > 8 implies at least J_10, not du Val   
-  # worksaround for missing vector_space_dimension (counting monomials degree by degree, 5 were already present before)
-  length(F4_red) + length(F5_red) + length(F6_red) < (9-5) || return false             
-  # E_k, k \in {6,7,8}
-  return true                          
+  # we now already know essentially a hypersurface of corank 2, count degrees of freedom cubicpart
+  cubiccount = vector_space_dimension(F1quo,2)
+  cubiccount < 3 || return  (false, typof(smooth))                      ## at least X_9
+  cubiccount > 1 || return  (true, (:D,tau))                            ## D_series
+
+  # it is definitely in the E/J series
+  tau < 9 || return(false, typeof(smooth))                          ## at least J_10
+  return (true, (:E,tau))                                               ## E_6, E_7, E_8
+end
+
+function vector_space_dimension(M::SubquoModule)
+  
+  R = base_ring(M)
+  F = ambient_free_module(M)
+  Mq,_ = sub(F,rels(M))
+
+  ambient_representatives_generators(M) == gens(F) || error("not implemented for M/N with non-trivial M")
+
+  o = default_ordering(M)
+  LM = leading_module(Mq,o)
+
+  has_monomials_on_all_axes(LM) || error("not a finite dimensional vector space")
+  
+  d = 0
+  sum_dim = 0
+  tempdim = vector_space_dimension(M,0)
+
+  while tempdim > 0
+    sum_dim = sum_dim + tempdim
+    d = d+1
+    tempdim = vector_space_dimension(M,d)
+  end
+ 
+  return sum_dim
+end
+
+function vector_space_dimension(M::SubquoModule,d::Int64)
+  R = base_ring(M)
+  F = ambient_free_module(M)
+  Mq,_ = sub(F,rels(M))
+
+  ambient_representatives_generators(M) == gens(F) || error("not implemented for M/N with non-trivial M")
+
+  o = default_ordering(M)
+  LM = leading_module(Mq,o)
+
+  return length([x*e for x in Oscar.all_monomials(R, d) for e in gens(F) if !(x*e in LM)])
+end
+  
+function has_monomials_on_all_axes(M::SubquoModule)
+  R = base_ring(M)
+
+  length(rels(M)) == 0 || error("not implemented for quotients")
+  
+  ambient_rank = ngens(ambient_free_module(M))
+  genlist = ambient_representatives_generators(M)
+  explist = Tuple{Vector{Int64}, Int64, Int}[]
+  for x in genlist
+    tempexp = leading_exponent(x)
+    tempdeg = sum(tempexp[1])
+    push!(explist,(tempexp[1],tempexp[2],tempdeg))
+  end
+  all(findfirst(x -> (x[1][i] == x[3] && x[2]==j),explist) != nothing for i in 1:ngens(R) for j in 1:ambient_rank) || return false
+  return true
 end
 
