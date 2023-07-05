a367e65f0b98b06fd36a7c18fe8abfb0a4553aaa
voggesbe <101730185+voggesbe@users.noreply.github.com>
2023-07-05 17:42:19 +0200

    Update root_systems.jl

--- experimental/LieAlgebras/src/root_systems.jl
+++ experimental/LieAlgebras/src/root_systems.jl
@@ -55,83 +55,80 @@ end
 ###############################################################################
 
 function CartanMatrix(S::String)
-  S1=S[1:1]
-		l=length(S)
-		n=parse(Int64,S[2:l])
-		S1=GAP.Globals.String(S[1:1])[2:2]
-		m=GAP.Globals.Int(n)
-
-		RS=GAP.Globals.RootSystem(S1,n)
-		CG=GAP.Globals.CartanMatrix(RS)
-		C=Matrix{fmpq}(CG)
+  S1 = S[1:1]
+  l = length(S)
+  n = parse(Int64, S[2:l])
+  S1 = GAP.Obj(string(S[1:1]))
+  RS = GAP.Globals.RootSystem(S1, n)
+  CG = GAP.Globals.CartanMatrix(RS)
+  C = Matrix{fmpq}(CG)
 	return C
 end
 
 function CartanMatrix(R::RootSystem)
-	S=R.root_system_type
-	C=CartanMatrix(S)
+  S = R.root_system_type
+	C = CartanMatrix(S)
 	return C
 end
 
 function DynkinDiagram(S::String)
-	S1=S[1:1]
-	l=length(S)
-	n=parse(Int64,S[2:l])
-	D=""
+  S1 = S[1:1]
+	l = length(S)
+	n = parse(Int64,S[2:l])
+	D = ""
 	if S1 == "A"
-		for i=1:(n-1)
-			D=D* string(i) * " - "
+    for i = 1:(n-1)
+       D = D * string(i) * " - "
 		end
-		D=D* string(n)
-	elseif S1=="B"
-		for i=1:(n-2)
-			D=D* string(i) * " - "
+    D = D * string(n)
+	elseif S1 == "B"
+	  for i = 1:(n-2)
+		  D = D * string(i) * " - "
 		end
-		D=D* string(n-1) * " >=> "*string(n)
-	elseif S1=="C"
-		for i=1:(n-2)
-			D=D* string(i) * " - "
+		D = D * string(n-1) * " >=> " * string(n)
+	elseif S1 == "C"
+	  for i = 1:(n-2)
+      D = D * string(i) * " - "
 		end
-		D=D* string(n-1) * " <=< "*string(n)
-	elseif S1=="D"
-		for i=1:4*n-10
-			D=D*" "
+		D = D * string(n-1) * " <=< " * string(n)
+	elseif S1 == "D"
+		for i = 1:4*n-10
+			D = D * " "
 		end
-		D=D* string(n-1)*"\n"
-		for i=1:4*n-11
-			D=D*" "
+		D = D * string(n-1) * "\n"
+		for i = 1:4*n-11
+			D = D * " "
 		end
-		D=D*"/\n"
-		for i=1:n-3
-			D=D* string(i) * " - "
+		D = D * "/\n"
+		for i = 1:n-3
+			D = D * string(i) * " - "
 		end
-		D=D*string(n-2) *"\n"
-		for i=1:4*n-12
-			D=D*" "
+		D = D * string(n-2) * "\n"
+		for i = 1:4*n-12
+			D = D * " "
 		end
-		D=D*" \\ \n"
-		for i=1:4*n-10
-			D=D*" "
+		D = D * " \\ \n"
+		for i = 1:4*n-10
+			D = D * " "
 		end
-		D=D* string(n)
-	elseif S1=="E"
-		if n==6
-			D="1 - 3 - 4 - 5 - 6\n        |\n        2"
-		elseif n==7
-			D="1 - 3 - 4 - 5 - 6 - 7\n        |\n        2"
-		elseif n==8
-			D="1 - 3 - 4 - 5 - 6 - 7 - 8\n        |\n        2"
+		D = D * string(n)
+	elseif S1 == "E"
+		if n == 6
+			D = "1 - 3 - 4 - 5 - 6\n        |\n        2"
+		elseif n == 7
+		  D = "1 - 3 - 4 - 5 - 6 - 7\n        |\n        2"
+		elseif n == 8
+			D = "1 - 3 - 4 - 5 - 6 - 7 - 8\n        |\n        2"
 		end
-	elseif S1=="F"
-		D="1 - 2 >=> 3 - 4"#this is changed from GAP
-	elseif S1=="G"
-		D="1 >>> 2"
+	elseif S1 == "F"
+		D = "1 - 2 >=> 3 - 4"
+	elseif S1 == "G"
+		D = "1 >>> 2"
 	end
 	print(D)
 end
 
 function DynkinDiagram(R::RootSystem)
-	S=R.root_system_type
+	S = R.root_system_type
 	DynkinDiagram(S)
 end
-
