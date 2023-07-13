4c71f17d5dd437323241f55e97c9bcaa92c84fae
Lars Kastner <lars.kastner@posteo.de>
2023-07-13 22:07:39 +0200

    ToricGeometry: Reduce number of using statements in tests

--- test/AlgebraicGeometry/ToricVarieties/toric_blowups.jl
+++ test/AlgebraicGeometry/ToricVarieties/toric_blowups.jl
@@ -1,6 +1,3 @@
-using Oscar
-using Test
-
 @testset "Blowup of toric varieties (set_attributes = $set_attributes)" for set_attributes in [true, false]
     
     P2 = projective_space(NormalToricVariety, 2; set_attributes)
