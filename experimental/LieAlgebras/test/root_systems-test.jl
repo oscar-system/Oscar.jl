ba68c8c95d97da85c57284f987f718c9a684a262
Laura Voggesberger <laura.voggesberger@rub.de>
2023-07-19 11:50:30 +0200

    added tests for root_systems

--- experimental/LieAlgebras/test/root_systems-test.jl
+++ experimental/LieAlgebras/test/root_systems-test.jl
@@ -1,5 +1,6 @@
 @testset "LieAlgebras.RootSystem" begin
     R = RootSystem("F4")
     @test cartan_matrix(R) == MatrixSpace(QQ, 4, 4)([2, -1, 0, 0, -1, 2, -2, 0, 0, -1, 2, -1, 0, 0, -1, 2])
-    
+    @test size(R,1) == 48
+    @test size("F4") = size(R)
 end
