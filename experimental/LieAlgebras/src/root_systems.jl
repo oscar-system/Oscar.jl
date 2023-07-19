daf92cabf5d533bff42508656e5c88a49997c205
Laura Voggesberger <laura.voggesberger@rub.de>
2023-07-19 15:40:12 +0200

    updated root_systems.jl

--- experimental/LieAlgebras/src/root_systems.jl
+++ experimental/LieAlgebras/src/root_systems.jl
@@ -31,7 +31,6 @@ number_of_roots(R::RootSystem) = size(R.roots)[1]
 
 @doc raw"""
   number_of_roots(S::String)
-
 Return the numbers of roots in the root system of type `S`
 """
 number_of_roots(S::Symbol, n::Int64) = number_of_roots(RootSystem(S, n))
