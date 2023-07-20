e7a88ca5c940a859b4bfd39619b6d3ec0447b605
Laura Voggesberger <101730185+voggesbe@users.noreply.github.com>
2023-07-20 10:25:44 +0200

    Update root_systems-test.jl

--- experimental/LieAlgebras/test/root_systems-test.jl
+++ experimental/LieAlgebras/test/root_systems-test.jl
@@ -2,5 +2,5 @@
     R = RootSystem("F4")
     @test cartan_matrix(R) == MatrixSpace(QQ, 4, 4)([2, -1, 0, 0, -1, 2, -2, 0, 0, -1, 2, -1, 0, 0, -1, 2])
     @test size(R,1) == 48
-    @test size("F4") = size(R)
+    @test size("F4") == size(R)
 end
