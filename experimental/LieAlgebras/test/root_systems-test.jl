c146b939d41c6dbd4dbb66b17b7dba3d93b06be4
Laura Voggesberger <101730185+voggesbe@users.noreply.github.com>
2023-07-20 10:27:53 +0200

    Update root_systems-test.jl

--- experimental/LieAlgebras/test/root_systems-test.jl
+++ experimental/LieAlgebras/test/root_systems-test.jl
@@ -11,11 +11,16 @@ end
     R = RootSystem("F4")
     @test cartan_matrix(R) == MatrixSpace(QQ, 4, 4)([2, -1, 0, 0, -1, 2, -2, 0, 0, -1, 2, -1, 0, 0, -1, 2])
 <<<<<<< HEAD
+<<<<<<< HEAD
     
 end
 >>>>>>> changes to LieAlgebras export, added tests
 =======
     @test size(R,1) == 48
     @test size("F4") == size(R)
+=======
+    @test number_of_roots(R) == 48
+    @test number_of_roots("F4") == number_of_roots(R)
+>>>>>>> Update root_systems-test.jl
 end
 >>>>>>> updated LieAlgebras.jl, root_systems.jl, simple_lie_algebra.jlm root_systems-test.jl, and runtests.jl
