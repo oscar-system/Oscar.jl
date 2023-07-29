c7e9c72173fc6e8a366b2afe50c7a6cd0a8cc0ac
ederc <ederc@mathematik.uni-kl.de>
2023-07-29 07:04:10 +0200

    Update test/Rings/MPolyQuo.jl
    Co-authored-by: Lars Göttgens <lars.goettgens@gmail.com>

--- test/Rings/MPolyQuo.jl
+++ test/Rings/MPolyQuo.jl
@@ -30,7 +30,7 @@
   f = A(y^2-y^4)
   @test simplify(f) == A(-y^3 + y^2 + 1)
 
-  A,p = quo(R, [x^3+y,y^2], ordering=lex(R))
+  A,p = quo(R, [x^3+y,y^2]; ordering=lex(R))
   @test A.I == ideal(R, [x^3+y, y^2])
 
   B,q = quo(R, x^3+y,y^2, ordering=lex(R))
