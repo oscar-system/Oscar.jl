39a857136489d715c8cbd06c781182e3b336901d
Laura Voggesberger <laura.voggesberger@rub.de>
2023-08-01 16:25:55 +0200

    changed to rebase to master

--- experimental/LieAlgebras/src/root_systems.jl
+++ experimental/LieAlgebras/src/root_systems.jl
@@ -2,26 +2,19 @@ mutable struct RootSystem
   roots::Vector{Vector{Int}}
   simple_roots::Vector{Vector{Int}}
   positive_roots::Vector{Vector{Int}}
-  root_system_type::Tuple{String, Int64}
-  @doc raw"""
-    RootSystem(S::String) -> RootSystem
-  Return the root system of type `S` where `S` is a string consisting out of
-  a letter `A`, `B`, `C`, `D`, `E`, `F`, `G` followed by an integer.
-  For the exceptional root system, the integers are fixed, e.g. `G2`, `F4`, `E6`, ...
-  """
-  function RootSystem(S::String)
-    # S is a string detailing the type of the indecomposable root system made out of a letter, e.g. "A", "B", "C",... and an integer for the number of simple roots
-    S = parse_root_string(S)
-    S1 = GAP.Obj(S[1])
-    RS = GAP.Globals.RootSystem(S1, S[2])
-    Ro_1 = GAP.Globals.PositiveRoots(RS)
-    Ro_2 = GAP.Globals.NegativeRoots(RS)
-    sR = GAP.Globals.SimpleSystem(RS)
-    sR = [[sR[i][j] for j=1:length(sR[i])] for i=1:length(sR)]
-    Ro1 = [[Ro_1[i][j] for j=1:length(Ro_1[i])] for i=1:length(Ro_1)]
-    Ro2 = [[Ro_2[i][j] for j=1:length(Ro_2[i])] for i=1:length(Ro_2)]
-    Ro = reduce(vcat, (Ro1, Ro2))
-    return new(Ro,sR,Ro1,S)
+  root_system_type::Tuple{Symbol, Int64}
+
+  function RootSystem(S::Symbol, n::Int64)
+    # S is a symbol detailing the type of the indecomposable root system 
+    # e.g. "A", "B", "C",... and n is an integer for the number of simple roots
+    S1 = GAP.Obj(S)
+    RS = GAP.Globals.RootSystem(S1, n)
+    sR = Vector{Vector{Int}}(GAP.Globals.SimpleSystem(RS))
+    Ro1 = Vector{Vector{Int}}(GAP.Globals.PositiveRoots(RS))
+    Ro2 = Vector{Vector{Int}}(GAP.Globals.NegativeRoots(RS))
+    Ro = vcat(Ro1, Ro2)
+    t = (S, n)
+    return new(Ro, sR, Ro1, t)
   end
 end
 
@@ -30,26 +23,33 @@ end
 #   Basic manipulation
 #
 ###############################################################################
- @doc raw"""
-   number_of_roots(R::RootSystem)
+@doc raw"""
+  number_of_roots(R::RootSystem)
 
- Return the numbers of roots in the root system `R`
-  """
+Return the numbers of roots in the root system `R`
+"""
 number_of_roots(R::RootSystem) = size(R.roots)[1]
 
 @doc raw"""
-   number_of_roots(S::String)
+  number_of_roots(S::String)
 
 Return the numbers of roots in the root system of type `S`
 """
-number_of_roots(S::String) = number_of_roots(RootSystem(S))
+number_of_roots(S::Symbol, n::Int64) = number_of_roots(RootSystem(S, n))
 
 @doc raw"""
-   getindex(R::RootSystem, r::Int)
+  getindex(R::RootSystem, r::Int)
 
-Return the `r`=th root in the vector of roots in the root system `R`.
+Return the `r`-th root of the root system `R`.
 """
 getindex(R::RootSystem, r::Int) = getindex(R.roots, r)
+
+@doc raw"""
+  root_system_type(R::RootSystem)
+
+Return the Dynkin type of the root system `R`.
+"""
+root_system_type(R::RootSystem) = R.root_system_type
 
 ###############################################################################
 #
@@ -59,9 +59,45 @@ getindex(R::RootSystem, r::Int) = getindex(R.roots, r)
 
 function show(io::IO, R::RootSystem)
   print(io, "Root system of type ")
-  show(io, R.root_system_type[1] * string(R.root_system_type[2]))
+  show(io, string(R.root_system_type[1]) * string(R.root_system_type[2]))
 end
 
+###############################################################################
+#
+#   Comparison
+#
+###############################################################################
+
+function Base.:(==)(R1::RootSystem, R2::RootSystem)
+  return R1.roots == R2.roots &&
+         R1.root_system_type != R2.root_system_type
+end
+
+function Base.hash(R::RootSystem, h::UInt)
+  b = 0x9d96557cb5f07773 % UInt
+  h = hash(R.positive_roots, h)
+  h = hash(R.root_system_type, h)
+  h = hash(R.roots, h)
+  h = hash(R.simple_roots, h)
+  return xor(h, b)
+end
+###############################################################################
+#
+#   Constructor
+#
+###############################################################################
+@doc raw"""
+  root_system(S::String) -> root_system
+Return the root system of type `S` where `S` is a string consisting out of
+a letter `A`, `B`, `C`, `D`, `E`, `F`, `G` followed by an integer.
+For the exceptional root system, the integers are fixed, e.g. `G2`, `F4`, `E6`, ...
+"""
+function root_system(S::Symbol, n::Int64)
+  @req S in [:A, :B, :C, :D, :E, :F, :G] "Unknown Dynkin type"
+  # S is a symbol detailing the type of the indecomposable root system 
+  # e.g. "A", "B", "C",... and n is an integer for the number of simple roots
+  return RootSystem(S,n)
+end
 ###############################################################################
 #
 #   further functions
@@ -72,10 +108,10 @@ end
 
  Return the Cartan matrix of the type of root system specified by `S`
   """
-function cartan_matrix(S::String)
-  S = parse_root_string(S)
-  S1 = GAP.Obj(S[1])
-  RS = GAP.Globals.RootSystem(S1, S[2])
+function cartan_matrix(S::Symbol, n::Int64)
+  @req S in [:A, :B, :C, :D, :E, :F, :G] "Unknown Dynkin type"
+  S1 = GAP.Obj(S)
+  RS = GAP.Globals.RootSystem(S1, n)
   CG = GAP.Globals.CartanMatrix(RS)
   C = matrix(QQ, CG)
   return C
@@ -88,8 +124,10 @@ end
   """
 function cartan_matrix(R::RootSystem)
   S = R.root_system_type
-  S2 = S[1] * string(S[2])
-  C = cartan_matrix(S2)
+  S1 = GAP.Obj(S[1])
+  RS = GAP.Globals.RootSystem(S1, S[2])
+  CG = GAP.Globals.CartanMatrix(RS)
+  C = matrix(QQ, CG)
   return C
 end
  
@@ -98,61 +136,84 @@ end
 
  Return the Dynkin diagram of the type of root system specified by `S`
   """
-function dynkin_diagram(S::String)
-  S = parse_root_string(S)
-  S1 = S[1]
-	n = S[2]
-	D = ""
-	if S1 == "A"
+function dynkin_diagram(S::Symbol, n::Int64)
+  @req S in [:A, :B, :C, :D, :E, :F, :G] "Unknown Dynkin type"
+   D = ""
+  
+  if S == :A
+  
     for i = 1:(n-1)
-       D = D * string(i) * " - "
-		end
-    D = D * string(n)
-	elseif S1 == "B"
-	  for i = 1:(n-2)
-		  D = D * string(i) * " - "
-		end
-		D = D * string(n-1) * " >=> " * string(n)
-	elseif S1 == "C"
-	  for i = 1:(n-2)
       D = D * string(i) * " - "
-		end
-		D = D * string(n-1) * " <=< " * string(n)
-	elseif S1 == "D"
-		for i = 1:4*n-10
-			D = D * " "
-		end
-		D = D * string(n-1) * "\n"
-		for i = 1:4*n-11
-			D = D * " "
-		end
-		D = D * "/\n"
-		for i = 1:n-3
-			D = D * string(i) * " - "
-		end
-		D = D * string(n-2) * "\n"
-		for i = 1:4*n-12
-			D = D * " "
-		end
-		D = D * " \\ \n"
-		for i = 1:4*n-10
-			D = D * " "
-		end
-		D = D * string(n)
-	elseif S1 == "E"
-		if n == 6
-			D = "1 - 3 - 4 - 5 - 6\n        |\n        2"
-		elseif n == 7
-		  D = "1 - 3 - 4 - 5 - 6 - 7\n        |\n        2"
-		elseif n == 8
-			D = "1 - 3 - 4 - 5 - 6 - 7 - 8\n        |\n        2"
-		end
-	elseif S1 == "F"
-		D = "1 - 2 >=> 3 - 4"
-	elseif S1 == "G"
-		D = "1 >>> 2"
-	end
-	print(D)
+    end
+    D = D * string(n)
+    
+  elseif S == :B
+  
+    for i = 1:(n-2)
+      D = D * string(i) * " - "
+    end
+    D = D * string(n-1) * " >=> " * string(n)
+    
+  elseif S == :C
+  
+    for i = 1:(n-2)
+      D = D * string(i) * " - "
+    end
+    D = D * string(n-1) * " <=< " * string(n)
+    
+  elseif S == :D
+    if n >= 4
+      for i = 1:4*n-10
+        D = D * " "
+      end
+      D = D * string(n-1) * "\n"
+      for i = 1:4*n-11
+        D = D * " "
+      end
+      D = D * "/\n"
+      for i = 1:n-3
+        D = D * string(i) * " - "
+      end
+      D = D * string(n-2) * "\n"
+      for i = 1:4*n-12
+        D = D * " "
+      end
+      D = D * " \\ \n"
+      for i = 1:4*n-10
+        D = D * " "
+      end
+      D = D * string(n)
+    else
+      error("This root system doesn't exist.")
+    end
+    
+  elseif S == :E
+    if n == 6
+      D = "1 - 3 - 4 - 5 - 6\n        |\n        2"
+    elseif n == 7
+      D = "1 - 3 - 4 - 5 - 6 - 7\n        |\n        2"
+    elseif n == 8
+      D = "1 - 3 - 4 - 5 - 6 - 7 - 8\n        |\n        2"
+    else
+      error("This root system doesn't exist.")
+    end
+  
+  elseif S == :F
+    if n == 4
+      D = "1 - 2 >=> 3 - 4"
+    else
+      error("This root system doesn't exist.")
+    end
+  elseif S == :G
+    if n == 2
+      D = "1 >>> 2"
+    else
+      error("This root system doesn't exist.")
+    end
+  else
+    error("This root system doesn't exist.")
+  end
+  print(D)
 end
 @doc raw"""
    dynkin_diagram(R::RootSystem)
@@ -161,16 +222,6 @@ end
   """
 function dynkin_diagram(R::RootSystem)
 	S = R.root_system_type
-  S2 = S[1] * string(S[2])
-	dynkin_diagram(S2)
+	return dynkin_diagram(S[1],S[2])
 end
 
-###############################################################################
-#
-#   helper functions
-#
-###############################################################################
-function parse_root_string(S::String)
-  n = parse(Int64, S[2:end])
-  return (S[1:1], n)  
-end
--- experimental/LieAlgebras/src/simple_lie_algebra.jl
+++ experimental/LieAlgebras/src/simple_lie_algebra.jl
@@ -1,36 +1,53 @@
 @attributes mutable struct SimpleLieAlgebra{C<:RingElement} <: LieAlgebra{C}
 #Construct a simple Lie algebra over a given ring with a given root system
-  mat_space::MatSpace
   base_ring::Ring
   root_system::RootSystem
   dim::Int
   s::Vector{Symbol}
-  root_type::Tuple{String,Int64}
+  root_type::Tuple{Symbol,Int64}
+  struct_consts::Matrix{SRow{C}}
 
-  function SimpleLieAlgebra{C}(R::Ring, S::String, cached::Bool) where {C<:RingElement}
-    RS = RootSystem(S)
-    di = number_of_roots(RS) + length(RS.simple_roots)
-    M = MatrixSpace(R, 1, di)
-    s = [Symbol("e_$i") for i in 1:di]
-    st=RS.root_system_type
-    return get_cached!(SimpleLieAlgebraDict, (R, RS, M), cached) do
-    new{C}(M,R,RS,di,s,st)
+  function SimpleLieAlgebra{C}(
+    R::Ring, S::Symbol, n::Int64; cached::Bool=true
+  ) where {C<:RingElement}
+    RS = RootSystem(S,n)
+    dimL = number_of_roots(RS) + length(RS.simple_roots)
+    s = [Symbol("e_$i") for i in 1:dimL]
+    st = root_system_type(RS)
+    #get the structure constants of the Lie algebra L 
+    #note that it is enoough to do this over Q, as we can later coerce the constants
+    #into the ring R
+    coeffs_iso = inv(Oscar.iso_oscar_gap(QQ))
+    LG = GAP.Globals.SimpleLieAlgebra(
+      GAP.Obj(S), n, domain(coeffs_iso)
+    )
+    sc_table_G =
+      (
+        entry -> (entry[1], Vector{elem_type(RO)}(map(coeffs_iso, entry[2])))
+      ).(
+        Matrix{Tuple{Vector{Int},Vector{GAP.Obj}}}(
+          (GAP.Globals.StructureConstantsTable(GAPWrap.Basis(LG)))[1:dimL]
+        )
+      )
+    struct_consts = Matrix{SRow{elem_type(RO)}}(undef, dimL, dimL)
+    for i in 1:dimL, j in 1:dimL
+      struct_consts[i, j] = sparse_row(
+        RO, Tuple{Int,elem_type(R)}[(k, R(c)) for (k, c) in zip(sc_table_G[i, j]...)]
+      )
+    end
+    return get_cached!(SimpleLieAlgebraDict, (R, RS), cached) do
+    new{C}(R, RS, dimL, s, st, struct_consts)
     end::SimpleLieAlgebra{C}
   end
 end
 
 const SimpleLieAlgebraDict = CacheDictType{
-  Tuple{Ring,RootSystem,MatSpace},SimpleLieAlgebra
+  Tuple{Ring,RootSystem},SimpleLieAlgebra
 }()
 
 mutable struct SimpleLieAlgebraElem{C<:RingElement} <: LieAlgebraElem{C}
   parent::SimpleLieAlgebra{C}
   mat::MatElem{C}
-  function SimpleLieAlgebraElem{C}(
-  L::SimpleLieAlgebra{C},
-  M::MatElem{C}) where {C<:RingElement}
-     return new{C}(L,M)
-  end
 end
 
 ###############################################################################
@@ -45,8 +62,6 @@ elem_type(::Type{SimpleLieAlgebra{C}}) where {C<:RingElement} = SimpleLieAlgebra
 
 parent(x::SimpleLieAlgebraElem{C}) where {C<:RingElement} = x.parent
 
-base_ring(x::SimpleLieAlgebraElem{C}) where {C<:RingElement} = x.parent.base_ring
-
 base_ring(L::SimpleLieAlgebra{C}) where {C<:RingElement} = L.base_ring::parent_type(C)
 
 dim(L::SimpleLieAlgebra{C}) where {C<:RingElement} = L.dim
@@ -55,22 +70,10 @@ root_system(L::SimpleLieAlgebra{C}) where {C<:RingElement} = L.root_system
 
 root_type(L::SimpleLieAlgebra{C}) where {C<:RingElement} = L.root_type
 
-matrix(f::SimpleLieAlgebraElem) = f.mat
-
-@doc raw"""
-    characteristic(L::SimpleLieAlgebra{T}) -> Int64
-
-Return the characteristic of the base ring of the Lie algebra `L`.
-"""
-characteristic(L::SimpleLieAlgebra{T}) where T <: RingElement = characteristic(base_ring(L))
-
-# linear indexing 
-getindex(f::SimpleLieAlgebraElem, r::Int, c::Int) = f.mat[r, c]
-
-Base.@propagate_inbounds function Base.setindex!(f::SimpleLieAlgebraElem{T}, d, r) where {T <: RingElem}
-	matrix(f)[1, r]=d
-	return f
-end
+#  Base.@propagate_inbounds function Base.setindex!(f::SimpleLieAlgebraElem{T}, d, r) where {T <: RingElem}
+#    Generic._matrix(f)[1, r] = d
+#    return f
+#  end
 
 function symbols(L::SimpleLieAlgebra) 
   return L.s
@@ -81,13 +84,32 @@ end
 #   String I/O
 #
 ###############################################################################
-
-function Base.show(io::IO, V::SimpleLieAlgebra)
-  print(io, "Simple Lie Algebra over ")
-  print(IOContext(io, :compact => true), base_ring(V))
+function Base.show(io::IO, ::MIME"text/plain", L::SimpleLieAlgebra)
+  io = pretty(io)
+  println(io, "Simple Lie algebra")
+  println(
+    io, Indent(), "of type $(string(root_type(L)[1])*string(root_type(L)[2]))", Dedent()
+  )
+  println(io, Indent(), "of dimension $(dim(L))", Dedent())
+  print(io, "over ")
+  print(io, Lowercase(), coefficient_ring(L))
 end
 
-show(io::IO, x::SimpleLieAlgebraElem) = show(io, matrix(x))
+function Base.show(io::IO, L::SimpleLieAlgebra)
+  if get(io, :supercompact, false)
+    print(io, "Simple Lie algebra")
+  else
+    io = pretty(io)
+    print(io, "Simple Lie algebra over ", Lowercase())
+    print(IOContext(io, :supercompact => true), coefficient_ring(L))
+  end
+end
+
+# function Base.show(io::IO, V::SimpleLieAlgebra)
+#   print(io, "Simple Lie Algebra over ")
+#   print(IOContext(io, :compact => true), base_ring(V))
+# end
+
 
 ###############################################################################
 #
@@ -95,45 +117,19 @@ show(io::IO, x::SimpleLieAlgebraElem) = show(io, matrix(x))
 #
 ###############################################################################
 
-# Unary operations
-
-function -(f::SimpleLieAlgebraElem)
-  R = parent(f)
-  return R(-matrix(f))
-end
-
 # Binary operations
 
-function +(f::SimpleLieAlgebraElem{T}, g::SimpleLieAlgebraElem{T}) where T <: RingElement
-  parent(f) != parent(g) && error("Incompatible Lie algebras")
-  R = parent(f)
-  return R(matrix(f) + matrix(g))
-end
-
-function -(f::SimpleLieAlgebraElem{T}, g::SimpleLieAlgebraElem{T}) where T <: RingElement
-  parent(f) != parent(g) && error("Incompatible Lie algebras")
-  R = parent(f)
-  return R(matrix(f) - matrix(g))
-end
-
-*(x::RingElem, f::SimpleLieAlgebraElem{T}) where T <: RingElement = parent(f)(x*f.mat)
-
-function *(f::SimpleLieAlgebraElem{T}, g::SimpleLieAlgebraElem{T}) where T <: RingElement
-  parent(f) != parent(g) && error("Incompatible Lie algebras")
-  L = parent(f)
-  ad = adjoint_matrix(L)
-  r = L()
-  for i = 1:ncols(matrix(f))
-    for j = 1:ncols(matrix(g))
-      r = r+f[i]*g[j]*L(transpose(ad[i][:, j]))
-   	end
-  end
-  return r
-end
-
-function bracket(x::SimpleLieAlgebraElem{C}, y::SimpleLieAlgebraElem{C}) where {C<:RingElement}
+function bracket(
+  x::SimpleLieAlgebraElem{C},
+  y::SimpleLieAlgebraElem{C},
+) where {C<:RingElement}
   check_parent(x, y)
-  return x*y
+  L = parent(x)
+  mat = sum(
+    cxi * cyj * L.struct_consts[i, j] for (i, cxi) in enumerate(coefficients(x)),
+    (j, cyj) in enumerate(coefficients(y))
+  )
+  return L(mat)
 end
 
 ###############################################################################
@@ -143,13 +139,19 @@ end
 ###############################################################################
 
 function ==(L::SimpleLieAlgebra{T}, M::SimpleLieAlgebra{T}) where T <: RingElement
-  (L.root_system).root_system_type != (M.root_system).root_system_type && error("Incompatible Root systems")
-  return L.mat_space == M.mat_space
+  return base_ring(L) == base_ring(M) &&
+         dim(L) == dim(M) &&
+         root_system(L) == root_system(M)
 end
 
-function ==(f::SimpleLieAlgebraElem{T}, g::SimpleLieAlgebraElem{T}) where T <: RingElement
-  parent(f) != parent(g) && error("Incompatible Lie algebras")
-  return matrix(f) == matrix(g)
+function Base.hash(x::SimpleLieAlgebra{C}, h::UInt) where {C<:RingElement}
+  b = 0x9072419641dbc7d0 % UInt
+  h = hash(base_ring(L), h)
+  h = hash(dim(L), h)
+  h = hash(mat_space(L),h)
+  h = hash(root_system(L),h)
+  h = hash(root_type(L),h)
+  return xor(h, b)
 end
 ###############################################################################
 #
@@ -158,33 +160,15 @@ end
 ###############################################################################
 
 @doc raw"""
-    zero(L::SimpleLieAlgebra{T}) -> SimpleLieAlgebraElem{T}
-
-Return the zero element of the Lie algebra `L`.
-"""
-function zero(L::SimpleLieAlgebra{T}) where {T <: RingElement} 
-  x = zero(L.mat_space)
-	f = SimpleLieAlgebraElem{T}(L, x)
-	return f
-end
-
-function (L::SimpleLieAlgebra{T})() where {T <: RingElement}
-  return zero(L)
-end
-
-function (L::SimpleLieAlgebra{T})(A::MatElem{T}) where {T <: RingElem}
-  f = SimpleLieAlgebraElem{T}(L, L.mat_space(A))
-  return f
-end
-
-@doc raw"""
     lie_algebra(R::Ring, S::String, cached::Bool=true) -> SimpleLieAlgebra{elem_type(R)}
 
 Construct the simple Lie algebra over the ring `R` with root system of type `S`
+The internally used basis of this Lie algebra is the Chevalley basis.
 """
-function lie_algebra(R::Ring, S::String, cached::Bool=true)
+function lie_algebra(R::Ring, S::Symbol, n::Int64; cached::Bool=true)
+  @req S in [:A, :B, :C, :D, :E, :F, :G] "Unknown Dynkin type"
   T = elem_type(R)
-  return SimpleLieAlgebra{T}(R, S, cached)
+  return SimpleLieAlgebra{T}(R, S, n; cached)
 end
 
 ###############################################################################
@@ -196,72 +180,90 @@ end
 @doc raw"""
     chevalley_basis(L::SimpleLieAlgebra{T}) -> Vector{Vector{SimpleLieAlgebraElem{T}}}
 
-Give the Chevalley basis of the simple Lie algebra `L`
+Give the Chevalley basis of the simple Lie algebra `L` in three vectors, stating first the positive root vectors, 
+then the negative root vectors and finally the basis of the Cartan subalgebra. The order of root vectors corresponds
+to the order or the roots in the root system.
 """
 function chevalley_basis(L::SimpleLieAlgebra)
-	RS = root_system(L)
-	Rp = RS.positive_roots
-	sR = RS.simple_roots
-	
-	n = length(Rp)
-	m = length(sR)
-	#root vectors
-	e1 = elem_type(L)[]
-	e2 = elem_type(L)[]
-	for i = 1:n
-		z1 = L()
-		z1[i] = 1
-		z2 = L()
-		z2[i+n] = 1
-		e1 = reduce(vcat, (e1, z1))
-		e2 = reduce(vcat, (e2, z2))
-	end
-	#basis for cartan algebra
-	e3 = elem_type(L)[]
-	for i = 1:m
-		z = L()
-		z[i+2*n] = 1
-		e3 = reduce(vcat, (e3, z))
-	end
-	return [e1, e2, e3]
+  RS = root_system(L)
+  Rp = RS.positive_roots
+  sR = RS.simple_roots
+  n = length(Rp)
+  m = length(sR)
+  B = basis(L)
+  #root vectors
+  r_plus = [B[i] for i = 1:n]
+  r_minus = [B[i] for i = (n + 1):(2 * n)]
+  #basis for cartan algebra
+  h = [B[i] for i = (2 * n + 1): dim(L)]
+  return [r_plus, r_minus, h]
 end 
 
 @doc raw"""
-    adjoint_matrix(L::SimpleLieAlgebra{T}) -> Vector{Any}
+    adjoint_matrix(x::SimpleLieAlgebraElem{T}) -> MatSpaceElem{T}
+
+Give the adjoint matrix of the Lie algebra element `x` with respect 
+to the Chevalley basis.
+The adjoint matrix is the matrix of the adjoint representation of the element x 
+w.r.t. the Chevalley basis B. The adjoint map is the left multiplication by x. 
+The j-th column of the resulting matrix represents the image of the the j-th basis vector of B 
+under left multiplication by x. 
+"""
+function adjoint_matrix(x::SimpleLieAlgebraElem{T}) where {T<:RingElement} 
+  #computes the adjoint matrix of the element x with respect to the chevalley basis.
+  L = parent(x)
+  #compute x acting on the basis vectors
+  Ad = zero_matrix(base_ring(x), dim(L), dim(L))
+  for i in 1:dim(L)
+    Ad[:,i]=coefficients(bracket(x,basis(L,i)))
+  end
+  return Ad
+end
+
+@doc raw"""
+    adjoint_matrix(x::SimpleLieAlgebraElem{T}) -> MatSpaceElem{T}
+
+Give the adjoint matrix of the Lie algebra element `x` with respect 
+to the basis `B`. Note that we expect the elements in `B` to be given with coefficients w.r.t 
+the Chevalley basis but the coefficents of `x` are given w.r.t B.
+The adjoint matrix is the matrix of the adjoint representation of the element x 
+w.r.t. the `B`. The adjoint map is the left multiplication by x. 
+The j-th column of the resulting matrix represents the image of the the j-th basis vector of B 
+under left multiplication by x. 
+"""
+function adjoint_matrix(
+  x::SimpleLieAlgebraElem{T}, B::Vector{SimpleLieAlgebraElem{T}}
+) where {T<:RingElement}
+  R = base_ring(x)
+  #compute the base change matrix from the Chevalley basis to B 
+  M_B = matrix(R, [[B[i][j] for j in 1:length(Generic._matrix(B[i]))] for i in 1:length(B)])
+  iM_B = inv(M_B)
+  #transform x to coordinates over Chevalley basis
+  y = L(Generic._matrix(x) * M_B)
+  ad_ch = adjoint_matrix(y)
+  ad = iM_B * ad_ch * M_B
+  return ad
+end
+
+@doc raw"""
+    adjoint_matrix(L::SimpleLieAlgebra{T}) -> Vector{MatSpaceElem{T}}
 
 Give the adjoint matrices of all basis vectors acting on the Lie algebra `L` with respect 
-to the Chevalley basis of `L`
+to the Chevalley basis of `L`.
+The i-th adjoint matrix is the matrix of the adjoint representation of the i-th basis element x 
+w.r.t. the Chevalley basis B. The adjoint map is the left multiplication by x. 
+The j-th column of the resulting matrix represents the image of the the j-th basis vector of B 
+under left multiplication by x. 
 """
 function adjoint_matrix(L::SimpleLieAlgebra{T}) where T <: RingElement #computes the adjoint matrix with respect to the Chevalley basis.
-	St = L.root_type
-	R = base_ring(L)	
-	n = St[2]
-	
-	Q = GAP.Globals.Rationals
-	S = GAP.Obj(St[1])
-	LG = GAP.Globals.SimpleLieAlgebra(S, n, Q) #define the Lie algebra in Gap over the rationals, we are interested in the structure constants
-  #they remain the same over rings of characteristic 0, otherwise we have the structure constants mod char(R)
-	d = GAP.Globals.Dimension(LG)
-	
-	ch = GAP.Globals.ChevalleyBasis(LG)
-	ch1 = GAP.Globals.ShallowCopy(ch[1])
-	ch2 = GAP.Globals.ShallowCopy(ch[2])
-	ch3 = GAP.Globals.ShallowCopy(ch[3])
-	ch = ch1
-	GAP.Globals.Append(ch, ch2)
-	GAP.Globals.Append(ch, ch3)
-	BL = GAP.Globals.Basis(LG, ch)
-	ad = dense_matrix_type(R)[]
-	for i = 1:d
-		Ad = GAP.Globals.AdjointMatrix(BL, BL[i])
-		A = [[Ad[i][j] for j = 1:length(Ad[i])] for i = 1:length(Ad)]
-		MA = zero_matrix(R, d, d)
-		for j = 1:d
-			MA[j, 1:d] = A[j]
-		end
-		push!(ad, MA)
-		
-	end 
-	return ad
+  R = base_ring(L)
+  ad = dense_matrix_type(R)[]
+  d = dim(L)
+  B = basis(L)
+  for i = 1:d
+    Ad = adjoint_matrix(B[i])
+    push!(ad, Ad)
+  end 
+  return ad
 end
 
