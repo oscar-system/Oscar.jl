f9d29c79e8f2b20fd3932d84125a0395d6e2e31f
Laura Voggesberger <101730185+voggesbe@users.noreply.github.com>
2023-08-02 10:19:28 +0200

    Update root_systems-test.jl

--- experimental/LieAlgebras/test/root_systems-test.jl
+++ experimental/LieAlgebras/test/root_systems-test.jl
@@ -6,7 +6,7 @@
   @test cartan_matrix(R) ==
     matrix(QQ, 4, 4, [2, -1, 0, 0, -1, 2, -2, 0, 0, -1, 2, -1, 0, 0, -1, 2])
     @test number_of_roots(R) == 48
-    @test number_of_roots("F4") == number_of_roots(R)
+    @test number_of_roots(:F,4) == number_of_roots(R)
 end
 
 =======
