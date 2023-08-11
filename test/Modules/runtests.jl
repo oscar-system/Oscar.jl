17a640ed6aa57a19eaf170f9c34884226afc21b5
John Abbott <John.Abbott@rptu.de>
2023-08-11 14:09:14 +0200

    Added new test hilbert.jl

--- test/Modules/runtests.jl
+++ test/Modules/runtests.jl
@@ -4,6 +4,7 @@ using Test
 include("UngradedModules.jl")
 include("FreeModElem-orderings.jl")
 include("ModulesGraded.jl")
+include("hilbert.jl")
 include("module-localizations.jl")
 include("local_rings.jl")
 include("MPolyQuo.jl")
