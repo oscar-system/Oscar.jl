64386a10e81080271e35ed26491ba393d3cdf6f5
Martin Bies <MBies87@googlemail.com>
2023-08-16 01:00:52 +0200

    [FTheoryTools] Organize code more efficiently

--- experimental/FTheoryTools/src/TateModels/constructors.jl
+++ experimental/FTheoryTools/src/TateModels/constructors.jl
@@ -222,67 +222,20 @@ function global_tate_model(auxiliary_base_ring::MPolyRing, auxiliary_base_gradin
   
   # Construct the model
   if toric_sample
-    return _construct_toric_sample(auxiliary_base_grading, gens_base_names, d, ais)
+    (S, auxiliary_base_space, auxiliary_ambient_space) = _construct_toric_sample(auxiliary_base_grading, gens_base_names, d)
+    R = cox_ring(auxiliary_ambient_space)
+  else
+    (S, auxiliary_base_space, auxiliary_ambient_space) = _construct_generic_sample(auxiliary_base_grading, gens_base_names, d)
+    R = coordinate_ring(auxiliary_ambient_space)
   end
-  return _construct_generic_sample(auxiliary_base_grading, gens_base_names, d, ais)
-end
-
-
-function _construct_toric_sample(auxiliary_base_grading::Matrix{Int64}, base_vars::Vector{String}, d::Int, ais::Vector{T}) where {T<:MPolyRingElem}
-  # convert Tate sections into polynomials of the auxiliary base
-  auxiliary_base_space = _auxiliary_base_space(base_vars, auxiliary_base_grading, d)
-  S = cox_ring(auxiliary_base_space)
   ring_map = hom(parent(ais[1]), S, gens(S)[1:ngens(parent(ais[1]))])
   (a1, a2, a3, a4, a6) = [ring_map(k) for k in ais]
-  
-  # construct ambient space
-  fiber_ambient_space = weighted_projective_space(NormalToricVariety, [2,3,1])
-  set_coordinate_names(fiber_ambient_space, ["x", "y", "z"])
-  D1 = [0 for i in 1:rank(class_group(auxiliary_base_space))]
-  D1[1] = 2
-  D1 = toric_divisor_class(auxiliary_base_space, D1)
-  D2 = [0 for i in 1:rank(class_group(auxiliary_base_space))]
-  D2[1] = 3
-  D2 = toric_divisor_class(auxiliary_base_space, D2)
-  auxiliary_ambient_space = _ambient_space(auxiliary_base_space, fiber_ambient_space, D1, D2)
-  
-  # construct the model
-  pt = _tate_polynomial([a1, a2, a3, a4, a6], cox_ring(auxiliary_ambient_space))
-  model = GlobalTateModel(a1, a2, a3, a4, a6, pt, toric_covered_scheme(auxiliary_base_space), toric_covered_scheme(auxiliary_ambient_space))
+  pt = _tate_polynomial([a1, a2, a3, a4, a6], R)
+  model = GlobalTateModel(a1, a2, a3, a4, a6, pt, auxiliary_base_space, auxiliary_ambient_space)
   set_attribute!(model, :base_fully_specified, false)
   return model
 end
 
-
-function _construct_generic_sample(base_grading::Matrix{Int64}, base_vars::Vector{String}, d::Int, ais::Vector{T}) where {T<:MPolyRingElem}
-  # Convert Tate sections into polynomials of the auxiliary base
-  base_space = family_of_spaces(PolynomialRing(QQ, base_vars, cached = false)[1], base_grading, d)
-  S = coordinate_ring(base_space)
-  ring_map = hom(parent(ais[1]), S, gens(S)[1:ngens(parent(ais[1]))])
-  (a1, a2, a3, a4, a6) = [ring_map(k) for k in ais]
-  
-  # Construct ambient space
-  ambient_space_vars = vcat(base_vars, ["x", "y", "z"])
-  coordinate_ring_ambient_space = PolynomialRing(QQ, ambient_space_vars, cached = false)[1]
-  ambient_space_grading = zero_matrix(Int, nrows(base_grading)+1,ncols(base_grading)+3)
-  for i in 1:nrows(base_grading)
-    for j in 1:ncols(base_grading)
-      ambient_space_grading[i,j] = base_grading[i,j]
-    end
-  end
-  ambient_space_grading[1,ncols(base_grading)+1] = 2
-  ambient_space_grading[1,ncols(base_grading)+2] = 3
-  ambient_space_grading[nrows(base_grading) + 1,ncols(base_grading) + 1] = 2
-  ambient_space_grading[nrows(base_grading) + 1,ncols(base_grading) + 2] = 3
-  ambient_space_grading[nrows(base_grading) + 1,ncols(base_grading) + 3] = 1
-  ambient_space = family_of_spaces(coordinate_ring_ambient_space, ambient_space_grading, d+2)
-  
-  # Construct the model
-  pt = _tate_polynomial([a1, a2, a3, a4, a6], coordinate_ring(ambient_space))
-  model = GlobalTateModel(a1, a2, a3, a4, a6, pt, base_space, ambient_space)
-  set_attribute!(model, :base_fully_specified, false)
-  return model
-end
 
 
 ################################################
