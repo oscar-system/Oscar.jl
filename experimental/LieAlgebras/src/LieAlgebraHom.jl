efea629514dbdb63606dd2ba5bc29dd6db783bc9
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2023-08-18 16:05:30 +0200

    LieAlgebras: Add docs for LieAlgebraHom

--- experimental/LieAlgebras/src/LieAlgebraHom.jl
+++ experimental/LieAlgebras/src/LieAlgebraHom.jl
@@ -38,6 +38,13 @@ end
 #
 ###############################################################################
 
+@doc raw"""
+    matrix(h::LieAlgebraHom) -> MatElem
+    
+Return the transformation matrix of `h` w.r.t. the bases of the domain and codomain.
+
+Note: The matrix operates on the coefficient vectors from the right.
+"""
 function matrix(h::LieAlgebraHom{<:LieAlgebra,<:LieAlgebra{C2}}) where {C2<:RingElement}
   return (h.matrix)::dense_matrix_type(C2)
 end
@@ -73,25 +80,50 @@ end
 #
 ###############################################################################
 
+@doc raw"""
+    image(h::LieAlgebraHom, x::LieAlgebraElem) -> LieAlgebraElem
+
+Return the image of `x` under `h`.
+"""
 function image(h::LieAlgebraHom, x::LieAlgebraElem)
   @req parent(x) === domain(h) "Domain mismatch"
   return codomain(h)(_matrix(x) * h.matrix)
 end
 
+@doc raw"""
+    image(h::LieAlgebraHom) -> LieSubalgebra
+
+Return the image of `h` as a Lie subalgebra of the codomain.
+"""
 function image(h::LieAlgebraHom)
   return sub(codomain(h), [image(h, x) for x in basis(domain(h))])
 end
 
+@doc raw"""
+    image(h::LieAlgebraHom, I::LieAlgebraIdeal) -> LieSubalgebra
+
+Return the image of `I` under `h` as a Lie subalgebra of the codomain.
+"""
 function image(h::LieAlgebraHom, I::LieAlgebraIdeal)
   @req base_lie_algebra(I) === domain(h) "Domain mismatch"
   return sub(codomain(h), [image(h, x) for x in basis(I)])
 end
 
+@doc raw"""
+    image(h::LieAlgebraHom, S::LieSubalgebra) -> LieSubalgebra
+
+Return the image of `S` under `h` as a Lie subalgebra of the codomain.
+"""
 function image(h::LieAlgebraHom, S::LieSubalgebra)
   @req base_lie_algebra(S) === domain(h) "Domain mismatch"
   return sub(codomain(h), [image(h, x) for x in basis(S)])
 end
 
+@doc raw"""
+    kernel(h::LieAlgebraHom) -> LieAlgebraIdeal
+
+Return the kernel of `h` as an ideal of the domain.
+"""
 function kernel(h::LieAlgebraHom)
   ker_dim, ker_b = left_kernel(matrix(h))
   return ideal(domain(h), [domain(h)(ker_b[i, :]) for i in 1:ker_dim])
@@ -103,6 +135,13 @@ end
 #
 ###############################################################################
 
+@doc raw"""
+    compose(f::LieAlgebraHom, g::LieAlgebraHom) -> LieAlgebraHom
+
+Return the composition of `f` and `g`, i.e. the homomorphism `h` such that
+`h(x) = g(f(x))` for all `x` in the domain of `f`.
+The codomain of `f` must be identical to the domain of `g`.
+"""
 function compose(
   f::LieAlgebraHom{T1,T2}, g::LieAlgebraHom{T2,T3}
 ) where {T1<:LieAlgebra,T2<:LieAlgebra,T3<:LieAlgebra}
@@ -119,11 +158,24 @@ function compose(
   return h
 end
 
+@doc raw"""
+    inv(h::LieAlgebraHom) -> LieAlgebraHom
+
+Return the inverse of `h`.
+Requires `h` to be an isomorphism.
+"""
 function inv(h::LieAlgebraHom)
   @req is_isomorphism(h) "Homomorphism must be invertible"
   return h.inverse_isomorphism
 end
 
+@doc raw"""
+    is_isomorphism(h::LieAlgebraHom) -> Bool
+
+Return `true` if `h` is an isomorphism.
+This function tries to invert the transformation matrix of `h` and caches the result.
+The inverse isomorphism can be cheaply accessed via `inv(h)` after calling this function.
+"""
 function is_isomorphism(h::LieAlgebraHom)
   isdefined(h, :inverse_isomorphism) && return true
   fl, invmat = is_invertible_with_inverse(h.matrix)
@@ -139,18 +191,88 @@ end
 #
 ###############################################################################
 
+@doc raw"""
+    hom(L1::LieAlgebra, L2::LieAlgebra, imgs::Vector{<:LieAlgebraElem}; check::Bool=true) -> LieAlgebraHom
+
+Construct the homomorphism from `L1` to `L2` by sending the `i`-th basis element of `L1` 
+to `imgs[i]` and extending linearly.
+All elements of `imgs` must lie in `L2`.
+
+By setting `check=false`, the linear map is not checked to be compatible with the Lie bracket.
+
+# Examples
+```jldoctest
+julia> L1 = special_linear_lie_algebra(QQ, 2);
+
+julia> L2 = special_linear_lie_algebra(QQ, 3);
+
+julia> h = hom(L1, L2, [basis(L2, 1), basis(L2, 4), basis(L2, 7)]) # embed sl_2 into sl_3
+Lie algebra morphism
+  from special linear Lie algebra of degree 2 over QQ
+  to   special linear Lie algebra of degree 3 over QQ
+  
+julia> [(x, h(x)) for x in basis(L1)]
+  3-element Vector{Tuple{LinearLieAlgebraElem{QQFieldElem}, LinearLieAlgebraElem{QQFieldElem}}}:
+   (e_1_2, e_1_2)
+   (f_1_2, f_1_2)
+   (h_1, h_1)
+"""
 function hom(
   L1::LieAlgebra{C}, L2::LieAlgebra{C}, imgs::Vector{<:LieAlgebraElem{C}}; check::Bool=true
 ) where {C<:RingElement}
   return LieAlgebraHom(L1, L2, imgs; check)
 end
 
+@doc raw"""
+    hom(L1::LieAlgebra, L2::LieAlgebra, mat::MatElem; check::Bool=true) -> LieAlgebraHom
+
+Construct the homomorphism from `L1` to `L2` by acting with the matrix `mat`
+from the right on the coefficient vector w.r.t. the basis of `L1`.
+`mat` must be a matrix of size `dim(L1) \times dim(L2)` over `coefficient_ring(L2)`.
+
+By setting `check=false`, the linear map is not checked to be compatible with the Lie bracket.
+
+# Examples
+```jldoctest
+julia> L1 = special_linear_lie_algebra(QQ, 2);
+
+julia> L2 = general_linear_lie_algebra(QQ, 2);
+
+julia> h = hom(L1, L2, matrix(QQ, [0 1 0 0; 0 0 1 0; 1 0 0 -1]))
+Lie algebra morphism
+  from special linear Lie algebra of degree 2 over QQ
+  to   general linear Lie algebra of degree 2 over QQ
+
+julia> [(x, h(x)) for x in basis(L1)]
+3-element Vector{Tuple{LinearLieAlgebraElem{QQFieldElem}, LinearLieAlgebraElem{QQFieldElem}}}:
+ (e_1_2, x_1_2)
+ (f_1_2, x_2_1)
+ (h_1, x_1_1 - x_2_2)
+"""
 function hom(
   L1::LieAlgebra{C}, L2::LieAlgebra{C}, mat::MatElem{C}; check::Bool=true
 ) where {C<:RingElement}
   return LieAlgebraHom(L1, L2, mat; check)
 end
 
+@doc raw"""
+    identity_map(L::LieAlgebra) -> LieAlgebraHom
+
+Construct the identity map on `L`.
+
+# Examples
+```jldoctest
+julia> L = special_linear_lie_algebra(QQ, 3)
+Special linear Lie algebra of degree 3
+  of dimension 8
+over rational field
+
+julia> identity_map(L)
+Lie algebra morphism
+  from special linear Lie algebra of degree 3 over QQ
+  to   special linear Lie algebra of degree 3 over QQ
+```
+"""
 function identity_map(L::LieAlgebra)
   return hom(L, L, basis(L); check=false)
 end
