56e9b140a6dfce5d3ff81b6afe7479a4f3aa71db
antonydellavecchia <antonydellavecchia@gmail.com>
2023-08-21 16:35:04 +0200

    vector instead of tuple is fine in toric test

--- test/Serialization/ToricGeometry.jl
+++ test/Serialization/ToricGeometry.jl
@@ -12,7 +12,7 @@
       pp = projective_space(NormalToricVariety, 2)
       td0 = toric_divisor(pp, [1,1,2])
       td1 = toric_divisor(pp, [1,1,3])
-      vtd = (td0, td1)
+      vtd = [td0, td1]
       test_save_load_roundtrip(path, vtd) do loaded
         @test coefficients(td0) == coefficients(loaded[1])
         @test coefficients(td1) == coefficients(loaded[2])
