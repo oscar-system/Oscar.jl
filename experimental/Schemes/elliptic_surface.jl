78df113432eef977f2bc5286210dd3ee6af9f6bb
simonbrandhorst <brandhorst@math.uni-sb.de>
2023-08-22 10:51:10 +0200

    Apply suggestions from code review

--- experimental/Schemes/elliptic_surface.jl
+++ experimental/Schemes/elliptic_surface.jl
@@ -515,10 +515,10 @@ function relatively_minimal_model(E::EllipticSurface)
     Y0 = Y1
     inc_Y0 = inc_Y1
     X0 = X1
-    set_attribute!(Y0, :is_irreducible, true)
+    set_attribute!(Y0, :is_irreducible=> true)
     set_attribute!(Y0, :is_reduced=>true)
     set_attribute!(Y0, :is_integral=>true)
-    set_attribute!(X0, :is_irreducible, true)
+    set_attribute!(X0, :is_irreducible=> true)
     set_attribute!(X0, :is_reduced=>true)
     set_attribute!(X0, :is_integral=>true)
   end
