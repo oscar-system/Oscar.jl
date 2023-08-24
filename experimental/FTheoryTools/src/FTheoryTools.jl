8c4b56e82ed6004634bd02e1923f0a8330a79652
antonydellavecchia <antonydellavecchia@gmail.com>
2023-08-24 14:23:54 +0200

    serialization updated

--- experimental/FTheoryTools/src/FTheoryTools.jl
+++ experimental/FTheoryTools/src/FTheoryTools.jl
@@ -20,6 +20,7 @@ include("HypersurfaceModels/properties.jl")
 include("HypersurfaceModels/methods.jl")
 
 include("standard_constructions.jl")
+include("serialization.jl")
 
 include("LiteratureModels/constructors.jl")
 include("LiteratureModels/attributes.jl")
