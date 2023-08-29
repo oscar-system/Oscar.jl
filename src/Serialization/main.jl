072b6b46e3e14d3d791e7dfd2f43dbe0860b52f0
antonydellavecchia <antonydellavecchia@gmail.com>
2023-08-29 12:29:29 +0200

    retrigger tests

--- src/Serialization/main.jl
+++ src/Serialization/main.jl
@@ -155,7 +155,7 @@ const oscar_serialization_version = get_version_info()
 
 ################################################################################
 # (De|En)coding types
-#
+
 # parameters of type should not matter here
 const reverse_type_map = Dict{String, Type}()
 
