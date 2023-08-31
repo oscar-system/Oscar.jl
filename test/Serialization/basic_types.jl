679a29a33886cc6e0e246fcf98af66af23388429
antonydellavecchia <antonydellavecchia@gmail.com>
2023-08-31 12:13:11 +0200

    serialize ZZModRing (note the capital ZZ)

--- test/Serialization/basic_types.jl
+++ test/Serialization/basic_types.jl
@@ -42,49 +42,50 @@ function test_save_load_roundtrip(func, path, original::T; params=nothing) where
 end
 
 @testset "basic_types" begin
-    
-    mktempdir() do path
-        @testset "Testing (de)serialization of $(T)" for T in 
-            (
-                UInt, UInt8, UInt16, UInt32, UInt64, UInt128,
-                Int, Int8, Int16, Int32, Int64, Int128,
-                Float16, Float32, Float64,
-                BigInt,
-                ZZRingElem,
-                QQFieldElem,
-                residue_ring(ZZ, 6),
-                Nemo.fpField(UInt(7)),
-                Nemo.FpField(ZZRingElem(7)),
-                #PadicField(7, 30),
-                #TropicalSemiring()
-            )
-            original = T(1)
-            test_save_load_roundtrip(path, original) do loaded
-              @test loaded == original
-            end
-        end
-        
-        @testset "String" begin
-            original = "original"
-            test_save_load_roundtrip(path, original) do loaded
-              @test loaded == original
-            end
-        end
-
-        @testset "Symbol" begin
-            original = :original
-            test_save_load_roundtrip(path, original) do loaded
-              @test loaded == original
-            end
-        end
-
-        @testset "Singleton types" begin
-            test_save_load_roundtrip(path, ZZ) do loaded
-              @test loaded === ZZ
-            end
-            test_save_load_roundtrip(path, QQ) do loaded
-              @test loaded === QQ
-            end
-        end
+  
+  mktempdir() do path
+    @testset "Testing (de)serialization of $(T)" for T in 
+      (
+        UInt, UInt8, UInt16, UInt32, UInt64, UInt128,
+        Int, Int8, Int16, Int32, Int64, Int128,
+        Float16, Float32, Float64,
+        BigInt,
+        ZZRingElem,
+        QQFieldElem,
+        residue_ring(ZZ, ZZ(6)),
+        residue_ring(ZZ, 6),
+        Nemo.fpField(UInt(7)),
+        Nemo.FpField(ZZRingElem(7)),
+        #PadicField(7, 30),
+        #TropicalSemiring()
+      )
+      original = T(1)
+      test_save_load_roundtrip(path, original) do loaded
+        @test loaded == original
+      end
     end
+    
+    @testset "String" begin
+      original = "original"
+      test_save_load_roundtrip(path, original) do loaded
+        @test loaded == original
+      end
+    end
+
+    @testset "Symbol" begin
+      original = :original
+      test_save_load_roundtrip(path, original) do loaded
+        @test loaded == original
+      end
+    end
+
+    @testset "Singleton types" begin
+      test_save_load_roundtrip(path, ZZ) do loaded
+        @test loaded === ZZ
+      end
+      test_save_load_roundtrip(path, QQ) do loaded
+        @test loaded === QQ
+      end
+    end
+  end
 end
