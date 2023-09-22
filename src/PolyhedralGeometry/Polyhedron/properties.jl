f893da5dfbebe05083e7fb9f47fc53629c623ebc
Zoe Geiselmann <geiselmann@math.tu-berlin.de>
2023-09-22 10:31:37 +0200

    remove keepat in vertex_sizes

--- src/PolyhedralGeometry/Polyhedron/properties.jl
+++ src/PolyhedralGeometry/Polyhedron/properties.jl
@@ -786,7 +786,7 @@ function vertex_sizes(P::Polyhedron{T}) where T<:scalar_types
   res = Vector{Int}(pm_object(P).VERTEX_SIZES)
   fai = [i+1 for i in Vector{Int}(pm_object(P).FAR_FACE)]
   vertices = symdiff(Vector{Int}(1:pm_object(P).N_VERTICES),fai)
-  return Base.keepat!(res,vertices)
+  return [res[i] for i in vertices]
 end
 
 ###############################################################################
