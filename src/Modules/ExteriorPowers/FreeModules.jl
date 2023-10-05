1e0b12747096f42be4fe32d842cb554ae956f4ee
Matthias Zach <zach@math.uni-hannover.de>
2023-10-05 14:12:57 +0200

    Extend optional caching to Koszul complexes.

--- src/Modules/ExteriorPowers/FreeModules.jl
+++ src/Modules/ExteriorPowers/FreeModules.jl
@@ -170,11 +170,11 @@ end
 # Koszul homology
 ########################################################################
 
-function koszul_complex(v::FreeModElem)
+function koszul_complex(v::FreeModElem; cached::Bool=true)
   F = parent(v)
   R = base_ring(F)
   n = rank(F)
-  ext_powers = [exterior_power(F, p) for p in 0:n]
+  ext_powers = [exterior_power(F, p, cached=cached) for p in 0:n]
   boundary_maps = [wedge_multiplication_map(ext_powers[i+1], ext_powers[i+2], v) for i in 0:n-1]
   Z = is_graded(F) ? graded_free_module(R, []) : free_module(R, 0)
   pushfirst!(boundary_maps, hom(Z, domain(first(boundary_maps)), elem_type(domain(first(boundary_maps)))[]))
@@ -182,65 +182,65 @@ function koszul_complex(v::FreeModElem)
   return chain_complex(boundary_maps, seed=-1)
 end
 
-function koszul_complex(v::FreeModElem, M::ModuleFP)
-  K = koszul_complex(v)
+function koszul_complex(v::FreeModElem, M::ModuleFP; cached::Bool=true)
+  K = koszul_complex(v, cached=cached)
   KM = tensor_product(K, M)
   return KM
 end
 
-function koszul_homology(v::FreeModElem, i::Int)
+function koszul_homology(v::FreeModElem, i::Int; cached::Bool=true)
   F = parent(v)
   n = rank(F)
 
   # Catch the edge cases
   if i == n # This captures the homological degree zero due to the convention of the chain_complex constructor
-    phi = wedge_multiplication_map(exterior_power(F, 0), F, v)
+    phi = wedge_multiplication_map(exterior_power(F, 0, cached=cached), F, v)
     return kernel(phi)[1]
   end
 
   if iszero(i) # Homology at the last entry of the complex.
-    phi = wedge_multiplication_map(exterior_power(F, n-1), exterior_power(F, n), v)
+    phi = wedge_multiplication_map(exterior_power(F, n-1), exterior_power(F, n, cached=cached), v)
     return cokernel(phi)[1]
   end
 
-  ext_powers = [exterior_power(F, p) for p in i-1:i+1]
+  ext_powers = [exterior_power(F, p, cached=cached) for p in i-1:i+1]
   boundary_maps = [wedge_multiplication_map(ext_powers[p], ext_powers[p+1], v) for p in 1:2]
   K = chain_complex(boundary_maps)
   return homology(K, 1)
 end
 
-function koszul_homology(v::FreeModElem, M::ModuleFP, i::Int)
+function koszul_homology(v::FreeModElem, M::ModuleFP, i::Int; cached::Bool=true)
   F = parent(v)
   n = rank(F)
 
   # Catch the edge cases
   if i == n # This captures the homological degree zero due to the convention of the chain_complex constructor
-    phi = wedge_multiplication_map(exterior_power(F, 0), F, v)
+    phi = wedge_multiplication_map(exterior_power(F, 0, cached=cached), F, v)
     K = chain_complex([phi])
     KM = tensor_product(K, M)
     return kernel(map(KM, 1))[1]
   end
 
   if iszero(i) # Homology at the last entry of the complex.
-    phi = wedge_multiplication_map(exterior_power(F, n-1), exterior_power(F, n), v)
+    phi = wedge_multiplication_map(exterior_power(F, n-1), exterior_power(F, n, cached=cached), v)
     K = chain_complex([phi])
     KM = tensor_product(K, M)
     return cokernel(map(K, 1)) # TODO: cokernel does not seem to return a map by default. Why?
   end
 
-  ext_powers = [exterior_power(F, p) for p in i-1:i+1]
+  ext_powers = [exterior_power(F, p, cached=cached) for p in i-1:i+1]
   boundary_maps = [wedge_multiplication_map(ext_powers[p], ext_powers[p+1], v) for p in 1:2]
   K = chain_complex(boundary_maps)
   return homology(K, 1)
 end
 
-function koszul_dual(F::FreeMod)
+function koszul_dual(F::FreeMod; cached::Bool=true)
   success, M, p = is_exterior_power(F)
   !success && error("module must be an exterior power of some other module")
-  return exterior_power(M, rank(M) - p)
+  return exterior_power(M, rank(M) - p, cached=cached)
 end
 
-function koszul_duals(v::Vector{T}) where {T<:FreeModElem}
+function koszul_duals(v::Vector{T}; cached::Bool=true) where {T<:FreeModElem}
   isempty(v) && error("list of elements must not be empty")
   all(u->parent(u) === parent(first(v)), v[2:end]) || error("parent mismatch")
 
@@ -253,7 +253,7 @@ function koszul_duals(v::Vector{T}) where {T<:FreeModElem}
   ind = [ordered_multi_index(r, p, n) for r in k]
   comp = [ordered_multi_index([i for i in 1:n if !(i in indices(I))], n) for I in ind]
   lin_ind = linear_index.(comp)
-  F_dual = koszul_dual(F)
+  F_dual = koszul_dual(F, cached=cached)
   results = [F_dual[j] for j in lin_ind]
   for r in 1:length(v)
     sign, _ = _wedge(ind[r], comp[r])
@@ -262,8 +262,8 @@ function koszul_duals(v::Vector{T}) where {T<:FreeModElem}
   return results 
 end
 
-function koszul_dual(v::FreeModElem)
-  return first(koszul_duals([v]))
+function koszul_dual(v::FreeModElem; cached::Bool=true)
+  return first(koszul_duals([v], cached=cached))
 end
 
 function induced_map_on_exterior_power(phi::FreeModuleHom{<:FreeMod, <:FreeMod, Nothing}, p::Int;
