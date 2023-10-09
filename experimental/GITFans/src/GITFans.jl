4c90a70dc7a43899063b791ffcfd0ba550e104a5
ThomasBreuer <sam@math.rwth-aachen.de>
2023-10-09 14:12:24 +0200

    fixed a typo in an example

--- experimental/GITFans/src/GITFans.jl
+++ experimental/GITFans/src/GITFans.jl
@@ -162,7 +162,7 @@ julia> Q = matrix(QQ, [ 1  1 ; -1  1 ; -1 -1 ; 1 -1 ])
 [-1   -1]
 [ 1   -1]
 
-julia> S, emb = symmetry_group_qmatrix(Q);
+julia> S, emb = symmetry_group_q_matrix(Q);
 
 julia> describe(S)
 "D8"
