d8a3b0ab3b0d31649477d8582e7226a4710580f1
Daniel Corey <dcorey2814@gmail.com>
2023-10-25 11:18:13 -0700

    removed file runtests.jl

--- test/Combinatorics/runtests.jl
+++
@@ -1,9 +0,0 @@
-using Oscar
-using Test
-
-include("SimplicialComplexes.jl")
-include("Matroids/Matroids.jl")
-include("Matroids/Chow_Ring.jl")
-include("Graph.jl")
-include("Matroids/realization_space.jl")
-
