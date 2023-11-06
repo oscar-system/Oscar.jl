20b40e8f7b65f3af98ac09fc919322803400e249
Pascal Schumann <s8pcschu@stud.uni-saarland.de>
2023-11-06 12:27:25 +0100

    Origami Object

--- experimental/Origami/src/Origami.jl
+++ experimental/Origami/src/Origami.jl
@@ -1,15 +1,48 @@
 # Add your new types, functions, and methods here.
 
-mutable struct ExampleStruct
-  i::Int
+export origami, veech_group
+
+module OrigamiHelper
+
+using ..GAP
+
+function __init__()
+  GAP.Packages.install("https://ag-weitze-schmithusen.github.io/ModularGroup/PackageInfo.g")
+  GAP.Packages.install("https://ag-weitze-schmithusen.github.io/Origami/PackageInfo.g")
+  GAP.Packages.load("Origami")
 end
+
+end
+
+include("types.jl")
 
 @doc raw"""
-    my_access_func(S::ExampleStruct)
+    origami(h::PermGroupElem, v::PermGroupElem)
 
-This is a dummy sample function to teach the use of docstrings.
+blablabla
+# Examples
+```jldoctest
+julia> h = @perm (1,2)
+(1,2)
+
+julia> v = @perm (1,2)
+(1,2)
+
+julia> o = origami(h,v)
+Origami ((1,2),(1,2))
+```
+
 """
-function my_access_func(S::ExampleStruct)
-  return S.i
+function origami(h::PermGroupElem, v::PermGroupElem)
+  return Origami(GAP.Globals.Origami(GapObj(h), GapObj(v)), h, v)
 end
 
+function Base.show(io::IO, O::Origami)
+  print(io, "Origami ($(O.h),$(O.v))")
+end
+
+GapObj(O::Origami) = O.o
+
+function veech_group(O::Origami)
+  GAP.Globals.VeechGroup(GapObj(O))
+end
