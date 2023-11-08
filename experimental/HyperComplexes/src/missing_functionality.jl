1fa976bf8fd9f02870af2204567d662d1fcd5367
Matthias Zach <zach@math.uni-hannover.de>
2023-11-08 11:43:20 +0100

    Deprecate duplicates.

--- experimental/HyperComplexes/src/missing_functionality.jl
+++ experimental/HyperComplexes/src/missing_functionality.jl
@@ -1,5 +1,7 @@
-morphism_type(::Type{T}) where {T<:ModuleFP} = ModuleFPHom{T, T}
+# The commented lines are also contained in the DoubleComplexes 
+# and hence removed here to suppress warnings.
+#morphism_type(::Type{T}) where {T<:ModuleFP} = ModuleFPHom{T, T}
 morphism_type(::Type{T}) where {T<:FreeMod} = FreeModuleHom{T, T}
 
-typ(C::ComplexOfMorphisms) = C.typ
-tensor_product(v::Vector{<:ModuleFP}) = tensor_product(v...)
+#typ(C::ComplexOfMorphisms) = C.typ
+#tensor_product(v::Vector{<:ModuleFP}) = tensor_product(v...)
