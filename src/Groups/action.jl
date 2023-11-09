308915edf2f4d6f14691f4f9a045176b3dbae796
Johannes Schmitt <jschmitt@posteo.eu>
2023-11-09 12:35:28 +0100

    Clarify the right action on polynomials

--- src/Groups/action.jl
+++ src/Groups/action.jl
@@ -281,19 +281,23 @@ function permuted(pnt::T, x::PermGroupElem) where T <: Tuple
 end
 
 
-"""
+@doc raw"""
     on_indeterminates(f::GAP.GapObj, p::PermGroupElem)
     on_indeterminates(f::MPolyRingElem, p::PermGroupElem)
     on_indeterminates(f::FreeAssAlgElem, p::PermGroupElem)
     on_indeterminates(f::MPolyIdeal, p::PermGroupElem)
     on_indeterminates(f::GAP.GapObj, p::MatrixGroupElem)
-    on_indeterminates(f::MPolyRingElem{T}, p::MatrixGroupElem{T, S}) where T where S
+    on_indeterminates(f::MPolyRingElem{T}, p::MatrixGroupElem{T}) where T
     on_indeterminates(f::MPolyIdeal, p::MatrixGroupElem)
 
 Return the image of `f` under `p`.
-If `p` is a `PermGroupElem` then it acts via permuting the indeterminates,
-if `p` is a `MatrixGroupElem` then it acts via evaluating `f` at the
+
+If `p` is a `PermGroupElem` then it acts via permuting the indeterminates.
+
+If `p` is a `MatrixGroupElem` then it acts via evaluating `f` at the
 vector obtained by multiplying `p` with the (column) vector of indeterminates.
+This corresponds to considering the variables of `parent(f)` as the basis of a
+vector space on which `p` acts by multiplication from the right.
 
 For `MPolyRingElem`, `FreeAssAlgElem`, and `MPolyIdeal` objects,
 one can also call `^` instead of `on_indeterminates`.
@@ -369,10 +373,10 @@ function on_indeterminates(f::GapObj, p::MatrixGroupElem)
   return GAPWrap.Value(f, indets, p.X * indets)
 end
 
-function on_indeterminates(f::MPolyRingElem{T}, p::MatrixGroupElem{T, S}) where T where S
+function on_indeterminates(f::MPolyRingElem{T}, p::MatrixGroupElem{T}) where T
   @assert base_ring(f) == base_ring(p)
   @assert ngens(parent(f)) == degree(parent(p))
-  act = Oscar.right_action(parent(f), p)
+  act = right_action(parent(f), p)
   return act(f)
 end
 
