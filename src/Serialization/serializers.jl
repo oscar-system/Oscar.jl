2412595c6d63d549715434ac437e8ccccaeffe50
antonydellavecchia <antonydellavecchia@gmail.com>
2023-11-23 23:52:17 +0100

    added utility haskey function for Deserializer State

--- src/Serialization/serializers.jl
+++ src/Serialization/serializers.jl
@@ -1,4 +1,5 @@
 using JSON3
+import Base.haskey
 
 ################################################################################
 # (de)Serializer States
@@ -150,11 +151,11 @@ function load_ref(s::DeserializerState)
   return loaded_ref
 end
 
-#function haskey(s::DeserializerState, key::Symbol)
-#  load_node(s) do obj
-#    key in keys(obj)
-#  end
-#end
+function haskey(s::DeserializerState, key::Symbol)
+  load_node(s) do obj
+    key in keys(obj)
+  end
+end
 
 function set_key(s::DeserializerState, key::Union{Symbol, Int})
   @req isnothing(s.key) "Object at Key :$(s.key) hasn't been deserialized yet."
