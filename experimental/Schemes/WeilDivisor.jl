a43f5ca29016844440dfca4a9429de29cb9692f7
HechtiDerLachs <zach@mathematik.uni-kl.de>
2023-11-23 14:27:38 +0100

    Remove method ambiguity for multiplication.

--- experimental/Schemes/WeilDivisor.jl
+++ experimental/Schemes/WeilDivisor.jl
@@ -830,3 +830,5 @@ function forget_toric_structure(td::ToricDivisor)
   return underlying_divisor(td)::WeilDivisor
 end
 
+# Method ambiguity requires the following
+Base.:*(c::Int, td::ToricDivisor) = toric_divisor(toric_variety(td), [ZZRingElem(c)*x for x in coefficients(td)])
