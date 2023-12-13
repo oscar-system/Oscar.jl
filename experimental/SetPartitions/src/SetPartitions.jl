10e0d897af51caecdd33c562117ff02e4c7cfb53
pinguly <nicolasfaross@gmail.com>
2023-12-13 14:30:39 +0100

    improving imports/exports

--- experimental/SetPartitions/src/SetPartitions.jl
+++ experimental/SetPartitions/src/SetPartitions.jl
@@ -1,37 +1,33 @@
 module SetPartitions
 
 import Base: 
-    hash, 
     ==, 
-    copy
-
-import Oscar:
-    involution,
-    tensor_product,
-    size,
     *,
+    adjoint,
+    copy,
+    hash,
+    size
+    
+import Oscar:
     ⊗,
-    adjoint
+    involution,
+    tensor_product
+   
 
-export SetPartition
 export ColoredPartition
+export SetPartition
 export SpatialPartition
-export tensor_product
+
 export composition
 export composition_loops
-export involution
+export construct_category
+export is_balanced
+export is_noncrossing
+export is_pair
+export print_trace
 export rotation
 export vertical_reflection
-export is_balanced
-export is_pair
-export is_noncrossing
-export construct_category
-export print_trace
-export size
-export *
-export ⊗
-export ⋅
-export adjoint
+
 
 include("AbstractPartition.jl")
 include("Util.jl")
@@ -44,22 +40,16 @@ end
 
 using .SetPartitions
 
+export ColoredPartition
 export SetPartition
 export SpatialPartition
-export ColoredPartition
-export tensor_product
+
 export composition
 export composition_loops
-export involution
+export construct_category
+export is_balanced
+export is_noncrossing
+export is_pair
+export print_trace
 export rotation
 export vertical_reflection
-export is_balanced
-export is_pair
-export is_noncrossing
-export construct_category
-export print_trace
-export size
-export *
-export ⊗
-export ⋅
-export adjoint
