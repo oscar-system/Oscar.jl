070b364c3538130584fd14a779816917bddee30c
Matthias Zach <zach@math.uni-hannover.de>
2023-12-21 22:20:37 +0100

    Turn off check.

--- experimental/Schemes/Auxiliary.jl
+++ experimental/Schemes/Auxiliary.jl
@@ -93,7 +93,7 @@ function pullback(f::AbsCoveredSchemeMorphism, II::IdealSheaf)
     pbf = pullback(f_U)
     ID[U] = ideal(OO(U), pbf.(gens(II(V))))
   end
-  return IdealSheaf(X, ID, check=true)
+  return IdealSheaf(X, ID, check=false)
 end
 
 function pullback(f::CompositeCoveredSchemeMorphism, C::EffectiveCartierDivisor)
