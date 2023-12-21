c0008bec5c9ac7b8c948c0e91fca9b4cbd52b87c
Lax202 <ramesh@mathematik.uni-kl.de>
2023-12-21 12:40:40 +0100

    added comments for the tests that don't work - needed to print complexes

--- test/Modules/Module-NCRing.jl
+++ test/Modules/Module-NCRing.jl
@@ -4,4 +4,10 @@
     @test ngens(M) == 3
     N = Oscar.SubModuleOfFreeModule(M, gens(M)[1:2])
     @test gens(N) == gens(M)[1:2]
+    H = hom(M,M,gens(M))
+    #fails here
+    #W = free_module(E,2)
+    #G = hom(W,M, gens(M)[1:2])
+    #this is becuase the line below fails
+    #@test parent(gens(M[1])) == M
 end
