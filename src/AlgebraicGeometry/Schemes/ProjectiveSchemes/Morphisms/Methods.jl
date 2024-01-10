44ce48391681bbb2f93ce8a5c9ed5c791dd80495
Matthias Zach <zach@math.uni-hannover.de>
2024-01-10 21:17:03 +0100

    Implement prototype version of induced morphism on covered schemes from morphism of projective schemes.

--- src/AlgebraicGeometry/Schemes/ProjectiveSchemes/Morphisms/Methods.jl
+++ src/AlgebraicGeometry/Schemes/ProjectiveSchemes/Morphisms/Methods.jl
@@ -51,7 +51,9 @@ with default covering
     3: [(x//z), (y//z)]
 ```
 """
-@attr function covered_scheme_morphism(f::AbsProjectiveSchemeMorphism)
+@attr function covered_scheme_morphism(
+    f::AbsProjectiveSchemeMorphism{<:Any, <:Any, PBT}
+  ) where {PBT <: MPolyAnyMap{<:Any, <:Any, Nothing}} # No map on the base rings
   PX = domain(f)
   PY = codomain(f)
   SX = ambient_coordinate_ring(PX)
@@ -77,6 +79,49 @@ with default covering
                                    [OO(U_ij)(dehom(pbf(gen(SY, k))))*u for k in 1:ngens(SY) if k != j]
                                   )
                               )
+    end
+  end
+  # We skip the glueings for the time being.
+  # Eventually, they should be made lazy.
+  CC = Covering(collect(keys(mor_dict)), IdDict{Tuple{AbsSpec, AbsSpec}, AbsGlueing}())
+  inherit_glueings!(CC, default_covering(X))
+  phi = CoveringMorphism(CC, default_covering(Y), mor_dict, check=false)
+  push!(coverings(X), CC)
+
+  ff = CoveredSchemeMorphism(X, Y, phi)
+  return ff
+end
+
+@attr function covered_scheme_morphism(
+    f::AbsProjectiveSchemeMorphism
+  ) # with map on the base rings
+  PX = domain(f)
+  PY = codomain(f)
+  SX = ambient_coordinate_ring(PX)
+  SY = ambient_coordinate_ring(PY)
+  pbf = pullback(f) # The pullback on the free polynomial rings, not the quotients
+  coeff_map = coefficient_map(pbf)
+
+  X = covered_scheme(PX)
+  Y = covered_scheme(PY)
+
+  mor_dict = IdDict{AbsSpec, AbsSpecMor}()
+  U = affine_charts(X)
+  for i in 1:ngens(SX)
+    U_i = U[i]
+    dehom = dehomogenization_map(PX, U_i) # the dehomogenization map SX → 𝒪(Uᵢ)
+    for j in 1:ngens(SY)
+      y = gen(SY, j)
+      denom = dehom(pbf(y))
+      V_j = affine_charts(Y)[j]
+      U_ij = PrincipalOpenSubset(U_i, denom)
+      u = inv(OO(U_ij)(denom))
+      mor_dict[U_ij] = SpecMor(U_ij, V_j, 
+                               hom(OO(V_j), OO(U_ij), coeff_map,
+                                   [OO(U_ij)(dehom(pbf(gen(SY, k))))*u for k in 1:ngens(SY) if k != j]
+                                  )
+                              )
+      #@assert _has_coefficient_map(pullback(mor_dict[U_ij]))
     end
   end
   # We skip the glueings for the time being.
