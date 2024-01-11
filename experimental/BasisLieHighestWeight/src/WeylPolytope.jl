a5be219f7a467c8fb3b43c06754031e346b6753d
Ghislain Fourier <133408128+gfourier@users.noreply.github.com>
2024-01-11 12:38:36 +0100

    Update experimental/BasisLieHighestWeight/src/WeylPolytope.jl
    Co-authored-by: Lars Göttgens <lars.goettgens@gmail.com>

--- experimental/BasisLieHighestWeight/src/WeylPolytope.jl
+++ experimental/BasisLieHighestWeight/src/WeylPolytope.jl
@@ -64,7 +64,6 @@ function get_lattice_points_of_weightspace(
   weight_roots::Vector{Vector{QQFieldElem}}, 
   L::LieAlgebraStructure, 
   weight::Vector{ZZRingElem}, 
-  highest_weight::Vector{ZZRingElem}, 
   zero_coordinates::Set{Any}
 )
   # calculate all integer solutions to the following linear program:
