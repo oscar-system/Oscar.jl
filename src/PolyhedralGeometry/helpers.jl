37f2b990bd4eeecbb7291abb735a8d57a8be9b30
Benjamin Lorenz <benlorenz@users.noreply.github.com>
2024-01-31 10:30:07 +0100

    polyhedron: make sure empty [] vector works as right hand side (#3278)
    * polyhedral: make sure empty [] vector works as right hand side
    
    make matrix construction more robust as well
    
    * Polyhedral: fix another polymake conversion

--- src/PolyhedralGeometry/helpers.jl
+++ src/PolyhedralGeometry/helpers.jl
@@ -79,7 +79,7 @@ function assure_matrix_polymake(m::Union{AbstractMatrix{Any}, AbstractMatrix{Fie
     a, b = size(m)
     if a > 0
         i = findfirst(_cannot_convert_to_fmpq, m)
-        t = typeof(m[i])
+        t = i === nothing ? QQFieldElem : typeof(m[i])
         if t <: Union{Polymake.Rational, Polymake.QuadraticExtension{Polymake.Rational}, Polymake.OscarNumber, Float64}
             m = Polymake.Matrix{Polymake.convert_to_pm_type(t)}(m)
         else
@@ -93,7 +93,7 @@ end
 
 assure_matrix_polymake(m::AbstractMatrix{<:FieldElem}) = Polymake.Matrix{Polymake.OscarNumber}(m)
 
-assure_matrix_polymake(m::MatElem) = Polymake.OscarNumber.(m)
+assure_matrix_polymake(m::MatElem) = Polymake.Matrix{_scalar_type_to_polymake(eltype(m))}(m)
 
 assure_matrix_polymake(m::Union{Oscar.ZZMatrix, Oscar.QQMatrix, AbstractMatrix{<:Union{QQFieldElem, ZZRingElem, Base.Integer, Base.Rational, Polymake.Rational, Polymake.QuadraticExtension, Polymake.OscarNumber, Float64}}}) = m
 
@@ -101,8 +101,8 @@ assure_matrix_polymake(m::SubArray{T, 2, U, V, W}) where {T<:Union{Polymake.Rati
 
 function assure_vector_polymake(v::Union{AbstractVector{Any}, AbstractVector{FieldElem}})
     i = findfirst(_cannot_convert_to_fmpq, v)
-    v = Polymake.Vector{_scalar_type_to_polymake(typeof(v[i]))}(v)
-    return v
+    T = i === nothing ? QQFieldElem : typeof(v[i])
+    return Polymake.Vector{_scalar_type_to_polymake(T)}(v)
 end
 
 assure_vector_polymake(v::AbstractVector{<:FieldElem}) = Polymake.Vector{Polymake.OscarNumber}(v)
