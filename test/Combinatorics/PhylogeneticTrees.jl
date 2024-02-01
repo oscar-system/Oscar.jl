e63cde2dff5ee284ae5a341e1543b2df57e4a633
andreicomaneci <aco.aco.ro@gmail.com>
2024-02-01 23:55:01 +0100

    fix tests

--- test/Combinatorics/PhylogeneticTrees.jl
+++ test/Combinatorics/PhylogeneticTrees.jl
@@ -6,17 +6,16 @@
 
   @test equidistant(ptree1) == true
 
-  # this will need a conversion
   M = matrix(QQ, [0 4 9; 4 0 9; 9 9 0])
   ptree2 = phylogenetic_tree(M, ["a", "b", "c"])
 
   cophenetic_matrix(ptree2)
-  @test newick(ptree2) == "c:4.5,(a:2,b:2):2.5;"
+  @test newick(ptree2) == "c:9/2,(a:2,b:2):5/2;"
   @test taxa(ptree2) == ["a", "b", "c"]
   
-  ptree2_test = phylogenetic_tree([0 4 9; 4 0 9.0; 9 9 0], ["a", "b", "c"])
-  tmc = tropical_median_consensus(ptree2_test, ptree2)
-  tmc_v = tropical_median_consensus([ptree2_test, ptree2])
-  @test newick(tmc) == "c:4.5,(a:2,b:2):2.5;"
-  @test newick(tmc_v) == "c:4.5,(a:2,b:2):2.5;"
+  ptree1_test = phylogenetic_tree([0 4 9; 4 0 9.0; 9 9 0], ["A", "B", "C"])
+  tmc = tropical_median_consensus(ptree1_test, ptree1)
+  tmc_v = tropical_median_consensus([ptree1_test, ptree1])
+  @test newick(tmc) == "C:9,(A:6.5,B:6.5):2.5;"
+  @test newick(tmc_v) == "C:9,(A:6.5,B:6.5):2.5;"
 end
