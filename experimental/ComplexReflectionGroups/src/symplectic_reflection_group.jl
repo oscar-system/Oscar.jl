dc9aa76c276c6fc21b59895771b92ecc1f45fbbc
Ulrich Thiel <thiel@mathematik.uni-kl.de>
2024-02-02 09:06:36 +0000

    Added symplectic_reflection_group

---
+++ experimental/ComplexReflectionGroups/src/symplectic_reflection_group.jl
@@ -0,0 +1,38 @@
+# This file implements explicit models of symplectic reflection groups.
+#
+# Ulrich Thiel, 2024
+
+export symplectic_reflection_group
+export is_symplectic_reflection_group
+
+function symplectic_reflection_group(W::MatrixGroup)
+
+    if !is_complex_reflection_group(W)
+        throw(ArgumentError("Group must be a complex reflection group"))
+    end
+
+    K = base_ring(W)
+    n = degree(W)
+    matspace = matrix_space(K, 2*n, 2*n)
+    gens_symp = elem_type(matspace)[]
+
+    for w in gens(W)
+        w_symp = matspace(block_diagonal_matrix([matrix(w), transpose(matrix(w^-1))]))
+        push!(gens_symp, w_symp)
+    end
+
+    W_symp = matrix_group(gens_symp)
+
+    set_attribute!(W_symp, :order, order(C))
+    set_attribute!(W_symp, :is_symplectic_reflection_group, true)
+
+    return W_symp
+end
+
+function is_symplectic_reflection_group(G::MatrixGroup)
+    if has_attribute(G, :is_symplectic_reflection_group)
+        return get_attribute(G, :is_symplectic_reflection_group)
+    end
+    return false 
+    #this should be upgraded later to work with a general matrix group
+end
