1c92407801bd9e6914f4839f4aa99808779ddf3e
Marcel Wack <63490664+Sequenzer@users.noreply.github.com>
2024-02-03 14:51:22 +0100

    added graph_from_edges function (#3300)
    * added graph_from_edges function
    
    * deleted useless lines
    
    * added optional argument additional_vertices for isolated vertices
    
    * fixed typo in Documentation
    
    * changed graph_from_edges to follow LarsK suggestions
    
    * fixed documentation
    
    * n_vertices < n_needed now throws an error

--- src/exports.jl
+++ src/exports.jl
@@ -585,6 +585,7 @@ export graded_polynomial_ring
 export grading_group
 export graph
 export graph_from_adjacency_matrix
+export graph_from_edges
 export grassmann_pluecker_ideal
 export grid_morphism
 export groebner_basis
