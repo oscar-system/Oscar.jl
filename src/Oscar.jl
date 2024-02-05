71c3107c927ce86cf6f24be2accfb44c73f428d4
Johannes Schmitt <schmitt@mathematik.uni-kl.de>
2024-02-05 10:25:34 +0100

    "Integer combinatorics" -> "Enumerative combinatorics"

--- src/Oscar.jl
+++ src/Oscar.jl
@@ -238,7 +238,7 @@ include("Combinatorics/SimplicialComplexes.jl")
 include("Combinatorics/OrderedMultiIndex.jl")
 include("Combinatorics/Matroids/JMatroids.jl")
 include("Combinatorics/Compositions.jl")
-include("Combinatorics/IntegerCombinatorics/IntegerCombinatorics.jl")
+include("Combinatorics/EnumerativeCombinatorics/EnumerativeCombinatorics.jl")
 
 include("StraightLinePrograms/StraightLinePrograms.jl")
 include("Rings/lazypolys.jl") # uses StraightLinePrograms
