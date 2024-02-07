c13fc9f5a206256f9e1faaaebd2a75f20d7cdbec
Antony Della Vecchia <vecchia@math.tu-berlin.de>
2024-02-07 10:26:45 +0100

    Revert "some restructuring"
    This reverts commit 0a9d603fe78105159e7eb883e6e088e7496fd854.

--- src/Combinatorics/Graphs/structs.jl
+++ src/Combinatorics/Graphs/structs.jl
@@ -5,3 +5,7 @@ struct Graph{T <: Union{Directed, Undirected}}
   pm_graph::Polymake.Graph{T}
 end
 
+mutable struct PhylogeneticTree{T <: Union{Float64, QQFieldElem}}
+  pm_ptree::Polymake.LibPolymake.BigObjectAllocated
+end
+
