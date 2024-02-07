334b61baf2cab0a8321e6175fc426969dc0cbec3
Antony Della Vecchia <vecchia@math.tu-berlin.de>
2024-02-07 10:37:54 +0100

    returning graph from edges to the bottom of file + add include

--- src/Combinatorics/Graphs/functions.jl
+++ src/Combinatorics/Graphs/functions.jl
@@ -5,9 +5,6 @@ function pm_object(G::Graph{T}) where {T <: Union{Directed, Undirected}}
     return G.pm_graph
 end
 
-function pm_object(PT::PhylogeneticTree)
-  return PT.pm_ptree
-end
 
 ################################################################################
 ################################################################################
@@ -83,54 +80,6 @@ function graph_from_adjacency_matrix(::Type{T}, G::Union{MatElem, Matrix}) where
 end
 
 
-function graph_from_edges(::Type{T},
-  edges::Vector{Edge},
-  n_vertices::Int=-1) where {T <: Union{Directed, Undirected}} 
-
-  n_needed = maximum(reduce(append!,[[src(e),dst(e)] for e in edges]))
-  @req (n_vertices >= n_needed || n_vertices < 0)  "n_vertices must be at least the maximum vertex in the edges"
-
-  g = Graph{T}(max(n_needed, n_vertices))
-  for e in edges
-    add_edge!(g, src(e), dst(e))
-  end
-
-  return g
-end
-
-graph_from_edges(::Type{T},
-edges::EdgeIterator,
-n_vertices::Int=-1) where {T <: Union{Directed, Undirected}} = graph_from_edges(T, collect(edges), n_vertices)
-
-@doc raw"""
-  graph_from_edges(::Type{T}, edges::Vector{Vector{Int}}) where {T <:Union{Directed, Undirected}}
-
-Creates a graph from a vector of edges. Optionally, you could input the number of vertices, but if this number is lower than the maximum vertex in the edges, this argument will be ignored.
-
-# Examples 1
-```jldoctest
-julia> G = graph_from_edges(Undirected, [[1,3],[3,5],[4,5],[2,4],[2,3]])
-Undirected graph with 5 nodes and the following edges:
-(3, 1)(3, 2)(4, 2)(5, 3)(5, 4)
-
-```
-
-```
-# Examples 2
-```jldoctest
-julia> G = graph_from_edges(Undirected,[[1,3]])
-Undirected graph with 3 nodes and the following edges:
-(3, 1))
-
-```
-"""
-graph_from_edges(::Type{T},
-edges::Vector{Vector{Int}},
-n_vertices::Int=-1) where {T <: Union{Directed, Undirected}} = graph_from_edges(T, [Edge(e[1], e[2]) for e in edges], n_vertices)
-
-graph_from_edges(
-edges::Vector{Vector{Int}},
-n_vertices::Int=-1) = graph_from_edges(Undirected, [Edge(e[1], e[2]) for e in edges], n_vertices)
 
 _has_node(G::Graph, node::Int64) = 0 < node <= nvertices(G)
 
@@ -1134,289 +1083,51 @@ function Base.show(io::IO, G::Graph{T})  where {T <: Union{Polymake.Directed, Po
   end
 end
 
-################################################################################
-################################################################################
-##  Phylogenetic Trees
-################################################################################
-################################################################################
-@doc raw"""
-    phylogenetic_tree(T::Type{<:Union{Float64, QQFieldElem}}, newick::String)
+function graph_from_edges(::Type{T},
+  edges::Vector{Edge},
+  n_vertices::Int=-1) where {T <: Union{Directed, Undirected}} 
 
-Constructs a phylogenetic tree with Newick representation `newick`. `T` indicates 
-the numerical type of the edge lengths.
+  n_needed = maximum(reduce(append!,[[src(e),dst(e)] for e in edges]))
+  @req (n_vertices >= n_needed || n_vertices < 0)  "n_vertices must be at least the maximum vertex in the edges"
 
-# Examples
-Make a phylogenetic tree with 4 leaves from its Newick representation and print 
-its taxa and cophenetic matrix.
-```jldoctest
-julia> phylo_t = phylogenetic_tree(Float64, "((H:3,(C:1,B:1):2):1,G:4);");
-
-julia> taxa(phylo_t)
-4-element Vector{String}:
- "B"
- "C"
- "G"
- "H"
-
-julia> cophenetic_matrix(phylo_t)
-4×4 Matrix{Float64}:
- 0.0  2.0  8.0  6.0
- 2.0  0.0  8.0  6.0
- 8.0  8.0  0.0  8.0
- 6.0  6.0  8.0  0.0
-```
-"""
-function phylogenetic_tree(T::Type{<:Union{Float64, QQFieldElem}}, newick::String)
-  pm_ptree = Polymake.graph.PhylogeneticTree{Polymake.convert_to_pm_type(T)}(NEWICK = newick)
-
-  # load graph properties
-  pm_ptree.ADJACENCY
-  
-  return PhylogeneticTree{T}(pm_ptree)
-end
-
-@doc raw"""
-   phylogenetic_tree(M::Matrix{T}, taxa::Vector{String}) where T <: Union{Float64, QQFieldElem}
-
-Constructs a phylogenetic tree with cophenetic matrix `M` and taxa `taxa`. The matrix `M` must be
-ultrametric, otherwise an error will be thrown.
-
-# Examples
-Make a phylogenetic tree on 4 taxa with given cophenetic matrix and print one Newick representation.
-
-```jldoctest
-julia> mat = [0. 2 8 6; 2 0 8 6; 8 8 0 8; 6 6 8 0]
-4×4 Matrix{Float64}:
- 0.0  2.0  8.0  6.0
- 2.0  0.0  8.0  6.0
- 8.0  8.0  0.0  8.0
- 6.0  6.0  8.0  0.0
-
-julia> tax = ["Bonobo", "Chimpanzee", "Gorilla", "Human"]
-4-element Vector{String}:
- "Bonobo"
- "Chimpanzee"
- "Gorilla"
- "Human"
-
-julia> tree_mat = phylogenetic_tree(mat, tax);
-
-julia> newick(tree_mat)
-"Gorilla:4,(Human:3,(Bonobo:1,Chimpanzee:1):2):1;"
-```
-"""
-function phylogenetic_tree(M::Matrix{Float64}, taxa::Vector{String})
-  n_taxa = length(taxa)
-  @req (n_taxa, n_taxa) == size(M) "Number of taxa should match the rows and columns of the given matrix"
-  pm_ptree = Polymake.graph.PhylogeneticTree{Float64}(COPHENETIC_MATRIX = M, TAXA = taxa)
-  return PhylogeneticTree{Float64}(pm_ptree)
-end
-
-function phylogenetic_tree(M::QQMatrix, taxa::Vector{String})
-  n_taxa = length(taxa)
-  @req (n_taxa, n_taxa) == size(M) "Number of taxa should match the rows and columns of the given matrix"
-  pm_T = Polymake.convert_to_pm_type(QQFieldElem)
-  cp_M = convert(Matrix{pm_T}, Matrix{QQFieldElem}(M))
-
-  pm_ptree = Polymake.graph.PhylogeneticTree{pm_T}(
-    COPHENETIC_MATRIX = M, TAXA = taxa
-  )
-  return PhylogeneticTree{QQFieldElem}(pm_ptree)
-end
-
-@doc raw"""
-    adjacency_tree(ptree::PhylogeneticTree)
-
-Returns the underlying graph of the phylogenetic tree `ptree`.
-
-# Examples
-Make a phylogenetic tree with given Newick format and print its underlying graph.
-
-```jldoctest
-julia> ptree = phylogenetic_tree(Float64, "((H:3,(C:1,B:1):2):1,G:4);");
-
-julia> adjacency_tree(ptree)
-Undirected graph with 7 nodes and the following edges:
-(2, 1)(3, 2)(4, 2)(5, 4)(6, 4)(7, 1)
-```
-"""
-function adjacency_tree(ptree::PhylogeneticTree)
-  return Graph{Undirected}(ptree.pm_ptree.ADJACENCY)
-end
-
-function Base.show(io::IO, ptree::PhylogeneticTree{T}) where T
-  print(io, "Phylogenetic tree with $T type coefficients")
-end
-
-@doc raw"""
-    equidistant(ptree::PhylogeneticTree)
-
-Checks if the phylogenetic tree `ptree` is equidistant.
-
-# Examples
-Make a phylogenetic tree with given Newick format and check if it is equidistant.
-
-```jldoctest
-julia> ptree = phylogenetic_tree(Float64, "((H:3,(C:1,B:1):2):1,G:4);");
-
-julia> equidistant(ptree)
-true
-```
-"""
-function equidistant(ptree::PhylogeneticTree)
-  return ptree.pm_ptree.EQUIDISTANT
-end
-
-
-@doc raw"""
-    cophenetic_matrix(ptree::PhylogeneticTree)
-
-Returns the cophenetic matrix of the phylogenetic tree `ptree`.
-
-# Examples
-Make a phylogenetic tree with given Newick format and print its cophenetic matrix.
-
-```jldoctest
-julia> ptree = phylogenetic_tree(Float64, "((H:3,(C:1,B:1):2):1,G:4);");
-
-julia> cophenetic_matrix(ptree)
-4×4 Matrix{Float64}:
- 0.0  2.0  8.0  6.0
- 2.0  0.0  8.0  6.0
- 8.0  8.0  0.0  8.0
- 6.0  6.0  8.0  0.0
-```
-"""
-function cophenetic_matrix(ptree::PhylogeneticTree{Float64})
-  return convert(Matrix, ptree.pm_ptree.COPHENETIC_MATRIX)
-end
-
-function cophenetic_matrix(ptree::PhylogeneticTree{QQFieldElem})
-  return matrix(QQ, convert(Matrix{QQFieldElem}, ptree.pm_ptree.COPHENETIC_MATRIX))
-end
-
-@doc raw"""
-    taxa(ptree::PhylogeneticTree)
-
-Returns the taxa of the phylogenetic tree `ptree`.
-
-# Examples
-Make a phylogenetic tree with given Newick format and print its taxa.
-
-```jldoctest
-julia> ptree = phylogenetic_tree(Float64, "((H:3,(C:1,B:1):2):1,G:4);");
-
-julia> taxa(ptree)
-4-element Vector{String}:
- "B"
- "C"
- "G"
- "H"
-```
-"""
-function taxa(ptree::PhylogeneticTree)
-  return convert(Array{String}, ptree.pm_ptree.TAXA)
-end
-
-@doc raw"""
-    newick(ptree::PhylogeneticTree)
-
-Returns a Newick representation of the phylogenetic tree `ptree`.
-
-# Examples
-Make a phylogenetic tree from a matrix and print a Newick representation of it.
-
-```jldoctest
-julia> mat = [0. 2 8 6; 2 0 8 6; 8 8 0 8; 6 6 8 0]
-4×4 Matrix{Float64}:
- 0.0  2.0  8.0  6.0
- 2.0  0.0  8.0  6.0
- 8.0  8.0  0.0  8.0
- 6.0  6.0  8.0  0.0
-
-julia> tax = ["Bonobo", "Chimpanzee", "Gorilla", "Human"]
-4-element Vector{String}:
- "Bonobo"
- "Chimpanzee"
- "Gorilla"
- "Human"
-
-julia> tree_mat = phylogenetic_tree(mat, tax);
-
-julia> newick(tree_mat)
-"Gorilla:4,(Human:3,(Bonobo:1,Chimpanzee:1):2):1;"
-```
-"""
-function newick(ptree::PhylogeneticTree)
-  return convert(String, ptree.pm_ptree.NEWICK)
-end
-
-
-@doc raw"""
-    tropical_median_consensus(arr::Vector{PhylogeneticTree{T}})
-
-Computes the tropical median consensus tree of the phylogenetic trees from
-the vector `arr`.
-
-# Examples
-Compute the tropical median consensus of three trees and print one of its
-Newick representations.
-
-```jldoctest
-julia> t1 = phylogenetic_tree(Float64, "((H:30,(C:10,B:10):20):10,G:40);");
-
-julia> t2 = phylogenetic_tree(Float64, "(((H:10,C:10):20,B:30):10,G:40);");
-
-julia> t3 = phylogenetic_tree(Float64, "((H:25,C:25):15,(B:15,G:15):25);");
-
-julia> arr = [t1, t2, t3];
-
-julia> tc = tropical_median_consensus(arr);
-
-julia> newick(tc)
-"G:40,(B:35,(C:30,H:30):5):5;"
-```
-"""
-function tropical_median_consensus(arr::Vector{PhylogeneticTree{T}}) where {T <: Union{Float64, QQFieldElem}}
-
-  n = length(arr)
-  @req n > 0 "The vector must not be empty"
-
-  phylo_type = Polymake.bigobject_type(arr[1].pm_ptree)
-  pm_arr = Polymake.Array{Polymake.BigObject}(phylo_type, n)
-  
-  for i in 1:n
-    pm_arr[i] = arr[i].pm_ptree
+  g = Graph{T}(max(n_needed, n_vertices))
+  for e in edges
+    add_edge!(g, src(e), dst(e))
   end
-  
-  pm_cons_tree = Polymake.tropical.tropical_median_consensus(pm_arr)
-  return PhylogeneticTree{T}(pm_cons_tree)
+
+  return g
 end
 
+graph_from_edges(::Type{T},
+edges::EdgeIterator,
+n_vertices::Int=-1) where {T <: Union{Directed, Undirected}} = graph_from_edges(T, collect(edges), n_vertices)
 
 @doc raw"""
-    tropical_median_consensus(trees::Vararg{PhylogeneticTree, N}) where {N}
+  graph_from_edges(::Type{T}, edges::Vector{Vector{Int}}) where {T <:Union{Directed, Undirected}}
 
-Computes the tropical median consensus tree of any number of phylogenetic trees
-given as parameters.
+Creates a graph from a vector of edges. Optionally, you could input the number of vertices, but if this number is lower than the maximum vertex in the edges, this argument will be ignored.
 
-# Examples
-Compute the tropical median consensus of three trees and print one of its
-Newick representations.
-
+# Examples 1
 ```jldoctest
-julia> t1 = phylogenetic_tree(Float64, "((H:30,(C:10,B:10):20):10,G:40);");
+julia> G = graph_from_edges(Undirected, [[1,3],[3,5],[4,5],[2,4],[2,3]])
+Undirected graph with 5 nodes and the following edges:
+(3, 1)(3, 2)(4, 2)(5, 3)(5, 4)
 
-julia> t2 = phylogenetic_tree(Float64, "(((H:10,C:10):20,B:30):10,G:40);");
+```
 
-julia> t3 = phylogenetic_tree(Float64, "((H:25,C:25):15,(B:15,G:15):25);");
+```
+# Examples 2
+```jldoctest
+julia> G = graph_from_edges(Undirected,[[1,3]])
+Undirected graph with 3 nodes and the following edges:
+(3, 1))
 
-julia> tc = tropical_median_consensus(t1, t2, t3);
-
-julia> newick(tc)
-"G:40,(B:35,(C:30,H:30):5):5;"
 ```
 """
-function tropical_median_consensus(trees::Vararg{PhylogeneticTree, N}) where {N}
-  return tropical_median_consensus(collect(trees))
-end
+graph_from_edges(::Type{T},
+edges::Vector{Vector{Int}},
+n_vertices::Int=-1) where {T <: Union{Directed, Undirected}} = graph_from_edges(T, [Edge(e[1], e[2]) for e in edges], n_vertices)
+
+graph_from_edges(
+edges::Vector{Vector{Int}},
+n_vertices::Int=-1) = graph_from_edges(Undirected, [Edge(e[1], e[2]) for e in edges], n_vertices)
--- src/Combinatorics/Graphs/structs.jl
+++ src/Combinatorics/Graphs/structs.jl
@@ -5,7 +5,3 @@ struct Graph{T <: Union{Directed, Undirected}}
   pm_graph::Polymake.Graph{T}
 end
 
-mutable struct PhylogeneticTree{T <: Union{Float64, QQFieldElem}}
-  pm_ptree::Polymake.LibPolymake.BigObjectAllocated
-end
-
