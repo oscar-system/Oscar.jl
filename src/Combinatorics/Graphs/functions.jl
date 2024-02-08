2d9ef00092634c375e3e910d84f8a08bb7aa44bb
Antony Della Vecchia <vecchia@math.tu-berlin.de>
2024-02-08 18:11:03 +0100

    clean whitespace

--- src/Combinatorics/Graphs/functions.jl
+++ src/Combinatorics/Graphs/functions.jl
@@ -1084,11 +1084,11 @@ end
 
 function graph_from_edges(::Type{T},
                           edges::Vector{Edge},
-                          n_vertices::Int=-1) where {T <: Union{Directed, Undirected}} 
+                          n_vertices::Int=-1) where {T <: Union{Directed, Undirected}}
 
   n_needed = maximum(reduce(append!,[[src(e),dst(e)] for e in edges]))
   @req (n_vertices >= n_needed || n_vertices < 0)  "n_vertices must be at least the maximum vertex in the edges"
-   
+
   g = Graph{T}(max(n_needed, n_vertices))
   for e in edges
     add_edge!(g, src(e), dst(e))
@@ -1130,4 +1130,3 @@ function graph_from_edges(edges::Vector{Vector{Int}},
                           n_vertices::Int=-1)
   return graph_from_edges(Undirected, [Edge(e[1], e[2]) for e in edges], n_vertices)
 end
-
