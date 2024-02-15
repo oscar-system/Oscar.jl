5d079e19b624d3709363f56f2f599462b0af13df
Benjamin Lorenz <lorenz@math.tu-berlin.de>
2024-02-15 16:10:59 +0100

    serialization: add helpers to deconstruct polymake bigobject to dict
    currently with a few warnings that might be removed later

--- src/Serialization/polymake.jl
+++ src/Serialization/polymake.jl
@@ -64,3 +64,60 @@ function load_from_polymake(jsondict::Dict{Symbol, Any})
   end
 end
 
+_pmdata_for_oscar(bo::Polymake.BigObject, coeff::Union{Field,Nothing}) = _bigobject_to_dict(bo, coeff)
+
+_pmdata_for_oscar(::Nothing, coeff::Union{Field,Nothing}) = nothing
+_pmdata_for_oscar(v::Union{Bool,Int64,Float64,String}, coeff::Union{Field,Nothing}) = v
+if Polymake.CxxWrap.CxxLong != Int64
+  _pmdata_for_oscar(i::Polymake.CxxWrap.CxxLong, coeff::Union{Field,Nothing}) = Int64(i)
+end
+
+_pmdata_for_oscar(im::IncidenceMatrix, coeff::Union{Field,Nothing}) = im
+
+_pmdata_for_oscar(g::Polymake.Graph{T}, coeff::Union{Field,Nothing}) where T = Graph{T}(g)
+
+_pmdata_for_oscar(m::Polymake.Matrix, coeff::Union{Field,Nothing}) = matrix(coeff, m)
+_pmdata_for_oscar(m::Polymake.Matrix{<:Polymake.Integer}, coeff::Union{Field,Nothing}) = matrix(ZZ, m)
+_pmdata_for_oscar(m::Polymake.Matrix{<:Polymake.Rational}, coeff::Union{Field,Nothing}) = matrix(QQ, m)
+
+_pmdata_for_oscar(m::Polymake.SparseMatrix, coeff::Union{Field,Nothing}) = _pmdata_for_oscar(Polymake.common.dense(m), coeff)
+
+_pmdata_for_oscar(v::Polymake.Vector, coeff::Union{Field,Nothing}) = collect(tt, map(F, v))
+_pmdata_for_oscar(v::Polymake.Vector{<:Polymake.Integer}, coeff::Union{Field,Nothing}) = collect(ZZRingElem, map(ZZ, v))
+_pmdata_for_oscar(v::Polymake.Vector{<:Polymake.Rational}, coeff::Union{Field,Nothing}) = collect(QQFieldElem, map(QQ, v))
+
+_pmdata_for_oscar(v::Polymake.SparseVector, coeff::Union{Field,Nothing}) = _pmdata_for_oscar(Polymake.common.dense(v), coeff)
+
+_pmdata_for_oscar(s::Polymake.Integer, coeff::Union{Field,Nothing}) = ZZ(s)
+_pmdata_for_oscar(s::Polymake.Rational, coeff::Union{Field,Nothing}) = QQ(s)
+_pmdata_for_oscar(s::Polymake.OscarNumber, coeff::Union{Field,Nothing}) = coeff(s)
+
+_pmdata_for_oscar(s::Polymake.CxxWrap.StdString, coeff::Union{Field,Nothing}) = String(s)
+
+_pmdata_for_oscar(a::Polymake.Array, coeff::Union{Field,Nothing}) = [_pmdata_for_oscar(e, coeff) for e in a]
+_pmdata_for_oscar(s::Polymake.Set, coeff::Union{Field,Nothing}) = Set(_pmdata_for_oscar(e, coeff) for e in s)
+
+
+function _bigobject_to_dict(bo::Polymake.BigObject, coeff::Union{Field,Nothing})
+  data = Dict{String,Any}()
+  data["_type"] = Polymake.type_name(bo)
+  data["_coeff"] = coeff
+  for pname in Polymake.list_properties(bo)
+    p = Polymake.give(bo, pname)
+    if p isa Polymake.PropertyValue
+      @warn "missing c++ mapping: skipping $pname of type $(Polymake.typeinfo_string(p, true))"
+    else
+      try
+        obj = _pmdata_for_oscar(p, coeff)
+        data[pname] = obj
+      catch MethodError
+        @warn "failed to convert $pname of type $(typeof(p)) to Oscar, skipping"
+      end
+    end
+  end
+  data
+end
+
+function _polyhedral_object_as_dict(x::Oscar.PolyhedralObjectUnion)
+  return _bigobject_to_dict(Oscar.pm_object(x), coefficient_field(x))
+end
