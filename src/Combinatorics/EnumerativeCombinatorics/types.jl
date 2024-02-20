526c45b0adf7493a5a3e98ed1281dd3bed56fec6
Morgan Rodgers <morgan.joaquin@gmail.com>
2024-02-20 13:11:27 +0100

    Implement `show` method for Partition iterator, rename type

--- src/Combinatorics/EnumerativeCombinatorics/types.jl
+++ src/Combinatorics/EnumerativeCombinatorics/types.jl
@@ -10,6 +10,12 @@ end
 
 struct PartitionSet{T<:IntegerUnion}
   n::T
+
+  function PartitionSet(n::T) where T<:IntegerUnion
+    @req n >= 0 "n >= 0 required"
+    return new{T}(n)
+  end
+  
 end
 
 ################################################################################
