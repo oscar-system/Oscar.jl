5cceb0118c5f96e843b299634a5a512d5661d62d
Erik Paemurru <computeralgebra@runbox.com>
2024-02-21 18:20:42 +0100

    Fix missed Spec -> AffineScheme

--- src/AlgebraicGeometry/Schemes/AffineSchemes/Objects/Attributes.jl
+++ src/AlgebraicGeometry/Schemes/AffineSchemes/Objects/Attributes.jl
@@ -481,7 +481,7 @@ julia> codim(Y)
   return dim(ideal(ambient_coordinate_ring(X), [zero(ambient_coordinate_ring(X))])) - dim(X)
 end
 
-function degree(X::Spec{BRT, RT}; check::Bool=true) where {BRT<:Field, RT}
+function degree(X::AffineScheme{BRT, RT}; check::Bool=true) where {BRT<:Field, RT}
   @check dim(X) == 0 "the affine scheme X needs to be zero-dimensional"
   get_attribute!(X, :degree) do
     return vector_space_dimension(OO(X))
