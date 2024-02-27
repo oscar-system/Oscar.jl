e44be1cd4317870fe4f0c2e29e729d45d7350bc2
Erik Paemurru <computeralgebra@runbox.com>
2024-02-27 15:48:41 +0100

    Print one-line banner for small terminals

--- src/Oscar.jl
+++ src/Oscar.jl
@@ -39,15 +39,17 @@ if Sys.iswindows()
 end
 
 function _print_banner()
-  println(
-    raw"""  ___   ____   ____    _    ____
-           / _ \ / ___| / ___|  / \  |  _ \   |  Combining ANTIC, GAP, Polymake, Singular
-          | | | |\___ \| |     / _ \ | |_) |  |  Type "?Oscar" for more information
-          | |_| | ___) | |___ / ___ \|  _ <   |  Manual: https://docs.oscar-system.org
-           \___/ |____/ \____/_/   \_\_| \_\  |  Version """ * "$VERSION_NUMBER")
+  if displaysize(stdout)[2] >= 79
+    println(
+      raw"""  ___   ____   ____    _    ____
+             / _ \ / ___| / ___|  / \  |  _ \   |  Combining ANTIC, GAP, Polymake, Singular
+            | | | |\___ \| |     / _ \ | |_) |  |  Type "?Oscar" for more information
+            | |_| | ___) | |___ / ___ \|  _ <   |  Manual: https://docs.oscar-system.org
+             \___/ |____/ \____/_/   \_\_| \_\  |  Version """ * "$VERSION_NUMBER")
+  else
+    println("OSCAR $VERSION_NUMBER  https://docs.oscar-system.org  Type \"?Oscar\" for help")
+  end
 end
-
-
 
 function __init__()
   if Sys.iswindows()
