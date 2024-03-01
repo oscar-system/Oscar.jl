07bcbd9e4a217117ebf62bec29af6edd4a2da5d3
antonydellavecchia <antonydellavecchia@gmail.com>
2024-03-01 13:21:09 +0100

    fixes loading polymake simplicial complex and other types

--- src/Serialization/polymake.jl
+++ src/Serialization/polymake.jl
@@ -36,13 +36,15 @@ function load_from_polymake(::Type{T}, jsondict::Dict{Symbol, Any}) where {
   T<:Union{Cone{<:scalar_types}, Polyhedron{<:scalar_types}, PolyhedralFan{<:scalar_types}, 
            PolyhedralComplex{<:scalar_types}, SubdivisionOfPoints{<:scalar_types}, SimplicialComplex}}
   inner_object = Polymake.call_function(:common, :deserialize_json_string, json(jsondict))
-  if T <: PolyhedralObject{QQFieldElem}
-     return T(inner_object)
-   elseif T <: PolyhedralObject{Float64}
-     return T(inner_object, AbstractAlgebra.Floats{Float64}())
-   else
+  if T <: PolyhedralObject{Float64}
+    return T(inner_object, AbstractAlgebra.Floats{Float64}())
+  else
+    try
+      return T(inner_object)
+    catch e
       error("Unsupported object type $T for loading polymake object")
-   end
+    end
+  end
 end
 
 
