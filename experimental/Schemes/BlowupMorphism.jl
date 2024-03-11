153e848ef626fbbcb87e971247be5c764d37c1d2
HechtiDerLachs <zach@mathematik.uni-kl.de>
2024-03-11 11:23:46 +0100

    Fix doctests.

--- experimental/Schemes/BlowupMorphism.jl
+++ experimental/Schemes/BlowupMorphism.jl
@@ -213,8 +213,8 @@ julia> bl = blow_up(A3, I)
 Blowup
   of scheme over QQ covered with 1 patch
     1b: [x, y, z]   affine 3-space
-  in prime ideal sheaf on Scheme over QQ covered with 1 patch
-    1b: [x, y, z]   affine 3-space extended from Ideal (x, y, z) on Affine 3-space
+  in prime ideal sheaf on scheme over QQ covered with 1 patch
+    1b: [x, y, z]   affine 3-space extended from ideal (x, y, z) on affine 3-space
 with domain
   scheme over QQ covered with 3 patches
     1a: [(s1//s0), (s2//s0), x]   scheme(0, 0, 0)
