0b1605b014e18b4ba3e9432f7adad9d0122776bc
HechtiDerLachs <zach@mathematik.uni-kl.de>
2024-03-13 16:20:38 +0100

    Create ideal sheaves from homogeneous ideals in the cox-ring in general.

--- experimental/Schemes/ToricIdealSheaves/constructors.jl
+++ experimental/Schemes/ToricIdealSheaves/constructors.jl
@@ -53,51 +53,56 @@ function IdealSheaf(X::NormalToricVariety, I::MPolyIdeal)
   # We currently only support this provided that the following conditions are met:
   # 1. All maximal cones are smooth, i.e. the fan is smooth/X is smooth.
   # 2. The dimension of all maximal cones matches the dimension of the fan.
-  @req is_smooth(X) "Currently, ideal sheaves are only supported for smooth toric varieties"
   @req is_pure(X) "Currently, ideal sheaves require that all maximal cones have the dimension of the variety"
 
   ideal_dict = IdDict{AbsAffineScheme, Ideal}()
 
-  # We need to dehomogenize the ideal I in the Cox ring S to the local
-  # charts U_sigma, with sigma a cone in the fan of the variety.
-  # To this end we use Proposition 5.2.10 from Cox-Little-Schenck, p. 223ff.
-  # Abstractly, the chart U_sigma is isomorphic to C^n with n the number
-  # of ray generators of sigma, owing to the assumptions 1 an 2 above.
-  # Note however that the coordinates of C^n are not one to one to
-  # the homogeneous coordinates of the Cox ring. Rather, the coordinates 
-  # of the affine pieces correspond to the `hilbert_basis(polarize(sigma))`.
+  if is_smooth(X)
+    # We need to dehomogenize the ideal I in the Cox ring S to the local
+    # charts U_sigma, with sigma a cone in the fan of the variety.
+    # To this end we use Proposition 5.2.10 from Cox-Little-Schenck, p. 223ff.
+    # Abstractly, the chart U_sigma is isomorphic to C^n with n the number
+    # of ray generators of sigma, owing to the assumptions 1 an 2 above.
+    # Note however that the coordinates of C^n are not one to one to
+    # the homogeneous coordinates of the Cox ring. Rather, the coordinates 
+    # of the affine pieces correspond to the `hilbert_basis(polarize(sigma))`.
 
-  # TODO: In the long run we should think about making the creation of the 
-  # following dictionary lazy. But this requires partial rewriting of the 
-  # ideal sheaves as a whole, so we postpone it for the moment.
+    # TODO: In the long run we should think about making the creation of the 
+    # following dictionary lazy. But this requires partial rewriting of the 
+    # ideal sheaves as a whole, so we postpone it for the moment.
 
-  IM = maximal_cones(IncidenceMatrix, X)
-  for (k, U) in enumerate(affine_charts(X))
+    IM = maximal_cones(IncidenceMatrix, X)
+    for (k, U) in enumerate(affine_charts(X))
 
-    # We first create the morphism \pi_s* from p. 224, l. 3.
-    indices = [k for k in row(IM, k)]
-    help_ring, x_rho = polynomial_ring(QQ, ["x_$j" for j in indices])
-    imgs_phi_star = [j in indices ? x_rho[findfirst(k->k==j, indices)] : one(help_ring) for j in 1:n_rays(X)]
-    phi_s_star = hom(cox_ring(X), help_ring, imgs_phi_star)
+      # We first create the morphism \pi_s* from p. 224, l. 3.
+      indices = [k for k in row(IM, k)]
+      help_ring, x_rho = polynomial_ring(QQ, ["x_$j" for j in indices])
+      imgs_phi_star = [j in indices ? x_rho[findfirst(k->k==j, indices)] : one(help_ring) for j in 1:n_rays(X)]
+      phi_s_star = hom(cox_ring(X), help_ring, imgs_phi_star)
 
-    # Now we need to create the inverse of alpha*.
-    imgs_alpha_star = elem_type(help_ring)[]
-    for m in hilbert_basis(weight_cone(U))
-      img = one(help_ring)
-      for j in 1:length(indices)
-        u_rho = matrix(ZZ,rays(X))[indices[j]:(indices[j]),:]
-        expo = (u_rho*m)[1]
-        img = img * x_rho[j]^expo
+      # Now we need to create the inverse of alpha*.
+      imgs_alpha_star = elem_type(help_ring)[]
+      for m in hilbert_basis(weight_cone(U))
+        img = one(help_ring)
+        for j in 1:length(indices)
+          u_rho = matrix(ZZ,rays(X))[indices[j]:(indices[j]),:]
+          expo = (u_rho*m)[1]
+          img = img * x_rho[j]^expo
+        end
+        push!(imgs_alpha_star, img)
       end
-      push!(imgs_alpha_star, img)
-    end
-    alpha_star = hom(OO(U), help_ring, imgs_alpha_star)
+      alpha_star = hom(OO(U), help_ring, imgs_alpha_star)
 
-    # TODO: There should be better ways to create this map!
-    # Presumably, one can invert the matrix with the `expo`s as entries?
-    # @larskastner If you can confirm this, let's change this.
-    alpha_star_inv = inverse(alpha_star)
-    ideal_dict[U] = ideal(OO(U), [alpha_star_inv(phi_s_star(g)) for g in gens(I)])
+      # TODO: There should be better ways to create this map!
+      # Presumably, one can invert the matrix with the `expo`s as entries?
+      # @larskastner If you can confirm this, let's change this.
+      alpha_star_inv = inverse(alpha_star)
+      ideal_dict[U] = ideal(OO(U), [alpha_star_inv(phi_s_star(g)) for g in gens(I)])
+    end
+  else
+    for (k, U) in enumerate(affine_charts(X))
+      ideal_dict[U] = _dehomogenize_to_chart(X, I, k)
+    end
   end
 
   return IdealSheaf(X, ideal_dict, check=true) # TODO: Set the check to false eventually.
@@ -142,21 +147,14 @@ Theorem 3.2.6.
 function ideal_sheaf(X::NormalToricVariety, tau::Cone)
   @assert tau in fan(X) "cone must be in the fan of the variety"
   A = _to_matrix(rays(tau))
-  @show matrix(ZZ, rays(tau))
   A = matrix(ZZ, rays(tau))
   (r, K) = kernel(A)
-  @show K
-  @show typeof(K)
   w = _colum_vectors_to_rays(K)
-  @show w
   w = vcat(w, -w)
-  @show w
   # TODO: Can we use a direct command instead of this hack?
   tau_perp = positive_hull(w)
   # Maybe it's a mistake in the book and we really need the dual?
   #tau_perp = polarize(tau)
-  @show rays(tau_perp)
-  @show lineality_space(tau_perp)
   ideal_dict = IdDict{AbsAffineScheme, Ideal}()
   # We are using Equation (3.2.7) in CLS to determine the local 
   # form of the ideal.
@@ -164,7 +162,6 @@ function ideal_sheaf(X::NormalToricVariety, tau::Cone)
     cu = cone(U)
     cu_pol = weight_cone(U)
     inter = intersect(tau_perp, cu_pol)
-    @show rays(inter)
     @assert is_pointed(inter) "intersection must be a pointed cone"
     hb_inter = hilbert_basis(inter)
     if iszero(length(hb_inter))
@@ -172,23 +169,15 @@ function ideal_sheaf(X::NormalToricVariety, tau::Cone)
       #ideal_dict[U] = ideal(OO(U), elem_type(OO(U))[])
       continue
     end
-    @show hb_inter
     B = _to_integer_column_matrix(hb_inter)
     hb_cu_pol = hilbert_basis(cu_pol)
     A = _to_integer_column_matrix(hb_cu_pol)
     C = identity_matrix(ZZ, ncols(A))
-    @show A
-    @show B
-    @show C
     # For some reason `solve_mixed` returns the transpose of the actual solution, 
     # so we have to correct this.
     S = transpose(solve_mixed(ZZMatrix, A, B, C))
-    @show S
     x = gens(OO(U))
-    @show x
-    @show prod(x[i]^S[i, 1] for i in 1:length(x); init=one(OO(U)))
     g = elem_type(OO(U))[prod(x[i]^S[i, j] for i in 1:length(x); init=one(OO(U))) for j in 1:ncols(S)] # The generators of the ideal on U
-    @show g
     ideal_dict[U] = ideal(OO(U), g)
   end
   #return IdealSheaf(X, ideal_dict, check=true) #TODO: Set to false
@@ -205,3 +194,28 @@ function Base.in(tau::Cone, Sigma::PolyhedralFan)
   all_cones = cones(Sigma)
   return any(j->all(i -> all_cones[j, i], indices), 1:nrows(all_cones))
 end
+
+function _dehomogenize_to_chart(X::NormalToricVariety, I::MPolyIdeal, k::Int)
+  S = cox_ring(X)
+  r = matrix(ZZ, rays(X))
+  max_cones = maximal_cones(X)
+  sigma = max_cones[k]
+  ray_indices_sigma = ray_indices(maximal_cones(X))[k, :]
+  loc_vars = [x for (i, x) in enumerate(gens(S)) if !ray_indices_sigma[i]]
+  inv_set = MPolyPowersOfElement(S, loc_vars)
+  S_loc, loc_map = localization(S, inv_set)
+  I_loc = loc_map(I)
+  
+  U = affine_charts(X)[k]
+  hb_U = hilbert_basis(polarize(sigma)) # corresponds to the variables of OO(U)
+  x = gens(S_loc)
+  img_gens = [prod(x[l]^Int(dot(hb_U[j], r[l, :])) for l in 1:nrows(r); init=one(S_loc)) for j in 1:length(hb_U)]
+  A, pr = quo(S_loc, I_loc)
+  y = pr.(img_gens)
+  # preimage is not implemented for these maps, but kernel works. So we use that for the moment.
+  #beta = hom(OO(U), S_loc, img_gens; check=true)
+  help = hom(OO(U), A, y)
+  #J = preimage(beta, I_loc)
+  kernel(help)
+end
+
