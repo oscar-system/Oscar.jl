969aa7c37f77dd5dc152085950b5c43fdd9ed0f8
anne <anne@krueger-berg.de>
2024-03-15 14:35:55 +0100

    test for locus of order at least b

--- test/AlgebraicGeometry/Schemes/Resolution_structure.jl
+++ test/AlgebraicGeometry/Schemes/Resolution_structure.jl
@@ -37,8 +37,18 @@ end
 @testset "order of an ideal" begin
   R,(x,y,z) = polynomial_ring(QQ,3)
   W = CoveredScheme(spec(R))
-  I = ideal(R,[x*y,x^3+y^3+z^3-x*y*z])
+  I = ideal(R,[(x+y)^3+z^4])
   IS = IdealSheaf(W,affine_charts(W)[1], gens(I))
-  Y = Oscar.max_order_locus(IS)
+  IY = Oscar.max_order_locus(IS)
+  decomp = Oscar.maximal_associated_points(IY)
+  @test length(decomp) == 1
+  @test decomp[1] == IdealSheaf(W,affine_charts(W)[1],[z,x+y])
+  JS = IdealSheaf(W, affine_charts(W)[1],[x^2*y^2-z^5,x^3])
+  li = Oscar._delta_list(JS)
+  @test length(li) == 3
+  @test li[1] == JS
+  @test Oscar.radical(li[2]) == IdealSheaf(W,affine_charts(W)[1],[x,z])
+  @test Oscar.radical(li[2]) == Oscar.radical(Oscar.locus_of_order_geq_b(JS,2))
+  @test Oscar.radical(li[3]) == IdealSheaf(W,affine_charts(W)[1],[x,y,z])
 end
 
