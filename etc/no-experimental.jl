c5c82d0f4645ab2b5ac7d8ff3a8ffadbd7998687
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2024-03-18 17:31:29 +0100

    Make whitelist minimal (with current master)

--- etc/no-experimental.jl
+++ etc/no-experimental.jl
@@ -29,29 +29,14 @@ function main(args)
       "experimental2",
       [
         "Experimental.jl",              # needs to stay here forever
-        "BasisLieHighestWeight",        # TODO: remove from this list
-        "DoubleAndHyperComplexes",      # TODO: remove from this list
-        "EnumerativeCombinatorics",     # TODO: remove from this list
-        "ExperimentalTemplate",         # TODO: remove from this list
-        "ExteriorAlgebra",              # TODO: remove from this list
-        "FTheoryTools",                 # TODO: remove from this list
-        "GaloisGrp",                    # TODO: remove from this list
-        "GITFans",                      # TODO: remove from this list
-        "GModule",                      # TODO: remove from this list
-        "IntersectionTheory",           # TODO: remove from this list
-        "InvariantTheory",              # TODO: remove from this list
-        "LieAlgebras",                  # TODO: remove from this list
-        "LinearQuotients",              # TODO: remove from this list
-        "MatrixGroups",                 # TODO: remove from this list
-        "MatroidRealizationSpaces",     # TODO: remove from this list
-        "ModStd",                       # TODO: remove from this list
-        "OrthogonalDiscriminants",      # TODO: remove from this list
-        "QuadFormAndIsom",              # TODO: remove from this list
-        "Rings",                        # TODO: remove from this list
-        "Schemes",                      # TODO: remove from this list
-        "SetPartitions",                # TODO: remove from this list
-        "StandardFiniteFields",         # TODO: remove from this list
-        "SymmetricIntersections",       # TODO: remove from this list
+        "DoubleAndHyperComplexes",      # TODO: remove from this list. `MethodError: no method matching simplify(::SubquoModule{MPolyQuoRingElem{MPolyDecRingElem{FqMPolyRingElem, AbstractAlgebra.Generic.MPoly{FqMPolyRingElem}}}})`
+        "ExteriorAlgebra",              # TODO: remove from this list. `undefined binding 'exterior_algebra' in `@docs` block in src/NoncommutativeAlgebra/PBWAlgebras/quotients.md:40-42` and `Error During Test at /home/runner/work/Oscar.jl/Oscar.jl/test/Rings/PBWAlgebraQuo.jl:41`
+        "GaloisGrp",                    # TODO: remove from this list. `no docs found for 'fixed_field(C::Oscar.GaloisGrp.GaloisCtx, s::Vector{PermGroup})' in `@docs` block in src/NumberTheory/galois.md:275-278`
+        "GModule",                      # TODO: remove from this list. many doctest failures and `MethodError: no method matching numerator(::QQPolyRingElem)`
+        "InvariantTheory",              # TODO: remove from this list. `undefined binding 'linearly_reductive_group' in `@docs` block in src/InvariantTheory/reductive_groups.md:53-55` and more docs errors`
+        "ModStd",                       # TODO: remove from this list. `MethodError: no method matching monomial(::QQMPolyRing, ::Vector{Int64})` and many similar errors
+        "Rings",                        # TODO: remove from this list. used by Rings/binomial_ideals.jl, see https://github.com/oscar-system/Oscar.jl/blob/13282dfd07b6aee58e433a45353f48261cda787b/src/Oscar.jl#L268
+        "Schemes",                      # TODO: untangle src/AlgebraicGeometry/Schemes/ and experimental/Schemes/
       ],
     )
   elseif command == "cleanup"
