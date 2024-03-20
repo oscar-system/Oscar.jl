06f14037144f792c9877c1b5f5b6510cd3c6c19a
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2024-03-20 15:00:43 +0100

    Remove inconvienient script

--- experimental/Experimental.jl
+++ experimental/Experimental.jl
@@ -26,10 +26,27 @@ append!(exppkgs, orderedpkgs)
 # force trigger recompile when folder changes
 include_dependency(".")
 
+# for the NoExperimental CI job, remove all experimental packages from the inclusion lists
+if haskey(ENV, "OSCAR_TEST_NOEXPERIMENTAL")
+  # List of all experimental packages that are currently needed for the CI job to succeed.
+  # Eventually, this list should be empty.
+  whitelist = String[
+    "DoubleAndHyperComplexes",    # `MethodError: no method matching simplify(::SubquoModule{MPolyQuoRingElem{MPolyDecRingElem{FqMPolyRingElem, AbstractAlgebra.Generic.MPoly{FqMPolyRingElem}}}})`
+    "ExteriorAlgebra",            # `undefined binding 'exterior_algebra' in `@docs` block in src/NoncommutativeAlgebra/PBWAlgebras/quotients.md:40-42` and `Error During Test at /home/runner/work/Oscar.jl/Oscar.jl/test/Rings/PBWAlgebraQuo.jl:41`
+    "GaloisGrp",                  # `no docs found for 'fixed_field(C::Oscar.GaloisGrp.GaloisCtx, s::Vector{PermGroup})' in `@docs` block in src/NumberTheory/galois.md:275-278`
+    "GModule",                    # many doctest failures and `MethodError: no method matching numerator(::QQPolyRingElem)`
+    "InvariantTheory",            # `undefined binding 'linearly_reductive_group' in `@docs` block in src/InvariantTheory/reductive_groups.md:53-55` and more docs errors`
+    "ModStd",                     # `MethodError: no method matching monomial(::QQMPolyRing, ::Vector{Int64})` and many similar errors
+    "Rings",                      # used by Rings/binomial_ideals.jl, see https://github.com/oscar-system/Oscar.jl/blob/13282dfd07b6aee58e433a45353f48261cda787b/src/Oscar.jl#L268
+    "Schemes",                    # TODO: untangle src/AlgebraicGeometry/Schemes/ and experimental/Schemes/
+  ]
+  filter!(in(whitelist), exppkgs)
+  filter!(in(whitelist), oldexppkgs)
+end
+
 # Error if something is incomplete in experimental
 for pkg in exppkgs
   if !isfile(joinpath(expdir, pkg, "src", "$pkg.jl"))
-    haskey(ENV, "OSCAR_TEST_NOEXPERIMENTAL") && continue
     error("experimental/$pkg is incomplete: $pkg/src/$pkg.jl missing. See the documentation at https://docs.oscar-system.org/dev/Experimental/intro/ for details.")
   end
   path = joinpath(expdir, pkg, "test")
@@ -43,7 +60,6 @@ end
 # Force some structure for `oldexppkgs`
 for pkg in oldexppkgs
   if !isfile(joinpath(expdir, pkg, "$pkg.jl"))
-    haskey(ENV, "OSCAR_TEST_NOEXPERIMENTAL") && continue
     error("experimental/$pkg is incomplete: $pkg/$pkg.jl missing. Please fix this or remove $pkg from `oldexppkgs`.")
   end
   # Load the package
