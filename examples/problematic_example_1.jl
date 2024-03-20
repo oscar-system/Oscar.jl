b1166bd12ce088e24148d22027158edf0e142085
Kamillo Ferry <kafe@kafe.dev>
2024-03-20 13:45:03 +0100

    Add problematic example that does not work (for now)

---
+++ examples/problematic_example_1.jl
@@ -0,0 +1,15 @@
+using Oscar
+using Oscar.GroebnerWalk
+
+R, (x,y,z,u,v) = polynomial_ring(QQ, ["x","y","z","u","v"])
+
+o1 = weight_ordering([1,1,1,0,0], degrevlex(R))
+o2 = weight_ordering([0,0,0,1,1], degrevlex(R))
+
+I1 = ideal([
+    u + u^2 - 2*v - 2*u^2*v + 2*u*v^2 - x,
+    -6*u + 2*v + v^2 - 5*v^3 + 2*u*v^2 - 4*u^2*v^2 - y,
+    -2 + 2*u^2 + 6*v - 3*u^2*v^2 - z
+])
+
+groebner_walk(I1, o1, o2)
