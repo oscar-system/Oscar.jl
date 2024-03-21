a65b60bcd34141b426b343804e93941a117bb8d9
Kamillo Ferry <kafe@kafe.dev>
2024-03-21 02:21:47 +0100

    Replace manual computation of maximum

--- src/walk.jl
+++ src/walk.jl
@@ -1464,22 +1464,10 @@ end
 # computes the representation of the matrixorder defined by T.
 function representation_vector(G::Oscar.IdealGens, T::Matrix{Int})
   n = size(T)[1]
-  M = 0
-  for i in 1:n
-    for j in 1:n
-      temp = T[i, j]
-      if M < temp
-        M = temp
-      end
-    end
-  end
-  d0 = 0
-  for g in Singular.gens(G)
-    temp = deg(g, n)
-    if d0 < temp
-      d0 = temp
-    end
-  end
+  M = maximum(T)
+  
+  d0 = maximum(g -> deg(g, n), gens(g))
+
   d = M * (2 * d0^2 + (n + 1) * d0)
   w = d^(n - 1) * T[1, :]
   for i in 2:n
