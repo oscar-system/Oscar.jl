6dfc46f6f38e9fee85fff5e9cbc3c42d78363515
Kamillo Ferry <kafe@kafe.dev>
2024-03-21 02:22:14 +0100

    Add verbosity scope for `groebner_walk`

--- src/GroebnerWalk.jl
+++ src/GroebnerWalk.jl
@@ -6,11 +6,9 @@ include("walk.jl")
 export groebner_walk
 
 function __init__()
-    add_verbosity_scope(:intermediate)
-    add_verbosity_scope(:detailed)
+    add_verbosity_scope(:groebner_walk)
 
-    set_verbosity_level(:intermediate, 1)
-    set_verbosity_level(:detailed, 2)
+    set_verbosity_level(:groebner_walk, 0)
 end
 
 end
