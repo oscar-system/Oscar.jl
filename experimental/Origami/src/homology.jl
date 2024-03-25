885cbb029462d79a8933bda9ca51a43001aea207
s-ertl-github <simon-ertl@gmx.de>
2024-03-25 21:08:48 +0100

    added some more functionality

---
+++ experimental/Origami/src/homology.jl
@@ -0,0 +1,12 @@
+function symplectic_basis_of_homology(O::Origami)
+    return GAP.Globals.SymplecticBasisOfHomology(GapObj(O))
+end
+
+function has_spin_structure(O::Origami)
+    return GAP.Globals.HasSpinStructure(GapObj(O))
+end
+
+function spin_parity(O::Origami)
+    return GAP.Globals.SpinParity(GapObj(O))
+end
+
