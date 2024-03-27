cbf038cb2268c219db8477b6753cdca7616b3005
Alexej Jordan <ajordan@hotmail.de>
2024-03-27 14:08:52 +0100

    _is_prismic_or_antiprismic additionally checks the graph

--- src/PolyhedralGeometry/Polyhedron/properties.jl
+++ src/PolyhedralGeometry/Polyhedron/properties.jl
@@ -1481,7 +1481,21 @@ function _is_prismic_or_antiprismic(P::Polyhedron)
   n = nvfs[a][1]
   # P has to have exactly n vertices and
   # the amount of squares needs to be n or the amount of triangles needs to be 2n
-  return 2n == n_vertices(P) && (5 - m)*n == nvfs[b][2]
+  2n == n_vertices(P) && (5 - m)*n == nvfs[b][2] || return false
+  dg = dualgraph(P)
+  ngon_is = findall(x -> x == n, nvf)
+  has_edge(dg, ngon_is...) && return false
+  rem_vertex!(dg, ngon_is[2])
+  rem_vertex!(dg, ngon_is[1])
+  degs = [length(neighbors(dg, i)) for i in 1:n_vertices(dg)]
+  degs == fill(2, n_vertices(dg)) && is_connected(dg) || return false
+  dg = dualgraph(P)
+  if m == 3
+    bigdg = Polymake.graph.Graph(ADJACENCY = dg.pm_graph)
+    return bigdg.BIPARTITE
+  else
+    return true
+  end
 end
 
 @doc raw"""
