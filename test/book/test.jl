993d3fcf7fd32596cdca0f04353ef89c23124fe8
Benjamin Lorenz <benlorenz@users.noreply.github.com>
2024-04-16 14:38:23 +0200

    booktest: make path filter more robust (#3613)

--- test/book/test.jl
+++ test/book/test.jl
@@ -53,7 +53,7 @@ isdefined(Main, :FakeTerminals) || include(joinpath(pkgdir(REPL),"test","FakeTer
       # remove timings
       result = replace(result, r"^\s*[0-9\.]+ seconds \(.* allocations: .*\)$"m => "<timing>\n")
       # this removes the package version slug, filename and linenumber
-      result = replace(result, r" @ \w* ?~/\.julia/packages/(?:Nemo|Hecke|AbstractAlgebra|Polymake)/\K[\w\d]+/.*\.jl:\d+"m => "")
+      result = replace(result, r"^(.* @ \w* ?)~?/[\w/.-]*(Nemo|Hecke|AbstractAlgebra|Polymake)(?:\.jl)?/[\w\d]+/.*\.jl:\d+"m => s"\1 \2")
       lafter = length(result)
     end
     return strip(result)
