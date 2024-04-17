a1c9dfb3862ca8a8a3b4c7a018529a546bed83cb
bacher <bacher@mis.mpg.de>
2024-04-17 16:11:46 +0200

    added runtests

--- experimental/GraphicalModels/test/runtests.jl
+++ experimental/GraphicalModels/test/runtests.jl
@@ -1,4 +1,7 @@
-@testset "Experimental template tests" begin
-  S = Oscar.ExampleStruct(5)
-  @test 5 == Oscar.my_access_func(S)
+@testset "Graphical Models tests" begin
+  tree = graph_from_edges(Directed,[[4,1],[4,2],[4,3]])
+  model = jukes_cantor_model(tree)
+  @test is_isomorphic(graph(model), graph_from_edges(Directed,[[4,1],[4,2],[4,3]]))
+  @test Oscar.number_states(model) == 4
+  @test model isa Oscar.PhylogeneticModel
 end
