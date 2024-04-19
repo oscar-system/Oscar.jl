a4cb717413c85e76ab8fb9f60c929dfbcdf27a25
Ben Hollering <benjamin.hollering@mis.mpg.de>
2024-04-19 17:46:20 +0200

    added parameterizations and vanishing ideals for directed and undirected gaussian graphical models

--- experimental/AlgebraicStatistics/src/GraphicalModels.jl
+++ experimental/AlgebraicStatistics/src/GraphicalModels.jl
@@ -1,28 +1,111 @@
-struct GraphicalModel{G, T}
-    graph::Graph{G}
-    ring::T
-    param_ring::Ring
-    param_gens::Dict
+import Oscar.graph
+import Oscar.vertices
+import Oscar.gens
+
+###################################################################################
+#
+#       Additional functions for manipulating variable indices
+#
+###################################################################################
+
+
+function var_index(x)
+
+    str = string(x)
+    index = "["*split(str, "[")[2]
+
+    eval(Meta.parse(index))
 end
 
 
-include("GaussianGraphicalModels.jl")
-#include("DiscreteGraphicalModels.jl")
+
+###################################################################################
+#
+#       Additional functions for graphs
+#
+###################################################################################
+
+
+function vertices(G::Graph)
+
+    E = [[src(e), dst(e)] for e in edges(G)]
+    
+    sort(unique(reduce(vcat, E)))
+end
 
 
 
-# add
+###################################################################################
+#
+#       Additional functions for matrices
+#
+###################################################################################
+
+
+function cofactor(A)
+    
+    matrix(base_ring(A), [[(-1)^(i+j)*det(A[filter(l -> l != i, 1:n_rows(A)), filter(l -> l != j, 1:n_columns(A))]) for j in 1:n_columns(A)] for i in 1:n_rows(A)])
+end
+
+
+function adjoint(A::Generic.MatSpaceElem)
+    
+    transpose(cofactor(A))
+end
+
+
+
+###################################################################################
+#
+#       Generic Graphical Models
+#
+###################################################################################
+
+
+# G, directed or undirected 
+# T, GaussianRing or MarkovRing - specifies whether it is a Gaussian or Discrete graphical model
+struct GraphicalModel{G, T}
+    graph::G
+    ring::T
+    param_ring::Ring
+    param_gens
+end
+
+
+
+# todo
 function graph(M::GraphicalModel)
 
     M.graph
 end
 
 
-# add
+
+# todo
 function ring(M::GraphicalModel)
 
     M.ring
 end
+
+
+
+function param_ring(M::GraphicalModel)
+
+    M.param_ring
+end
+
+
+
+function param_gens(M::GraphicalModel)
+
+    M.param_gens
+end
+
+
+
+
+include("GaussianGraphicalModels.jl")
+#include("DiscreteGraphicalModels.jl")
 
 
 
@@ -32,6 +115,7 @@ end
 
 
 # computes the vanishing ideal of a directed gaussian graphical model
-function vanishing_ideal(R::GraphicalModel)
+function vanishing_ideal(M::GraphicalModel)
 
+    kernel(parameterization(M))
 end
