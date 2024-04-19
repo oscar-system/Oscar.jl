e93470134162dd2e4b5c289b088d1e86619403b6
Kamillo Ferry <kafe@kafe.dev>
2024-04-20 00:05:04 +0200

    Change benchmark

--- examples/benchmark/simple.jl
+++ examples/benchmark/simple.jl
@@ -2,13 +2,12 @@
 function simple(io)
     R,(x,y) = QQ[:x,:y]
     I = ideal([y^4+ x^3-x^2+x,x^4])
-
     
     print(io, "simple,")
     t = @belapsed groebner_walk($I; algorithm=:standard)
     print(io, t, ",")
     t = @belapsed groebner_walk($I; algorithm=:generic)
     print(io, t, ",")
-    t = @belapsed groebner_basis($I; ordering=lex(R))
+    t = @belapsed groebner_basis($I; ordering=lex($R))
     println(io, t)
 end
