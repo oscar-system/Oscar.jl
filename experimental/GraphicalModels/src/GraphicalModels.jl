ff883211f8f000df3c471f1398ce62611fea9805
bacher <bacher@mis.mpg.de>
2024-04-26 14:27:08 +0200

    update test files and exports

--- experimental/GraphicalModels/src/GraphicalModels.jl
+++ experimental/GraphicalModels/src/GraphicalModels.jl
@@ -2,4 +2,25 @@ include("PhylogeneticModels.jl")
 include("PhylogeneticAuxiliary.jl")
 include("PhylogeneticParametrisation.jl")
 
+#export models
+export cavender_farris_neyman_model 
 export jukes_cantor_model
+export kimura2_model
+export kimura3_model
+export general_markov_model
+
+#export probability and fourier map
+export probability_map
+export fourier_map
+
+# export functions to calculate equivalent classes
+export compute_equivalent_classes #are these good names for general Oscar? Do we want both exported?
+export sum_equivalent_classes #are these good names for general Oscar?
+
+# export transformation matrices
+export specialized_fourier_transform
+export inverse_specialized_fourier_transform
+
+# export structs for GroupBasedPhylogeneticModel,PhylogeneticModel
+export PhylogeneticModel
+export GroupBasedPhylogeneticModel
