d2330fa063f4de0e22fca1b70f58f55f824f501f
Matthias Zach <85350711+HechtiDerLachs@users.noreply.github.com>
2024-05-06 14:21:25 +0200

    Some fix for hypercomplexes (#3686)
    * Fix kernel computation.
    
    * Add tests.
    
    * Use Schreyer's resolution.
    
    * Add tests.
    
    * Fix tests.

--- experimental/DoubleAndHyperComplexes/src/Morphisms/simplified_complexes.jl
+++ experimental/DoubleAndHyperComplexes/src/Morphisms/simplified_complexes.jl
@@ -658,8 +658,8 @@ function kernel(simp::SimplifiedComplex{ChainType}, p::Int, i::Tuple) where {Cha
     return K, inc
   end
 
-  psi = map_to_original(simp)[i]
-  phi = map(c, p, i)
+  psi = map_to_original_complex(simp)[i]
+  phi = map(original_complex(simp), p, i)
   K, inc = kernel(compose(psi, phi))
   c.kernel_cache[(i, p)] = inc
   return K, inc
