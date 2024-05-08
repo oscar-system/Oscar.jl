96eb52edea2ab893861b3bc35666714a89d7094e
Morgan Rodgers <morgan.joaquin@gmail.com>
2024-05-08 10:37:14 +0200

    add Partitions type for iterator

--- src/Combinatorics/EnumerativeCombinatorics/types.jl
+++ src/Combinatorics/EnumerativeCombinatorics/types.jl
@@ -134,6 +134,16 @@ struct Partition{T<:IntegerUnion} <: AbstractVector{T}
   p::Vector{T}
 end
 
+struct Partitions{T<:IntegerUnion}
+  n::T
+
+  function Partitions(n::T) where T<:IntegerUnion
+    @req n >= 0 "n >= 0 required"
+    return new{T}(n)
+  end
+
+end
+
 ################################################################################
 #
 #  Young Tableaux
