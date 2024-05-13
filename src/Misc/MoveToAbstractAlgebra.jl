ad554b33028c5ae7592e655a6aea9dfa67f2de53
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2024-05-13 17:49:33 +0200

    Address comments

--- src/Misc/MoveToAbstractAlgebra.jl
+++ src/Misc/MoveToAbstractAlgebra.jl
@@ -38,59 +38,13 @@ function is_alternating(B::MatElem)
   n == ncols(B) || return false
 
   for i in 1:n
-    B[i, i] == 0 || return false
+    is_zero_entry(B, i, i) || return false
     for j in (i + 1):n
       B[i, j] == -B[j, i] || return false
     end
   end
 
   return true
-end
-
-@doc raw"""
-    upper_triangular_matrix(L)
-
-Return the upper triangular matrix whose entries on and above the diagonal are the elements of `L`.
-
-An exception is thrown whenever the length of `L` is not equal to $n(n+1)/2$,
-for some integer $n$.
-"""
-function upper_triangular_matrix(L)
-  T = eltype(L)
-  @assert T <: RingElem "L must be a collection of ring elements"
-  d = Int(floor((sqrt(1 + 8 * length(L)) - 1) / 2))
-  @req length(L) == div(d * (d + 1), 2) "Input vector of invalid length"
-  R = parent(L[1])
-  x = zero_matrix(R, d, d)
-  pos = 1
-  for i in 1:d, j in i:d
-    x[i, j] = L[pos]
-    pos += 1
-  end
-  return x
-end
-
-@doc raw"""
-    lower_triangular_matrix(L)
-
-Return the lower triangular matrix whose entries on and below the diagonal are the elements of `L`.
-
-An exception is thrown whenever the length of `L` is not equal to $n(n+1)/2$,
-for some integer $n$.
-"""
-function lower_triangular_matrix(L)
-  T = eltype(L)
-  @assert T <: RingElem "L must be a collection of ring elements"
-  d = Int(floor((sqrt(1 + 8 * length(L)) - 1) / 2))
-  @req length(L) == div(d * (d + 1), 2) "Input vector of invalid length"
-  R = parent(L[1])
-  x = zero_matrix(R, d, d)
-  pos = 1
-  for i in 1:d, j in 1:i
-    x[i, j] = L[pos]
-    pos += 1
-  end
-  return x
 end
 
 function Base.copy(f::MPolyRingElem)
