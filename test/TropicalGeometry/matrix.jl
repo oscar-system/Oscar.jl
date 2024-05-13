ccdc1cee6436217281ccc386694d81a9f7a04b87
Yue Ren <yue.ren.kl@gmail.com>
2024-05-13 19:53:26 +0100

    TropicalGeometry: improved test for det

--- test/TropicalGeometry/matrix.jl
+++ test/TropicalGeometry/matrix.jl
@@ -3,10 +3,10 @@
     for minOrMax in (min,max)
         T = tropical_semiring(minOrMax)
         @testset "det(A::Generic.MatSpaceElem{<:TropicalSemiringElem})" begin
-            A = matrix(T,[1 2; 3 5]) # matrix over tropical numbers
+            A = Matrix(T[1 2; 3 5]) # (julia) matrix over tropical numbers
             @test det(A) == (minOrMax==min ? T(5) : T(6))
             R, (x,y) = T[:x,:y]
-            A = identity_matrix(R,2) # matrix over tropical polynomials
+            A = identity_matrix(R,2) # (Oscar) matrix over tropical polynomials
             @test det(A) == one(R)
         end
     end
