c83e5d04f2b5e0c069bf55d161c988ba2c782528
Yue Ren <yue.ren.kl@gmail.com>
2024-05-13 18:24:15 +0100

    TropicalGeometry: fixed det over tropical polynomials

--- test/TropicalGeometry/matrix.jl
+++ test/TropicalGeometry/matrix.jl
@@ -3,8 +3,11 @@
     for minOrMax in (min,max)
         T = tropical_semiring(minOrMax)
         @testset "det(A::Generic.MatSpaceElem{<:TropicalSemiringElem})" begin
-            A = matrix(T,[1 2; 3 5])
+            A = matrix(T,[1 2; 3 5]) # matrix over tropical numbers
             @test det(A) == (minOrMax==min ? T(5) : T(6))
+            R, (x,y) = T[:x,:y]
+            A = identity_matrix(R,2) # matrix over tropical polynomials
+            @test det(A) == one(R)
         end
     end
 
