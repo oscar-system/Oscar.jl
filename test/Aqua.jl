851f9e55d7e0df6c33167e2fca1c0590ce025a88
Aaruni Kaushik <aaruni96@users.noreply.github.com>
2024-05-15 15:27:32 +0200

    Update test/Aqua.jl
    Co-authored-by: Lars Göttgens <lars.goettgens@gmail.com>

--- test/Aqua.jl
+++ test/Aqua.jl
@@ -6,7 +6,6 @@ using Aqua
     ambiguities=false,      # TODO: fix ambiguities
     unbound_args=false,     # TODO: fix unbound type parameters
     piracies=false,         # TODO: check the reported methods to be moved upstream
-    persistent_tasks=false  # TODO: FIXME | setting this to true breaks Oscar.build()
   )
   @test length(Aqua.detect_unbound_args_recursively(Oscar)) <= 16
 end
