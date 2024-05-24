344688cfd199f7718068c6d1f8f8bdba3db3e3e4
KilianBruns <kilian.bruns@gmx.de>
2024-05-24 19:54:41 +0200

    hasse_deriv for Rloc and RQ

--- src/AlgebraicGeometry/HasseSchmidt/examplesHasseSchmidt.jl
+++ src/AlgebraicGeometry/HasseSchmidt/examplesHasseSchmidt.jl
@@ -1,5 +1,5 @@
 
-
+### POLYNOMIALRING
 # EXAMPLE hasse_deriv (IZ == 0)
 R, x = polynomial_ring(ZZ, 4, "x")
 IZ = ideal(R, [zero(R)])
@@ -15,20 +15,23 @@ hasse_deriv(IZ, IX)
 R, x = polynomial_ring(ZZ, 4, "x")
 IX = ideal(R, [3*x[2],4*x[4]^3])
 IZ = ideal(R, [zero(0)])
-y = x
+# y = x
+systemOfParameters = [1]
 M = matrix(R, 1, 1, [R(0)])
 hasse_deriv(IZ, IX, y, M) # working, since i call hasse_deriv(IZ, IX) for IZ = 0
 
 R, x = polynomial_ring(ZZ, 4, "x")
 IZ = ideal(R, [x[1]])
 IX = IZ + ideal(R, [3*x[2], 4*x[4]^3])
-y = x[2:4]
+# y = x[2:4]
+systemOfParameters = [2, 3, 4]
 M = matrix(R, 1, 1, [R(1)])
 hasse_deriv(IZ, IX, y, M) # working 
 
 R, x = polynomial_ring(ZZ, 8, "x")
 IZ = ideal(R, [x[1]])
-y = x[2:8]
+# y = x[2:8]
+systemOfParameters = [2, 3, 4, 5, 6, 7, 8]
 M = matrix(R, 1, 1, [R(1)])
 IX = IZ + ideal(R, [x[2]*x[3]]) # working
 IX = IZ + ideal(R, [x[2]^2]) # working
@@ -39,7 +42,8 @@ hasse_deriv(IZ, IX, y, M)
 
 R, x = polynomial_ring(ZZ, 8, "x")
 IZ = ideal(R, [x[1]*x[2] - 1])
-y = x[2:8]
+# y = x[2:8]
+systemOfParameters = [2, 3, 4, 5, 6, 7, 8]
 M = matrix(R, 1, 1, [x[2]])
 IX = IZ # working # error (IZ and IX cannot be equal.)
 IX = IZ + ideal(R, [x[1]]) # working
@@ -57,7 +61,8 @@ hasse_deriv(IZ, IX, y, M)
 
 R, x = polynomial_ring(ZZ, 8, "x")
 IZ = ideal(R, [x[1]*x[2] - 1])
-y = vcat(x[1], x[3:8])
+# y = vcat(x[1], x[3:8])
+systemOfParameters = [1, 3, 4, 5, 6, 7, 8]
 M = matrix(R, 1, 1, [x[1]])
 IX = IZ + ideal(R, [x[2]]) # working
 IX = IZ + ideal(R, [x[2], x[3]]) # working
@@ -74,3 +79,33 @@ IX = IZ + ideal(R, [x[3]^3 + x[4]^2]) # working
 IX = IZ + ideal(R, [x[3]^3 + x[2]^2]) # working
 IX = IZ + ideal(R, [x[4]^3 + x[6]^4]) # working
 hasse_deriv(IZ, IX, y, M)
+
+### FAKTORRING / QUOTIENTENRING
+
+### LOKALISIERTER RING
+
+# Oscar.MPolyLocalizedIdeal
+R, (x, y, z) = polynomial_ring(QQ, ["x", "y", "z"])
+P = ideal(R, [x])
+U = complement_of_prime_ideal(P)
+Rloc, iota = localization(R, U)
+IZ = ideal(Rloc, [zero(Rloc)])
+IX = ideal(Rloc, [-y^3 + x^2])
+
+# MPolyQuoIdeal
+# T, t = polynomial_ring(QQ, "t")
+# K, a =  number_field(2*t^2-1, "a")
+R, (x, y) = polynomial_ring(QQ, ["x", "y"])
+I = ideal(R, [x - 1])  # I = ideal(R, [x-1, x-a])
+RQ, _ = quo(R, I)
+IZ = ideal(RQ, [zero(RQ)])
+IX = ideal(RQ, [-y^3 + x^2])
+
+R, (x, y) = polynomial_ring(QQ, ["x", "y"])
+I = ideal(R, [x^3 - 1])
+RQ, phi = quo(R, I) 
+P = ideal(R, [y])
+U = complement_of_prime_ideal(P)
+RQL, iota = localization(RQ, U) 
+IZ = ideal(RQL, [zero(RQL)])
+IX = ideal(RQL, [-y^3 + x^2])  # RESOVLED: IZ and IX are equal (need to understand why ^^ ) # Need to find an example with IZ != IX 
