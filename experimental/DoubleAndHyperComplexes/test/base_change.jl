8bdde8814e40de0cfdade68f09e4d83586836ceb
Matthias Zach <zach@math.uni-hannover.de>
2024-05-24 21:40:59 +0200

    Add tests.

--- experimental/DoubleAndHyperComplexes/test/base_change.jl
+++ experimental/DoubleAndHyperComplexes/test/base_change.jl
@@ -7,5 +7,30 @@
 
   L, loc = localization(R, U)
 
-  change_base_ring(loc, C)
+  CC, red = change_base_ring(loc, C)
+  @test !is_zero(CC[0])
+  @test !is_zero(CC[1])
+  @test !is_zero(CC[2])
+  a = compose(map(C, 1), red[0]) 
+  b = compose(red[1], map(CC, 1))
+  @test all(a(g) == b(g) for g in gens(C[1]))
+  a = compose(map(C, 2), red[1]) 
+  b = compose(red[2], map(CC, 2))
+  @test all(a(g) == b(g) for g in gens(C[2]))
+
+  F1 = FreeMod(ZZ, 2)
+  v = 15*F1[1] + 21*F1[2]
+  C = koszul_complex(Oscar.KoszulComplex, v)
+
+  CC, red = change_base_ring(QQ, C)
+  @test !is_zero(CC[0])
+  @test !is_zero(CC[1])
+  @test !is_zero(CC[2])
+  a = compose(map(C, 1), red[0]) 
+  b = compose(red[1], map(CC, 1))
+  @test all(a(g) == b(g) for g in gens(C[1]))
+  a = compose(map(C, 2), red[1]) 
+  b = compose(red[2], map(CC, 2))
+  @test all(a(g) == b(g) for g in gens(C[2]))
 end
+
