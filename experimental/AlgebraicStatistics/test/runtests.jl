0ba6d7daa055a57982cbb528d28c9e9b87b1f145
Ben Hollering <benjamin.hollering@mis.mpg.de>
2024-05-29 13:03:57 +0200

    added documentation pages for graphical models and gaussian graphical models

--- experimental/AlgebraicStatistics/test/runtests.jl
+++ experimental/AlgebraicStatistics/test/runtests.jl
@@ -1,28 +1,13 @@
 @testset "GaussianGraphicalModels" begin
 
   S = gaussian_ring(3)
+  s = gens(S)
   G = graph_from_edges(Directed, [[1,2],[2,3]])
   M = graphical_model(G, S)
-  
+
+  @test vanishing_ideal(M) == ideal([-s[1, 2]*s[2, 3] + s[1, 3]*s[2, 2]])
 end
 
-
-
-@testset "DiscreteGraphicalModels" begin
-  
-end
-
-
-
-@testset "ConditionalIndependence" begin
-  
-end
-
-
-
-@testset "Phylogenetics" begin
-  
-end
 
 
 
