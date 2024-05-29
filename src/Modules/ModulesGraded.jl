743439529406831b0b24d935c679b4d42a019d00
Lax202 <ramesh@mathematik.uni-kl.de>
2024-05-29 13:17:27 +0200

    fixed with wdecker

--- src/Modules/ModulesGraded.jl
+++ src/Modules/ModulesGraded.jl
@@ -1455,14 +1455,12 @@ total: 1  3  2
 """
 function betti_table(F::FreeResolution; project::Union{FinGenAbGroupElem, Nothing} = nothing, reverse_direction::Bool=false)
   @assert is_graded(F) "resolution must be graded"
-  @assert is_standard_graded(base_ring(F)) "resolution must be defined over a standard graded ring"
   generator_count = Dict{Tuple{Int, Any}, Int}()
   C = F.C
   rng = Hecke.map_range(C)
   n = first(rng)
   for i in 0:n
     module_degrees = F[i].d
-    module_degrees === nothing && error("One of the modules in the graded free resolution is not graded.")
     for degree in module_degrees
       idx = (i, degree)
       generator_count[idx] = get(generator_count, idx, 0) + 1
