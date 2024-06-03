2b34cbcedef8b8eec2cde64470aee681f2c526a7
Kamillo Ferry <ferry@math.tu-berlin.de>
2024-06-03 16:07:18 +0200

    Clean up

--- src/GroebnerWalk.jl
+++ src/GroebnerWalk.jl
@@ -1,24 +1,15 @@
 module GroebnerWalk
 using Oscar
 
-exponent_vectors = f->leading_exponent_vector.(monomials(f))
-
 include("markedGB.jl")
-include("generic_walk.jl")
-include("walk.jl")
+include("common.jl")
 
 include("standard_walk.jl")
+include("generic_walk.jl")
 include("perturbed_walk.jl")
-include("fractal_walk.jl")
 
 import Oscar: weight_ordering, ZZRingElem, MonomialOrdering, ZZMatrix, IdealGens
 import Oscar.Orderings: MatrixOrdering, _support_indices
-
-function weight_ordering(w::Vector{ZZRingElem}, o::MonomialOrdering)
-    i = _support_indices(o.o)
-    m = ZZMatrix(1, length(w), w)
-    return MonomialOrdering(base_ring(o), MatrixOrdering(i, m, false))*o
-end
 
 export groebner_walk
 
