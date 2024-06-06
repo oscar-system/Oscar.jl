6cefb181569928c085e0728bb4f5d28c7f4d1fdf
Yue Ren <yue.ren.kl@gmail.com>
2024-06-06 11:06:14 +0100

    TropicalGeometry: tropical variety overhaul
    TropicalGeometry: beginning wrapping Singular's tropicalVariety
    
    TropicalGeometry: first draft of Singular's tropicalVariety wrapper
    
    TropicalGeometry: tropical_variety overhaul
    
    TropicalGeometry: shorten attributes copying
    
    TropicalGeometry: tropical_variety overhaul
    
    Update src/TropicalGeometry/variety_binomial.jl
    
    Co-authored-by: Max Horn <max@quendi.de>
    
    Update src/TropicalGeometry/variety_binomial.jl
    
    Co-authored-by: Max Horn <max@quendi.de>
    
    Update src/TropicalGeometry/variety_binomial.jl
    
    Co-authored-by: Max Horn <max@quendi.de>
    
    Update src/TropicalGeometry/variety_prime.jl
    
    Co-authored-by: Max Horn <max@quendi.de>
    
    Update src/TropicalGeometry/variety_zerodimensional.jl
    
    Co-authored-by: Max Horn <max@quendi.de>
    
    Update src/TropicalGeometry/variety_affine_linear.jl
    
    Co-authored-by: Max Horn <max@quendi.de>
    
    Update src/TropicalGeometry/variety_principal.jl
    
    Co-authored-by: Max Horn <max@quendi.de>
    
    Udate src/TropicalGeometry/variety_binomial.jl
    
    TropicalGeometry: resolved merge conflict
    
    TropicalGeometry: changed attribute inheritance
    
    new: tropical_varieties and primary check in tropical_variety
    
    fix: missing stable_intersection in documentation
    
    fix: improved error message

--- test/TropicalGeometry/variety.jl
+++ test/TropicalGeometry/variety.jl
@@ -19,11 +19,11 @@
         R,(x,y,z,w) = QQ[:x, :y, :z, :w]
         I = ideal(R,[x+2*y,z+4*w])
         nu = tropical_semiring_map(QQ,2)
-        TropV = first(tropical_variety(I,nu))
+        TropV = tropical_variety(I,nu)
         TropL = tropical_linear_space(I,nu)
         @test issetequal(maximal_polyhedra(TropV),maximal_polyhedra(TropL))
         nu = tropical_semiring_map(QQ,2,max)
-        TropV = first(tropical_variety(I,nu))
+        TropV = tropical_variety(I,nu)
         TropL = tropical_linear_space(I,nu)
         @test issetequal(maximal_polyhedra(TropV),maximal_polyhedra(TropL))
     end
@@ -33,14 +33,14 @@
         f = x*y*z+2
         nu = tropical_semiring_map(QQ,2)
         TropH = tropical_hypersurface(f,nu)
-        TropV = first(tropical_variety(ideal(R,f),nu))
+        TropV = tropical_variety(ideal(R,f),nu)
         @test issetequal(maximal_polyhedra(TropH),maximal_polyhedra(TropV))
         nu = tropical_semiring_map(QQ,2,max)
-        TropV = first(tropical_variety(ideal(R,f),nu))
+        TropV = tropical_variety(ideal(R,f),nu)
         TropH = tropical_hypersurface(f,nu)
         @test issetequal(maximal_polyhedra(TropH),maximal_polyhedra(TropV))
     end
-   
+
     @testset "tropical prevarieties" begin
       G = grassmann_pluecker_ideal(2,4)
       f = gens(G)[1]
@@ -58,5 +58,28 @@
       TG = tropical_hypersurface.(gens(G))
       T = tropical_prevariety(TG)
       @test length(rays_modulo_lineality(T)[1]) == 10
+
+    # running tropical_variety and all its subroutines
+    @testset "testing tropical_variety" begin
+
+        # principal ideals
+        R,(x,y,z) = QQ["x","y","z"]
+        f = x^2+y^2+z^2+1
+        TropV = tropical_variety(ideal(R,f))
+        @test f_vector(TropV) == [1,4,6]
+
+        # binomial ideals
+        f = x^2+1
+        g = y^2+1
+        TropV = tropical_variety(ideal(R,[f,g]))
+        @test f_vector(TropV) == [0,1]
+
+        # affine linear ideals
+        f = x+z+1
+        g = y+z+1
+        TropV = tropical_variety(ideal(R,[f,g]))
+        @test f_vector(TropV) == [1,3]
+
+        # general ideals, see doctests
     end
 end
