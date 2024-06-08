450978d2374d6415a0777919c05beec710a25d05
Yue Ren <yue.ren.kl@gmail.com>
2024-06-08 15:02:06 +0100

    TropicalGeometry: first draft of Singular's tropicalVariety wrapper

--- src/TropicalGeometry/variety.jl
+++ src/TropicalGeometry/variety.jl
@@ -205,7 +205,7 @@ function tropical_variety(I::Union{MPolyIdeal,MPolyRingElem}, nu::Union{Tropical
             TropV = tropical_variety_linear(P,nu,weighted_polyhedral_complex_only=weighted_polyhedral_complex_only)
         else
             # P general
-            error("general tropical varieties currently unsupported")
+            TropV = tropical_variety_singular(P,nu,weighted_polyhedral_complex_only=weighted_polyhedral_complex_only)
         end
         push!(tropicalVarieties,TropV)
     end
@@ -422,7 +422,11 @@ end
 #
 ################################################################################
 
-function singular_fan_string_to_oscar_fan(input_string::String)
+
+# this function takes the string of a gfan fan and converts it to an OSCAR polyhedral complex
+# if negateFan==true, return the negative of the fan (because Singular is max-convention only, and OSCAR is flexible)
+# if dehomogenizeFan==true, the gfan fan is a homogenized polyhedral complex, and we need to dehomogenize it
+function singular_fan_string_to_oscar_complex(input_string::String, negateFan::Bool=false, dehomogenizeFan::Bool=false)
 
     # Extracting the RAYS, ORTH_LINEALITY_SPACE and MAXIMAL_CONES sections
     stringsParsed = Vector{SubString{String}}[]
@@ -434,29 +438,79 @@ function singular_fan_string_to_oscar_fan(input_string::String)
     end
 
     # Convert Rays and ORTH_LINEALITY_SPACE to matrices
+    # and negate if necessary
     rayGenerators = matrix(QQ,[parse.(Int, split(line)) for line in stringsParsed[1]])
     linealityGenerators = matrix(QQ,[parse.(Int, split(line)) for line in stringsParsed[2]])
+    if negateFan
+        rayGenerators *= -1
+    end
 
-    # Convert MAXIMAL_CONES to a IncidenceMatrix
-    maxConeIncidences = IncidenceMatrix([parse.(Int, split(replace(line, r"[{}]" => ""), r"\s+")) .+ 1 for line in stringsParsed[3]])
+    # Convert MAXIMAL_CONES to a Matrix (for later manipulation)
+    coneIncidences = Base.stack([parse.(Int, split(replace(line, r"[{}]" => ""), r"\s+")) .+ 1 for line in stringsParsed[3]],dims=1)
 
-    return polyhedral_fan(maxConeIncidences, rayGenerators, linealityGenerators)
+    if dehomogenizeFan
+        # if the singular fan is a homogenized polyhedral complex,
+        # identify which rays have a non-zero first entry and actually represent vertices
+        # then strip the first coordinate of the rays and lineality generators
+        rayIndices = findall(iszero, rayGenerators[:,1])
+        rayGenerators = rayGenerators[:,2:end]
+        linealityGenerators = linealityGenerators[:,2:end]
+        # in some cases, the first unit vector can be a lineality generator
+        # hence we need to filter out potential zero rows from linealityGenerators
+        linealityGenerators = linealityGenerators[findall(i->!iszero(linealityGenerators[i,:]),1:nrows(linealityGenerators)),:]
+        return polyhedral_complex(IncidenceMatrix(coneIncidences), rayGenerators, rayIndices, linealityGenerators)
+    else
+        # if the singular fan is an honest polyhedral fan
+        # we need to add the origin as a vertex and add it to all polyhedra
+        rayIndices = collect(1:nrows(rayGenerators))
+        rayGenerators = vcat(rayGenerators, zero_matrix(QQ,1,ncols(rayGenerators)))
+        coneIncidences = hcat(coneIncidences, ones(Int,nrows(coneIncidences),1))
+        return polyhedral_complex(IncidenceMatrix(coneIncidences), rayGenerators, rayIndices, linealityGenerators)
+    end
 
 end
 
 
-function tropical_variety_singular(I::MPolyIdeal,nu::TropicalSemiringMap{QQField,Nothing,typeof(max)})
+# this function takes the tropical variety as a polyhedral complex,
+# computes the weights of the tropical variety, and constructs the tropical variety
+function compute_weights_and_construct_tropical_variety(Sigma::PolyhedralComplex,I::MPolyIdeal,nu::TropicalSemiringMap)
+    # TODO: compute multiplicities
+    mults = ones(ZZRingElem,n_maximal_polyhedra(Sigma))
+    TropI = TropicalVariety{typeof(convention(nu)),true}(Sigma,mults)
+    set_attribute!(TropI,:algebraic_ideal,I)
+    set_attribute!(TropI,:tropical_semiring_map,nu)
+    return TropI
+end
+
+
+# trivial valuation case
+function tropical_variety_singular(I::MPolyIdeal, nu::TropicalSemiringMap{QQField,Nothing,<:Union{typeof(min),typeof(max)}}; weighted_polyhedral_complex_only::Bool=false)
     R = base_ring(I)
-    singularConstructRing = "ring r=0,("*join(string.(symbols(R)),",")*"),dp; "
-    singularConstructIdeal = "ideal I = "*join(string.(gens(I)), ",")*"; "
-    singularLoadLibrary = "if (!defined(tropicalVariety)) { LIB \"tropical.lib\"; }; "
-    singularComputeTropicalVariety = "fan TropI = tropicalVariety(I); string TropIString = string(TropI);"
-    singularCommand = singularConstructRing*singularConstructIdeal*singularLoadLibrary*singularComputeTropicalVariety
+    singularCommand = join(["ring r=0,("*join(string.(symbols(R)),",")*"),dp;",
+                            "ideal I = "*join(string.(gens(I)), ",")*";",
+                            "if (!defined(tropicalVariety)) { LIB \"tropical.lib\"; };",
+                            "fan TropI = tropicalVariety(I);",
+                            "string TropIString = string(TropI);"])
     Singular.call_interpreter(singularCommand)
     TropIString = Singular.lookup_library_symbol("Top", "TropIString")
-    return singular_fan_string_to_oscar_fan(TropIString)
+    Sigma = singular_fan_string_to_oscar_complex(TropIString,convention(nu)==max,false)
+    TropI = compute_weights_and_construct_tropical_variety(Sigma,I,nu)
+    return TropI
 end
 
+function tropical_variety_singular(I::MPolyIdeal, nu::TropicalSemiringMap{QQField,ZZRingElem,<:Union{typeof(min),typeof(max)}}; weighted_polyhedral_complex_only::Bool=false)
+    R = base_ring(I)
+    singularCommand = join(["ring r=0,("*join(string.(symbols(R)),",")*"),dp;",
+                            "ideal I = "*join(string.(gens(I)), ",")*";",
+                            "if (!defined(tropicalVariety)) { LIB \"tropical.lib\"; };",
+                            "fan TropI = tropicalVariety(I,number("*string(uniformizer(nu))*"));",
+                            "string TropIString = string(TropI);"])
+    Singular.call_interpreter(singularCommand)
+    TropIString = Singular.lookup_library_symbol("Top", "TropIString")
+    Sigma = singular_fan_string_to_oscar_complex(TropIString,convention(nu)==max,true)
+    TropI = compute_weights_and_construct_tropical_variety(Sigma,I,nu)
+    return TropI
+end
 
 # #=======
 # tropical variety of an ideal
