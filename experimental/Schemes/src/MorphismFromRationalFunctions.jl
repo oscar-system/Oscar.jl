c0ff2ca3f756e427bc49bac982d2426ca83f321a
HechtiDerLachs <zach@mathematik.uni-kl.de>
2024-06-18 15:55:20 +0200

    Disable faulty exclusion of charts.

--- experimental/Schemes/src/MorphismFromRationalFunctions.jl
+++ experimental/Schemes/src/MorphismFromRationalFunctions.jl
@@ -909,7 +909,8 @@ function _prepare_pushforward_prime_divisor(
   sorted_charts = copy(codomain_charts)
   if has_decomposition_info(default_covering(Y))
     info = decomposition_info(default_covering(Y))
-    sorted_charts = filter!(V->dim(OO(V)) - dim(ideal(OO(V), elem_type(OO(V))[OO(V)(a) for a in info[V]])) <= 1, sorted_charts)
+    # Enabling the following line seems to lead to wrong results. Why?
+    #sorted_charts = filter!(V->dim(OO(V)) - dim(ideal(OO(V), elem_type(OO(V))[OO(V)(a) for a in info[V]])) <= 1, sorted_charts)
   end
 
   function compl(V::AbsAffineScheme)
