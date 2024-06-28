e46bd05b06a4ad945478ed8c2aab0c6d26569655
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2024-06-28 18:47:16 +0200

    Add direct sums of Lie algebras

---
+++ experimental/LieAlgebras/src/DirectSumLieAlgebra.jl
@@ -0,0 +1,168 @@
+@attributes mutable struct DirectSumLieAlgebra{C<:FieldElem} <: LieAlgebra{C}
+  R::Field
+  dim::Int
+  summands::Vector{<:LieAlgebra{C}}
+  s::Vector{Symbol}
+
+  function DirectSumLieAlgebra{C}(
+    R::Field,
+    summands::Vector{<:LieAlgebra{C}}
+    ) where {C<:FieldElem}
+    @req all(x -> coefficient_ring(x) == R, summands) "Summands must have the same coefficient ring."
+    totaldim = sum(dim, summands; init=0)
+    s = [Symbol("$(x)^($(i))") for (i, S) in enumerate(summands) for x in symbols(S)]
+    L = new{C}(R, totaldim, summands, s)
+    
+    return L
+  end
+end
+
+struct DirectSumLieAlgebraElem{C<:FieldElem} <: LieAlgebraElem{C}
+  parent::DirectSumLieAlgebra{C}
+  mat::MatElem{C}
+end
+
+
+###############################################################################
+#
+#   Basic manipulation
+#
+###############################################################################
+
+parent_type(::Type{DirectSumLieAlgebraElem{C}}) where {C<:FieldElem} = DirectSumLieAlgebra{C}
+
+elem_type(::Type{DirectSumLieAlgebra{C}}) where {C<:FieldElem} = DirectSumLieAlgebraElem{C}
+
+parent(x::DirectSumLieAlgebraElem) = x.parent
+
+coefficient_ring(L::DirectSumLieAlgebra{C}) where {C<:FieldElem} = L.R::parent_type(C)
+
+dim(L::DirectSumLieAlgebra) = L.dim
+
+
+###############################################################################
+#
+#   String I/O
+#
+###############################################################################
+
+function Base.show(io::IO, mime::MIME"text/plain", L::DirectSumLieAlgebra)
+  @show_name(io, L)
+  @show_special(io, mime, L)
+  io = pretty(io)
+  println(io, "Direct sum Lie algebra")
+  println(io, Indent(), "of dimension $(dim(L))", Dedent())
+  println(io, "with summands", Indent())
+  for S in L.summands
+    println(terse(io), Lowercase(), S)
+  end
+  print(io, Dedent(), "over ")
+  print(io, Lowercase(), coefficient_ring(L))
+end
+
+function Base.show(io::IO, L::DirectSumLieAlgebra)
+  @show_name(io, L)
+  @show_special(io, L)
+  if is_terse(io)
+    print(io, "Direct sum Lie algebra")
+  else
+    io = pretty(io)
+    print(io, "Direct sum of $(_number_of_direct_product_factors(L)) Lie algebras over ", Lowercase())
+    print(terse(io), coefficient_ring(L))
+  end
+end
+
+function symbols(L::DirectSumLieAlgebra)
+  return L.s
+end
+
+###############################################################################
+#
+#   Parent object call overload
+#
+###############################################################################
+
+# no special ones
+
+###############################################################################
+#
+#   Arithmetic operations
+#
+###############################################################################
+
+function bracket(
+  x::DirectSumLieAlgebraElem{C}, y::DirectSumLieAlgebraElem{C}
+) where {C<:FieldElem}
+  check_parent(x, y)
+  D = parent(x)
+  vec = zero(_matrix(x))
+  index = 0
+  for S in D.summands
+    offset = index
+    dimS = dim(S)
+    index += dimS
+    
+    xS_vec = _matrix(x)[:, offset + 1:offset + dimS]
+    iszero(xS_vec) && continue
+    xS = S(xS_vec)
+    yS_vec = _matrix(y)[:, offset + 1:offset + dimS]
+    iszero(yS_vec) && continue
+    yS = S(_matrix(y)[:, offset + 1:offset + dimS])
+    vec[:, offset + 1:offset + dimS] = _matrix(bracket(xS, yS))
+  end
+  return D(vec)
+end
+
+###############################################################################
+#
+#   Properties
+#
+###############################################################################
+
+function is_abelian(L::DirectSumLieAlgebra)
+  return all(is_abelian, L.summands)
+end
+
+
+###############################################################################
+#
+#   Direct sum functionality
+#
+###############################################################################
+
+AbstractAlgebra._number_of_direct_product_factors(D::DirectSumLieAlgebra) = length(D.summands)
+
+function canonical_injection(D::DirectSumLieAlgebra, i::Int)
+  @boundscheck @req 1 <= i <= _number_of_direct_product_factors(D) "Invalid index."
+  S = D.summands[i]
+  mat = Generic.inj_proj_mat(coefficient_ring(D), dim(S), dim(D), sum(dim, D.summands[1:i-1]; init=0)+1)
+  return hom(S, D, mat; check=false)
+end
+
+function canonical_projection(D::DirectSumLieAlgebra, i::Int)
+  @boundscheck @req 1 <= i <= _number_of_direct_product_factors(D) "Invalid index."
+  S = D.summands[i]
+  mat = Generic.inj_proj_mat(coefficient_ring(D), dim(D), dim(S), sum(dim, D.summands[1:i-1]; init=0)+1 )
+  return hom(D, S, mat; check=false)
+end
+
+
+###############################################################################
+#
+#   Constructor
+#
+###############################################################################
+
+function direct_sum(Ss::Vector{<:LieAlgebra{C}}) where {C<:FieldElem}
+  R = coefficient_ring(Ss[1])
+  return direct_sum(R, Ss)
+end
+
+function direct_sum(R::Field, Ss::Vector{<:LieAlgebra{C}}) where {C<:FieldElem}
+  @req elem_type(R) == C "Coefficient ring mismatch."
+  return DirectSumLieAlgebra{C}(R, Ss)
+end
+
+function direct_sum(S::LieAlgebra{C}, Ss::LieAlgebra{C}...) where {C<:FieldElem}
+  return direct_sum(LieAlgebra{C}[S; Ss...])
+end
---
+++ experimental/LieAlgebras/test/DirectSumLieAlgebra-test.jl
@@ -0,0 +1,102 @@
+@testset "LieAlgebras.DirectSumLieAlgebra" begin
+  @testset "conformance tests" begin
+    @testset "sl_2(QQ)" begin
+      S1 = special_linear_lie_algebra(QQ, 2)
+      L = direct_sum(S1)
+
+      lie_algebra_conformance_test(
+        L, DirectSumLieAlgebra{QQFieldElem}, DirectSumLieAlgebraElem{QQFieldElem}
+      )
+      @test dim(L) == dim(S1)
+
+      L = direct_sum([S1])
+      lie_algebra_conformance_test(
+        L, DirectSumLieAlgebra{QQFieldElem}, DirectSumLieAlgebraElem{QQFieldElem}
+      )
+      @test dim(L) == dim(S1)
+    end
+
+    @testset "sl_2(GF(7)) ⊕ gl_3(GF(7))" begin
+      GF7 = GF(7)
+      S1 = special_linear_lie_algebra(GF7, 2)
+      S2 = general_linear_lie_algebra(GF7, 3)
+
+      L = direct_sum(S1, S2)
+      lie_algebra_conformance_test(
+        L, DirectSumLieAlgebra{FqFieldElem}, DirectSumLieAlgebraElem{FqFieldElem}
+      )
+      @test dim(L) == dim(S1) + dim(S2)
+
+      L = direct_sum([S1, S2])
+      lie_algebra_conformance_test(
+        L, DirectSumLieAlgebra{FqFieldElem}, DirectSumLieAlgebraElem{FqFieldElem}
+      )
+      @test dim(L) == dim(S1) + dim(S2)
+    end
+
+    @testset "sl_3(CF(4)) ⊕ B3(CF(4)) ⊕ G2(CF(4))" begin
+      CF4 = cyclotomic_field(4)[1]
+      S1 = special_linear_lie_algebra(CF4, 3)
+      S2 = lie_algebra(CF4, :B, 3)
+      S3 = lie_algebra(CF4, :G, 2)
+      L = direct_sum(S1, S2, S3)
+
+      lie_algebra_conformance_test(
+        L, DirectSumLieAlgebra{AbsSimpleNumFieldElem},
+        DirectSumLieAlgebraElem{AbsSimpleNumFieldElem}
+      )
+      @test dim(L) == dim(S1) + dim(S2) + dim(S3)
+
+      L = direct_sum([S1, S2, S3])
+      lie_algebra_conformance_test(
+        L, DirectSumLieAlgebra{AbsSimpleNumFieldElem},
+        DirectSumLieAlgebraElem{AbsSimpleNumFieldElem}
+      )
+      @test dim(L) == dim(S1) + dim(S2) + dim(S3)
+    end
+
+    @testset "empty sum over QQ" begin
+      L = direct_sum(QQ, LieAlgebra{QQFieldElem}[])
+
+      lie_algebra_conformance_test(
+        L,
+        DirectSumLieAlgebra{QQFieldElem},
+        DirectSumLieAlgebraElem{QQFieldElem},
+      )
+      @test dim(L) == 0
+      @test is_abelian(L)
+    end
+  end
+
+  @testset "properties" begin
+    S1 = special_linear_lie_algebra(QQ, 2)
+    S2 = lie_algebra(QQ, :A, 1)
+    L = direct_sum(S1, S2)
+
+    @test length(canonical_injections(L)) == 2
+    @test all(i -> canonical_injection(L, i) == canonical_injections(L)[i], 1:2)
+    @test all(i -> codomain(canonical_injection(L, i)) == L, 1:2)
+    @test domain(canonical_injection(L, 1)) == S1
+    @test domain(canonical_injection(L, 2)) == S2
+
+    @test length(canonical_projections(L)) == 2
+    @test all(i -> canonical_projection(L, i) == canonical_projections(L)[i], 1:2)
+    @test all(i -> domain(canonical_projection(L, i)) == L, 1:2)
+    @test codomain(canonical_projection(L, 1)) == S1
+    @test codomain(canonical_projection(L, 2)) == S2
+
+    for _ in 1:5
+      x = L(rand(-10:10, dim(L)))
+      @test x == sum([canonical_injection(L, i)(canonical_projection(L, i)(x)) for i in 1:2])
+
+      x1 = S1(rand(-10:10, dim(S1)))
+      x2 = S2(rand(-10:10, dim(S2)))
+      @test x1 == canonical_projection(L, 1)(canonical_injection(L, 1)(x1))
+      @test x2 == canonical_projection(L, 2)(canonical_injection(L, 2)(x2))
+      @test iszero(canonical_projection(L, 2)(canonical_injection(L, 1)(x1)))
+      @test iszero(canonical_projection(L, 1)(canonical_injection(L, 2)(x2)))
+    end
+
+    @test !is_abelian(L)
+  end
+end
