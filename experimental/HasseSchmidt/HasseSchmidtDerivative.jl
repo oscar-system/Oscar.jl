22b3e59101795476dfd68feae350dc2469d1ce84
KilianBruns <kilian.bruns@gmx.de>
2024-07-04 13:28:06 +0200

    removed some comments

--- experimental/HasseSchmidt/HasseSchmidtDerivative.jl
+++ experimental/HasseSchmidt/HasseSchmidtDerivative.jl
@@ -34,18 +34,15 @@ julia> hasse_derivatives(f)
 function hasse_derivatives(f::MPolyRingElem)
   R = parent(f)
   n = ngens(R)
-  # degreef = maximum(degrees(f))
   # define new ring with more variables: R[x1, ..., xn] -> R[x1, ..., xn, t1, ..., tn]
   Rtemp, _ = polynomial_ring(R, "y" => 1:n, "t" => 1:n)
   # replace f(x_i) -> f(y_i + t_i)
   F = evaluate(f, gens(Rtemp)[1:n] + gens(Rtemp)[n+1:2n])
-  # i = 1 # counter to iterate though degrees of monomials
-  # HasseDerivativesList = empty([f])
-  HasseDerivativesList = [f]
+  HasseDerivativesList = [f] # initializing with the zero'th HS derivative: f itself
   varR = vcat(gens(R), ones(typeof(base_ring(R)(1)), n))
   # getting hasse derivs without extra attention on ordering
   for term in terms(F)
-    if sum(degrees(term)[n+1:2n]) != 0
+    if sum(degrees(term)[n+1:2n]) != 0 # 
       # hasse derivatives are the factors in front of the monomial in t
       push!(HasseDerivativesList, evaluate(term, varR))
     end
