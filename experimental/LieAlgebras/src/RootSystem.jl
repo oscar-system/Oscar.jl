962e8f8aabd3b86b0d213baf7999ba8b8caadf88
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2024-07-14 11:33:19 +0200

    Cache `QQMatrix(bilinear_form(::RootSystem))`

--- experimental/LieAlgebras/src/RootSystem.jl
+++ experimental/LieAlgebras/src/RootSystem.jl
@@ -112,6 +112,10 @@ end
   return cartan_bilinear_form(cartan_matrix(R); check=false)
 end
 
+@attr QQMatrix function _bilinear_form_QQ(R::RootSystem)
+  return QQMatrix(bilinear_form(R))
+end
+
 @doc raw"""
     cartan_matrix(R::RootSystem) -> ZZMatrix
 
@@ -502,7 +506,7 @@ end
 function dot(r1::RootSpaceElem, r2::RootSpaceElem)
   @req root_system(r1) === root_system(r2) "$r1 and $r2 must belong to the same root space"
 
-  return dot(coefficients(r1) * bilinear_form(root_system(r1)), coefficients(r2))
+  return dot(coefficients(r1) * _bilinear_form_QQ(root_system(r1)), coefficients(r2))
 end
 
 @doc raw"""
