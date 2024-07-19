4448d5bc0aa7cc77ec03afd3d04bcbeb18d1ab71
Michael Joswig <joswig@math.tu-berlin.de>
2024-07-19 14:34:46 +0200

    forward declaration

--- src/aliases.jl
+++ src/aliases.jl
@@ -15,6 +15,7 @@ function n_polyhedra end
 function n_rays end
 function n_vertices end
 function dual_graph end
+function is_equidistant end
 function vertex_edge_graph end
 
 function number_of_compositions end
