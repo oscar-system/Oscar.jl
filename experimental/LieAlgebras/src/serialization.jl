66af597499e6a417a9aab0cc145b9e1b08eb4c81
antonydellavecchia <antonydellavecchia@gmail.com>
2024-07-29 17:25:17 +0200

    serialization for SRow

--- experimental/LieAlgebras/src/serialization.jl
+++ experimental/LieAlgebras/src/serialization.jl
@@ -10,7 +10,7 @@ end
 
 function load_object(s::DeserializerState, ::Type{AbstractLieAlgebra})
   R = load_typed_object(s, :base_ring)
-  struct_consts = load_object(s, :struct_consts)
-  s = load_object(s, :symbols)
+  struct_consts = load_object(s, Matrix, (SRow, R), :struct_consts)
+  s = load_object(s, Vector, Symbol, :symbols)
   return lie_algebra(R, struct_consts, s; check=false)
 end
