a7f80a0aba54d27e205ebd77bcef3da5cfa5b27d
antonydellavecchia <antonydellavecchia@gmail.com>
2024-07-29 17:48:15 +0200

    fix for general lie algebras

--- experimental/LieAlgebras/src/serialization.jl
+++ experimental/LieAlgebras/src/serialization.jl
@@ -21,15 +21,16 @@ function save_object(s::SerializerState, L::LinearLieAlgebra)
   save_data_dict(s) do
     save_typed_object(s, coefficient_ring(L), :base_ring)
     save_object(s, L.n, :n)
-    save_object(s, matrix_repr_basis(L), :basis)
+    save_typed_object(s, matrix_repr_basis(L), :basis)
     save_object(s, symbols(L), :symbols)
   end
 end
 
 function load_object(s::DeserializerState, ::Type{LinearLieAlgebra})
   R = load_typed_object(s, :base_ring)
+  T = elem_type(R)
   n = load_object(s, Int, :n)
-  basis = load_object(s, Vector, dense_matrix_type(R), :basis)
+  basis = load_typed_object(s, :basis)
   s = load_object(s, Vector, Symbol, :symbols)
   return lie_algebra(R, n, basis, s; check=false)
 end
