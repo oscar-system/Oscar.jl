b0ed460f31517c5c1f37313c72697b603e1408cb
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2024-07-30 17:27:35 +0200

    Some optimizations

--- experimental/LieAlgebras/src/serialization.jl
+++ experimental/LieAlgebras/src/serialization.jl
@@ -21,16 +21,15 @@ function save_object(s::SerializerState, L::LinearLieAlgebra)
   save_data_dict(s) do
     save_typed_object(s, coefficient_ring(L), :base_ring)
     save_object(s, L.n, :n)
-    save_typed_object(s, matrix_repr_basis(L), :basis)
+    save_object(s, matrix_repr_basis(L), :basis)
     save_object(s, symbols(L), :symbols)
   end
 end
 
 function load_object(s::DeserializerState, ::Type{LinearLieAlgebra})
   R = load_typed_object(s, :base_ring)
-  T = elem_type(R)
   n = load_object(s, Int, :n)
-  basis = load_typed_object(s, :basis)
+  basis = load_object(s, Vector, (dense_matrix_type(R), matrix_space(R, n, n)), :basis)
   s = load_object(s, Vector, Symbol, :symbols)
   return lie_algebra(R, n, basis, s; check=false)
 end
