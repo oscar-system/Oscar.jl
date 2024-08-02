ad298cf4bc719c93833e9e503eec558fc7b586da
HechtiDerLachs <zach@mathematik.uni-kl.de>
2024-08-02 22:00:29 +0200

    Add verbosity scope for exterior algebras.

--- src/Oscar.jl
+++ src/Oscar.jl
@@ -177,6 +177,8 @@ function __init__()
   add_verbosity_scope(:ZZLatWithIsom)
   
   add_assertion_scope(:IdealSheaves)
+  
+  add_verbosity_scope(:ExteriorAlgebras)
 
   # Pkg.is_manifest_current() returns false if the manifest might be out of date
   # (but might return nothing when there is no project_hash)
