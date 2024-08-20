841d37e83de16a411faa728c8389c52c9433abbc
Johannes Schmitt <schmitt@mathematik.uni-kl.de>
2024-08-20 10:41:45 +0200

    Fix #4026

--- src/Rings/groebner.jl
+++ src/Rings/groebner.jl
@@ -1282,7 +1282,7 @@ with respect to the ordering
 function _fglm(G::IdealGens, ordering::MonomialOrdering)
   (G.isGB == true && G.isReduced == true) || error("Input must be a reduced Gröbner basis.") 
   Singular.dimension(singular_generators(G)) == 0 || error("Dimension of corresponding ideal must be zero.")
-  SR_destination, = Singular.polynomial_ring(base_ring(G.Sx),["$i" for i in gens(G.Sx)]; ordering = Singular.ordering_as_symbol(singular(ordering)))
+  SR_destination, = Singular.polynomial_ring(base_ring(G.Sx),["$i" for i in gens(G.Sx)]; ordering = singular(ordering))
 
   ptr = Singular.libSingular.fglmzero(G.S.ptr, G.Sx.ptr, SR_destination.ptr)
   return IdealGens(base_ring(G), Singular.sideal{Singular.spoly}(SR_destination, ptr, true))
