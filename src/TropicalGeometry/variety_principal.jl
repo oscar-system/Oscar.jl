05818f967f264af9f62900ff5309f9136a185306
Yue Ren <yue.ren.kl@gmail.com>
2024-09-16 09:55:23 +0100

    Update src/TropicalGeometry/variety_principal.jl
    Co-authored-by: Max Horn <max@quendi.de>

--- src/TropicalGeometry/variety_principal.jl
+++ src/TropicalGeometry/variety_principal.jl
@@ -10,7 +10,7 @@ function tropical_variety_principal(I::MPolyIdeal,nu::TropicalSemiringMap; weigh
     ###
     # Construct TropicalVariety from TropicalHypersurface
     ###
-    g = first(gens(I))
+    g = gen(I,1)
     TropV = tropical_variety(tropical_hypersurface(g,nu,weighted_polyhedral_complex_only=weighted_polyhedral_complex_only))
     if !weighted_polyhedral_complex_only
         set_attribute!(TropV,:algebraic_ideal,I)
