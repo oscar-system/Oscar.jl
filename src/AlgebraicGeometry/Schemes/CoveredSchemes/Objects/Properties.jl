7bdf0fd3a9f210a8edad052cfd34698fb2237cd3
HechtiDerLachs <zach@mathematik.uni-kl.de>
2024-09-20 23:56:57 +0200

    Implement one more smoothness test.

--- src/AlgebraicGeometry/Schemes/CoveredSchemes/Objects/Properties.jl
+++ src/AlgebraicGeometry/Schemes/CoveredSchemes/Objects/Properties.jl
@@ -42,6 +42,49 @@ is_smooth(X::AbsCoveredScheme) = is_smooth(underlying_scheme(X))
   return all(is_smooth, affine_charts(X))
 end
 
+### Recursive function to broadcast parent-like objects to all other workers
+function _put_params_rec(channels::Vector{RemoteChannel{Channel{T}}}, a::Any) where {T}
+  !serialize_with_id(a) && return # check whether we actually need to send something; if not, quit.
+  error("recursive broadcasting is not implemented for objects of type $(typeof(a))")
+end
+
+function _put_params_rec(channels::Vector{RemoteChannel{Channel{T}}}, a::MPolyRing) where {T}
+  _put_params_rec(channels, coefficient_ring(a))
+  put_params(channels, a)
+  @hassert :Parallelization 2 _check_other_sides(a)
+end
+
+function _put_params_rec(channels::Vector{RemoteChannel{Channel{T}}}, a::MPolyQuoRing) where {T}
+  _put_params_rec(channels, base_ring(a))
+  put_params(channels, a)
+  @hassert :Parallelization 2 _check_other_sides(a)
+end
+
+function _put_params_rec(channels::Vector{RemoteChannel{Channel{T}}}, a::MPolyLocRing) where {T}
+  _put_params_rec(channels, base_ring(a))
+  put_params(channels, a)
+  @hassert :Parallelization 2 _check_other_sides(a)
+end
+
+function _put_params_rec(channels::Vector{RemoteChannel{Channel{T}}}, a::MPolyQuoLocRing) where {T}
+  _put_params_rec(channels, base_ring(a))
+  _put_params_rec(channels, localized_ring(a))
+  _put_params_rec(channels, underlying_quotient(a))
+  put_params(channels, a)
+  @hassert :Parallelization 2 _check_other_sides(a)
+end
+
+function _check_other_sides(R::Any)
+  ref = get(global_serializer_state.obj_to_id, R, nothing)
+  ref === nothing && return false
+  for wid in workers()
+    remotecall_fetch((a)->haskey(Oscar.global_serializer_state.id_to_obj, Oscar.UUID(a)), wid, string(ref)) || return false
+  end
+  return true
+end
+
+### Functions to make parent-like objects known to particular 
+# other workers through particular channels
 function _put_and_take(channel::RemoteChannel, wid::Int, a::Any)
   @show a
   @show serialize_with_id(a)
@@ -87,6 +130,126 @@ function _check_other_side(R::Any, wid::Int)
   @show typeof(R)
   @assert ref !== nothing
   @assert remotecall_fetch((a)->haskey(Oscar.global_serializer_state.id_to_obj, Oscar.UUID(a)), wid, string(ref))
+end
+
+function is_smooth_parallel2(X::CoveredScheme)
+  channels = Oscar.params_channels(Ring)
+  n = length(channels)
+  wids = workers()
+  for (i, U) in enumerate(affine_charts(X))
+    _put_params_rec(channels, OO(U))
+  end
+  cov = default_covering(X)
+  data = [(U, ideal(OO(U), decomposition_info(cov)[U])) for U in affine_charts(X)]
+  function compute(dat)
+    return _is_smooth(dat[1]; focus=dat[2])
+  end
+  result = pmap(compute, data)
+  return all(isone(x) for x in result)
+end
+
+function _is_smooth(U::AbsAffineScheme{<:Field, RT};
+    focus::Ideal
+  ) where {RT <: MPolyRing}
+  return true
+end
+
+function _is_smooth(U::AbsAffineScheme{<:Field, RT};
+    focus::Ideal
+  ) where {RT <: MPolyLocRing}
+  return true
+end
+
+function _is_smooth(U::AbsAffineScheme{<:Field, RT};
+    focus::Ideal = ideal(OO(U), elem_type(OO(U))[])
+  ) where {RT <: Union{MPolyQuoRing, MPolyQuoLocRing}}
+  if is_known_to_be_irreducible(U) || true
+    g = gens(modulus(OO(U)))
+    Q, pr = quo(OO(U), focus)
+    A = map_entries(Q, jacobian_matrix(g))
+    return has_constant_rank(A)
+  end
+  return error("not implemented")
+end
+
+function has_constant_rank(
+    A::MatrixElem;
+    focus::Union{Ideal, Nothing} = nothing
+  )
+  @show size(A)
+  if focus !== nothing
+    Q, pr = quo(base_ring(A), focus)
+    AA = map_entries(pr, A)
+    return has_constant_rank(AA)
+  end
+  R = base_ring(A)
+  bound = 5
+  if nrows(A) <= bound || ncols(A) <= bound
+    for r in 1:3
+      @show r
+      I = ideal(R, minors(A, r))
+      is_one(I) && continue
+      is_zero(I) && return true
+      return false
+    end
+  end
+      
+  is_zero(A) && return true
+  m = nrows(A)
+  n = ncols(A)
+  entry_list = elem_type(R)[A[i, j] for i in 1:m for j in 1:n]
+  I = ideal(R, entry_list)
+  is_one(I) || return false
+  c = coordinates(one(R), I)
+  ind = _non_zero_indices(c)
+  @show length(ind)
+  ind_pairs = [(div(k-1, n)+1, mod(k-1, n)+1) for k in ind]
+  foc_eqns = elem_type(R)[]
+  for (i, j) in ind_pairs
+    @show "$(length(foc_eqns)+1)-th entry at ($i, $j) out of $(length(ind_pairs))"
+    U = powers_of_element(lifted_numerator(A[i, j]))
+    focus = ideal(R, foc_eqns)
+    Q, pr = quo(R, focus)
+    L, loc_map = localization(Q, U)
+    tmp = map_entries(pr, A)
+    LA = map_entries(loc_map, tmp)
+    multiply_row!(LA, inv(LA[i, j]), i)
+    for k in 1:m
+      k == i && continue
+      add_row!(LA, -LA[k, j], i, k)
+    end
+    sub = hcat(vcat(LA[1:i-1, 1:j-1], LA[i+1:m, 1:j-1]),
+               vcat(LA[1:i-1, j+1:n], LA[i+1:m, j+1:n]))
+    @time res = has_constant_rank(sub) 
+    res || return false
+    push!(foc_eqns, A[i, j])
+  end
+  return true
+end
+
+_non_zero_indices(c::SRow) = c.pos
+_non_zero_indices(c::Vector) = [k for k in 1:length(c) if !is_zero(c[k])]
+_non_zero_indices(c::MatrixElem) = [k for k in 1:length(c) if !is_zero(c[1, k])]
+
+
+function is_known_to_be_irreducible(U::AbsAffineScheme{<:Field, RT}) where {RT <: MPolyRing}
+  return true
+end
+
+function is_known_to_be_irreducible(U::AbsAffineScheme{<:Field, RT}) where {RT <: MPolyLocRing}
+  return true
+end
+
+function is_known_to_be_irreducible(
+    U::AbsAffineScheme{<:Field, RT}
+  ) where {RT <: Union{MPolyQuoRing, MPolyQuoLocRing}}
+  has_attribute(U, :is_irreducible) && return is_irreducible(U)
+  I = modulus(OO(U))
+  if has_attribute(I, :is_prime)
+    set_attribute!(U, :is_irreducible=>is_prime(I))
+    return is_prime(I)
+  end
+  return false
 end
 
 function is_smooth_parallel(X::CoveredScheme)
