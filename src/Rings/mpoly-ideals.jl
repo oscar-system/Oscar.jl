4a085a1b113de147b948a0aa55fb57ff56c73ada
HechtiDerLachs <zach@mathematik.uni-kl.de>
2024-09-24 16:51:24 +0200

    Revert "Set the default hash to zero for ideals."
    This reverts commit 981ed6328a6953ba3ce73e6ae7958b0d8701faa1.

--- src/Rings/mpoly-ideals.jl
+++ src/Rings/mpoly-ideals.jl
@@ -1159,7 +1159,13 @@ function minimal_primes(
       end
       J = K
     end
-    result = unique!(filter!(!is_one, vcat([minimal_primes(j; algorithm, factor_generators=false) for j in J]...)))
+    # unique! seems to fail here. We have to do it manually.
+    pre_result = filter!(!is_one, vcat([minimal_primes(j; algorithm, factor_generators=false) for j in J]...))
+    result = typeof(I)[]
+    for p in pre_result
+      p in result && continue
+      push!(result, p)
+    end
 
     # The list might not consist of minimal primes only. We have to discard the embedded ones
     final_list = typeof(I)[]
@@ -2260,10 +2266,3 @@ function flag_pluecker_ideal(ring::MPolyRing{<: FieldElem}, dimensions::Vector{I
                    isReduced=true,
                    isGB=true))
 end
-
-# Since most ideals implement `==`, they have to implement the hash function.
-# See issue #4143 for problems entailed. This should fix it. 
-function hash(I::Ideal, c::UInt)
-  return UInt(0)
-end
-
