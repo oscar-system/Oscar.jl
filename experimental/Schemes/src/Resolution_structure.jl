2d0775975ca7cd5837b61ebe923c7df1aeea081c
Simon Brandhorst <brandhorst@math.uni-sb.de>
2024-09-25 17:07:06 +0200

    move stuff around

--- experimental/Schemes/src/Resolution_structure.jl
+++ experimental/Schemes/src/Resolution_structure.jl
@@ -7,123 +7,6 @@ export exceptional_locus
 export NormalizationMorphism
 export locus_of_maximal_order
 
-##############################################################################
-## Concrete Type for normalization
-## very similar to CoveredSchemeMorphism, but allowing disjoint handling
-## of disjoint components
-##############################################################################
-@doc raw"""
-    NormalizationMorphism{
-                  DomainType<:AbsCoveredScheme,
-                  CodomainType<:AbsCoveredScheme
-      } <:AbsCoveredSchemeMorphism{
-                                 DomainType,
-                                 CodomainType,
-                                 Nothing,
-                                 NormalizationMorphism,
-                                }
-A datastructure to encode normalizations of covered schemes.
-
-It is described as the morphism from the new scheme to the original one, containing
-information on the decomposition of the new scheme into disjoint components.
-(This is the type of the return value of  `normalization(X::AbsCoveredScheme)`.)
-"""
-@attributes mutable struct NormalizationMorphism{
-    DomainType<:AbsCoveredScheme,
-    CodomainType<:AbsCoveredScheme
-   } <:AbsCoveredSchemeMorphism{
-                                 DomainType,
-                                 CodomainType,
-                                 Nothing,
-                                 NormalizationMorphism,
-                                }
-
-  underlying_morphism::CoveredSchemeMorphism
-  inclusions::Vector{<:AbsCoveredSchemeMorphism}
-
-  function NormalizationMorphism(
-      f::CoveredSchemeMorphism,
-      inclusions::Vector{<:AbsCoveredSchemeMorphism};
-      check::Bool=true
-    ) 
-    @check is_normal(X) "not a normalization morphism"
-    @assert all(inc->codomain(inc) === domain(f), inclusions) "domains and codomains do not match"
-    ret_value = new{typeof(domain(f)),typeof(codomain(f))}(f,inclusions)
-    return ret_value    
-  end
-
-  function NormalizationMorphism(
-      f::CoveredSchemeMorphism;
-      check::Bool=true
-    )
-    @check is_normal(X) "not a normalization morphism"
-    ret_value = new{typeof(domain(f)),typeof(codomain(f))}(f,[identity_map(X)])
-    return ret_value    
-  end
-
-end
-
-#####################################################################################################
-# Desingularization morphism: birational map between covered schemes with smooth domain
-#####################################################################################################
-@doc raw"""
-    MixedBlowUpSequence{
-              DomainType<:AbsCoveredScheme,
-              CodomainType<:AbsCoveredScheme
-            }<:AbsDesingMor{  DomainType,
-                              CodomainType, 
-                              MixedBlowUpSequence{DomainType, CodomainType}
-                                              }
-A datastructure to encode sequences of blow-ups and normalizations of covered schemes
-as needed for desingularization of non-embedded schemes by the approaches of Zariski and of
-Lipman. 
-"""
-
-@attributes mutable struct MixedBlowUpSequence{
-                                                DomainType<:AbsCoveredScheme,
-                                                CodomainType<:AbsCoveredScheme
-                                              }<:AbsDesingMor{  DomainType,
-                                                                CodomainType, 
-                                                                MixedBlowUpSequence{DomainType, CodomainType}
-                                              }
-  maps::Vector{Union{<:BlowupMorphism,<:NormalizationMorphism}}       # count right to left:
-                                                 # original scheme is codomain of map 1
-  # boolean flags
-  resolves_sing::Bool                            # domain not smooth yet?
-  is_trivial::Bool                               # codomain already smooth?
-  is_strong::Bool                                # snc divisors ensured?
-
-  # fields for caching, to be filled during desingularization
-  # always carried along to domain(maps[end])) using strict_transform
-  ex_div::Vector{AbsIdealSheaf}      # list of exc. divisors arising from individual steps
-  dont_meet::Vector{Tuple{Int,Int}}             # mostly for dim=2: intersections which cannot exist
-                                                # according to intermediate computations
-  caution_multi_charts::Vector{Tuple{Int,Int}}  # only for dim=2: intersection of divisors not
-                                                # entirely visible in a single chart
-
-  # keep track of the normalization steps
-  normalization_steps::Vector{Int}
-
-  # fields for caching to be filled a posteriori (on demand, only if partial_res==false)
-  underlying_morphism::CompositeCoveredSchemeMorphism{DomainType, CodomainType}
-  exceptional_divisor::AbsWeilDivisor
-  exceptional_locus::AbsAlgebraicCycle
-
-  function MixedBlowUpSequence(maps::Vector{<:AbsCoveredSchemeMorphism})
-    n = length(maps)
-    for i in 1:n
-      @assert all(x->((x isa BlowupMorphism) || (x isa NormalizationMorphism)), maps) "only blow-ups and normalizations allowed"
-    end
-    for i in 1:n-1
-      @assert domain(maps[i]) === codomain(maps[i+1]) "not a sequence of morphisms"
-    end
-    resi = new{typeof(domain(maps[end])),typeof(codomain(first(maps)))}(maps)
-    resi.normalization_steps = [i for i in 1:n if maps[i] isa NormalizationMorphism]
-    return resi
-  end
-
-end
-
 
 ##################################################################################################
 # getters
@@ -1253,7 +1136,7 @@ function divisor_intersections_with_X(current_div::Vector{<:AbsIdealSheaf}, I_X:
 
 # initialization: each divisor + I_X
   for k in 1:length(current_div)
-    Idiv = ideal_sheaf(current_div[k]) + I_X
+    Idiv = current_div[k] + I_X
     if !is_one(Idiv)
       inter_div_dict[[k]] = (Idiv,0)
       push!(old_keys, [k])
--- src/AlgebraicGeometry/Schemes/CoveredSchemes/Morphisms/Types.jl
+++ src/AlgebraicGeometry/Schemes/CoveredSchemes/Morphisms/Types.jl
@@ -49,3 +49,61 @@ underlying_morphism(f::CoveredSchemeMorphism) = f
 function CoveredSchemeMorphism(f::CoveredSchemeMorphism)
   return f
 end
+
+
+
+##############################################################################
+## Concrete Type for normalization
+## very similar to CoveredSchemeMorphism, but allowing disjoint handling
+## of disjoint components
+##############################################################################
+@doc raw"""
+    NormalizationMorphism{
+                  DomainType<:AbsCoveredScheme,
+                  CodomainType<:AbsCoveredScheme
+      } <:AbsCoveredSchemeMorphism{
+                                 DomainType,
+                                 CodomainType,
+                                 Nothing,
+                                 NormalizationMorphism,
+                                }
+A datastructure to encode normalizations of covered schemes.
+
+It is described as the morphism from the new scheme to the original one, containing
+information on the decomposition of the new scheme into disjoint components.
+(This is the type of the return value of  `normalization(X::AbsCoveredScheme)`.)
+"""
+@attributes mutable struct NormalizationMorphism{
+    DomainType<:AbsCoveredScheme,
+    CodomainType<:AbsCoveredScheme
+   } <:AbsCoveredSchemeMorphism{
+                                 DomainType,
+                                 CodomainType,
+                                 Nothing,
+                                 NormalizationMorphism,
+                                }
+
+  underlying_morphism::CoveredSchemeMorphism
+  inclusions::Vector{<:AbsCoveredSchemeMorphism}
+
+  function NormalizationMorphism(
+      f::CoveredSchemeMorphism,
+      inclusions::Vector{<:AbsCoveredSchemeMorphism};
+      check::Bool=true
+    ) 
+    @check is_normal(X) "not a normalization morphism"
+    @assert all(inc->codomain(inc) === domain(f), inclusions) "domains and codomains do not match"
+    ret_value = new{typeof(domain(f)),typeof(codomain(f))}(f,inclusions)
+    return ret_value    
+  end
+
+  function NormalizationMorphism(
+      f::CoveredSchemeMorphism;
+      check::Bool=true
+    )
+    @check is_normal(X) "not a normalization morphism"
+    ret_value = new{typeof(domain(f)),typeof(codomain(f))}(f,[identity_map(X)])
+    return ret_value    
+  end
+
+end
