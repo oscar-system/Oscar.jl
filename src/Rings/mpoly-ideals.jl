5052fa111848b9c4979106b32193357bf0d66dbc
HechtiDerLachs <zach@mathematik.uni-kl.de>
2024-09-27 11:08:27 +0200

    Avoid hashing of types for ideals.

--- src/Rings/mpoly-ideals.jl
+++ src/Rings/mpoly-ideals.jl
@@ -2291,6 +2291,6 @@ end
 # See issue #4143 for problems entailed. Interestingly, this does not yet fix 
 # the failure of unique! on lists of ideals.
 function hash(I::Ideal, c::UInt)
-  return hash(typeof(I), hash(base_ring(I), c))
+  return hash(base_ring(I), c)
 end
 
