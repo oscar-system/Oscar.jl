64291080efbd0de2f1a6dac97a6b2a9d1ed4c6fb
HechtiDerLachs <zach@mathematik.uni-kl.de>
2024-10-01 15:38:21 +0200

    Fix doctests in AG.

--- src/AlgebraicGeometry/Schemes/AffineSchemes/Objects/Attributes.jl
+++ src/AlgebraicGeometry/Schemes/AffineSchemes/Objects/Attributes.jl
@@ -653,7 +653,7 @@ julia> singular_locus(A3)
 (scheme(1), Hom: scheme(1) -> affine 3-space)
 
 julia> singular_locus(X)
-(scheme(x^2 - y^2 + z^2, z, y, x), Hom: scheme(x^2 - y^2 + z^2, z, y, x) -> scheme(x^2 - y^2 + z^2))
+(scheme(x^2 - y^2 + z^2, 2*x, -2*y, 2*z), Hom: scheme(x^2 - y^2 + z^2, 2*x, -2*y, 2*z) -> scheme(x^2 - y^2 + z^2))
 
 julia> U = complement_of_point_ideal(R, [0,0,0])
 Complement
