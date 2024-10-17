decfa6ce6f44e351298b8e6b4d244e05f4878f6c
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2024-10-17 17:33:16 +0200

    Try to fix matrixgroup test

--- src/Groups/matrices/MatGrp.jl
+++ src/Groups/matrices/MatGrp.jl
@@ -623,7 +623,7 @@ function map_entries(f, G::MatrixGroup)
 end
 
 function map_entries(R::Ring, G::MatrixGroup)
-  imgs = [map_entries(R, matrix(x)) for x in gens(G)]
+  imgs = dense_matrix_type(R)[map_entries(R, matrix(x)) for x in gens(G)]
   return matrix_group(R, degree(G), imgs)
 end
 
