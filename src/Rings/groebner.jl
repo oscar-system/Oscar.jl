23c9af35ab9d0c206f81eaa4e096fe7ce117e25e
ederc <ederc@mathematik.uni-kl.de>
2024-10-25 10:45:45 +0200

    fixes application check for f4

--- src/Rings/groebner.jl
+++ src/Rings/groebner.jl
@@ -343,7 +343,7 @@ function is_f4_applicable(I::MPolyIdeal, ordering::MonomialOrdering)
             && ((coefficient_ring(I) isa FqField
                  && absolute_degree(coefficient_ring(I)) == 1
                  && characteristic(coefficient_ring(I)) < 2^31)
-                || coefficient_ring(I) == QQ))
+                || base_ring(I) isa QQMPolyRing))
 end
 
 @doc raw"""
