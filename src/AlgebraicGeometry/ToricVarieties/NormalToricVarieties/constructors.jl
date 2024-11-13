3899e8780a6204f447ce8b6f90b32aa499220ff7
Martin Bies <MBies87@googlemail.com>
2024-11-13 14:15:51 +0100

    [ToricVarieties] Add hash function for NormalToricVariety

--- src/AlgebraicGeometry/ToricVarieties/NormalToricVarieties/constructors.jl
+++ src/AlgebraicGeometry/ToricVarieties/NormalToricVarieties/constructors.jl
@@ -187,6 +187,12 @@ function Base.:(==)(tv1::NormalToricVariety, tv2::NormalToricVariety)
   error("Equality of normal toric varieties is computationally very demanding. More details in the documentation.")
 end
 
+function Base.hash(tv::NormalToricVariety, h::UInt)
+  b = 0x3f737124abf342f8 % UInt
+  h = hash(tv.polymakeNTV, h)
+  return xor(h, b)
+end
+
 
 
 ######################
