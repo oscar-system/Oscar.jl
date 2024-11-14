78b8af0f4efce0712923065200780e8ed215be59
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2024-11-14 19:19:22 +0100

    Change character from GAP to OSCAR

--- experimental/BasisLieHighestWeight/src/WeylPolytope.jl
+++ experimental/BasisLieHighestWeight/src/WeylPolytope.jl
@@ -1,47 +1,15 @@
 
 @doc raw"""
-    orbit_weylgroup(L::LieAlgebraStructure, weight_w::Vector{ZZRingElem}) -> Vector{Vector{ZZRingElem}}
-
-Computes the orbit of the weight `weight_w` given as coefficients to the fundamental weights
-$\omega_i$ under the action of the Weyl group of the Lie algebra `L`.
-"""
-function orbit_weylgroup(L::LieAlgebraStructure, weight_w::Vector{ZZRingElem})
-  # initialization
-  weyl_group = GAPWrap.WeylGroup(GAPWrap.RootSystem(codomain(Oscar.iso_oscar_gap(L.lie_algebra))))
-  orbit_iterator = GAPWrap.WeylOrbitIterator(weyl_group, GAP.Obj(Int.(weight_w)))
-  vertices = Vector{ZZRingElem}[]
-
-  # operate with the weylgroup on weight_vector
-  while !(GAPWrap.IsDoneIterator(orbit_iterator))
-    w = GAPWrap.NextIterator(orbit_iterator)
-    push!(vertices, Vector{ZZRingElem}(w))
-  end
-
-  return vertices
-end
-
-@doc raw"""
     get_dim_weightspace(L::LieAlgebraStructure, highest_weight::Vector{ZZRingElem}) -> Dict{Vector{ZZRingElem},Int}
 
 Computes the dimension of the weight spaces of the Lie algebra `L` module with highest weight `highest_weight`.
-For all dominant weights, the dimension is computed with GAP. For the remaining weights, the dimension is
-calculated by checking which dominant weight lies in the orbit of the weight under the action of the Weyl group.
 
 The weights are given as coefficients to the fundamental weights $\omega_i$.
 """
 function get_dim_weightspace(L::LieAlgebraStructure, highest_weight::Vector{ZZRingElem})
-  # calculate dimension for dominant weights with GAP
-  root_system = root_system_gap(L)
-  dominant_char = GAP.Globals.DominantCharacter(root_system, GAP.Obj(Int.(highest_weight)))
-  dominant_weights = map(weight -> ZZ.(weight), dominant_char[1])
-  dominant_weights_dim = Int.(dominant_char[2])
   weightspaces = Dict{Vector{ZZRingElem},Int}()
-
-  # calculate dimension for the rest by checking which dominant weight lies in the orbit
-  for (dominant_weight, dim) in zip(dominant_weights, dominant_weights_dim)
-    for weight in orbit_weylgroup(L, dominant_weight)
-      weightspaces[highest_weight - weight] = dim
-    end
+  for (weight, dim) in character(L.lie_algebra, highest_weight)
+    weightspaces[highest_weight - ZZ.(weight)] = dim
   end
   return weightspaces
 end
