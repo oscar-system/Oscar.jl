63e383483d445100f374a331cbb4765ef112a42f
HechtiDerLachs <zach@mathematik.uni-kl.de>
2024-11-17 19:13:31 +0100

    Initialize experimental folder for contributions from Durham.

---
+++ experimental/Durham2024/src/Durham2024.jl
@@ -0,0 +1,12 @@
+# pushforward of algebraic cycles for proper maps
+
+@doc raw"""
+    pushforward(p::BlowupMorphism, W::AbsAlgebraicCycle)
+
+Given a `BlowupMorphism` ``p : X → Y`` and an `AbsAlgebraicCycle` ``W ∈ Z_k(X)``,
+compute ``p_*(W) ∈ Z_k(Y)``.
+"""
+function pushforward(p::BlowupMorphism, W::AbsAlgebraicCycle)
+  error("not implemented")
+end
+
