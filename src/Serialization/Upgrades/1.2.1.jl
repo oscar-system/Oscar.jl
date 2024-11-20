60ab287a34008ff716a18900e5b6bb6d970d5470
antonydellavecchia <antonydellavecchia@gmail.com>
2024-11-20 18:33:35 +0100

    fixes for tests

--- src/Serialization/Upgrades/1.2.1.jl
+++ src/Serialization/Upgrades/1.2.1.jl
@@ -4,8 +4,8 @@ push!(upgrade_scripts_set, UpgradeScript(
     upgraded_dict = dict
     if haskey(dict, :_type) && dict[:_type] == "FqField"
       if dict[:data] isa Dict
-        if !(haskey(dict[:data], :def_pol)) 
-          upgraded_dict[:data][:def_pol] = dict[:data]
+        if !(haskey(dict[:data], :def_pol))
+          upgraded_dict[:data][:def_pol] = copy(dict[:data])
         end
       end
     end
