84e8f4bf741cf29ee0f6eb5aa6f41589ceec34ad
antonydellavecchia <antonydellavecchia@gmail.com>
2024-11-20 18:04:00 +0100

    fixes version numbers for release and includes test

--- src/Serialization/Upgrades/1.2.1.jl
+++ src/Serialization/Upgrades/1.2.1.jl
@@ -1,6 +1,6 @@
 push!(upgrade_scripts_set, UpgradeScript(
-  v"1.3.0",
-  function upgrade_1_3_0(s::UpgradeState, dict::Dict)
+  v"1.2.1",
+  function upgrade_1_2_1(s::UpgradeState, dict::Dict)
     upgraded_dict = dict
     if haskey(dict, :_type) && dict[:_type] == "FqField"
       if dict[:data] isa Dict
