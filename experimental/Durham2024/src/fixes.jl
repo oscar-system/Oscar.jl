b0a2c91fd66e3d4d6025a467b5eb95d2c9eee91d
HechtiDerLachs <zach@mathematik.uni-kl.de>
2024-11-21 17:55:35 +0100

    Fix up signature of self_intersection...

--- experimental/Durham2024/src/fixes.jl
+++ experimental/Durham2024/src/fixes.jl
@@ -20,8 +20,11 @@ function canonical_divisor(X:: AbsAffineScheme{<:Field, <:MPolyRing})
   return CartierDivisor(covered_scheme(X), ZZ)
 end
 
-function self_intersection_via_adjunction(K_X::AbsWeilDivisor, C::AbsWeilDivisor, g::Int)
- return 2g-2-intersect(K_X, C)
+function self_intersection_via_adjunction(K_X::CartierDivisor, C::EffectiveCartierDivisor, g::Int)
+  return 2g-2-intersect(K_X, C)
+end
+function self_intersection_via_adjunction(K_X::CartierDivisor, C::CartierDivisor, g::Int)
+    return 2g-2-sum(a*intersect(K_X, comp) for (a, comp) in coefficient_dict(C); init=0)
 end
 
 
