37405a6b77e1edbd54ab9135dbed9b0e7ee5b5be
mlodyjesienin <filipzzz@interia.eu>
2024-11-21 22:23:17 +0100

    3valency & 3connectivity checks added again

--- src/AlgebraicGeometry/Curves/MaximalMumfordCurve.jl
+++ src/AlgebraicGeometry/Curves/MaximalMumfordCurve.jl
@@ -42,6 +42,24 @@ defined by ideal with 5 generators
 
 ```
 """
+"""
+TODO: Move check functions to src/Combinatorics/Graphs/functions.jl file
+"""
+function check_i_valency(G::Graph, i::int)
+  return all(v -> degree(G, v) == i, vertices(G))
+end
+
+function check_i_connectivity(G::Graph, i::int)
+  for combination in AbstractAlgebra.combinations(collect(edges(G)),i-1)
+    G1 =deepcopy(G)
+    rem_edge!.(Ref(G1),combination)
+    if !is_connected(G1)
+      return false 
+    end
+  end
+  return true 
+end
+
 function graph_curve(G::Graph; check::Bool=true)
     C,_ = graph_curve_with_vertex_ideals(G; check=check)
     return C
@@ -49,7 +67,8 @@ end
 
 function graph_curve_with_vertex_ideals(G::Graph; check::Bool=true)
     if check
-        @req all(isequal(3),degree(G)) "G is not trivalent"
+        @req check_i_valency(G,3) "G is not trivalent"
+        @req check_i_connectivity(G,3) "G is not three-connected"
     end
 
     R,_ = graded_polynomial_ring(QQ,Int(nv(G)//2+1))
