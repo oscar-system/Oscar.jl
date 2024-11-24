3db78a0869454ac8a8b5c075fd4e3abe343592b0
HechtiDerLachs <zach@mathematik.uni-kl.de>
2024-11-25 00:32:31 +0100

    Fix up moving of divisors, etc.

--- experimental/Durham2024/src/fixes.jl
+++ experimental/Durham2024/src/fixes.jl
@@ -33,11 +33,33 @@ end
 scheme(f::VarietyFunctionFieldElem) = scheme(parent(f))
 variety(f::VarietyFunctionFieldElem) = variety(parent(f))
 
+# Compute the colength of I_P in the localization R_P.
+# This assumes that R itself is a domain and that I is of height 1. 
+# Then there exists a regular 
+# point on Spec(R) and hence R_P is also regular and a UFD. 
+# Being of height 1, I_P must then be principal.
 function _colength_in_localization(I::Ideal, P::Ideal)
+  R = base_ring(I)
+  @assert R === base_ring(P)
+  U = MPolyComplementOfPrimeIdeal(saturated_ideal(P))
+  L, loc = localization(R, U)
+  I_loc = loc(I)
+  @assert base_ring(I_loc) === L
+  P_loc = loc(P)
+  x = _find_principal_generator(P_loc)
+  y = one(L)
+  k = 0
+  while true
+    (y in I_loc) && return k
+    y = y*x
+    k += 1
+  end
+    
   return _colength_in_localization(saturated_ideal(I), saturated_ideal(P))
 end
 
-function _find_principal_generator(I::MPolyLocalizedIdeal)
+# same assumptions as above apply.
+function _find_principal_generator(I::Union{<:MPolyLocalizedIdeal, <:MPolyQuoLocalizedIdeal})
   L = base_ring(I)
   g = gens(I)
   g = sort!(g, by=x->total_degree(lifted_numerator(x)))
@@ -48,38 +70,15 @@ function _find_principal_generator(I::MPolyLocalizedIdeal)
   error("no principal generator found")
 end
 
-function _colength_in_localization(I::MPolyIdeal, P::MPolyIdeal)
-  R = base_ring(I)
-  @assert R === base_ring(P)
-  U = MPolyComplementOfPrimeIdeal(P)
-  L, loc = localization(R, U)
-  I_loc = loc(I)
-  P_loc = loc(P)
-  x = _find_principal_generator(P_loc)
-  y = one(L)
-  k = 0
-  while true
-    y in I && return k
-    y = y*x
-    k += 1
-  end
-    
-
-  @assert !isone(I_loc)
-  F = free_module(L, 1)
-  IF, inc_IF = I_loc*F
-  M = cokernel(inc_IF)
-  return length(M)
-end
-
-
 function weil_divisor(
     f::VarietyFunctionFieldElem; 
     ring::Ring=ZZ, covering::Covering=default_covering(scheme(f))
   )
+  @vprint :Divisors 4 "calculating principal divisor for $f\n"
   X = scheme(f)
   ideal_dict = IdDict{AbsIdealSheaf, elem_type(ring)}()
   for U in patches(covering)
+    @vprint :Divisors 4 "doing patch $U\n"
     inc_dict = IdDict{AbsIdealSheaf, elem_type(ring)}()
     f_loc = f[U]
     num = numerator(f_loc)
@@ -88,18 +87,25 @@ function weil_divisor(
     den_ideal = ideal(OO(U), den)
     num_dec = primary_decomposition(num_ideal)
     den_dec = primary_decomposition(den_ideal)
+    @vprint :Divisors 4 "  numerator:\n"
     for (_, P) in num_dec
+      @vprint :Divisors 4 "    $P\n"
       # If this component was already seen in another patch, skip it.
       new_comp = PrimeIdealSheafFromChart(X, U, P)
+      @vprint :Divisors 4 "    $(any(new_comp == PP for PP in keys(ideal_dict)) ? "already found" : "new component")\n"
       any(new_comp == PP for PP in keys(ideal_dict)) && continue 
       c = _colength_in_localization(num_ideal, P)
+      @vprint :Divisors 4 "    multiplicity $c\n"
       inc_dict[new_comp] = c
     end
+    @vprint :Divisors 4 "  denominator:\n"
     for (_, P) in den_dec
       # If this component was already seen in another patch, skip it.
       new_comp = PrimeIdealSheafFromChart(X, U, P)
+      @vprint :Divisors 4 "    $(any(new_comp == PP for PP in keys(ideal_dict)) ? "already found" : "new component")\n"
       any(new_comp == PP for PP in keys(ideal_dict)) && continue 
       c = _colength_in_localization(den_ideal, P)
+      @vprint :Divisors 4 "    multiplicity $c\n"
       key_list = collect(keys(inc_dict))
       k = findfirst(==(new_comp), key_list)
       if k === nothing
@@ -166,7 +172,9 @@ function intersect(E::EffectiveCartierDivisor, D::AbsWeilDivisor; check::Bool=tr
   @assert X === scheme(D)
   R = coefficient_ring(D)
   result = AlgebraicCycle(X, R)
+  cpcd = copy(coefficient_dict(D))
   DD = irreducible_decomposition(D)
+  cpcd = copy(coefficient_dict(DD))
   for (P, a) in coefficient_dict(DD)
     _, inc_P = sub(P)
     if is_zero(pullback(inc_P, ideal_sheaf(E)))
@@ -187,10 +195,7 @@ function is_zero(II::AbsIdealSheaf)
   return all(iszero(II(U)) for U in affine_charts(scheme(II)))
 end
 
-function +(C::CartierDivisor, E::EffectiveCartierDivisor)
-  return C + 1*E
-end
-
 function -(C::CartierDivisor, E::EffectiveCartierDivisor)
   return C - 1*E
 end
+
