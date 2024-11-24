b16cf8f2eb98a56360fff5a56824295c252a6194
HechtiDerLachs <zach@mathematik.uni-kl.de>
2024-11-25 00:27:44 +0100

    Fix up irreducible_decomposition.

--- src/AlgebraicGeometry/Schemes/Divisors/AlgebraicCycles.jl
+++ src/AlgebraicGeometry/Schemes/Divisors/AlgebraicCycles.jl
@@ -454,24 +454,31 @@ Return a cycle ``E`` equal to ``D`` but as a formal sum ``E = ∑ₖ aₖ ⋅ I�
 where the `components` ``Iₖ`` of ``E`` are all sheaves of prime ideals.
 """
 function irreducible_decomposition(D::AbsAlgebraicCycle)
+  @vprint :Divisors 4 "computing irreducible decomposition for $D"
   if all(is_prime, keys(coefficient_dict(D))) 
-    coeff_dict = copy(coefficient_dict(D))
-    for (P, c) in coeff_dict
-      for (Q, d) in coeff_dict
-        P === Q && continue
-        P != Q && continue
-        # P == Q from here onwards
-        if c == -d
-          delete!(coeff_dict, P)
-          delete!(coeff_dict, Q)
-        else
-          coefficient_dict(D)[P] = d + c
-          delete!(coeff_dict, Q)
-        end
+    @vprint :Divisors 4 "  all components are already prime, cleaning up..."
+    buckets = Vector{Vector{AbsIdealSheaf}}()
+    for P in keys(coefficient_dict(D))
+      found = false
+      for bucket in buckets
+	if P == first(bucket)
+	  push!(bucket, P)
+	  found = true
+	  break
+	end
       end
+      !found && push!(buckets, [P])
+    end
+    R = coefficient_ring(D)
+    coeff_dict = IdDict{AbsIdealSheaf, elem_type(R)}()
+    for bucket in buckets
+      c = sum(D[P] for P in bucket; init=zero(R))
+      is_zero(c) && continue
+      coeff_dict[first(bucket)] = c
     end
     return AlgebraicCycle(scheme(D), coefficient_ring(D), coeff_dict; check=false)
   end
+  @vprint :Divisors 4 "  decomposing components"
   result = zero(D)
   for (I, a) in coefficient_dict(D)
     next_dict = IdDict{AbsIdealSheaf, elem_type(coefficient_ring(D))}()
