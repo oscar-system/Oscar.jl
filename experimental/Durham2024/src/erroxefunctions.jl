4296b5b660bf3f10faf185b4ab8f172cc2b8d8cb
HechtiDerLachs <zach@mathematik.uni-kl.de>
2024-11-25 14:43:45 +0100

    Remove Durham file for the moment (to be added in a separate PR).

--- experimental/Durham2024/src/erroxefunctions.jl
+++
@@ -1,8 +0,0 @@
-function embedding(C::EffectiveCartierDivisor)
-	I=ideal_sheaf(C);
-	C1,inc_C = sub(I);
-	return inc_C
-end
-
-function reduce_to_surface()
-end
--- experimental/Durham2024/test/runtests.jl
+++
@@ -1,3 +0,0 @@
-include("fixes.jl")
-#include("runtests2.jl") # disabled for the moment; needs cleaning.
-
