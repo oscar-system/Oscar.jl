58cb2935444eec10dccbdb8355f8fff04589ee03
Claus Fieker <fieker@mathematik.uni-kl.de>
2024-12-12 11:35:52 +0100

    remove stuff

--- experimental/GModule/src/GrpExt.jl
+++ experimental/GModule/src/GrpExt.jl
@@ -121,15 +121,6 @@ function Oscar.isomorphism(::Type{FPGroup}, E::GrpExt)
   #both can/ should be handled by shiftgens (or sylable/ create)
 end
 
-<<<<<<< Updated upstream
-function Oscar.syllables(g::Union{PcGroupElem, SubPcGroupElem})
-  l = GAPWrap.ExtRepOfObj(GapObj(g))
-  @assert iseven(length(l))
-  return Pair{Int, ZZRingElem}[l[i-1] => l[i] for i = 2:2:length(l)]
-end
-
-=======
->>>>>>> Stashed changes
 # convert syllables in canonical form into exponent vector
 #Thomas
 function exponent_vector(sylls::Vector{Pair{Int64, ZZRingElem}}, n)
@@ -141,18 +132,6 @@ function exponent_vector(sylls::Vector{Pair{Int64, ZZRingElem}}, n)
   return res
 end
 
-<<<<<<< Updated upstream
-# convert syllables in canonical form into group element
-#Thomas
-function (G::PcGroup)(sylls::Vector{Pair{Int64, ZZRingElem}})
-  e = exponent_vector(sylls, ngens(G))
-  pcgs = Oscar.GAPWrap.FamilyPcgs(GapObj(G))
-  x = Oscar.GAPWrap.PcElementByExponentsNC(pcgs, GapObj(e, true))
-  return Oscar.group_element(G, x)
-end
-
-=======
->>>>>>> Stashed changes
 function Oscar.isomorphism(::Type{PcGroup}, E::GrpExt)
   c = E.c
   C = c.C
