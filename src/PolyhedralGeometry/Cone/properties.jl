7b32ade35af78dec0c7a497e5b28fc4b3f186a53
Lars Kastner <lars.kastner@posteo.de>
2024-12-12 22:51:34 +0100

    [PolyhedralGeometry] Streamline docs a bit

--- src/PolyhedralGeometry/Cone/properties.jl
+++ src/PolyhedralGeometry/Cone/properties.jl
@@ -535,6 +535,7 @@ Return the facets of `C` in the format defined by `as`.
 
 The allowed values for `as` are
 * `Halfspace` (or its subtype `LinearHalfspace`),
+* `Hyperplane` (or its subtype `LinearHyperplane1),
 * `Cone`.
 
 # Examples
