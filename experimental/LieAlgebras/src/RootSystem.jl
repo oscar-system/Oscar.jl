664e4f3384140ba4b2e206b72a243242d5cb90b8
Janika Peters <janika.peters@rwth-aachen.de>
2024-12-19 10:40:33 +0100

    Address comments

--- experimental/LieAlgebras/src/RootSystem.jl
+++ experimental/LieAlgebras/src/RootSystem.jl
@@ -1113,15 +1113,12 @@ function reflection(beta::RootSpaceElem)
 
   b, index_of_beta = is_positive_root_with_index(beta)
   if !b
-    index_of_beta -= rk
+    index_of_beta = is_root_with_index(beta) - rk
   end
 
-  found_simple_root = false
+  found_simple_root = index_of_beta <= rk
   current_index = index_of_beta
-  list_of_indizes = Int[]
-  if index_of_beta <= rk
-    found_simple_root = true
-  end
+  list_of_indices = Int[]
   while !found_simple_root
     for j in 1:rk
       next_index = W.refl[j, current_index]
@@ -1131,12 +1128,12 @@ function reflection(beta::RootSpaceElem)
         if current_index <= rk
           found_simple_root = true
         end
-        push!(list_of_indizes, j)
+        push!(list_of_indices, j)
         break
       end
     end
   end
-  return W([list_of_indizes; current_index; reverse(list_of_indizes)])
+  return W([list_of_indices; current_index; reverse(list_of_indices)])
 end
 
 @doc raw"""
@@ -1887,9 +1884,11 @@ Instead of a Dict{WeightLatticeElem, <:IntegerUnion} `groupringelem`, a single W
 ```jldoctest
 julia> R = root_system(:A, 3);
 
-julia> pos_r = positive_root(R, 4);
+julia> pos_r = positive_root(R, 4)
+a_1 + a_2
 
-julia> w = WeightLatticeElem(R, [1, 0, 0]);
+julia> w = fundamental_weight(R, 1)
+w_1
 
 julia> demazure_operator(pos_r, w)
 Dict{WeightLatticeElem, Int64} with 2 entries:
