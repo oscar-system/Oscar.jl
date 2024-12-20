6f894d14847b9a816cc9cc4ceee662de7147c7f8
Fabian Lenzen <fabian.lenzen@tu-berlin.de>
2024-12-20 11:39:14 +0100

    New function argmin (argmin with filter, default, and custom comparator)

--- experimental/AlgebraicShifting/src/AlgebraicShifting.jl
+++ experimental/AlgebraicShifting/src/AlgebraicShifting.jl
@@ -14,6 +14,7 @@ export partial_shift_graph
 export partial_shift_graph_vertices
 export rothe_matrix
 export uniform_hypergraph
+export eargmin
 
 function independent_columns(A::MatElem)
   col_indices = Int[]
@@ -27,4 +28,23 @@ function independent_columns(A::MatElem)
     row_index > row_bound && break
   end
   return col_indices
+end
+
+""" eargmin(f, xs; filter=_->true, default=nothing, lt=Base.isless)
+
+  Extended argmin function. Allows custum filter, default value and comparator.
+  The collection xs need not have linear nor 1-based indexing.
+"""
+function eargmin(f, xs; filter=_->true, default=nothing, lt=Base.isless)
+  best = nothing
+  i_best = default
+  for i in eachindex(xs)
+    x = xs[i]
+    filter(x) || continue
+    if isnothing(best) || lt(f(x), best)
+      best = f(x)
+      i_best = i
+    end
+  end
+  return i_best
 end
