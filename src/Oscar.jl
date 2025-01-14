e9933834bfba9f2be4e9acd1c9a6b60079087a10
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-01-14 11:09:12 +0100

    Bump GAP.jl to 0.13 (#4421)
    and adjust loading / installation of GAP packages
    
    ---------
    Co-authored-by: Max Horn <max@quendi.de>

--- src/Oscar.jl
+++ src/Oscar.jl
@@ -87,22 +87,6 @@ function __init__()
   # `Julia.Oscar` if Oscar is loaded indirectly as a package dependency)
   GAP.Globals.BindGlobal(GapObj("Oscar"), Oscar)
 
-  # Up to now, hopefully the GAP packages listed below have not been loaded.
-  # We want newer versions of some GAP packages than the distributed ones.
-  # (But we do not complain if the installation fails.)
-  for (pkg, version) in [
-     ("recog", "1.4.2"),
-     ("repsn", "3.1.1"),
-     ]
-    # Avoid downloading something if the requested version is already loaded.
-#TODO: Remove this check as soon as GAP.jl contains it,
-#      see https://github.com/oscar-system/GAP.jl/pull/1019.
-    info = GAP.Globals.GAPInfo.PackagesLoaded
-    if !(hasproperty(info, pkg) && version == string(getproperty(info, pkg)[2]))
-      GAP.Packages.install(pkg, version, interactive = false, quiet = true)
-    end
-  end
-
   withenv("TERMINFO_DIRS" => joinpath(GAP.GAP_jll.Readline_jll.Ncurses_jll.find_artifact_dir(), "share", "terminfo")) do
     GAP.Packages.load("browse"; install=true) # needed for all_character_table_names doctest
   end
@@ -111,6 +95,7 @@ function __init__()
      "atlasrep",
      "ctbllib",  # character tables
      "crisp",    # faster normal subgroups, socles, p-socles for finite solvable groups
+     "ferret",   # backtrack in permutation groups
      "fga",      # dealing with free groups
      "forms",    # bilinear/sesquilinear/quadratic forms
      "packagemanager", # has been loaded already by GAP.jl
@@ -123,12 +108,6 @@ function __init__()
      "wedderga", # provides a function to compute Schur indices
      ]
     GAP.Packages.load(pkg) || error("cannot load the GAP package $pkg")
-  end
-  # We want some GAP packages. (It is no error if they cannot be loaded.)
-  for pkg in [
-     "ferret",   # backtrack in permutation groups
-     ]
-    GAP.Packages.load(pkg)
   end
   # Load the OscarInterface package in the end.
   # It needs some other GAP packages,
