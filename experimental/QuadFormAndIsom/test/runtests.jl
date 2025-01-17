26c347fc01d2da732ad62fcc6306307980814ed9
StevellM <muller@math.uni-sb.de>
2025-01-17 15:45:25 +0100

    fix HMM and further cleanup

--- experimental/QuadFormAndIsom/test/runtests.jl
+++ experimental/QuadFormAndIsom/test/runtests.jl
@@ -435,10 +435,11 @@ end
   GLf, _ = image_centralizer_in_Oq(Lf)
   @test order(GLf) == 96
 
-  # Non working example
   B = matrix(QQ, 6, 6, [1 0 0 0 0 0; 0 1 0 0 0 0; 0 0 1 0 0 0; 0 0 0 1 0 0; 0 0 0 0 1 0; 0 0 0 0 0 1]);
   G = matrix(QQ, 6, 6, [0 0 0 0 0 3; 0 0 0 0 3 0; 0 0 -6 0 0 0; 0 0 0 -6 0 0; 0 3 0 0 0 0; 3 0 0 0 0 0]);
   L = integer_lattice(B, gram = G);
   f = matrix(QQ, 6, 6, [0 1 -1 1 2 0; -1 -2 1 -1 0 -2; 0 0 0 1 2 -2; 2 2 -1 0 -2 2; -1 0 0 1 2 -1; 0 1 0 1 1 0]);
   Lf = integer_lattice_with_isometry(L, f);
+  GLf, _ = image_centralizer_in_Oq(Lf)
+  @test order(GLf) == 24192
 end
