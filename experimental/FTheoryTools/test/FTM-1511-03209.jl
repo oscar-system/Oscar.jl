4bcd2d86107029e5b26e25e5d8350df763a35663
Martin Bies <HereAround@users.noreply.github.com>
2025-01-21 20:06:30 +0100

    [FTheoryTools] Support D3-tadpole (#4487) and a few general code improvements
    * Implement D3-tadpole for family of G4-fluxes.
    * Serialize the D3-tadpole for special families of G4-fluxes.
    * Support D3-tadpole also for individual G4-fluxes.
    * General code improvements.
    * Ensure that `inter_dict` is loaded in as `Dict{NTuple{4, Int64}, ZZRingElem}`.
    * Use `MPolyBuildCtx` builder for creation of the tadpole polynomial.
    
    ---------
    Co-authored-by: Max Horn <max@quendi.de>

--- experimental/FTheoryTools/test/FTM-1511-03209.jl
+++ experimental/FTheoryTools/test/FTM-1511-03209.jl
@@ -1,7 +1,8 @@
 @testset "Test Downloading Artifact and elementary properties" begin
-  h = literature_model(arxiv_id = "1511.03209")
-  h_resolved = resolve(h, 1)
+  t = literature_model(arxiv_id = "1511.03209")
+  t_resolved = resolve(h, 1)
 
-  @test n_rays(ambient_space(h)) == 104
-  @test n_rays(ambient_space(h_resolved)) == 310
+  @test n_rays(ambient_space(t)) == 104
+  @test n_rays(ambient_space(t_resolved)) == 310
+  @test typeof(get_attribute(t_resolved, :inter_dict)) == Dict{NTuple{4, Int64}, ZZRingElem}
 end
