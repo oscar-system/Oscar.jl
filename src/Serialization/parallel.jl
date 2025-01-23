feeda4ca8f947dc5ce92e43649b0f54a8af23ab0
HechtiDerLachs <zach@mathematik.uni-kl.de>
2025-01-23 14:00:57 +0100

    Add tests.

--- src/Serialization/parallel.jl
+++ src/Serialization/parallel.jl
@@ -42,23 +42,25 @@ end
 # As said above, the generic (de-)serialization should do, but in case 
 # you want something specialized, you can overwrite the methods here. 
 function save_object(s::SerializerState, ds::SampleDataStruct)
-  @show "serializing"
   save_data_dict(s) do
     save_object(s, ds.elems, :elems)
   end
 end
 
 function load_object(s::DeserializerState, ::Type{<:SampleDataStruct}, params::Dict)
-  @show "deserializing..."
-  R = params[:ring]
+  R = params[:parent]
   return SampleDataStruct(load_object(s, Vector{elem_type(R)}, R, :elems))
 end
 
 # Finally, implement the function which should be called on an instance 
 # of your record (on the workers) to carry out the actual task.
+#
+# Note that if you want to use `wait_first_parallel`, then the return value must 
+# be of the form `(success, result)` where `success` is a `Bool` indicating 
+# whether the worker has obtained an affirmative result in any reasonable sense. 
+# Execution is stopped only if a return pair with `success==true` is found. 
 function _compute(ds::SampleDataStruct)
-  @show "hello"
-  return gcd(ds.elems...)
+  return true, gcd(ds.elems...)
 end
 
 ########################################################################
@@ -66,46 +68,64 @@ end
 ########################################################################
 function wait_all_parallel(
     task_list::Vector{TaskType};
-    workers::Vector{Int}=Oscar.workers() # Specify which workers to use
+    workers::Vector{Int}=Oscar.workers(), # Specify which workers to use
   ) where {TaskType} # TaskType is the type of the task to be deployed.
   n = length(task_list)
   w = length(workers)
   is_zero(w) && !isempty(task_list) && error("zero workers available for non-trivial task; aborting")
-
-  # deploy the parents to the workers at once in the beginning
-  channel_all_workers = params_channels(Any)
-  individual_channels = Dict{Int, RemoteChannel}(i => RemoteChannel(()->Channel{Any}(32), i) for i in workers)
-  assigned_workers = IdDict{TaskType, Int}()
-  futures = Dict{Int, Vector{Future}}()
-  fut_vec = Future[]
-  for (i, task) in enumerate(task_list)
-    @show i
-    wid = workers[mod(i, w) + 1]
-    channel = individual_channels[wid]
-    @show isready(channel)
-    @show type_params(task)
-    put!(channel, type_params(task)[2])
-    @show "sending data..."
-    @show isready(channel)
-    @show task
-    #put!(channel, task)
-    @show "sent data..."
-    #put!(channel, parent(first(task.elems)))
-    #remotecall(take!, wid, channel)
-    assigned_workers[task] = wid
-    fut = remotecall(_compute, wid, task)
-    @show fut
-    @show typeof(fut)
-    push!(get!(futures, wid, Future[]), fut)
-    push!(fut_vec, fut)
-  end
+  fut_vec = _collect_futures(_deploy_work(task_list, workers))
   @sync fut_vec
   return fetch.(fut_vec)
 end
 
 function wait_first_parallel(
     task_list::Vector{T};
-    workers::Vector{Int}=Oscar.workers() # Specify which workers to use
+    workers::Vector{Int}=Oscar.workers(), # Specify which workers to use
+    kill_workers::Bool=false
   ) where {T} # T is the type of the task to be deployed.
+  n = length(task_list)
+  w = length(workers)
+  is_zero(w) && !isempty(task_list) && error("zero workers available for non-trivial task; aborting")
+  futures = _deploy_work(task_list, workers)
+  while true
+    for (wid, fut_vec) in futures
+      if any(isready, fut_vec)
+        k = findfirst(isready, fut_vec)
+        fut = fut_vec[k]
+        success, result = fetch(fut)
+        if success
+          # kill the workers if asked for
+          kill_workers && map(rmprocs, workers)
+          return result
+        end
+      end
+    end
+  end
+end
+
+function _deploy_work(
+    task_list::Vector{TaskType},
+    workers::Vector{Int}
+  ) where {TaskType}
+  w = length(workers)
+  individual_channels = Dict{Int, RemoteChannel}(i => RemoteChannel(()->Channel{Any}(32), i) for i in workers)
+  assigned_workers = IdDict{TaskType, Int}()
+  futures = Dict{Int, Vector{Future}}()
+  fut_vec = Future[]
+  for (i, task) in enumerate(task_list)
+    wid = workers[mod(i, w) + 1]
+    channel = individual_channels[wid]
+    put!(channel, type_params(task)[2])
+    #remotecall(take!, wid, channel)
+    assigned_workers[task] = wid
+    fut = remotecall(_compute, wid, task)
+    push!(get!(futures, wid, Future[]), fut)
+    push!(fut_vec, fut)
+  end
+  return futures
+end
+
+function _collect_futures(futures::Dict{Int, Vector{Future}})
+  return vcat([futs for (_, futs) in futures]...)
 end
 
