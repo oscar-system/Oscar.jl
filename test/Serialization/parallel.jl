11909b823ce2a7493c994388db3ffce5322755e8
HechtiDerLachs <zach@mathematik.uni-kl.de>
2025-01-29 11:27:48 +0100

    Fix exposure.

--- test/Serialization/parallel.jl
+++ test/Serialization/parallel.jl
@@ -20,7 +20,7 @@ end
   R, (x, y) = QQ[:x, :y]
   l = [x^2, x*y, y^2]
   
-  a = [SampleDataStruct([R[a;] for (i, a) in enumerate(l) if i != k]) for k in 1:length(l)]
+  a = [SampleDataStruct(a for (i, a) in enumerate(l) if i != k]) for k in 1:length(l)]
   success, res1 = Oscar.parallel_all(a)
   @test success
   @test all(p in [one(R), x, y] for p in res1)
@@ -32,7 +32,7 @@ end
 @testset "sending dictionaries bug" begin
   R, (x, y) = QQ[:x, :y]
   l = [x^2, x*y, y^2]
-  a = [SampleDataStruct([R[a;] for (i, a) in enumerate(l) if i != k]) for k in 1:length(l)]
+  a = [SampleDataStruct([Dict(:a => R[a;]) for (i, a) in enumerate(l) if i != k]) for k in 1:length(l)]
   @everywhere begin
     function Oscar._compute(ds::SampleDataStruct)
       return true, prod([c[:a] for c in ds.elems])
