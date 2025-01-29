6bb98031169b6315ce770b4e66089d4670fc3a4e
HechtiDerLachs <zach@mathematik.uni-kl.de>
2025-01-29 11:21:11 +0100

    Expose a bug with sending dictionaries.

--- test/Serialization/parallel.jl
+++ test/Serialization/parallel.jl
@@ -4,7 +4,7 @@ procs = addprocs(1)
 @everywhere using Oscar
 
 @everywhere begin
-  struct SampleDataStruct{T} <: Oscar.ParallelTask where T <: Vector{<:RingElem}
+  struct SampleDataStruct{T} <: Oscar.ParallelTask where T
     elems::T
   end
 
@@ -19,13 +19,26 @@ end
 @testset "easy parallelization" begin
   R, (x, y) = QQ[:x, :y]
   l = [x^2, x*y, y^2]
-  a = [SampleDataStruct([a for (i, a) in enumerate(l) if i != k]) for k in 1:length(l)]
+  
+  a = [SampleDataStruct([R[a;] for (i, a) in enumerate(l) if i != k]) for k in 1:length(l)]
   success, res1 = Oscar.parallel_all(a)
   @test success
   @test all(p in [one(R), x, y] for p in res1)
   success, _, res2 = Oscar.parallel_any(a)
   @test success
   @test res2 in [one(R), x, y]
+end
+
+@testset "sending dictionaries bug" begin
+  R, (x, y) = QQ[:x, :y]
+  l = [x^2, x*y, y^2]
+  a = [SampleDataStruct([R[a;] for (i, a) in enumerate(l) if i != k]) for k in 1:length(l)]
+  @everywhere begin
+    function Oscar._compute(ds::SampleDataStruct)
+      return true, prod([c[:a] for c in ds.elems])
+    end
+  end
+  success, res1 = Oscar.parallel_all(a) # Fails
 end
 
 map(rmprocs, procs)
