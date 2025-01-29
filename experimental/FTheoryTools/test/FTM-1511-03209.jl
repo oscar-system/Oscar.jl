e550b810ca00d1052ece0c53d0832826584fcbb2
Martin Bies <HereAround@users.noreply.github.com>
2025-01-29 18:48:08 +0100

    [FTheoryTools] More improvements (#4500)
    * [FTheoryTools] Support family_of_g4_fluxes for a given g4-flux
    * [FTheoryTools] Bug fix and increased efficiency in D3-tadpole computation
    * [FTheoryTools] Update printing of family of G4-fluxes based on tadpole constraint
    * [FTheoryTools] Implement method to check verticality for any flux family
    * [FTheoryTools] Implement method to check non-abelian gauge group breaking by any flux family
    * [FTheoryTools] Implement method to check quantization for any flux family
    * [FTheoryTools] Implement arithmetics for G4-fluxes
    * [FTheoryTools] Introduce "chosen_g4_flux_basis", revoke exporting "ambient_space_models_of_g4_fluxes"
    * [FTheoryTools] Update docstrings
    * [FTheoryTools] Use new attr macro for D3-tadpole of G4-flux family
    * [FTheoryTools] Consolidate and improve docstring for special_flux_family and make various functions internal
    * [FTheoryTools] Bug fix in attribute setter of flux instances
    * [FTheoryTools] G4-fluxes print details in the same order as G4-flux family
    * [FTheoryTools] Make use of attr-macro in more places
    
    ---------
    Co-authored-by: Lars Göttgens <lars.goettgens@gmail.com>

--- experimental/FTheoryTools/test/FTM-1511-03209.jl
+++ experimental/FTheoryTools/test/FTM-1511-03209.jl
@@ -1,8 +1,18 @@
 @testset "Test Downloading Artifact and elementary properties" begin
   t = literature_model(arxiv_id = "1511.03209")
   t_resolved = resolve(t, 1)
-
+  f1 = special_flux_family(t_resolved, check = false)
+  g1 = random_flux_instance(f1, check = false)
+  f2 = special_flux_family(t_resolved, vert = true, check = false)
+  g2 = random_flux_instance(f2, check = false)
+  f3 = special_flux_family(t_resolved, vert = true, not_breaking = true, check = false)
+  g3 = random_flux_instance(f3, check = false)
+  
   @test n_rays(ambient_space(t)) == 104
   @test n_rays(ambient_space(t_resolved)) == 310
   @test typeof(get_attribute(t_resolved, :inter_dict)) == Dict{NTuple{4, Int64}, ZZRingElem}
+  @test length(chosen_g4_flux_basis(h2)) == 629
+  @test passes_elementary_quantization_checks(g1) == true
+  @test passes_verticality_checks(g2) == true
+  @test breaks_non_abelian_gauge_group(g3) == false
 end
