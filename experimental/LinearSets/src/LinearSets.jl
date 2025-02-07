52fd916060ff2a7be7629f2431f6b09c2ed47f66
Morgan Rodgers <morgan.joaquin@gmail.com>
2025-02-07 14:37:10 +0100

    Fix `_points` and `_vector_space`

--- experimental/LinearSets/src/LinearSets.jl
+++ experimental/LinearSets/src/LinearSets.jl
@@ -5,7 +5,7 @@ using Oscar
 
 # Need collection of normalized vectors
 # needs tweaking if order(base_ring(V)) is not prime....
-function _points(V::AbstractAlgebra.FreeModule{T}) where T <: FinFieldElem
+function _points(V::AbstractAlgebra.Generic.FreeModule{<:FinFieldElem})
     p = order(base_ring(V))
     @req is_prime(p) "Currently only works for V over a prime field"
 
@@ -13,18 +13,19 @@ function _points(V::AbstractAlgebra.FreeModule{T}) where T <: FinFieldElem
     n = length(B)
 
     # TODO : should convert these to linear combinations of B instead of raw conversion.
-    [V(digits(p^s + j, base = p, pad = n)) for s in 1:n for j in 0:(p^(s-1)-1)]
+    # But this creates overhead, maybe only do this if B is nonstandard...
+    [V(reverse(digits(p^s + j, base = Int(p), pad = n))) for s in 0:(n-1) for j in 0:(p^s - 1)]
 end
 
 # want to represent field E as a vector space over a subfield.
-function _vector_space(E::FiniteField, phi::Map)
+function _vector_space(E::FinField, phi::Map)
     k = domain(phi)
     @req is_prime(order(k)) "Currently only works for V over a prime field"
     n = degree(E)
     V = vector_space(k,n)
-    phi = map_from_func(a -> V(absolute_coordinates(a)), E, k)
+    rho = map_from_func(a -> V(absolute_coordinates(a)), E, V)
 
-    return V, phi
+    return V, rho
 end
 
 # need to store:
@@ -33,12 +34,12 @@ end
 #     3. set S = Points((Fq)^n) as elements of E
 # TODO decide: Second argument- subfield k, or an embedding k -> E ?
 # NOTE at the moment, we only support F = prime field...
-function linear_set_field_attributes!(K::FinField, phi::Map)
+function linear_set_field_attributes!(E::FinField, phi::Map)
     k = domain(phi)
+    V, rho = _vector_space(E, phi)
     n = divexact(absolute_degree(K), absolute_degree(k))
-    phi = absolute_coordinates
-    S =
-end function
+    S = _points(V)
+end
 
 
 # nu = first(x for x in E if (x != 0 && !is_square(-x) && is_square(-x-1)))
