0caad7bd1c0f17260bb47ed5b5d4c4ba96ea99d9
Torben Wiedemann <7226617+TWiedemann@users.noreply.github.com>
2025-02-11 01:00:29 +0100

    `map_word` and `parabolic_subgroup` for Weyl groups (#4519)

--- experimental/LieAlgebras/src/exports.jl
+++ experimental/LieAlgebras/src/exports.jl
@@ -40,6 +40,8 @@ export killing_matrix
 export lie_algebra
 export lower_central_series
 export matrix_repr_basis
+export parabolic_subgroup
+export parabolic_subgroup_with_projection
 export show_dynkin_diagram
 export simple_module
 export special_linear_lie_algebra
