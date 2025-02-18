58f4f5e575e63d5ae3b8b92617a8f1fa5a17c355
Antony Della Vecchia <vecchia@math.tu-berlin.de>
2025-02-18 17:44:33 +0100

    upgrade fqPolyrepFields

--- src/Serialization/Upgrades/1.3.0.jl
+++ src/Serialization/Upgrades/1.3.0.jl
@@ -1,7 +1,5 @@
 function upgrade_NamedTuple(d::Dict)
-  println(json(d, 2))
   for (i, entry) in enumerate(d[:data])
-    println(entry)
     if entry isa Dict
       func = Symbol(string("upgrade_", entry[:_type][:name]))
       upgraded_entry = @eval $func($dict)
@@ -9,15 +7,40 @@ function upgrade_NamedTuple(d::Dict)
   end
 end
 
+function upgrade_PolyRingElem(d::Dict)
+  return d
+end
+
+function upgrade_PolyRing(d::Dict)
+  return d
+end
+
+function upgrade_fqPolyRepField(d::Dict)
+  upgraded_dict = d
+  upgraded_dict[:_type] = Dict(
+    :name => d[:_type],
+    :params => d[:data][:def_pol][:_type][:params]
+  )
+  upgraded_dict[:data] = d[:data][:def_pol][:data]
+  return upgraded_dict
+end
+
 push!(upgrade_scripts_set, UpgradeScript(
   v"1.3.0",
   function upgrade_1_3_0(s::UpgradeState, dict::Dict)
-    if haskey(dict, :_type) 
-      func = Symbol(string("upgrade_", dict[:_type][:name]))
+    if haskey(dict, :_type)
+      if dict[:_type] isa Dict && haskey(dict[:_type], :name)
+        type_name = dict[:_type][:name]
+      else
+        type_name = dict[:_type]
+      end
+      func = Symbol(string("upgrade_", type_name))
       upgraded_dict = @eval $func($dict)
+        
     elseif haskey(dict, :data) && dict[:data] isa Dict
       upgraded_dict[:data] = upgrade_1_3_0(s, dict[:data])
     end
+
     if haskey(dict, :_refs)
       upgraded_refs = Dict()
       for (k, v) in dict[:_refs]
