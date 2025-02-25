8955b46862ba09259bc234e913c55347d6a1e8db
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-02-25 14:18:09 +0100

    Adapt whitelist

--- experimental/NoExperimental_whitelist_.jl
+++ experimental/NoExperimental_whitelist_.jl
@@ -2,6 +2,7 @@
 # Eventually, this list should be empty.
 whitelist = String[
   "DoubleAndHyperComplexes",    # `MethodError: no method matching simplify(::SubquoModule{MPolyQuoRingElem{MPolyDecRingElem{FqMPolyRingElem, AbstractAlgebra.Generic.MPoly{FqMPolyRingElem}}}})`
+  "ExteriorAlgebra",            # `test/Modules/PBWModules.jl` calls `exterior_algebra(::Field, ::Int)`
   "GModule",                    # `MethodError: no method matching (::FinGenAbGroup)(::FinGenAbGroupElem)`
   "ModStd",                     # `MethodError: no method matching monomial(::QQMPolyRing, ::Vector{Int64})` and many similar errors
   "Schemes",                    # TODO: untangle src/AlgebraicGeometry/Schemes/ and experimental/Schemes/
