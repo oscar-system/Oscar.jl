e94f953a6cd45480ba7cbd06ef874935a50ff870
Antony Della Vecchia <vecchia@math.tu-berlin.de>
2025-02-27 15:19:37 +0100

    graph attribute maps

--- src/Combinatorics/Graphs/structs.jl
+++ src/Combinatorics/Graphs/structs.jl
@@ -1,7 +1,11 @@
 import Oscar: Polymake
 import Oscar.Polymake: Directed, Undirected
 
-struct Graph{T <: Union{Directed, Undirected}}
+@attributes mutable struct Graph{T <: Union{Directed, Undirected}}
   pm_graph::Polymake.Graph{T}
+  
+  function Graph{T}(nverts::Int) where T  <: Union{Directed, Undirected}
+    return new{T}(Polymake.Graph{T}(nverts))
+  end
 end
 
