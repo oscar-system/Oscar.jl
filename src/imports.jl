54e73fc09c886ea1d1a014b7ae0b47db8c904971
antonydellavecchia <antonydellavecchia@gmail.com>
2025-02-28 11:22:48 +0100

    import GraphMap types from Polymake.jl

--- src/imports.jl
+++ src/imports.jl
@@ -163,6 +163,10 @@ import Nemo:
   ZZRing,
   ZZRingElem
 
+import Polymake:
+  EdgeMap,
+  NodeMap
+
 # By default we import everything exported by Hecke, and then also re-export
 # it -- with the exception of identifiers listed in `exclude_hecke` below:
 let exclude_hecke = [
