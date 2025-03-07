34caab571c78328121b64956735c3d7b0cb74c04
Antony Della Vecchia <vecchia@math.tu-berlin.de>
2025-03-07 09:37:18 +0100

    revert IPC test

--- test/Serialization/IPC.jl
+++ test/Serialization/IPC.jl
@@ -4,19 +4,22 @@ process_ids = addprocs(1)
 
 @everywhere using Oscar
 
-wp = WorkerPool(RemoteChannel(()->Oscar.RefChannel{Any}()))
-foreach(w->push!(wp, w), process_ids)
+@testset "Interprocess Serialization" begin
+  channels = Oscar.params_channels(Union{Ring, MatSpace})
+
   Qx, x = QQ[:x]
   F, a = number_field(x^2 + x + 1)
   MR = matrix_space(F, 2, 2)
+
+  Oscar.put_params(channels, Qx)
+  Oscar.put_params(channels, F)
+  Oscar.put_params(channels, MR)
   
   c = [MR([a^i F(1); a a + 1]) for i in 1:5]
-  dets = pmap(det, wp, c)
+  dets = pmap(det, c)
   total = reduce(*, dets)
 
   @test total == F(4)
 end
 
 map(rmprocs, process_ids)
-
-
