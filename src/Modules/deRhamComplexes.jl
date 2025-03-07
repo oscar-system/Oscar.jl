d4973f4f4c32e91911e5501aabeff4f6f161fd61
Matthias Zach <85350711+HechtiDerLachs@users.noreply.github.com>
2025-03-07 12:25:01 +0100

    Use constructor with symbols.
    Co-authored-by: Max Horn <max@quendi.de>

--- src/Modules/deRhamComplexes.jl
+++ src/Modules/deRhamComplexes.jl
@@ -295,12 +295,8 @@ function _relative_kaehler_differentials(
     R::Union{MPolyRing, MPolyLocRing},
     ind::Vector{Int} # indices of the parameter variables in increasing order
   )
-  n = ngens(R)
-  m = length(ind)
-  result = FreeMod(R, n-m)
-  ind_comp = [i for i in 1:n if !(i in ind)]
-  symb = symbols(R)[ind_comp]
-  result.S = [Symbol(:d, symb[i]) for i in 1:(n-m)]
+  symb = [Symbol(:d, symb[i]) for i in 1:n if !(i in ind)]
+  result = FreeMod(R, symb)
   #set_attribute!(result, :show, show_kaehler_differentials)
   #set_attribute!(result, :is_kaehler_differential_module, (R, 1))
   return result
