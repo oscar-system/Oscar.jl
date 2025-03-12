dca9a8da37a5fab0e8bd746d62d07d08472450fa
antonydellavecchia <antonydellavecchia@gmail.com>
2025-03-12 10:03:32 +0100

    move description to docs

--- src/Serialization/parallel.jl
+++ src/Serialization/parallel.jl
@@ -1,39 +1,5 @@
-########################################################################
-# Simple single-machine parallelization
-#
-# In this file we explain and implement simple patterns for parallelization 
-# on a single machine with multiple cores. 
-#
-# This can be used to deploy embarassingly parallel tasks on multiple 
-# cores and either wait for all of them to finish, or for the first 
-# successful computation on one of the cores. 
-########################################################################
-
-# The pattern is the following.
-#   1. Choose a parallel task to be carried out.
-#   2. Wrap up the input data for the task in a concrete instance, say 
-#      `MyParallelTask`, of `ParallelTask` according to the rules below. 
-#   3. Implement `_compute(::MyParallelTask)` to carry out the task at hand.
-#   4. Use `parallel_all` and `parallel_any` on `Vector`s of `MyParallelTask` 
-#      to do things in parallel. 
-
 # An abstract type from which all concrete tasks should be derived. 
 abstract type ParallelTask end 
-# In order for the generic code below to work, any concrete instance 
-# must be of the form 
-#
-# struct MyParallelTask{T1, T2, ...} <: ParallelTask
-#   field1::T1
-#   field2::T2
-#   ...
-# end
-#
-# that is with *concrete types* for the fields with *every one* of those 
-# types appearing in the same order as type parameters. 
-
-# The following is a generic implementation which hopefully serves for 
-# most concrete tasks automatically. The methods might need to be overwritten, 
-# though!
 
 # A generic implementation to extract all parent-like objects appearing 
 # in the fields of a concrete instance of a `ParallelTask`. Knowing these 
