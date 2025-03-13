42e7bfbee983d2b5ce37850b6af471f485fbccc8
anne <anne@krueger-berg.de>
2025-03-13 15:22:24 +0100

    MaxContact data type

--- experimental/Schemes/src/Types.jl
+++ experimental/Schemes/src/Types.jl
@@ -8,7 +8,7 @@ a covering ``C``, which is refined in such a way that on each chart
 a hypersurface of maximal contact may be chosen uniformly, and
 internal data on the maximal contact.
 """
-@attributes mutable struct MaxContactObject{BaseRingType} <: AbsCoveredScheme{BaseRingType}
+@attributes mutable struct MaxContactObject{BaseRingType}
   W_orig::AbsCoveredScheme                  ## the true ambient space
   C::Covering                               ## refined covering of W allowing max contact hypersurfaces
   max_contact_data::IdDict{<:AbsAffineScheme,
@@ -17,7 +17,7 @@ internal data on the maximal contact.
                                             ## second vector: order in which maximal contact hypersurface
                                             ##        max_contact_data(U)[1][i] occurred
                                             ## note: original ambient equations are marked by order 0
-  ambient_param_data::IdDict{<:AbsAffineSchme,<:Tuple{Vector{Int},Vector{Tuple{RingElem,Int}},
+  ambient_param_data::IdDict{<:AbsAffineScheme,<:Tuple{Vector{Int},Vector{Tuple{RingElem,Int}},
                        Vector{<:MatrixElem}}}
                                             ## vector: indices of rows in minor i.e.dependent variables
                                             ## ring element: data on minor selected by the row indices
@@ -27,9 +27,9 @@ internal data on the maximal contact.
                                             ##      for jacobian matrix of max_contact_data(U)[1]
                                             ##      again in blocks corresponding to the steps
 
-  function MaxContactCoveredScheme(
-               W<:AbsCoveredScheme,
-               Cov<:Covering,
+  function MaxContactObject(
+               W::AbsCoveredScheme,
+               Cov::Covering,
                max_contact_data::IdDict{<:AbsAffineScheme,<:Tuple{Vector{MPolyRingElem},Vector{Int}}},
                ambient_param_data::IdDict{<:AbsAffineScheme,
                                           <:Tuple{Vector{Int},Vector{Tuple{RingElem,Int}}, Vector{<:MatrixElem}}})
