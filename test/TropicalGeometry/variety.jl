b573d198026a3dc036591449a19fae2d75786c4e
Dante Luber <danteluber@gmail.com>
2025-03-18 19:22:12 +0100

    test semiring map input

--- test/TropicalGeometry/variety.jl
+++ test/TropicalGeometry/variety.jl
@@ -50,7 +50,8 @@
 	G = grassmann_pluecker_ideal(2,5)
 	T = tropical_prevariety(gens(G))
 	@test length(rays_modulo_lineality(T)[1]) == 10
-	T = tropical_prevariety(max,gens(G))
+	nu = tropical_semiring_map(QQ,max)
+	T = tropical_prevariety(gens(G),nu)
 	@test length(rays_modulo_lineality(T)[1]) == 10
 	TG = tropical_hypersurface.(gens(G))
 	T = tropical_prevariety(TG)
