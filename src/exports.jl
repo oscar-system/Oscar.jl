aadf544f92edb9eb0d7af55d38a51ae0773c9fae
antonydellavecchia <antonydellavecchia@gmail.com>
2025-03-21 12:29:17 +0100

    Adds Attributes to Graphs (#4663)
    * graph attribute maps
    
    * Apply suggestions from code review
    
    * import GraphMap types from Polymake.jl
    
    * move graph imports into struct.jl
    
    * Apply suggestions from code review
    
    Co-authored-by: Benjamin Lorenz <benlorenz@users.noreply.github.com>
    
    * renamed function and added printing when graph maps are attached
    
    * add tests
    
    * Polymake.jl version bump
    
    * update doc imports
    
    * fix docs
    
    * fixed throwing error
    
    * use @req instead
    
    * progress on multiple gaprh maps
    
    * allowing labels for vertex and edges to share a name
    
    * add_label! function
    
    * include graph in graph map
    
    * fixed docs
    
    * bound type params
    
    * bound more type params
    
    * bound graph from labelled edges
    
    * typo
    
    * trying to get passed this unbounded parameters error
    
    * fix doc string
    
    * fix printing
    
    * fix kwargs
    
    * fixing signature
    
    * fix docs
    
    * fix printing
    
    * fix typo
    
    * export labellings function + private helper function
    
    * forgot type in signature
    
    * typo
    
    * fix docs
    
    * visulization updates for graphs
    
    * Apply suggestions from code review
    
    Co-authored-by: Benjamin Lorenz <benlorenz@users.noreply.github.com>
    
    * suggestions from code review
    
    * adjusting docs and printings
    
    * change output of _graph_maps
    
    * fix docs
    
    * Apply suggestions from code review
    
    Co-authored-by: Benjamin Lorenz <benlorenz@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Benjamin Lorenz <benlorenz@users.noreply.github.com>

--- src/exports.jl
+++ src/exports.jl
@@ -216,6 +216,7 @@ export action_function
 export action_homomorphism
 export add_edge!
 export add_gluing!
+export label!
 export add_vertex!
 export add_vertices!
 export adjacency_matrix
@@ -682,6 +683,7 @@ export graph
 export graph_curve
 export graph_from_adjacency_matrix
 export graph_from_edges
+export graph_from_labeled_edges
 export grassmann_pluecker_ideal
 export groebner_basis
 export groebner_basis_f4
@@ -1035,6 +1037,7 @@ export koszul_complex
 export koszul_homology
 export koszul_matrix
 export labeled_matrix_formatted
+export labelings
 export laplacian_matrix
 export lattice_homomorphism
 export lattice_of_one_parameter_subgroups
