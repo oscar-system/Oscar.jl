b984af5d86b4e20536f127b84525db4f43626a74
Morgan Rodgers <morgan.joaquin@gmail.com>
2025-03-27 12:44:49 +0100

    reorganize tests

--- test/Combinatorics/EnumerativeCombinatorics/multipartitions.jl
+++ test/Combinatorics/EnumerativeCombinatorics/multipartitions.jl
@@ -4,30 +4,15 @@
   @test multipartition([T[3,2], T[]]) == multipartition([Partition(T[3,2]),Partition(T[])])
 
   # multi-partitions
-  check = true
-  for n in 0:10
-    for r in 1:5
-      MP = multipartitions(T(n),r)
-      # check that all multipartitions are distinct
-      if MP != unique(MP)
-        check = false
-        break
-      end
-      # check that multipartititons are really multipartitions of n
-      for mp in MP
-        if sum(mp) != n
-          check = false
-          break
-        end
-      end
-      # check that all multipartitions have r parts
-      if length(MP) != 0 && unique([ length(mp) for mp in MP ]) != [r]
-        check = false
-        break
-      end
+  @testset "multipartitions($n, $r) for integer type $T" for n in 0:10, r in 1:5
+    MP = multipartitions(T(n),r)
+
+    @test MP == unique(MP)
+    @test length(MP) == 0 || unique([ length(mp) for mp in MP ]) == [r]
+    for mp in MP
+      @test sum(mp) == n
     end
   end
-  @test check == true
 
   #counting
   for n in 0:5
