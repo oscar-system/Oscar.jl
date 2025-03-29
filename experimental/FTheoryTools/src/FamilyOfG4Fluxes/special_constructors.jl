d17130f565c0ac0b967fefb556c9017140a766fc
Martin Bies <HereAround@users.noreply.github.com>
2025-03-29 10:55:20 +0100

    Update experimental/FTheoryTools/src/FamilyOfG4Fluxes/special_constructors.jl
    Co-authored-by: Miķelis Emīls Miķelsons <mikelis.emils.mikelsons@gmail.com>

--- experimental/FTheoryTools/src/FamilyOfG4Fluxes/special_constructors.jl
+++ experimental/FTheoryTools/src/FamilyOfG4Fluxes/special_constructors.jl
@@ -342,7 +342,7 @@ function special_flux_family(m::AbstractFTheoryModel; not_breaking::Bool = false
     # Identify the offset vector by integrating 1/2 c2 against pair of toric divisors on the hypersurface.
     c2 = lift(polynomial(chern_class(m, 2, check = check)))
     coeffs = collect(coefficients(c2))
-    M = collect(exponents(lift(chern_class)))
+    M = collect(exponents(lift(c2)))
     non_zero_exponents = Vector{Tuple{Int64, Int64}}()
     for my_row in M
       i1 = findfirst(x -> x != 0, my_row)
