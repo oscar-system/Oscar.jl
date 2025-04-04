b6a2f4fbfb98efa2ed00a34f08546f3cd0fb07ec
anne <anne@krueger-berg.de>
2025-04-04 23:14:45 +0200

    fix test for the time being (needs to be undone later on)

--- test/AlgebraicGeometry/Schemes/MaxContact.jl
+++ test/AlgebraicGeometry/Schemes/MaxContact.jl
@@ -7,16 +7,16 @@
   IS = IdealSheaf(W,I)
   WC = scheme(IS)
   inc_X = Oscar.CoveredClosedEmbedding(WC,IS)
-  MC = _initialize_max_contact_object(inc_X)
+  MC = Oscar._initialize_max_contact_object(inc_X)
   DL = Oscar._delta_list(IS)
   @test length(DL) == 2
   MC2 =_max_contact_step(MC, DL[2], 2)
   U = first(covering(MC2))
-  mc_data = maximal_contact_data(MC2)[U]
-  @test ambient_orders(mc_data) == [0,2]
-  @test dependent_variables(mc_data) == [3,1]
-  @test max_contact_minor_data(mc_data) == [(1,1),(1,1)]
-  @test prepared_jacobi_matrices(mc_data)[1] == prepared_jacobi_matrices(maximal_contact_data(MC)[U])[1]
+  mc_data = Oscar.maximal_contact_data(MC2)[U]
+  @test Oscar.ambient_orders(mc_data) == [0,2]
+  @test Oscar.dependent_variables(mc_data) == [3,1]
+  @test Oscar.max_contact_minor_data(mc_data) == [(1,1),(1,1)]
+  @test Oscar.prepared_jacobi_matrices(mc_data)[1] == Oscar.prepared_jacobi_matrices(maximal_contact_data(MC)[U])[1]
   R,(x,y,z) = polynomial_ring(QQ,3)
   J = ideal(R,[x-z])
   S,piS = quo(R,J)
@@ -25,16 +25,16 @@
   IS = IdealSheaf(W,I)
   WC = scheme(IS)
   inc_X = Oscar.CoveredClosedEmbedding(WC,IS)
-  MC = _initialize_max_contact_object(inc_X)
+  MC = Oscar._initialize_max_contact_object(inc_X)
   DL = Oscar._delta_list(IS)
   @test length(DL) == 2
-  MC2 =_max_contact_step(MC, DL[2], 2)
+  MC2 =_Oscar.max_contact_step(MC, DL[2], 2)
   U = first(covering(MC2))
-  mc_data = maximal_contact_data(MC2)[U]
-  @test ambient_orders(mc_data) == [0,2]
-  @test dependent_variables(mc_data) == [3,1]
-  @test max_contact_minor_data(mc_data) == [(-1,1),(-1,1)]
-  @test prepared_jacobi_matrices(mc_data)[1] == prepared_jacobi_matrices(maximal_contact_data(MC)[U])[1]
+  mc_data = Oscar.maximal_contact_data(MC2)[U]
+  @test Oscar.ambient_orders(mc_data) == [0,2]
+  @test Oscar.dependent_variables(mc_data) == [3,1]
+  @test Oscar.max_contact_minor_data(mc_data) == [(-1,1),(-1,1)]
+  @test Oscar.prepared_jacobi_matrices(mc_data)[1] == Oscar.prepared_jacobi_matrices(maximal_contact_data(MC)[U])[1]
   R,(x,y,z) = polynomial_ring(QQ,3)
   J = ideal(R,[x*y-1])
   S,piS = quo(R,J)
@@ -43,7 +43,7 @@
   IS = IdealSheaf(W,I)
   WC = scheme(IS)
   inc_X = Oscar.CoveredClosedEmbedding(WC,IS)
-  MC = _initialize_max_contact_object(inc_X)
+  MC = Oscar._initialize_max_contact_object(inc_X)
   DL = Oscar._delta_list(IS)
   @test length(DL) == 1
 end
