d597a380c95965ae343e1c8069e9be8658873d79
antonydellavecchia <antonydellavecchia@gmail.com>
2025-04-04 16:03:03 +0200

    moving struct in test

--- experimental/Parallel/test/OscarWorkerPool.jl
+++ experimental/Parallel/test/OscarWorkerPool.jl
@@ -1,22 +1,17 @@
 @testset "Parallel" begin
   using Distributed
   # Start a worker pool with Oscar running on the workers.
-  oscar_worker_pool(1) do wp
-    @testset "pmap" begin
-      R, (x, y, z) = GF(101)[:x, :y, :z]
-      @test pmap(f->f^2, wp, gens(R)) == [x^2, y^2, z^2]
+  # Define an auxiliary context object for the distributed computation
+  mutable struct CtxIO
+    a::Vector
+    b::Dict{Int, Any}
+    function CtxIO(a::Vector)
+      return new(a, Dict{Int, Any}())
     end
+  end
 
+  oscar_worker_pool(1) do wp
     @testset "compute distributed" begin
-      # Define an auxiliary context object for the distributed computation
-      mutable struct CtxIO
-        a::Vector
-        b::Dict{Int, Any}
-        function CtxIO(a::Vector)
-          return new(a, Dict{Int, Any}())
-        end
-      end
-
       # Implement methods for `pop_task!` and `process_result!` for this 
       # type as explained in their docstrings. 
       Oscar.pop_task!(ctx::CtxIO) = is_empty(ctx.a) ? nothing : (length(ctx.a), x->x^2, pop!(ctx.a))
@@ -32,6 +27,11 @@
       # Verify that the result is correct. 
       @test all(x in keys(ctx.b) for x in [1, 2, 3])
       @test all(a^2 in values(ctx.b) for a in gens(R))
+    end
+
+    @testset "pmap" begin
+      R, (x, y, z) = GF(101)[:x, :y, :z]
+      @test pmap(f->f^2, wp, gens(R)) == [x^2, y^2, z^2]
     end
   end
 end
