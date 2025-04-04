30d6d1eb9e400fa939bb2220d501ecb42a0c4a70
HechtiDerLachs <zach@mathematik.uni-kl.de>
2025-04-04 19:32:51 +0200

    Update first example.

--- experimental/InjectiveResolutions/src/examples/localCohomology/first_example.jl
+++ experimental/InjectiveResolutions/src/examples/localCohomology/first_example.jl
@@ -5,10 +5,28 @@ R_Q,phi = quo(S,J)
 x,y,z = gens(R_Q)
 
 # get MonoidAlgebra
-kQ = get_monoid_algebra(R_Q)
+kQ = Oscar.MonoidAlgebra(R_Q);
+x, y, z = gens(kQ);
+
+Oscar.polyhedral_cone(kQ)
+Oscar.cone(kQ)
+Oscar.faces(kQ)
+Oscar.hyperplanes(kQ)
+Oscar.is_pointed(kQ)
+Oscar.zonotope(kQ)
+
+@test one(kQ) - 2 == -one(kQ)
 
 #example of computing local cohomology modules 
 I_M = ideal(kQ,[x^2*z,x^4*y])
+
+@test !(x in I_M)
+@test x^2*z in I_M
+coordinates(x^2*z, I_M)
+
+@test is_subset(I_M, radical(I_M))
+@test I_M != radical(I_M)
+
 I = ideal(kQ,[x,y,z]) #maximal ideal
 M = quotient_ring_as_module(I_M)
 
