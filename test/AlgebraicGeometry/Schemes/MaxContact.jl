8075d55439fb9ac65decfd174e96755d2f71368c
anne <anne@krueger-berg.de>
2025-04-06 20:38:41 +0200

    yet more Oscar. to allow tests for intermediate state

--- test/AlgebraicGeometry/Schemes/MaxContact.jl
+++ test/AlgebraicGeometry/Schemes/MaxContact.jl
@@ -11,12 +11,12 @@
   DL = Oscar._delta_list(IS)
   @test length(DL) == 2
   MC2 = Oscar._max_contact_step(MC, DL[2], 2)
-  U = first(covering(MC2))
+  U = first(Oscar.covering(MC2))
   mc_data = Oscar.maximal_contact_data(MC2)[U]
   @test Oscar.ambient_orders(mc_data) == [0,2]
   @test Oscar.dependent_variables(mc_data) == [3,1]
   @test Oscar.max_contact_minor_data(mc_data) == [(1,1),(1,1)]
-  @test Oscar.prepared_jacobi_matrices(mc_data)[1] == Oscar.prepared_jacobi_matrices(maximal_contact_data(MC)[U])[1]
+  @test Oscar.prepared_jacobi_matrices(mc_data)[1] == Oscar.prepared_jacobi_matrices(Oscar.maximal_contact_data(MC)[U])[1]
   R,(x,y,z) = polynomial_ring(QQ,3)
   J = ideal(R,[x-z])
   S,piS = quo(R,J)
@@ -28,13 +28,13 @@
   MC = Oscar._initialize_max_contact_object(inc_X)
   DL = Oscar._delta_list(IS)
   @test length(DL) == 2
-  MC2 =_Oscar.max_contact_step(MC, DL[2], 2)
-  U = first(covering(MC2))
+  MC2 = Oscar._max_contact_step(MC, DL[2], 2)
+  U = first(Oscar.covering(MC2))
   mc_data = Oscar.maximal_contact_data(MC2)[U]
   @test Oscar.ambient_orders(mc_data) == [0,2]
   @test Oscar.dependent_variables(mc_data) == [3,1]
   @test Oscar.max_contact_minor_data(mc_data) == [(-1,1),(-1,1)]
-  @test Oscar.prepared_jacobi_matrices(mc_data)[1] == Oscar.prepared_jacobi_matrices(maximal_contact_data(MC)[U])[1]
+  @test Oscar.prepared_jacobi_matrices(mc_data)[1] == Oscar.prepared_jacobi_matrices(Oscar.maximal_contact_data(MC)[U])[1]
   R,(x,y,z) = polynomial_ring(QQ,3)
   J = ideal(R,[x*y-1])
   S,piS = quo(R,J)
