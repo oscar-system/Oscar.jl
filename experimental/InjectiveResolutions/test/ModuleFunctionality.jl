3efbf6403561252d31116e2b80e2137d5e623351
HechtiDerLachs <zach@mathematik.uni-kl.de>
2025-04-25 11:17:45 +0200

    Add tests [no ci].

--- experimental/InjectiveResolutions/test/ModuleFunctionality.jl
+++ experimental/InjectiveResolutions/test/ModuleFunctionality.jl
@@ -19,3 +19,25 @@
   @test F[1] - F[2] in kernel(phi)[1]
 end
 
+@testset "graded modules over MonoidAlgebras and their ideals" begin
+# definition of monoid algebra as quotient of polynomial ring
+S, (x, y, z) = graded_polynomial_ring(QQ, ["x", "y", "z"]; weights=[[0, 1], [1, 1], [2, 1]])
+J = ideal(S, [x*z-y^2])
+R_Q, phi = quo(S, J)
+
+# get MonoidAlgebra
+kQ = Oscar.MonoidAlgebra(R_Q)
+
+@test is_graded(kQ)
+G = grading_group(kQ)
+(x, y, z) = gens(kQ)
+degree.(gens(kQ))
+
+F = graded_free_module(kQ, [zero(G)])
+I, inc = sub(F, [x*F[1] for x in gens(kQ)])
+J = ideal(kQ, gens(kQ))
+Oscar._saturation(I.sub, J)
+
+end
+
+
