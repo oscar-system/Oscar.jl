c67ebf5fe50f0a2c32b84dd7574c173ad8a7d5d6
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-04-25 18:42:53 +0200

    Update docs part of ExperimentalTemplate (#4805)

--- docs/make_work.jl
+++ docs/make_work.jl
@@ -35,16 +35,7 @@ Base.print(io::IO, b::Base.Docs.Binding) = print(io, b.var)
 # get the prefix `Experimental/PACKAGE_NAME`.
 #
 # Example:
-# 1. cat experimental/PlaneCurve/docs/doc.main:
-# [
-#    "plane_curves.md",
-# ]
-# after `add_prefix_to_experimental_docs` becomes
-# [
-#    "Experimental/PlaneCurve/plane_curves.md",
-# ]
-#
-# 2. cat experimental/FTheoryTools/docs/doc.main 
+# cat experimental/FTheoryTools/docs/doc.main 
 # [
 #    "F-Theory Tools" => [
 #       "introduction.md",
