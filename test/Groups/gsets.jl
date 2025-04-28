31a27f5ffd3527054e44b55f28159d55adc5f421
ThomasBreuer <sam@math.rwth-aachen.de>
2025-04-28 11:28:59 +0200

    fix the test ...

--- test/Groups/gsets.jl
+++ test/Groups/gsets.jl
@@ -170,6 +170,8 @@
   G = symmetric_group(6)
   Omega = gset(G, permuted, [[0,1,0,1,0,1], [1,2,3,4,5,6]])
   acthom = action_homomorphism(Omega)
+  g = gen(G, 1)
+  pi = permutation(Omega, g)
   @test pi == g^acthom
   @test has_preimage_with_preimage(acthom, pi)[1]
   @test order(image(acthom)[1]) == 720
