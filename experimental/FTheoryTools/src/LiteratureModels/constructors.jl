f21047ee64f385f11909b0db6b612786a851d43b
Martin Bies <MBies87@googlemail.com>
2025-04-30 13:58:23 +0200

    [FTheoryTools] Clean up literature model constructor

--- experimental/FTheoryTools/src/LiteratureModels/constructors.jl
+++ experimental/FTheoryTools/src/LiteratureModels/constructors.jl
@@ -235,10 +235,6 @@ function literature_model(model_dict::Dict{String, Any}; model_parameters::Dict{
     qsmd_path = artifact"QSMDB"
     qsm_model = load(joinpath(qsmd_path, "$k.mrdi"))
     set_exceptional_classes(qsm_model, string.(model_dict["model_data"]["exceptional_classes"]))
-    # Set meta data attributes
-    #cfs = matrix(ZZ, transpose(hcat([[eval_poly(weight, ZZ) for weight in vec] for vec in model_dict["model_data"]["classes_of_tunable_sections_in_basis_of_Kbar_and_defining_classes"]]...)))
-    #cfs = vcat([[Int(k) for k in cfs[i:i,:]] for i in 1:nrows(cfs)]...)
-    #model_dict["model_data"]["classes_of_tunable_sections_in_basis_of_Kbar_and_defining_classes"] = cfs
     #_set_all_attributes(qsm_model, model_dict, model_parameters)
     return qsm_model
   end
@@ -255,6 +251,7 @@ function literature_model(model_dict::Dict{String, Any}; model_parameters::Dict{
     base_space = load(directory)
     set_attribute!(base_space, :coordinate_names, ["w$i" for i in 0:100])
     model = global_tate_model(base_space, completeness_check = false)
+    model_dict["literature_identifier"] = "1511_03209"
     _set_all_attributes(model, model_dict, model_parameters)
     return model
     =#
