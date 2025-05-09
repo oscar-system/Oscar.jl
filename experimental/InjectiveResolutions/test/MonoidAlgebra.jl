d79770f6da5079f2b61471895c28aa414dacecc7
HechtiDerLachs <zach@mathematik.uni-kl.de>
2025-05-09 12:04:25 +0200

    Add ring conformance tests.

---
+++ experimental/InjectiveResolutions/test/MonoidAlgebra.jl
@@ -0,0 +1,20 @@
+@testset "Ring interface for MonoidAlgebra" begin
+  a = 2
+  function ConformanceTests.generate_element(A::MonoidAlgebra{<:FieldElem, T}) where {T<:MPolyQuoRing}
+    return A(rand(base_ring(A.algebra), -a:a, 0:a, 0:a))
+  end
+
+  function ConformanceTests.generate_element(A::MonoidAlgebra{<:FieldElem, T}) where {T<:MPolyRing}
+    return A(rand(A.algebra, -a:a, 0:a, 0:a))
+  end
+
+  S, (x, y, z) = graded_polynomial_ring(QQ, ["x", "y", "z"]; weights=[[0, 1], [1, 1], [2, 1]])
+  J = ideal(S, [x*z-y^2])
+  R_Q, phi = quo(S, J)
+
+  # get MonoidAlgebra
+  kQ = Oscar.MonoidAlgebra(R_Q)
+  Oscar.ConformanceTests.test_Ring_interface(kQ)
+  Oscar.ConformanceTests.test_Ring_interface_recursive(kQ)
+end
+
