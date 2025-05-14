08311b33a75b1ed0156693cdd3fcfbc4752861f5
HechtiDerLachs <zach@mathematik.uni-kl.de>
2025-05-14 11:11:45 +0200

    Remove deprecated test files.

--- experimental/InjectiveResolutions/src/examples/semigroupRing/semi_group_5.jl
+++
@@ -1,9 +0,0 @@
-kQ = monoid_algebra_from_lattice([[1, 0, 0], [1, 1, 0], [1, 1, 1], [1, 0, 1]], QQ)
-a, b, c, d = gens(kQ)
-
-#some longer examples
-I = ideal(kQ, [a^2*b, c^2])
-@time inj_res = injective_res(M, 3)
-
-I = ideal(kQ, [a^2*b, c^2, d*a^4])
-@time inj_res = injective_res(I, 3)
