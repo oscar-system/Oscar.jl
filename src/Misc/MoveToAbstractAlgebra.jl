08c390adbcc691ddb61205c4a0f3147a7ddc3b5f
Matthias Zach <85350711+HechtiDerLachs@users.noreply.github.com>
2025-05-16 11:02:25 +0200

    Improve comparison of morphisms of modules (#4706)
    Co-authored-by: Lars Göttgens <lars.goettgens@rwth-aachen.de>

--- src/Misc/MoveToAbstractAlgebra.jl
+++ src/Misc/MoveToAbstractAlgebra.jl
@@ -60,6 +60,17 @@ function (a::Generic.RationalFunctionFieldElem)(b::RingElem)
   return divexact(numerator(a)(b), denominator(a)(b))
 end
 
+########################################################################
+# Part of PR #4706
+function is_equal_as_morphism(f::Map, g::Map)
+  f === g && return true
+  domain(f) === domain(g) || return false
+  codomain(f) === codomain(g) || return false
+  error("comparison of maps $f and $g not possible")
+end
+# end of changes in PR #4706
+########################################################################
+
 function evaluate(f::AbstractAlgebra.Generic.FracFieldElem{<:MPolyRingElem}, a::Vector{T}) where {T<:RingElem}
   return evaluate(numerator(f), a)//evaluate(denominator(f), a)
 end
--- src/Misc/MoveToHecke.jl
+++ src/Misc/MoveToHecke.jl
@@ -122,3 +122,15 @@ function isomorphism(::Type{FinGenAbGroup}, G::Group; on_gens::Bool=false)
 end
 #
 ###############################################################################
+
+########################################################################
+# Part of PR #4706
+function is_equal_as_morphism(f::MapFromFunc, g::MapFromFunc)
+  f === g && return true
+  domain(f) === domain(g) || return false
+  codomain(f) === codomain(g) || return false
+  f.f === g.f && return true
+  error("comparison of maps $f and $g not possible")
+end
+# end of changes in PR #4706
+########################################################################
