fa50f71627642415fd1d77c01998d2a5bbb8b0d7
Ben Hollering <benjamin.hollering@mis.mpg.de>
2025-05-26 15:33:36 +0200

    added test for MultigradedImplicitization for K3P networks

--- experimental/AlgebraicStatistics/test/MultigradedImplicitization.jl
+++ experimental/AlgebraicStatistics/test/MultigradedImplicitization.jl
@@ -18,3 +18,43 @@
   @test components[G([0, 0, 0, 0, 1, 1, 1, 1])] == [y11 * y22 - y12 * y21]
   @test components[G([1, 1, 1, 1, 0, 0, 0, 0])] == [x11 * x22 - x12 * x21]
 end
+
+
+@testset "K3P" begin
+  using Distributed, DataStructures
+
+  G = abelian_group(2, 2)
+  elG = collect(G)
+  T = graph_from_edges(Directed, [[6, 5], [5, 1], [5, 2], [6, 3], [6, 4]])
+  M = kimura3_model(T)
+
+
+  inds = [g for g in collect(Iterators.product([elG for _ in 1:4]...)) if sum(g) == elG[1]]
+  S, p = polynomial_ring(QQ, "q" => inds)
+  q = Dict()
+  for i in 1:length(p)
+    q[inds[i]] = p[i]
+  end
+
+
+  param_inds = [(i, g) for i in 1:8 for g in elG]
+  R, b = polynomial_ring(QQ, "a" => param_inds)
+  a = Dict()
+  for i in 1:length(param_inds)
+    ind = param_inds[i]
+    a[ind[1], ind[2]] = b[i]
+  end
+
+  images = []
+
+  for g in inds
+
+    im = a[1, g[1]]*a[2, g[2]]*a[3, g[3]]*a[4, g[4]]*(a[5, g[1]]*a[6, g[1]+g[2]]*a[7, g[4]] + a[6, g[2]]*a[7, g[2]+g[3]]*a[8, g[1]])
+    push!(images, im)
+  end
+
+  phi = hom(S, R, images);
+
+  owp = oscar_worker_pool(8)
+  components_of_kernel(2, phi, wp = owp)
+end
