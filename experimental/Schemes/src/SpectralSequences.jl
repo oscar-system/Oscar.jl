dc7fbb58affa5f16e342db069a2a0d515a57bda8
HechtiDerLachs <zach@mathematik.uni-kl.de>
2025-06-04 10:40:23 +0200

    Remove deprecated code.

--- experimental/Schemes/src/SpectralSequences.jl
+++ experimental/Schemes/src/SpectralSequences.jl
@@ -199,20 +199,6 @@ function can_compute_map(cssp::CSSPage, i::Int, j::Int)
   return can_compute_index(cssp, i, j) && can_compute_index(cssp, i-p, j+p-1)
 end
 
-function produce_entry_on_second_page(cssp::CSSPage, i::Int, j::Int)
-  css = spectral_sequence(cssp)
-  p = page_number(cssp)
-  #first_page = css[1]
-  previous_page = css[p-1]
-  H = previous_page[i, j]
-  Z, inc_Z = can_compute_map(previous_page, i, j) ? kernel(map(previous_page, i, j)) : sub(H, gens(H))
-  B, inc_B = can_compute_map(previous_page, i+p-1, j-p+2) ? image(map(previous_page, i+p-1, j-p+2)) : sub(H, elem_type(H)[])
-  return quo(Z, B)[1]
-  return SubquoModule(F, ambient_representatives_generators(Z), 
-                      ambient_representatives_generators(B))
-end
-
-
 function maps(cssp::CSSPage)
   if !isdefined(cssp, :maps)
     cssp.maps = Dict{Tuple{Int, Int}, ModuleFPHom}()
@@ -257,14 +243,8 @@ function produce_lifted_kernel_generators_on_initial_page(cssp::CSSPage, i::Int,
       phi = multiplication_map(ctx, p, e, d, j)
       vvv = phi(vv2)
       @assert !is_zero(vvv)
-      ind = findfirst(ll == l for (ll, _) in buckets)
-      if isnothing(ind)
-        push!(buckets, (l, vvv))
-      else
-        error("this should not happen")
-        _, www = buckets[ind]
-        buckets[ind] = (l, www + vvv)
-      end
+      @assert !any(ll == l for (ll, _) in buckets)
+      push!(buckets, (l, vvv))
     end
     @assert !is_empty(e)
     push!(result, (e, buckets))
@@ -376,13 +356,8 @@ function produce_lifted_kernel_generators(cssp::CSSPage, i::Int, j::Int)
       e, g = v[l]
       # bring everything to the same denominators
       for (j, v) in (e != sup_exp ? Tuple{Int, <:FreeModElem}[(j, ctx[e, sup_exp, -inter_degs[j]][jj](v)) for (j, v) in g] : g)
-        ind = findfirst(k==j for (k, _) in buckets)
-        if isnothing(ind)
-          push!(buckets, (j, c*v))
-        else
-          _, w = buckets[ind]
-          buckets[ind] = (j, w + c*v)
-        end
+        @assert !any(k==j for (k, _) in buckets)
+        push!(buckets, (j, c*v))
       end
     end
       
@@ -753,98 +728,6 @@ function multiplication_map(
   dom_strand_inc = inclusion_map(dom_cplx)[j]
   cod_strand_pr = projection_map(cod_cplx)[j]
   return MapFromFunc(dom, cod, v->cod_strand_pr(p*dom_strand_inc(v)))
-end
-
-function produce_map_on_initial_page(cssp::CSSPage, i::Int, j::Int)
-  dom = cssp[i, j]::FreeMod # the cohomology model
-  cod = cssp[i-1, j]::FreeMod # the cohomology model
-  c = graded_complex(cssp) # the original complex of graded modules
-  orig_dom = c[i]::FreeMod # the graded module for this column
-  orig_cod = c[i-1]::FreeMod # the graded module for the column of the codomain
-  orig_map = map(c, i)::FreeModuleHom # the original map between the graded modules
-  # initialize the result
-  result = hom(dom, cod, elem_type(cod)[zero(cod) for _ in 1:ngens(dom)])
-  # `ctx` holds precomputed truncated Cech-complexes, their inclusions, and their strands
-  ctx = pushforward_ctx(cssp)
-  # the cohomology model is a direct sum of modules for each generator of `orig_dom`.
-  dom_proj = canonical_projections(dom) # projections for the direct sum
-  cod_inj = canonical_injections(cod)   # inclusions for the direct sum
-
-  # We assemble the induced maps on the cohomology model.
-  # It comes in blocks for the generators of `orig_dom` and `orig_cod`.
-  for (k, v) in enumerate(images_of_generators(orig_map))
-    d0 = -degree(orig_dom[k]) # the relevant degree in the domain for this block
-    e0 = _minimal_exponent_vector(ctx, d0) # determines the exponent for the 
-                                           # truncated Cech-complex for the cohomology 
-                                           # model in the domain for this block
-    dom_complex = ctx[e0, d0] # the said truncated Cech-complex
-    dom_summand = dom_complex[j] 
-    pr_k = dom_proj[k] # from the cohomology module to the summand
-    @assert domain(pr_k) === dom
-    dom_coh_complex = cohomology_model(ctx, d0) # the simplified version of dom_complex
-    dom_coh = dom_coh_complex[j]
-    @assert codomain(pr_k) === dom_coh
-
-    h_inj = cohomology_model_inclusion(ctx, d0, j) # from the summand to the minimal 
-                                                   # truncated Cech complex
-    @assert codomain(pr_k) === domain(h_inj)
-    dom_strand = codomain(h_inj)
-
-    dom_str_inc = inclusion_map(dom_complex)[j]
-    @assert domain(dom_str_inc) === dom_strand
-
-    for (l, p) in coordinates(v)
-      # On the `(k, l)`-th block we have the map which is induced by multiplication 
-      # with `p` as `-⋅p : S¹ → S¹`. 
-      # We extract this map on the strands of the truncated Cech-complex for the 
-      # exponent vector `e0` (for degree `-d0`). This will then be projected 
-      # to the minimal degree for the `l`-th generator and subsequently to the 
-      # cohomology model. 
-      inc_l = cod_inj[l] # inclusion of the direct summand for the `l`-th generator
-                         # of the codomain
-      @assert codomain(inc_l) === cod
-
-      # Assemble the morphism given by multiplication with p
-      #
-      #   x ↦ p⋅x : dom_strand → ext_cod_strand
-      #
-      deg_p = degree(p; check=false)
-      d1 = d0 + deg_p
-      @assert d1 == -degree(orig_cod[l])
-      h_pr = cohomology_model_projection(ctx, d1, j)
-      cod_coh = cohomology_model(ctx, d1)[j]
-      @assert cod_coh === codomain(h_pr)
-      @assert domain(inc_l) === codomain(h_pr)
-
-      e1 = _minimal_exponent_vector(ctx, d1)
-      cod_cplx_ext = ctx[e0, d1]
-      cod_cplx = ctx[e1, d1]
-      strand_reduction = ctx[e0, e1, d1]
-      @assert domain(strand_reduction) === cod_cplx_ext
-      @assert codomain(strand_reduction) === cod_cplx
-
-
-      cod_str_pr = projection_map(cod_cplx_ext)[j] # the map from the graded module to the extended strand
-      ext_cod_strand = cod_cplx_ext[j]
-      @assert codomain(cod_str_pr) === ext_cod_strand
-
-      #img_gens = elem_type(ext_cod_strand)[cod_str_pr(p*dom_str_inc(v)) for v in gens(dom_strand)]
-      #induced_map = hom(dom_strand, ext_cod_strand, img_gens)
-      induced_map = multiplication_map(ctx, p, e0, d0, j)
-      @assert domain(induced_map) === dom_strand
-      @assert codomain(induced_map) === ext_cod_strand
-
-      h_pr = cohomology_model_projection(ctx, d1, j)
-      red = strand_reduction[j]
-      block_img_gens = elem_type(cod_coh)[h_pr(
-                                            red(
-                                              induced_map(
-                                                h_inj(v)
-                                          ))) for v in gens(dom_coh)]
-      result += compose(compose(dom_proj[k], hom(dom_coh, cod_coh, block_img_gens)), cod_inj[l])
-    end
-  end
-  return result
 end
 
 function stable_index(css::CohomologySpectralSequence, i::Int, j::Int)
