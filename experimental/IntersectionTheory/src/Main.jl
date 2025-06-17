b5a37baad0e28b0ccb29f94824fa4f86a9023a42
Wolfram Decker <decker@mathematik.uni-kl.de>
2025-06-17 20:33:07 +0200

    pass along attribute `:alg` in `projective_bundle``, more in docu (#5008)

--- experimental/IntersectionTheory/src/Main.jl
+++ experimental/IntersectionTheory/src/Main.jl
@@ -1698,12 +1698,13 @@ betti_numbers(X::AbstractVariety) = length.(basis(X))
 @doc raw"""
     integral(c:::Union{MPolyDecRingElem, MPolyQuoRingElem})
 
-Given an element `c` of the Chow ring of an abstract variety, return the integral of `c`.
+Given an element `c` of the Chow ring of an abstract variety `X`, say, return the integral of `c`.
 
 !!! note
-    If the abstract variety has been given a point class, the integral will be an element of the coefficient ring of the Chow ring.
-    That is, typically, in the applications we discuss here, it will be a rational (if not integral) number (the degree of the 0-dimensional part
-    of `c`) or an element of a function field of type $\mathbb Q(t_1, \dots, t_r)$.  If no point class is given, the 0-dimensional
+    If the abstract variety has been given a point class, and `length(basis(X, dim(X)) == 1`,
+    then the integral will be an element of the coefficient ring of the Chow ring.
+    That is, typically, in the applications we discuss here, it will be a rational number (the degree of the 0-dimensional part
+    of `c`) or an element of a function field of type $\mathbb Q(t_1, \dots, t_r)$.  If one of the conditions is not fulfilled, the 0-dimensional
     part of `c` is returned.
 
 # Examples
@@ -2466,6 +2467,9 @@ function projective_bundle(F::AbstractBundle; symbol::String = "z")
   PF.struct_map = p
   set_attribute!(PF, :description => "Projectivization of $F")
   set_attribute!(PF, :grassmannian => :relative)
+  if get_attribute(X, :alg) == true
+    set_attribute!(PF, :alg => true)
+  end
   return PF
 end
 
