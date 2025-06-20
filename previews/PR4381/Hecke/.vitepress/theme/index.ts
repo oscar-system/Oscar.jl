0e652bf9beca272af948bd68866c3edd8fc116f5
Documenter.jl <documenter@juliadocs.github.io>
2025-06-20 10:51:29 +0000

    build based on 61bbfb4

--- previews/PR4381/Hecke/.vitepress/theme/index.ts
+++ previews/PR4381/Hecke/.vitepress/theme/index.ts
@@ -1,15 +1,33 @@
 // .vitepress/theme/index.ts
 import { h } from 'vue'
-import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import VersionPicker from "./VersionPicker.vue"
+import type { Theme as ThemeConfig } from 'vitepress'
+
+import { 
+  NolebaseEnhancedReadabilitiesMenu, 
+  NolebaseEnhancedReadabilitiesScreenMenu, 
+} from '@nolebase/vitepress-plugin-enhanced-readabilities/client'
+
+import VersionPicker from "../../components/VersionPicker.vue"
 import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
+
+import '@nolebase/vitepress-plugin-enhanced-readabilities/client/style.css'
 import './style.css'
 
-export default {
+export const Theme: ThemeConfig = {
   extends: DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'nav-bar-content-after': () => [
+        h(NolebaseEnhancedReadabilitiesMenu), // Enhanced Readabilities menu
+      ],
+      // A enhanced readabilities menu for narrower screens (usually smaller than iPad Mini)
+      'nav-screen-content-after': () => h(NolebaseEnhancedReadabilitiesScreenMenu),
+    })
+  },
   enhanceApp({ app, router, siteData }) {
     enhanceAppWithTabs(app);
     app.component('VersionPicker', VersionPicker);
   }
-} satisfies Theme
+}
+export default Theme
