94d8fe1091f50bb62c98e43b4ac27385b73b625a
Kamillo Ferry <kafe@kafe.dev>
2025-06-22 23:02:08 +0200

    Add some docstrings

--- src/TropicalGeometry/TropicalPolyhedron/constructors.jl
+++ src/TropicalGeometry/TropicalPolyhedron/constructors.jl
@@ -3,11 +3,21 @@ const MinOrMax = Union{typeof(min), typeof(max)}
 struct TropicalPolyhedron{M<:MinOrMax} <: PolyhedralObject{QQFieldElem}
   pm_tpolytope::Polymake.BigObject
 
+  @doc raw"""
+      TropicalPolyhedron{M}(p::Polymake.BigObject) where M<:MinOrMax
+
+  Constructs a `TropicalPolyhedron` corresponding to a `Polymake.BigObject` with tropical addition `M`.
+  """
   TropicalPolyhedron{M}(p::Polymake.BigObject) where {M<:MinOrMax} = new{M}(p)
 end
 struct TropicalPointConfiguration{M<:MinOrMax} <: PolyhedralObject{QQFieldElem}
   pm_tpolytope::Polymake.BigObject
 
+  @doc raw"""
+      TropicalPointConfiguration{M}(p::Polymake.BigObject) where M<:MinOrMax
+
+  Constructs a `TropicalPointConfiguration` corresponding to a `Polymake.BigObject` with tropical addition `M`.
+  """
   TropicalPointConfiguration{M}(p::Polymake.BigObject) where {M<:MinOrMax} = new{M}(p)
 end
 
@@ -25,9 +35,27 @@ tropical_point_configuration(P::TropicalPolyhedron{M}) where {M<:MinOrMax} = Tro
 tropical_convex_hull(V::AbstractVector{T}) where {T<:AbstractVector{<:TropicalSemiringElem}} = tropical_convex_hull(matrix(V))
 tropical_point_configuration(V::AbstractVector{T}) where {T<:AbstractVector{<:TropicalSemiringElem}} = tropical_point_configuration(matrix(V))
 
+@doc raw"""
+    tropical_convex_hull(V::MatElem{TropicalSemiringElem{M}}) where M<:MinOrMax
+
+Constructs the tropical convex hull of the vertices `V` in the tropical projective torus
+with `M` as tropical addition.
+"""
 tropical_convex_hull(V::MatElem{TropicalSemiringElem{M}}) where {M<:MinOrMax} = _tropical_convex_hull(TropicalPolyhedron, V)
+@doc raw"""
+    tropical_point_configuration(V::MatElem{TropicalSemiringElem{M}}) where M<:MinOrMax
+
+Constructs the tropical point configuration of the vertices `V` in the tropical projective torus
+with `M` as tropical addition.
+"""
 tropical_point_configuration(V::MatElem{TropicalSemiringElem{M}}) where {M<:MinOrMax} = _tropical_convex_hull(TropicalPointConfiguration, V)
 
+@doc raw"""
+    tropical_convex_hull(convention::MinOrMax, V::QQMatrix)
+
+Constructs the tropical convex hull of the vertices `V` in the tropical projective torus
+with `convention` as tropical addition.
+"""
 tropical_convex_hull(convention::MinOrMax, V::QQMatrix) = _tropical_convex_hull(TropicalPolyhedron, convention, V)
 tropical_point_configuration(convention::MinOrMax, V::QQMatrix) = _tropical_convex_hull(TropicalPointConfiguration, convention, V)
 
@@ -43,6 +71,12 @@ function _tropical_convex_hull(as::Union{Type{TropicalPolyhedron},Type{TropicalP
   return as{typeof(convention)}(p)
 end
 
+@doc raw"""
+    tropical_polyhedron(A::MatElem{TropicalSemiringElem{M}}, B::MatElem{TropicalSemiringElem{M}}) where M<:MinOrMax
+
+Constructs a `TropicalPolyhedron` as the set of points in the tropical projective torus satisfying the inequalities $A\odot x\leq B\odot x$
+using `M` as the tropical addition.
+"""
 function tropical_polyhedron(A::MatElem{TropicalSemiringElem{M}}, B::MatElem{TropicalSemiringElem{M}}) where {M<:MinOrMax}
   p = Polymake.tropical.Polytope{M.instance}(INEQUALITIES=(A,B))
 
@@ -52,5 +86,9 @@ function tropical_polyhedron(convention::MinOrMax, A::QQMatrix, B::QQMatrix)
   p = Polymake.tropical.Polytope{convention}(INEQUALITIES=(A,B))
 
   return TropicalPolyhedron{typeof(convention)}(p)
+end
+
+struct ProjectiveTropicalPolyhedron{M<:MinOrMax} <: PolyhedralObject{QQFieldElem}
+  
 end
 
