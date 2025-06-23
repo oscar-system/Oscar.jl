4179287e73dae996a8d4c2fb08f3405a5a93ad18
Kamillo Ferry <kafe@kafe.dev>
2025-06-23 22:48:52 +0200

    Backport fix for pseudovertices from polymake

--- src/TropicalGeometry/TropicalPolyhedron/properties.jl
+++ src/TropicalGeometry/TropicalPolyhedron/properties.jl
@@ -4,6 +4,7 @@ function vertices(as::Type{PointVector{T}}, P::TropicalPolyhedron) where {T<:Tro
   return SubObjectIterator{as}(P, _vertex_of_polyhedron, n)
 end
 vertices(P::TropicalPolyhedron{M}) where {M<:MinOrMax} = vertices(PointVector{TropicalSemiringElem{M}}, P)
+n_vertices(P::TropicalPolyhedron) = size(pm_object(P).VERTICES, 1)
 
 function _vertex_of_polyhedron(::Type{PointVector{TropicalSemiringElem{M}}}, P::TropicalPolyhedron, i::Int) where {M<:MinOrMax}
   T = tropical_semiring(convention(P))
@@ -14,14 +15,29 @@ function _vertex_of_polyhedron(::Type{PointVector{TropicalSemiringElem{M}}}, P::
   )
 end
 
-n_vertices(P::TropicalPolyhedron) = size(pm_object(P).VERTICES, 1)
 
-function pseudovertices(as::Type{PointVector{T}}, P::Union{TropicalPolyhedron,TropicalPointConfiguration}) where {T<:TropicalSemiringElem}
+function pseudovertices(as::Type{PointVector{T}}, P::TropicalPolyhedron) where {T<:TropicalSemiringElem}
+  CT = pm_object(P).PSEUDOVERTEX_COARSE_COVECTORS
+  ind = findall(1:size(CT, 1)) do i
+    all(!=(0), CT[i,:])
+  end
+  n = length(ind)
+  TT = tropical_semiring(convention(P))
+
+  return SubObjectIterator{as}(
+    P,
+    (_,_,i)->point_vector(TT,TT.(pm_object(P).PSEUDOVERTICES[ind[i],2:end])),
+    n
+  )
+end
+
+function pseudovertices(as::Type{PointVector{T}}, P::TropicalPointConfiguration) where {T<:TropicalSemiringElem}
   n = n_pseudovertices(P)
 
   return SubObjectIterator{as}(P, _pseudovertices, n)
 end
 pseudovertices(P::Union{TropicalPolyhedron{M},TropicalPointConfiguration{M}}) where {M<:MinOrMax} = pseudovertices(PointVector{TropicalSemiringElem{M}}, P)
+n_pseudovertices(P::Union{TropicalPolyhedron,TropicalPointConfiguration}) = pm_object(P).PSEUDOVERTICES |> size |> first
 
 function _pseudovertices(::Type{PointVector{TropicalSemiringElem{M}}}, P::Union{TropicalPolyhedron,TropicalPointConfiguration}, i::Int) where {M<:MinOrMax}
   T = tropical_semiring(convention(P))
@@ -32,14 +48,13 @@ function _pseudovertices(::Type{PointVector{TropicalSemiringElem{M}}}, P::Union{
   )
 end
 
-n_pseudovertices(P::Union{TropicalPolyhedron,TropicalPointConfiguration}) = pm_object(P).PSEUDOVERTICES |> size |> first
-
 function points(as::Type{PointVector{T}}, P::TropicalPointConfiguration) where {T<:TropicalSemiringElem}
   n = n_points(P)
 
   return SubObjectIterator{as}(P, _points, n)
 end
 points(P::TropicalPointConfiguration{M}) where {M<:MinOrMax} = points(PointVector{TropicalSemiringElem{M}}, P)
+n_points(P::TropicalPointConfiguration) = pm_object(P).POINTS |> size |> first
 
 function _points(::Type{PointVector{TropicalSemiringElem{M}}}, P::TropicalPointConfiguration, i::Int) where {M<:MinOrMax}
   T = tropical_semiring(convention(P))
@@ -49,8 +64,6 @@ function _points(::Type{PointVector{TropicalSemiringElem{M}}}, P::TropicalPointC
     T.(P.pm_tpolytope.POINTS[i,:])
   )
 end
-
-n_points(P::TropicalPointConfiguration) = pm_object(P).POINTS |> size |> first
 
 dim(P::TropicalPolyhedron) = covector_decomposition(P) |> dim
 ambient_dim(P::Union{TropicalPolyhedron,TropicalPointConfiguration}) = pm_object(P).PROJECTIVE_AMBIENT_DIM
@@ -92,7 +105,16 @@ end
 Get the covector decomposition of the tropical polytope `P` as a `PolyhedralComplex`.
 """
 function covector_decomposition(P::TropicalPolyhedron)
-  return Polymake.tropical.polytope_subdivision_as_complex(P.pm_tpolytope) |> polyhedral_complex
+  pv = pm_object(P).PSEUDOVERTICES
+  cov = pm_object(P).POLYTOPE_MAXIMAL_COVECTOR_CELLS
+  ct = pm_object(P).PSEUDOVERTEX_COARSE_COVECTORS
+  ind = findall(1:size(ct, 1)) do i
+    all(!=(0), ct[i,:])
+  end
+
+  return Polymake.fan.PolyhedralComplex(VERTICES=pv[ind,:],MAXIMAL_POLYTOPES=cov[:,ind]) |> polyhedral_complex
+  ## The following is sufficient for Polymake v4.14 and above
+  #return Polymake.tropical.polytope_subdivision_as_complex(P.pm_tpolytope) |> polyhedral_complex
 end
 
 @doc raw"""
