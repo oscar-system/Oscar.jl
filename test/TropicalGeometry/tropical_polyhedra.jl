8df3015551ca77859b0c7343bd16a5b79cd5d89a
Kamillo Ferry <kafe@kafe.dev>
2025-06-24 00:22:09 +0200

    Add some tests and tentative tests

--- test/TropicalGeometry/tropical_polyhedra.jl
+++ test/TropicalGeometry/tropical_polyhedra.jl
@@ -1,24 +1,121 @@
 @testset "TropicalPolyhedron{min}" begin
   TT = tropical_semiring(min)
   pts1_a = TT[0 -1 0; 0 -4 -1]
-  P1_a = tropical_convex_hull(pts1_a)
-  @test P1_a isa TropicalPolyhedron{typeof(min)}
-  @test dim(P1_a) == 1
-
-  ## Adding redundant points gives same tropical polytope with same properties
   pts1_b = TT[0 -1 0; 0 -4 -1; 0 -3 0]
-  P1_b = tropical_convex_hull(pts1_b)
-  @test issetequal(P1_a |> vertices, P1_b |> vertices)
-  @test dim(P1_b) == 1
+  P1_a = tropical_convex_hull(pts1_a)
+
+  @test P1_a isa TropicalPolyhedron{typeof(min)}
+  @test ambient_dim(P1_a) == 2
+  @test dim(P1_a) == 1
+  @test issetequal(vertices(P1_a), eachrow(pts1_a))
+  @test n_vertices(P1_a) == 2
+  @test issetequal(pseudovertices(P1_a), eachrow(pts1_b))
+  # @test n_pseudovertices(P1_a) == 3
 
   cov1 = maximal_covectors(P1_a)
   @test issetequal(cov1, [IncidenceMatrix([[1], [2], [2]]), IncidenceMatrix([[1], [2], [1]])])
 
+  covdec1_a = covector_decomposition(P1_a)
+  @test issetequal(covdec1_a |> vertices,
+    Vector{QQFieldElem}[
+      [-1, 0],
+      [-4, -1],
+      [-3, 0]
+    ])
+  @test issetequal(covector_decomposition(P1_a; dehomogenize_by=2) |> vertices,
+    Vector{QQFieldElem}[
+      [1, 1],
+      [4, 3],
+      [3, 3]
+    ])
+  @test maximal_polyhedra(IncidenceMatrix, covdec1_a) == IncidenceMatrix([[1,3],[2,3]]) 
+
+  ## Adding redundant points gives same tropical polytope with same properties
+  P1_b = tropical_convex_hull(pts1_b)
+  @test issetequal(P1_a |> vertices, P1_b |> vertices)
+  @test dim(P1_b) == 1
+
   pts2 = TT[0 1 2; inf 0 3; inf inf 0]
   P2 = tropical_convex_hull(pts2)
+  @test ambient_dim(P2) == 2
   @test dim(P2) == 2
+  # @test issetequal(vertices(P2), eachrow(pts2))
+  # @test n_vertices(P2) == 3
+  # @test issetequal(pseudovertices(P2), [eachrow(pts2)...,TT.([0,-1,2])])
+  # @test n_pseudovertices(P2) == ???? (2 or 4)
   @test maximal_covectors(P2) |> length == 1
   @test maximal_covectors(P2) |> first == IncidenceMatrix([[1], [2], [3]])
+
+  covdec2 = covector_decomposition(P2)
+  @test issetequal(covdec2 |> vertices,
+    Vector{QQFieldElem}[
+      [-1, 2],
+      [ 1, 2]
+    ])
+  @test issetequal(covdec2 |> rays .|> Vector{QQFieldElem},
+    Vector{QQFieldElem}[
+      [-1, -1],
+      [ 0, -1]
+    ])
+  @test maximal_polyhedra(IncidenceMatrix, covdec2) == IncidenceMatrix([[1,2,3,4]])
+
+  pts3 = TT[0 1 4; inf 0 2; inf inf 0]
+  P3 = tropical_convex_hull(pts3)
+  @test ambient_dim(P3) == 2
+  @test dim(P3) == 2
+  # @test issetequal(vertices(P3), eachrow(pts3))
+  # @test n_vertices(P3) == 3
+  # @test issetequal(pseudovertices(P3), [eachrow(pts3)...,TT.([0,1,3])])
+  # @test n_pseudovertices(P3) == ???? (2 or 4)
+  @test issetequal(maximal_covectors(P3),
+    [
+      IncidenceMatrix([[1],[1],[2,3]]),
+      IncidenceMatrix([[1],[2],[3]])
+    ])
+
+  covdec3 = covector_decomposition(P3)
+  @test issetequal(covdec3 |> vertices,
+    Vector{QQFieldElem}[
+      [1, 4],
+      [1, 3]
+    ])
+  @test issetequal(covdec3 |> rays .|> Vector{QQFieldElem},
+    Vector{QQFieldElem}[
+      [-1, -1],
+      [ 0, -1]
+    ])
+  @test maximal_polyhedra(IncidenceMatrix, covdec3) == IncidenceMatrix([[1,2], [2,3,4]])
+
+  pts4 = QQ[0 1 0; 0 4 1; 0 3 3; 0 0 2]
+  P4 = tropical_convex_hull(min,pts4)
+  @test ambient_dim(P3) == 2
+  @test dim(P3) == 2
+  @test issetequal(vertices(P4), eachrow(TT.(pts4)))
+  @test n_vertices(P4) == 4
+  # @test issetequal(pseudovertices(P3), [eachrow(pts3)...,TT.([0,1,3])])
+  # @test n_pseudovertices(P3) == ???? (2 or 4)
+
+  @test issetequal(maximal_covectors(P4),
+    [
+      IncidenceMatrix([[2],[1,3,4],[2]]),
+      IncidenceMatrix([[3],[4],[1,2]]),
+      IncidenceMatrix([[3],[1,4],[2]]),
+      IncidenceMatrix([[2,3],[4],[1]])
+    ])
+
+  @test issetequal(pseudovertices(P4),
+    [
+     TT.([0,1,0]),
+     TT.([0,4,1]),
+     TT.([0,3,3]),
+     TT.([0,0,2]),
+     TT.([0,0,0]),
+     TT.([0,2,1]),
+     TT.([0,3,1]),
+     TT.([0,3,2]),
+     TT.([0,1,3]),
+     TT.([0,0,1])
+    ])
 end
 
 @testset "TropicalPointConfiguration{min}" begin
@@ -27,43 +124,44 @@ end
   C1_a = tropical_point_configuration(pts1_a)
   @test C1_a isa TropicalPointConfiguration{typeof(min)}
 
+  ## Converting between TropicalPolyhedron and TropicalPointConfiguration should pass around the same polymake object
   P1_a = tropical_convex_hull(C1_a)
-  @test pm_object(C1_a) == pm_object(P1_a)
+  @test Oscar.pm_object(C1_a) == Oscar.pm_object(P1_a)
 
   cov1 = maximal_covectors(C1_a)
   @test issetequal(cov1,
     [
      IncidenceMatrix([Int[], Int[], [1,2]]),
-     IncidenceMatrix([Int[], [1], [2]]),
-     IncidenceMatrix([Int[], [1,2], Int[]]),
-     IncidenceMatrix([[2], [1], Int[]]),
-     IncidenceMatrix([[1,2], Int[], Int[]]),
-     IncidenceMatrix([[2], Int[], [1]]),
+     IncidenceMatrix([Int[], [2], [1]]),
+     IncidenceMatrix([Int[], [2,1], Int[]]),
+     IncidenceMatrix([[1], [2], Int[]]),
+     IncidenceMatrix([[2,1], Int[], Int[]]),
+     IncidenceMatrix([[1], Int[], [2]]),
     ])
 
   pts1_b = TT[0 -1 0; 0 -4 -1; 0 -3 0]
-  C1_b = tropical_convex_hull(pts1_b)
+  C1_b = tropical_point_configuration(pts1_b)
   cov1_b = maximal_covectors(C1_b)
   @test issetequal(cov1_b,
     [
      IncidenceMatrix([Int[], Int[], [1,2,3]]),
-     IncidenceMatrix([Int[], [1, 3], [2]]),
+     IncidenceMatrix([Int[], [2, 3], [1]]),
      IncidenceMatrix([Int[], [1,2,3], Int[]]),
-     IncidenceMatrix([[2], [1,3], Int[]]),
-     IncidenceMatrix([[2,3], [1], Int[]]),
+     IncidenceMatrix([[1], [2,3], Int[]]),
+     IncidenceMatrix([[1,3], [2], Int[]]),
      IncidenceMatrix([[1,2,3], Int[], Int[]]),
-     IncidenceMatrix([[2,3], Int[], [1]])
+     IncidenceMatrix([[1,3], Int[], [2]])
     ])
 
   covdec1_a = covector_decomposition(C1_a)
   @test issetequal(covdec1_a |> vertices,
-    [
+    Vector{QQFieldElem}[
       [-1, 0],
       [-4, -1],
       [-3, 0]
     ])
-  @test issetequal(covdec1_a |> rays,
-    [
+  @test issetequal(covdec1_a |> rays .|> Vector{QQFieldElem},
+    Vector{QQFieldElem}[
       [1,1],
       [-1,0],
       [0,-1]
@@ -75,9 +173,20 @@ end
   @test issetequal(cov2,
     [
       IncidenceMatrix([[1],[2],[3]]),
-      IncidenceMatrix([Int[],[12],[3]]),
-      IncidenceMatrix([Int[],[2],[13]]),
-      IncidenceMatrix([[1],Int[],[23]]),
-      IncidenceMatrix([Int[],Int[],[123]])
+      IncidenceMatrix([Int[],[1,2],[3]]),
+      IncidenceMatrix([Int[],[2],[1,3]]),
+      IncidenceMatrix([[1],Int[],[2,3]]),
+      IncidenceMatrix([Int[],Int[],[1,2,3]])
+    ])
+
+  pts3 = TT[0 1 4; inf 0 2; inf inf 0]
+  C3 = tropical_point_configuration(pts3)
+  @test issetequal(maximal_covectors(C3),
+    [
+      IncidenceMatrix([Int[],[1],[2,3]]),
+      IncidenceMatrix([Int[],Int[],[1,2,3]]),
+      IncidenceMatrix([Int[],[1,2],[3]]),
+      IncidenceMatrix([[1],Int[],[2,3]]),
+      IncidenceMatrix([[1],[2],[3]])
     ])
 end
