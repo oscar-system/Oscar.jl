059c5a6676cee64f208aa016515b42dc98403025
HechtiDerLachs <zach@mathematik.uni-kl.de>
2025-06-30 12:36:53 +0200

    Revert "Fix sign error."
    This reverts commit 7e1c01f1e0996425f83d9a16f79d7223a66e2756.

--- experimental/Schemes/src/DerivedPushforward.jl
+++ experimental/Schemes/src/DerivedPushforward.jl
@@ -656,7 +656,7 @@ function _minimal_exponent_vector(ctx::ToricCtx, d::FinGenAbGroupElem)
   # but only part of the algorithm is executed and explicit lattice points are 
   # calculated for each polyhedron.
   return get!(ctx.exp_vec_cache, d) do
-    rationoms, ctx.D = cohomology_support(toric_variety(ctx), -Int[d[i] for i in 1:rank(parent(d))]; D=get_chamber_dict(ctx))
+    rationoms, ctx.D = cohomology_support(toric_variety(ctx), Int[d[i] for i in 1:rank(parent(d))]; D=get_chamber_dict(ctx))
     k = -minimum(hcat(rationoms...); init=-1)
     n = ngens(irrelevant_ideal(toric_variety(ctx)))
     return Int[k for i in 1:n]
