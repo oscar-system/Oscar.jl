ab44f3e84c4308b6d7ad69234b1c8f8084ad69e6
Wolfram Decker <decker@mathematik.uni-kl.de>
2025-06-30 21:49:11 +0200

    Free resolutions: correct length function

--- src/Modules/UngradedModules/FreeResolutions.jl
+++ src/Modules/UngradedModules/FreeResolutions.jl
@@ -779,6 +779,14 @@ julia> B = R[x^2; x*y; y^2; z^4];
 
 julia> M = SubquoModule(A, B);
 
+julia> fr = free_resolution(M, length = 2)
+Free resolution of M
+R^2 <---- R^6 <---- R^6
+0         1         2
+
+julia> length(fr)
+2
+
 julia> fr = free_resolution(M)
 Free resolution of M
 R^2 <---- R^6 <---- R^6 <---- R^2 <---- 0
@@ -791,7 +799,7 @@ julia> length(fr)
 """
 function length(F::FreeResolution)
   if !is_complete(F.C)
-    idx = findfirst(is_zero(domain(phi)) for phi in F.C.maps)
+    idx = findfirst(is_zero(F.C[i]) for i = 0:first(map_range(F.C)))-2
     isnothing(idx) && return first(map_range(F.C))
     return idx::Int
   end
