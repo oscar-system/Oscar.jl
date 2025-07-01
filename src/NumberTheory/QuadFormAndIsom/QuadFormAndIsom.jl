4347491ad7a09064c2160a36451cf42389ef2126
StevellM <muller@math.uni-sb.de>
2025-07-01 10:33:38 +0200

    fix files to be included

--- src/NumberTheory/QuadFormAndIsom/QuadFormAndIsom.jl
+++ src/NumberTheory/QuadFormAndIsom/QuadFormAndIsom.jl
@@ -3,7 +3,6 @@ function set_lwi_level(k::Int)
   set_verbosity_level(:ZZLatWithIsom, k)
 end
 
-include("exports.jl")
 include("types.jl")
 include("spaces_with_isometry.jl")
 include("lattices_with_isometry.jl")
@@ -11,4 +10,3 @@ include("hermitian_miranda_morrison.jl")
 include("enumeration.jl")
 include("embeddings.jl")
 include("printings.jl")
-include("serialization.jl")
