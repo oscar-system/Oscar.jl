ecb16efeac1d74a8cec8254e9f138ad33a93a84c
Mikelis Emils Mikelsons <mikelis.emils.mikelsons@gmail.com>
2025-07-11 10:29:05 +0200

    Fix bug

--- src/Groups/matrices/matrix_manipulation.jl
+++ src/Groups/matrices/matrix_manipulation.jl
@@ -64,26 +64,26 @@ the direct sum of `U` and `W`.
 """
 function complement(V::AbstractAlgebra.Generic.FreeModule{T}, W::AbstractAlgebra.Generic.Submodule{T}) where T <: FieldElem
    @assert is_submodule(V,W) "The second argument is not a subspace of the first one"
-   if rank(W)==0 return sub(V,basis(V)) end
+   if vector_space_dim(W)==0 return sub(V,basis(V)) end
 
    e = W.map
 
-   H = matrix( vcat([e(g) for g in gens(W)], [zero(V) for i in 1:(rank(V)-rank(W)) ]) )
-   A_left = identity_matrix(base_ring(V), rank(V))
-   A_right = identity_matrix(base_ring(V), rank(V))
-   for rn in 1:rank(W)     # rn = row number
+   H = matrix( vcat([e(g) for g in gens(W)], [zero(V) for i in 1:(vector_space_dim(V)-vector_space_dim(W)) ]) )
+   A_left = identity_matrix(base_ring(V), vector_space_dim(V))
+   A_right = identity_matrix(base_ring(V), vector_space_dim(V))
+   for rn in 1:vector_space_dim(W)     # rn = row number
       cn = rn    # column number
       while H[rn,cn]==0 cn+=1 end   # bring on the left the first non-zero entry
       swap_cols!(H,rn,cn)
       swap_rows!(A_right,rn,cn)
-      for j in rn+1:rank(W)
+      for j in rn+1:vector_space_dim(W)
          add_row!(H,H[j,rn]*H[rn,rn]^-1,rn,j)
          add_column!(A_left,A_left[j,rn]*A_left[rn,rn]^-1,j,rn)
       end
    end
-   for j in rank(W)+1:rank(V)  H[j,j]=1  end
+   for j in vector_space_dim(W)+1:vector_space_dim(V)  H[j,j]=1  end
    H = A_left*H*A_right
-   _gens = [V([H[i,j] for j in 1:rank(V)]) for i in rank(W)+1:rank(V) ]
+   _gens = [V([H[i,j] for j in 1:vector_space_dim(V)]) for i in vector_space_dim(W)+1:vector_space_dim(V) ]
 
    return sub(V,_gens)
 end
