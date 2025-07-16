a6469292b5a908e4103ee9f0cdac582f5b534457
Tobias Braun <tobias.braun2@rwth-aachen.de>
2025-07-16 17:03:05 +0200

    Minor changes

--- experimental/ActionPolyRing/src/Types.jl
+++ experimental/ActionPolyRing/src/Types.jl
@@ -5,7 +5,8 @@ export ActionPolyRing,
        difference_polynomial_ring,
        ndiffs,
        nelementary_symbols,
-       elementary_symbols
+       elementary_symbols,
+       diff_action
 
 #######################################
 #
@@ -46,10 +47,10 @@ mutable struct DifferencePolyRing{T} <: ActionPolyRing{T}
       jet, var = (i, zeroind), dpr(elemvars[i])
       jet_to_var[jet], var_to_jet[var] = var, jet
     end
-   
+
     dpr.jet_to_var = jet_to_var
     dpr.var_to_jet = var_to_jet
-
+    
     return dpr
   end
  
@@ -63,7 +64,7 @@ mutable struct DifferencePolyRing{T} <: ActionPolyRing{T}
     upoly_ring, elemvars = universal_polynomial_ring(R, elem_syms_index)
     
     dpr = new{T}(upoly_ring, elementary_symbols, ndiffs, internal_ordering)
-    
+
     jet_to_var = Dict{Tuple{Int, Vector{Int}}, DifferencePolyRingElem{T}}()
     var_to_jet = Dict{DifferencePolyRingElem{T}, Tuple{Int, Vector{Int}}}()
 
@@ -92,6 +93,11 @@ mutable struct DifferencePolyRingElem{T} <: ActionPolyRingElem{T}
     new{T}(upre, dpr)
   end
 
+  function DifferencePolyRingElem{T}(dpr::DifferencePolyRing{T}, mpre::MPolyRingElem{T}) where {T}
+    @req dpr.upoly_ring.mpoly_ring === parent(mpre) "The parent does not match"
+    new{T}(mpre, dpr)
+  end
+
 end
 
 elem_type(::Type{DifferencePolyRing{T}}) where {T} = DifferencePolyRingElem{T}
@@ -103,7 +109,6 @@ base_ring_type(::Type{<:ActionPolyRing{T}}) where {T} = parent_type(T)
 is_domain_type(::Type{<:ActionPolyRingElem{T}}) where {T} = is_domain_type(T)
 
 is_exact_type(::Type{<:ActionPolyRingElem{T}}) where {T} = is_exact_type(T)
-
 
 #######################################
 #
@@ -143,6 +148,7 @@ end
 ##### Elements #####
 
 ### Difference ###
+
 (dpr::DifferencePolyRing)() = DifferencePolyRingElem{elem_type(base_ring(dpr))}(dpr)
 
 (dpr::DifferencePolyRing)(upre::AbstractAlgebra.Generic.UniversalPolyRingElem) = DifferencePolyRingElem{elem_type(base_ring(dpr))}(dpr, upre)
@@ -222,10 +228,17 @@ function degree(apre::ActionPolyRingElem, i::Int, jet::Vector{Int})
 end
 
 @doc raw"""
+    vars(p::ActionPolyRingElem)
+
+Return the variables actually occuring in `p`.
+"""
+vars(apre::ActionPolyRingElem) = parent(apre).(vars(__poly(apre)))
+
+@doc raw"""
     gen(apr::ActionPolyRing, i::Int, jet::Vector{Int}) -> Int
 
 Return the the `i`-th elementary variable with multiindex `jet` in the action polynomial
-`apr`. If this jet variable is valid but still untracked, it is tracked afterwards.
+`apr`. If this jet variable was untracked, it is tracked afterwards.
 """
 function gen(apr::ActionPolyRing, i::Int, jet::Vector)
   @req __is_valid_jet(apr, i, jet) "invalid jet variable"
@@ -233,10 +246,8 @@ function gen(apr::ActionPolyRing, i::Int, jet::Vector)
   if haskey(jtv, (i,jet))
     return jtv[(i, jet)]
   end
-  upoly_new_var = gen(apr.upoly_ring, string(apr.elementary_symbols[i]) * "[" * join(jet) * "]")
-  jtv[(i, jet)] = apr(upoly_new_var)
-  apr.var_to_jet[apr(upoly_new_var)] = (i, jet)
-  return jtv[(i, jet)]
+  __add_new_jetvar!(apr, i, jet)
+  return jtv[(i,jet)]
 end
 
 @doc raw"""
@@ -257,14 +268,23 @@ Return the number of generators of the action polynomial ring `apr`.
 number_of_generators(apr::ActionPolyRing) = number_of_generators(apr.upoly_ring)
 
 @doc raw"""
-    diff(p::ActionPolyRingElem, i::Int) -> ActionPolyRingElem
+    diff_action(p::ActionPolyRingElem, i::Int) -> ActionPolyRingElem
 
-Apply the `i`-th action to the polynomial `p`, that is, increase the multiindex of each
+Apply the `i`-th diff-action to the polynomial `p`, that is, increase the multiindex of each
 variable occuring in `p` at position `i` by one and return the resulting polynomial.
 """
-function diff(apre::ActionPolyRingElem, i::Int)
-  @req i in 1:ndiffs(parent(apre)) "index out of range"
-  return
+function diff_action(apre::ActionPolyRingElem, i::Int)
+  apr = parent(apre)
+  @req i in 1:ndiffs(apr) "index out of range"
+  jet_idxs = map(var -> apr.var_to_jet[var], vars(apre))
+  for j in 1:length(jet_idxs)
+    jet_idx = jet_idxs[j]
+    jet_idx[2][i] += 1 #increase the i-th multiindex by 1
+    if !haskey(apr.jet_to_var, jet_idx) #if this leads to an untracked variable...
+      __add_new_jetvar!(apr, jet_idx[1], jet_idx[2]) #track it
+    end
+    
+  end
 end
 
 #######################################
@@ -277,4 +297,12 @@ __poly(apre::ActionPolyRingElem) = apre.p
 
 #Check if the jet_to_var dictionary of apr could contain the key (i,jet).
 __is_valid_jet(apr::ActionPolyRing, i::Int, jet::Vector{Int}) = i in 1:nelementary_symbols(apr) && length(jet) == ndiffs(apr) && all(j -> j>=0, jet)
+
+function __add_new_jetvar!(apr::ActionPolyRing, i::Int, jet::Vector{Int})
+  jtv = apr.jet_to_var
+  upoly_new_var = gen(apr.upoly_ring, string(apr.elementary_symbols[i]) * "[" * join(jet) * "]")
+  jtv[(i, jet)] = apr(upoly_new_var)
+  apr.var_to_jet[apr(upoly_new_var)] = (i, jet)
+  return jtv[(i, jet)]
+end
 
