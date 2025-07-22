5832e1fb099117f477c484b0559c0826f438085c
Antony Della Vecchia <vecchia@math.tu-berlin.de>
2025-07-22 15:57:09 +0200

    add mongodb block to yml

--- experimental/OscarDB/test/runtests.jl
+++ experimental/OscarDB/test/runtests.jl
@@ -1,4 +1,18 @@
-@testset "Experimental template tests" begin
-  S = Oscar.ExampleStruct(5)
-  @test 5 == Oscar.my_access_func(S)
+using Oscar.OscarDB.Mongoc
+
+@testset "OscarDB" begin
+  db = Oscar.OscarDB.get_db()
+  collection_tsc = db["TransitiveSimplicialComplexes"]
+  
+  @testset "Types" begin
+    @test Oscar.OscarDB.get_db() isa Oscar.OscarDB.Database
+    @test db["TransitiveSimplicialComplexes"] isa Oscar.OscarDB.Collection
+    @test collection_tsc isa Oscar.OscarDB.Collection
+  end
+
+  @testset "Querying" begin
+    tsc = Oscar.OscarDB.find_one(db["TransitiveSimplicialComplexes"],
+                                 Dict("data.betti_numbers" => ["0", "1", "0"]))
+    @test betti_numbers(tsc) == [0, 1, 0]
+  end
 end
