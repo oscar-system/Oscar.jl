949a601cb8c99db290f5f829b24a6ea294613c65
Martin Bies <MBies87@googlemail.com>
2025-07-26 11:14:42 +0200

    [FTheoryTools] Fix broken links

--- experimental/FTheoryTools/src/HypersurfaceModels/methods.jl
+++ experimental/FTheoryTools/src/HypersurfaceModels/methods.jl
@@ -7,7 +7,7 @@
 
 Assigns a Weierstrass model to the given hypersurface model.
 
-See [`weierstrass_model(h::HypersurfaceModel)`[@ref] for an example use.
+See [`weierstrass_model(h::HypersurfaceModel)`](@ref) for an example use.
 """
 function set_weierstrass_model(h::HypersurfaceModel, w::WeierstrassModel)
   set_attribute!(h, :weierstrass_model => w)
@@ -18,7 +18,7 @@ end
 
 Assigns a global Tate model to the given hypersurface model.
 
-See [`global_tate_model(h::HypersurfaceModel)`[@ref] for an example use.
+See [`global_tate_model(h::HypersurfaceModel)`](@ref) for an example use.
 """
 function set_global_tate_model(h::HypersurfaceModel, t::GlobalTateModel)
   set_attribute!(h, :global_tate_model => t)
