3c5a1daff795882a37c8b48789c4efdccaf89fc0
Martin Bies <MBies87@googlemail.com>
2025-07-29 18:48:46 +0200

    [FTheoryTools] Absorb standard constructors into model constructor files

--- experimental/FTheoryTools/src/WeierstrassModels/constructors.jl
+++ experimental/FTheoryTools/src/WeierstrassModels/constructors.jl
@@ -79,6 +79,54 @@ function weierstrass_model(base::NormalToricVariety,
 end
 
 
+@doc raw"""
+    weierstrass_model_over_projective_space(d::Int)
+
+Construct a Weierstrass model over the ``d``-dimensional projective space,
+represented as a toric variety. The Weierstrass sections ``f`` and ``g`` are
+automatically generated with pseudorandom coefficients.
+
+# Examples
+```jldoctest
+julia> weierstrass_model_over_projective_space(3)
+Weierstrass model over a concrete base
+```
+"""
+weierstrass_model_over_projective_space(d::Int) = weierstrass_model(projective_space(NormalToricVariety, d); completeness_check = false)
+
+
+@doc raw"""
+    weierstrass_model_over_hirzebruch_surface(r::Int)
+
+Construct a Weierstrass model over the Hirzebruch surface ``F_r``,
+represented as a toric variety. The Weierstrass sections ``f`` and ``g`` are
+automatically generated with pseudorandom coefficients.
+
+# Examples
+```jldoctest
+julia> weierstrass_model_over_hirzebruch_surface(1)
+Weierstrass model over a concrete base
+```
+"""
+weierstrass_model_over_hirzebruch_surface(r::Int) = weierstrass_model(hirzebruch_surface(NormalToricVariety, r); completeness_check = false)
+
+
+@doc raw"""
+    weierstrass_model_over_del_pezzo_surface(b::Int)
+
+Construct a Weierstrass model over the del Pezzo surface ``\text{dP}_b``,
+represented as a toric variety. The Weierstrass sections ``f`` and ``g`` are
+automatically generated with pseudorandom coefficients.
+
+# Examples
+```jldoctest
+julia> weierstrass_model_over_del_pezzo_surface(3)
+Weierstrass model over a concrete base
+```
+"""
+weierstrass_model_over_del_pezzo_surface(b::Int) = weierstrass_model(del_pezzo_surface(NormalToricVariety, b); completeness_check = false)
+
+
 #####################################################################
 # 2: Constructors with unspecified base
 #####################################################################
