cc5493d5c36989425a7c70839da14a0242c6ff5d
Martin Bies <MBies87@googlemail.com>
2025-08-15 14:42:01 +0200

    [FTheoryTools] Drop support for cox_ring of FamilyOfSpaces

--- experimental/FTheoryTools/src/G4Fluxes/properties.jl
+++ experimental/FTheoryTools/src/G4Fluxes/properties.jl
@@ -120,7 +120,7 @@ G4-flux candidate
   # Compute the cohomology class corresponding to the hypersurface equation
   cy = polynomial(cohomology_class(toric_divisor_class(ambient_space(m), degree(hypersurface_equation(m)))))
    
-  n = ngens(cox_ring(base_space(m)))
+  n = ngens(coordinate_ring(base_space(m)))
   c_ds = [polynomial(cohomology_class(d)) for d in torusinvariant_prime_divisors(ambient_space(m))[1:n]]
   zero_sec = zero_section_class(m)
 
@@ -237,11 +237,11 @@ G4-flux candidate
   cy = polynomial(cohomology_class(toric_divisor_class(ambient_space(m), degree(hypersurface_equation(m)))))
 
   # Identify the cohomology classes of all base divisors
-  n = ngens(cox_ring(base_space(m)))
+  n = ngens(coordinate_ring(base_space(m)))
   c_ds = [polynomial(cohomology_class(d)) for d in torusinvariant_prime_divisors(ambient_space(m))[1:n]]
 
   # Identify the cohomology classes of all exceptional divisors
-  gS = gens(cox_ring(ambient_space(m)))
+  gS = gens(coordinate_ring(ambient_space(m)))
   exceptional_divisor_positions = exceptional_divisor_indices(m)
   exceptional_divisors = torusinvariant_prime_divisors(ambient_space(m))[exceptional_divisor_positions]
   c_ei = [polynomial(cohomology_class(d)) for d in exceptional_divisors]
