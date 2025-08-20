ea946efc1247a09eb780c7986540d193c817c67d
Antony Della Vecchia <vecchia@math.tu-berlin.de>
2025-08-20 10:11:11 +0200

    fix printing

--- src/Combinatorics/PhylogeneticTrees.jl
+++ src/Combinatorics/PhylogeneticTrees.jl
@@ -127,6 +127,10 @@ function adjacency_tree(ptree::PhylogeneticTree)
   return dir_tree
 end
 
+function Base.show(io::IO, m::MIME"text/plain", ptree::PhylogeneticTree{T}) where T
+  print(io, "Phylogenetic tree with $T type coefficients")
+end
+  
 function Base.show(io::IO, ptree::PhylogeneticTree{T}) where T
   print(io, "Phylogenetic tree with $T type coefficients")
 end
