fac8a80f3963c5c15f7eff3165e5bf66d9cb19fe
HechtiDerLachs <zach@mathematik.uni-kl.de>
2025-08-26 14:46:49 +0200

    Add support for simple serialization of SubquoModuleElems.

--- src/Serialization/SparseModules.jl
+++ src/Serialization/SparseModules.jl
@@ -1,4 +1,5 @@
 #=
+# TODO: Serialization of sparse rows makes trouble with the code below. I don't know why.
 @register_serialization_type SRow 
 
 type_params(v::T) where {T<:SRow} = TypeParams(T, base_ring(v))
@@ -15,6 +16,9 @@ function load_object(s::DeserializerState, ::Type{<:SRow}, parent_ring::Ring)
 end
 =#
 
+########################################################################
+# Serialization of `FreeMod`s and their elements
+########################################################################
 
 @register_serialization_type FreeMod uses_id
 
@@ -53,5 +57,50 @@ function load_object(s::DeserializerState, ::Type{<:FreeModElem}, parent::FreeMo
   vals = load_object(s, Vector{RET}, P, :values)
   r = sparse_row(P, pos, vals)
   return FreeModElem(r, parent)
+end
+
+########################################################################
+# Serialization of `SubquoModule`s and their elements
+########################################################################
+
+@register_serialization_type SubquoModule uses_id
+
+type_params(F::T) where {T <: SubquoModule} = TypeParams(T, :base_ring=>base_ring(F))
+
+function save_object(s::SerializerState, M::SubquoModule)
+  save_data_dict(s) do
+    save_object(s, ambient_free_module(M), :F)
+    save_object(s, ambient_representatives_generators(M), :sub)
+    save_object(s, relations(M), :quo)
+  end
+end
+
+function load_object(s::DeserializerState, ::Type{<:SubquoModule}, params::Dict)
+  R = params[:base_ring]::Ring
+  F = load_object(s, FreeMod{elem_type(R)}, :F)
+  I = SubModuleOfFreeModule(F, load_object(s, Vector{FreeModElem{elem_type(R)}}, F, :sub))
+  N = SubModuleOfFreeModule(F, load_object(s, Vector{FreeModElem{elem_type(R)}}, F, :quo))
+  return SubquoModule(I, N)
+end
+
+@register_serialization_type SubquoModuleElem 
+
+type_params(a::T) where {T<:SubquoModuleElem} = TypeParams(T, parent(a))
+
+function save_object(s::SerializerState, v::SubquoModuleElem)
+  r = coordinates(v)
+  save_data_dict(s) do
+    save_object(s, r.pos, :positions)
+    save_object(s, r.values, :values)
+  end
+end
+
+function load_object(s::DeserializerState, ::Type{<:SubquoModuleElem}, parent::SubquoModule)
+  P = base_ring(parent)
+  RET = elem_type(P)
+  pos = load_object(s, Vector{Int}, :positions)
+  vals = load_object(s, Vector{RET}, P, :values)
+  r = sparse_row(P, pos, vals)
+  return SubquoModuleElem(r, parent)
 end
 
