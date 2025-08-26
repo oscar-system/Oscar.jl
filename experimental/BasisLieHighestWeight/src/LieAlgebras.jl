29104f4aba0b8d96165e402f06d4736720a391cf
Janika Peters <janika.peters@rwth-aachen.de>
2025-08-26 14:11:48 +0200

    Adapt `matrices_of_operators` to handle all roots

--- experimental/BasisLieHighestWeight/src/LieAlgebras.jl
+++ experimental/BasisLieHighestWeight/src/LieAlgebras.jl
@@ -6,16 +6,20 @@ function matrices_of_operators(
   struct_consts = lie_algebra_simple_module_struct_consts_gap(L, highest_weight)
   dimV = size(struct_consts, 2)
   transformation_matrices = [
-    sparse_matrix(coefficient_ring(L), dimV, dimV) for _ in 1:number_of_positive_roots(R)
+    sparse_matrix(coefficient_ring(L), dimV, dimV) for _ in 1:number_of_roots(R)
   ]
-  for i in 1:number_of_positive_roots(R), j in 1:dimV
-    transformation_matrices[i][j] = struct_consts[i + number_of_positive_roots(R), j] # take f_alpha for positive root alpha
+  for i in 1:number_of_roots(R), j in 1:dimV
+    transformation_matrices[i][j] = struct_consts[i, j]
   end
 
   matrices_of_operators = map(operators) do op
-    fl, i = is_positive_root_with_index(op)
+    fl, i = is_root_with_index(op)
     @assert fl
-    change_base_ring(ZZ, transformation_matrices[i])
+    if i <= number_of_positive_roots(R)
+      return change_base_ring(ZZ, transformation_matrices[i + number_of_positive_roots(R)])  # take f_alpha for positive root alpha
+    else 
+      return change_base_ring(ZZ, transformation_matrices[i - number_of_positive_roots(R)])  # take e_alpha for negative root alpha
+    end
   end
 
   return matrices_of_operators
