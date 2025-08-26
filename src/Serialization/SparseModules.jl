bc873f1a5cd70a15a0b5fb2c316479ac8aa8bd67
HechtiDerLachs <zach@mathematik.uni-kl.de>
2025-08-26 15:20:15 +0200

    Some improvements.

--- src/Serialization/SparseModules.jl
+++ src/Serialization/SparseModules.jl
@@ -22,7 +22,7 @@ end
 
 @register_serialization_type FreeMod uses_id
 
-type_params(F::T) where {T <: FreeMod} = TypeParams(T, :base_ring=>base_ring(F))
+type_params(F::T) where {T <: FreeMod} = TypeParams(T, base_ring(F))
 
 function save_object(s::SerializerState, F::FreeMod)
   save_data_dict(s) do
@@ -31,7 +31,7 @@ function save_object(s::SerializerState, F::FreeMod)
   end
 end
 
-function load_object(s::DeserializerState, ::Type{<:FreeMod}, params::Dict)
+function load_object(s::DeserializerState, ::Type{<:FreeMod}, params::Ring)
   R = params[:base_ring]::Ring
   symbs = load_object(s, Vector{Symbol}, :symbols)
   #rk = load_object(s, Int, :rank)
@@ -65,19 +65,18 @@ end
 
 @register_serialization_type SubquoModule uses_id
 
-type_params(F::T) where {T <: SubquoModule} = TypeParams(T, :base_ring=>base_ring(F))
+type_params(M::T) where {T <: SubquoModule} = TypeParams(T, ambient_free_module(M))
 
 function save_object(s::SerializerState, M::SubquoModule)
   save_data_dict(s) do
-    save_object(s, ambient_free_module(M), :F)
     save_object(s, ambient_representatives_generators(M), :sub)
     save_object(s, relations(M), :quo)
   end
 end
 
-function load_object(s::DeserializerState, ::Type{<:SubquoModule}, params::Dict)
-  R = params[:base_ring]::Ring
-  F = load_object(s, FreeMod{elem_type(R)}, :F)
+function load_object(s::DeserializerState, ::Type{<:SubquoModule}, params::FreeMod)
+  F = params
+  R = base_ring(F)
   I = SubModuleOfFreeModule(F, load_object(s, Vector{FreeModElem{elem_type(R)}}, F, :sub))
   N = SubModuleOfFreeModule(F, load_object(s, Vector{FreeModElem{elem_type(R)}}, F, :quo))
   return SubquoModule(I, N)
