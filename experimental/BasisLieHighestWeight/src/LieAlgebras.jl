1379e4660716a093f5932b5bb25ca975d45d143b
Janika Peters <janika.peters@rwth-aachen.de>
2025-08-27 10:23:35 +0200

    Apply suggestion

--- experimental/BasisLieHighestWeight/src/LieAlgebras.jl
+++ experimental/BasisLieHighestWeight/src/LieAlgebras.jl
@@ -13,12 +13,14 @@ function matrices_of_operators(
   end
 
   matrices_of_operators = map(operators) do op
-    fl, i = is_root_with_index(op)
-    @assert fl
-    if i <= number_of_positive_roots(R)
-      return change_base_ring(ZZ, transformation_matrices[i + number_of_positive_roots(R)])  # take f_alpha for positive root alpha
-    else 
-      return change_base_ring(ZZ, transformation_matrices[i - number_of_positive_roots(R)])  # take e_alpha for negative root alpha
+    is_pos_root, pos_i = is_positive_root_with_index(op)
+    is_neg_root, neg_i = is_negative_root_with_index(op)
+    if is_pos_root
+      return change_base_ring(ZZ, transformation_matrices[pos_i + number_of_positive_roots(R)])  # take f_alpha for positive root alpha
+    else if is_neg_root
+      return change_base_ring(ZZ, transformation_matrices[neg_i - number_of_positive_roots(R)])  # take e_alpha for negative root alpha
+    else
+      error("unreachable")
     end
   end
 
