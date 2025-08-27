52551282d9bea809366fc8234a5739efe3cbcc2c
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-08-27 11:18:25 +0200

    Rewrite `matrices_of_operators`

--- experimental/BasisLieHighestWeight/src/LieAlgebras.jl
+++ experimental/BasisLieHighestWeight/src/LieAlgebras.jl
@@ -5,20 +5,23 @@ function matrices_of_operators(
   R = root_system(L)
   struct_consts = lie_algebra_simple_module_struct_consts_gap(L, highest_weight)
   dimV = size(struct_consts, 2)
-  transformation_matrices = [
-    sparse_matrix(coefficient_ring(L), dimV, dimV) for _ in 1:number_of_roots(R)
+  transformation_matrices_e = [
+    sparse_matrix(coefficient_ring(L), dimV, dimV) for _ in 1:number_of_positive_roots(R)
   ]
-  for i in 1:number_of_roots(R), j in 1:dimV
-    transformation_matrices[i][j] = struct_consts[i, j]
+  transformation_matrices_f = [
+    sparse_matrix(coefficient_ring(L), dimV, dimV) for _ in 1:number_of_positive_roots(R)
+  ]
+  for i in 1:number_of_positive_roots(R), j in 1:dimV
+    transformation_matrices_e[i][j] = struct_consts[i, j]
+    transformation_matrices_f[i][j] = struct_consts[i + number_of_positive_roots(R), j]
   end
 
   matrices_of_operators = map(operators) do op
-    is_pos_root, pos_i = is_positive_root_with_index(op)
-    is_neg_root, neg_i = is_negative_root_with_index(op)
-    if is_pos_root
-      return change_base_ring(ZZ, transformation_matrices[pos_i + number_of_positive_roots(R)])  # take f_alpha for positive root alpha
-    elseif is_neg_root
-      return change_base_ring(ZZ, transformation_matrices[neg_i])  # take e_alpha for negative root alpha
+    # operators act downwards, so we need to swap e and f
+    if ((fl, i) = is_positive_root_with_index(op); fl)
+      return change_base_ring(ZZ, transformation_matrices_f[i])
+    elseif ((fl, i) = is_negative_root_with_index(op); fl)
+      return change_base_ring(ZZ, transformation_matrices_e[i])
     else
       error("unreachable")
     end
