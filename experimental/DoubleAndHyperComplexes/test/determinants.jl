abadef6fc563097f246e94bc8045d25f35f57a5f
HechtiDerLachs <zach@mathematik.uni-kl.de>
2025-08-29 13:02:52 +0200

    Fix tests.

--- experimental/DoubleAndHyperComplexes/test/determinants.jl
+++ experimental/DoubleAndHyperComplexes/test/determinants.jl
@@ -11,7 +11,7 @@
   @test p == det(c; lower_bound=-5);
   @test p == det(c; direction=:from_right_to_left)
   
-  I = ideal(R, x)
+  I = ideal(R, [x])
   k, _ = quo(R, I)
   M = quotient_ring_as_module(k)
   res, _ = free_resolution(Oscar.SimpleFreeResolution, M)
@@ -21,10 +21,10 @@
   @test p == 1//det(c; lower_bound=-5);
   @test p == 1//det(c; direction=:from_right_to_left)
   cc = Oscar.ReflectedComplex(c)
-  @test p == det(cc; direction=:from_right_to_left, upper_bound=5)
-  @test p == det(cc; direction=:from_left_to_right, lower_bound=-5)
+  @test p == 1//det(cc; direction=:from_right_to_left, upper_bound=5)
+  @test p == 1//det(cc; direction=:from_left_to_right, lower_bound=-5)
   ccc = hom(cc, free_module(R, 1))
-  @test p == 1//det(ccc; lower_bound=-5);
-  @test p == 1//det(ccc; direction=:from_right_to_left)
+  @test p == det(ccc; lower_bound=-5);
+  @test p == det(ccc; direction=:from_right_to_left)
 end
 
