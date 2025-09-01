a32bc33ea0b1d10a15dcfc651f5c889839320912
antonydellavecchia <antonydellavecchia@gmail.com>
2025-09-01 17:44:07 +0200

    use new files for lps and phylo trees

--- test/Serialization/upgrades/setup_tests.jl
+++ test/Serialization/upgrades/setup_tests.jl
@@ -6,7 +6,7 @@ if !isdefined(Main, :serialization_upgrade_test_path) ||
   !isdir(Main.serialization_upgrade_test_path) ||
   !isfile(joinpath(Main.serialization_upgrade_test_path, "LICENSE.md"))
 
-  serialization_upgrade_test_path = let commit_hash = "90c84f6b03d22d60cee6db5a55879456d7def9c2"
+  serialization_upgrade_test_path = let commit_hash = "051f76410adb7cefdb30fa8238c5c28434cf7720"
     tarball = Downloads.download("https://github.com/oscar-system/serialization-upgrade-tests/archive/$(commit_hash).tar.gz")
 
     destpath = open(CodecZlib.GzipDecompressorStream, tarball) do io
