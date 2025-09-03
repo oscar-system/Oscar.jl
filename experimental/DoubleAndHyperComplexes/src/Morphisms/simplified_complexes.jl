d13a5413796cfb3c75d40ee0df0f1846d0437e5c
HechtiDerLachs <zach@mathematik.uni-kl.de>
2025-09-03 14:08:10 +0200

    Adapt docstring.

--- experimental/DoubleAndHyperComplexes/src/Morphisms/simplified_complexes.jl
+++ experimental/DoubleAndHyperComplexes/src/Morphisms/simplified_complexes.jl
@@ -366,9 +366,6 @@ If `F` is a free resolution of either a positively graded module `M`, or a modul
 
 If `M` is not (positively) graded or its `base_ring` is not local, use `simplify` to obtain an ''improved'' resolution.
 
-!!! note
-    If `F` is not complete, the minimal free resolution is computed only up to the second last known non-zero module in the resolution `F`. 
-
 # Examples
 ```jldoctest
 julia> R, (w, x, y, z) = graded_polynomial_ring(QQ, [:w, :x, :y, :z]);
