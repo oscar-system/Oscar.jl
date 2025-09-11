721526d0c03679eb32dd3c5a706d3ff010b60163
varuntrehan7 <varuntrehan7@gmail.com>
2025-09-12 05:18:05 +0530

    add functions for fixed_points

--- src/Groups/perm.jl
+++ src/Groups/perm.jl
@@ -162,15 +162,20 @@ julia> length(moved_points(gen(s, 1)))
 """
 @gapattribute moved_points(x::Union{PermGroupElem,PermGroup}) = Vector{Int}(GAP.Globals.MovedPoints(GapObj(x)))
 
-@doc raw"""
+@doc """
+    fixed_points(x::PermGroupElem) -> Vector{Int}
     fixed_points(G::PermGroup) -> Vector{Int}
 
-Return the list of fixed points of the permutation group `G`.  
-A fixed point is an integer in `1:degree(G)` that is not moved by any element of `G`.
+Return the vector of points in `1:degree(x)` or `1:degree(G)` that are
+fixed by all elements (for groups) or by `x` (for elements).
 
 # Examples
+
 ```jldoctest
-julia> g = symmetric_group(4);  s = sylow_subgroup(g, 3)[1];
+julia> g = symmetric_group(4)
+S₄
+
+julia> s = sylow_subgroup(g, 3)[1];
 
 julia> fixed_points(s)
 Int64[]
@@ -181,10 +186,29 @@ julia> fixed_points(one(s))
  2
  3
  4
+
+julia> σ = PermGroupElem([1,2,3,4] => [1,2,4,3], g)
+PermGroupElem((3 4))
+
+julia> fixed_points(σ)
+2-element Vector{Int64}:
+ 1
+ 2
 ```
 """
-@gapattribute fixed_points(G::PermGroup) = setdiff!(collect(1:degree(G)), moved_points(G))
+# Fixed points of a single permutation element
+function fixed_points(p::PermGroupElem)
+  # Return all points in 1:degree that are not moved by p
+  points = 1:degree(parent(p))
+  return setdiff(points, moved_points(p))
+end
 
+# Fixed points of a permutation group
+function fixed_points(G::PermGroup)
+  # Return all points in 1:degree that are not moved by any element of G
+  points = 1:degree(G)
+  return setdiff(points, moved_points(G))
+end
 
 @doc raw"""
     number_of_moved_points(x::PermGroupElem) -> Int
