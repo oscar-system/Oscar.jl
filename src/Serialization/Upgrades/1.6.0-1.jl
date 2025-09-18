4b1988cb175531f915fc3e2922c48ce02b7f4b4c
Antony Della Vecchia <vecchia@math.tu-berlin.de>
2025-09-18 17:46:51 +0200

    skeleton for stream lined upgrades

--- src/Serialization/Upgrades/1.6.0-1.jl
+++ src/Serialization/Upgrades/1.6.0-1.jl
@@ -1,6 +1,8 @@
 push!(upgrade_scripts_set, UpgradeScript(
   v"1.6.0-1",
-  function upgraded_1_6_0_1(s::UpgradeState, dict::Dict)
+  function upgrade_1_6_0_1(s::UpgradeState, dict::Dict)
+    upgrade_containers(upgrade_1_6_0_1, s, dict)
+
     
   end
 ))
