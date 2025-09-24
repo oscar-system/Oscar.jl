fe5cc9e01c1c9fc7e3e6b951f0a609aa88b31962
anne <anne@krueger-berg.de>
2025-09-24 22:38:10 +0200

    polishing of docs and tests

--- src/AlgebraicGeometry/RationalPoint/FiniteRationalPointSet.jl
+++ src/AlgebraicGeometry/RationalPoint/FiniteRationalPointSet.jl
@@ -9,6 +9,14 @@ function Base.iterate(PS::Oscar.FiniteRationalPointSet,i::Int=1)
 end
 
 # constructors for FiniteRationalPointSet
+@doc raw"""
+   finite_point_set(X::AffineAlgebraicSet,v::Vector)
+   finite_point_set(X::AffineVariety,v::Vector)
+   finite_point_set(X::ProjectiveAlgebraicSet,v::Vector)
+   finite_point_set(X::ProjectiveVariety,v::Vector)
+
+Create a FiniteRationalPointSet of the rational points of X specified by v.
+"""
 function finite_point_set(X::Union{AffineAlgebraicSet{T},
                                   AffineVariety{T}},
                          v::Vector{Vector{<:FieldElem}}) where T <: Field
