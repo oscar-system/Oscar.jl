358012b4640edace66313364b975df890b857a26
Simon Brandhorst <brandhorst@math.uni-sb.de>
2025-09-26 08:57:47 +0200

    Apply suggestion from @simonbrandhorst

--- src/AlgebraicGeometry/Schemes/ProjectiveAlgebraicSet/Objects/Methods.jl
+++ src/AlgebraicGeometry/Schemes/ProjectiveAlgebraicSet/Objects/Methods.jl
@@ -39,8 +39,8 @@ end
     rational_points(::Type{S}, X::ProjectiveAlgebraicSet)
 
 If $X$ is defined over a field, say, $k$, return the $k$-rational
-points of $X$ as an instance of $S$. Here, S must be one of Vector
-and AbsRationalPointSet.
+points of $X$ as an instance of `S`. Here, `S` must be one of [`Vector`](@ref)
+and [`AbsRationalPointSet`](@ref).
 
 !!! note
 The zero-dimensional condition is checked by the function.
