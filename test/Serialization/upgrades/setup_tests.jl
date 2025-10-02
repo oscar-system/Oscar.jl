c0715d98976f8dbf6cf0c32b4a79ab784d1c6222
Antony Della Vecchia <vecchia@math.tu-berlin.de>
2025-10-02 13:56:08 +0200

    Revert "update hash to master branch (serialization-upgrade-tests)"
    This reverts commit dc0dba432c40ef826e5f1fa263c951665ce6ea81.

--- test/Serialization/upgrades/setup_tests.jl
+++ test/Serialization/upgrades/setup_tests.jl
@@ -6,7 +6,7 @@ if !isdefined(Main, :serialization_upgrade_test_path) ||
   !isdir(Main.serialization_upgrade_test_path) ||
   !isfile(joinpath(Main.serialization_upgrade_test_path, "LICENSE.md"))
 
-  serialization_upgrade_test_path = let commit_hash = "ce778d995ea63dd73307e7a9ecde1bf5b215ba22"
+  serialization_upgrade_test_path = let commit_hash = "0b7ff9e5e1135ddeaf83bd20313946183522e0ae"
     tarball = Downloads.download("https://github.com/oscar-system/serialization-upgrade-tests/archive/$(commit_hash).tar.gz")
 
     destpath = open(CodecZlib.GzipDecompressorStream, tarball) do io
