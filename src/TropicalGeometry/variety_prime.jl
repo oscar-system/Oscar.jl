f6bce47b7bb56a2dda7e5d11cee310aed9199603
Yue Ren <yue.ren.kl@gmail.com>
2025-10-02 08:40:57 +0100

    TropicalGeometry: make renaming function return hom instead of ideal

--- src/TropicalGeometry/variety_prime.jl
+++ src/TropicalGeometry/variety_prime.jl
@@ -19,23 +19,23 @@ function tropical_variety_prime(I::MPolyIdeal, nu::TropicalSemiringMap; weighted
     return dehomogenize_post_tropicalization(TropIh)
 end
 
-# rename the variables of I to x1, ..., xn for n<10 or x01, ..., x10 for n<100 etc.
+# Given a polynomial ring S, return a map into an isomorphic polynomial ring S->R
+# but with variables renamed to be Singular compatible.
+# The new names are x1, ..., xn if nvars(R)<10, x01, ..., x10 if nvar(R)<100, etc.
 # zero padding is necessary because Singular interpreter does not like variable names
 # that are substrings of other variable names.
-function make_variable_names_singular_compatible(I::MPolyIdeal)
-    R = base_ring(I)
-    n = nvars(R)
+function hom_rename_variables_singular_compatible(S::MPolyRing)
+    n = nvars(S)
     l = ndigits(n)
-
     newVarNames = [ "x" * lpad(string(i),l,'0') for i in 1:n ]
-    S,x = polynomial_ring(base_ring(R), newVarNames)
-    changeVarNames = hom(R,S,x)
-    return changeVarNames(I)
+    R,x = polynomial_ring(base_ring(S), newVarNames)
+    return hom(S,R,x)
 end
 
 # trivial valuation
 function tropical_variety_prime_singular(I::MPolyIdeal, nu::TropicalSemiringMap{QQField,Nothing,<:Union{typeof(min),typeof(max)}}; weighted_polyhedral_complex_only::Bool=false)
-    I = make_variable_names_singular_compatible(I)
+    phi = hom_rename_variables_singular_compatible(base_ring(I))
+    I = phi(I)
     R = base_ring(I)
     singularCommand = join(["ring r=0,("*join(string.(symbols(R)),",")*"),dp;",
                             "ideal I = "*join(string.(gens(I)), ",")*";",
@@ -55,7 +55,8 @@ end
 
 # p-adic valuation
 function tropical_variety_prime_singular(I::MPolyIdeal, nu::TropicalSemiringMap{QQField,ZZRingElem,<:Union{typeof(min),typeof(max)}}; weighted_polyhedral_complex_only::Bool=false)
-    I = make_variable_names_singular_compatible(I)
+    phi = hom_rename_variables_singular_compatible(base_ring(I))
+    I = phi(I)
     R = base_ring(I)
     singularCommand = join(["ring r=0,("*join(string.(symbols(R)),",")*"),dp;",
                             "ideal I = "*join(string.(gens(I)), ",")*";",
