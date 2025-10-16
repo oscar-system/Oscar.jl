ff038a2ae1e3a00c8010fdac3405aecd66e011e8
Antony Della Vecchia <vecchia@math.tu-berlin.de>
2025-10-16 17:37:04 +0200

    fix save for rational function field

--- src/Serialization/Fields.jl
+++ src/Serialization/Fields.jl
@@ -283,21 +283,23 @@ end
 @register_serialization_type AbstractAlgebra.Generic.RationalFunctionField "RationalFunctionField" uses_id
 
 function save_object(s::SerializerState,
-                     RF::AbstractAlgebra.Generic.RationalFunctionField)
+                     RF::AbstractAlgebra.Generic.RationalFunctionField{<: FieldElem, <: MPolyRingElem})
   save_data_dict(s) do
     syms = symbols(RF)
+    save_object(s, syms, :symbols)
+  end
+end
 
-    if isone(length(syms))
-      save_object(s, syms[1], :symbol)
-    else
-      save_object(s, syms, :symbols)
-    end
+function save_object(s::SerializerState,
+                     RF::AbstractAlgebra.Generic.RationalFunctionField{<:FieldElem, <: PolyRingElem})
+  save_data_dict(s) do
+    save_object(s, symbols(RF)[1], :symbol)
   end
 end
 
 function load_object(s::DeserializerState,
                      ::Type{<: AbstractAlgebra.Generic.RationalFunctionField}, R::Ring)
-  haskey(s, :symbol) && return   return rational_function_field(R, load_object(s, Symbol, :symbol), cached=false)[1]
+  haskey(s, :symbol) && return rational_function_field(R, load_object(s, Symbol, :symbol), cached=false)[1]
 
   return rational_function_field(R, load_object(s, Vector{Symbol}, :symbols), cached=false)[1]
 end
