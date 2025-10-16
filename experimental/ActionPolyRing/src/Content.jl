e0f411ae83ede4bab86573aa810b4903b01d8551
Tobias Braun <Tobias.braun2@rwth-aachen.de>
2025-10-16 16:00:50 +0200

    Update experimental/ActionPolyRing/src/Content.jl
    Co-authored-by: Max Horn <max@quendi.de>

--- experimental/ActionPolyRing/src/Content.jl
+++ experimental/ActionPolyRing/src/Content.jl
@@ -900,7 +900,7 @@ is_univariate(apre::ActionPolyRingElem) = is_univariate(data(apre))
 function is_univariate_with_data(apre::ActionPolyRingElem)
   flag, gen_idx = is_univariate_with_data(data(apre))
   is_zero(gen_idx) && return flag, gen_idx
-  return (flag, findfirst(==(gen_idx), __perm_for_sort(parent(apre))))
+  return flag, findfirst(==(gen_idx), __perm_for_sort(parent(apre)))
 end
 
 @doc raw"""
