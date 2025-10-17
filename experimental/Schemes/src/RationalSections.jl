23c8789583aa5ea5dfde3051b456b1b407d93673
HechtiDerLachs <zach@mathematik.uni-kl.de>
2025-10-17 19:03:06 +0200

    Add custom trivializing_covering and rational sections.

--- experimental/Schemes/src/RationalSections.jl
+++ experimental/Schemes/src/RationalSections.jl
@@ -17,6 +17,7 @@ function (s::RationalSection)(U::AbsAffineScheme)
 end
 
 function produce_rational_section(s::RationalSection, U::AbsAffineScheme)
+  @assert has_ancestor(x->any(y===x for y in patches(trivializing_covering(sheaf(s)))), U)
   if isempty(s.cache)
     num = den = one(OO(U))
     return num, den
@@ -49,13 +50,24 @@ function produce_rational_section(s::RationalSection, U::AbsAffineScheme)
         new_num = trans(num*e)[1]
         new_den = trans(den*e)[1]
         frac = (lifted_numerator(new_num)*lifted_denominator(new_den))//(lifted_denominator(new_num)*lifted_numerator(new_den))
+        res = F(U, UV)
+        comp = matrix(res)[1, 1]
+        frac = frac*(lifted_denominator(comp)//lifted_numerator(comp))
         return OO(U)(numerator(frac)), OO(U)(denominator(frac))
+      else
+        VV = __find_chart(V, triv_cov)
+        res = F(VV, V)
+        e = first(gens(F(V)))
+        new_num = preimage(res, num*e)[1]
+        new_den = preimage(res, den*e)[1]
+        s.cache[VV] = (new_num, new_den)
+        return produce_rational_section(s, U)
       end
     end
   end
 
   V = __find_chart(U, triv_cov)
-  num, den = F(V)
+  num, den = s(V)
   # cache has been filled by the previous call so that the next 
   # one is caught above
   return produce_rational_section(s, U)
