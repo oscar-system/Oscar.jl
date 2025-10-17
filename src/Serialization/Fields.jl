ace0ae331277c60caa27a2ac2a63267bef30034b
antonydellavecchia <antonydellavecchia@gmail.com>
2025-10-17 10:19:23 +0200

    fix for rational function field (#5459)
    * fix for rational function field
    
    * fix save for rational function field
    
    * upgrade + add test
    
    * add test + upgrade
    
    * fix upgrade
    
    * fix poly test

--- src/Serialization/Fields.jl
+++ src/Serialization/Fields.jl
@@ -283,24 +283,25 @@ end
 @register_serialization_type AbstractAlgebra.Generic.RationalFunctionField "RationalFunctionField" uses_id
 
 function save_object(s::SerializerState,
-                     RF::AbstractAlgebra.Generic.RationalFunctionField)
+                     RF::AbstractAlgebra.Generic.RationalFunctionField{<: FieldElem, <: MPolyRingElem})
   save_data_dict(s) do
     syms = symbols(RF)
     save_object(s, syms, :symbols)
   end
 end
 
+function save_object(s::SerializerState,
+                     RF::AbstractAlgebra.Generic.RationalFunctionField{<:FieldElem, <: PolyRingElem})
+  save_data_dict(s) do
+    save_object(s, symbols(RF)[1], :symbol)
+  end
+end
+
 function load_object(s::DeserializerState,
                      ::Type{<: AbstractAlgebra.Generic.RationalFunctionField}, R::Ring)
-  # ensure proper types of univariate case on load
-  symbols = load_node(s, :symbols) do symbols_data
-    if symbols_data isa Vector
-      return Symbol.(symbols_data)
-    else
-      return Symbol(symbols_data)
-    end
-  end
-  return rational_function_field(R, symbols, cached=false)[1]
+  haskey(s, :symbol) && return rational_function_field(R, load_object(s, Symbol, :symbol), cached=false)[1]
+
+  return rational_function_field(R, load_object(s, Vector{Symbol}, :symbols), cached=false)[1]
 end
 
 #elements
