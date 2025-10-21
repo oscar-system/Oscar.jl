d8ffb8ccddef87bcaaba431df8e84224c5fd8acc
Morgan Rodgers <morgan.joaquin@gmail.com>
2025-10-21 10:36:26 +0200

    remove `Experimental` version of `multicombinations`

--- experimental/LieAlgebras/src/Combinatorics.jl
+++ experimental/LieAlgebras/src/Combinatorics.jl
@@ -2,42 +2,6 @@
 # Most implementations here are quite slow and should be replaced by a
 # more efficient implementation.
 
-function multicombinations(n::Integer, k::Integer)
-  return sort!(
-    reverse.(
-      reduce(
-        vcat, data.(partitions(i, k, 1, n)) for i in 0:(k * n); init=Vector{Int}[]
-      )::Vector{Vector{Int}}
-    ),
-  )
-end
-
-@doc raw"""
-    Oscar.LieAlgebras.multicombinations(v::AbstractVector{T}, k::Integer) where {T}
-
-Return an iterator over all combinations of `k` elements of `v` with repetitions.
-In each iteration, the elements are returned in the order they appear in `v`.
-The order of the combinations is lexicographic.
-
-```jldoctest
-julia> collect(Oscar.LieAlgebras.multicombinations([1, 2, 3, 4], 2))
-10-element Vector{Vector{Int64}}:
- [1, 1]
- [1, 2]
- [1, 3]
- [1, 4]
- [2, 2]
- [2, 3]
- [2, 4]
- [3, 3]
- [3, 4]
- [4, 4]
-```
-"""
-function multicombinations(v::AbstractVector{T}, k::Integer) where {T}
-  reorder(v, inds) = [v[i] for i in inds]
-  return (reorder(v, inds) for inds in multicombinations(length(v), k))
-end
 
 function permutations(n::Integer)
   return map(Vector{Int}, AbstractAlgebra.SymmetricGroup(n))
