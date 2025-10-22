87f75b4fdafb8001bd73a9a0fb10349c9ad4d0bf
Alheydis Geiger <alheydisgeiger@web.de>
2025-10-22 19:38:52 +0200

    serialization for selfprojecting realization space added

--- experimental/MatroidRealizationSpaces/src/serialization.jl
+++ experimental/MatroidRealizationSpaces/src/serialization.jl
@@ -35,3 +35,37 @@ function load_object(s::DeserializerState, ::Type{<:MatroidRealizationSpace}, di
   
   return RS
 end
+
+
+@register_serialization_type MatroidRealizationSpace_SelfProj uses_id
+type_params(M::MatroidRealizationSpace_SelfProj) = TypeParams(MatroidRealizationSpace_SelfProj,
+                                                     :matrix_space=>parent(selfproj_realization_matrix(M)),
+                                                     :ground_ring=>M.ground_ring)
+
+function save_object(s::SerializerState, M::MatroidRealizationSpace_SelfProj) 
+  save_data_dict(s) do
+    save_object(s, defining_ideal(M), :defining_ideal)
+    save_object(s, inequations(M), :inequations)
+    if isnothing(selfproj_realization_matrix(M))
+      save_object(s, matrix(ambient_ring(M),0,0,[]), :selfproj_realization_matrix)
+    else
+      save_object(s, selfproj_realization_matrix(M), :selfproj_realization_matrix)
+    end
+#    save_object(s, M.one_realization, :one_realization)
+  end
+end
+
+
+function load_object(s::DeserializerState, ::Type{<:MatroidRealizationSpace_SelfProj}, dict::Dict)
+  MS = dict[:matrix_space];
+  R = base_ring(MS)
+  GR = dict[:ground_ring];
+  I = load_object(s, MPolyIdeal, R, :defining_ideal)
+  Ineqs = load_object(s, Vector{MPolyRingElem}, R, :inequations)
+  Mat = load_object(s, MatElem, MS, :selfproj_realization_matrix)
+  RMat = isempty(Mat) ? nothing : Mat
+  char = characteristic(coefficient_ring(R))
+  q = !iszero(char) ? order(coefficient_ring(R)) : nothing
+ # RS.one_realization = load_object(s, Bool, :one_realization)
+  return MatroidRealizationSpace_SelfProj(I, Ineqs, R, RMat, char, q, GR)
+end
