876a802110f220901fea01cb86c30ea9806ed683
varuntrehan7 <varuntrehan7@gmail.com>
2025-11-03 06:22:18 +0530

    Add explicit IntegerUnion types to pcgroup_code signature

--- src/Groups/pcgroup.jl
+++ src/Groups/pcgroup.jl
@@ -953,9 +953,10 @@ function code_pcgroup(G::PcGroup)
 end
 
 """
-   pcgroup_code(code, size)
+    pcgroup_code(code::IntegerUnion, size::IntegerUnion)
 
 Given an integer `code` and order `size` , return the polycyclic group it encodes.
+Both `code` and `size` can be of type `Int`, `BigInt`, or `ZZRingElem`. Internally, these are converted to `GapInt` before calling the GAP function `PcGroupCode`.
 The accepted codes and resulting groups match those of GAP's `PcGroupCode` and Magma's `SmallGroupDecoding`.
 
 # Examples
@@ -973,6 +974,6 @@ julia> code_pcgroup(G) == code_pcgroup(H)
 true
 ```
 """
-function pcgroup_code(code, size) 
-  PcGroup(GAP.Globals.PcGroupCode(Int(code), Int(size)))
+function pcgroup_code(code::IntegerUnion, size::IntegerUnion)
+  return PcGroup(GAP.Globals.PcGroupCode(Int(code), Int(size)))
 end
