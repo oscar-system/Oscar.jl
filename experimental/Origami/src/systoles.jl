59cacfef6a446ff5441d9c1afab6764c01794460
Sebastian Engelhardt <seen00001@stud.uni-saarland.de>
2025-11-05 18:06:00 +0100

    replace gap_to_julia call with Float64 conversion

--- experimental/Origami/src/systoles.jl
+++ experimental/Origami/src/systoles.jl
@@ -1,18 +1,18 @@
 function systolic_ratio(ori::Origami)
-    return GAP.gap_to_julia(GAP.Globals.SystolicRatio(ori.o))[:systolic_ratio]
+  return Float64(GAP.Globals.SystolicRatio(ori.o).systolic_ratio)
 end
 
 function systolic_ratio_bigger_one_over_pi_in_h11(deg::Int)
-    oris = Origami[]
-    for d in 1:deg
-        orisDeg = origamis([1,1], d)
-        append!(oris, orisDeg)
-    end
-    systolicRatios = map(systolic_ratio, oris)
-    threshold = 1 / pi
-    amountBigger = count(x -> x > threshold, systolicRatios)
-    total = length(oris)
-    percentage = (amountBigger / total) * 100
-    rounded_percentage = round(percentage, digits=2)
-    return (total, amountBigger, "$rounded_percentage%")
+  oris = Origami[]
+  for d in 1:deg
+    orisDeg = origamis([1, 1], d)
+    append!(oris, orisDeg)
+  end
+  systolicRatios = map(systolic_ratio, oris)
+  threshold = 1 / pi
+  amountBigger = count(x -> x > threshold, systolicRatios)
+  total = length(oris)
+  percentage = (amountBigger / total) * 100
+  rounded_percentage = round(percentage; digits=2)
+  return (total, amountBigger, "$rounded_percentage%")
 end
