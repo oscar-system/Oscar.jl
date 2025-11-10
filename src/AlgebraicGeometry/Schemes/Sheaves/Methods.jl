5c409a43e23a31e016ef155276e03f02343ec8b6
HechtiDerLachs <zach@mathematik.uni-kl.de>
2025-11-10 02:31:23 +0100

    Work in progress on simplified_covering etc.

--- src/AlgebraicGeometry/Schemes/Sheaves/Methods.jl
+++ src/AlgebraicGeometry/Schemes/Sheaves/Methods.jl
@@ -884,15 +884,15 @@ function _strict_transform(M::SubquoModule{T}, I::Ideal) where {T<:MPolyQuoLocRi
   poly_gens = elem_type(FA)[]
   for v in relations(M)
     den = lcm([lifted_denominator(c) for (_, c) in coordinates(v)]...)
-    vv = sum(den*lifted_numerator(c)*FA[i] for (i, c) in coordinates(v); init=zero(FA))
+    vv = sum(divexact(den, lifted_denominator(c))*lifted_numerator(c)*FA[i] for (i, c) in coordinates(v); init=zero(FA))
     push!(poly_gens, vv)
   end
-  #@assert all(is_zero(M(sum(c*F[i] for (i, c) in coordinates(v); init=zero(F)))) for v in poly_gens)
+  @assert all(is_zero(M(sum(c*F[i] for (i, c) in coordinates(v); init=zero(F)))) for v in poly_gens)
   U = SubModuleOfFreeModule(FA, poly_gens)
   poly_gens_id = elem_type(A)[numerator(f) for f in gens(I)]
   IA = ideal(A, poly_gens_id)
   poly_sat = _saturation(U, IA)
-  #@assert all(g in poly_sat for g in gens(U))
+  @assert all(g in poly_sat for g in gens(U))
   res, _ = quo(F, elem_type(F)[sum(L(c)*F[i] for (i, c) in coordinates(v); init=zero(F)) for v in gens(poly_sat)])
   #@assert all(is_zero(res(v)) for v in relations(M))
   return res
@@ -915,8 +915,32 @@ end
 
 function produce_object(
     FF::SimplifiedSheaf, 
+    U::PrincipalOpenSubset
+  )
+  M = original_sheaf(FF)
+  X = scheme(FF)
+  U in default_covering(M) && return _simplify_from_original(FF, U)
+  V = ambient_scheme(U)
+  FFV = FF(V)
+  rest = OO(X)(V, U)
+  FFV_res, rest_map = change_base_ring(rest, FFV)
+  res, simp_map = _simplify(FFV_res)
+  full_res = compose(rest_map, inv(simp_map; check=false))
+  add_incoming_restriction!(FF, V, res, full_res)
+  for (V, res) in incoming_restrictions(FF, V)
+    add_incoming_restriction!(FF, V, res, compose(res, full_res))
+  end
+  return res
+end
+
+function produce_object(
+    FF::SimplifiedSheaf, 
     U::AbsAffineScheme
   )
+  return _simplify_from_original(FF, U)
+end
+
+function _simplify_from_original(FF::SimplifiedSheaf, U::AbsAffineScheme)
   M = original_sheaf(FF)
   res, id = _simplify(M(U))
   @assert domain(id) === res
@@ -940,45 +964,155 @@ function restriction_map(F::SimplifiedSheaf, V::AbsAffineScheme, U::AbsAffineSch
   # fill the cache
   dom = F(V)
   cod = F(U)
-  id_dom = identifying_maps(F)[V]
-  id_cod = identifying_maps(F)[U]
-  res_M = M(V, U)
-  @assert dom === domain(id_dom)
-  @assert cod === domain(id_cod)
-  img_gens = images_of_generators(id_dom)
-  img_gens = res_M.(img_gens)
-  id_cod_inv = inv(id_cod)
-  img_gens = elem_type(cod)[id_cod_inv(x) for x in img_gens]
-  #img_gens = elem_type(cod)[preimage(id_cod, v) for v in img_gens]
-  res =  hom(dom, cod, img_gens, OO(scheme(F))(V, U))
-  check = F.check
-  #@check is_welldefined(res)
-  return res
+  if V in default_covering(M)
+    return _restrict_from_def_cov(F, V, U)
+  end
+  W = __find_chart(V, default_covering(M))
+  res_from_def_cov_dom = F(W, V)
+  res_from_def_cov_cod = F(W, U)
+  X = scheme(F)
+  res_rings = OO(X)(V, U)
+  img_gens = elem_type(cod)[]
+  im_dom, im_dom_inc = image(res_from_def_cov_dom)
+  for (i, g) in enumerate(gens(dom))
+    c = coordinates(preimage(im_dom_inc, g))
+    push!(img_gens, sum(res_rings(f)*images_of_generators(res_from_def_cov_cod)[i] for (i, f) in c; init=zero(cod)))
+  end
+  return hom(dom, cod, img_gens, res_rings)
+end
+
+function _restrict_from_def_cov(F::SimplifiedSheaf, V::AbsAffineScheme, U::AbsAffineScheme)
+  M = original_sheaf(F)
+  X = scheme(F)
+  W = __find_chart(U, default_covering(M))
+  V === W && return _restrict_directly_from_def_cov(F, V, U)
+  glue = default_covering(M)[V, W]
+  _, WV = gluing_domains(glue)
+  F(V) # fill the cache
+  F(W) # fill the cache
+  id_V = identifying_maps(F)[V]
+  id_W = identifying_maps(F)[W]
+  M_res = M(V, WV)
+  im_M_res, inc_im_M_res = image(M(W, WV))
+  M_W_to_U = M(W, U)
+  dom = F(V)
+  cod = F(U)
+  img_gens = images_of_generators(id_V) # in M(V)
+  img_gens = M_res.(img_gens) # in M(WV)
+  img_gens_coords = [coordinates(preimage(inc_im_M_res, g)) for g in img_gens] # in the images of the generators of M(W)
+
+  other_img_gens = images_of_generators(compose(inv(id_W; check=false), F(W, U)))
+  ring_res = OO(X)(WV, U)
+  img_gens = elem_type(cod)[sum(ring_res(f)*other_img_gens[i] for (i, f) in c; init=zero(cod)) for c in img_gens_coords]
+  return hom(dom, cod, img_gens, OO(X)(V, U))
+end
+
+function _restrict_directly_from_def_cov(F::SimplifiedSheaf, V::AbsAffineScheme, U::PrincipalOpenSubset)
+  FU = F(U)
+  inc = incoming_restrictions(F, FU)
+  W = ambient_scheme(U)
+  if haskey(inc, W)
+    res = inc[W]
+    return compose(_restrict_directly_from_def_cov(F, V, W), res)
+  end
+  @assert haskey(identifying_maps(F), U)
+  id_V = identifying_maps(F)[V]
+  id_U = identifying_maps(F)[U]
+  M = original_sheaf(F)
+  return compose(id_V, compose(M(V, U), inv(id_U; check=false)))
+end
+
+function _restrict_directly_from_def_cov(F::SimplifiedSheaf, V::AbsAffineScheme, U::AbsAffineScheme)
+  @assert haskey(identifying_maps(F), U)
+  id_V = identifying_maps(F)[V]
+  id_U = identifying_maps(F)[U]
+  M = original_sheaf(F)
+  return compose(id_V, compose(M(V, U), inv(id_U; check=false)))
 end
 
 sheaf_of_rings(M::SimplifiedSheaf) = sheaf_of_rings(original_sheaf(M))
 sheaf_of_rings(M::StrictTransformSheaf) = sheaf_of_rings(original_sheaf(M))
 default_covering(M::StrictTransformSheaf) = domain(covering_morphism(morphism(M)))
 
-@attr Covering function trivializing_covering(M::SimplifiedSheaf; covering::Covering=default_covering(scheme(M)))
+@attr Covering function trivializing_covering(M::SimplifiedSheaf; covering::Covering=simplified_covering(M))
   new_patches = Vector{AbsAffineScheme}(copy(patches(covering)))
   ind = findfirst(!(M(U) isa FreeMod || all(is_zero, relations(M(U)))) for U in new_patches)
   while !isnothing(ind)
     U = popat!(new_patches, ind)
     MU = M(U)
-    ideal(OO(U), one(OO(U)))
     I = ideal(OO(U), reduce(vcat, Vector{elem_type(OO(U))}[elem_type(OO(U))[c for (_, c) in coordinates(v)] for v in relations(MU)]; init=elem_type(OO(U))[]))
     one(OO(U)) in I || error("module is not locally free")
     c = coordinates(one(OO(U)), I)
     non_zero_entries = [(i, c[i]) for i in 1:ngens(I) if !is_zero(c[i])]
     for i in 1:ngens(I)
-      h = c[i]
-      is_zero(h) && continue
-      push!(new_patches, PrincipalOpenSubset(U, gen(I, i)))
+      is_zero(c[i]) && continue
+      h = gen(I, i)
+      fac = factor(lifted_numerator(h))
+      push!(new_patches, PrincipalOpenSubset(U, [a for (a, _) in fac]))
     end
     ind = findfirst(!(M(U) isa FreeMod || all(is_zero, relations(M(U)))) for U in new_patches)
   end
   new_cov = Covering(new_patches)
+  inherit_gluings!(new_cov, covering)
+  if has_decomposition_info(covering)
+    inherit_decomposition_info!(scheme(M), new_cov; covering)
+  end
+  return new_cov
+end
+
+@attr Covering function simplified_covering(M::SimplifiedSheaf; covering::Covering=default_covering(scheme(M)), cut_off::Int=0)
+  @vprint :NashResolutions 4 "computing simplifying covering for $M...\n"
+  done_patches = AbsAffineScheme[]
+  new_patches = AbsAffineScheme[]
+  for (ind, U) in enumerate(patches(covering))
+    MM = M(U)
+    if MM isa FreeMod || all(is_zero, relations(MM)) || ngens(MM) <= cut_off
+      push!(done_patches, U)
+    else
+      push!(new_patches, U)
+    end
+  end
+  
+  while !isempty(new_patches)
+    U = popfirst!(new_patches)
+    @vprint :NashResolutions 4 "  Looking at patch with $(ngens(M(U))) generators..."
+    MU = M(U)
+    @show isempty(U)
+    I = ideal(OO(U), reduce(vcat, Vector{elem_type(OO(U))}[elem_type(OO(U))[c for (_, c) in coordinates(v)] for v in relations(MU)]; init=elem_type(OO(U))[]))
+
+    if !(one(OO(U)) in I)
+      @vprint :NashResolutions 4 " first non-trivial Fitting ideal is not the unit ideal.\n"
+      # nothing more can be done about M on U
+      push!(done_patches, U)
+      continue
+    end
+
+    # split U in a covering on which M will have fewer generators
+    descendants = AbsAffineScheme[]
+    c = coordinates(one(OO(U)), I)
+    non_zero_entries = [(i, c[i]) for i in 1:ngens(I) if !is_zero(c[i])]
+    @vprint :NashResolutions 4 " splitting for $non_zero_entries\n"
+    for i in 1:ngens(I)
+      is_zero(c[i]) && continue
+      h = gen(I, i)
+      is_zero(h) && continue
+      fac = factor(lifted_numerator(h))
+      @show h
+      @show collect(fac)
+      push!(descendants, PrincipalOpenSubset(U, [x for (x, _) in fac]))
+    end
+
+    for V in descendants
+      MV = M(V)
+      if ngens(MV) <= cut_off || all(is_zero, relations(MV))
+        push!(done_patches, V)
+      else
+        push!(new_patches, V)
+      end
+    end
+  end
+
+  new_cov = Covering(done_patches)
   inherit_gluings!(new_cov, covering)
   if has_decomposition_info(covering)
     inherit_decomposition_info!(scheme(M), new_cov; covering)
@@ -995,17 +1129,23 @@ end
 
 function resolve_module(M::SubquoModule{T}; 
     r0::Union{Int, Nothing}=nothing,
-    scheme::AbsCoveredScheme=covered_scheme(spec(base_ring(M)))
+    scheme::AbsCoveredScheme=covered_scheme(spec(base_ring(M))),
+    focus::Ideal=ideal(base_ring(M), elem_type(base_ring(M))[])
   ) where {T<:RingElem}
   MM = SimplifiedSheaf(module_as_sheaf(M; scheme); check=false)
-  return resolve_module(MM; r0)
+  return resolve_module(MM; r0) # TODO: Pass on the focus
 end
 
-function resolve_module(M::AbsCoherentSheaf; r0::Union{Int, Nothing}=nothing)
-  return _resolve_module(SimplifiedSheaf(M; check=false), AbsCoveredSchemeMorphism[]; r0)
+function resolve_module(M::AbsCoherentSheaf; 
+    r0::Union{Int, Nothing}=nothing, 
+    focus::AbsIdealSheaf=zero_ideal_sheaf(scheme(M))
+  )
+  return _resolve_module(SimplifiedSheaf(M; check=false), AbsCoveredSchemeMorphism[]; r0, focus)
 end
 
-function _resolve_module(M::AbsCoherentSheaf, blowdowns::Vector{AbsCoveredSchemeMorphism}; r0::Union{Int, Nothing}=nothing)
+function _resolve_module(
+    M::SimplifiedSheaf, blowdowns::Vector{AbsCoveredSchemeMorphism}; 
+    r0::Union{Int, Nothing}=nothing, focus::AbsIdealSheaf=zero_ideal_sheaf(scheme(M)))
   # We first look for some non-trivial fitting ideal, either from above or from 
   # below, depending on whether or not `r0` is `nothing`. 
   # In case the Fitting ideals jump from 0 to 1 (or the other way around), 
@@ -1013,18 +1153,21 @@ function _resolve_module(M::AbsCoherentSheaf, blowdowns::Vector{AbsCoveredScheme
   # the first non-trivial fitting ideal from above. 
   fitts = AbsIdealSheaf[]
   X = scheme(M)
+  Z, inc_Z = sub(focus; covering=simplified_covering(X))
+  pb_M = SimplifiedSheaf(pullback(inc_Z, M))
   @vprint :NashResolutions 1 "call to resolve_module on $X\n"
-  simp_cov = simplified_covering(X)
+  simp_cov = simplified_covering(pb_M; covering=simplified_covering(Z))
+  #simp_cov = simplified_covering(X)
   if isnothing(r0)
     p = 0
     while true
-      fitt = FittingIdealSheaf(M, p)
+      fitt = FittingIdealSheaf(pb_M, p; covering=simp_cov)
       push!(fitts, fitt)
-      if is_zero(fitt)
+      if is_zero(fitt; covering=simp_cov)
         p += 1
         continue
       end
-      if is_one(fitt; covering=simplified_covering(X))
+      if is_one(fitt; covering=simp_cov)
         return M, blowdows
       end
       break
@@ -1032,26 +1175,36 @@ function _resolve_module(M::AbsCoherentSheaf, blowdowns::Vector{AbsCoveredScheme
   else
     p = r0
     while true
-      fitt = FittingIdealSheaf(M, p)
+      fitt = FittingIdealSheaf(pb_M, p; covering=simp_cov)
       @vprint :NashResolutions 2 "computing $p-th sheaf of Fitting ideals\n"
       push!(fitts, fitt)
-      if is_one(fitt; covering=simplified_covering(X))
+      if is_one(fitt; covering=simp_cov)
         @vprint :NashResolutions 2 "... which is the unit ideal\n"
         p -= 1
         continue
       end
-      if is_zero(fitt)
+      if is_zero(fitt; covering=simp_cov)
         @vprint :NashResolutions 2 "... which is the zero ideal! Finishing now.\n"
         return M, blowdowns
       end
       break
     end
   end
-  I = simplify(radical(fitts[end]))
-  @vprint :NashResolutions 2 "blowing up in $I...\n"
+  @vprint :NashResolutions 2 "numbers of generators in the patches:\n"
+  for U in simp_cov
+    @vprint :NashResolutions 2 "  $(ngens(pb_M(U)))\n"
+  end
+  @vprint :NashResolutions 2 "blowing up in the $p-th Fitting ideal.\n"
+  I = simplify(radical(pushforward(inc_Z, fitts[end])))
+  @vprint :NashResolutions 2 "blowing up in $I on patches\n"
+  for U in simplified_covering(X)
+    @vprint :NashResolutions 2 "  $(is_empty(U)) -> $(ngens(M(U))), $(length(relations(M(U)))), $(is_zero(I(U))), $(is_one(I(U)))\n"
+  end
+  @show is_one(I)
+  @show is_zero(I)
   bl = blow_up(I; covering=simplified_covering(X))
   st_M = SimplifiedSheaf(StrictTransformSheaf(bl, M; check=false); check=false)
-  return _resolve_module(st_M, push!(blowdowns, bl); r0)
+  return _resolve_module(st_M, push!(blowdowns, bl); r0, focus=ideal_sheaf(exceptional_divisor(bl)))
 end
 
 function produce_object(M::ExteriorPowerSheaf, U::AbsAffineScheme)
