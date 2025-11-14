6dfbca4203f64e13fa41fa9c5ba42f3c56cb33db
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-11-14 17:02:32 +0100

    Bump Documenter to 1.16 (#5548)
    (cherry picked from commit 04a67e731b2777816125a81956f838e4af1572a2)

--- docs/documenter_helpers.jl
+++ docs/documenter_helpers.jl
@@ -1,25 +1,12 @@
 # this function is slightly more specific than the one from documenter, print the corresponding code location
 # calls the original function via invoke and prints a timing for the doctest
-if isdefined(Documenter, :DocTests)
-  function Documenter.DocTests.eval_repl(block, sandbox::Module, meta::Dict, doc::Documenter.Documents.Document, page)
-    src_lines = Documenter.Utilities.find_block_in_file(block.code, meta[:CurrentFile])
-    # skip stats if there was a failure
-    if length(doc.internal.errors) > 0
-      invoke(Documenter.DocTests.eval_repl, Tuple{Any,Any,Dict,Documenter.Documents.Document,Any}, block, sandbox, meta, doc, page)
-    else
-      println("page: $(Documenter.Utilities.locrepr(meta[:CurrentFile], src_lines))")
-      @time invoke(Documenter.DocTests.eval_repl, Tuple{Any,Any,Dict,Documenter.Documents.Document,Any}, block, sandbox, meta, doc, page)
-    end
-  end
-else
-  function Documenter.eval_repl(block, sandbox::Module, meta::Dict, doc::Documenter.Document, page)
-    src_lines = Documenter.find_block_in_file(block.code, meta[:CurrentFile])
-    # skip stats if there was a failure
-    if length(doc.internal.errors) > 0
-      invoke(Documenter.eval_repl, Tuple{Any,Any,Dict,Documenter.Document,Any}, block, sandbox, meta, doc, page)
-    else
-      println("page: $(Documenter.locrepr(meta[:CurrentFile], src_lines))")
-      @time invoke(Documenter.eval_repl, Tuple{Any,Any,Dict,Documenter.Document,Any}, block, sandbox, meta, doc, page)
-    end
+function Documenter.eval_repl(block::Documenter.MarkdownAST.CodeBlock, sandbox::Module, meta::Dict, doc::Documenter.Document, page)
+  src_lines = Documenter.find_block_in_file(block.code, meta[:CurrentFile])
+  # skip stats if there was a failure
+  if length(doc.internal.errors) > 0
+    invoke(Documenter.eval_repl, Tuple{Documenter.MarkdownAST.CodeBlock,Any,Dict,Documenter.Document,Any}, block, sandbox, meta, doc, page)
+  else
+    println("page: $(Documenter.locrepr(meta[:CurrentFile], src_lines))")
+    @time invoke(Documenter.eval_repl, Tuple{Documenter.MarkdownAST.CodeBlock,Any,Dict,Documenter.Document,Any}, block, sandbox, meta, doc, page)
   end
 end
