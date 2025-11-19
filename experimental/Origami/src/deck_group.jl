6101a904adcce8fefa300ff093aafa2968e3078d
Sebastian Engelhardt <seen00001@stud.uni-saarland.de>
2025-11-19 15:08:17 +0100

    Code tweaks in deck_group

--- experimental/Origami/src/deck_group.jl
+++ experimental/Origami/src/deck_group.jl
@@ -1,43 +1,37 @@
 function in_deck_group(o::Origami, sigma::PermGroupElem)
   h = horizontal_perm(o)
   v = vertical_perm(o)
-  for i in 1:degree(o)
-    if (i^sigma)^h != (i^h)^sigma || (i^sigma)^v != (i^v)^sigma
-      return false
-    end
-  end
-  return true
+  return h == h^sigma && v == v^sigma
 end
 
 function deck_group(o::Origami)
   function candidate_for_deck(origami, j)
-    sheets_to_visit = 2:degree(origami)
-    sigma = [j]
-    append!(sigma, fill(-1, degree(origami) - 1))
+    h = horizontal_perm(origami)
+    v = vertical_perm(origami)
+    sym = perm_group(origami)
+    sheets_to_visit = collect(2:degree(origami))
+    sigma = fill(-1, degree(origami))
+    sigma[1] = j
     while length(sheets_to_visit) > 0
       found_predecessor = false
-      h = horizontal_perm(origami)
-      v = vertical_perm(origami)
       for i in sheets_to_visit
-        for tau in [h, v]
+        for tau in (h, v)
           pos_iterator = searchsorted(sheets_to_visit, i^(tau^-1))
           if first(pos_iterator) > last(pos_iterator)
             found_predecessor = true
-            sigma[i] = sigma[i ^ (tau ^ -1)]^tau
+            sigma[i] = sigma[i^(tau^-1)]^tau
             deleteat!(sheets_to_visit, findfirst(item -> item == i,
               sheets_to_visit))
-            break;
+            break
           end
         end
-        if found_predecessor
-          break;
-        end
+        found_predecessor && break
       end
     end
-    return perm(sigma)
+    return perm(sym, sigma)
   end
 
-  deck::Vector{PermGroupElem} = []
+  deck = PermGroupElem[]
   for i in 1:degree(o)
     candidate = candidate_for_deck(o, i)
     if GapObj(candidate) != GAP.Globals.fail && in_deck_group(o, candidate)
