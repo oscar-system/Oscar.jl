5f62b1ea69c3267bc3d6fa1914c00f565ed7d3e0
Marcel Wack <wack@math.tu-berlin.de>
2025-11-25 15:29:55 +0100

    improved callback helper

--- src/Rings/groebner/f4ncgb.jl
+++ src/Rings/groebner/f4ncgb.jl
@@ -105,13 +105,13 @@ end
 
 mutable struct f4ncgb_polys_helper
   gens::Vector{AbstractAlgebra.Generic.FreeAssociativeAlgebraElem{QQFieldElem}}
-  current_poly::AbstractAlgebra.Generic.FreeAssociativeAlgebraElem{QQFieldElem}
+  current_poly::Vector{Tuple{QQFieldElem,Vector{Int}}}
   parent::FreeAssociativeAlgebra{QQFieldElem}
   function f4ncgb_polys_helper(ring::FreeAssociativeAlgebra{QQFieldElem})
     r = new()
     r.gens = AbstractAlgebra.Generic.FreeAssociativeAlgebraElem{QQFieldElem}[]
     r.parent = ring
-    r.current_poly = zero(r.parent)
+    r.current_poly = [(zero(QQ),Vector{Int}())]
     return r
   end
 end 
@@ -128,22 +128,31 @@ function add_cb(pa::Ptr{Nothing},
 
   a = unsafe_pointer_to_objref(convert(Ptr{f4ncgb_polys_helper}, pa))
   P = parent(a)
+  n = nvars(P)
   vars = unsafe_wrap(Array, pvars, varcount)
   numerator = unsafe_load(pnumerator)
   denominator = unsafe_load(pdenominator)
-  monomial = P(QQ(numerator,denominator))
+  monomial = QQ(numerator,denominator)
+  new_vars = [n - Int(i)+1 for i in vars]
+  push!(a.current_poly,(monomial,new_vars))
+  #=
   for i in vars
     monomial *= P[nvars(P)-Int(i)+1]
   end
   a.current_poly += monomial
+  =#
   return nothing
 
 end
 
 function end_poly_cb(pa::Ptr{Nothing})
   a = unsafe_pointer_to_objref(convert(Ptr{f4ncgb_polys_helper}, pa))
-  push!(a.gens, a.current_poly)
-  a.current_poly = zero(parent(a))
+  C = MPolyBuildCtx(a.parent)
+  for (c,mon) in a.current_poly
+    push_term!(C,QQ(c),mon)
+  end
+  push!(a.gens, finish(C))
+  a.current_poly = [(zero(QQ),Vector{Int}())]
   return nothing
 end
 
