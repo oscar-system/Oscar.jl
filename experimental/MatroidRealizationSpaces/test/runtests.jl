f74d5f552ea4a683e126234f05b0e3d0ff06041c
Alheydis Geiger <alheydisgeiger@web.de>
2025-12-05 12:34:13 +0100

    added first tests for selfprojecting realization spaces

--- experimental/MatroidRealizationSpaces/test/runtests.jl
+++ experimental/MatroidRealizationSpaces/test/runtests.jl
@@ -149,3 +149,64 @@ end
     @test is_reduced(RS) == false
 end
 
+@testset "selfprojecting realization spaces" begin
+    M1 = fano_matroid() #not realizable over char 0
+    M2 = uniform_matroid(3,6)
+    M3 = matroid_from_bases([[1,2,7,8],[3,4,6,8],[2,4,7,8],[2,5,7,8],[3,4,6,7],[3,4,7,8],[1,5,6,7],[1,4,5,8],[1,4,5,7],[1,4,5,6],[4,5,6,8],[2,5,6,7],[4,5,7,8],[1,3,6,8],[3,4,5,7],[3,4,5,6],[1,5,6,8],[1,3,6,7],[4,5,6,7],[3,5,7,8],[2,5,6,8],[3,4,5,8],[1,3,4,7],[1,3,4,6],[1,3,4,8],[2,4,5,7],[2,4,5,6],[1,3,5,8],[1,3,5,7],[2,3,5,7],[2,3,4,8],[1,3,5,6],[2,3,5,6],[2,3,5,8],[2,4,5,8],[1,2,3,7],[1,2,3,6],[1,2,3,8],[2,3,4,7],[2,3,4,6],[1,3,7,8],[1,2,6,8],[1,2,6,7],[1,5,7,8],[2,3,7,8],[2,4,6,7],[1,2,5,8],[1,2,5,7],[1,2,5,6],[1,4,6,7],[1,2,4,7],[1,2,4,6],[3,5,6,7],[1,4,7,8],[2,3,6,8],[3,5,6,8],[2,4,6,8],[2,3,6,7],[1,2,4,8],[1,4,6,8]],8) #isomorphic to number 9 in the database (4,8), R not equal to S
+    M4 = vamos_matroid() # not selfprojecting
+    M5 = matroid_from_bases([[1,5,8],[4,6,7],[3,7,8],[5,7,8],[1,2,8],[3,4,8],[1,2,7],[1,3,8],[1,2,6],[4,7,8],[4,6,8],[1,3,7],[1,3,6],[3,4,7],[2,7,8],[3,4,6],[4,5,7],[4,5,6],[1,4,8],[1,4,7],[1,4,6],[2,5,7],[2,5,6],[4,5,8],[2,5,8],[3,5,7],[5,6,7],[3,5,6],[2,4,7],[2,4,6],[3,6,8],[3,5,8],[3,6,7],[2,6,8],[2,3,8],[5,6,8],[2,4,8],[2,6,7],[1,5,7],[1,5,6],[2,3,7],[2,3,6]],8) #isomorphic to number 52 in database (3,8); R = S
+    M6 = matroid_from_bases([[3,4,8,9],[2,3,5,7],[1,4,6,7],[2,3,5,6],[2,3,5,9],[1,4,6,9],[1,4,6,8],[1,5,8,9],[2,5,7,9],[2,5,7,8],[2,6,7,9],[2,6,7,8],[4,5,8,9],[4,6,8,9],[3,5,8,9],[1,5,6,7],[2,5,8,9],[2,4,6,9],[2,4,6,8],[5,7,8,9],[1,5,6,9],[1,5,6,8],[1,6,8,9],[5,6,8,9],[2,5,6,7],[1,3,6,9],[1,6,7,9],[1,3,5,9],[2,5,6,9],[1,3,6,8],[1,6,7,8],[1,3,5,8],[2,5,6,8],[1,2,7,9],[1,2,7,8],[1,2,6,9],[1,3,5,6],[1,2,6,8],[1,3,6,7],[6,7,8,9],[1,3,4,7],[1,2,6,7],[1,3,4,6],[1,3,4,5],[4,5,7,9],[1,3,4,9],[1,3,4,8],[2,3,7,9],[2,3,7,8],[2,4,8,9],[1,2,3,5],[1,2,3,7],[1,2,3,6],[1,2,3,9],[1,2,3,8],[2,4,7,9],[2,4,7,8],[4,6,7,9],[1,4,7,9],[1,5,7,9],[4,6,7,8],[1,4,7,8],[1,5,7,8],[2,6,8,9],[3,5,6,7],[3,6,7,9],[3,6,7,8],[3,5,6,9],[3,5,6,8],[4,7,8,9],[3,5,7,9],[3,5,7,8],[3,4,5,7],[3,4,5,6],[3,4,7,9],[2,3,6,9],[3,4,7,8],[2,3,6,8],[3,4,5,8],[2,3,6,7],[1,2,4,7],[1,2,4,6],[1,2,4,5],[2,4,5,7],[2,4,5,6],[1,2,4,9],[1,2,4,8],[2,3,4,9],[2,3,4,8],[1,3,8,9],[5,6,7,9],[2,4,5,9],[5,6,7,8],[2,4,5,8],[3,7,8,9],[2,3,4,5],[1,2,8,9],[2,3,4,7],[2,3,4,6],[3,4,6,9],[3,4,6,8],[1,7,8,9],[1,3,7,9],[1,3,7,8],[4,5,6,9],[4,5,6,8],[1,4,5,9],[1,4,5,8],[1,2,5,9],[1,2,5,8],[3,4,6,7],[1,2,5,7],[4,5,6,7],[1,4,5,7],[1,4,5,6],[2,3,8,9]],9)# isomorphic to numer 2375 in the database (4,9), R not empty, S empty
+
+    R1, mat_M1 = Oscar.selfprojecting_realization_matrix(M1, [1,2,4]); #this should be (nothing, nothing)
+    R2, mat_M2 = Oscar.selfprojecting_realization_matrix(M2, [1,2,3]);
+    R3, mat_M3 = Oscar.selfprojecting_realization_matrix(M3, [4,5,6,7]);
+    #R4, mat_M4 = Oscar.selfprojecting_realization_matrix(M4, [1,2,3,5]);#this should give an error ! How to test this?!
+    R5, mat_M5 = Oscar.selfprojecting_realization_matrix(M5, [1,5,6]);
+    R6, mat_M6 = Oscar.selfprojecting_realization_matrix(M6, [1,2,5,7]);#this should give (Ring, nothing)
+    
+    x2 = gens(R2)
+    x3 = gens(R3)
+    x5 = gens(R5)
+    x6 = gens(R6)
+
+    @testset "is_selfprojecting" begin
+        @test is_selfprojecting(M1) == true
+        @test is_selfprojecting(M2) == true
+        @test is_selfprojecting(M3) == true
+        @test is_selfprojecting(M4) == false
+        @test is_selfprojecting(M5) == true
+        @test is_selfprojecting(M6) == true
+    end
+    @testset "selfprojecting_realization_matrix" begin
+        
+        @test coefficient_ring(R2) == QQ
+        @test coefficient_ring(R3) == QQ
+        @test coefficient_ring(R5) == QQ
+        
+        @test length(x2) == 4
+        @test length(x3) == 5
+        @test length(x5) == 3
+        @test length(x6) == 3
+
+        @test mat_M1 ==  nothing
+        @test mat_M2 ==  matrix(R2, [1 0 0 1 1 1;0 1 0 1 x2[1] x2[3];0 0 1 1 x2[2] x2[4]])
+        @test mat_M3 ==  matrix(R3,  [1 1 1 1 0 0 0 0; 1 x3[1] x3[3] 0 1 0 0 0; 1 x3[2] x3[4] 0 0 1 0 1; 1 x3[2] x3[4] 0 0 0 1 x3[5]])
+        @test mat_M5 ==  matrix(R5, [1 1 1 1 0 0 1 1; 0 1 x5[1] x5[2] 1 0 0 0; 0 0 0 0 0 1 1 x5[3]])
+        @test mat_M6 == nothing
+    end
+    @testset "selfprojecting_realization_space_ideal" begin
+       # @test selfprojecting_realization_ideal(M1) == ideal(#which ring?!,[1])
+       #need a test for a completely not realizable matroid!
+        @test selfprojecting_realization_ideal(M2) == ideal(base_ring(R2),[x2[1]*x2[2]*x2[3] - x2[1]*x2[2]*x2[4] - x2[1]*x2[3]*x2[4] + x2[1]*x2[4] + x2[2]*x2[3]*x2[4] - x2[2]*x2[3]])
+        @test selfprojecting_realization_ideal(M3) ==ideal(base_ring(R3),[x3[1]*x3[2]*x3[3] - x3[1]*x3[2]*x3[4] - x3[1]*x3[3]*x3[4] + x3[1]*x3[4] + x3[2]*x3[3]*x3[4] - x3[2]*x3[3]])
+       # @test selfprojecting_realization_ideal(M4) #this should give an error because M4 is not selfprojecting
+        @test selfprojecting_realization_ideal(M5) == ideal(base_ring(R5),[0])
+        @test selfprojecting_realization_ideal(M6) == ideal(base_ring(R6),[1])
+    end
+    @testset "dimension" begin
+        @test dimension(selfprojecting_realization_space(M2)) == 3
+        @test dimension(selfprojecting_realization_space(M3)) == 5
+        @test dimension(selfprojecting_realization_space(M5)) == 3
+        @test dimension(selfprojecting_realization_space(M6)) == 3
+    end
+end
