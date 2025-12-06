1cad8a041efdbf73acf5e03fa071ef79e8ff71ce
HechtiDerLachs <zach@mathematik.uni-kl.de>
2025-12-06 17:41:19 +0100

    Update comments and docstrings.

--- src/Rings/puiseux_wrapper.jl
+++ src/Rings/puiseux_wrapper.jl
@@ -2,22 +2,19 @@
 # Wrapper functionality for `PuiseuxExpansions.lib` in Singular        #
 ########################################################################
 
-# A wrapper for the raw output of `puiseux`.
-# It returns a list of lists. It seems that the outer list contains 
-# only one element. The inner list contains 
+# A wrapper for Singular's `Singular.LibPuiseuxexpansions.puiseux`. 
+# It returns either a list of lists with entries of the following form: 
 #   * a polynomial `h`
 #   * an integer `e`
-#   * more stuff that I can not make sense of at the moment; probably some multiplicities
+#   * more stuff that is returned for internal purposes in the Singular library. 
+#     We discard this information
 # We interpret the result as `h` being the polynomial which evaluates 
 # to the Puiseux expansion of `f` on `y^(1//e)`. 
-function _puiseux(f::MPolyRingElem{T}, max_deg::Int, at_origin::Bool=true) where {T<:FieldElem}
-  P = parent(f)
-  SP = singular_poly_ring(P)
-  Sf = SP(f)
-  raw = Singular.LibPuiseuxexpansions.puiseux(Sf, max_deg, Int(at_origin))
-  @assert length(raw) == 1 "longer list then expected for output"
-  return [_process_result(P, data...) for data in raw]
-end
+#
+# Otherwise, it returns a ring and a dictionary with additional information 
+# on required extension fields for presentation of the result. Which one 
+# of the two cases applies is not foreseeable from the input alone. We 
+# therefore catch it with dispatch on `_process_result`. 
 
 function _process_result(P::MPolyRing, prec::Int, SE::Singular.PolyRing, res::Dict, rest...)
   # create the necesessary field extension on the Oscar side
@@ -66,7 +63,8 @@ end
 
 Compute the Puiseux expansion of `f` up to degree `precision` and returns a list of branches. 
 
-!!! note For the moment we only support bivariate polynomials over `QQ`. Puiseux expansion may produce number fields as coefficient rings for the output. As this is not foreseeable a priori, no guarantee can be given about the parent objects for the output. 
+!!! note 
+    For the moment we only support bivariate polynomials over `QQ`. Puiseux expansion may produce number fields as coefficient rings for the output. As this is not foreseeable a priori, no guarantee can be given about the parent objects for the output. 
 ```jldoctest
 julia> R, (x, y) = QQ[:x, :y]
 (Multivariate polynomial ring in 2 variables over QQ, QQMPolyRingElem[x, y])
