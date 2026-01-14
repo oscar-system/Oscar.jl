4e4736473ce79020e3d7853c6c3edd7d7d379cce
HechtiDerLachs <zach@mathematik.uni-kl.de>
2026-01-14 15:30:39 +0100

    Revert "Take out the induced maps on exterior powers again."
    This reverts commit 59c6e2d01526b2390f56440ee270c744102e3591.

--- src/Modules/ExteriorPowers/Generic.jl
+++ src/Modules/ExteriorPowers/Generic.jl
@@ -194,3 +194,33 @@ function hom(M::FreeMod, N::FreeMod, phi::FreeModuleHom)
   return induced_map_on_exterior_power(phi, p; domain=M, codomain=N)
 end
 
+function exterior_power(phi::ModuleFPHom{<:ModuleFP, <:ModuleFP, Nothing}, p::Int; 
+    domain::ModuleFP=exterior_power(domain(phi), p)[1],
+    codomain::ModuleFP=exterior_power(codomain(phi), p)[1]
+  )
+  if ngens(Oscar.domain(phi)) == ngens(Oscar.codomain(phi)) == p
+    return hom(domain, codomain, [det(matrix(phi))*codomain[1]]; check=false)
+  end
+
+  orig_img_gens = images_of_generators(phi)
+  dec = wedge_generator_decompose_function(domain)
+  dom_gens_dec = Vector{elem_type(Oscar.domain(phi))}[collect(dec(g)) for g in gens(domain)]
+  img_gens = elem_type(codomain)[wedge(phi.(v); parent=codomain) for v in dom_gens_dec]
+  return hom(domain, codomain, img_gens)
+end
+
+# with coefficient map
+function exterior_power(phi::ModuleFPHom{<:ModuleFP, <:ModuleFP}, p::Int; 
+    domain::ModuleFP=exterior_power(domain(phi), p)[1],
+    codomain::ModuleFP=exterior_power(codomain(phi), p)[1]
+  )
+  if ngens(Oscar.domain(phi)) == ngens(Oscar.codomain(phi)) == p
+    return hom(domain, codomain, [det(matrix(phi))*codomain[1]], base_ring_map(phi))
+  end
+  orig_img_gens = images_of_generators(phi)
+  dec = wedge_generator_decompose_function(domain)
+  dom_gens_dec = Vector{elem_type(Oscar.domain(phi))}[collect(dec(g)) for g in gens(domain)]
+  img_gens = elem_type(codomain)[wedge(phi.(v); parent=codomain) for v in dom_gens_dec]
+  return hom(domain, codomain, img_gens, base_ring_map(phi))
+end
+
