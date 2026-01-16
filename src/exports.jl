b46adfdacdd4ace1995b25dc729b59894f9e3f5d
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2026-01-16 17:34:47 +0100

    Add Ree group and Suzuki group constructors (#5640)

--- src/exports.jl
+++ src/exports.jl
@@ -1568,6 +1568,7 @@ export reduced_expressions
 export reduced_groebner_basis
 export reduced_scheme
 export reducible_fibers
+export ree_group
 export reflect, reflect!
 export reflection
 export register_morphism!
@@ -1752,6 +1753,7 @@ export subquotient
 export subscheme
 export subsystem
 export support_function
+export suzuki_group
 export syllables
 export sylow_subgroup
 export sylow_system, has_sylow_system, set_sylow_system
