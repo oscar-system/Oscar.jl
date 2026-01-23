766d96265696c3ddb5b8e5a9de8a1a5866a3e758
Yue Ren <yue.ren.kl@gmail.com>
2026-01-23 21:49:26 +0000

    TropicalGeometry: tropical_variety_prime_singular, fixed convention logic

--- src/TropicalGeometry/variety_prime.jl
+++ src/TropicalGeometry/variety_prime.jl
@@ -23,7 +23,7 @@ end
 function tropical_variety_prime_singular(I::MPolyIdeal, nu::TropicalSemiringMap{QQField,Nothing,<:Union{typeof(min),typeof(max)}}; weighted_polyhedral_complex_only::Bool=false)
     sI = Oscar.singular_generators(I)
     TropIString = Singular.low_level_caller("tropical", "tropicalVariety_as_string", (sI, ))
-    Sigma = gfan_fan_string_to_oscar_complex(TropIString,convention(nu)==max,false)
+    Sigma = gfan_fan_string_to_oscar_complex(TropIString,convention(nu)==min,false)
     TropI = compute_weights_and_construct_tropical_variety(Sigma,I,nu)
     if !weighted_polyhedral_complex_only
         set_attribute!(TropI,:algebraic_ideal,I)
