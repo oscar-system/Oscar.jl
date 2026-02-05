446babfdd64889be4441766639a47e6f1da954c2
Antony Della Vecchia <vecchia@math.tu-berlin.de>
2026-02-05 17:43:23 +0100

    rename fuinctions

--- ext/HomotopyContinuationExt.jl
+++ ext/HomotopyContinuationExt.jl
@@ -12,7 +12,7 @@ Takes a polynomial `f` an `MPolyRingElem` and converts it into a
 objects used in the expression have the same names as the generators
 in the `parent(f)` (including brackets as in `x[1]`).
 """
-function poly_to_expr(f::MPolyRingElem)
+function Oscar.poly_to_expr(f::MPolyRingElem)
   # Get a list of HomotopyContinuation variables whose names are the
   # same as the ones in the Oscar polynomial f.
   v = Variable.(symbols(parent(f)))
@@ -28,7 +28,7 @@ end
 Takes an `MPolyIdeal` and turns it into a `HomotopyContinuation.System`
 containing the ideal generators. It forwards all `args` to `HomotopyContinuation.System`.
 """
-System(I::MPolyIdeal; args...) = HomotopyContinuation.System(poly_to_expr.(gens(I)); args...)
+Oscar.System(I::MPolyIdeal; args...) = HomotopyContinuation.System(poly_to_expr.(gens(I)); args...)
 
 """
     nsolve(I::MPolyIdeal; args...)
@@ -36,9 +36,9 @@ System(I::MPolyIdeal; args...) = HomotopyContinuation.System(poly_to_expr.(gens(
 Call `HomotopyContinuation.solve` on the `HomotopyContinuation.System` derived
 from `I` forwarding all `args`.
 """
-nsolve(I::MPolyIdeal; show_progress=false, args...) = HomotopyContinuation.solve(System(I); show_progress, args...)
+Oscar.nsolve(I::MPolyIdeal; show_progress=false, args...) = HomotopyContinuation.solve(System(I); show_progress, args...)
 
-witness_set(I::MPolyIdeal; show_progress=false, args...) = HomotopyContinuation.witness_set(System(I); show_progress, args...)
+Oscar.witness_set(I::MPolyIdeal; show_progress=false, args...) = HomotopyContinuation.witness_set(System(I); show_progress, args...)
 
 """
     ndim(I::MPolyIdeal)
