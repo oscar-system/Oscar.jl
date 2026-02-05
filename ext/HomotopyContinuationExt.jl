3c68a5ea021eed39a15aeca4df1eeff850f318ff
Antony Della Vecchia <vecchia@math.tu-berlin.de>
2026-02-05 17:22:57 +0100

    attempting top create ext

--- ext/HomotopyContinuationExt.jl
+++ ext/HomotopyContinuationExt.jl
@@ -1,5 +1,7 @@
 module HomotopyContinuationExt
 
+using Oscar, HomotopyContinuation
+
 import HomotopyContinuation: HomotopyContinuation, Variable
 
 """
@@ -13,11 +15,41 @@ in the `parent(f)` (including brackets as in `x[1]`).
 function poly_to_expr(f::MPolyRingElem)
   # Get a list of HomotopyContinuation variables whose names are the
   # same as the ones in the Oscar polynomial f.
-  v = map(x -> Variable(symbol(x)), gens(parent(f)))
+  v = Variable.(symbols(parent(f)))
   # Make the HomotopyContinuation expression
-  sum([
-    prod([Rational(c), [v[i]^e for (i,e) in enumerate(a)]...]...)
+  +([
+    *([Float64(c), [v[i]^e for (i,e) in enumerate(a)]...]...)
     for (c,a) in coefficients_and_exponents(f) ]...)
+end
+
+"""
+    System(I::MPolyIdeal; args...)
+
+Takes an `MPolyIdeal` and turns it into a `HomotopyContinuation.System`
+containing the ideal generators. It forwards all `args` to `HomotopyContinuation.System`.
+"""
+System(I::MPolyIdeal; args...) = HomotopyContinuation.System(poly_to_expr.(gens(I)); args...)
+
+"""
+    nsolve(I::MPolyIdeal; args...)
+
+Call `HomotopyContinuation.solve` on the `HomotopyContinuation.System` derived
+from `I` forwarding all `args`.
+"""
+nsolve(I::MPolyIdeal; show_progress=false, args...) = HomotopyContinuation.solve(System(I); show_progress, args...)
+
+witness_set(I::MPolyIdeal; show_progress=false, args...) = HomotopyContinuation.witness_set(System(I); show_progress, args...)
+
+"""
+    ndim(I::MPolyIdeal)
+
+Compute the dimension of `I` numerically.
+"""
+function ndim(I::MPolyIdeal)
+  # This is provided by HomotopyContinuation.jl and computes the
+  # dimension based on the Jacobian rank at a random point.
+  F = System(I)
+  HomotopyContinuation.nvariables(F) - LinearAlgebra.rank(HomotopyContinuation.fixed(F))
 end
 
 end
