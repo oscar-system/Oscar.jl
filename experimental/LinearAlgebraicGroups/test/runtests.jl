bf01cf8462642fd973670796c883f23f2e414331
Janika Peters <janika.peters@rwth-aachen.de>
2026-02-14 14:16:03 +0100

    Add more tests

--- experimental/LinearAlgebraicGroups/test/runtests.jl
+++ experimental/LinearAlgebraicGroups/test/runtests.jl
@@ -6,3 +6,33 @@
   ConformanceTests.test_Group_interface(LAG)
   ConformanceTests.test_GroupElem_interface(rand(LAG, 2)...)
 end
+
+@testset "LAG subgroups test $(LAGname)" for (LAGname, LAG) in [
+  ("A4", linear_algebraic_group(root_system(:A, 4), GF(7))),
+  ("A5", linear_algebraic_group(:A, 5, GF(5))),
+  ("A2", linear_algebraic_group(root_system(:A, 2), GF(3))),
+]
+for alpha in positive_roots(root_system(LAG))
+  U = root_subgroup(LAG, alpha)
+  b, _ = is_subgroup(U,LAG)
+  @test b
+end
+T = maximal_torus(LAG)
+B = borel(LAG)
+b, _ = is_subgroup(B,LAG)
+@test b 
+b, _ = is_subgroup(T,LAG)
+@test b
+end
+
+@testset "Bruhat decomposition" begin
+  LAG = linear_algebraic_group(root_system(:A, 2), GF(3))
+  bruh = bruhat_decomp(LAG)
+  W = weyl_group(root_system(LAG))
+  @test length(bruh) == order(W)
+  check = 0
+  for cell in bruh
+    check += order(cell)
+  end
+  @test check == order(LAG)
+end
