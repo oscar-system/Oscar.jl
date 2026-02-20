45e5b2f047e97fbfbadc1792c865a2067f43357d
antonydellavecchia <antonydellavecchia@gmail.com>
2026-02-20 12:56:30 +0100

    some changes to function names

--- test/exts.jl
+++ test/exts.jl
@@ -2,7 +2,7 @@
   @testset "HomotopyContinuation" begin
     Qxy, (x, y) = QQ[:x, :y]
     I = ideal([x^2 - x * y + y^3, y^2 - x - 4//5])
-    @test iszero(Oscar.ndim(I))
+    @test iszero(HomotopyContinuation.dim(I))
     result = Oscar.nsolve(I)
     @test [r.multiplicity for r in result.path_results] == [1, 1, 1, 1]
 
