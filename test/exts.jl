123f89c4e60c04730adfa8ff811f1281b95057d5
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2026-02-20 14:43:55 +0100

    Apply suggestions from code review

--- test/exts.jl
+++ test/exts.jl
@@ -5,11 +5,11 @@ using HomotopyContinuation
     Qxy, (x, y) = QQ[:x, :y]
     I = ideal([x^2 - x * y + y^3, y^2 - x - 4//5])
     @test iszero(HomotopyContinuation.dim(I))
-    result = Oscar.nsolve(I)
+    result = HomotopyContinuation.solve(I)
     @test [r.multiplicity for r in result.path_results] == [1, 1, 1, 1]
 
     J = ideal([x^2 - x * y + y^3])
-    ws = Oscar.witness_set(J)
+    ws = HomotopyContinuation.witness_set(J)
     @test length(ws.R) == 3
   end
 end
