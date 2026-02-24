abb7ef780bcff7762c86a88d8c11874f272bde97
Zoe Geiselmann <geiselmann@math.tu-berlin.de>
2026-02-24 11:44:08 +0100

    Tutte group first implementation

--- src/Combinatorics/Matroids/tutte.jl
+++ src/Combinatorics/Matroids/tutte.jl
@@ -1,55 +1,4 @@
 
-export tutte_group, circuits, circuit, circuit
-
-struct TutteGroup
-  group::FinGenAbGroup
-end
-
-#=
-    def __init__(self, M, char2 = False):
-        
-        B = list(M.bases())
-        idx = {B[i] : i for i in range(len(B))}
-        idx[frozenset()] = len(B)
-        self._M = M
-        self._bases = B
-        self._idx = idx
-        
-        if char2:
-            R = [{frozenset():1}]
-        else:
-            R = [{frozenset():2}]
-        
-        
-        for X in M.nonbases():
-            if M.rank(X)==M.full_rank()-1: 
-                C=set(M.circuit(X))
-                D=set(M.cocircuit(M.groundset()-X))
-                e = C.pop()
-                f = D.pop()
-                for g in C:
-                    for h in D:
-                        R.append(self.cross_ratio(X-set([e,g]),e,g,f,h))
-        self._TT = matrix(ZZ, 1,len(B)+1, sparse = True)
-        
-        self.add_relations(R)
-        
-
-=#
-#=
-M0 = matroid_from_nonbases([[3,4]],4)
-tutte_group(M0)
-M = pappus_matroid()
-tutte_group(M)
-circuit(M0,[3,4])
-M1 = matroid_from_circuits([[1,2,3,4],[5,6,7,2],[1,5,6,7,3,4]],7)
-tutte_group(M1)
-
-NB = nonbases(M1)
-circuits.(Ref(M1),NB)
-circuits(M1,matroid_groundset(M1))
-circuits(M1)
-=#
 
 function circuits(M::Matroid, S)
   return circuits(restriction(M,S))
@@ -57,28 +6,67 @@ end
 
 function circuit(M::Matroid, S)
   #Check for independence
+  @req rank(M) != length(matroid_groundset(M)) "The rank of the matroid should be smaller than the cardinality of the ground set"
   return circuits(M,S)[1] 
 end
 
 function cocircuits(M::Matroid, S)
-  cocircuits(M,
+  MD = dual_matroid(M)
+  return circuits(MD,S)
+end
+
+function cocircuit(M::Matroid, S)
+  #Check for independence
+  @req !iszero(rank(M)) "The rank of the matroid should be smaller than the cardinality of the ground set"
+  return cocircuits(M,S)[1] 
 end
 
 
 
-function tutte_group(M::Matroid)
+
+function tutte_group(R::Ring, M::Matroid)
   B = bases(M)
+  idx = Dict{Set{Int}, Int}(Set(k) => i for (i,k) in enumerate(B))
+  #idx[Set{Int}()] = length(B) + 1 #this is to index the epsilon
   gs = matroid_groundset(M)
-  ret = []
+  if characteristic(R)==2
+            v = zeros(Int, length(B)+1)
+            v[end] = 1 #this is for the epsilon
+            relations = [v]
+  else
+            v = zeros(Int, length(B)+1)
+            v[end] = 2
+            relations = [v]
+  end
   for X in nonbases(M)
     if rank(M,X) == rank(M)-1
       C = circuit(M,X)
-      D = setdiff(gs,X)
-      push!(ret,D)
-    else
-      println("hello there")
+      D = cocircuit(M,setdiff(gs,X))
+      e = popfirst!(C)
+      f = popfirst!(D)
+      #push!(ret,D)
+      for g in C
+        for h in D
+          v = zeros(Int, length(B)+1)
+          I = setdiff(Set(X), [e,g])
+          v[idx[union(I, [e,f])]] = 1
+          v[idx[union(I, [e,h])]] = 1
+          v[idx[union(I, [g,f])]] = -1
+          v[idx[union(I, [g,h])]] = -1
+          v[end] = sum([e<g, g<f, f<h, h<e]) #this is the index for the epsilon
+          push!(relations, v)
+       end
+     end
     end
   end
-  return ret
+  relations_matrix = matrix(ZZ, relations)
+  return abelian_group(relations_matrix)
+end
+
+
+export tutte_group, circuits, circuit, circuit
+
+struct TutteGroup
+  group::FinGenAbGroup
 end
 
