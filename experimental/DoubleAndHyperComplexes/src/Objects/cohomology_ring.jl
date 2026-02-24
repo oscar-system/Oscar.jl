cde35cde73f899a29e8dcec2b31176c753d13e9b
HechtiDerLachs <zach@mathematik.uni-kl.de>
2026-02-24 12:12:01 +0100

    First implementation of addition.

--- experimental/DoubleAndHyperComplexes/src/Objects/cohomology_ring.jl
+++ experimental/DoubleAndHyperComplexes/src/Objects/cohomology_ring.jl
@@ -9,15 +9,17 @@ mutable struct SimplicialCohomologyRing{T} <: NCRing
   end
 end
 
+simplicial_co_complex(A::SimplicialCohomologyRing) = A.C
+
 mutable struct SimplicialCohomologyRingElem{T}
   parent::SimplicialCohomologyRing{T}
-  homog_elem::Union{SubQuoModuleElem{T}, Nothing}
+  homog_elem::Union{SubquoModuleElem{T}, Nothing}
   homog_deg::Union{Int, Nothing}
   coeff::Dict{Int, SubquoModuleElem{T}}
 
   # Constructor for homogeneous elements
   function SimplicialCohomologyRingElem(
-      A::SimplicialCohomologyRingElem{T}, 
+      A::SimplicialCohomologyRing{T}, 
       p::Int, v::SubquoModuleElem{T}
     ) where {T}
     return new{T}(A, v, p)
@@ -25,7 +27,7 @@ mutable struct SimplicialCohomologyRingElem{T}
 
   # Constructor for mixed elements
   function SimplicialCohomologyRingElem(
-      A::SimplicialCohomologyRingElem{T}, 
+      A::SimplicialCohomologyRing{T}, 
       coeff::Dict{Int, SubquoModuleElem{T}}
     ) where {T}
     return new{T}(A, nothing, nothing, coeff)
@@ -33,9 +35,95 @@ mutable struct SimplicialCohomologyRingElem{T}
 
   # Constructor for the zero
   function SimplicialCohomologyRingElem(
-      A::SimplicialCohomologyRingElem{T}
+      A::SimplicialCohomologyRing{T}
     ) where {T}
     return new{T}(A, nothing, nothing)
   end
 end
+
+zero(A::SimplicialCohomologyRing) = SimplicialCohomologyRingElem(A)
+
+function one(A::SimplicialCohomologyRing)
+  SimplicialCohomologyRingElem(A, 0, sum(gens(homology(simplicial_co_complex(A), 0)[1])))
+end
+
+function deepcopy_internal(d::IdDict, a::SimplicialCohomologyRingElem)
+  result = parent(a)()
+  if !isnothing(a.homog_elem)
+    result.homog_elem = deepcopy_internal(d, a.homog_elem)
+    result.homog_deg = copy(a.homog_deg)
+    return result
+  end
+  result.coeff = deepcopy_internal(d, a.coeff)
+  return result
+end
+
+function +(a::SimplicialCohomologyRingElem, b::SimplicialCohomologyRingElem)
+  @assert parent(a) === parent(b) "parent mismatch"
+  is_zero(a) && return copy(b)
+  is_zero(b) && return copy(a)
+  result = parent(a)() # unassigned zero element
+  if isnothing(a.homog_elem)
+    result.coeff = copy(a.coeff) # the result will not be homogeneously stored 
+    !isdefined(a.coeff) && return copy(b) # a was the zero element in the end
+    if isnothing(b.homog_elem)
+      for (q, v) in b.coeff
+        w = get(result.coeff, q, nothing)
+        if isnothing(w)
+          result.coeff[q] = v
+        else
+          res = w + v
+          if is_zero(res)
+            delete!(result.coeff, q)
+          else
+            result.coeff[q] = res
+          end
+        end
+      end
+      return result
+    else # b is homogeneous
+      q = b.homog_deg
+      w = get(result.coeff, q, nothing)
+      if isnothing(w)
+        result.coeff[q] = b.homog_elem
+      else
+        res = w + b.homog_elem
+        if is_zero(res)
+          delete!(result.coeff, q)
+        else
+          result.coeff[q] = res
+        end
+      end
+    end
+  else # a is homogeneous
+    q = a.homog_deg
+    if isnothing(b.homog_elem) # b is not homogeneous
+      result.coeff = copy(b.coeff) # result will not be homogeneously stored
+      w = get(result.coeff, q, nothing)
+      if isnothing(w)
+        result.coeff[q] = a.homog_elem
+      else
+        res = w + a.homog_elem
+        if iszero(res)
+          delete!(result.coeff, q)
+        else
+          result.coeff[q] = res
+        end
+      end
+      return result
+    else # b is homogeneous
+      p = b.homog_deg
+      if p == q
+        result.homog_deg = p
+        result.homog_elem = a.homog_elem + b.homog_elem
+        return result
+      else
+        result.coeff = Dict{Int, SubquoModuleElem{T}}([q => a.homog_elem, p => b.homog_elem])
+        return result
+      end
+    end
+  end
+end
+
+
 
