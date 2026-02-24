3f21e35d80a5a89e62b4f909e434f4f3457c99ca
Lena Weis <weis@math.tu-berlin.de>
2026-02-24 15:25:17 +0100

    refrence + docs

--- src/Combinatorics/Matroids/tutte.jl
+++ src/Combinatorics/Matroids/tutte.jl
@@ -1,44 +1,54 @@
+"""
+  circuits(M::Matroid, S::Union{AbstractVector{T}, AbstractSet{T}}) where T<:GroundsetType
 
-
-function circuits(M::Matroid, S)
+  Return the list of circuits of the matroid contained in `S`. 
+"""
+function circuits(M::Matroid, S::Union{AbstractVector{T}, AbstractSet{T}}) where T<:GroundsetType
   @req all(s in matroid_groundset(M) for s in S) "The restriction set has to be a subset of the matroid's ground set"
   return circuits(restriction(M,S))
 end
 
-function circuit(M::Matroid, S)
-  #Check for independence
-  @req rank(M) != length(matroid_groundset(M)) "The rank of the matroid should be smaller than the cardinality of the ground set"
-  return circuits(M,S)[1] 
+function _circuit(M::Matroid, S::Union{AbstractVector{T}, AbstractSet{T}}) where T<:GroundsetType
+  C = circuits(M, S)
+    @req len(C) > 0 "S does not contain a circuit of the matroid"
+  return C[1] 
 end
 
-function cocircuits(M::Matroid, S)
+"""
+  cocircuits(M::Matroid, S::Union{AbstractVector{T}, AbstractSet{T}}) where T<:GroundsetType
+
+  Return the list of cocircuits of matroid contained in S.
+"""
+function cocircuits(M::Matroid, S::Union{AbstractVector{T}, AbstractSet{T}}) where T<:GroundsetType
   MD = dual_matroid(M)
   return circuits(MD,S)
 end
 
-function cocircuit(M::Matroid, S)
-  #Check for independence
+function _cocircuit(M::Matroid, S::Union{AbstractVector{T}, AbstractSet{T}}) where T<:GroundsetType
+  
   @req !iszero(rank(M)) "The rank of the matroid should be smaller than the cardinality of the ground set"
   return cocircuits(M,S)[1] 
 end
 
+"""
+  Computes the Tutte group of a matroid M for a ring with characteristic `char`.
+  For more details; see [DW89](@cite).
+"""
 function tutte_group(R::Ring, M::Matroid)
   B = bases(M)
   idx = Dict{Set{Int}, Int}(Set(k) => i for (i,k) in enumerate(B))
   #idx[Set{Int}()] = length(B) + 1 #this is to index the epsilon
   gs = matroid_groundset(M)
-  if characteristic(R)==2
-            v = zeros(Int, length(B)+1)
-            v[end] = 1 #this is for the epsilon
-            relations = [v]
+  v = zeros(Int, length(B)+1)
+  if char == 2
+    v[end] = 1 #this is for the epsilon
   else
-            v = zeros(Int, length(B)+1)
-            v[end] = 2
-            relations = [v]
+    v[end] = 2
   end
+  relations = [v]
   for X in nonbases(M)
     if rank(M,X) == rank(M)-1
-      C = circuit(M,X)
+      C = _circuit(M,X)
       D = cocircuit(M,setdiff(gs,X))
       e = popfirst!(C)
       f = popfirst!(D)
@@ -62,26 +72,24 @@ function tutte_group(R::Ring, M::Matroid)
 end
 
 """
-  DW_condition(R::Ring, M::Matroid)
-
-
+  False if matroid M is not realizable over R.
+  True if inconclusive. 
+  Note that the Tutte group only yields a necessary (and no sufficient) criterion for realizability of M over R;
+  see Corollary 1 in Section 3 of [DW89](@cite).
 ```julia
-julia> DW_condition(QQ,uniform_matroid(2,4));
+julia> tutte_realizable(QQ,uniform_matroid(2,4));
 false
-julia> DW_condition(QQ,fano_matroid())
+julia> tutte_realizable(QQ,fano_matroid())
 true
 ```
-
-```
 """
-function DW_condition(R::Ring, M::Matroid)
+function tutte_realizable(R::Ring, M::Matroid)
   T = tutte_group(R,M);
-  return DW_condition(T)
-end
-function DW_condition(G::FinGenAbGroup)
-  n = ngens(G)
-  e = G([ i == n ? 1 : 0 for i in 1:n])
-  return is_one(e)
+  return tutte_realizable(T)
 end
 
-export tutte_group, circuits, circuit, cocircuit, DW_condition
+function tutte_realizable(G::FinGenAbGroup)
+  return !is_one(G[end])
+end
+
+export tutte_group, circuits, cocircuit, tutte_realizable
