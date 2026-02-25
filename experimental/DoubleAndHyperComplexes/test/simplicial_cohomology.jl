f8fb049af9aeaf2de539ee8509e34a808198dbda
Kevin Kuehn <kuehn@math.tu-berlin.de>
2026-02-25 14:17:55 +0100

    Add tests for functions is_domain_type and is_exact_type.

--- experimental/DoubleAndHyperComplexes/test/simplicial_cohomology.jl
+++ experimental/DoubleAndHyperComplexes/test/simplicial_cohomology.jl
@@ -17,4 +17,16 @@
     @test AK_23 <: Oscar.SimplicialCohomologyRing
 
     @test base_ring(AK_23) == GF(23)
+
+    K = simplicial_complex([[1,2,3],[2,3,4]])
+    Cmplx_p = Oscar.SimplicialCoComplex(padic_field(7, precision = 30),K)
+    R_p = Oscar.SimplicialCohomologyRing(Cmplx_p)
+    @test is_exact_type(typeof(one(R_p))) == false
+    @test is_domain_type(typeof(one(R_p))) == false
+    
+    Cmplx = Oscar.SimplicialCoComplex(ZZ,K)
+    R = Oscar.SimplicialCohomologyRing(Cmplx)
+    @test is_exact_type(typeof(one(R))) == true
+    @test is_domain_type(typeof(one(R))) == false
+
 end
