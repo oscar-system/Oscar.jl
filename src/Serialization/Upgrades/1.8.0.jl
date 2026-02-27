679f0827ba0f53f5892ef5436deab3de15f1f7a5
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2026-02-27 15:29:34 +0100

    Also recurse into attributes

--- src/Serialization/Upgrades/1.8.0.jl
+++ src/Serialization/Upgrades/1.8.0.jl
@@ -2,7 +2,7 @@ push!(upgrade_scripts_set, UpgradeScript(
   v"1.8.0",
   function upgrade_1_8_0(s::UpgradeState, dict::AbstractDict{Symbol, Any})
     # recurse upgrade on containers
-    upgrade_containers(upgrade_1_8_0, s, dict)
+    upgrade_recursive(upgrade_1_8_0, s, dict)
 
     if dict[:_type] isa AbstractDict && haskey(dict[:_type], :name)
       type_name = dict[:_type][:name]
