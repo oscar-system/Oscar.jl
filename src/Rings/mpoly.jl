217d6626dabb5f69fa02ab7a941d7dcd0caa9dc7
Max Horn <max@quendi.de>
2026-03-26 00:34:08 +0100

    Apply suggestions from code review
    Co-authored-by: Max Horn <max@quendi.de>

--- src/Rings/mpoly.jl
+++ src/Rings/mpoly.jl
@@ -1172,6 +1172,4 @@ Return the vector of all monomials of degree `d` in `A` with respect to the stan
 !!! note
     To obtain an iterator instead of a vector as return value use [`monomials_of_degree`](@ref).
 ""
-function monomial_basis(A::MPolyRing, d::IntegerUnion)
-  return collect(monomials_of_degree(A,d))
-end
+monomial_basis(A::MPolyRing, d::IntegerUnion) = collect(monomials_of_degree(A,d))
