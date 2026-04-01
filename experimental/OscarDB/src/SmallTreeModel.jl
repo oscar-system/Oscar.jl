0b0a46e792670d6ae610a22cf11e4122128304f8
garrote <marina.garrote@mis.mpg.de>
2026-04-01 13:35:40 +0200

    new constructor for SmallPhylogenenticModel

--- experimental/OscarDB/src/SmallTreeModel.jl
+++ experimental/OscarDB/src/SmallTreeModel.jl
@@ -11,7 +11,6 @@ struct SmallGroupBasedModel
   model::Union{GroupBasedPhylogeneticModel{PhylogeneticTree{QQFieldElem}},
                GroupBasedPhylogeneticModel{PhylogeneticNetwork{QQFieldElem}}} ## Can this be just ::GroupBasedPhylogeneticModel?
   model_type::String # ex: jukes_cantor_model
-  phylogenetic_model_id::String
   n_leaves::Int
   level::Int
   n_cycles::Int
@@ -36,8 +35,7 @@ Creates a `SmallGroupBasedModel` which is the struct used to populate the collec
 function small_group_based_model(name::String,
                           model::Union{GroupBasedPhylogeneticModel{PhylogeneticTree{QQFieldElem}},
                                        GroupBasedPhylogeneticModel{PhylogeneticNetwork{QQFieldElem}}},
-                          model_type::String,
-                          phylogenetic_model_id::String)
+                          model_type::String)
 
   G = graph(model)
   f = parametrization(model)
@@ -48,7 +46,6 @@ function small_group_based_model(name::String,
     name,
     model,
     model_type,
-    phylogenetic_model_id,
     Oscar.n_leaves(G),
     Oscar.level_phylogenetic_network(graph_from_edges(Directed, edges(G))),
     sum([length(h)-1 for h in Oscar.hybrid_edges(graph_from_edges(Directed, edges(G)))]),
@@ -72,7 +69,7 @@ Return the `GroupBasedPhylogeneticModel` of the small group-based model `sgb`.
 group_based_phylogenetic_model(sgb::SmallGroupBasedModel) = sgb.model
 
 @doc raw"""
-    phylogenetic_model(sgb::SmallGroupBasedModel)
+  phylogenetic_model(sgb::SmallGroupBasedModel)
 
 Return the `PhylogeneticModel` of the small group-based model `sgb`.
 """
@@ -87,84 +84,98 @@ model_type(sgb::SmallGroupBasedModel) = sgb.model_type
 
 #### ????
 @doc raw"""
-    phylogenetic_model_id(sgb::SmallGroupBasedModel)
+  phylogenetic_model_id(sgb::SmallGroupBasedModel)
 
 Return the id of the corresponding `SmallPhylogeneticModel` from the collection "AlgebraicStatistics.SmallPhylogeneticModels" of the `OscarDB`
 """
 phylogenetic_model_id(sgb::SmallGroupBasedModel) = sgb.phylogenetic_model_id
 
 @doc raw"""
-    graph(sgb::SmallGroupBasedModel)
+  graph(sgb::SmallGroupBasedModel)
 
 Return the graph of the small group-based model `sgb`.
 """
 graph(sgb::SmallGroupBasedModel) = graph(group_based_phylogenetic_model(sgb))
 
 @doc raw"""
-    n_leaves(sgb::SmallGroupBasedModel)
+  n_leaves(sgb::SmallGroupBasedModel)
 
-Return the number of leaves of the small group-based model `sgb`.
+Return the number of leaves of the phylogenetic tree or network in the small group-based model `sgb`.
 """
-n_leaves(sgb::SmallGroupBasedModel) = n_leaves(graph(sgb))
+n_leaves(sgb::SmallGroupBasedModel) = sgb.n_leaves
 
 @doc raw"""
-    dim(sgb::SmallGroupBasedModel)
+  level(sgb::SmallGroupBasedModel)
+
+Return the level of the phylogenetic network `N` in the small group-based model `sgb`. If `N` is a tree, the ouptput is zero.
+"""
+level(sgb::SmallGroupBasedModel) = sgb.level
+
+@doc raw"""
+  n_cycles(sgb::SmallGroupBasedModel)
+
+Return the number of cycles in the phylogenetic network `N` in the small group-based model `sgb`. If `N` is a tree, the ouptput is zero.
+"""
+n_cycles(sgb::SmallGroupBasedModel) = sgb.n_cycles
+
+@doc raw"""
+  dim(sgb::SmallGroupBasedModel)
 
 Return the dimension of the small group-based model `sgb`.
 """
 dim(sgb::SmallGroupBasedModel) = sgb.dimension
 
 @doc raw"""
-    degree(sgb::SmallGroupBasedModel)
+  degree(sgb::SmallGroupBasedModel)
 
 Return the degree of the small group-based model `sgb`.
 """
 degree(sgb::SmallGroupBasedModel) = sgb.degree
 
 @doc raw"""
-    n_coordinates(sgb::SmallGroupBasedModel)
+  n_coordinates(sgb::SmallGroupBasedModel)
 
 Return the dimension of the smallest linear subspace containing the small group-based model `sgb` (in Fourier coordinates).
 """
 n_coordinates(sgb::SmallGroupBasedModel) = sgb.n_coordinates
 
 @doc raw"""
-    dimension_singular_locus(sgb::SmallGroupBasedModel)
+  dimension_singular_locus(sgb::SmallGroupBasedModel)
 
 Return the dimension of the singular locus of the small group-based model `sgb`.
 """
 dimension_singular_locus(sgb::SmallGroupBasedModel) = sgb.dim_sl
 
 @doc raw"""
-    degree_singular_locus(sgb::SmallGroupBasedModel)
+  degree_singular_locus(sgb::SmallGroupBasedModel)
 
 Return the degree of the singular locus of the small group-based model `sgb`.
 """
 degree_singular_locus(sgb::SmallGroupBasedModel) = sgb.deg_sl
 
 @doc raw"""
-    euclidean_distance_degree(sgb::SmallGroupBasedModel)
+  euclidean_distance_degree(sgb::SmallGroupBasedModel)
 
 Return the Euclidean distance degree the small group-based model `sgb`.
 """
 euclidean_distance_degree(sgb::SmallGroupBasedModel) = sgb.EDdeg
 
 @doc raw"""
-    parametrization(sgb::SmallGroupBasedModel)
+  parametrization(sgb::SmallGroupBasedModel)
 
 Return the parametrization of the small group-based model `sgb` (in Fourier coordinates).
 """
 parametrization(sgb::SmallGroupBasedModel) = sgb.parametrization
 
 @doc raw"""
-    equivalent_classes(sgb::SmallGroupBasedModel)
+  equivalent_classes(sgb::SmallGroupBasedModel)
 
 Return the equivalent classes of the small group-based model `sgb` (in Fourier coordinates).
 """
 equivalent_classes(sgb::SmallGroupBasedModel) = sgb.eq_classes
 
 @doc raw"""
-    vanishing_ideal(sgb::SmallGroupBasedModel)
+  vanishing_ideal(sgb::SmallGroupBasedModel)
 
 Return the vanishing ideal of the small group-based model `sgb` (in Fourier coordinates).
 """
@@ -198,8 +209,8 @@ struct SmallPhylogeneticModel
   vanishing_ideal::Union{MPolyIdeal{QQMPolyRingElem}, Nothing}
 end
 @doc raw"""
-    small_phylogenetic_model(name::String, model::Union{PhylogeneticModel{PhylogeneticTree{QQFieldElem}},
-                             PhylogeneticModel{PhylogeneticNetwork{QQFieldElem}}}, model_type::String, extended_model_id::Union{String, Nothing})
+  small_phylogenetic_model(name::String, model::Union{PhylogeneticModel{PhylogeneticTree{QQFieldElem}},
+                            PhylogeneticModel{PhylogeneticNetwork{QQFieldElem}}}, model_type::String, extended_model_id::Union{String, Nothing})
 
 Creates a `SmallPhylogeneticModel` which is the struct used to populate the collection "AlgebraicStatistics.SmallPhylogeneticModels" of the `OscarDB`
 """
@@ -210,7 +221,6 @@ function small_phylogenetic_model(name::String,
                           extended_model_id::String="")
 
   G = graph(model)
-  f = parametrization(model)
   ec = equivalent_classes(model)
   # I = vanishing_ideal(model)
 
@@ -229,22 +239,53 @@ function small_phylogenetic_model(name::String,
     nothing,
     nothing,
     nothing,
-    f,
+    parametrization(model),
     ec,
     nothing
   )
 end
 
+function small_phylogenetic_model(name::String, sbg::SmallGroupBasedModel)
+  gb_model = group_based_phylogenetic_model(sbg)
+  f = coordinate_change(gb_model)
+  I = vanishing_ideal(sbg)
+  Ip = f(I)
+
+  model = phylogenetic_model(sbg)
+  ec = equivalent_classes(model)
+
+  return SmallPhylogeneticModel(
+    name,
+    model,
+    model_type(sbg),
+    sbg._id,
+    n_leaves(sbg),
+    level(sbg),
+    n_cycles(sbg),
+    dim(sbg),
+    degree(sbg),
+    length(ec),
+    dimension_singular_locus(sbg),
+    degree_singular_locus(sbg),
+    nothing,
+    euclidean_distance_degree(sbg),
+    parametrization(model),
+    ec,
+    Ip
+  )
+
+end
+
 
 @doc raw"""
-    phylogenetic_model(spm::SmallPhylogeneticModel)
+  phylogenetic_model(spm::SmallPhylogeneticModel)
 
 Return the `PhylogeneticModel` of the small phylogenetic model `spm`.
 """
 phylogenetic_model(spm::SmallPhylogeneticModel) = spm.model
 
 @doc raw"""
-    model_type(spm::SmallPhylogeneticModel)
+  model_type(spm::SmallPhylogeneticModel)
 
 Return the model type of the `spm` as a small phylogenetic model string.
 """
@@ -252,91 +293,105 @@ model_type(spm::SmallPhylogeneticModel) = spm.model_type
 
 #### ????
 @doc raw"""
-    extended_model_id(sgb::SmallPhylogeneticModel)
+  extended_model_id(spm::SmallPhylogeneticModel)
 
 Return the id of the corresponding `GroupBasedModel` from the collection "AlgebraicStatistics.GroupBasedModels" of the `OscarDB`
 """
-extended_model_id(sgb::SmallPhylogeneticModel) = sgb.extended_model_id
+extended_model_id(spm::SmallPhylogeneticModel) = spm.extended_model_id
 
 @doc raw"""
-    graph(spm::SmallPhylogeneticModel)
+  graph(spm::SmallPhylogeneticModel)
 
 Return the graph of the small phylogenetic model `spm`.
 """
 graph(spm::SmallPhylogeneticModel) = graph(phylogenetic_model(spm))
 
 @doc raw"""
-    n_leaves(spm::SmallPhylogeneticModel)
+  n_leaves(spm::SmallPhylogeneticModel)
 
-Return the number of leaves of the small phylogenetic model `spm`.
+Return the number of leaves of the phylogenetic tree or network in the small phylogenetic model `spm`.
 """
-n_leaves(spm::SmallPhylogeneticModel) = n_leaves(graph(spm))
+n_leaves(spm::SmallPhylogeneticModel) = spm.n_leaves
 
 @doc raw"""
-    dim(spm::SmallPhylogeneticModel)
+  level(spm::SmallPhylogeneticModel)
+
+Return the level of the phylogenetic network `N` in the small phylogenetic model `spm`. If `N` is a tree, the ouptput is zero.
+"""
+level(spm::SmallPhylogeneticModel) = spm.level
+
+@doc raw"""
+  n_cycles(spm::SmallPhylogeneticModel)
+
+Return the number of cycles in the phylogenetic network `N` in the small phylogenetic model `spm`. If `N` is a tree, the ouptput is zero.
+"""
+n_cycles(spm::SmallPhylogeneticModel) = spm.n_cycles
+
+@doc raw"""
+  dim(spm::SmallPhylogeneticModel)
 
 Return the dimension of the small phylogenetic model `spm`.
 """
 dim(spm::SmallPhylogeneticModel) = spm.dimension
 
 @doc raw"""
-    degree(spm::SmallPhylogeneticModel)
+  degree(spm::SmallPhylogeneticModel)
 
 Return the degree of the small phylogenetic model `spm`.
 """
 degree(spm::SmallPhylogeneticModel) = spm.degree
 
 @doc raw"""
-    n_coordinates(spm::SmallPhylogeneticModel)
+  n_coordinates(spm::SmallPhylogeneticModel)
 
 Return the dimension of the smallest linear subspace containing the small phylogenetic model `spm`.
 """
 n_coordinates(spm::SmallPhylogeneticModel) = spm.n_coordinates
 
 @doc raw"""
-    dimension_singular_locus(spm::SmallPhylogeneticModel)
+  dimension_singular_locus(spm::SmallPhylogeneticModel)
 
 Return the dimension of the singular locus of the small phylogenetic model `spm`.
 """
 dimension_singular_locus(spm::SmallPhylogeneticModel) = spm.dim_sl
 
 @doc raw"""
-    degree_singular_locus(spm::SmallPhylogeneticModel)
+  degree_singular_locus(spm::SmallPhylogeneticModel)
 
 Return the degree of the singular locus of the small phylogenetic model `spm`.
 """
 degree_singular_locus(spm::SmallPhylogeneticModel) = spm.deg_sl
 
 @doc raw"""
-    euclidean_distance_degree(spm::SmallPhylogeneticModel)
+  euclidean_distance_degree(spm::SmallPhylogeneticModel)
 
 Return the Euclidean distance degree the small phylogenetic model `spm`.
 """
 euclidean_distance_degree(spm::SmallPhylogeneticModel) = spm.EDdeg
 
 @doc raw"""
-    maximum_likelihood_degree(spm::SmallPhylogeneticModel)
+  maximum_likelihood_degree(spm::SmallPhylogeneticModel)
 
 Return the maximum likelihood degree the small phylogenetic model `spm`.
 """
 maximum_likelihood_degree(spm::SmallPhylogeneticModel) = spm.MLdeg
 
 @doc raw"""
-    parametrization(spm::SmallPhylogeneticModel)
+  parametrization(spm::SmallPhylogeneticModel)
 
 Return the parametrization of the small phylogenetic model `spm`.
 """
 parametrization(spm::SmallPhylogeneticModel) = spm.parametrization
 
 @doc raw"""
-    equivalent_classes(spm::SmallPhylogeneticModel)
+  equivalent_classes(spm::SmallPhylogeneticModel)
 
 Return the equivalent classes of the small phylogenetic model `spm`.
 """
 equivalent_classes(spm::SmallPhylogeneticModel) = spm.eq_classes
 
 @doc raw"""
-    vanishing_ideal(spm::SmallPhylogeneticModel)
+  vanishing_ideal(spm::SmallPhylogeneticModel)
 
 Return the vanishing ideal of the small phylogenetic model `spm`.
 """
@@ -345,4 +400,5 @@ vanishing_ideal(spm::SmallPhylogeneticModel) = spm.vanishing_ideal
 function Base.show(io::IO, spm::SmallPhylogeneticModel)
   print(io, "Small phylogenetic model $(spm._id)")
 end
+
 
