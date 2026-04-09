675ef08ff41e5b0de1559fc212928b41f4ac8636
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2026-04-09 11:01:09 +0200

    Update hash

--- test/Serialization/upgrades/setup_tests.jl
+++ test/Serialization/upgrades/setup_tests.jl
@@ -8,7 +8,7 @@ if !isdefined(Main, :serialization_upgrade_test_path) ||
   !isdir(Main.serialization_upgrade_test_path) ||
   !isfile(joinpath(Main.serialization_upgrade_test_path, "LICENSE.md"))
 
-  serialization_upgrade_test_path = let commit_hash = "eb9f9dcd294e95c469c33ed8c588cdc6accc10c8"
+  serialization_upgrade_test_path = let commit_hash = "e3473ad614217bc19fad7f809de2d863984be157"
     tarball = Downloads.download("https://github.com/oscar-system/serialization-upgrade-tests/archive/$(commit_hash).tar.gz")
 
     destpath = open(CodecZlib.GzipDecompressorStream, tarball) do io
