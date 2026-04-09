4ff68300cdd33f3b194bb2a2b135c4dd4d587577
Benjamin Lorenz <benlorenz@users.noreply.github.com>
2026-04-09 12:40:24 +0200

    Update experimental/AlgebraicStatistics/test/serialization-upgrades.jl

--- experimental/AlgebraicStatistics/test/serialization-upgrades.jl
+++ experimental/AlgebraicStatistics/test/serialization-upgrades.jl
@@ -2,7 +2,7 @@
   @testset "load Lie GroupBasedPhylogeneticModel serialized wtih 1.7.0" begin
     test_upgrade_folder("version_1_7_0";
       only=[
-        "GroupbasedPhylogeneticModel",
+        "GroupBasedPhylogeneticModel",
       ],
     )
   end
