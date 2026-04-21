37f01ffa528802413794d02dfd121c11fd05f8fb
bpiroddi <270620104+bpiroddi@users.noreply.github.com>
2026-04-21 11:46:03 +0200

    fixed list of includes

--- src/Oscar.jl
+++ src/Oscar.jl
@@ -279,7 +279,6 @@ include("Rings/ReesAlgebra.jl") # Needs ModuleFP
 include("NumberTheory/NmbThy.jl")
 include("NumberTheory/QuadFormAndIsom.jl")
 include("NumberTheory/vinberg.jl")
-include("NumberTheory/ZLattices.jl")
 include("NumberTheory/embedding_with_roots.jl")
 
 include("Combinatorics/Graphs/structs.jl")
