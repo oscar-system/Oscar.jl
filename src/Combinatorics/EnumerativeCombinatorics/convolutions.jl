771d4813850abb3169045062baec76bfe9388064
Tobias271828 <178478878+Tobias271828@users.noreply.github.com>
2026-05-12 15:14:00 +0200

    Update src/Combinatorics/EnumerativeCombinatorics/convolutions.jl
    Co-authored-by: Lars Göttgens <lars.goettgens@gmail.com>

--- src/Combinatorics/EnumerativeCombinatorics/convolutions.jl
+++ src/Combinatorics/EnumerativeCombinatorics/convolutions.jl
@@ -23,9 +23,5 @@ julia> convolution([1, 2, 3, 4], [5, 6, 7])
 """
 function convolution(a::Vector{T}, b::Vector{T}) where T
   (length(a) == 0 || length(b) == 0) && return eltype(a)[]
-  r = zeros(eltype(a), length(a) + length(b) - 1)
-  for i in eachindex(r)
-    r[i] = sum(a[k]*b[i-k+1] for k in max(1, i - length(b) + 1):min(i, length(a)))
-  end
-  r
+  return [sum(a[k]*b[i-k+1] for k in max(1, i - length(b) + 1):min(i, length(a))) for i in 1:length(a) + length(b) - 1)]
 end
