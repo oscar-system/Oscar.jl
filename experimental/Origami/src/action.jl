38892433cc6e710a16d77057c5d06d992a19ab90
Nicolas Löw <nicolas.loew009@gmail.com>
2026-05-17 14:40:31 +0200

    added documentation for all functions in action.jl

--- experimental/Origami/src/action.jl
+++ experimental/Origami/src/action.jl
@@ -1,28 +1,100 @@
 # see https://arxiv.org/pdf/1809.10327.pdf Lemma 5.4
 
+@doc raw"""
+    action_s(o::Origami)
+
+For a given origami o this methods computes the origami S.o where S = [0 -1; 1 0] is one of the two generators of SL2(Z). 
+
+# Examples
+```jldoctest
+julia> o = origami(cperm([1,6,4,7,5,3], [2,8]), cperm([1,4,5,3,8,2,6]))
+Origami ((1,6,4,7,5,3)(2,8),(1,4,5,3,8,2,6), 8)
+
+julia> action_s(o)
+Origami ((1,6,2,8,3,5,4),(1,6,4,7,5,3)(2,8), 8)
+```
+"""
 function action_s(o::Origami)
   h = horizontal_perm(o)
   v = vertical_perm(o)
   return origami_disconnected(v^-1, h, degree(o))
 end
 
+@doc raw"""
+    action_t(o::Origami)
+
+For a given origami o this methods computes the origami T.o where T = [1 1; 0 1] is one of the two generators of SL2(Z). 
+
+# Examples
+```jldoctest
+julia> o = origami(cperm([1,6,4,7,5,3], [2,8]), cperm([1,4,5,3,8,2,6]))
+Origami ((1,6,4,7,5,3)(2,8),(1,4,5,3,8,2,6), 8)
+
+julia> action_s_inv(o)
+Origami ((1,4,5,3,8,2,6),(1,3,5,7,4,6)(2,8), 8)
+```
+"""
 function action_t(o::Origami)
   h = horizontal_perm(o)
   return origami_disconnected(h, h^-1 * vertical_perm(o), degree(o))
 end
 
+@doc raw"""
+    action_t_inv(o::Origami)
+
+For a given origami o this methods computes the origami T^-1.o where T = [1 1; 0 1] is one of the two generators of SL2(Z). 
+
+# Examples
+```jldoctest
+julia> o = origami(cperm([1,6,4,7,5,3], [2,8]), cperm([1,4,5,3,8,2,6]))
+Origami ((1,6,4,7,5,3)(2,8),(1,4,5,3,8,2,6), 8)
+
+julia> action_t_inv(o)
+Origami ((1,6,4,7,5,3)(2,8),(3,4,7)(5,8,6), 8)
+```
+"""
 function action_t_inv(o::Origami)
   h = horizontal_perm(o)
   v = vertical_perm(o)
   return origami_disconnected(h, h * v, degree(o))
 end
 
+
+@doc raw"""
+    action_s_inv(o::Origami)
+
+For a given origami o this methods computes the origami S^-1.o where S = [0 -1; 1 0] is one of the two generators of SL2(Z). 
+
+# Examples
+```jldoctest
+julia> o = origami(cperm([1,6,4,7,5,3], [2,8]), cperm([1,4,5,3,8,2,6]))
+Origami ((1,6,4,7,5,3)(2,8),(1,4,5,3,8,2,6), 8)
+
+julia> action_s_inv(o)
+Origami ((1,4,5,3,8,2,6),(1,3,5,7,4,6)(2,8), 8)
+```
+"""
 function action_s_inv(o::Origami)
   h = horizontal_perm(o)
   v = vertical_perm(o)
   return origami_disconnected(v, h^-1, degree(o))
 end
 
+
+@doc raw"""
+    action_sl2(A::ZZMatrix,o::Origami)
+
+This function computes A.o, where A is a Matrix in SL2(Z) and o is an origami.
+
+# Examples
+```jldoctest
+julia> o = origami(cperm([1,6,4,7,5,3], [2,8]), cperm([1,4,5,3,8,2,6]))
+Origami ((1,6,4,7,5,3)(2,8),(1,4,5,3,8,2,6), 8)
+
+julia> action_sl2(ZZ[0 -1; 1 1], o)
+Origami ((1,4,6,8)(5,7),(1,6,4,7,5,3)(2,8), 8)
+```
+"""
 function action_sl2(A::ZZMatrix, o::Origami)
   @req is_one(det(A)) "matrix has to be an element of SL2(Z)"
 
