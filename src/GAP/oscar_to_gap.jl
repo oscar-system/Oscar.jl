06d3b92bc1850c6a9c53ce2b79ac3d0d3dde809a
Morgan Rodgers <morgan.joaquin@gmail.com>
2026-05-19 13:54:37 +0200

    Migration Oscar->GAP.jl

--- src/GAP/oscar_to_gap.jl
+++ src/GAP/oscar_to_gap.jl
@@ -2,31 +2,6 @@
 ## (extends the conversions from GAP.jl's `src/julia_to_gap.jl`,
 ## where low level Julia objects are treated)
 
-## element of cyclotomic field to GAP cyclotomic
-GAP.@install function GapObj(obj::AbsSimpleNumFieldElem)
-    F = parent(obj)
-    @req Nemo.is_cyclo_type(F) "the element does not lie in a cyclotomic field"
-    N = get_attribute(F, :cyclo)
-    v = zeros(QQFieldElem, N)
-    coeffs = coefficients(obj)
-    v[1:length(coeffs)] = coeffs
-    return GAPWrap.CycList(GapObj(v; recursive = true))
-end
-
-## `QQAbFieldElem` to GAP cyclotomic
-GAP.@install function GapObj(elm::QQAbFieldElem)
-    coeffs = [Nemo.coeff(elm.data, i) for i in 0:(elm.c-1)]  # QQFieldElem
-    return GAPWrap.CycList(GapObj(coeffs; recursive = true))
-end
-
-## matrix of elements of cyclotomic field to GAP matrix of cyclotomics
-GAP.@install function GapObj(obj::AbstractAlgebra.Generic.MatSpaceElem{AbsSimpleNumFieldElem})
-    F = base_ring(obj)
-    @req Nemo.is_cyclo_type(F) "the matrix entries do not lie in a cyclotomic field"
-    mat = [GapObj(obj[i,j]) for i in 1:nrows(obj), j in 1:ncols(obj)]
-    return GapObj(mat)
-end
-
 function has_GapObj_with_GapObj(input; recursive::Bool = false)
   try
     res = GapObj(input, recursive = recursive)
