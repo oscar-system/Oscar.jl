2f41e51dcd53b79fab0bea77d25129dca515efed
antonydellavecchia <antonydellavecchia@gmail.com>
2026-05-21 18:44:24 +0200

    fix typo from an earlier PR

--- experimental/AlgebraicStatistics/test/serialization-upgrades.jl
+++ experimental/AlgebraicStatistics/test/serialization-upgrades.jl
@@ -1,5 +1,5 @@
 @testset "Serialization.Upgrades" begin
-  @testset "load Lie GroupBasedPhylogeneticModel serialized wtih 1.7.0" begin
+  @testset "load GroupBasedPhylogeneticModel serialized wtih 1.7.0" begin
     test_upgrade_folder("version_1_7_0";
       only=[
         "GroupBasedPhylogeneticModel",
