37753312447351bb39b3bb122a665145338198a9
Morgan Rodgers <morgan.joaquin@gmail.com>
2026-06-12 17:49:48 +0200

    move more tests

--- test/GAP/oscar_to_gap.jl
+++ test/GAP/oscar_to_gap.jl
@@ -2,13 +2,8 @@
   # from `QQAbFieldElem`
   F, z = abelian_closure(QQ)
   e5 = GAP.Globals.E(5)
+  
   @test GAP.Obj(z(5) + z(5)^4) == e5 + e5^4
-
-  # not supported conversions
-  F, z = quadratic_field(5)
-  @test_throws ArgumentError GAP.Obj(z)
-
-  @test_throws ArgumentError GAP.Obj(matrix(F, 1, 1, [z]))
 end
 
 
