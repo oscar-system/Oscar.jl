5f9b3917e4eaff370fa02a8a69e0aba353bc0730
antonydellavecchia <antonydellavecchia@gmail.com>
2026-06-14 20:20:25 +0200

    more updates for depot_path

--- experimental/Parallel/src/Parallel.jl
+++ experimental/Parallel/src/Parallel.jl
@@ -39,13 +39,9 @@ mutable struct OscarWorkerPool <: AbstractWorkerPool
     wp = WorkerPool(wids)
     # @everywhere can only be used on top-level, so have to do `remotecall_eval` here.
     if !isnothing(project)
+      depot_path = copy(Base.DEPOT_PATH)
       asyncmap(wids) do wid
-        remotecall_eval(Main, wid, :(
-          if haskey(ENV, "JULIA_DEPOT_PATH")
-            empty!(Base.DEPOT_PATH)
-            append!(Base.DEPOT_PATH, split(ENV["JULIA_DEPOT_PATH"], ":"))
-          end
-        ))
+        remotecall_eval(Main, wid, :(empty!(Base.DEPOT_PATH); append!(Base.DEPOT_PATH, $depot_path)))
         remotecall_eval(Main, wid, :(using Pkg; Pkg.activate($project); using Oscar))
         remotecall_eval(Main, wid, init_expr)
       end
