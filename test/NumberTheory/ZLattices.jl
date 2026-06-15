22f89eff4c6d8f0c0a557e36158adfb62b899b2b
Kazak-11 <kazakov.artem2@yandex.ru>
2026-06-15 12:56:35 +0200

    Add test set

--- test/NumberTheory/ZLattices.jl
+++ test/NumberTheory/ZLattices.jl
@@ -8,3 +8,52 @@ end
   @test 7 == length(unique!(Oscar.invariant_function_graph_hash.(L)))
   @test 7 == length(unique!(Oscar.oscar_invariant_function.(L)))
 end 
+@testset "Lattice Canonical Form" begin
+  function create_gram(data)  # helper function to create gram matricies of latticies
+    U = upper_triangular_matrix(data)
+    return U + transpose(U) - diagonal_matrix(diagonal(U))
+  end
+
+  n = 10
+  matrix1 = [2,1,-1,1,1,0,0,0,0,-1,2,-1,0,0,0,0,0,0,-1,2,0,0,0,0,0,0,0,2,1,0,0,0,0,-1,2,0,0,0,0,-1,4,-2,2,2,0,4,-2,-2,0,4,0,0,4,0,4]
+  matrix2 = [2,1,1,-1,-1,1,-1,1,-1,-1,2,0,-1,-1,1,-1,1,-1,0,4,-2,1,2,-2,-1,-1,0,4,1,-2,2,1,2,1,4,1,-1,0,0,1,4,-2,1,-2,-1,4,-1,2,1,4,0,-1,4,1,4]
+  G1 = create_gram(matrix1)
+  G2 = create_gram(matrix2)
+  U = hnf_with_transform(matrix(ZZ,n,n,rand(0:1,n^2)))[2];
+  L1 = integer_lattice(gram = G1);
+  L2 = integer_lattice(gram = G2);
+  L3 = lattice_in_same_ambient_space(L1,U*basis_matrix(L1));
+  @test canonical_form(L1) != canonical_form(L2)
+  @test canonical_form(L1) == canonical_form(L3)
+
+  n = 17
+  matrix1 = [2,1,-1,0,0,0,1,1,0,1,1,0,-1,0,0,-1,-1,2,0,0,0,0,1,1,0,0,1,0,0,0,0,-1,-1,2,0,0,0,-1,-1,0,0,-1,0,1,0,0,1,0,2,0,0,1,1,0,-1,1,0,0,0,-1,0,0,2,0,1,-1,-1,1,-1,0,1,0,0,-1,1,2,1,1,0,-1,-1,1,1,-1,0,-1,1,4,2,-1,0,0,1,1,0,-1,-2,0,4,1,-1,1,1,0,0,-1,-1,-1,2,-1,1,0,0,0,0,1,0,4,-1,0,-1,1,0,0,-1,4,-1,-1,0,0,0,-1,2,0,0,0,0,0,4,-1,0,-1,1,2,0,1,-1,2,0,0,4,0,4]
+  matrix2 = [2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,-1,-1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,-1,-1,1,2,1,1,1,1,1,-1,1,0,0,1,0,1,1,0,2,1,1,1,1,-1,1,-1,1,0,1,0,0,-1,2,1,1,1,-1,1,-1,1,1,1,1,1,-1,2,1,1,-1,1,0,0,0,0,0,0,-1,2,1,-1,1,-1,1,1,0,1,1,-1,2,-1,1,0,0,0,1,0,0,-1,2,-1,1,-1,0,-1,0,0,1,2,-1,1,0,1,0,0,-1,4,-3,-1,-1,-1,-1,2,4,1,1,1,1,-2,4,0,3,3,0,4,-1,0,-1,4,3,0,4,0,4]
+  G1 = create_gram(matrix1)
+  G2 = create_gram(matrix2)
+  U = hnf_with_transform(matrix(ZZ,n,n,rand(0:1,n^2)))[2];
+  L1 = integer_lattice(gram = G1);
+  L2 = integer_lattice(gram = G2);
+  L3 = lattice_in_same_ambient_space(L1,U*basis_matrix(L1));
+  #@test canonical_form(L1) != canonical_form(L2)
+  #test canonical_form(L1) == canonical_form(L3)
+
+  n = 18
+  matrix1 = [4,0,0,2,1,2,2,1,0,0,0,0,0,0,0,0,0,0,4,0,2,3,1,0,2,0,0,0,0,0,0,0,0,0,0,4,2,3,3,1,1,0,0,0,0,0,0,0,0,0,0,4,4,3,2,2,0,0,0,0,0,0,0,0,0,0,6,4,2,3,0,0,0,0,0,0,0,0,0,0,4,2,2,0,0,0,0,0,0,0,0,0,0,2,1,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,4,0,0,2,1,2,2,1,0,0,4,0,2,3,1,0,2,0,0,4,2,3,3,1,1,0,0,4,4,3,2,2,0,0,6,4,2,3,0,0,4,2,2,0,0,2,1,0,0,2,0,0,4,0,8]
+  matrix2 = [2,1,1,1,0,0,0,0,1,1,-1,1,-1,0,-1,1,1,1,2,1,0,0,0,0,0,0,0,-1,1,-1,0,-1,1,0,1,2,0,0,0,0,0,1,1,-1,0,0,0,0,0,1,0,2,0,0,0,0,1,1,0,0,0,0,-1,0,0,1,2,0,0,0,-1,-1,-1,-1,1,0,0,0,0,0,2,1,-1,-1,-1,-1,1,-1,1,-1,-1,-1,1,2,-1,-1,-1,-1,1,-1,0,-1,-1,0,1,2,1,1,0,-1,1,0,0,0,1,-1,4,3,0,-1,1,0,1,1,2,0,4,1,-1,1,0,0,0,1,-1,4,0,1,-1,1,0,-1,-2,4,-3,0,-1,1,0,1,4,0,1,-1,0,-2,2,-1,-1,-1,0,4,1,1,-1,4,1,1,4,0,4]
+  G1 = create_gram(matrix1)
+  G2 = create_gram(matrix2)
+  U = hnf_with_transform(matrix(ZZ,n,n,rand(0:1,n^2)))[2];
+  L1 = integer_lattice(gram = G1);
+  L2 = integer_lattice(gram = G2);
+  L3 = lattice_in_same_ambient_space(L1,U*basis_matrix(L1));
+  @info "first try of long calcs"
+  @test canonical_form(L1) != canonical_form(L2)
+  @info "second try of long calcs"
+  @test canonical_form(L1) != canonical_form(L2)
+  @test canonical_form(L1) == canonical_form(L3)
+  @info "is_isometric_with_isometry 1"
+  @time is_isometric_with_isometry(L1, L2)
+  @info "is_isometric_with_isometry 2"
+  @time is_isometric_with_isometry(L1, L2)
+end
