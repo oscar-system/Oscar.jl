5e4ee4b2a69aeabd08ecac5cb37f701ce228014a
Long-zm123 <longgoforit@163.com>
2026-06-16 12:53:39 +0800

    Add `regular_action_homomorphism` function for a finite group `G`.

--- src/exports.jl
+++ src/exports.jl
@@ -1590,6 +1590,7 @@ export register_morphism!
 export regular_120_cell
 export regular_24_cell
 export regular_600_cell
+export regular_action_homomorphism
 export regular_character
 export regular_triangulation
 export regular_triangulations
