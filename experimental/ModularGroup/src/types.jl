b95c25bca5a7d3dc7cbc575b82447ef0a890ac6f
Sebastian Engelhardt <seen00001@stud.uni-saarland.de>
2026-06-22 01:12:26 +0200

    Now also cache perm_group and coset_action_hom

--- experimental/ModularGroup/src/types.jl
+++ experimental/ModularGroup/src/types.jl
@@ -1,6 +1,8 @@
-struct ModularGroup
+mutable struct ModularGroup
   s::PermGroupElem
   t::PermGroupElem
   r::PermGroupElem
   j::PermGroupElem
+  perm_group_cache::Union{Nothing, PermGroup}
+  coset_action_hom_cache::Union{Nothing, GAPGroupHomomorphism{FPGroup, PermGroup}}
 end
