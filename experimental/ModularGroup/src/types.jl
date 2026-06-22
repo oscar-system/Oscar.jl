f73c8ba7bfe47891db3f7b10df195f00fc97a6e5
Sebastian Engelhardt <seen00001@stud.uni-saarland.de>
2026-06-22 23:18:19 +0200

    (Hopefully) correctly use attributes now

--- experimental/ModularGroup/src/types.jl
+++ experimental/ModularGroup/src/types.jl
@@ -1,8 +1,7 @@
-mutable struct ModularGroup
+@attributes mutable struct ModularGroup
   s::PermGroupElem
   t::PermGroupElem
   r::PermGroupElem
   j::PermGroupElem
-  perm_group_cache::Union{Nothing, PermGroup}
-  coset_action_hom_cache::Union{Nothing, GAPGroupHomomorphism{FPGroup, PermGroup}}
+  ModularGroup(s::PermGroupElem, t::PermGroupElem, r::PermGroupElem, j::PermGroupElem) = new(s, t, r, j)
 end
