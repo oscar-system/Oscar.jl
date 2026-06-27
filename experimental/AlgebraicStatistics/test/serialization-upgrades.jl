b1a6694250532c01c2f73b2e86296c140b4de809
antonydellavecchia <antonydellavecchia@gmail.com>
2026-06-27 10:49:17 +0200

    add upgrade tests for graphical models

--- experimental/AlgebraicStatistics/test/serialization-upgrades.jl
+++ experimental/AlgebraicStatistics/test/serialization-upgrades.jl
@@ -3,16 +3,9 @@
     test_upgrade_folder("version_1_7_0";
       only=[
         "GroupBasedPhylogeneticModel",
+        "PhylogeneticModel",
+        "GaussianGraphicalModel"
       ],
     )
   end
-  # TODO: add PhylogeneticModel and GroupBasedPhylogeneticModel example files to
-  # oscar-system/serialization-upgrade-tests repo under version_1_8_0+1/ before enabling this test
-  # @testset "load PhylogeneticModel and GroupBasedPhylogeneticModel serialized with 1.8.0+1" begin
-  #   test_upgrade_folder("version_1_8_0+1";
-  #     only=[
-  #       "PhylogeneticModel",
-  #     ],
-  #   )
-  # end
 end
