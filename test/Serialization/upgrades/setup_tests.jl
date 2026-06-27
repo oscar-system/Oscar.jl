a9c9e24a6022e911f8458039af845cabb009f828
antonydellavecchia <antonydellavecchia@gmail.com>
2026-06-27 10:11:38 +0200

    update hash for upgrade tests

--- test/Serialization/upgrades/setup_tests.jl
+++ test/Serialization/upgrades/setup_tests.jl
@@ -9,7 +9,7 @@ if !isdefined(Main, :serialization_upgrade_test_path) ||
   !isfile(joinpath(Main.serialization_upgrade_test_path, "LICENSE.md"))
 
 
-  serialization_upgrade_test_path = let commit_hash = "e3473ad614217bc19fad7f809de2d863984be157"
+  serialization_upgrade_test_path = let commit_hash = "5b059efb8a28fbc88d3d0d442ef9fc8549bd6050"
     tarball = Downloads.download("https://github.com/oscar-system/serialization-upgrade-tests/archive/$(commit_hash).tar.gz")
 
     destpath = open(CodecZlib.GzipDecompressorStream, tarball) do io
