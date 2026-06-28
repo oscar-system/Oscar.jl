40abd35d7e720e1c4cfc6693a08ea622df0c8837
Sebastian Engelhardt <seen00001@stud.uni-saarland.de>
2026-06-28 13:39:09 +0200

    Move struct CylinderDiagram to types.jl

--- experimental/Origami/src/generate.jl
+++ experimental/Origami/src/generate.jl
@@ -1,26 +1,5 @@
 # TODO: correct typing
 
-# the bijection between the cycles of the bot/top permutation
-# is given implicitly by using lists and considering the order
-# of the cycles
-struct CylinderDiagram
-  bot::Vector{Vector{Int}}
-  top::Vector{Vector{Int}}
-  cycles_count::Int
-  separatrix_count::Int
-  function CylinderDiagram(bot::Vector{Vector{Int}}, top::Vector{Vector{Int}})
-    max = 1
-    for cycle in bot
-      for i in cycle
-        if i > max
-          max = i
-        end
-      end
-    end
-    new(bot, top, length(bot), max + 1)
-  end
-end
-
 function cylinders(cylinder_diagram::CylinderDiagram)
   result = Vector{Vector{Vector{Int}}}()
   for i in 1:(cylinder_diagram.cycles_count)
--- experimental/Origami/src/types.jl
+++ experimental/Origami/src/types.jl
@@ -11,3 +11,24 @@ struct Normal_stored_origami
   v::PermGroupElem
   g::GAPGroup
 end
+
+# the bijection between the cycles of the bot/top permutation
+# is given implicitly by using lists and considering the order
+# of the cycles
+struct CylinderDiagram
+  bot::Vector{Vector{Int}}
+  top::Vector{Vector{Int}}
+  cycles_count::Int
+  separatrix_count::Int
+  function CylinderDiagram(bot::Vector{Vector{Int}}, top::Vector{Vector{Int}})
+    max = 1
+    for cycle in bot
+      for i in cycle
+        if i > max
+          max = i
+        end
+      end
+    end
+    new(bot, top, length(bot), max + 1)
+  end
+end
