631dd7633e65109d7aeff9351f462fc9078ce9bc
Long-zm123 <longgoforit@163.com>
2026-07-15 10:57:11 +0800

    add Examples of digraph

--- experimental/Digraphs/src/Constructors.jl
+++ experimental/Digraphs/src/Constructors.jl
@@ -1,3 +1,45 @@
+@doc raw"""
+    digraph(adj::Vector{Vector{Int}}; mut::Bool=false) -> Digraph
+    digraph(labels::Vector{<:AbstractString}, source::Vector{<:AbstractString}, range::Vector{<:AbstractString}; mut::Bool=false) -> Digraph
+    digraph(n::Int64, source::Vector{Int}, range::Vector{Int}; mut::Bool=false) -> Digraph
+    digraph(list::Vector{<:Any}, func::Function; mut::Bool=false) -> Digraph
+    digraph(G::T, list::Vector{<:Any}, act::Function, rel::Function; mut::Bool=false) where T<:GAPGroup -> Digraph
+    digraph(obj::GapObj) -> Digraph
+    digraph(name::T) where T <: AbstractString -> Digraph
+
+# Examples
+```jldoctest
+julia> d = digraph([[2, 3], [1, 3], [1, 2]])
+Digraph with 3 vertices, 6 edges
+
+julia> p = ["a", "b", "c"];
+
+julia> d = digraph(p, p[[1, 1, 2, 2, 3, 3]], p[[2, 3, 1, 3, 1, 2]])
+Digraph with 3 vertices, 6 edges
+
+julia> d = digraph(3, [1, 1, 2, 2, 3, 3], [2, 3, 1, 3, 1, 2])
+Digraph with 3 vertices, 6 edges
+
+julia> rel(x, y) = x != y;
+
+julia> d = digraph([1, 2, 3], rel)
+Digraph with 3 vertices, 6 edges
+
+julia> act(x, g) = x^g;
+
+julia> d = digraph(symmetric_group(3), [1, 2, 3], act, rel)
+Digraph with 3 vertices, 6 edges
+
+julia> d = digraph("Diamond")
+Digraph with 4 vertices, 10 edges
+
+julia> d = GAP.Globals.CompleteGraph(GapObj(symmetric_group(3)))
+GAP: rec( adjacencies := [ [ 2, 3 ] ], group := Sym( [ 1 .. 3 ] ), isGraph := true, isSimple := true, order := 3, representatives := [ 1 ], schreierVector := [ -1, 1, 1 ] )
+
+julia> digraph(d)
+Digraph with 3 vertices, 6 edges
+```
+"""
 function digraph(adj::Vector{Vector{Int}}; mut::Bool=false)
   filter = mut ? GAP.Globals.IsMutableDigraph : GAP.Globals.IsImmutableDigraph
   d = DigraphWrap.Digraph(filter, GapObj(adj, recursive = true))
@@ -22,7 +64,7 @@ function digraph(list::Vector{<:Any}, func::Function; mut::Bool=false)
   return Digraph(d)
 end
 
-function digraph(G::GAPGroup, list::Vector{<:Any}, act::Function, rel::Function)
+function digraph(G::T, list::Vector{<:Any}, act::Function, rel::Function; mut::Bool=false) where T<:GAPGroup
   filter = mut ? GAP.Globals.IsMutableDigraph : GAP.Globals.IsImmutableDigraph
   d = DigraphWrap.Digraph(filter, GapObj(G), GapObj(list, recursive = true), GapObj(act), GapObj(rel))
   return Digraph(d)
