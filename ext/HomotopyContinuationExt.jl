e639d1c1c657ffcdb236d25a55fb575206315347
Oskar Henriksson <oskar.henriksson@mpi-cbg.de>
2026-08-18 00:28:57 +0200

    Use keywords for solving instead of expression formation

--- ext/HomotopyContinuationExt.jl
+++ ext/HomotopyContinuationExt.jl
@@ -68,7 +68,7 @@ System(I::Vector{QQMPolyRingElem}; args...) = System(Expression.(I); args...)
 System(I::MPolyIdeal{QQMPolyRingElem}; args...) = System(gens(I); args...)
 
 function Oscar.solve_numerical(I::Vector{QQMPolyRingElem}; show_progress=false, threading=false, args...)
-  return HomotopyContinuation.solve(System(Expression.(I), args...), show_progress=show_progress, threading=threading)
+  return HomotopyContinuation.solve(System(I); show_progress=show_progress, threading=threading, args...)
 end
                                     
 function Oscar.solve_numerical(I::MPolyIdeal{QQMPolyRingElem}; args...)
