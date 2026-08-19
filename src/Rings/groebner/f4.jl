6f23114280bf54d711d3d65adac82e1ee92cba04
ederc <ederc@mathematik.uni-kl.de>
2026-08-19 18:55:38 +0200

    Update src/Rings/groebner/f4.jl
    Co-authored-by: Benjamin Lorenz <benlorenz@users.noreply.github.com>

--- src/Rings/groebner/f4.jl
+++ src/Rings/groebner/f4.jl
@@ -58,7 +58,7 @@ function groebner_basis_f4(
         truncate_lifting::Int=0
         )
 
-    info_level = get_verbosity_level(:AlgebraicSolving) > 2 ? 2 : get_verbosity_level(:AlgebraicSolving)
+    info_level = min(2, get_verbosity_level(:AlgebraicSolving))
 
     AI   = AlgebraicSolving.Ideal(oscar_generators(I))
     if eliminate == 0
