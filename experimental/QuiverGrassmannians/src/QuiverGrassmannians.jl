b70c08f22d3f69ca7e5ef8e6cd1894545a5abc75
Dante <danteluber@gmail.com>
2026-08-19 13:38:10 +0200

    add check that matrix entries can be coerced into specified field

--- experimental/QuiverGrassmannians/src/QuiverGrassmannians.jl
+++ experimental/QuiverGrassmannians/src/QuiverGrassmannians.jl
@@ -34,7 +34,12 @@ struct QuiverRepresentation{C <: FieldElem}
     function QuiverRepresentation(quiver::Graph{Directed}, ambient_dims::Vector{Int}, input_matrices::Vector, base_field)
         @req n_vertices(quiver) == length(ambient_dims) "each vertex needs an ambient dimension"
         @req n_edges(quiver) == length(input_matrices) "each edge needs a linear map"
-        #add check that base_ring(first(A)) is subring of base_field
+        K = base_ring(first(input_matrices))
+        try
+            base_field(K(1))
+        catch
+            error("The matrix entries cannot be coerced into the specified ring")
+        end
         check_matrix_dimensions(quiver, ambient_dims, input_matrices)
         input_matrices = [matrix(base_field,A) for A in input_matrices] #allows user to 
         vertex_vector_spaces = [vector_space(base_field,ambient_dims[i]) for i in vertices(quiver)]
