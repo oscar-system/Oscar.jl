42dbffbd58b3c3f83dd475e67c618f110b43a7ab
Tobias Braun <tobias.braun2@rwth-aachen.de>
2026-09-04 17:30:44 +0200

    Empty conversion data + tests

--- experimental/ActionPolyRing/test/Conversion.jl
+++ experimental/ActionPolyRing/test/Conversion.jl
@@ -2,6 +2,7 @@
   
   __algebraic_conversion_data = Oscar.__algebraic_conversion_data
   __jtv = Oscar.__jtv
+  __eov = Oscar.__extract_occ_vars
 
   @testset "Difference to algebraic" begin
     dpr, (u, v) = difference_polynomial_ring(QQ, [:u, :v], 2)
@@ -26,6 +27,16 @@
       @test ngens(conv.mpr) == 3
     end
 
+    @testset "Empty conversion data" begin
+      conv = __algebraic_conversion_data(dpr, elem_type(dpr)[])
+
+      @test conv(conv(dpr(0))) == dpr(0)
+      @test conv(conv(dpr(17))) == dpr(17)
+      @test ngens(conv.mpr) == 0
+      @test_throws ArgumentError conv(u)
+      @test_throws ArgumentError conv(v)
+    end
+
     @testset "Forward and Backward mappings" begin
       conv = __algebraic_conversion_data(dpr, [u_0, u_x, u_y, v_x])
       
@@ -47,7 +58,6 @@
 
     @testset "Error handling" begin
       conv = __algebraic_conversion_data(dpr, [u_x, u_y])
-      
       @test_throws ArgumentError conv(u_x^2 + v_y)
       
       dpr2, u2 = difference_polynomial_ring(QQ, :u, 2)
@@ -57,12 +67,12 @@
       @test_throws ArgumentError conv(x + y)
       
       _ = dpr[1, [10, 10]]
-        
       @test_throws ArgumentError conv(u_x)
         
       mpr_gen = gen(conv.mpr, 1)
       @test_throws ArgumentError conv(gen(conv.mpr, 1))
     end
+    
     @testset "Variable ordering (revsorted)" begin
       conv_sorted = __algebraic_conversion_data(dpr, [u_0, u_y, u_x])
       @test u_0 < u_y < u_x
@@ -74,6 +84,27 @@
       @test conv_unsorted(u_0) == gen(conv_unsorted.mpr, 1)
       @test conv_unsorted(u_y) == gen(conv_unsorted.mpr, 2)
       @test conv_unsorted(u_x) == gen(conv_unsorted.mpr, 3)
+    end
+    
+    @testset "Extracting variables" begin
+      p = v_y^2 + v_x - u_0
+      q = v_x * u_x - u_y
+      
+      @test_throws ArgumentError __algebraic_conversion_data(dpr, [p, q])
+      conv = __algebraic_conversion_data(dpr, __eov([p, q]))
+      @test conv.supported_vars == Set(union(vars(p), vars(q)))
+      @test u_0 < v_y < u_y < v_x < u_x
+
+      @test conv(u_0) == gen(conv.mpr, 5)
+      @test conv(v_y) == gen(conv.mpr, 4)
+      @test conv(u_y) == gen(conv.mpr, 3)
+      @test conv(v_x) == gen(conv.mpr, 2)
+      @test conv(u_x) == gen(conv.mpr, 1)
+      @test_throws ArgumentError conv(v[0, 0])
+
+      @test __algebraic_conversion_data(dpr, __eov([p])).supported_vars == Set(vars(p))
+      @test __algebraic_conversion_data(dpr, __eov([q])).supported_vars == Set(vars(q))
+      @test_throws ArgumentError __algebraic_conversion_data(dpr, __eov(elem_type(dpr)[]))
     end
   end
   
@@ -99,6 +130,16 @@
       @test conv.supported_vars == Set([u_x, u_y, v_x])
       @test ngens(conv.mpr) == 3
     end
+    
+    @testset "Empty conversion data" begin
+      conv = __algebraic_conversion_data(dpr, elem_type(dpr)[])
+
+      @test conv(conv(dpr(0))) == dpr(0)
+      @test conv(conv(dpr(17))) == dpr(17)
+      @test ngens(conv.mpr) == 0
+      @test_throws ArgumentError conv(u)
+      @test_throws ArgumentError conv(v)
+    end
 
     @testset "Forward and Backward mappings" begin
       conv = __algebraic_conversion_data(dpr, [u_0, u_x, u_y, v_x])
@@ -121,7 +162,6 @@
 
     @testset "Error handling" begin
       conv = __algebraic_conversion_data(dpr, [u_x, u_y])
-      
       @test_throws ArgumentError conv(u_x^2 + v_y)
       
       dpr2, u2 = differential_polynomial_ring(QQ, :u, 2)
@@ -131,12 +171,12 @@
       @test_throws ArgumentError conv(x + y)
       
       _ = dpr[1, [10, 10]]
-        
       @test_throws ArgumentError conv(u_x)
         
       mpr_gen = gen(conv.mpr, 1)
       @test_throws ArgumentError conv(gen(conv.mpr, 1))
     end
+    
     @testset "Variable ordering (revsorted)" begin
       conv_sorted = __algebraic_conversion_data(dpr, [u_0, u_y, u_x])
       @test u_0 < u_y < u_x
@@ -148,6 +188,27 @@
       @test conv_unsorted(u_0) == gen(conv_unsorted.mpr, 1)
       @test conv_unsorted(u_y) == gen(conv_unsorted.mpr, 2)
       @test conv_unsorted(u_x) == gen(conv_unsorted.mpr, 3)
+    end
+    
+    @testset "Extracting variables" begin
+      p = v_y^2 + v_x - u_0
+      q = v_x * u_x - u_y
+      
+      @test_throws ArgumentError __algebraic_conversion_data(dpr, [p, q])
+      conv = __algebraic_conversion_data(dpr, __eov([p, q]))
+      @test conv.supported_vars == Set(union(vars(p), vars(q)))
+      @test u_0 < v_y < u_y < v_x < u_x
+
+      @test conv(u_0) == gen(conv.mpr, 5)
+      @test conv(v_y) == gen(conv.mpr, 4)
+      @test conv(u_y) == gen(conv.mpr, 3)
+      @test conv(v_x) == gen(conv.mpr, 2)
+      @test conv(u_x) == gen(conv.mpr, 1)
+      @test_throws ArgumentError conv(v[0, 0])
+
+      @test __algebraic_conversion_data(dpr, __eov([p])).supported_vars == Set(vars(p))
+      @test __algebraic_conversion_data(dpr, __eov([q])).supported_vars == Set(vars(q))
+      @test_throws ArgumentError __algebraic_conversion_data(dpr, __eov(elem_type(dpr)[]))
     end
   end
 end
