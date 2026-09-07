ef698b24de9206ada7e8e6787b1edf4d5748bd99
ederc <ederc@mathematik.uni-kl.de>
2026-09-07 12:35:17 +0200

    adds uov signed message opening to tests

--- test/Cryptography/UOV/runtests.jl
+++ test/Cryptography/UOV/runtests.jl
@@ -6,24 +6,24 @@
   @test isdefined(Oscar, :uov_5)
 
   # Test parameter values
-  @test Oscar.uov_1p.gf == 256
-  @test Oscar.uov_1p.n == 112
-  @test Oscar.uov_1p.m == 44
+  @test uov_1p.gf == 256
+  @test uov_1p.n == 112
+  @test uov_1p.m == 44
 
-  @test Oscar.uov_1s.gf == 16
-  @test Oscar.uov_1s.n == 160
-  @test Oscar.uov_1s.m == 64
+  @test uov_1s.gf == 16
+  @test uov_1s.n == 160
+  @test uov_1s.m == 64
 
-  @test Oscar.uov_3.gf == 256
-  @test Oscar.uov_3.n == 184
-  @test Oscar.uov_3.m == 72
+  @test uov_3.gf == 256
+  @test uov_3.n == 184
+  @test uov_3.m == 72
 
-  @test Oscar.uov_5.gf == 256
-  @test Oscar.uov_5.n == 244
-  @test Oscar.uov_5.m == 96
+  @test uov_5.gf == 256
+  @test uov_5.n == 244
+  @test uov_5.m == 96
 
   # Test all variants exist
-  @test length(Oscar.uov_all) == 12
+  @test length(uov_all) == 12
 end
 
 @testset "uov functions exported" begin
@@ -39,41 +39,50 @@ end
   msg = rand(UInt8, 32)
 
   # Test uov_1p (and its pkc/skc variants)
-  for uov in (Oscar.uov_1p, Oscar.uov_1p_pkc, Oscar.uov_1p_pkc_skc)
-      pk, sk = Oscar.keygen(uov)
-      sig = Oscar.sign(uov, msg, sk)
+  for uov in (uov_1p, uov_1p_pkc, uov_1p_pkc_skc)
+      pk, sk = keygen(uov)
+      sig = sign(uov, msg, sk)
       @test length(sig) == uov.sig_sz
-      @test Oscar.verify(uov, sig, msg, pk)
+      @test verify(uov, sig, msg, pk)
   end
 
   # Test uov_1s (GF(16) field)
-  for uov in (Oscar.uov_1s, Oscar.uov_1s_pkc, Oscar.uov_1s_pkc_skc)
-      pk, sk = Oscar.keygen(uov)
-      sig = Oscar.sign(uov, msg, sk)
+  for uov in (uov_1s, uov_1s_pkc, uov_1s_pkc_skc)
+      pk, sk = keygen(uov)
+      sig = sign(uov, msg, sk)
       @test length(sig) == uov.sig_sz
-      @test Oscar.verify(uov, sig, msg, pk)
+      @test verify(uov, sig, msg, pk)
   end
 
   # Test uov_3
-  for uov in (Oscar.uov_3, Oscar.uov_3_pkc, Oscar.uov_3_pkc_skc)
-      pk, sk = Oscar.keygen(uov)
-      sig = Oscar.sign(uov, msg, sk)
+  for uov in (uov_3, uov_3_pkc, uov_3_pkc_skc)
+      pk, sk = keygen(uov)
+      sig = sign(uov, msg, sk)
       @test length(sig) == uov.sig_sz
-      @test Oscar.verify(uov, sig, msg, pk)
+      @test verify(uov, sig, msg, pk)
   end
 
   # Test uov_5 (largest parameter set)
-  for uov in (Oscar.uov_5, Oscar.uov_5_pkc, Oscar.uov_5_pkc_skc)
-      pk, sk = Oscar.keygen(uov)
-      sig = Oscar.sign(uov, msg, sk)
+  for uov in (uov_5, uov_5_pkc, uov_5_pkc_skc)
+      pk, sk = keygen(uov)
+      sig = sign(uov, msg, sk)
       @test length(sig) == uov.sig_sz
-      @test Oscar.verify(uov, sig, msg, pk)
+      @test verify(uov, sig, msg, pk)
   end
 
   # Test that verification rejects a tampered signature
-  pk, sk = Oscar.keygen(Oscar.uov_1p)
-  sig = Oscar.sign(Oscar.uov_1p, msg, sk)
+  pk, sk = keygen(uov_1p)
+  sig = sign(uov_1p, msg, sk)
   sig_bad = copy(sig)
   sig_bad[1] = sig_bad[1] ⊻ 0xFF
-  @test !Oscar.verify(Oscar.uov_1p, sig_bad, msg, pk)
+  @test !verify(uov_1p, sig_bad, msg, pk)
+end
+
+@testset "uov message opening" begin
+  # Test opening of a signed message
+  uov = uov_1p
+  pk, sk = keygen(uov)
+  msg = UInt8[1, 2, 3]
+  sm = vcat(msg, sign(uov, msg, sk))
+  uov_open(uov, sm, pk)
 end
